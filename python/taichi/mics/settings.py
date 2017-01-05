911562b9b5292a123d6926a9457cf0583a76659b
Yuanming Hu <iterator.advance@gmail.com>
2017-01-06 01:15:32 +0800

    TAICHI_ROOT

--- python/taichi/mics/settings.py
+++ python/taichi/mics/settings.py
@@ -7,8 +7,16 @@ def get_num_cores():
     return os.environ.get('TAICHI_NUM_THREADS', default_num_threads)
 
 
+def get_root_directory():
+    return os.environ['TAICHI_ROOT_DIR'] + '/'
+
+
+def get_bin_directory():
+    return os.environ.get('TAICHI_BIN_DIR', get_root_directory() + '/taichi/build/')
+
+
 def get_output_directory():
-    return os.environ.get('TAICHI_OUTPUT_DIR', '../taichi_output')
+    return os.environ.get('TAICHI_OUTPUT_DIR', get_root_directory() + '/taichi_outputs/')
 
 
 def get_output_path(path):
@@ -16,7 +24,7 @@ def get_output_path(path):
 
 
 def get_asset_directory():
-    return os.environ.get('TAICHI_ASSET_DIR', '../taichi_asset')
+    return os.environ.get('TAICHI_ASSET_DIR', get_root_directory() + '/taichi_assets/')
 
 
 def get_asset_path(path):

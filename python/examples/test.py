45a935a24a00aa51a55138f79891278ce0d7846d
Yuanming Hu <iterator.advance@gmail.com>
2017-01-27 00:46:43 +0800

    Fixed some warnings in dcraw

--- python/examples/test.py
+++ python/examples/test.py
@@ -4,6 +4,9 @@ from taichi.gui.image_viewer import show_image
 
 tc.core.test()
 
+tex = tc.Texture('const', value=(0.1, 0.1, 0.1, 0.1))
+tex = tex.flip(0)
+
 try:
     tc.core.test_raise_error()
 except Exception as e:
@@ -12,5 +15,6 @@ except Exception as e:
 img = tc.util.imread(tc.get_asset_path('textures/vegas.jpg'))
 show_image('img', img)
 time.sleep(1)
+
 
 print 'Testing finished.'

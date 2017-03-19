afae5699dbc643ac50cd773b4e2c1b2d42cc62d9
Yuanming Hu <iterator.advance@gmail.com>
2017-03-18 17:33:53 -0700

    Fixed initialization of LevelSet 2D

--- include/taichi/math/levelset_2d.h
+++ include/taichi/math/levelset_2d.h
@@ -6,17 +6,20 @@
 
 TC_NAMESPACE_BEGIN
 
-class LevelSet2D : public Array2D<float> {
+class LevelSet2D : public Array2D<real> {
 public:
-    float friction = 1.0f;
+    real friction = 1.0f;
     LevelSet2D() : LevelSet2D(0, 0) {}
     LevelSet2D(int width, int height, Vector2 offset=Vector2(0.5f, 0.5f)) {
         initialize(width, height, offset);
     }
     void initialize(int width, int height, Vector2 offset) {
-        Array2D<float>::initialize(width, height, 1, offset);
+        Array2D<real>::initialize(width, height, std::numeric_limits<real>::infinity(), offset);
     }
-    void add_sphere(Vector2 center, float radius, bool inside_out = false);
+    void initialize(int width, int height, Vector2 offset, real value) {
+        Array2D<real>::initialize(width, height, value, offset);
+    }
+    void add_sphere(Vector2 center, real radius, bool inside_out = false);
 
     void add_polygon(std::vector<Vector2> polygon, bool inside_out = false);
 
@@ -24,11 +27,11 @@ public:
 
     Vector2 get_normalized_gradient(const Vector2 &pos) const;
 
-    static float fraction_outside(float phi_a, float phi_b) {
+    static real fraction_outside(real phi_a, real phi_b) {
         return 1.0f - fraction_inside(phi_a, phi_b);
     }
 
-    static float fraction_inside(float phi_a, float phi_b) {
+    static real fraction_inside(real phi_a, real phi_b) {
         if (phi_a < 0 && phi_b < 0)
             return 1;
         if (phi_a < 0 && phi_b >= 0)
@@ -39,7 +42,7 @@ public:
             return 0;
     }
 
-    Array2D<float> rasterize(int width, int height);
+    Array2D<real> rasterize(int width, int height);
 };
 
 

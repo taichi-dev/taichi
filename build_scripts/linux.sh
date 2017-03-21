39bffcf25fff790284273765f4730c594f2fa406
Yuanming Hu <iterator.advance@gmail.com>
2017-03-21 23:09:50 +0800

    OSX Travis Support

---
+++ build_scripts/linux.sh
@@ -0,0 +1,12 @@
+apt-get install cmake libtbb-dev alien dpkg-dev debhelper freeglut3-dev python-scipy python-tk python-imaging-tk python-pip
+pip install numpy future futures watchdog Pillow scipy pybind11
+wget https://github.com/embree/embree/releases/download/v2.13.0/embree-2.13.0.x86_64.rpm.tar.gz
+tar xzf embree-2.13.0.x86_64.rpm.tar.gz
+alien embree-lib-2.13.0-1.x86_64.rpm
+alien embree-devel-2.13.0-1.x86_64.rpm
+alien embree-examples-2.13.0-1.x86_64.rpm
+dpkg -i embree-lib_2.13.0-2_amd64.deb
+dpkg -i embree-devel_2.13.0-2_amd64.deb
+dpkg -i embree-examples_2.13.0-2_amd64.deb
+export CXX="g++-6" CC="gcc-6"
+cmake ..

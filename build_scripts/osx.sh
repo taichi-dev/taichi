3f7c3698e011e93b7d67de2327538269c3acf0e6
Yuanming Hu <iterator.advance@gmail.com>
2017-03-22 00:12:18 +0800

    OSX CI fix

--- build_scripts/osx.sh
+++ build_scripts/osx.sh
@@ -1,7 +1,7 @@
-brew install cmake tbb freeglut
 pip install numpy future futures watchdog Pillow scipy pybind11
 wget https://github.com/embree/embree/releases/download/v2.14.0/embree-2.14.0.x86_64.macosx.tar.gz
 tar -xzf embree-2.14.0.x86_64.macosx.tar.gz
 source embree-2.14.0.x86_64.macosx/embree-vars.sh
-cmake .. -DEMBREE_LIBRARY=embree-2.14.0.x86_64.macosx/lib/libembree.2.dylib -DEMBREE_INCLUDE_PATH:PATH=embree-2.14.0.x86_64.macosx/include
+cp embree-2.14.0.x86_64.macosx/lib/*.dylib .
+cmake .. -DEMBREE_INCLUDE_PATH:PATH=embree-2.14.0.x86_64.macosx/include -DEMBREE_LIBRARY=libembree.2.dylib
 make -j4

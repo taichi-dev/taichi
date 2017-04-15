5a0f515574d0df3a0ab950166ba30db624a41136
Yuanming Hu <iterator.advance@gmail.com>
2017-04-15 16:07:02 +0800

    Stiffness limited shooting

--- python/examples/simulation/2d/ampm/shoot.py
+++ python/examples/simulation/2d/ampm/shoot.py
@@ -5,21 +5,23 @@ import taichi as tc
 if __name__ == '__main__':
     scale = 64
     res = (scale, scale)
-    frame_dt = 1e-2
+    frame_dt = 1e-3
     async = True
+    bullet = True
     gravity = (0, 0)
     if async:
-        simulator = MPMSimulator(res=res, simulation_time=15, frame_dt=frame_dt, base_delta_t=1e-5, async=True,
-                                 maximum_delta_t=1e-2, debug_input=(32, 5, 0, 0), cfl=1.0, gravity=gravity)
+        simulator = MPMSimulator(res=res, simulation_time=0.3, frame_dt=frame_dt, base_delta_t=1e-6, async=True,
+                                 maximum_delta_t=2e-1, debug_input=(128, 7, 0, 0), cfl=0.1, gravity=gravity)
     else:
-        simulator = MPMSimulator(res=res, simulation_time=15, frame_dt=frame_dt, base_delta_t=1e-4, async=False,
+        simulator = MPMSimulator(res=res, simulation_time=0.3, frame_dt=frame_dt, base_delta_t=5e-6, async=False,
                                  maximum_delta_t=1e-1, debug_input=(1024, 4, 0, 0), gravity=gravity)
 
 
     def event(s):
-        s.add_particles_sphere(Vector(0.2, 0.5), 0.05, 'ep',
-                               compression=0.4, velocity=Vector(50, 0),
-                               theta_c=1, theta_s=1)
+        if bullet:
+            s.add_particles_sphere(Vector(0.2, 0.5), 0.03, 'ep',
+                                   compression=0.1, velocity=Vector(50, 0), lambda_0=1e10, mu_0=1e10,
+                                   theta_c=1, theta_s=1)
 
         center_x, center_y = 0.5, 0.5
         w, h = 0.05, 0.35
@@ -39,4 +41,4 @@ if __name__ == '__main__':
     levelset.add_polygon([(bd, bd), (1 - bd, bd), (1 - bd, 1 - bd), (bd, 1 - bd)], True)
     simulator.set_levelset(levelset)
     window = SimulationWindow(512, simulator, color_schemes['snow'], levelset_supersampling=2, show_images=True,
-                              substep=async, video_output=True, need_press=False)
+                              substep=False, video_output=False, need_press=False)

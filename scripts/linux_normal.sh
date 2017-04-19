119bf6681859b227f4cb4c3970b6bd75d7128da3
Yuanming Hu <iterator.advance@gmail.com>
2017-04-19 15:11:46 +0800

    Fixed builds?

--- scripts/linux_normal.sh
+++ scripts/linux_normal.sh
@@ -1,6 +1,3 @@
-mkdir build
-cd build
-sudo ../scripts/linux_prereq.sh
 export CXX="g++-6" CC="gcc-6"
 cmake .. -DEMBREE_INCLUDE_PATH:PATH=/usr/include -DEMBREE_LIBRARY=/usr/lib64/libembree.so
 make -j4

29dafe6c30af8efbf1a9f1b44ad2458cc382aaac
Yuanming Hu <iterator.advance@gmail.com>
2017-04-30 22:45:20 +0800

    Added python/examples/rendering/demo_read_cpp_particles.py

---
+++ src/tests/test_volumetric_io.cpp
@@ -0,0 +1,32 @@
+/*******************************************************************************
+    Taichi - Physically based Computer Graphics Library
+
+    Copyright (c) 2016 Yuanming Hu <yuanmhu@gmail.com>
+
+    All rights reserved. Use of this source code is governed by
+    the MIT license as written in the LICENSE file.
+*******************************************************************************/
+
+#include <taichi/io/io.h>
+#include <taichi/visualization/particle_visualization.h>
+
+TC_NAMESPACE_BEGIN
+
+void test_volumetric_io() {
+    const int nx = 128, ny = 128, nz = 64, step = 16;
+    std::vector<RenderParticle> particles;
+    Vector3 center = Vector3(nx, ny, nz) * 0.5f;
+    for (int i = 0; i < nx; i++) {
+        for (int j = 0; j < ny; j++) {
+            for (int k = 0; k < nz; k++) {
+                int flag = (i / step) + (j / step) + (k / step);
+                if (flag % 2 == 0) {
+                    particles.push_back(RenderParticle(Vector3(i, j, k) - center, Vector3(1, 1, 1)));
+                }
+            }
+        }
+    }
+    write_vector_to_disk(&particles, "particles.bin");
+}
+
+TC_NAMESPACE_END

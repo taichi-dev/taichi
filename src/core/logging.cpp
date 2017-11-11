cc9ce1536b10f18aef324f5051764fd4bedc51f6
Yuanming Hu <yuanmhu@gmail.com>
2017-11-11 00:35:55 -0500

    signal handlers

--- src/core/logging.cpp
+++ src/core/logging.cpp
@@ -8,9 +8,38 @@
 *******************************************************************************/
 
 #include <taichi/common/logging.h>
+#include <taichi/system/threading.h>
+#include <signal.h>
 
 TC_NAMESPACE_BEGIN
 
 std::shared_ptr<spdlog::logger> console = spdlog::stdout_color_mt("console");
+
+void handler(int signo) {
+  TC_ERR("Received signal {} ({})", signo, strsignal(signo));
+  TC_FLUSH_LOGGER;
+  taichi::print_traceback();
+  std::exit(-1);
+}
+
+#define TC_REGISTER_SIGNAL_HANDLER(name, handler)                    \
+  {                                                                  \
+    if (signal(name, handler) == SIG_ERR)                            \
+      std::printf("Can not register signal handler for" #name "\n"); \
+  }
+
+class OnStartUp {
+ public:
+  OnStartUp() {
+    TC_REGISTER_SIGNAL_HANDLER(SIGSEGV, handler);
+    TC_REGISTER_SIGNAL_HANDLER(SIGABRT, handler);
+    TC_REGISTER_SIGNAL_HANDLER(SIGBUS, handler);
+    TC_REGISTER_SIGNAL_HANDLER(SIGFPE, handler);
+    spdlog::set_level(spdlog::level::trace);
+    TC_TRACE("Taichi core started. Thread ID={}", PID::get_pid());
+  }
+};
+
+static OnStartUp on_start_up;
 
 TC_NAMESPACE_END

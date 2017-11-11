f59957b22a4edb239ba82cd49999c2de872c3bd7
Yuanming Hu <yuanmhu@gmail.com>
2017-11-11 00:43:47 -0500

    file structures

---
+++ include/taichi/geometry/mesh.h
@@ -0,0 +1,42 @@
+/*******************************************************************************
+    Taichi - Physically based Computer Graphics Library
+
+    Copyright (c) 2017 Yuanming Hu <yuanmhu@gmail.com>
+
+    All rights reserved. Use of this source code is governed by
+    the MIT license as written in the LICENSE file.
+*******************************************************************************/
+
+#pragma once
+
+#include <memory>
+#include <vector>
+#include <taichi/math.h>
+
+TC_NAMESPACE_BEGIN
+
+template <int dim>
+struct Element {
+  using Vector = VectorND<dim, real>;
+  using Vectori = VectorND<dim, int>;
+  using Matrix = MatrixND<dim, real>;
+  using MatrixP = MatrixND<dim + 1, real>;
+
+  Vector v[dim];
+  bool open_end[dim];
+
+  Element() {
+    for (int i = 0; i < dim; i++) {
+      v[i] = Vector(0.0_f);
+      open_end[i] = false;
+    }
+  }
+};
+
+template <int dim>
+struct ElementMesh {
+  using Elem = Element<dim>;
+  std::vector<Elem> elements;
+};
+
+TC_NAMESPACE_END
---
+++ include/taichi/math/angular.h
@@ -0,0 +1,142 @@
+/*******************************************************************************
+    Taichi - Physically based Computer Graphics Library
+
+    Copyright (c) 2017 Yuanming Hu <yuanmhu@gmail.com>
+
+    All rights reserved. Use of this source code is governed by
+    the MIT license as written in the LICENSE file.
+*******************************************************************************/
+
+#pragma once
+
+#include <taichi/util.h>
+#include <taichi/math/math.h>
+#include <taichi/math/vector.h>
+#include <Eigen/Geometry>
+#include <type_traits>
+
+TC_NAMESPACE_BEGIN
+
+template <int dim>
+class AngularVelocity {
+ public:
+  using Vector = VectorND<dim, real>;
+  using ValueType = typename std::conditional_t<dim == 2, real, Vector>;
+
+  ValueType value;
+
+  AngularVelocity() {
+    value = ValueType(0.0_f);
+  }
+  AngularVelocity(const ValueType &value) : value(value){};
+
+  AngularVelocity &operator+=(const AngularVelocity &o) {
+    *this = *this + o;
+    return *this;
+  }
+
+  AngularVelocity operator+(const AngularVelocity &o) const {
+    return AngularVelocity(value + o.value);
+  }
+
+  AngularVelocity operator*(real val) const {
+    return AngularVelocity(value * val);
+  }
+
+  Vector cross(const Vector &input) const {
+    Vector ret;
+    TC_STATIC_IF(dim == 2) {
+      ret = Vector(-input.y, input.x) * value;
+    }
+    TC_STATIC_ELSE {
+      ret = cross(value, input);
+    }
+    TC_STATIC_END_IF;
+    return ret;
+  }
+};
+
+template <int dim>
+class Rotation {
+ public:
+  using Vector = VectorND<dim, real>;
+  using Matrix = MatrixND<dim, real>;
+  using AngVel = AngularVelocity<dim>;
+  using ValueType =
+      typename std::conditional_t<dim == 2, real, Eigen::Quaternion<real>>;
+
+  ValueType value;
+
+  Rotation() {
+    TC_STATIC_IF(dim == 2) {
+      value = 0;
+    }
+    TC_STATIC_ELSE {
+      value = Eigen::Quaternion<real>(1, 0, 0, 0);
+    }
+    TC_STATIC_END_IF
+    return;
+  }
+
+  Rotation(real value) {  // initialize
+    // according to dim
+    TC_STATIC_IF(dim == 2) {
+      this->value = value;
+    }
+    TC_STATIC_ELSE {
+      this->value = Eigen::Quaternion<real>(1, 0, 0, 0);
+    }
+    TC_STATIC_END_IF
+    return;
+  }
+
+  Rotation operator-() const {
+    Rotation ret;
+    TC_STATIC_IF(dim == 2) {
+      ret = Rotation(-value);
+    }
+    TC_STATIC_ELSE{TC_NOT_IMPLEMENTED} TC_STATIC_END_IF return ret;
+  }
+
+  Matrix get_rotation_matrix() const {
+    Matrix ret;
+    TC_STATIC_IF(dim == 2) {
+      ret[0][0] = std::cos(value);
+      ret[1][0] = -std::sin(value);
+      ret[0][1] = std::sin(value);
+      ret[1][1] = std::cos(value);
+    }
+    TC_STATIC_ELSE {
+      auto mat = value.toRotationMatrix();
+      for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+          ret[i][j] = mat(j, i);
+        }
+      }
+    }
+    TC_STATIC_END_IF
+    return ret;
+  }
+
+  void apply_angular_velocity(AngVel &vel, real dt) {
+    TC_STATIC_IF(dim == 2) {
+      value += dt * vel.value;
+    }
+    TC_STATIC_ELSE {
+      real ot = length(vel.value) * dt;
+      real s = std::sin(ot / 2);
+      real c = std::cos(ot / 2);
+      Eigen::Quaternion<real> omega_t(c, s * vel.value[0], s * vel.value[1],
+                                      s * vel.value[2]);
+      value = omega_t * value;
+    }
+    TC_STATIC_END_IF
+    return;
+  }
+
+  Vector rotate(const Vector &vector) const {
+    return get_rotation_matrix() * vector;
+  }
+};
+
+TC_NAMESPACE_END

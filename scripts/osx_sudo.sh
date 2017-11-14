ddd5ec4270639b2f684edc49992e7b8f21e32518
Yuanming Hu <yuanmhu@gmail.com>
2017-11-14 00:50:36 -0500

    removed python package installation in CI

--- scripts/osx_sudo.sh
+++ scripts/osx_sudo.sh
@@ -1,4 +1,3 @@
-pip install numpy future futures watchdog Pillow scipy pybind11
 wget https://github.com/embree/embree/releases/download/v2.14.0/embree-2.14.0.x86_64.macosx.tar.gz
 tar -xzf embree-2.14.0.x86_64.macosx.tar.gz
 source embree-2.14.0.x86_64.macosx/embree-vars.sh

ca43092d3a004598b6d1a13fdd3d2dee7a7117bc
Yuanming Hu <yuanmhu@gmail.com>
2017-11-17 22:04:00 -0500

    pgm code

--- include/taichi/visualization/image_buffer.cpp
+++ include/taichi/visualization/image_buffer.cpp
@@ -187,4 +187,18 @@ template void Array2D<Vector3>::write_as_image(const std::string &filename);
 
 template void Array2D<Vector4>::write_as_image(const std::string &filename);
 
+void write_pgm(Array2D<real> img, const std::string &fn) {
+  std::ofstream fs(fn, std::ios_base::binary);
+  Vector2i res = img.get_res();
+  fs << fmt::format("P5\n{} {}\n{}\n", res[0], res[1], 255);
+  for (int j = 0; j < res[1]; j++) {
+    std::string line;
+    for (int i = 0; i < res[0]; i++) {
+      uint8_t v = clamp((int)(img[i][res[1] - j - 1] * 255), 0, 255);
+      line.push_back(v);
+    }
+    fs.write(line.c_str(), line.size());
+  }
+}
+
 TC_NAMESPACE_END

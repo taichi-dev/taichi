488942e11269fb4058bd11c0e610f53a5d96e95d
Yuanming Hu <yuanmhu@gmail.com>
2017-12-02 01:54:27 -0500

    element transforms

--- include/taichi/geometry/mesh.h
+++ include/taichi/geometry/mesh.h
@@ -117,4 +117,49 @@ struct ElementMesh {
   }
 };
 
+TC_FORCE_INLINE real distance_to_segment(const Vector2 &pos,
+                                         const Vector2 &a,
+                                         const Vector2 &b) {
+  real t = dot(pos - a, b - a) / length2(b - a);
+  // t = clamp(t, 0.0_f, 1.0_f);
+  if (0.0_f <= t && t <= 1.0_f) {
+    return length(pos - lerp(t, a, b));
+  } else {
+    return 1e20_f;
+  }
+}
+
+TC_FORCE_INLINE real distance_to_triangle(const Vector3 &pos,
+                                     const Element<3> &tri) {
+  Vector3 normal = tri.get_normal();
+  real height = dot(normal, (pos - tri.v[0]));
+
+  const Vector3 inter_local = pos - normal * height - tri.v[0];
+  const Vector3 u = tri.v[1] - tri.v[0], v = tri.v[2] - tri.v[0];
+  real uv = dot(u, v), vv = dot(v, v), wu = dot(inter_local, u), uu = dot(u, u),
+       wv = dot(inter_local, v);
+  real dom = uv * uv - uu * vv;
+  real coord_u = (uv * wv - vv * wu) / dom;
+  real coord_v = (uv * wu - uu * wv) / dom;
+
+  if (coord_u >= 0 && coord_v >= 0 && coord_u + coord_v <= 1)
+    return std::abs(height);
+  return 1e20_f;
+}
+
+// Note: assuming world origin aligns with elem.v[0]
+TC_FORCE_INLINE Matrix2 world_to_element(const Element<2> &elem) {
+  Vector2 v = elem.v[1] - elem.v[0];
+  Vector2 n = normalized(Vector2(v.y, -v.x));
+  return inversed(Matrix2(v, n));
+}
+
+// Note: assuming world origin aligns with elem.v[0]
+TC_FORCE_INLINE Matrix3 world_to_element(const Element<3> &elem) {
+  Vector3 u = elem.v[1] - elem.v[0];
+  Vector3 v = elem.v[2] - elem.v[0];
+  Vector3 n = normalized(cross(u, v));
+  return inversed(Matrix3(u, v, n));
+}
+
 TC_NAMESPACE_END

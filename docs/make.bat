ab262e6490326c3f9ebab79e33f5b15682b0d250
Yuanming Hu <yuanmhu@gmail.com>
2018-03-01 17:39:22 -0500

    docs folder structure

--- docs/make.bat
+++ docs/make.bat
@@ -7,7 +7,7 @@ REM Command file for Sphinx documentation
 if "%SPHINXBUILD%" == "" (
 	set SPHINXBUILD=sphinx-build
 )
-set SOURCEDIR=source
+set SOURCEDIR=.
 set BUILDDIR=build
 set SPHINXPROJ=taichi
 

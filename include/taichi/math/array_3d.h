b97c232de75f99d75453902c2267b876b4589079
Yuanming Hu <yuanmhu@gmail.com>
2018-04-17 22:13:20 -0400

    TArray

--- include/taichi/math/array_3d.h
+++ include/taichi/math/array_3d.h
@@ -701,6 +701,9 @@ class ArrayND<3, T> {
 template <typename T>
 using Array3D = ArrayND<3, T>;
 
+template <int dim, typename T>
+using TArray = ArrayND<dim, T>;
+
 template <typename T>
 void print(const Array3D<T> &arr) {
   arr.print("");

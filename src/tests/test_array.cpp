a57a89abfced5c3c620af0dc4eeab960c90ff713
Yuanming Hu <yuanmhu@gmail.com>
2018-08-04 13:27:24 -0400

    Removed useless files

--- src/tests/test_array.cpp
+++ src/tests/test_array.cpp
@@ -6,19 +6,16 @@
 #include <taichi/common/util.h>
 #include <taichi/common/task.h>
 #include <taichi/math/array.h>
+#include <taichi/testing.h>
 
 TC_NAMESPACE_BEGIN
 
-class TestArray : public Task {
-  virtual void run(const std::vector<std::string> &parameters) {
-    using Array = Array2D<real>;
-    Array A(Vector2i(5, 6));
-    Array B(A);
-    TC_P(A.get_res());
-    TC_P(B.get_res());
-  }
-};
-
-TC_IMPLEMENTATION(Task, TestArray, "test_array")
+TC_TEST("array") {
+  using Array = Array2D<real>;
+  Array A(Vector2i(5, 6));
+  Array B(A);
+  TC_P(A.get_res());
+  TC_P(B.get_res());
+}
 
 TC_NAMESPACE_END

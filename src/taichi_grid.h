232acbfb04a690359d0c6c5793524b21fd9012c8
Yuanming Hu <yuanmhu@gmail.com>
2018-08-06 17:45:45 -0400

    moved REPEAT27 to main lib

--- src/taichi_grid.h
+++ src/taichi_grid.h
@@ -493,35 +493,6 @@ struct TAncestors {
   }
 };
 
-#define TC_REPEAT27(F) \
-  F(0);                \
-  F(1);                \
-  F(2);                \
-  F(3);                \
-  F(4);                \
-  F(5);                \
-  F(6);                \
-  F(7);                \
-  F(8);                \
-  F(9);                \
-  F(10);               \
-  F(11);               \
-  F(12);               \
-  F(13);               \
-  F(14);               \
-  F(15);               \
-  F(16);               \
-  F(17);               \
-  F(18);               \
-  F(19);               \
-  F(20);               \
-  F(21);               \
-  F(22);               \
-  F(23);               \
-  F(24);               \
-  F(25);               \
-  F(26);
-
 // If ComponentType is void, gather the whole node.
 template <typename Block, typename ComponentType = void>
 struct TGridScratchPad {

51d0b124ae7287eab3fbce7a31f77a3feb8af9e7
Yuanming Hu <yuanmhu@gmail.com>
2018-11-25 20:19:12 -0500

    ABC seems working

--- src/sound.cpp
+++ src/sound.cpp
@@ -5,11 +5,11 @@
 
 TC_NAMESPACE_BEGIN
 
-constexpr int n = 30;
+constexpr int n = 100;
 constexpr real room_size = 10.0_f;
 constexpr real dx = room_size / n;
 constexpr real c = 340;
-constexpr real alpha = 0.001;
+constexpr real alpha = 0.00000;
 
 Array3D<real> p, q, r;
 
@@ -18,7 +18,7 @@ void advance(real dt) {
   std::swap(q.data, r.data);
   r.reset_zero();
   constexpr real inv_dx2 = pow<2>(1.0_f / dx);
-  for (int i = 1; i < n - 1; i++) {
+  for (int i = 1; i < n - 2; i++) {
     for (int j = 1; j < n - 1; j++) {
       for (int k = 1; k < n - 1; k++) {
         real laplacian_p =
@@ -33,6 +33,29 @@ void advance(real dt) {
         r[i][j][k] = 2 * q[i][j][k] +
                      (c * c * dt * dt + c * alpha * dt) * laplacian_q -
                      p[i][j][k] - c * alpha * dt * laplacian_p;
+      }
+    }
+  }
+
+  auto lambda = c * dt / dx;
+  auto inv_lambda = 1.0_f / lambda;
+  for (int i = n - 2; i < n - 1; i++) {
+    for (int j = 1; j < n - 1; j++) {
+      for (int k = 1; k < n - 1; k++) {
+        auto scale = lambda * lambda / (1 + 2 * lambda);
+        r[i][j][k] =
+            scale *
+            ((2 * inv_lambda * inv_lambda + 4 * inv_lambda - 6 - 4 * lambda) *
+                 q[i][j][k] -
+             (inv_lambda * inv_lambda + 2 * inv_lambda) * p[i][j][k] +
+             p[i + 1][j][k] - p[i - 1][j][k] + 2 * q[i - 1][j][k] +
+             (1 + lambda) * (q[i][j + 1][k] + q[i][j - 1][k] + q[i][j][k + 1] +
+                             q[i][j][k - 1]));
+        q[i + 1][j][k] =
+            p[i + 1][j][k] + q[i - 1][j][k] - p[i - 1][j][k] -
+            2 * inv_lambda * (r[i][j][k] + p[i][j][k] - 2 * q[i][j][k]) +
+            lambda * (q[i][j + 1][k] + q[i][j - 1][k] + q[i][j][k + 1] +
+                      q[i][j][k - 1] - 4 * q[i][j][k]);
       }
     }
   }
@@ -51,13 +74,13 @@ auto sound = []() {
   dt = 1_f / 44100;
   TC_P(dt);
 
-  r[n / 2][n / 2][n / 2] = 1;
 
   for (int T = 0; T < 1000; T++) {
     for (int i = 0; i < 5; i++) {
       real left, right;
       fscanf(f, "%f%f", &left, &right);
       advance(dt);
+      r[n / 2][n / 2][n / 2] = std::sin(std::min(t * 2000, 12 * pi));
       // r[n / 4][n / 2] = (left + right) / 65536.0;
       t += dt;
       wav_file.add_sound(dt, r[n / 4 + 4][n / 2][n / 2]);

4ff63b4bab00bc815db6af592fc09c083e9217e6
Yuanming Hu <yuanmhu@gmail.com>
2018-12-28 01:04:16 -0500

    fixed clear buffer position

--- lang/mpm.cpp
+++ lang/mpm.cpp
@@ -64,7 +64,6 @@ auto mpm = []() {
   auto p2g = prog.def([&]() {
     auto index = Expr::index(0);
     for_loop(index, {0, n_particles}, [&] {
-
       auto x = particle_x[index];
       auto v = particle_v[index];
       // auto F = particle_F[index];
@@ -95,51 +94,6 @@ auto mpm = []() {
         }
       }
 
-      // ** gs = 1
-      /*
-      auto offset =
-          cast<int32>(offset_x) * imm(n) + cast<int32>(offset_y) * imm(1);
-
-      auto clamp = [](const Expr &e) {
-        return min(max(imm(2), e), imm(n - 2));
-      };
-
-      // weights
-      auto w00 = (imm(1.0f) - wx) * (imm(1.0f) - wy);
-      auto w01 = (imm(1.0f) - wx) * wy;
-      auto w10 = wx * (imm(1.0f) - wy);
-      auto w11 = wx * wy;
-
-      w00.name("w00");
-      w01.name("w01");
-      w10.name("w10");
-      w11.name("w11");
-
-      Expr node = max(Expr::index(0) + offset, imm(0));
-      Int32 i = clamp(node >> imm((int)bit::log2int(n))).name("i");  // node / n
-      // Int32 i = clamp(node / imm(n)).name("i"); // node / n
-      Int32 j = clamp(node & imm(n - 1)).name("j");  // node % n
-      // Int32 j = clamp(node % imm(n)).name("j"); // node % n
-      node = i * imm(n) + j;
-      node.name("node");
-
-      if (use_adapter) {
-        prog.adapter(2).set(1, 4).convert(w00, w01, w10, w11);
-        prog.adapter(3).set(1, 4).convert(node);
-      }
-
-      // ** gs = 4
-      for (int k = 0; k < nattr; k++) {
-        auto v00 = attr[0][k][node + imm(0)].name("v00");
-        auto v01 = attr[0][k][node + imm(1)].name("v01");
-        auto v10 = attr[0][k][node + imm(n)].name("v10");
-        auto v11 = attr[0][k][node + imm(n + 1)].name("v11");
-
-        attr[1][k][index] = w00 * v00 + w01 * v01 + w10 * v10 + w11 * v11;
-        // attr[1][k][index] = w00 * v00;
-        attr[1][k][index].name(fmt::format("output{}", k));
-      }
-      */
     });
   });
 
@@ -193,11 +147,11 @@ auto mpm = []() {
                                0.5;
         }
       }
+      prog.clear_buffer(1);
     }
 
     gui.update();
     // gui.screenshot(fmt::format("images/{:04d}.png", f));
-    prog.clear_buffer(1);
   }
 };
 TC_REGISTER_TASK(mpm);

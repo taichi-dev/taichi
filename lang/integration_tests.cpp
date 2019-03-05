96c51bd87c67060239d1c0fcdaac0ba0f1eca976
Yuanming Hu <yuanmhu@gmail.com>
2019-03-05 18:10:22 -0500

    simd mpm works for one particle...

--- lang/integration_tests.cpp
+++ lang/integration_tests.cpp
@@ -49,6 +49,7 @@ TC_ALIGNED(64) const static __m128 grid_pos_offset_[27] = {
 // clang-format on
 
 struct MPMContext {
+ public:
   struct Particle {
     Vector3 pos, v;
     Matrix3 C;
@@ -57,7 +58,7 @@ struct MPMContext {
     Particle() {
       pos = Vector3::rand() * 0.5_f + Vector3(0.25_f);
       v = Vector3::rand();
-      C = Matrix3::rand();
+      // C = Matrix3::rand();
       J = 1.0_f * rand() + 1;
     }
   };
@@ -69,7 +70,8 @@ struct MPMContext {
   static constexpr real dx = 1.0_f / n;
   static constexpr real dt = 1e-3_f;
 
-  Vector4 grid[n][n][n];
+  using Grid = Vector4[n][n][n];
+  Grid grid;
 
   void clear_grid() {
     std::memset(grid, 0, sizeof(grid));
@@ -81,13 +83,14 @@ struct MPMContext {
     particles.resize(n_particles);
     for (int i = 0; i < n_particles; i++)
       particles[i] = Particle();
+    clear_grid();
   }
 
   void p2g() {
     const auto inv_dx = 1.0_f / dx;
-    for (int i = 0; i < n_particles; i++) {
+    for (int p_i = 0; p_i < n_particles; p_i++) {
       using Vec = Vector3;
-      auto &p = particles[i];
+      auto &p = particles[p_i];
       Vector3i base_coord = (p.pos * inv_dx - Vector3(0.5_f)).cast<int>();
       auto fx = p.pos * inv_dx - base_coord.cast<real>();
       // Quadratic kernels  [http://mpm.graphics   Eqn. 123, with x=fx,
@@ -106,7 +109,7 @@ struct MPMContext {
             auto dpos = (Vec(i, j, k) - fx) * dx;
             Vector4 mv(p.v * mass, mass);  // translational momentum
             grid[base_coord.x + i][base_coord.y + j][base_coord.z + k] +=
-                w[i].x * w[j].y * (mv + Vector4(affine * dpos, 0));
+                w[i].x * w[j].y * w[k].z * (mv + Vector4(affine * dpos, 0));
           }
         }
       }
@@ -159,6 +162,8 @@ struct MPMContext {
       Vectori grid_base_pos(get_stencil_start(pos_[0]),
                             get_stencil_start(pos_[1]),
                             get_stencil_start(pos_[2]));
+      auto base_offset =
+          grid_base_pos[0] * n * n + grid_base_pos[1] * n + grid_base_pos[2];
       Vector grid_base_pos_f = Vector(grid_base_pos);
 
       MLSMPMFastKernel32 kernel(_mm_sub_ps(pos_, grid_base_pos_f), inv_delta_x);
@@ -169,10 +174,10 @@ struct MPMContext {
       const __m128 v = p.v.v;
       __m128 mass_ = _mm_set1_ps(mass);
       // Note, apic_b has delta_x issue
-      const Matrix3f apic_b_inv_d_mass = p.C * (4.0_f * mass);
+      const Matrix3f apic_b_inv_d_mass = -mass * dx * p.C;
       const __m128 mass_v = _mm_mul_ps(_mm_set1_ps(mass), v);
 
-      auto stress = Matrix3(p.J - 1);
+      auto stress = dt * vol * Matrix3(p.J - 1);
 
       __m128 delta_t_tmp_force_[3];
       Matrix3 &delta_t_tmp_force =
@@ -186,20 +191,20 @@ struct MPMContext {
 
       for (int i = 0; i < 3; i++)
         affine[i] = _mm_fmadd_ps(stress[i], S, apic_b_inv_d_mass[i]);
-#define LOOP(node_id)                                                        \
-  {                                                                          \
-    __m128 dpos = _mm_sub_ps(rela_pos, grid_pos_offset_[node_id]);           \
-    __m128 weight =                                                          \
-        _mm_set1_ps(kernels[node_id / 9][node_id / 3 % 3][node_id % 3]);     \
-    __m128 affine_prod = _mm_fmadd_ps(                                       \
-        affine[2], broadcast(dpos, 2),                                       \
-        _mm_fmadd_ps(affine[1], broadcast(dpos, 1),                          \
-                     _mm_fmadd_ps(affine[0], broadcast(dpos, 0), mass_v)));  \
-    __m128 contrib = _mm_blend_ps(mass_, affine_prod, 0x7);                  \
-    __m128 delta = _mm_mul_ps(weight, contrib);                              \
-    auto &g =                                                                \
-        grid[0][0][node_id / 9 * n * n + node_id / 3 % 3 * n + node_id % 3]; \
-    g = _mm_add_ps(g, delta);                                                \
+#define LOOP(node_id)                                                       \
+  {                                                                         \
+    __m128 dpos = _mm_sub_ps(rela_pos, grid_pos_offset_[node_id]);          \
+    __m128 weight =                                                         \
+        _mm_set1_ps(kernels[node_id / 9][node_id / 3 % 3][node_id % 3]);    \
+    __m128 affine_prod = _mm_fmadd_ps(                                      \
+        affine[2], broadcast(dpos, 2),                                      \
+        _mm_fmadd_ps(affine[1], broadcast(dpos, 1),                         \
+                     _mm_fmadd_ps(affine[0], broadcast(dpos, 0), mass_v))); \
+    __m128 contrib = _mm_blend_ps(mass_, affine_prod, 0x7);                 \
+    __m128 delta = _mm_mul_ps(weight, contrib);                             \
+    auto &g = grid[0][0][node_id / 9 * n * n + node_id / 3 % 3 * n +        \
+                         node_id % 3 + base_offset];                        \
+    g = _mm_add_ps(g, delta);                                               \
   }
       TC_REPEAT27(LOOP);
 #undef LOOP
@@ -209,8 +214,25 @@ struct MPMContext {
 };
 
 TC_TEST("simd_mpm") {
-  MPMContext context(16);
-  context.p2g();
+  while (1) {
+    MPMContext context(1);
+    context.p2g();
+
+    MPMContext::Grid grid_gt = context.grid;
+    context.clear_grid();
+    context.p2g_intrinsics();
+
+    for (int i = 0; i < context.n; i++) {
+      for (int j = 0; j < context.n; j++) {
+        for (int k = 0; k < context.n; k++) {
+          for (int d = 0; d < 4; d++) {
+            // TC_INFO("{} {} {} {} , {}", i, j, k, d, grid_gt[i][j][k][d]);
+            TC_CHECK_EQUAL(grid_gt[i][j][k][d], context.grid[i][j][k][d], 1e-3_f);
+          }
+        }
+      }
+    }
+  }
 
   /*
   CoreState::set_trigger_gdb_when_crash(true);

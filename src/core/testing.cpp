86bbf2f95e776994e960440a2602d742295fe8d3
Yuanming Hu <yuanmhu@gmail.com>
2019-03-06 23:51:09 -0500

    Fixed windows build (testing.cpp)

--- src/core/testing.cpp
+++ src/core/testing.cpp
@@ -10,13 +10,13 @@ TC_NAMESPACE_BEGIN
 
 int run_tests(std::vector<std::string> argv) {
   char arg[] = "test";
-  char *argv_[1 + argv.size()];
+  std::vector<char *> argv_(1 + argv.size(), nullptr);
   argv_[0] = arg;
   for (int i = 0; i < (int)argv.size(); i++) {
     argv_[i + 1] = const_cast<char *>(argv[i].c_str());
   }
   Catch::Session session;
-  int returnCode = session.applyCommandLine(1 + argv.size(), argv_);
+  int returnCode = session.applyCommandLine(1 + argv.size(), argv_.data());
   if (returnCode != 0)
     return returnCode;
   return session.run();

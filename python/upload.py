d085f6ebf3508d5ecca7c350fb6364a34aac7ad9
Yuanming Hu <yuanmhu@gmail.com>
2019-03-10 16:28:11 -0400

    include built binaries in the package

--- python/upload.py
+++ python/upload.py
@@ -1,5 +1,9 @@
 import os
 
+os.makedirs('lib', exist_ok=True)
+os.system('cp ../build/libtaichi_core.so lib')
+os.system('cp ../external/lib/* lib')
+
 os.system('python3 -m pip install --user --upgrade twine setuptools wheel')
 os.system('python3 setup.py sdist bdist_wheel')
 os.system('python3 -m twine upload --repository-url https://test.pypi.org/legacy/ dist/* --verbose')

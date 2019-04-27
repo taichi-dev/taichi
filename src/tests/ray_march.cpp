dbc52336f1aaad58d21d9dde9d2bdc6c7659cec0
Yuanming Hu <yuanmhu@gmail.com>
2019-04-27 00:13:45 -0400

    simplify ray_march

--- src/tests/ray_march.cpp
+++ src/tests/ray_march.cpp
@@ -9,11 +9,11 @@ auto ray_march = [] {
   Program prog(Arch::gpu);
   prog.config.print_ir = true;
 
-  Global(color_r, f32);
-  Global(color_g, f32);
-  Global(color_b, f32);
+  Vector buffer(DataType::f32, 3);
 
-  layout([&]() { root.dense(0, n * n * 2).place(color_r, color_g, color_b); });
+  layout([&]() {
+    root.dense(0, n * n * 2).place(buffer(0), buffer(1), buffer(2));
+  });
 
   auto sdf = [&](Vector p_) {
     float alpha = -0.7f;
@@ -24,10 +24,8 @@ auto ray_march = [] {
 
     auto dist_sphere = p.norm() - 0.5_f;
     auto dist_walls = min(p(2) + 6.0f, p(1) + 1.0f);
-    Vector d(3);
-    d(0) = abs(p(0) - 1.0_f) - 0.3_f;
-    d(1) = abs(p(1) + 0.5_f) - 1.2_f;
-    d(2) = abs(p(2) - 1.0_f) - 0.2_f;
+    Vector d =
+        Var(abs(p + Vector({-1.0f, 0.5f, -1.0f})) - Vector({0.3f, 1.2f, 0.2f}));
     auto dist_cube = norm(d.map([](const Expr &v) { return max(v, 0.0f); })) +
                      min(max(max(d(0), d(1)), d(2)), 0.0_f);
     return min(dist_sphere, min(dist_walls, dist_cube));
@@ -84,9 +82,9 @@ auto ray_march = [] {
     For(0, n * n * 2, [&](Expr i) {
       auto orig = Var(Vector({0.0f, 0.0f, 12.0f}));
 
-      auto c = Var(Vector(
-          {fov * (cast<float>(i / n) / float(n / 2) - 2.0f),
-           fov * (cast<float>(i % n) / float(n / 2) - 1.0f), Expr(-1.0f)}));
+      auto c = Var(
+          Vector({fov * (cast<float>(i / n) / float(n / 2) - 2.0f),
+                  fov * (cast<float>(i % n) / float(n / 2) - 1.0f), -1.0f}));
 
       c = normalized(c);
 
@@ -112,9 +110,7 @@ auto ray_march = [] {
             });
       });
 
-      color_r[i] += color(0);
-      color_g[i] += color(1);
-      color_b[i] += color(2);
+      buffer[i] += color;
     });
   });
 
@@ -130,9 +126,9 @@ auto ray_march = [] {
     real scale = 1.0_f / ((frame + 1) * N);
     for (int i = 0; i < n * n * 2; i++) {
       gui.buffer[i / n][i % n] =
-          Vector4(tone_map(scale * color_r.val<float>(i)),
-                  tone_map(scale * color_g.val<float>(i)),
-                  tone_map(scale * color_b.val<float>(i)), 1);
+          Vector4(tone_map(scale * buffer(0).val<float>(i)),
+                  tone_map(scale * buffer(1).val<float>(i)),
+                  tone_map(scale * buffer(2).val<float>(i)), 1);
     }
     gui.update();
   }

6bd428f9f0057fd8825b4e65ec4a10a75ab103aa
Yuanming Hu <yuanmhu@gmail.com>
2019-04-27 00:57:58 -0400

    volume integration renderer

--- src/tests/volume_renderer.cpp
+++ src/tests/volume_renderer.cpp
@@ -19,55 +19,36 @@ auto volume_renderer = [] {
   prog.config.print_ir = true;
 
   Vector buffer(DataType::f32, 3);
+  Global(density, f32);
 
   layout([&]() {
-    root.dense(0, n * n * 2).place(buffer(0), buffer(1), buffer(2));
+    root.dense(Index(0), n * n * 2).place(buffer(0), buffer(1), buffer(2));
+    root.dense(Indices(0, 1, 2), grid_resolution).place(density);
   });
 
-  // The signed distance field of the geometry
-  auto sdf = [&](Vector q) {
-    float32 alpha = -0.7f;
-    auto p =
-        Vector({(cos(alpha) * q(0) + sin(alpha) * q(2) + 1.0f) % 2.0f - 1.0f,
-                q(1), -sin(alpha) * q(0) + cos(alpha) * q(2)});
-
-    auto dist_sphere = p.norm() - 0.5f;
-    auto dist_walls = min(p(2) + 6.0f, p(1) + 1.0f);
-    Vector d =
-        Var(abs(p + Vector({-1.0f, 0.5f, -1.0f})) - Vector({0.3f, 1.2f, 0.2f}));
-
-    auto dist_cube = norm(d.map([](const Expr &v) { return max(v, 0.0f); })) +
-                     min(max(max(d(0), d(1)), d(2)), 0.0f);
-
-    return min(dist_sphere, min(dist_walls, dist_cube));
-  };
-
-  float32 eps = 1e-5f;
-  float32 dist_limit = 1e2;
-  int limit = 200;
-
-  // shoot a ray
-  auto ray_march = [&](Vector p, Vector dir) {
-    auto j = Var(0);
-    auto dist = Var(0.0f);
-    While(j < limit && sdf(p + dist * dir) > eps && dist < dist_limit, [&] {
-      dist += sdf(p + dist * dir);
-      j += 1;
+  // If p is in the density field return the density, other 0
+  auto query_density = [&](Vector p) {
+    auto inside_box = Var(0.0f <= p(0) && p(0) < 1.0f && 0.0f <= p(1) &&
+                          p(1) < 1.0f && 0.0f <= p(2) && p(2) < 1.0f);
+    auto ret = Var(0.0f);
+    If(inside_box).Then([&] {
+      auto i = floor(p(0) * float32(grid_resolution));
+      auto j = floor(p(1) * float32(grid_resolution));
+      auto k = floor(p(2) * float32(grid_resolution));
+      ret = density[i, j, k];
     });
-    return dist;
+    return ret;
   };
 
   auto background = [](Vector dir) {
     return 1.0f * max(dir(1) + dir(0), 0.0f);
   };
 
-  float32 fov = 0.3;
+  float32 fov = 0.7;
 
   Kernel(main).def([&]() {
-    Parallelize(8);
-    Vectorize(8);
     For(0, n * n * 2, [&](Expr i) {
-      auto orig = Var(Vector({0.0f, 0.0f, 12.0f}));
+      auto orig = Var(Vector({0.5f, 0.3f, 1.5f}));
 
       auto c = Var(Vector({fov * (cast<float32>(i / n) / float32(n / 2) - 2.0f),
                            fov * (cast<float32>(i % n) / float32(n / 2) - 1.0f),
@@ -77,19 +58,29 @@ auto volume_renderer = [] {
 
       auto color = Var(Vector({1.0f, 1.0f, 1.0f}));
 
-      auto dist = Var(ray_march(orig, c));
-      If(dist < dist_limit).Then([&] {
-        color = color * (dist / (dist + 1.0f));
+      For(0, 200, [&](Expr k) {
+        auto p = Var(orig + c * ((cast<float32>(k) + Rand<float32>()) * 0.01f));
+        color *= (1.0_f - query_density(p) * 0.1f);
       });
 
       buffer[i] += color;
     });
   });
 
+  for (int i = 0; i < grid_resolution; i++) {
+    for (int j = 0; j < grid_resolution; j++) {
+      for (int k = 0; k < grid_resolution; k++) {
+        density.val<float32>(i, j, k) =
+            density_field[i * grid_resolution * grid_resolution +
+                          j * grid_resolution + k];
+      }
+    }
+  }
+
   GUI gui("Volume Renderer", Vector2i(n * 2, n));
 
   auto tone_map = [](real x) { return x; };
-  constexpr int N = 100;
+  constexpr int N = 10;
   for (int frame = 0;; frame++) {
     for (int i = 0; i < N; i++)
       main();

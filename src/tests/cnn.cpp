5627c32746639459c0823c406af038357d5a6455
Yuanming Hu <yuanmhu@gmail.com>
2019-05-01 13:34:23 -0400

    cnn visualization

--- src/tests/cnn.cpp
+++ src/tests/cnn.cpp
@@ -16,6 +16,8 @@ auto cnn = []() {
   constexpr int dim = 3;
   constexpr int n = 256;
 
+  constexpr int num_ch1 = 4, num_ch2 = 4;
+
   Global(layer1, f32);
   Global(layer2, f32);
   Global(weights, f32);
@@ -23,7 +25,8 @@ auto cnn = []() {
   layout([&]() {
     auto ijkl = Indices(0, 1, 2, 3);
     root.dense(ijkl, {n, n, n, 4}).place(layer1);
-    root.dense(ijkl, {3, 3, 3, 16}).place(weights);
+    root.dense(ijkl, {n, n, n, 4}).place(layer2);
+    root.dense(ijkl, {4, 4, 4, 16}).place(weights);
   });
 
   Kernel(forward).def([&] {
@@ -32,6 +35,41 @@ auto cnn = []() {
       // layer1[]
     });
   });
+
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      for (int k = 0; k < n; k++) {
+        auto len = (Vector3i(i, j, k) - Vector3i(n / 2)).length() / (float32)n;
+        if (0.4 < len && len < 0.5) {
+          layer1.val<float32>(i, j, k, 0) = len;
+        }
+      }
+    }
+  }
+
+  int gui_res = 512;
+  GUI gui("FEM", Vector2i(gui_res + 200, gui_res), false);
+  int layer = 1;
+  int k = 0;
+  int channel = 0;
+  gui.slider("z", k, 0, n - 1).slider("Layer", layer, 1, 2);
+
+  int scale = gui_res / n;
+  auto &canvas = gui.get_canvas();
+  for (int frame = 1;; frame++) {
+    for (int i = 0; i < gui_res - scale; i++) {
+      for (int j = 0; j < gui_res - scale; j++) {
+        real dx;
+        if (layer == 1) {
+          dx = layer1.val<float32>(i / scale, j / scale, k, channel);
+        } else {
+          dx = layer2.val<float32>(i / scale, j / scale, k, channel);
+        }
+        canvas.img[i][j] = Vector4(0.5f) + Vector4(dx) * 0.5f;
+      }
+    }
+    gui.update();
+  }
 };
 TC_REGISTER_TASK(cnn);
 

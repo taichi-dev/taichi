032c250c72919d32396cd19a5aed25a243bce69d
Tzu-Mao Li <Bachi722@gmail.com>
2019-05-16 11:10:50 -0400

    cnn compare

---
+++ cnn_compare/generate_bin.py
@@ -0,0 +1,26 @@
+import pyoctnet
+import numpy as np
+import argparse
+import os
+
+def main(path, res, channels):
+    pad = 2
+    n_threads = 1
+    R = np.identity(3, dtype = np.float32)
+
+    grid = pyoctnet.Octree.create_from_off(path, res,res,res, R, pad=pad, n_threads=n_threads)
+    dense = grid.to_cdhw()
+    dense = np.tile(np.reshape(dense, (1, 1, res, res, res)), (1, channels, 1, 1, 1))
+    print('Size: {}x{}x{}x{}'.format(channels, res, res, res))
+    out_path = os.path.splitext(os.path.basename(path))[0] + '.bin'
+    pyoctnet.write_dense(out_path, dense)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('model_paths', type = str, nargs = '+')
+    parser.add_argument('-r', '--res', type = int, default = 64)
+    parser.add_argument('-c', '--channels', type = int, default = 20)
+
+    args = parser.parse_args()
+    for m in args.model_paths:
+        main(m, args.res, args.channels)
---
+++ cnn_compare/test_mn10.lua
@@ -0,0 +1,45 @@
+#!/usr/bin/env th
+
+local common = dofile('common.lua')
+require('os')
+require('nn')
+require('cunn')
+require('cudnn')
+package.path = '/home/tzumao/octnet/th/?/init.lua;' .. package.path
+require('oc')
+
+local n_grids = 4096 -- Parallelization granularity
+net = nn.Sequential()
+  :add( oc.OctreeConvolutionMM(20, 26, n_grids) )
+
+local s = net:get(1).weight:size()
+local inc = 0.0
+for co = 1, s[1] do
+    for ci = 1, s[2] do
+        for i = 1, s[3] do
+            for j = 1, s[4] do
+                for k = 1, s[5] do
+                    net:get(1).weight[co][ci][i][j][k] = inc
+                    inc = inc + 0.1
+                end
+            end
+        end
+    end
+end
+net:cuda()
+
+vx_res = 256
+channels = 20
+local tensor = torch.FloatTensor(1, channels, vx_res, vx_res, vx_res)
+oc.read_dense_from_bin(arg[1], tensor)
+print(torch.nonzero(tensor):size(1))
+local input_cpu = oc.FloatOctree()
+input_cpu = input_cpu:create_from_dense(tensor)
+local input = input_cpu:cuda()
+
+local output = net:forward(input)
+local timer = torch.Timer()
+for i = 1, 20 do
+    output = net:forward(input)
+end
+print(string.format('[INFO] net fwd took %f[s]', timer:time().real / 20.0))

62389f6f1a3dae878d5867344f9f346dc9a34b4a
Yuanming Hu <yuanmhu@gmail.com>
2019-06-07 11:10:51 -0700

    mpm BC

--- python/mpm.py
+++ python/mpm.py
@@ -30,11 +30,13 @@ def mat():
 x, v = vec(), vec()
 grid_v = vec()
 grid_m = scalar()
+C = mat()
+J = scalar()
 
 
 @ti.layout
 def place():
-  ti.root.dense(ti.k, n_particles).place(x, v)
+  ti.root.dense(ti.k, n_particles).place(x, v, J, C)
   ti.root.dense(ti.ij, n_grid).place(grid_v, grid_m)
   ti.cfg().print_ir = True
 
@@ -48,17 +50,17 @@ def clear_grid():
 @ti.kernel
 def p2g():
   for p in x(0):
-    base_coord = ti.cast(x[p] * inv_dx - 0.5, ti.i32)
-    fx = x[p] * inv_dx - ti.cast(base_coord, ti.f32)
+    base = ti.cast(x[p] * inv_dx - 0.5, ti.i32)
+    fx = x[p] * inv_dx - ti.cast(base, ti.f32)
     w = [0.5 * ti.sqr(1.5 - fx), 0.75 - ti.sqr(fx - 1.0), 0.5 * ti.sqr(fx - 0.5)]
     for i in ti.static(range(3)):
       for j in ti.static(range(3)):
         # dpos = (ti.Vector([i, j]) - fx) * dx
         weight = w[i](0) * w[j](1)
-        grid_v[base_coord(0) + i, base_coord(1) + j].assign(
-          grid_v[base_coord(0) + i, base_coord(1) + j] + weight * p_mass * v[p])
-        grid_m[base_coord(0) + i, base_coord(1) + j].assign(
-          grid_m[base_coord(0) + i, base_coord(1) + j] + weight * p_mass)
+        grid_v[base(0) + i, base(1) + j].assign(
+          grid_v[base(0) + i, base(1) + j] + weight * p_mass * v[p])
+        grid_m[base(0) + i, base(1) + j].assign(
+          grid_m[base(0) + i, base(1) + j] + weight * p_mass)
 
 
 @ti.kernel
@@ -67,20 +69,23 @@ def grid_op():
     if grid_m[i, j] > 0.0:
       inv_m = 1.0 / grid_m[i, j]
       grid_v[i, j] = inv_m * grid_v[i, j]
+      if j < 5:
+        if grid_v(1)[i, j] < 0.0:
+          grid_v(1)[i, j] = 0.0
 
 
 @ti.kernel
 def g2p():
   for p in x(0):
-    base_coord = ti.cast(x[p] * inv_dx - 0.5, ti.i32)
-    fx = x[p] * inv_dx - ti.cast(base_coord, ti.f32)
+    base = ti.cast(x[p] * inv_dx - 0.5, ti.i32)
+    fx = x[p] * inv_dx - ti.cast(base, ti.f32)
     w = [0.5 * ti.sqr(1.5 - fx), 0.75 - ti.sqr(fx - 1.0), 0.5 * ti.sqr(fx - 0.5)]
-    local_v = ti.Vector([0.0, 0.0])
+    new_v = ti.Vector([0.0, 0.0])
     for i in ti.static(range(3)):
       for j in ti.static(range(3)):
         weight = w[i](0) * w[j](1)
-        local_v = local_v + weight * grid_v[base_coord(0) + i, base_coord(1) + j]
-    v[p].assign(local_v)
+        new_v = new_v + weight * grid_v[base(0) + i, base(1) + j]
+    v[p].assign(new_v)
     x[p] = x[p] + dt * v[p]
 
 
@@ -88,7 +93,8 @@ def g2p():
 def main():
   for i in range(n_particles):
     x[i] = [random.random() * 0.4 + 0.2, random.random() * 0.4 + 0.2]
-    v[i] = [1, 0]
+    v[i] = [0, -1]
+    J[i] = 1.0
 
   for f in range(100):
     for s in range(10):
@@ -108,7 +114,7 @@ def main():
       p_x = int(scale * x(0)[i] / dx)
       p_y = int(scale * x(1)[i] / dx)
       img[p_x, p_y] = 1
-    cv2.imshow('MPM', img)
+    cv2.imshow('MPM', img.swapaxes(0, 1)[::-1])
     cv2.waitKey(1)
 
 

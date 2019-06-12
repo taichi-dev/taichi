e16a0683434af4fb0af76f14fdc660810f07ab7d
Yuanming Hu <yuanmhu@gmail.com>
2019-06-12 16:18:31 -0700

    differentiate grid_v in/out

--- python/diffmpm.py
+++ python/diffmpm.py
@@ -9,11 +9,12 @@ n_particles = 8192 * 4
 n_grid = 256
 dx = 1 / n_grid
 inv_dx = 1 / dx
-dt = 1e-3
+dt = 1e-4
 p_mass = 1
 p_vol = 1
-E = 1
-steps = 128
+E = 100
+steps = 1024
+gravity = 9.8
 
 scalar = lambda: ti.var(dt=real)
 vec = lambda: ti.Vector(dim, dt=real)
@@ -21,7 +22,8 @@ mat = lambda: ti.Matrix(dim, dim, dt=real)
 f = ti.var(ti.i32)
 
 x, v = vec(), vec()
-grid_v, grid_m = vec(), scalar()
+grid_v_in, grid_m_in = vec(), scalar()
+grid_v_out = vec()
 C, J = mat(), scalar()
 
 # ti.cfg.arch = ti.x86_64
@@ -30,20 +32,25 @@ ti.cfg.arch = ti.cuda
 @ti.layout
 def place():
   ti.root.dense(ti.l, steps).dense(ti.k, n_particles).place(x, v, J, C)
-  ti.root.dense(ti.ij, n_grid).place(grid_v, grid_m)
+  ti.root.dense(ti.ij, n_grid).place(grid_v_in, grid_m_in, grid_v_out)
   ti.root.place(f)
 
 
 @ti.kernel
 def clear_grid():
-  for i, j in grid_m:
-    grid_v[i, j] = [0, 0]
-    grid_m[i, j] = 0
+  for i, j in grid_m_in:
+    grid_v_in[i, j] = [0, 0]
+    grid_m_in[i, j] = 0
 
 @ti.kernel
 def inc_f():
   global f
   f += 1
+
+@ti.kernel
+def dec_f():
+  global f
+  f -= 1
 
 @ti.kernel
 def p2g():
@@ -59,8 +66,8 @@ def p2g():
         offset = ti.Vector([i, j])
         dpos = (ti.cast(ti.Vector([i, j]), ti.f32) - fx) * dx
         weight = w[i](0) * w[j](1)
-        grid_v[base + offset].atomic_add(weight * (p_mass * v[f, p] + affine @ dpos))
-        grid_m[base + offset].atomic_add(weight * p_mass)
+        grid_v_in[base + offset].atomic_add(weight * (p_mass * v[f, p] + affine @ dpos))
+        grid_m_in[base + offset].atomic_add(weight * p_mass)
 
 
 bound = 3
@@ -68,19 +75,24 @@ bound = 3
 
 @ti.kernel
 def grid_op():
-  for i, j in grid_m:
-    if grid_m[i, j] > 0:
-      inv_m = 1 / grid_m[i, j]
-      grid_v[i, j] = inv_m * grid_v[i, j]
-      grid_v(1)[i, j] -= dt * 9.8
-      if i < bound and grid_v(0)[i, j] < 0:
-        grid_v(0)[i, j] = 0
-      if i > n_grid - bound and grid_v(0)[i, j] > 0:
-        grid_v(0)[i, j] = 0
-      if j < bound and grid_v(1)[i, j] < 0:
-        grid_v(1)[i, j] = 0
-      if j > n_grid - bound and grid_v(1)[i, j] > 0:
-        grid_v(1)[i, j] = 0
+  for i, j in grid_m_in:
+    if grid_m_in[i, j] > 0:
+      inv_m = 1 / grid_m_in[i, j]
+
+      grid_v_out[i, j] = inv_m * grid_v_in[i, j]
+
+      grid_v_out(1)[i, j] -= dt * gravity
+
+      if i < bound and grid_v_out(0)[i, j] < 0:
+        grid_v_out(0)[i, j] = 0
+      if i > n_grid - bound and grid_v_out(0)[i, j] > 0:
+        grid_v_out(0)[i, j] = 0
+      if j < bound and grid_v_out(1)[i, j] < 0:
+        grid_v_out(1)[i, j] = 0
+      if j > n_grid - bound and grid_v_out(1)[i, j] > 0:
+        grid_v_out(1)[i, j] = 0
+    else:
+      grid_v_out[i, j] = ti.Vector([0, 0])
 
 
 @ti.kernel
@@ -96,7 +108,7 @@ def g2p():
     for i in ti.static(range(3)):
       for j in ti.static(range(3)):
         dpos = ti.cast(ti.Vector([i, j]), ti.f32) - fx
-        g_v = grid_v[base(0) + i, base(1) + j]
+        g_v = grid_v_out[base(0) + i, base(1) + j]
         weight = w[i](0) * w[j](1)
         new_v += weight * g_v
         new_C += 4 * weight * ti.outer_product(g_v, dpos) * inv_dx
@@ -109,8 +121,8 @@ def g2p():
 
 def main():
   for i in range(n_particles):
-    x[0, i] = [random.random() * 0.4 + 0.2, random.random() * 0.4 + 0.2]
-    v[0, i] = [0, -1]
+    x[0, i] = [random.random() * 0.4 + 0.2, random.random() * 0.4 + 0.1]
+    v[0, i] = [0, -10]
     J[0, i] = 1
 
   for s in range(steps - 1):
@@ -120,10 +132,14 @@ def main():
     g2p()
     inc_f()
 
+  dec_f()
+  g2p.grad()
+  p2g.grad()
+
   ti.profiler_print()
 
   while True:
-    for s in range(10):
+    for s in range(0, steps - 1, 16):
       scale = 2
       img = np.zeros(shape=(scale * n_grid, scale * n_grid)) + 0.3
       for i in range(n_particles):

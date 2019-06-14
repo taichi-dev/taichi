054f0a019063bc4f921604e8a0ef974f9eeb8641
Yuanming Hu <yuanmhu@gmail.com>
2019-06-14 09:36:55 -0700

    reprod diffmpm param error

--- python/diffmpm.py
+++ python/diffmpm.py
@@ -48,29 +48,6 @@ def place():
     grid_v_in.grad, grid_m_in.grad, grid_v_out.grad)
   ti.root.place(init_v, init_v.grad, loss, loss.grad, x_avg, x_avg.grad)
 
-@ti.kernel
-def set_v():
-  for i in range(n_particles):
-    v[0, i] = init_v
-
-@ti.kernel
-def clear_grid():
-  for i, j in grid_m_in:
-    grid_v_in[i, j] = [0, 0]
-    grid_m_in[i, j] = 0
-    grid_v_in.grad[i, j] = [0, 0]
-    grid_m_in.grad[i, j] = 0
-    grid_v_out.grad[i, j] = [0, 0]
-
-@ti.kernel
-def clear_particle_grad():
-  # for all time steps and all particles
-  for f, i in x:
-    x.grad[f, i] = [0, 0]
-    v.grad[f, i] = [0, 0]
-    C.grad[f, i] = [[0, 0], [0, 0]]
-    F.grad[f, i] = [[0, 0], [0, 0]]
-
 
 @ti.kernel
 def p2g(f: ti.i32):
@@ -82,70 +59,6 @@ def p2g(f: ti.i32):
          0.5 * ti.sqr(fx - 0.5)]
     new_F = (ti.Matrix.diag(dim=2, val=1) + dt * C[f, p]) @ F[f, p]
     F[f + 1, p] = new_F
-
-    J = ti.determinant(new_F)
-    r, s = ti.polar_decompose(new_F)
-    cauchy = 2 * mu * (new_F - r) @ ti.transposed(new_F) + \
-             ti.Matrix.diag(2, la * (J - 1) * J)
-    stress = -(dt * p_vol * 4 * inv_dx * inv_dx) * cauchy
-    affine = stress + p_mass * C[f, p]
-    for i in ti.static(range(3)):
-      for j in ti.static(range(3)):
-        offset = ti.Vector([i, j])
-        dpos = (ti.cast(ti.Vector([i, j]), ti.f32) - fx) * dx
-        weight = w[i](0) * w[j](1)
-        grid_v_in[base + offset].atomic_add(
-          weight * (p_mass * v[f, p] + affine @ dpos))
-        grid_m_in[base + offset].atomic_add(weight * p_mass)
-
-
-bound = 3
-
-
-@ti.kernel
-def grid_op():
-  for i, j in grid_m_in:
-    v_out = ti.Vector([0.0, 0.0])
-    #if grid_m_in[i, j] > 0:
-
-    inv_m = 1 / (grid_m_in[i, j] + 1e-10)
-
-    v_out = inv_m * grid_v_in[i, j]
-
-    v_out(1).val -= dt * gravity
-
-    if i < bound and v_out(0) < 0:
-      v_out(0).val = 0
-    if i > n_grid - bound and v_out(0) > 0:
-      v_out(0).val = 0
-    if j < bound and v_out(1) < 0:
-      v_out(1).val = 0
-    if j > n_grid - bound and v_out(1) > 0:
-      v_out(1).val = 0
-    grid_v_out[i, j] = v_out
-
-
-@ti.kernel
-def g2p(f: ti.i32):
-  for p in range(0, n_particles):
-    base = ti.cast(x[f, p] * inv_dx - 0.5, ti.i32)
-    fx = x[f, p] * inv_dx - ti.cast(base, ti.f32)
-    w = [0.5 * ti.sqr(1.5 - fx), 0.75 - ti.sqr(fx - 1.0),
-         0.5 * ti.sqr(fx - 0.5)]
-    new_v = ti.Vector([0.0, 0.0])
-    new_C = ti.Matrix([[0.0, 0.0], [0.0, 0.0]])
-
-    for i in ti.static(range(3)):
-      for j in ti.static(range(3)):
-        dpos = ti.cast(ti.Vector([i, j]), ti.f32) - fx
-        g_v = grid_v_out[base(0) + i, base(1) + j]
-        weight = w[i](0) * w[j](1)
-        new_v += weight * g_v
-        new_C += 4 * weight * ti.outer_product(g_v, dpos) * inv_dx
-
-    v[f + 1, p] = new_v
-    x[f + 1, p] = x[f, p] + dt * v[f + 1, p]
-    C[f + 1, p] = new_C
 
 
 def main():

497855b8dd70586f508272df5e19839a557c3ca3
Yuanming Hu <yuanmhu@gmail.com>
2019-06-17 16:49:49 -0700

    2-layer mlp

--- python/mlp.py
+++ python/mlp.py
@@ -7,7 +7,12 @@ import pickle
 # ti.cfg.print_ir = True
 
 input = ti.var(ti.f32)
-weight = ti.var(ti.f32)
+
+weight1 = ti.var(ti.f32)
+output1 = ti.var(ti.f32)
+output1_nonlinear = ti.var(ti.f32)
+
+weight2 = ti.var(ti.f32)
 output = ti.var(ti.f32)
 output_exp = ti.var(ti.f32)
 output_softmax = ti.var(ti.f32)
@@ -17,27 +22,17 @@ loss = ti.var(ti.f32)
 learning_rate = ti.var(ti.f32)
 
 n_input = 28 ** 2
+n_hidden = 30
 n_output = 10
-
-
-@ti.kernel
-def init_weights():
-  for i in range(n_input):
-    for j in range(n_output):
-      weight[i, j] = ti.random() * 0.001
-
-
-@ti.kernel
-def clear_weight_grad():
-  for i in range(n_input):
-    for j in range(n_output):
-      weight.grad[i, j] = 0
 
 
 @ti.layout
 def layout():
   ti.root.dense(ti.i, n_input).place(input)
-  ti.root.dense(ti.ij, (n_input, n_output)).place(weight)
+  ti.root.dense(ti.ij, (n_input, n_hidden)).place(weight1)
+  ti.root.dense(ti.i, n_hidden).place(output1)
+  ti.root.dense(ti.i, n_hidden).place(output1_nonlinear)
+  ti.root.dense(ti.ij, (n_hidden, n_output)).place(weight2)
   ti.root.dense(ti.i, n_output).place(gt)
   ti.root.dense(ti.i, n_output).place(output)
   ti.root.dense(ti.i, n_output).place(output_exp)
@@ -47,8 +42,40 @@ def layout():
 
   ti.root.lazy_grad()
 
+@ti.kernel
+def init_weights1():
+  for i in range(n_input):
+    for j in range(n_hidden):
+      weight1[i, j] = ti.random() * 0.005
 
-def clear_outputs():
+@ti.kernel
+def init_weights2():
+  for i in range(n_hidden):
+    for j in range(n_output):
+      weight2[i, j] = ti.random() * 0.005
+
+
+@ti.kernel
+def clear_weight1_grad():
+  for i in range(n_input):
+    for j in range(n_hidden):
+      weight1.grad[i, j] = 0
+
+
+@ti.kernel
+def clear_weight2_grad():
+  for i in range(n_hidden):
+    for j in range(n_output):
+      weight2.grad[i, j] = 0
+
+def clear_output1():
+  for i in range(n_hidden):
+    output1[i] = 0
+    output1_nonlinear[i] = 0
+    output1.grad[i] = 0
+    output1_nonlinear.grad[i] = 0
+
+def clear_output2():
   for i in range(n_output):
     output[i] = 0
     output_exp[i] = 0
@@ -58,43 +85,68 @@ def clear_outputs():
     output_softmax.grad[i] = 0
 
 
+def layer(func):
+  layer.list.append(func)
+layer.list = []
+
+
+@layer
 @ti.kernel
-def layer1():
+def w1():
   for i in range(n_input):
-    for j in range(n_output):
-      output[j].atomic_add(input[i] * weight[i, j])
+    for j in range(n_hidden):
+      output1[j].atomic_add(input[i] * weight1[i, j])
 
-
+@layer
 @ti.kernel
-def layer2():
+def nonlinear1():
+  for i in range(n_hidden):
+    output1_nonlinear[i] = ti.tanh(output1[i])
+
+@layer
+@ti.kernel
+def w2():
+  for i in range(n_hidden):
+    for j in range(n_output):
+      output[j].atomic_add(output1_nonlinear[i] * weight2[i, j])
+
+@layer
+@ti.kernel
+def nonlinear2():
   for i in range(n_output):
     output_exp[i] = ti.exp(output[i])
 
-
+@layer
 @ti.kernel
-def layer3():
+def reduce():
   for i in range(n_output):
     softmax_sum.atomic_add(output_exp[i] + 1e-6)
 
-
+@layer
 @ti.kernel
-def layer4():
+def softmax():
   for i in range(n_output):
     output_softmax[i] = output_exp[i] / softmax_sum
 
-
+@layer
 @ti.kernel
-def layer5():
+def xent():
   for i in range(n_output):
     loss.atomic_add(-gt[i] * ti.log(output_softmax[i]) + (gt[i] - 1) * ti.log(
       1 - output_softmax[i]))
 
 
 @ti.kernel
-def gd():
+def gd_w1():
   for i in range(n_input):
+    for j in range(n_hidden):
+      weight1[i, j] -= learning_rate * weight1.grad[i, j]
+
+@ti.kernel
+def gd_w2():
+  for i in range(n_hidden):
     for j in range(n_output):
-      weight[i, j] -= learning_rate * weight.grad[i, j]
+      weight2[i, j] -= learning_rate * weight2.grad[i, j]
 
 
 with open('mnist.pkl', 'rb') as f:
@@ -105,7 +157,8 @@ training_labels = mnist['training_labels']
 test_images = mnist['test_images']
 test_labels = mnist['test_labels']
 
-init_weights()
+init_weights1()
+init_weights2()
 
 learning_rate = 1e-3
 
@@ -121,17 +174,16 @@ for k in range(12000):
   for j in range(n_output):
     gt[j] = int(training_labels[img_id] == j)
 
-  clear_weight_grad()
-  clear_outputs()
+  clear_output1()
+  clear_output2()
+  clear_weight1_grad()
+  clear_weight2_grad()
+
   softmax_sum[None] = 0
   loss[None] = 0
 
-  layer1()
-  layer2()
-  layer3()
-  layer4()
-  layer5()
-
+  for f in layer.list:
+    f()
 
   l = loss[None]
 
@@ -143,13 +195,11 @@ for k in range(12000):
   loss.grad[None] = 1
   softmax_sum.grad[None] = 0
 
-  layer5.grad()
-  layer4.grad()
-  layer3.grad()
-  layer2.grad()
-  layer1.grad()
+  for f in reversed(layer.list):
+    f.grad()
 
-  gd()
+  gd_w1()
+  gd_w2()
 
 ntest = len(test_images) // 5
 acc = 0
@@ -162,11 +212,15 @@ for k in range(ntest):
   for j in range(n_output):
     gt[j] = int(test_labels[k] == j)
 
-  clear_outputs()
-  clear_weight_grad()
+  clear_output1()
+  clear_output2()
+  clear_weight1_grad()
+  clear_weight2_grad()
+
   loss[None] = 0
 
-  layer1()
+  for f in layer.list:
+    f()
 
   logits = []
   for j in range(n_output):

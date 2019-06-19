5a7ca427c4fe514dfcf7aa0fd571ac15b8b031e6
Yuanming Hu <yuanmhu@gmail.com>
2019-06-19 14:55:43 -0700

    testhook

---
+++ python/test/test_binding.py
@@ -0,0 +1,10 @@
+import taichi_lang as ti
+
+def test_binding():
+  taichi_lang = ti.core
+  print(taichi_lang.BinaryOpType.mul)
+  one = taichi_lang.make_const_expr_i32(1)
+  two = taichi_lang.make_const_expr_i32(2)
+  expr = taichi_lang.make_binary_op_expr(taichi_lang.BinaryOpType.add, one, two)
+  print(expr.serialize())
+  print(taichi_lang.make_global_store_stmt(None, None))
---
+++ python/testhook.py
@@ -0,0 +1,3 @@
+import pytest
+
+pytest.main(['test/'])

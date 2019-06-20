859c3fcbf378d82b7591d1bea16cb42eee1fb174
Yuanming Hu <yuanmhu@gmail.com>
2019-06-19 17:26:58 -0700

    reproduce gpu struct for complex kernel failure

--- test/python/test_complex_kernels.py
+++ test/python/test_complex_kernels.py
@@ -1,36 +1,38 @@
 import taichi_lang as ti
 from pytest import approx
 
-# ti.cfg.arch = ti.cuda
-
-ti.cfg.print_ir = True
+# ti.cfg.print_ir = True
 
 def test_complex_kernels():
-  ti.reset()
+  for arch in [ti.x86_64, ti.cuda]:
+    ti.reset()
+    ti.cfg.arch = arch
 
-  a = ti.var(ti.f32)
-  b = ti.var(ti.f32)
+    a = ti.var(ti.f32)
+    b = ti.var(ti.f32)
 
-  n = 128
+    n = 128
 
-  @ti.layout
-  def place():
-    ti.root.dense(ti.i, n).place(a, b)
+    @ti.layout
+    def place():
+      ti.root.dense(ti.i, n).place(a, b)
 
-  @ti.kernel
-  def add():
+    @ti.kernel
+    def add():
+      for i in range(n):
+        a[i] += 1
+      for i in range(n):
+        b[i] += 2
+      for i in a:
+        b[i] += 3
+
     for i in range(n):
-      a[i] += 1
+      a[i] = i + 1
+      b[i] = i + 2
+    add()
+
     for i in range(n):
-      b[i] += 2
-
-  for i in range(n):
-    a[i] = i + 1
-    b[i] = i + 2
-  add()
-
-  for i in range(n):
-    assert a[i] == i + 2
-    assert b[i] == i + 4
+      assert a[i] == i + 2
+      assert b[i] == i + 7
 
 test_complex_kernels()

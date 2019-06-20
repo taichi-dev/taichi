1dc087e6332f9009eca166067bb536f62bd7b574
Yuanming Hu <yuanmhu@gmail.com>
2019-06-19 17:14:38 -0700

    reproduce complex kernel failure

---
+++ test/python/test_complex_kernels.py
@@ -0,0 +1,38 @@
+import taichi_lang as ti
+from pytest import approx
+
+# ti.cfg.arch = ti.cuda
+
+ti.cfg.print_ir = True
+
+def test_complex_kernels():
+  ti.reset()
+
+  a = ti.var(ti.f32)
+  b = ti.var(ti.f32)
+
+  n = 128
+
+  @ti.layout
+  def place():
+    ti.root.dense(ti.i, n).place(a, b)
+
+  @ti.kernel
+  def add():
+    for i in range(n):
+      a[i] += 1
+    for u in range(n):
+      b[i] += 2
+      ti.print(i)
+      ti.print(b[i])
+
+  for i in range(n):
+    a[i] = i + 1
+    b[i] = i + 2
+  add()
+
+  for i in range(n):
+    assert a[i] == i + 2
+    assert b[i] == i + 4
+
+test_complex_kernels()

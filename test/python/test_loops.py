64f8d8848edd4eb603f43a640644d93b41da91c3
Yuanming Hu <yuanmhu@gmail.com>
2019-06-21 11:16:36 -0700

    stronger test_loops

--- test/python/test_loops.py
+++ test/python/test_loops.py
@@ -8,23 +8,27 @@ def test_loops():
     x = ti.var(ti.f32)
     y = ti.var(ti.f32)
 
-    N = 16
+    N = 512
     @ti.layout
     def place():
       ti.root.dense(ti.i, N).place(x)
       ti.root.dense(ti.i, N).place(y)
       ti.root.lazy_grad()
 
-    for i in range(1, N):
-      y[i] = i - 5
+    for i in range(N // 2, N):
+      y[i] = i - 300
 
     @ti.kernel
     def func():
-      for i in range(1, N):
+      for i in range(N // 2 + 3, N):
         x[i] = ti.abs(y[i])
 
     func()
 
-    assert x[0] == abs(y[0])
-    for i in range(1, N):
+    for i in range(N // 2 + 3):
+      assert x[i] == 0
+
+    for i in range(N // 2 + 3, N):
       assert x[i] == abs(y[i])
+
+test_loops()

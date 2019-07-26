3c8bc1eea9e19401845b6316f006e3d976537f39
Yuanming Hu <yuanmhu@gmail.com>
2019-07-26 15:16:49 -0700

    removed -D_GLIBCXX_USE_CXX11_ABI=0

--- cmake/TaichiCXXFlags.cmake
+++ cmake/TaichiCXXFlags.cmake
@@ -63,9 +63,6 @@ else()
     message("Using float32 (single) precision as real")
 endif()
 
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -D_GLIBCXX_USE_CXX11_ABI=0")
-message("Using -D_GLIBCXX_USE_CXX11_ABI=0")
-
 if (TC_USE_MPI)
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -DTC_USE_MPI")
     message("Using MPI")

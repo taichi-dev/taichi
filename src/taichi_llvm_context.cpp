559a7c3731b5f41c7d93741e71817979cc1e6f66
Yuanming Hu <yuanmhu@gmail.com>
2019-07-26 16:07:04 -0700

    fixed all tests

--- src/taichi_llvm_context.cpp
+++ src/taichi_llvm_context.cpp
@@ -26,9 +26,14 @@ TLANG_NAMESPACE_BEGIN
 static llvm::ExitOnError exit_on_err;
 
 TaichiLLVMContext::TaichiLLVMContext() {
-  llvm::InitializeNativeTarget();
-  llvm::InitializeNativeTargetAsmPrinter();
-  llvm::InitializeNativeTargetAsmParser();
+  return;
+  static bool init = false;
+  if (!init) {
+    init = true;
+    llvm::InitializeNativeTarget();
+    llvm::InitializeNativeTargetAsmPrinter();
+    llvm::InitializeNativeTargetAsmParser();
+  }
   ctx = std::make_unique<llvm::LLVMContext>();
   jit = exit_on_err(TaichiLLVMJIT::Create());
 }

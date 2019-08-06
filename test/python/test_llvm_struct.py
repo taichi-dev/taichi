26a5a9e2ccddc3a41cb3fc1411a6917e41717c30
Yuanming Hu <yuanmhu@gmail.com>
2019-08-06 16:03:43 -0700

    llvm struct tests

---
+++ test/python/test_llvm_struct.py
@@ -0,0 +1,65 @@
+import taichi_lang as ti
+
+ti.cfg.use_llvm = True
+
+def test_linear():
+  ti.reset()
+
+  x = ti.var(ti.i32)
+  y = ti.var(ti.i32)
+
+  n = 128
+
+  @ti.layout
+  def place():
+    ti.root.dense(ti.i, n).place(x)
+    ti.root.dense(ti.i, n).place(y)
+
+  for i in range(n):
+    x[i] = i
+    y[i] = i + 123
+
+  for i in range(n):
+    assert x[i] == i
+    assert y[i] == i + 123
+
+def test_linear_nested():
+  ti.reset()
+
+  x = ti.var(ti.i32)
+  y = ti.var(ti.i32)
+
+  n = 128
+
+  @ti.layout
+  def place():
+    ti.root.dense(ti.i, n // 16).dense(ti.i, 16).place(x)
+    ti.root.dense(ti.i, n // 16).dense(ti.i, 16).place(y)
+
+  for i in range(n):
+    x[i] = i
+    y[i] = i + 123
+
+  for i in range(n):
+    assert x[i] == i
+    assert y[i] == i + 123
+
+def test_2d_nested():
+  ti.reset()
+
+  x = ti.var(ti.i32)
+
+  n = 128
+
+  @ti.layout
+  def place():
+    ti.root.dense(ti.ij, n // 16).dense(ti.ij, (32, 16)).place(x)
+
+  for i in range(n * 2):
+    for j in range(n):
+      x[i, j] = i + j * 10
+
+  for i in range(n * 2):
+    for j in range(n):
+      assert x[i, j] == i + j * 10
+

667e26f4743844a81438fabff7910b3220cd2492
Yuanming Hu <yuanmhu@gmail.com>
2019-08-07 12:02:25 -0700

    old scripts

---
+++ scripts/make_wave.py
@@ -0,0 +1,10 @@
+import scipy.io.wavfile
+import os
+
+os.system('ffmpeg -y -i data/happy-whistling-ukulele.mp3 -acodec pcm_s16le -ar 44100 data/input.wav')
+wav = scipy.io.wavfile.read('data/input.wav')[1]
+
+print(wav.shape)
+with open('data/wave.txt', 'w') as f:
+    for a in wav:
+        print(a[0], a[1], file=f)

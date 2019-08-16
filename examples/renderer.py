eae06df0dbb272448ad83608574093d5ecf440fd
Yuanming Hu <yuanmhu@gmail.com>
2019-08-16 14:53:52 -0700

    RGB renderer

--- examples/renderer.py
+++ examples/renderer.py
@@ -2,7 +2,7 @@ import taichi_lang as ti
 import numpy as np
 import cv2
 
-res = 128
+res = 1024
 color_buffer = ti.Vector(3, dt=ti.f32)
 
 @ti.layout
@@ -19,16 +19,17 @@ def render():
 @ti.kernel
 def copy(img: np.ndarray):
   for i, j in color_buffer(0):
-    img[i * res + j] = color_buffer[i, j](0) * 0.003
+    coord = (i * res + j) * 3
+    for c in ti.static(range(3)):
+      img[coord + c] = color_buffer[i, j](c) * 0.003
 
 def main():
   render()
   img = np.zeros((res * res * 3, ), dtype=np.float32)
   copy(img)
   img = img.reshape(res, res, 3)
-  while True:
-    cv2.imshow('img', img)
-    cv2.waitKey(1)
+  cv2.imshow('img', img)
+  cv2.waitKey(0)
 
 if __name__ == '__main__':
   main()

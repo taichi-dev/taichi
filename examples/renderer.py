c733c315076379bf3a8a6a03e57dd14e9f52936e
Yuanming Hu <yuanmhu@gmail.com>
2019-08-16 14:51:54 -0700

    renderer.py

--- examples/renderer.py
+++ examples/renderer.py
@@ -12,18 +12,20 @@ def buffers():
 @ti.kernel
 def render():
   for i, j in color_buffer(0):
-    color_buffer[i, j](0).val = i + j
+    color_buffer[i, j](0).val = i
+    color_buffer[i, j](1).val = j
+    color_buffer[i, j](2).val = i + j
 
 @ti.kernel
 def copy(img: np.ndarray):
   for i, j in color_buffer(0):
-    img[i * res + j] = color_buffer[i, j](0) * 0.01
+    img[i * res + j] = color_buffer[i, j](0) * 0.003
 
 def main():
   render()
-  img = np.zeros((res * res, ), dtype=np.float32)
+  img = np.zeros((res * res * 3, ), dtype=np.float32)
   copy(img)
-  img = img.reshape(res, res, 1)
+  img = img.reshape(res, res, 3)
   while True:
     cv2.imshow('img', img)
     cv2.waitKey(1)

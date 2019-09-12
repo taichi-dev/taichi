7103ccef0a6301da0fcd290540a040addaba71cf
Yuanming Hu <yuanmhu@gmail.com>
2019-09-11 20:43:05 -0400

    works without collision

--- examples/billiard.py
+++ examples/billiard.py
@@ -10,9 +10,9 @@ ti.set_default_fp(real)
 # ti.runtime.print_preprocessed = True
 
 max_steps = 2048
-vis_interval = 32
+vis_interval = 16
 output_vis_interval = 2
-steps = 1024
+steps = 16
 assert steps * 2 <= max_steps
 
 vis_resolution = 1024
@@ -34,7 +34,8 @@ impulse = vec()
 
 billiard_layers = 4
 n_balls = 1 + (1 + billiard_layers) * billiard_layers // 2
-target_ball = n_balls - 1
+# target_ball = n_balls - 1
+target_ball = 0
 goal = [0.8, 0.7]
 radius = 0.03
 elasticity = 0.8
@@ -73,8 +74,9 @@ def collide(t: ti.i32):
 @ti.kernel
 def advance(t: ti.i32):
   for i in range(n_balls):
-    v[t, i] = v[t - 1, i] + impulse[t, i]
-    x[t, i] = x[t - 1, i] + dt * v[t, i]
+    new_v = v[t - 1, i] + impulse[t, i]
+    v[t, i] = new_v
+    x[t, i] = x[t - 1, i] + dt * new_v #v[t, i]
 
 
 @ti.kernel
@@ -95,8 +97,6 @@ def initialize():
   v[0, 0] = init_v
 
 def forward(output=None):
-  init_x[None] = [0.1, 0.5]
-  init_v[None] = [0.3, 0.0]
   initialize()
   
   count = 0
@@ -106,7 +106,7 @@ def forward(output=None):
       x[0, count] = [i * 2 * radius + 0.5, j * 2 * radius + 0.5 - i * radius * 0.7]
   
   for t in range(1, steps):
-    collide(t - 1)
+    # collide(t - 1)
     advance(t)
     
     if (t + 1) % vis_interval == 0:
@@ -154,9 +154,9 @@ def backward():
   
   loss.grad[None] = -1
   compute_loss.grad()
-  for i in reversed(range(1, billiard_layers)):
+  for i in reversed(range(1, steps)):
     advance.grad(i)
-    collide.grad(i - 1)
+    # collide.grad(i - 1)
   initialize.grad()
   
   print(init_x.grad[None][0], init_x.grad[None][1], init_v.grad[None][0], init_v.grad[None][1])
@@ -167,6 +167,8 @@ def backward():
   
 
 def main():
+  init_x[None] = [0.1, 0.5]
+  init_v[None] = [0.3, 0.0]
   for iter in range(200):
     clear()
     forward()

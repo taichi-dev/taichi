08f2faa1e0f3375a4c37e07b9167ed3f650c47bd
Yuanming Hu <yuanmhu@gmail.com>
2019-09-20 09:13:55 -0400

    gui rect

--- examples/test_gui.py
+++ examples/test_gui.py
@@ -1,6 +1,10 @@
 import taichi as tc
 
+vec = tc.Vector
+
 gui = tc.core.GUI("Test GUI", tc.Vectori(512, 512))
+canvas = gui.get_canvas()
+canvas.rect(vec(0.3, 0.3), vec(0.6, 0.6)).radius(2).color(0xFFFFFF).close()
 
 while True:
   gui.update()

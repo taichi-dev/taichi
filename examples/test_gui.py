0007bdd675d6de05e888ad91578eed29a586b58d
Yuanming Hu <yuanmhu@gmail.com>
2019-09-20 09:17:47 -0400

    circle working

--- examples/test_gui.py
+++ examples/test_gui.py
@@ -4,7 +4,9 @@ vec = tc.Vector
 
 gui = tc.core.GUI("Test GUI", tc.Vectori(512, 512))
 canvas = gui.get_canvas()
-canvas.rect(vec(0.3, 0.3), vec(0.6, 0.6)).radius(2).color(0xFFFFFF).close()
+canvas.clear(0xFFFFFF)
+canvas.rect(vec(0.3, 0.3), vec(0.6, 0.6)).radius(2).color(0x000000).close()
+canvas.circle(vec(0.5, 0.5)).radius(10).color(0xFF0000)
 
 while True:
   gui.update()

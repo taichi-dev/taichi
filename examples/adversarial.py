9c6e47c13bf22a6e013ef34ce91b42f99628e1a1
Yuanming Hu <yuanmhu@gmail.com>
2019-09-23 21:18:53 -0400

    removed adam

--- examples/adversarial.py
+++ examples/adversarial.py
@@ -1,6 +1,5 @@
 """
-Created on Sat Nov 18 12:39:16 2016
-@author: manojacharya
+Originally created on Sat Nov 18 12:39:16 2016 by manojacharya
 """
 
 import torch
@@ -14,6 +13,9 @@ import matplotlib.pyplot as plt
 import torch.nn.functional as F
 import json
 
+
+means = np.array([0.485, 0.456, 0.406])
+std = np.array([0.229, 0.224, 0.225])
 normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                  std=[0.229, 0.224, 0.225])
 
@@ -51,52 +53,66 @@ def predict(img):
   pred = F.softmax(pred_raw)
   _, indices = torch.topk(pred, k=1)
   for ind in indices.data.numpy().ravel():
-    print("%.2f%% , class: %s , %s" % (
+    print("%.2f%% , class: %s (%s)" % (
     100 * pred.data[0][ind], str(ind), imagenet_labels[ind]))
 
 
 # In[image ]:
 
-peppers = imread("squirrel.jpg")
-img = imresize(peppers, (224, 224))
-imgtensor = transform(img)
-imgvar = Variable(imgtensor.unsqueeze(0), requires_grad=False)
-imgvard = Variable(imgtensor.unsqueeze(0), requires_grad=True)
-optimizer = torch.optim.Adam([imgvard], lr=0.1)
-loss_fn = nn.CrossEntropyLoss()
-
-label = torch.LongTensor(1)
-# classify the object as this label
-label[0] = 80
-label = Variable(label)
-eps = 2 / 255.0
-
-# %%
-Nepochs = 10
-print("Starting ...........", predict(imgvar))
-
-for epoch in range(Nepochs):
-  optimizer.zero_grad()
-  pred_raw = model(imgvard)
-  loss = loss_fn(pred_raw, label)
+def main():
+  # everything is RGB instead of BGR in this file
+  # import cv2
+  peppers = imread("squirrel.jpg")
+  # cv2.imshow('p', peppers)
+  # cv2.waitKey(0)
+  img = imresize(peppers, (224, 224)) / 255.0
+  # print(img.shape)
+  # imgtensor = transform(img)
+  # print(imgtensor.shape)
+  imgtensor = (img - means[None, None, :]) / std[None, None, :]
+  imgtensor = imgtensor[None, :, :, :].astype(np.float32)
+  imgtensor = torch.tensor(imgtensor)
+  imgtensor = torch.transpose(torch.transpose(imgtensor, 1, 3), 2, 3)
+  print(imgtensor.shape)
+  # imgtensor = transforms.ToTensor()(imgtensor)
   
-  diff = imgvard.data - imgvar.data
-  imgvard.data = torch.clamp(torch.abs(diff), max=eps) + imgvar.data
+  imgvar = Variable(imgtensor, requires_grad=False)
+  imgvard = Variable(imgtensor, requires_grad=True)
   
-  loss.backward()
-  print(imgvard.grad)
-  optimizer.step()
-  
-  print('epoch: {}/{}, loss: {}'.format(epoch + 1, Nepochs, loss.item()))
-  predict(imgvard)
-print('Finished Training')
+  loss_fn = nn.CrossEntropyLoss()
 
-# %%
-imshow(imgvard)
+  label = torch.LongTensor(1)
+  # classify the object as this label
+  label[0] = 1
+  label = Variable(label)
+  eps = 2 / 255.0
 
-# %%
-plt.figure()
-diffimg = diff[0].numpy()
-diffimg = diffimg.transpose((1, 2, 0))
-plt.imshow(diffimg)
-plt.show()
+  # %%
+  Nepochs = 10
+  print("Starting ...........", predict(imgvar))
+  for epoch in range(Nepochs):
+    pred_raw = model(imgvard)
+    loss = loss_fn(pred_raw, label)
+    
+    diff = imgvard.data - imgvar.data
+    imgvard.data = torch.clamp(torch.abs(diff), max=eps) + imgvar.data
+    
+    loss.backward()
+    imgvar -= imgvard.grad * 1
+    
+    print('epoch: {}/{}, loss: {}'.format(epoch + 1, Nepochs, loss.item()))
+    predict(imgvard)
+  print('Finished Training')
+
+  # %%
+  imshow(imgvard)
+
+  # %%
+  plt.figure()
+  diffimg = diff[0].numpy()
+  diffimg = diffimg.transpose((1, 2, 0))
+  plt.imshow(diffimg)
+  plt.show()
+
+if __name__ == '__main__':
+  main()

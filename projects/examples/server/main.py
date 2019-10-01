b84bd36254e1bfd2f4b29d8d9fabcedeea0b0f80
Yuanming Hu <yuanmhu@gmail.com>
2019-10-01 15:37:34 -0400

    removed legacy examples

--- projects/examples/server/main.py
+++
@@ -1,5 +0,0 @@
-import taichi as tc
-
-app = tc.get_pakua_server()
-
-app.run(port=1111 , host='0.0.0.0')
--- projects/examples/test/memory_usage.py
+++
@@ -1,8 +0,0 @@
-import taichi as tc
-
-# ti plot mem.txt   should show the result
-
-tc.start_memory_monitoring('mem.txt')
-a = []
-while True:
-  a.append(123)

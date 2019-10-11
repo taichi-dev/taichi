3b6cd1c410c82c1b62c5bdef7a0ebc088ea761a6
Yuanming Hu <yuanmhu@gmail.com>
2019-10-11 17:19:29 -0400

    cache

--- python/taichi/lang/__init__.py
+++ python/taichi/lang/__init__.py
@@ -20,6 +20,12 @@ cuda = core.gpu
 profiler_print = lambda: core.get_current_program().profiler_print()
 profiler_clear = lambda: core.get_current_program().profiler_clear()
 
+def cache_shared(v):
+  taichi_lang_core.cache(0, v.ptr)
+
+def cache_l1(v):
+  taichi_lang_core.cache(1, v.ptr)
+
 parallelize = core.parallelize
 vectorize = core.vectorize
 block_dim = core.block_dim

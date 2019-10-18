15f69814f72b85db8b78c5b1d9e83168dac20475
Yuanming Hu <yuanmhu@gmail.com>
2019-10-17 22:27:13 -0400

    c++ test case llvm_exception

--- lang/test/cpp/types.cpp
+++ lang/test/cpp/types.cpp
@@ -25,4 +25,25 @@ TC_TEST("float64") {
     TC_CHECK(a.val<float64>(i) == i * 2);
 };
 
+TC_TEST("llvm_exception") {
+  CoreState::set_trigger_gdb_when_crash(true);
+  int n = 1;
+  default_compile_config.use_llvm = true;
+  for (int i = 0; i < 2; i++) {
+    // Program prog(Arch::gpu);
+    Program prog(Arch::x86_64);
+    Global(a, f32);
+    // Global(b, f32);
+    layout([&]() { root.dense(Index(0), n).place(a); });
+    auto &func = kernel([&]() { For(0, n, [&](Expr i) { a[i] = 1; }); });
+
+    func();
+  };
+  try {
+    throw IRModified();
+  } catch (IRModified) {
+    TC_TAG;
+  }
+};
+
 TLANG_NAMESPACE_END

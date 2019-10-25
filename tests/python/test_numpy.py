1ea9162691594a6d07caf032ed2c1841524f3bc2
Yuanming Hu <yuanmhu@gmail.com>
2019-10-25 17:18:14 -0400

    numpy_integration -> test_numpy

---
+++ tests/python/test_numpy.py
@@ -0,0 +1,25 @@
+import taichi as ti
+import numpy as np
+
+@ti.program_test
+def test_numpy():
+  val = ti.var(ti.i32)
+
+  n = 4
+
+  @ti.layout
+  def values():
+    ti.root.dense(ti.i, n).place(val)
+
+  @ti.kernel
+  def test_numpy(arr: np.ndarray):
+    for i in range(n):
+      arr[i] = arr[i] ** 2
+
+  a = np.array([4, 8, 1, 24], dtype=np.float32)
+  for i in range(n):
+    a[i] = i * 2
+
+  test_numpy(a)
+  for i in range(n):
+    assert a[i] == i * i * 4

f34043c6d9a92e157fa0f919791c7d8469b0d786
Yuanming Hu <yuanmhu@gmail.com>
2019-11-01 23:34:43 -0400

    clean up; ti test_python and test_cpp

--- misc/baselines/benchmark.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import os
-
-os.makedirs('results', exist_ok=True)
-
-for f in os.listdir('vdb_dataset'):
-  print('Running on {}'.format(f))
-  os.system('ti benchmark_vdb {}'.format(f))
--- python/taichi/main.py
+++ python/taichi/main.py
@@ -5,42 +5,18 @@ import time
 import random
 from taichi.tools.video import make_video, interpolate_frames
 
-def run_pytest():
+def test_python():
   print("\nRunning python tests...\n")
-  # TODO: for some reason the python tests piss off cpp tests. Need to fix.
-  def test_python():
-    import taichi as ti
-    import pytest
-    pytest.main([os.path.join(ti.get_repo_directory(), 'tests')])
-    ti.reset()
-  test_python()
-    
-
-def plot(fn):
-  import matplotlib.pyplot as plt
-  with open(fn) as f:
-    lines = f.readlines()
-    T = []
-    M = []
-    for l in lines:
-      t, m = map(float, l.split(' '))
-      T.append(t)
-      M.append(m / 2 ** 30)
-
-  base_m = min(M)
-  base_t = T[0]
-  for i in range(len(T)):
-    T[i] -= base_t
-    M[i] -= base_m
-
-  plt.clf()
-  plt.plot(T, M)
-  plt.xlabel('Time (seconds)')
-  plt.ylabel('Memory Consumption (G Bytes)')
-  title = "Max={:3f} GB (taichi runtime={:3f} GB)".format(max(M), base_m)
-  plt.title(title)
-  plt.ylim(0, max(M) * 1.2)
-  plt.show()
+  import taichi as ti
+  import pytest
+  pytest.main([os.path.join(ti.get_repo_directory(), 'tests')])
+  ti.reset()
+  
+def test_cpp():
+  import taichi as ti
+  print("Running C++ tests...")
+  task = ti.Task('test')
+  task.run(*sys.argv[2:])
 
 def main(debug=False):
   lines = []
@@ -55,9 +31,9 @@ def main(debug=False):
                                 "\u2634 \u2635 \u2636 \u2637"))
   print(u'\n'.join(lines))
   print()
-  import taichi as tc
+  import taichi as ti
 
-  tc.tc_core.set_core_debug(debug)
+  ti.tc_core.set_core_debug(debug)
 
   argc = len(sys.argv)
   if argc == 1 or sys.argv[1] == 'help':
@@ -68,13 +44,12 @@ def main(debug=False):
       "           ti build                  |-> Build C++ files\n"
       "           ti amal                   |-> Generate amalgamated taichi.h\n"
       "           ti clean asm [*.s]        |-> Clean up gcc ASM\n"
-      "           ti plot [*.txt]           |-> Plot a memory usage curve\n"
       "           ti video                  |-> Make a video using *.png files in the current folder\n"
       "           ti convert                |-> Delete color controllers in a log file\n"
       "           ti exec                   |-> Invoke a executable in the 'build' folder\n"
       "           ti format                 |-> Format taichi and projects\n"
       "                                         (C++ source and python scripts)\n"
-      "           ti statement [statement]  |-> Execute a single statement (with taichi imported as tc\n"
+      "           ti statement [statement]  |-> Execute a single statement (with taichi imported as ti\n"
       "           ti [script.py]            |-> Run script\n"
       "           ti doc                    |-> Build documentation\n"
       "           ti merge                  |-> Merge images in folders horizontally\n"
@@ -98,10 +73,10 @@ def main(debug=False):
       print("Please specify [task name], e.g. test_math")
       exit(-1)
     name = sys.argv[2]
-    task = tc.Task(name)
+    task = ti.Task(name)
     task.run(*sys.argv[3:])
   elif mode == "debug":
-    tc.core.set_core_trigger_gdb_when_crash(True)
+    ti.core.set_core_trigger_gdb_when_crash(True)
     if argc <= 2:
       print("Please specify [file name], e.g. render.py")
       exit(-1)
@@ -109,42 +84,41 @@ def main(debug=False):
     with open(name) as script:
       script = script.read()
     exec(script, {'__name__': '__main__'})
+  elif mode == "test_python":
+    test_python()
+  elif mode == "test_cpp":
+    test_cpp()
   elif mode == "test":
-    if len(sys.argv) == 2:
-      run_pytest()
-    print("Running C++ tests...")
-    task = tc.Task('test')
-    task.run(*sys.argv[2:])
+    test_python()
+    test_cpp()
   elif mode == "build":
-    tc.core.build()
+    ti.core.build()
   elif mode == "format":
-    tc.core.format()
+    ti.core.format()
   elif mode == "statement":
     exec(sys.argv[2])
-  elif mode == "plot":
-    plot(sys.argv[2])
   elif mode == "update":
-    tc.core.update(True)
-    tc.core.build()
+    ti.core.update(True)
+    ti.core.build()
   elif mode == "asm":
     fn = sys.argv[2]
     os.system(r"sed '/^\s*\.\(L[A-Z]\|[a-z]\)/ d' {0} > clean_{0}".format(fn))
   elif mode == "exec":
     import subprocess
     exec_name = sys.argv[2]
-    folder = tc.get_bin_directory()
+    folder = ti.get_bin_directory()
     assert exec_name in os.listdir(folder)
     subprocess.call([os.path.join(folder, exec_name)] + sys.argv[3:])
   elif mode == "interpolate":
     interpolate_frames('.')
   elif mode == "amal":
     cwd = os.getcwd()
-    os.chdir(tc.get_repo_directory())
+    os.chdir(ti.get_repo_directory())
     with open('misc/amalgamate.py') as script:
       script = script.read()
     exec(script, {'__name__': '__main__'})
     os.chdir(cwd)
-    shutil.copy(os.path.join(tc.get_repo_directory(), 'build/taichi.h'), './taichi.h')
+    shutil.copy(os.path.join(ti.get_repo_directory(), 'build/taichi.h'), './taichi.h')
   elif mode == "doc":
     os.system('cd docs && sphinx-build -b html . build')
   elif mode == "video":
@@ -157,11 +131,11 @@ def main(debug=False):
     if len(sys.argv) >= 4:
       trunc = int(sys.argv[3])
       files = files[:trunc]
-    tc.info('Making video using {} png files...', len(files))
-    tc.info("frame_rate={}", frame_rate)
+    ti.info('Making video using {} png files...', len(files))
+    ti.info("frame_rate={}", frame_rate)
     output_fn = 'video.mp4'
     make_video(files, output_path=output_fn, frame_rate=frame_rate)
-    tc.info('Done! Output video file = {}', output_fn)
+    ti.info('Done! Output video file = {}', output_fn)
   elif mode == "convert":
     # http://www.commandlinefu.com/commands/view/3584/remove-color-codes-special-characters-with-sed
     # TODO: Windows support
@@ -186,7 +160,7 @@ def main(debug=False):
   else:
     name = sys.argv[1]
     print('Running task [{}]...'.format(name))
-    task = tc.Task(name)
+    task = ti.Task(name)
     task.run(*sys.argv[2:])
   print()
   print(">>> Running time: {:.2f}s".format(time.time() - t))

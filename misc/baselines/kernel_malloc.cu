4aea219cd7c289ddb65853b3e13ae361cf08e1ea
Yuanming Hu <yuanmhu@gmail.com>
2019-11-02 12:15:37 -0400

    test basic custom mem alloc

--- misc/baselines/kernel_malloc.cu
+++ misc/baselines/kernel_malloc.cu
@@ -1,11 +1,15 @@
+#include "cuda_runtime.h"
+#include <cstdio>
 #include "time.h"
 
 constexpr int segment_size = 1024;
 constexpr int threads = 512;
+__device__ char *pool;
 
 void __global__ alloc(int **pointers) {
   auto index = blockIdx.x * blockDim.x + threadIdx.x;
-  pointers[index] = (int *)malloc(segment_size);
+  // pointers[index] = (int *)malloc(segment_size);
+  pointers[index] = (int *)atomicAdd((unsigned long long *)&pool, segment_size);
 }
 
 void __global__ fill(int **pointers) {
@@ -17,7 +21,7 @@ void __global__ fill(int **pointers) {
 
 void __global__ free(int **pointers) {
   auto index = blockIdx.x * blockDim.x + threadIdx.x;
-  free(pointers[index]);
+  // free(pointers[index]);
 }
 
 int main() {
@@ -26,6 +30,9 @@ int main() {
 
   int bd = 32;
   for (int i = 0; i < 10; i++) {
+    char *pool_;
+    cudaMallocManaged(&pool_, segment_size * threads);
+    cudaMemcpyToSymbol(pool, &pool_, sizeof(void *));
     alloc<<<threads / bd, bd>>>(pointers);
     fill<<<threads / bd, bd>>>(pointers);
     free<<<threads / bd, bd>>>(pointers);

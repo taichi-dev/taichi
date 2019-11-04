0303f82dc180c52ff790fa1e7954bc5379240f71
Yuanming Hu <yuanmhu@gmail.com>
2019-11-04 11:38:04 -0500

    try fix Mac build

--- cmake/TaichiCore.cmake
+++ cmake/TaichiCore.cmake
@@ -92,12 +92,12 @@ if (NOT WIN32)
     target_link_libraries(${CORE_LIBRARY_NAME} pthread stdc++)
     if (APPLE)
         # OS X
+        # Ask OS X to minic Linux dynamic linking hebavior
+        target_link_libraries(${CORE_LIBRARY_NAME} PRIVATE "-undefined dynamic_lookup")
     else()
         # Linux
         target_link_libraries(${CORE_LIBRARY_NAME} stdc++fs X11)
         target_link_libraries(${CORE_LIBRARY_NAME} -static-libgcc -static-libstdc++)
-        # Ask OS X to minic Linux dynamic linking hebavior
-        target_link_libraries(${CORE_LIBRARY_NAME} PRIVATE "-undefined dynamic_lookup")
     endif()
 endif ()
 message("PYTHON_LIBRARIES" ${PYTHON_LIBRARIES})

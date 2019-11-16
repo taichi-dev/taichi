dcef3195be89f8f16944ee544eb8e5db2bcd0261
Yuanming Hu <yuanmhu@gmail.com>
2019-11-16 15:56:40 -0500

    Fixed pytorch binding

--- python/taichi/torch_io.py
+++ python/taichi/torch_io.py
@@ -1,28 +1,24 @@
-import taichi.lang as ti
+import taichi as ti
+import numpy as np
+
+@ti.kernel
+def from_torch_template(expr: ti.template(), torch_tensor: ti.ext_arr()):
+  for i in expr:
+    expr[i] = torch_tensor[i]
+
+@ti.kernel
+def to_torch_template(expr: ti.template(), torch_tensor: ti.ext_arr()):
+  for i in expr:
+    torch_tensor[i] = expr[i]
+
 
 def from_torch(expr, torch_tensor):
   if not expr.from_torch_:
-    import taichi as ti
-    import numpy as np
-    @ti.kernel
-    def ker(torch_tensor: np.ndarray):
-      for i in expr:
-        expr[i] = torch_tensor[i]
-    
-    ker.materialize()
-    expr.from_torch_ = lambda x: ker(x.contiguous())
+    expr.from_torch_ = lambda x: from_torch_template(expr, x.contiguous())
   expr.from_torch_(torch_tensor)
 
 def to_torch(expr, torch_tensor):
   if not expr.to_torch_:
-    import taichi as ti
-    import numpy as np
-    @ti.kernel
-    def ker(torch_tensor: np.ndarray):
-      for i in expr:
-        torch_tensor[i] = expr[i]
-    
-    ker.materialize()
-    expr.to_torch_ = lambda x: ker(x.contiguous())
+    expr.to_torch_ = lambda x: to_torch_template(expr, x.contiguous())
   expr.to_torch_(torch_tensor)
 

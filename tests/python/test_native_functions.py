33ef4ed463ff0f37caf983f350ab08099448df15
Yuanming Hu <yuanmhu@gmail.com>
2019-11-22 18:42:41 +1100

    test_native_functions.py

---
+++ tests/python/test_native_functions.py
@@ -0,0 +1,18 @@
+import taichi as ti
+
+@ti.all_archs
+def test_abs():
+  x = ti.var(ti.f32)
+
+  N = 16
+  @ti.layout
+  def place():
+    ti.root.dense(ti.i, N).place(x)
+
+  @ti.kernel
+  def func():
+    for i in range(N):
+      x[i] = abs(-i)
+
+  for i in range(N):
+    assert x[i] == i

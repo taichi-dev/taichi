9a33a1e34aada2e3884685cd5e7f590ae3511c5c
Yuanming Hu <yuanmhu@gmail.com>
2019-11-28 19:30:35 +0800

    test unordered tensor size reflection

--- tests/python/test_tensor_reflection.py
+++ tests/python/test_tensor_reflection.py
@@ -29,3 +29,18 @@ def test_POT():
 
   assert val.dim() == 3
   assert val.shape() == (n, m, p)
+
+@ti.all_archs
+def test_unordered():
+  val = ti.var(ti.i32)
+
+  n = 3
+  m = 7
+  p = 11
+
+  @ti.layout
+  def values():
+    ti.root.dense(ti.k, n).dense(ti.i, m).dense(ti.j, p).place(val)
+
+  assert val.dim() == 3
+  assert val.shape() == (n, m, p)

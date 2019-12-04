93a7fdd685c2b05a4cde901f6ef1543fa8f88e8d
Yuanming Hu <yuanmhu@gmail.com>
2019-12-04 19:19:37 +0800

    mpm99->mpm88

--- examples/mpm88.py
+++ examples/mpm88.py
@@ -53,10 +53,8 @@ def substep():
     base = (x[p] * inv_dx - 0.5).cast(int)
     fx = x[p] * inv_dx - base.cast(float)
     w = [0.5 * ti.sqr(1.5 - fx), 0.75 - ti.sqr(fx - 1.0), 0.5 * ti.sqr(fx - 0.5)]
-
     new_v = ti.Vector.zero(ti.f32, 2)
     new_C = ti.Matrix.zero(ti.f32, 2, 2)
-
     for i in ti.static(range(3)):
       for j in ti.static(range(3)):
         dpos = ti.Vector([i, j]).cast(float) - fx
@@ -64,7 +62,6 @@ def substep():
         weight = w[i][0] * w[j][1]
         new_v += weight * g_v
         new_C += 4 * weight * ti.outer_product(g_v, dpos) * inv_dx
-
     v[p] = new_v
     x[p] += dt * v[p]
     J[p] *= 1 + dt * new_C.trace()

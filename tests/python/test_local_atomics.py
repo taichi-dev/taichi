c08baa5af0c230975fc38101e8b025d63e0e210b
Yuanming Hu <yuanmhu@gmail.com>
2019-12-06 17:45:53 +0800

    .

---
+++ tests/python/test_local_atomics.py
@@ -0,0 +1,30 @@
+import taichi as ti
+
+@ti.all_archs
+def test_explicit_local_atomics():
+  A = ti.var(ti.f32, shape=())
+
+  @ti.kernel
+  def func():
+    a = 0
+    for i in range(10):
+      ti.atomic_add(a, i)
+    A[None] = a
+
+  func()
+  assert A[None] == 45
+
+@ti.all_archs
+def test_implicit_local_atomics():
+  A = ti.var(ti.f32, shape=())
+
+  @ti.kernel
+  def func():
+    a = 0
+    for i in range(10):
+      a += i
+    A[None] = a
+
+  func()
+  assert A[None] == 45
+

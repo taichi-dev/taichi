265d1d122b1e63c4f2328fd739e833fac5554fb5
Yuanming Hu <yuanmhu@gmail.com>
2019-12-12 11:37:58 -0500

    test_mpm88

---
+++ tests/python/test_mpm88.py
@@ -0,0 +1,103 @@
+import taichi as ti
+from pytest import approx
+
+@ti.all_archs
+def test_mpm88():
+  dim = 2; N = 64
+  n_particles = N * N; n_grid = 128
+  dx = 1 / n_grid; inv_dx = 1 / dx
+  dt = 2.0e-4
+  p_vol = (dx * 0.5) ** 2
+  p_rho = 1; p_mass = p_vol * p_rho
+  E = 400
+
+  x = ti.Vector(dim, dt=ti.f32, shape=n_particles)
+  v = ti.Vector(dim, dt=ti.f32, shape=n_particles)
+  C = ti.Matrix(dim, dim, dt=ti.f32, shape=n_particles)
+  J = ti.var(dt=ti.f32, shape=n_particles)
+  grid_v = ti.Vector(dim, dt=ti.f32, shape=(n_grid, n_grid))
+  grid_m = ti.var(dt=ti.f32, shape=(n_grid, n_grid))
+
+  ti.cfg.arch = ti.cuda
+
+  @ti.kernel
+  def substep():
+    for p in x:
+      base = (x[p] * inv_dx - 0.5).cast(int)
+      fx = x[p] * inv_dx - base.cast(float)
+      w = [0.5 * ti.sqr(1.5 - fx), 0.75 - ti.sqr(fx - 1), 0.5 * ti.sqr(fx - 0.5)]
+      stress = -dt * p_vol * (J[p] - 1) * 4 * inv_dx * inv_dx * E
+      affine = ti.Matrix([[stress, 0], [0, stress]]) + p_mass * C[p]
+      for i in ti.static(range(3)):
+        for j in ti.static(range(3)):
+          offset = ti.Vector([i, j])
+          dpos = (offset.cast(float) - fx) * dx
+          weight = w[i][0] * w[j][1]
+          grid_v[base + offset].atomic_add(weight * (p_mass * v[p] + affine @ dpos))
+          grid_m[base + offset].atomic_add(weight * p_mass)
+
+    for i, j in grid_m:
+      if grid_m[i, j] > 0:
+        bound = 3
+        inv_m = 1 / grid_m[i, j]
+        grid_v[i, j] = inv_m * grid_v[i, j]
+        grid_v[i, j][1] -= dt * 9.8
+        if i < bound and grid_v[i, j][0] < 0:
+          grid_v[i, j][0] = 0
+        if i > n_grid - bound and grid_v[i, j][0] > 0:
+          grid_v[i, j][0] = 0
+        if j < bound and grid_v[i, j][1] < 0:
+          grid_v[i, j][1] = 0
+        if j > n_grid - bound and grid_v[i, j][1] > 0:
+          grid_v[i, j][1] = 0
+
+    for p in x:
+      base = (x[p] * inv_dx - 0.5).cast(int)
+      fx = x[p] * inv_dx - base.cast(float)
+      w = [0.5 * ti.sqr(1.5 - fx), 0.75 - ti.sqr(fx - 1.0), 0.5 * ti.sqr(fx - 0.5)]
+      new_v = ti.Vector.zero(ti.f32, 2)
+      new_C = ti.Matrix.zero(ti.f32, 2, 2)
+      for i in ti.static(range(3)):
+        for j in ti.static(range(3)):
+          dpos = ti.Vector([i, j]).cast(float) - fx
+          g_v = grid_v[base + ti.Vector([i, j])]
+          weight = w[i][0] * w[j][1]
+          new_v += weight * g_v
+          new_C += 4 * weight * ti.outer_product(g_v, dpos) * inv_dx
+      v[p] = new_v
+      x[p] += dt * v[p]
+      J[p] *= 1 + dt * new_C.trace()
+      C[p] = new_C
+
+  # gui = ti.core.GUI("MPM88", ti.veci(512, 512))
+  # canvas = gui.get_canvas()
+
+  for i in range(n_particles):
+    x[i] = [i % N / N * 0.4 + 0.2, i / N / N * 0.4 + 0.05]
+    v[i] = [0, -3]
+    J[i] = 1
+
+  for frame in range(10):
+    for s in range(50):
+      grid_v.fill([0, 0])
+      grid_m.fill(0)
+      substep()
+
+
+  pos = x.to_numpy(as_vector=True)
+  pos[:, 1] *= 2
+  regression = [0.31722742,
+                0.15826741,
+                0.10224003,
+                0.07810827,
+                ]
+  for i in range(4):
+    assert (pos ** (i + 1)).mean() == approx(regression[i], rel=1e-4)
+
+
+  '''
+  canvas.clear(0x112F41)
+  for i in range(n_particles):
+    canvas.circle(ti.vec(pos[i, 0], pos[i, 1])).radius(1.5).color(0x068587).finish()
+  gui.update()
+  '''

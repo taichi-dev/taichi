4c12a6b7c6a488478ca620aa5d7f40272eaf0a0b
Yuanming Hu <yuanmhu@gmail.com>
2019-12-13 00:36:05 -0500

    test_dynamic_matrix

--- tests/python/test_dynamic.py
+++ tests/python/test_dynamic.py
@@ -40,3 +40,28 @@ def test_dynamic2():
   
   for i in range(n):
     assert x[i] == i
+
+
+
+@ti.all_archs
+def test_dynamic_matrix():
+  if ti.cfg.arch == ti.cuda:
+    return
+  
+  x = ti.Matrix(3, 2, dt=ti.f32)
+  n = 8192
+  
+  @ti.layout
+  def place():
+    ti.root.dynamic(ti.i, n, chunk_size=128).place(x)
+  
+  @ti.kernel
+  def func():
+    for i in range(n // 4):
+      x[i * 4][1, 0] = i
+  
+  func()
+  
+  for i in range(n // 4):
+    assert x[i * 4][1, 0] == i
+    assert x[i * 4 + 1][1, 0] == 0

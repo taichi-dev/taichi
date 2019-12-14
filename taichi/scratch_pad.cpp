538ac18de53f0cab9019cbcac2041d8c756f0493
Yuanming Hu <yuanmhu@gmail.com>
2019-12-13 22:57:51 -0500

    removed all legacy cpp taichi lang tests

--- taichi/scratch_pad.cpp
+++ taichi/scratch_pad.cpp
@@ -2,41 +2,4 @@
 
 TLANG_NAMESPACE_BEGIN
 
-TC_TEST("scratch_pad_bounds") {
-  Program prog;
-
-  int N = 8;
-
-  Global(x, i32);
-  SNode *block;
-
-  layout([&] {
-    auto ijk = Indices(0, 1, 2);
-    block = &root.dense(ijk, N).dense(ijk, N);
-    block->place(x);
-  });
-
-  ScratchPad pad(block);
-
-  pad.access({1, 2, -3}, ScratchPad::AccessFlag::read);
-
-  TC_CHECK(pad.bounds[0][0] == 1);
-  TC_CHECK(pad.bounds[0][1] == 2);
-  TC_CHECK(pad.bounds[0][2] == -3);
-
-  TC_CHECK(pad.bounds[1][0] == 2);
-  TC_CHECK(pad.bounds[1][1] == 3);
-  TC_CHECK(pad.bounds[1][2] == -2);
-
-  pad.access({4, -2, 5}, ScratchPad::AccessFlag::read);
-
-  TC_CHECK(pad.bounds[0][0] == 1);
-  TC_CHECK(pad.bounds[0][1] == -2);
-  TC_CHECK(pad.bounds[0][2] == -3);
-
-  TC_CHECK(pad.bounds[1][0] == 5);
-  TC_CHECK(pad.bounds[1][1] == 3);
-  TC_CHECK(pad.bounds[1][2] == 6);
-}
-
 TLANG_NAMESPACE_END

11a7fc75ebf4f5cf8c8ebec39d044d6ea7b544d2
Yuanming Hu <yuanmhu@gmail.com>
2019-12-21 18:28:17 -0500

    svd 3x3 f32 passed test

--- tests/python/test_svd.py
+++ tests/python/test_svd.py
@@ -1,4 +1,5 @@
 import taichi as ti
+import numpy as np
 from pytest import approx
 
 def svd(A, iters=5):
@@ -19,19 +20,38 @@ def svd(A, iters=5):
     sigma(i, i).assign(sig_entries[i])
   return U, sigma, V
 
+def mat_equal(A, B):
+  return np.max(np.abs(A - B)) < 1e-5
+
 @ti.all_archs
 def test_transpose():
   A = ti.Matrix(3, 3, dt=ti.f32, shape=())
+  A_reconstructed = ti.Matrix(3, 3, dt=ti.f32, shape=())
   U = ti.Matrix(3, 3, dt=ti.f32, shape=())
+  UtU = ti.Matrix(3, 3, dt=ti.f32, shape=())
   sigma = ti.Matrix(3, 3, dt=ti.f32, shape=())
   V = ti.Matrix(3, 3, dt=ti.f32, shape=())
+  VtV = ti.Matrix(3, 3, dt=ti.f32, shape=())
   
   @ti.kernel
   def run():
     for i in range(1):
       U[None], sigma[None], V[None] = svd(A[None])
+      UtU[None] = ti.transposed(U[None]) @ U[None]
+      VtV[None] = ti.transposed(V[None]) @ V[None]
+      A_reconstructed[None] = U[None] @ sigma[None] @ ti.transposed(V[None])
     
+  A[None] = [[1, 1, 3], [9, -3, 2], [-3, 4, 2]]
+  
   run()
+  
+  assert mat_equal(UtU.to_numpy(), np.eye(3))
+  assert mat_equal(VtV.to_numpy(), np.eye(3))
+  assert mat_equal(A_reconstructed.to_numpy(), A.to_numpy())
+  for i in range(3):
+    for j in range(3):
+      if i != j:
+        assert sigma[None][i, j] == approx(0)
 
 
 @ti.all_archs

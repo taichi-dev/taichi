40a357e4915613b9a5714d0a3f56045da254fe7e
Yuanming Hu <yuanmhu@gmail.com>
2019-12-20 20:34:40 -0500

    test_offload_order

---
+++ tests/python/test_ad_offload.py
@@ -0,0 +1,24 @@
+import taichi as ti
+
+@ti.all_archs
+def test_offload_order():
+  n = 128
+  x = ti.var(ti.f32, shape=(n), needs_grad=True)
+  y = ti.var(ti.f32, shape=(n), needs_grad=True)
+  z = ti.var(ti.f32, shape=(), needs_grad=True)
+  
+  @ti.kernel
+  def forward():
+    for i in x:
+      y[i] = x[i]
+      
+    for i in x:
+      z[None] += y[i]
+      
+  with ti.Tape(z):
+    forward()
+  
+  for i in range(n):
+    assert x.grad[i] == 1
+
+

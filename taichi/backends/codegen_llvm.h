ad139bc33f203a2a09e7f6fe3720789517804b52
Yuanming Hu <yuanmhu@gmail.com>
2019-12-26 18:38:17 -0500

    test_zero_inner_loop fixed; do the test first in loop codegen

--- taichi/backends/codegen_llvm.h
+++ taichi/backends/codegen_llvm.h
@@ -699,6 +699,7 @@ class CodeGenLLVM : public IRVisitor, public ModuleBuilder {
   void create_naive_range_for(RangeForStmt *for_stmt) {
     BasicBlock *body = BasicBlock::Create(*llvm_context, "loop_body", func);
     BasicBlock *after_loop = BasicBlock::Create(*llvm_context, "block", func);
+    BasicBlock *test = BasicBlock::Create(*llvm_context, "test", func);
     if (!for_stmt->reversed) {
       builder->CreateStore(for_stmt->begin->value, for_stmt->loop_var->value);
     } else {
@@ -706,27 +707,37 @@ class CodeGenLLVM : public IRVisitor, public ModuleBuilder {
           builder->CreateSub(for_stmt->end->value, tlctx->get_constant(1)),
           for_stmt->loop_var->value);
     }
-    builder->CreateBr(body);
+    builder->CreateBr(test);
 
-    // body cfg
-    builder->SetInsertPoint(body);
-
-    for_stmt->body->accept(this);
-
-    llvm::Value *cond = nullptr;
-    if (!for_stmt->reversed) {
-      create_increment(for_stmt->loop_var->value, tlctx->get_constant(1));
-      cond = builder->CreateICmp(llvm::CmpInst::Predicate::ICMP_SLT,
-                                 builder->CreateLoad(for_stmt->loop_var->value),
-                                 for_stmt->end->value);
-    } else {
-      create_increment(for_stmt->loop_var->value, tlctx->get_constant(-1));
-      cond = builder->CreateICmp(llvm::CmpInst::Predicate::ICMP_SGE,
-                                 builder->CreateLoad(for_stmt->loop_var->value),
-                                 for_stmt->begin->value);
+    {
+      // test block
+      builder->SetInsertPoint(test);
+      llvm::Value *cond;
+      if (!for_stmt->reversed) {
+        cond = builder->CreateICmp(llvm::CmpInst::Predicate::ICMP_SLT,
+                                   builder->CreateLoad(for_stmt->loop_var->value),
+                                   for_stmt->end->value);
+      } else {
+        cond = builder->CreateICmp(llvm::CmpInst::Predicate::ICMP_SGE,
+                                   builder->CreateLoad(for_stmt->loop_var->value),
+                                   for_stmt->begin->value);
+      }
+      builder->CreateCondBr(cond, body, after_loop);
     }
 
-    builder->CreateCondBr(cond, body, after_loop);
+    {
+      // body cfg
+      builder->SetInsertPoint(body);
+
+      for_stmt->body->accept(this);
+
+      if (!for_stmt->reversed) {
+        create_increment(for_stmt->loop_var->value, tlctx->get_constant(1));
+      } else {
+        create_increment(for_stmt->loop_var->value, tlctx->get_constant(-1));
+      }
+      builder->CreateBr(test);
+    }
 
     // next cfg
     builder->SetInsertPoint(after_loop);

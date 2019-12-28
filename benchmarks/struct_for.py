f442f836680c1c338c0083d4719d8eb6d855f002
Yuanming Hu <yuanmhu@gmail.com>
2019-12-28 14:08:58 -0500

    struct_for benchmark

---
+++ benchmarks/struct_for.py
@@ -0,0 +1,42 @@
+# originally by @KLozes
+
+import taichi as ti
+import time
+# ti.cfg.arch = ti.x86_64
+# ti.cfg.arch = ti.cuda
+
+a = ti.var(dt=ti.f32)
+N = 128
+
+@ti.layout
+def place():
+  ti.root.dense(ti.ij, [N,N]).dense(ti.ij, [8,8]).place(a)
+
+@ti.kernel
+def set1():
+  for i,j in a:
+    a[i,j] = 2.0
+
+@ti.kernel
+def set2():
+  for j in range(N*8):
+    for i in range(N*8):
+      a[i,j] = 2.0
+
+set1()
+set2()
+
+t = time.time()
+for n in range(100):
+  set1()
+elapsed = time.time() - t
+ti.get_runtime().sync()
+print(elapsed * 10, 'ms/iter')
+
+t = time.time()
+for n in range(100):
+  set2()
+elapsed = time.time() - t
+ti.get_runtime().sync()
+print(elapsed * 10, 'ms/iter')
+ti.profiler_print()

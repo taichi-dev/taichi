8e661a03b888415313b2bf6a3265e5fb4af9862e
Yuanming Hu <yuanmhu@gmail.com>
2019-12-29 08:07:15 -0500

    test_struct_for_dynamic

---
+++ tests/python/test_struct_for_dynamic.py
@@ -0,0 +1,24 @@
+import taichi as ti
+
+@ti.all_archs
+def test_dynamic():
+  x = ti.var(ti.i32)
+  y = ti.var(ti.i32, shape=())
+
+  n = 128
+
+  @ti.layout
+  def place():
+    ti.root.dynamic(ti.i, n * n).place(x)
+
+  @ti.kernel
+  def count():
+    for i in x:
+      y[None] += 1
+      
+  x[n // 3] = 1
+
+  count()
+
+  assert y[None] == n // 3 + 1
+

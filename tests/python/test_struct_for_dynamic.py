a4f713f99eb08f7d0b992628ceada66a49cc3041
Yuanming Hu <yuanmhu@gmail.com>
2019-12-30 11:45:39 -0500

    test_dense_dynamic

--- tests/python/test_struct_for_dynamic.py
+++ tests/python/test_struct_for_dynamic.py
@@ -24,3 +24,33 @@ def test_dynamic():
 
   assert y[None] == n // 3 + 1
 
+
+@ti.all_archs
+def test_dense_dynamic():
+  n = 128
+  
+  x = ti.var(ti.i32)
+  y = ti.var(ti.i32, shape=(n))
+  
+  @ti.layout
+  def place():
+    ti.root.dense(ti.i, n).dynamic(ti.j, n * 2).place(x)
+  
+  @ti.kernel
+  def append():
+    for i in range(n):
+      for j in range(i):
+        ti.append(x, i, j * 2)
+    
+  append()
+  
+  @ti.kernel
+  def count():
+    for i, j in x:
+      y[i] += x[i, j]
+  
+  count()
+  
+  for i in range(n):
+    print(i)
+    assert y[i] == i * (i - 1)

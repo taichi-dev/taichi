8acad6c2ed10be4fa1cbcc319d1566c5952a0f7d
Yuanming Hu <yuanmhu@gmail.com>
2020-01-05 19:53:40 -0500

    benchmark OOP

--- benchmarks/run.py
+++ benchmarks/run.py
@@ -1,14 +1,35 @@
 import os
 
-for f in sorted(os.listdir('.')):
-  if f != 'run.py' and f.endswith('.py'):
-    suite = None
-    f = f[:-3]
-    exec(f'import {f} as suite')
-    benchmarks = list(sorted(filter(lambda x: x.startswith('benchmark_'), dir(suite))))
-    print(f'Suite {f}:')
-    for b in benchmarks:
+class Suite:
+  def __init__(self, filename):
+    self.cases = []
+    self.name = filename[:-3]
+    loc = {}
+    exec(f'import {self.name} as suite', {}, loc)
+    suite = loc['suite']
+    case_keys = list(sorted(filter(lambda x: x.startswith('benchmark_'), dir(suite))))
+    self.cases = {k: getattr(suite, k) for k in case_keys}
+    
+  def print(self):
+    print(f'Suite {self.name}:')
+    for b in self.cases:
       print(' *', b)
+
+class TaichiBenchmark:
+  def __init__(self):
+    self.suites = []
+    for f in sorted(os.listdir('.')):
+      if f != 'run.py' and f.endswith('.py') and f[0] != '_':
+        self.suites.append(Suite(f))
+        
+        
+  def pprint(self):
+    for s in self.suites:
+      s.print()
+
+
+b = TaichiBenchmark()
+b.pprint()
 
 def run():
   return

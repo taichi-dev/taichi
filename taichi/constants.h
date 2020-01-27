a095cf403d5f79dfc4430104696dd3cdc8763d03
Yuanming Hu <yuanmhu@gmail.com>
2020-01-27 17:29:54 -0500

    more mem requests allowed

--- taichi/constants.h
+++ taichi/constants.h
@@ -4,6 +4,6 @@ constexpr int taichi_max_num_indices = 8;
 constexpr int taichi_max_num_args = 8;
 constexpr int taichi_max_num_snodes = 1024;
 constexpr int taichi_max_num_global_vars = 1024 * 1024;
-constexpr int taichi_max_num_mem_requests = 1024;
+constexpr int taichi_max_num_mem_requests = 1024 * 64;
 
 using assert_failed_type = void (*)(const char *);

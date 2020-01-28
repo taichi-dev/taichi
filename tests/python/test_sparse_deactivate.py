a9821e8d89f56405a718cde6917c67c949999b29
Yuanming Hu <yuanmhu@gmail.com>
2020-01-28 15:43:49 -0500

    test_sparse_deactivate

--- tests/python/test_sparse_deactivate.py
+++ tests/python/test_sparse_deactivate.py
@@ -2,32 +2,34 @@ import taichi as ti
 
 @ti.all_archs
 def test_pointer():
-  return
   x = ti.var(ti.f32)
   s = ti.var(ti.i32)
 
-  n = 128
+  n = 16
 
   @ti.layout
   def place():
     ti.root.dense(ti.i, n).pointer().dense(ti.i, n).place(x)
     ti.root.place(s)
 
+  s[None] = 0
+
   @ti.kernel
   def func():
     for i in x:
       s[None] += 1
 
+
   x[0] = 1
-  x[127] = 1
-  x[254] = 1
-  x[256 + n * n] = 1
-
-  x[257 + n * n] = 1
-  x[257 + n * n * 2] = 1
-  x[257 + n * n * 5] = 1
-
+  x[19] = 1
   func()
-  assert s[None] == 5 * n
-  print(x[257 + n * n * 7])
-  assert s[None] == 5 * n
+  assert s[None] == 32
+
+  @ti.kernel
+  def deactivate():
+    ti.deactivate(x.parent().parent(), 4)
+
+  deactivate()
+  s[None] = 0
+  func()
+  assert s[None] == 16

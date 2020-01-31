56e8351201941811134b1a33281248cf5ee46b43
Ye Kuang <yekuang.ky@gmail.com>
2020-01-31 20:11:23 +0800

    Move ObjC call APIs into a separate module in prep for the Metal backend

---
+++ taichi/platform/mac/objc_api.cpp
@@ -0,0 +1,17 @@
+#include "objc_api.h"
+
+namespace taichi {
+namespace mac {
+
+nsobj_unique_ptr<TC_NSString> wrap_string_as_ns_string(const std::string &str) {
+  constexpr int kNSUTF8StringEncoding = 4;
+  id ns_string = clscall("NSString", "alloc");
+  auto *ptr = cast_call<TC_NSString *>(
+      ns_string,
+      "initWithBytesNoCopy:length:encoding:freeWhenDone:", str.data(),
+      str.size(), kNSUTF8StringEncoding, false);
+  return wrap_as_nsobj_unique_ptr(ptr);
+}
+
+}  // namespace mac
+}  // namespace taichi

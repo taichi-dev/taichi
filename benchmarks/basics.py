e50fff34e4442f03dec68e1a6a085f05dbdf62f7
Yuanming Hu <yuanmhu@gmail.com>
2020-02-02 00:39:33 -0500

    benchmark memory bound operations

--- benchmarks/basics.py
+++ benchmarks/basics.py
@@ -1,12 +1,42 @@
 import taichi as ti
 
+# Memory bound operations
+
+N = 1024 ** 3 // 4
+
+# 4 B/it
 def benchmark_memset():
-  a = ti.var(dt=ti.i32, shape=1024 ** 2)
+  a = ti.var(dt=ti.f32, shape=N)
   
   @ti.kernel
   def memset():
     for i in a:
       a[i] = 1.0
   
-  return ti.benchmark(memset)
+  return ti.benchmark(memset, repeat=10)
 
+# 8 B/it
+def benchmark_memcpy():
+  a = ti.var(dt=ti.f32, shape=N)
+  b = ti.var(dt=ti.f32, shape=N)
+  
+  @ti.kernel
+  def memcpy():
+    for i in a:
+      a[i] = b[i]
+  
+  return ti.benchmark(memcpy, repeat=10)
+
+# 12 B/it
+def benchmark_saxpy():
+  x = ti.var(dt=ti.f32, shape=N)
+  y = ti.var(dt=ti.f32, shape=N)
+  z = ti.var(dt=ti.f32, shape=N)
+  
+  @ti.kernel
+  def task():
+    for i in x:
+      a = 123
+      z[i] = a * x[i] + y[i]
+      
+  return ti.benchmark(task, repeat=10)

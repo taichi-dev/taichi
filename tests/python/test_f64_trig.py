28cbc857532ccf65d2dd2946e56bc8a856930b65
Kenneth Lozes <klozes@system76-pc.localdomain>
2020-02-03 21:15:48 -0700

    test precision of sin/cos

--- tests/python/test_f64_trig.py
+++ tests/python/test_f64_trig.py
@@ -3,6 +3,7 @@ import taichi as ti
 
 @ti.all_archs
 def test_f64_sin():
+  from numpy import sin
   val = ti.var(ti.f64)
 
   @ti.layout
@@ -10,14 +11,19 @@ def test_f64_sin():
     ti.root.dense(ti.i, 8).place(val)
 
   @ti.kernel
-  def test_sin():
+  def fill_sin():
     for i in range(8):
       val[i] = ti.sin(ti.cast(i,ti.f64))
 
-  test_sin()
+  fill_sin()
+
+  # check that it is double precision
+  for i in range(8):
+    assert(abs(sin(float(i))-val[i]) < 1.0e-15)
 
 @ti.all_archs
 def test_f64_cos():
+  from numpy import cos
   val = ti.var(ti.f64)
 
   @ti.layout
@@ -25,8 +31,12 @@ def test_f64_cos():
     ti.root.dense(ti.i, 8).place(val)
 
   @ti.kernel
-  def test_cos():
+  def fill_cos():
     for i in range(8):
       val[i] = ti.cos(ti.cast(i,ti.f64))
 
-  test_cos()
+  fill_cos()
+
+  # check that it is double precision
+  for i in range(8):
+    assert(abs(cos(float(i))-val[i]) < 1.0e-15)

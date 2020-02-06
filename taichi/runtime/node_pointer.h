c78e7e7ebdcf61c4b08f830b42c98d3ebd6facad
Yuanming Hu <yuanmhu@gmail.com>
2020-02-05 21:13:00 -0500

    optimize pointer (de)adtivation

--- taichi/runtime/node_pointer.h
+++ taichi/runtime/node_pointer.h
@@ -9,29 +9,33 @@ STRUCT_FIELD(PointerMeta, _);
 
 void Pointer_activate(Ptr meta, Ptr node, int i) {
   Ptr lock = node;
-  locked_task(lock, [&] {
-    Ptr &data_ptr = *(Ptr *)(node + 8);
-    if (data_ptr == nullptr) {
-      auto smeta = (StructMeta *)meta;
-      auto rt = smeta->context->runtime;
-      auto alloc = rt->node_allocators[smeta->snode_id];
-      data_ptr = alloc->allocate();
-    }
-  });
+  Ptr &data_ptr = *(Ptr *)(node + 8);
+  if (data_ptr == nullptr) {
+    locked_task(lock, [&] {
+      if (data_ptr == nullptr) {
+        auto smeta = (StructMeta *)meta;
+        auto rt = smeta->context->runtime;
+        auto alloc = rt->node_allocators[smeta->snode_id];
+        data_ptr = alloc->allocate();
+      }
+    });
+  }
 }
 
 void Pointer_deactivate(Ptr meta, Ptr node) {
   Ptr lock = node;
-  locked_task(lock, [&] {
-    Ptr &data_ptr = *(Ptr *)(node + 8);
-    if (data_ptr != nullptr) {
-      auto smeta = (StructMeta *)meta;
-      auto rt = smeta->context->runtime;
-      auto alloc = rt->node_allocators[smeta->snode_id];
-      alloc->recycle(data_ptr);
-      data_ptr = nullptr;
-    }
-  });
+  Ptr &data_ptr = *(Ptr *)(node + 8);
+  if (data_ptr != nullptr) {
+    locked_task(lock, [&] {
+      if (data_ptr != nullptr) {
+        auto smeta = (StructMeta *)meta;
+        auto rt = smeta->context->runtime;
+        auto alloc = rt->node_allocators[smeta->snode_id];
+        alloc->recycle(data_ptr);
+        data_ptr = nullptr;
+      }
+    });
+  }
 }
 
 bool Pointer_is_active(Ptr meta, Ptr node, int i) {
@@ -44,7 +48,7 @@ void *Pointer_lookup_element(Ptr meta, Ptr node, int i) {
   if (data_ptr == nullptr) {
     auto smeta = (StructMeta *)meta;
     auto context = smeta->context;
-    data_ptr = ((Runtime *)context->runtime)->ambient_elements[smeta->snode_id];
+    data_ptr = (context->runtime)->ambient_elements[smeta->snode_id];
   }
   return data_ptr;
 }

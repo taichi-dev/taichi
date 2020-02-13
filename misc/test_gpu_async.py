5d63cada9b050cf0e4ac14037c05aca7d05e21bf
Yuanming Hu <yuanmhu@gmail.com>
2020-02-12 20:20:04 -0500

    misc/test_gpu_async.py

---
+++ misc/test_gpu_async.py
@@ -0,0 +1,16 @@
+import taichi as ti
+import time
+
+ti.init(arch=ti.cuda)
+
+a = ti.var(dt=ti.f32, shape=(1024 * 1024 * 1024))
+
+@ti.kernel
+def fill(x: ti.f32):
+  for i in a:
+    a[i] = x
+    
+for i in range(100):
+  t = time.time()
+  fill(i)
+  print(time.time() - t)

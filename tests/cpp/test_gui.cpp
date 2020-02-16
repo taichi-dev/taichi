f2792a6119c96518e32445c8299475ee0ecdcb25
Yuanming Hu <yuanmhu@gmail.com>
2020-02-16 16:50:58 -0500

    restored GUI test

--- tests/cpp/test_gui.cpp
+++ tests/cpp/test_gui.cpp
@@ -1,10 +1,12 @@
+// Note: this is not really a test case.
+
 #include <taichi/visual/gui.h>
 #include <taichi/common/task.h>
 
 TC_NAMESPACE_BEGIN
 
-auto test_bemo = []() {
-  GUI gui("Bemo Test", 1000, 400, false);
+auto test_gui = []() {
+  GUI gui("GUI Test", 1000, 400, false);
   auto canvas = *gui.canvas;
   real t = 0;
 
@@ -21,24 +23,28 @@ auto test_bemo = []() {
     for (int i = 0; i < 30; i++) {
       canvas.circle(i * 10 + 100, 250 + std::sin(t + i * 0.1_f) * 50_f)
           .color(0.7_f, 0.2_f, 0.0_f, 0.9_f)
-          .radius(5);
+          .radius(5)
+          .finish();
     }
-    canvas.color(0.0_f, 0.0_f, 1.0_f, 1.0_f).radius(5 + 2 * std::sin(t * 10_f));
+    canvas.color(0.0_f, 0.0_f, 1.0_f, 1.0_f)
+        .radius(5 + 2 * std::sin(t * 10_f));
     canvas.path()
         .path(Vector2(100, 100), Vector2(200, 75 + std::cos(t) * 50_f),
               Vector2(300, 75 + std::cos(t) * 50_f))
         .close()
         .color(0, 0, 0)
-        .width(5);
+        .width(5)
+        .finish();
 
     for (int i = 0; i < circle_count; i++) {
       canvas.circle(i * 10 + 100, 150 + std::sin(t + i * 0.1_f) * 50_f)
-          .radius(radius);
+          .radius(radius)
+          .finish();
     }
     gui.update();
   }
 };
 
-TC_REGISTER_TASK(test_bemo);
+TC_REGISTER_TASK(test_gui);
 
 TC_NAMESPACE_END

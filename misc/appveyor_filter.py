766e742dc7159e32b8daa3cce296243723e0283d
archibate <17721388340@163.com>
2020-02-17 16:31:21 +0800

    mistake in appveyor_filter.py

--- misc/appveyor_filter.py
+++ misc/appveyor_filter.py
@@ -3,6 +3,6 @@ import os
 
 msg = os.environ["APPVEYOR_REPO_COMMIT_MESSAGE"]
 if msg.startswith('[release]') or sys.version_info[1] == 6:
-  exit(1) # Build for this configuration (starts with '[release]', or python version is 3.6)
+  exit(0) # Build for this configuration (starts with '[release]', or python version is 3.6)
 else:
-  exit(0) # Do not build this configuration. See appveyor.yml
+  exit(1) # Do not build this configuration. See appveyor.yml

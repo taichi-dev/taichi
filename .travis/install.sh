04f7f415d1931f3929f6a98fe7193ce5f3b3431c
Detavern <detavern@live.com>
2020-02-22 04:07:47 +0800

    fix missing env in .travis/install.sh

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-# use brew & pyenv to build specified python
+# use brew & pyenv to build specified python on osx
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     export PATH="$HOME/.pyenv/bin:$PATH"
-    brew update
+    eval "${MATRIX_EVAL}"
+    echo "python version: $PYTHON_VERSION"
+    brew update > /dev/null
+    # use pyenv to build python
     eval "$(pyenv init -)"
-    pyenv install $PYENV_VERSION
-    pyenv global $PYENV_VERSION
+    pyenv install $PYTHON_VERSION
+    pyenv global $PYTHON_VERSION
     pyenv rehash
 fi

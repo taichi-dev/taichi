138e66e58812fb3065f77af82ae8fb72b1faacbb
Yuanming Hu <yuanmhu@gmail.com>
2020-02-24 13:28:14 -0500

    more set_image data types

--- python/taichi/misc/gui.py
+++ python/taichi/misc/gui.py
@@ -53,10 +53,12 @@ class GUI:
       img = img.to_numpy()
     assert isinstance(img, np.ndarray)
     assert len(img.shape) in [2, 3]
-    if img.dtype not in [np.float32, np.float64]:
-      img = img.astype(np.float32) * (1.0 / 255)
-    else:
+    if img.dtype in [np.uint8, np.uint16, np.uint32, np.uint64]:
+      img = img.astype(np.float32) * (1 / np.iinfo(img.dtype).max)
+    elif img.dtype in [np.float32, np.float64]:
       img = img.astype(np.float32)
+    else:
+      raise ValueError(f'Data type {img.dtype} not supported in GUI.set_image')
     if len(img.shape) == 2:
       img = img[..., None]
     if img.shape[2] == 1:

56d0b35393d9e71ae4d052586b262f327689e414
Yuanming Hu <yuanmhu@gmail.com>
2020-02-25 09:49:20 -0500

    test_image_io.py

---
+++ tests/python/test_image_io.py
@@ -0,0 +1,12 @@
+import taichi as ti
+import numpy as np
+import os
+
+def test_image_io():
+  pixel = (np.random.rand(128, 128, 3) * 255).astype(np.uint8)
+  for ext in ['bmp', 'png']: # jpg is also supported but hard to test here since it's lossy
+    fn = 'taichi-image-io-test.' + ext
+    ti.imwrite(pixel, fn)
+    pixel_r = ti.imread(fn)
+    assert (pixel_r == pixel).all()
+    os.remove(fn)

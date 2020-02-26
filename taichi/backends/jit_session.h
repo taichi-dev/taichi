fbddc856f59fc1d37320d5450ec011838794364e
Yuanming Hu <yuanmhu@gmail.com>
2020-02-25 21:29:29 -0500

    JITModuleCPU

--- taichi/backends/jit_session.h
+++ taichi/backends/jit_session.h
@@ -12,26 +12,31 @@ TLANG_NAMESPACE_BEGIN
 
 class JITModule {
  public:
-  JITModule(CompileConfig config, std::unique_ptr<llvm::Module> &&module) {
+  JITModule() {
   }
 
   // Lookup a serial function.
   // For example, a CPU function, or a serial GPU function
   // This function returns a function pointer
-  void *lookup_function(const std::string &name){TI_NOT_IMPLEMENTED}
+  virtual void *lookup_function(const std::string &name) {
+    TI_NOT_IMPLEMENTED
+  }
 
   // Lookup a parallel GPU kernel
   // The only argument to GPU kernels should be a Context
-  std::function<void()> lookup_spmd_function(const std::string &name) {
+  virtual std::function<void()> lookup_spmd_function(const std::string &name) {
     TI_NOT_IMPLEMENTED
+  }
+
+  virtual ~JITModule() {
   }
 };
 
-// Backend JIT environment/compiler for all archs
+// Backend JIT compiler for all archs
 
 class JITSession {
  protected:
-  // std::vector<std::unique_ptr<JITModule>> modules;
+  std::vector<std::unique_ptr<JITModule>> modules;
 
  public:
   JITSession() {
@@ -39,10 +44,9 @@ class JITSession {
 
   virtual const llvm::DataLayout &get_data_layout() const = 0;
 
-  // TODO: uint64 should be llvm::VModuleKey
-  virtual uint64 add_module(std::unique_ptr<llvm::Module> M) = 0;
+  virtual JITModule *add_module(std::unique_ptr<llvm::Module> M) = 0;
 
-  virtual void remove_module(uint64 K) = 0;
+  // virtual void remove_module(JITModule *module) = 0;
 
   virtual void *lookup(const std::string Name) = 0;
 

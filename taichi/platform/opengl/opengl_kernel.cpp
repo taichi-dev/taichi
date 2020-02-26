d8457b578732b662ee6efce6ee8e93f4fcbb2333
archibate <17721388340@163.com>
2020-02-26 21:16:39 +0800

    no more mallocs in SSBO

--- taichi/platform/opengl/opengl_kernel.cpp
+++ taichi/platform/opengl/opengl_kernel.cpp
@@ -8,12 +8,12 @@ TLANG_NAMESPACE_BEGIN
 namespace opengl {
 
 SSBO::SSBO(size_t data_size_)
-  : data(std::malloc(data_size_)), data_size(data_size_)
+  : data_(data_size_), data_size(data_size_)
 {}
 
 void SSBO::load_arguments_from(Context &ctx)
 {
-  uint64_t *data_i = (uint64_t *)data;
+  uint64_t *data_i = (uint64_t *)data();
   for (int i = 0; i < taichi_max_num_args; i++) {
     uint64_t value = ctx.get_arg<uint64_t>(i);
     data_i[i] = value;
@@ -22,21 +22,11 @@ void SSBO::load_arguments_from(Context &ctx)
 
 void SSBO::save_returns_to(Context &ctx)
 {
-  uint64_t *data_i = (uint64_t *)data;
+  uint64_t *data_i = (uint64_t *)data();
   for (int i = 0; i < taichi_max_num_args; i++) {
     uint64_t value = data_i[i];
     ctx.set_arg<uint64_t>(i, value);
   }
-}
-
-void SSBO::update(void *data_r)
-{
-  std::memcpy(data, data_r, data_size);
-}
-
-SSBO::~SSBO()
-{
-  std::free(data);
 }
 
 }  // namespace opengl
--- taichi/platform/opengl/opengl_kernel.h
+++ taichi/platform/opengl/opengl_kernel.h
@@ -16,19 +16,21 @@ namespace opengl {
 
 struct SSBO
 {
-  void *const data;
+  std::vector<uint8_t> data_;
   const size_t data_size;
 
   SSBO(size_t data_size);
-  ~SSBO();
 
   void load_arguments_from(Context &ctx);
   void save_returns_to(Context &ctx);
-  void update(void *data_r);
-
-  operator IOV()
+  inline void *data()
   {
-    return IOV{data, data_size};
+    return (void *)data_.data();
+  }
+
+  inline operator IOV()
+  {
+    return IOV{data(), data_size};
   }
 };
 

dee4883c83c3b64a0d84a1f4a4490e0ac93423b2
xumingkuan <xumingkuan0721@126.com>
2020-02-26 14:46:56 -0500

    Avoid duplicate evaluations in chaining comparison

--- tests/python/test_compare.py
+++ tests/python/test_compare.py
@@ -5,13 +5,27 @@ import taichi as ti
 def test_compare():
   a = ti.var(ti.i32)
   ti.root.dynamic(ti.i, 256).place(a)
+  b = ti.var(ti.i32, shape=())
+  c = ti.var(ti.i32, shape=())
+  d = ti.var(ti.i32, shape=())
 
   @ti.kernel
   def func():
-    # t = 0 <= ti.append(a.parent(), [], 1) < 2
-    t = 1 < 0
-    print(t)
-    # t = ti.chain_compare([0, 1], [2])
+    a[2] = 0 <= ti.append(a.parent(), [], 10) < 1
+    b[None] = 2
+    c[None] = 3
+    d[None] = 3
+    a[3] = b < c == d
+    a[4] = b <= c != d
+    a[5] = c > b < d
+    a[6] = c == d != b < d > b >= b <= c
+    # a[10] = b is not c  # not supported
 
   func()
-  assert a[1] == 0
+  assert a[0] == 10
+  assert a[1] == 0  # not appended twice
+  assert a[2]
+  assert a[3]
+  assert not a[4]
+  assert a[5]
+  assert a[6]

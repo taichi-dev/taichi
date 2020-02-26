5c99b20feb763b097f5ff0b35855d55f33a68b08
Ye Kuang <k-ye@users.noreply.github.com>
2020-02-26 10:18:12 +0900

    Make sure KernelTemplateMapper extractors's size is the same as the number of args (#534)
    * Make sure KernelTemplateMapper extractors's size is the same as the number of args
    
    Fixes #531
    
    * fix test

--- tests/python/test_empty.py
+++ tests/python/test_empty.py
@@ -8,3 +8,13 @@ def test_empty():
     pass
 
   func()
+
+@ti.all_archs
+def test_empty_args():
+
+  @ti.kernel
+  def func(x: ti.i32, arr: ti.ext_arr()):
+    pass
+
+  import numpy as np
+  func(42, np.arange(10, dtype=np.float32))

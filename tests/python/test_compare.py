069f596febe93cedaad19107fe5da3ad48566066
xumingkuan <xumingkuan0721@126.com>
2020-02-26 15:36:02 -0500

    improve tests

--- tests/python/test_compare.py
+++ tests/python/test_compare.py
@@ -2,7 +2,101 @@ import taichi as ti
 
 
 @ti.all_archs
-def test_compare():
+def test_compare_basics():
+  a = ti.var(ti.i32)
+  ti.root.dynamic(ti.i, 256).place(a)
+  b = ti.var(ti.i32, shape=())
+  c = ti.var(ti.i32, shape=())
+
+  @ti.kernel
+  def func():
+    b[None] = 3
+    c[None] = 5
+    a[0] = b < c
+    a[1] = b <= c
+    a[2] = b > c
+    a[3] = b >= c
+    a[4] = b == c
+    a[5] = b != c
+    a[6] = c < b
+    a[7] = c <= b
+    a[8] = c > b
+    a[9] = c >= b
+    a[10] = c == b
+    a[11] = c != b
+    # a[12] = b is not c  # not supported
+
+  func()
+  assert a[0]
+  assert a[1]
+  assert not a[2]
+  assert not a[3]
+  assert not a[4]
+  assert a[5]
+  assert not a[6]
+  assert not a[7]
+  assert a[8]
+  assert a[9]
+  assert not a[10]
+  assert a[11]
+
+
+@ti.all_archs
+def test_compare_equality():
+  a = ti.var(ti.i32)
+  ti.root.dynamic(ti.i, 256).place(a)
+  b = ti.var(ti.i32, shape=())
+  c = ti.var(ti.i32, shape=())
+
+  @ti.kernel
+  def func():
+    b[None] = 3
+    c[None] = 3
+    a[0] = b < c
+    a[1] = b <= c
+    a[2] = b > c
+    a[3] = b >= c
+    a[4] = b == c
+    a[5] = b != c
+    a[6] = c < b
+    a[7] = c <= b
+    a[8] = c > b
+    a[9] = c >= b
+    a[10] = c == b
+    a[11] = c != b
+
+  func()
+  assert not a[0]
+  assert a[1]
+  assert not a[2]
+  assert a[3]
+  assert a[4]
+  assert not a[5]
+  assert not a[6]
+  assert a[7]
+  assert not a[8]
+  assert a[9]
+  assert a[10]
+  assert not a[11]
+
+
+@ti.all_archs
+def test_no_duplicate_eval():
+  a = ti.var(ti.i32)
+  ti.root.dynamic(ti.i, 256).place(a)
+
+  @ti.kernel
+  def func():
+    a[2] = 0 <= ti.append(a.parent(), [], 10) < 1
+
+  func()
+  assert a[0] == 10
+  assert a[1] == 0  # not appended twice
+  assert a[2]  # ti.append returns 0
+
+
+@ti.all_archs
+def test_chain_compare():
   a = ti.var(ti.i32)
   ti.root.dynamic(ti.i, 256).place(a)
   b = ti.var(ti.i32, shape=())
@@ -11,21 +105,12 @@ def test_compare():
 
   @ti.kernel
   def func():
-    a[2] = 0 <= ti.append(a.parent(), [], 10) < 1
     b[None] = 2
     c[None] = 3
     d[None] = 3
-    a[3] = b < c == d
-    a[4] = b <= c != d
-    a[5] = c > b < d
-    a[6] = c == d != b < d > b >= b <= c
-    # a[10] = b is not c  # not supported
+    a[0] = c == d != b < d > b >= b <= c
+    a[1] = b <= c != d > b == b
 
   func()
-  assert a[0] == 10
-  assert a[1] == 0  # not appended twice
-  assert a[2]
-  assert a[3]
-  assert not a[4]
-  assert a[5]
-  assert a[6]
+  assert a[0]
+  assert not a[1]

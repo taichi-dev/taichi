4ee3102ed747f264e6912a74bcfd984ff4a3c749
archibate <17721388340@163.com>
2020-02-27 17:17:25 +0800

    support arch: uncomment with_opengl

--- python/taichi/lang/__init__.py
+++ python/taichi/lang/__init__.py
@@ -223,8 +223,8 @@ def supported_archs():
     archs.append(cuda)
   if ti.core.with_metal():
     archs.append(metal)
-  #if ti.core.with_opengl():
-  #  archs.append(opengl)
+  if ti.core.with_opengl():
+    archs.append(opengl)
   return archs
 
 class _ArchCheckers(object):

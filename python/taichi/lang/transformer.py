3798ebbaee90f0329c1287f27b2bb397db2f3a5a
xumingkuan <xumingkuan0721@126.com>
2020-02-27 11:45:57 -0500

    Disallow return in ti.kernel; Disallow function definition in ti.kernel and ti.func

--- python/taichi/lang/transformer.py
+++ python/taichi/lang/transformer.py
@@ -476,6 +476,12 @@ if 1:
     assert args.kwarg is None
     import taichi as ti
     if self.is_kernel:
+      for decorator in node.decorator_list:
+        if (isinstance(decorator, ast.Attribute)
+            and isinstance(decorator.value, ast.Name)
+            and decorator.value.id == 'ti'
+            and decorator.attr == 'func'):
+          raise TaichiSyntaxError("Function definition not allowed in 'ti.kernel'.")
       # Transform as kernel
       arg_decls = []
       for i, arg in enumerate(args.args):
@@ -504,6 +510,12 @@ if 1:
       # remove original args
       node.args.args = []
     else:
+      for decorator in node.decorator_list:
+        if (isinstance(decorator, ast.Attribute)
+            and isinstance(decorator.value, ast.Name)
+            and decorator.value.id == 'ti'
+            and decorator.attr == 'func'):
+          raise TaichiSyntaxError("Function definition not allowed in 'ti.func'.")
       # Transform as func (all parameters passed by value)
       arg_decls = []
       for i, arg in enumerate(args.args):
@@ -597,3 +609,8 @@ if 1:
     new_node.value.args[0].value.args[0] = node.test
     new_node.value.args[1] = self.parse_expr("'{}'".format(msg.strip()))
     return new_node
+
+  def visit_Return(self, node):
+    if self.is_kernel:
+      raise TaichiSyntaxError('"return" not allowed in \'ti.kernel\'. Please walk around by using \'ti.func\'.')
+    return node
---
+++ tests/python/test_return.py
@@ -0,0 +1,11 @@
+import taichi as ti
+
+
+@ti.must_throw(ti.TaichiSyntaxError)
+def test_return_in_kernel():
+
+  @ti.kernel
+  def kernel():
+    return 1
+
+  kernel()

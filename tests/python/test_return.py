8c03b8d3db71b386151a08711bf0cdb794482927
xumingkuan <xumingkuan0721@126.com>
2020-02-27 14:36:29 -0500

    revert

--- tests/python/test_return.py
+++ tests/python/test_return.py
@@ -9,21 +9,3 @@ def test_return_in_kernel():
     return 1
 
   kernel()
-
-
-@ti.all_archs
-def test_return_type():
-
-  @ti.func
-  def foo(x):
-    if x > 0:
-      return int(x)
-    else:
-      return float(x)
-
-  @ti.kernel
-  def kernel():
-    assert foo(2.6) == 2
-    assert foo(-2.4) == -2.4
-
-  kernel()

taichi-dev/taichi

commit 3712a2bd62ab97f5cd556b58ea576bae0a3be82a

Frontend kernel/function structure checking (#544)
* Disallow return in ti.kernel; Disallow function definition in ti.kernel and ti.func

* Disallow return in ti.kernel; Disallow function definition in ti.kernel and ti.func

* Update transformer.py

* Add test of return type

* revert

Co-authored-by: Yuanming Hu <yuanming-hu@users.noreply.github.com>

python/taichi/lang/transformer.py
@@ -478,6 +478,12 @@ if 1:
     assert args.kwarg is None
     import taichi as ti
     if self.is_kernel:
+      for decorator in node.decorator_list:
+        if (isinstance(decorator, ast.Attribute)
+            and isinstance(decorator.value, ast.Name)
+            and decorator.value.id == 'ti'
+            and decorator.attr == 'func'):
+          raise TaichiSyntaxError("Function definition not allowed in 'ti.kernel'.")
       # Transform as kernel
       arg_decls = []
       for i, arg in enumerate(args.args):
@@ -506,6 +512,12 @@ if 1:
       # remove original args
       node.args.args = []
     else:
+      for decorator in node.decorator_list:
+        if (isinstance(decorator, ast.Attribute)
+            and isinstance(decorator.value, ast.Name)
+            and decorator.value.id == 'ti'
+            and decorator.attr == 'func'):
+          raise TaichiSyntaxError("Function definition not allowed in 'ti.func'.")
       # Transform as func (all parameters passed by value)
       arg_decls = []
       for i, arg in enumerate(args.args):
@@ -601,3 +613,8 @@ if 1:
     new_node.value.args[0].value.args[0] = node.test
     new_node.value.args[1] = self.parse_expr("'{}'".format(msg.strip()))
     return new_node
+
+  def visit_Return(self, node):
+    if self.is_kernel:
+      raise TaichiSyntaxError('"return" not allowed in \'ti.kernel\'. Please walk around by storing the return result to a global variable.')
+    return node

tests/python/test_return.py
@@ -1,0 +1,11 @@
+import taichi as ti
+
+
+@ti.must_throw(ti.TaichiSyntaxError)
+def test_return_in_kernel():
+
+  @ti.kernel
+  def kernel():
+    return 1
+
+  kernel()

tests/python/test_syntax_errors.py
@@ -150,3 +150,37 @@ def test_ternary():
     a = 0 if True else 123
 
   func()
+
+
+@ti.must_throw(ti.TaichiSyntaxError)
+def test_func_def_in_kernel():
+
+  @ti.kernel
+  def kernel():
+
+    @ti.func
+    def func():
+      return 1
+
+    print(func())
+
+  kernel()
+
+
+@ti.must_throw(ti.TaichiSyntaxError)
+def test_func_def_in_func():
+
+  @ti.func
+  def func():
+
+    @ti.func
+    def func2():
+      return 1
+
+    return func2()
+
+  @ti.kernel
+  def kernel():
+    print(func())
+
+  kernel()

commit 1eee7fc22e28659059a4113b29aeea562086df64

fixed AST return value transform bug introduced in #544

examples/renderer.py
@@ -5,7 +5,7 @@ import time
 from renderer_utils import out_dir, ray_aabb_intersection, inf, eps, \
   intersect_sphere, sphere_aabb_intersect_motion, inside_taichi
 
-ti.init(arch=ti.cuda)
+ti.init(arch=ti.cuda, device_memory_GB=4)
 
 res = 1280, 720
 num_spheres = 1024

python/taichi/lang/transformer.py
@@ -615,6 +615,7 @@ if 1:
     return new_node
 
   def visit_Return(self, node):
+    self.generic_visit(node)
     if self.is_kernel:
       raise TaichiSyntaxError('"return" not allowed in \'ti.kernel\'. Please walk around by storing the return result to a global variable.')
     return node
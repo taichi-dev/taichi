b15ca8ca3cce52d3a71c7f6b9bc8be080b05be85
Yuanming Hu <yuanmhu@gmail.com>
2020-03-04 18:25:32 -0500

    simplify test_oop

--- tests/python/test_oop.py
+++ tests/python/test_oop.py
@@ -41,19 +41,18 @@ def test_oop():
       self.total = ti.var(ti.f32)
       self.increment = increment
 
-    def place(self, root):
-      root.dense(ti.ij, (self.n, self.m)).place(self.val)
-      root.place(self.total)
+      ti.root.dense(ti.ij, (self.n, self.m)).place(self.val)
+      ti.root.place(self.total)
 
     @ti.kernel
     def inc(self):
       for i, j in self.val:
-        ti.atomic_add(self.val[i, j], self.increment)
+        self.val[i, j] += self.increment
 
     @ti.kernel
     def inc2(self, increment: ti.i32):
       for i, j in self.val:
-        ti.atomic_add(self.val[i, j], increment)
+        self.val[i, j] += increment
 
     @ti.kernel
     def reduce(self):
@@ -64,12 +63,8 @@ def test_oop():
 
   double_total = ti.var(ti.f32)
 
-  @ti.layout
-  def place():
-    ti.root.place(
-        arr)  # Place an object. Make sure you defined place for that obj
-    ti.root.place(double_total)
-    ti.root.lazy_grad()
+  ti.root.place(double_total)
+  ti.root.lazy_grad()
 
   arr.inc()
   arr.inc.grad()

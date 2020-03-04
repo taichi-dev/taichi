bad302ea798d63091980f29d48879f1ca1c56a23
xumingkuan <xumingkuan0721@126.com>
2020-03-04 16:02:50 -0500

    Fix console color

--- taichi/core/logging.cpp
+++ taichi/core/logging.cpp
@@ -65,7 +65,7 @@ int Logger::level_enum_from_string(const std::string &level_name) {
 Logger::Logger() {
   console = spdlog::stdout_color_mt("console");
   console->flush_on(spdlog::level::trace);
-  TI_LOG_SET_PATTERN("[%L %D %X.%e] %v");
+  TI_LOG_SET_PATTERN("%^[%L %D %X.%e] %v%$");
 
   py::register_exception_translator([](std::exception_ptr p) {
     try {

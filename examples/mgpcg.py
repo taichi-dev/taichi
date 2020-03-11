1cc586973e196c0dac57feb55af4601c2d07f13d
Yuanming Hu <yuanmhu@gmail.com>
2020-03-11 18:29:50 -0400

    use array of scalars instead of ti.Vector in mgpcg.py

--- examples/mgpcg.py
+++ examples/mgpcg.py
@@ -18,8 +18,8 @@ N_ext = N // 2  # number of ext cells set so that that total grid size is still 
 N_tot = 2 * N
 
 # setup sparse simulation data arrays
-r = ti.Vector(n_mg_levels, dt=real)  # residual
-z = ti.Vector(n_mg_levels, dt=real)  # M^-1 r
+r = [ti.var(dt=real) for _ in range(n_mg_levels)]  # residual
+z = [ti.var(dt=real) for _ in range(n_mg_levels)] # M^-1 r
 x = ti.var(dt=real)  # solution
 p = ti.var(dt=real)  # conjugate gradient
 Ap = ti.var(dt=real)  # matrix-vector product
@@ -28,15 +28,10 @@ beta = ti.var(dt=real)  # step size
 sum = ti.var(dt=real)  # storage for reductions
 pixels = ti.var(dt=real, shape=(N_gui, N_gui))  # image buffer
 
-grid = ti.root.pointer(ti.ijk, [N_tot // 4]).dense(ti.ijk, 4)
-grid.place(x)
-grid.place(p)
-grid.place(Ap)
+grid = ti.root.pointer(ti.ijk, [N_tot // 4]).dense(ti.ijk, 4).place(x, p, Ap)
 
 for l in range(n_mg_levels):
-  grid = ti.root.pointer(ti.ijk, [N_tot // (4 * 2**l)]).dense(ti.ijk, 4)
-  grid.place(r(l))
-  grid.place(z(l))
+  grid = ti.root.pointer(ti.ijk, [N_tot // (4 * 2**l)]).dense(ti.ijk, 4).place(r[l], z[l])
 
 ti.root.place(alpha, beta, sum)
 
@@ -48,9 +43,9 @@ def init():
     xl = (i - N_ext) * 2.0 / N_tot
     yl = (j - N_ext) * 2.0 / N_tot
     zl = (k - N_ext) * 2.0 / N_tot
-    r(0)[i, j, k] = ti.sin(2.0 * np.pi * xl) * ti.sin(
+    r[0][i, j, k] = ti.sin(2.0 * np.pi * xl) * ti.sin(
         2.0 * np.pi * yl) * ti.sin(2.0 * np.pi * zl)
-    z(0)[i, j, k] = 0.0
+    z[0][i, j, k] = 0.0
     Ap[i, j, k] = 0.0
     p[i, j, k] = 0.0
     x[i, j, k] = 0.0
@@ -79,46 +74,46 @@ def update_x():
 @ti.kernel
 def update_r():
   for I in ti.grouped(p):
-    r(0)[I] -= alpha[None] * Ap[I]
+    r[0][I] -= alpha[None] * Ap[I]
 
 
 @ti.kernel
 def update_p():
   for I in ti.grouped(p):
-    p[I] = z(0)[I] + beta[None] * p[I]
+    p[I] = z[0][I] + beta[None] * p[I]
 
 
 @ti.kernel
 def restrict(l: ti.template()):
-  for i, j, k in r(l):
-    res = r(l)[i,j,k] - (6.0*z(l)[i,j,k] - z(l)[i+1,j,k] - z(l)[i-1,j,k]
-                        - z(l)[i,j+1,k] - z(l)[i,j-1,k] - z(l)[i,j,k+1] - z(l)[i,j,k-1])
-    r(l + 1)[i // 2, j // 2, k // 2] += res * 0.5
+  for i, j, k in r[l]:
+    res = r[l][i,j,k] - (6.0*z[l][i,j,k] - z[l][i+1,j,k] - z[l][i-1,j,k]
+                        - z[l][i,j+1,k] - z[l][i,j-1,k] - z[l][i,j,k+1] - z[l][i,j,k-1])
+    r[l + 1][i // 2, j // 2, k // 2] += res * 0.5
 
 
 @ti.kernel
 def prolongate(l: ti.template()):
-  for I in ti.grouped(z(l)):
-    z(l)[I] = z(l + 1)[I // 2]
+  for I in ti.grouped(z[l]):
+    z[l][I] = z[l + 1][I // 2]
 
 
 @ti.kernel
 def smooth(l: ti.template(), phase: ti.template()):
   # phase = red/black Gauss-Seidel phase
-  for i, j, k in r(l):
+  for i, j, k in r[l]:
     if (i + j + k) & 1 == phase:
-      z(l)[i,j,k] = (r(l)[i,j,k] + z(l)[i+1,j,k] + z(l)[i-1,j,k] \
-                                 + z(l)[i,j+1,k] + z(l)[i,j-1,k] \
-                                 + z(l)[i,j,k+1] + z(l)[i,j,k-1])/6.0
+      z[l][i,j,k] = (r[l][i,j,k] + z[l][i+1,j,k] + z[l][i-1,j,k] \
+                                 + z[l][i,j+1,k] + z[l][i,j-1,k] \
+                                 + z[l][i,j,k+1] + z[l][i,j,k-1])/6.0
 
 def apply_preconditioner():
-  z(0).fill(0)
+  z[0].fill(0)
   for l in range(n_mg_levels - 1):
     for i in range(pre_and_post_smoothing << l):
       smooth(l, 0)
       smooth(l, 1)
-    z(l + 1).fill(0)
-    r(l + 1).fill(0)
+    z[l + 1].fill(0)
+    r[l + 1].fill(0)
     restrict(l)
 
   for i in range(bottom_smoothing):
@@ -146,7 +141,7 @@ gui = ti.GUI("mgpcg", res=(N_gui, N_gui))
 init()
 
 sum[None] = 0.0
-reduce(r(0), r(0))
+reduce(r[0], r[0])
 initial_rTr = sum[None]
 
 # r = b - Ax = b    since x = 0
@@ -154,12 +149,12 @@ initial_rTr = sum[None]
 if use_multigrid:
   apply_preconditioner()
 else:
-  z(0).copy_from(r(0))
+  z[0].copy_from(r[0])
 
 update_p()
 
 sum[None] = 0.0
-reduce(z(0), r(0))
+reduce(z[0], r[0])
 old_zTr = sum[None]
 
 # CG
@@ -179,7 +174,7 @@ for i in range(400):
 
   # check for convergence
   sum[None] = 0.0
-  reduce(r(0), r(0))
+  reduce(r[0], r[0])
   rTr = sum[None]
   if rTr < initial_rTr * 1.0e-12:
     break
@@ -188,11 +183,11 @@ for i in range(400):
   if use_multigrid:
     apply_preconditioner()
   else:
-    z(0).copy_from(r(0))
+    z[0].copy_from(r[0])
 
   # beta = new_rTr / old_rTr
   sum[None] = 0.0
-  reduce(z(0), r(0))
+  reduce(z[0], r[0])
   new_zTr = sum[None]
   beta[None] = new_zTr / old_zTr
 

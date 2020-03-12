fe0e7122ace3e0b59dbb59db70ff8b60c98f5cd4
archibate <17721388340@163.com>
2020-03-12 23:55:22 +0800

    add ti.Matrix, static have at least one arg

--- python/taichi/lang/impl.py
+++ python/taichi/lang/impl.py
@@ -286,20 +286,19 @@ def indices(*x):
 index = indices
 
 
-def static(*xs):
-  if len(xs) > 1: # for python-ish pointer assign: x, y = ti.static(y, x)
-    return tuple(static(x) for x in xs)
-  else:
-    x = xs[0] # len != 0 -> Error
+def static(x, *xs):
+  if len(xs): # for python-ish pointer assign: x, y = ti.static(y, x)
+    return [static(x)] + [static(x) for x in xs]
+
   import taichi as ti
   assert get_runtime(
   ).inside_kernel, 'ti.static can only be used inside Taichi kernels'
-  assert isinstance(
-      x, (bool, int, float, range, list, tuple, ti.ndrange, ti.GroupedNDRange)
-  ) or (isinstance(x, ti.lang.expr.Expr) and x.ptr.is_global_var()
-      ), 'Input to ti.static must be compile-time constants or global pointers, instead of {}'.format(
-      type(x))
-  return x
+  if isinstance(x, (bool, int, float, range, list, tuple, ti.ndrange, ti.GroupedNDRange)):
+    return x
+  elif isinstance(x, (ti.lang.expr.Expr, ti.Matrix)) and x.ptr.is_global_var():
+    return x
+  else:
+    raise ValueError(f'Input to ti.static must be compile-time constants or global pointers, instead of {type(x)}')
 
 
 def grouped(x):

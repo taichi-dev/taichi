4d8ccb87f958779d045a3b7dabb577f514858cbf
Ye Kuang <yekuang.ky@gmail.com>
2020-03-18 08:38:23 +0900

    comments

--- taichi/platform/metal/shaders/prolog.h
+++ taichi/platform/metal/shaders/prolog.h
@@ -4,6 +4,10 @@
 #define STR2(...) #__VA_ARGS__
 #define STR(...) STR2(__VA_ARGS__)
 #else
+// If we are emitting to Metal source code, and the shader file is included by
+// some other shader file, then skip emitting the code for the nested shader,
+// otherwise there could be a symbol redefinition error. That is, we only emit
+// the source code for the shader being directly included by the host side.
 #define STR(...)
 #endif  // TI_METAL_NESTED_INCLUDE
 

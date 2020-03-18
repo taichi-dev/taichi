e93da7d587a437d38f5e075fef3eb14e247a0992
Ye Kuang <k-ye@users.noreply.github.com>
2020-03-19 03:37:19 +0900

    [Metal] Add Runtime shaders to support sparse SNode (#614)

---
+++ taichi/platform/metal/shaders/runtime_structs.metal.h
@@ -0,0 +1,67 @@
+#include "taichi/platform/metal/shaders/prolog.h"
+
+#ifdef TI_INSIDE_METAL_CODEGEN
+
+#ifndef TI_METAL_NESTED_INCLUDE
+#define METAL_BEGIN_RUNTIME_STRUCTS_DEF \
+  constexpr auto kMetalRuntimeStructsSourceCode =
+#define METAL_END_RUNTIME_STRUCTS_DEF ;
+#else
+#define METAL_BEGIN_RUNTIME_STRUCTS_DEF
+#define METAL_END_RUNTIME_STRUCTS_DEF
+#endif  // TI_METAL_NESTED_INCLUDE
+
+#else
+
+#include <cstdint>
+
+#include "taichi/inc/constants.h"
+
+static_assert(taichi_max_num_indices == 8,
+              "Please update kTaichiMaxNumIndices");
+
+#define METAL_BEGIN_RUNTIME_STRUCTS_DEF
+#define METAL_END_RUNTIME_STRUCTS_DEF
+
+#endif  // TI_INSIDE_METAL_CODEGEN
+
+METAL_BEGIN_RUNTIME_STRUCTS_DEF
+STR(
+    constant constexpr int kTaichiMaxNumIndices = 8;
+
+    struct ListgenElement {
+      int32_t coords[kTaichiMaxNumIndices];
+      int32_t root_mem_offset = 0;
+    };
+
+    struct ListManager {
+      int32_t element_stride = 0;
+      int32_t max_num_elems = 0;
+      int32_t next = 0;
+      int32_t mem_begin = 0;
+    };
+
+    struct SNodeMeta {
+      enum Type { Root = 0, Dense = 1, DenseBitmask = 2 };
+      int32_t element_stride = 0;
+      int32_t num_slots = 0;
+      int32_t mem_offset_in_parent = 0;
+      int32_t type = 0;
+    };
+
+    struct SNodeExtractors {
+      struct Extractor {
+        int32_t start = 0;
+        int32_t num_bits = 0;
+        int32_t acc_offset = 0;
+        int32_t num_elements = 0;
+      };
+
+      Extractor extractors[kTaichiMaxNumIndices];
+    };)
+METAL_END_RUNTIME_STRUCTS_DEF
+
+#undef METAL_BEGIN_RUNTIME_STRUCTS_DEF
+#undef METAL_END_RUNTIME_STRUCTS_DEF
+
+#include "taichi/platform/metal/shaders/epilog.h"

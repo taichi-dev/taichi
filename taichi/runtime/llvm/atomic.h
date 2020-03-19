48935408456a404e04f7a539a6a31b4777ecf021
klozes <lozesk@gmail.com>
2020-03-18 19:23:36 -0700

    macroize runtime atomics and add new ones

--- taichi/runtime/llvm/atomic.h
+++ taichi/runtime/llvm/atomic.h
@@ -2,65 +2,86 @@
 
 // These instructions should be replaced by CUDA intrinsics on GPUs
 
-int32 atomic_exchange_i32(volatile int32 *dest, int32 val) {
-  i32 ret;
-  __atomic_exchange(dest, &val, &ret, std::memory_order::memory_order_seq_cst);
-  return ret;
-}
+#define DEFINE_ATOMIC_EXCHANGE(T)                     \
+  T atomic_exchange_##T(volatile T *dest, T val) {    \
+    T ret;                                            \
+    __atomic_exchange(dest, &val, &ret,               \
+           std::memory_order::memory_order_seq_cst);  \
+    return ret;                                       \
+  }
 
-i64 atomic_exchange_i64(volatile int64 *dest, int64 val) {
-  i64 ret;
-  __atomic_exchange(dest, &val, &ret, std::memory_order::memory_order_seq_cst);
-  return ret;
-}
-
-u64 atomic_exchange_u64(volatile u64 *dest, u64 val) {
-  u64 ret;
-  __atomic_exchange(dest, &val, &ret, std::memory_order::memory_order_seq_cst);
-  return ret;
-}
+DEFINE_ATOMIC_EXCHANGE(i32)
+DEFINE_ATOMIC_EXCHANGE(i64)
+DEFINE_ATOMIC_EXCHANGE(u32)
+DEFINE_ATOMIC_EXCHANGE(u64)
 
 
-int32 atomic_add_i32(volatile int32 *dest, int32 val) {
-  return __atomic_fetch_add(dest, val, std::memory_order::memory_order_seq_cst);
+#define DEFINE_ATOMIC_OP_INTRINSIC(OP,T)               \
+  T atomic_##OP##_##T(volatile T *dest, T val) {       \
+    return __atomic_fetch_##OP(dest, val,              \
+           std::memory_order::memory_order_seq_cst);   \
+  }
+
+DEFINE_ATOMIC_OP_INTRINSIC(add,i32)
+DEFINE_ATOMIC_OP_INTRINSIC(add,i64)
+DEFINE_ATOMIC_OP_INTRINSIC(and,i32)
+DEFINE_ATOMIC_OP_INTRINSIC(and,i64)
+DEFINE_ATOMIC_OP_INTRINSIC(and,u32)
+DEFINE_ATOMIC_OP_INTRINSIC(and,u64)
+DEFINE_ATOMIC_OP_INTRINSIC(or,i32)
+DEFINE_ATOMIC_OP_INTRINSIC(or,i64)
+DEFINE_ATOMIC_OP_INTRINSIC(or,u32)
+DEFINE_ATOMIC_OP_INTRINSIC(or,u64)
+DEFINE_ATOMIC_OP_INTRINSIC(xor,i32)
+DEFINE_ATOMIC_OP_INTRINSIC(xor,i64)
+DEFINE_ATOMIC_OP_INTRINSIC(xor,u32)
+DEFINE_ATOMIC_OP_INTRINSIC(xor,u64)
+
+
+inline f32 add_f32(f32 a, f32 b) {
+  return a + b;
 }
 
-int64 atomic_add_i64(volatile int64 *dest, int64 val) {
-  return __atomic_fetch_add(dest, val, std::memory_order::memory_order_seq_cst);
+inline f64 add_f64(f64 a, f64 b) {
+  return a + b;
 }
 
-uint64 atomic_add_u64(volatile uint64 *dest, uint64 val) {
-  return __atomic_fetch_add(dest, val, std::memory_order::memory_order_seq_cst);
+inline f32 min_f32(f32 a, f32 b) {
+  return b > a ? a : b;
 }
 
-uint64 atomic_or_u64(volatile uint64 *dest, uint64 val) {
-  return __atomic_fetch_or(dest, val, std::memory_order::memory_order_seq_cst);
+inline f64 min_f64(f64 a, f64 b) {
+  return b > a ? a : b;
 }
 
-uint64 atomic_and_u64(volatile uint64 *dest, uint64 val) {
-  return __atomic_fetch_and(dest, val, std::memory_order::memory_order_seq_cst);
+inline f32 max_f32(f32 a, f32 b) {
+  return b < a ? a : b;
 }
 
-float32 atomic_add_f32(volatile float32 *dest, float32 inc) {
-  float32 old_val;
-  float32 new_val;
-  do {
-    old_val = *dest;
-    new_val = old_val + inc;
-  } while (!__atomic_compare_exchange(dest, &old_val, &new_val, true,
-                                      std::memory_order::memory_order_seq_cst,
-                                      std::memory_order::memory_order_seq_cst));
-  return old_val;
+inline f64 max_f64(f64 a, f64 b) {
+  return b < a ? a : b;
 }
 
-float64 atomic_add_f64(volatile float64 *dest, float64 inc) {
-  float64 old_val;
-  float64 new_val;
-  do {
-    old_val = *dest;
-    new_val = old_val + inc;
-  } while (!__atomic_compare_exchange(dest, &old_val, &new_val, true,
-                                      std::memory_order::memory_order_seq_cst,
-                                      std::memory_order::memory_order_seq_cst));
-  return old_val;
-}
+#define DEFINE_ATOMIC_OP_COMP_EXCH(OP, T)                                      \
+  T atomic_##OP##_##T(volatile T *dest, T inc) {                               \
+    T old_val;                                                                 \
+    T new_val;                                                                 \
+    do {                                                                       \
+      old_val = *dest;                                                         \
+      new_val = OP##_##T(old_val, inc);                                        \
+    } while (!__atomic_compare_exchange(dest, &old_val, &new_val, true,        \
+                                    std::memory_order::memory_order_seq_cst,   \
+                                    std::memory_order::memory_order_seq_cst)); \
+    return old_val;                                                            \
+  }
+
+DEFINE_ATOMIC_OP_COMP_EXCH(add,f32)
+DEFINE_ATOMIC_OP_COMP_EXCH(add,f64)
+DEFINE_ATOMIC_OP_COMP_EXCH(min,f32)
+DEFINE_ATOMIC_OP_COMP_EXCH(min,f64)
+DEFINE_ATOMIC_OP_COMP_EXCH(min,i32)
+DEFINE_ATOMIC_OP_COMP_EXCH(min,i64)
+DEFINE_ATOMIC_OP_COMP_EXCH(max,f32)
+DEFINE_ATOMIC_OP_COMP_EXCH(max,f64)
+DEFINE_ATOMIC_OP_COMP_EXCH(max,i32)
+DEFINE_ATOMIC_OP_COMP_EXCH(max,i64)

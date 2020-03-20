493e554987678ef77e84234a8438cfae6c014ca4
archibate <17721388340@163.com>
2020-03-21 01:05:16 +0800

    [skip ci] remove dirty TI_INFO debug

--- taichi/analysis/detect_for_with_break.cpp
+++ taichi/analysis/detect_for_with_break.cpp
@@ -15,21 +15,18 @@ class DetectForWithBreak : public BasicStmtVisitor {
   }
 
   void visit(FrontendWhileStmt *stmt) override {
-    TI_INFO("FOUND WHILE");
     loop_stack.push_back(stmt);
     stmt->body->accept(this);
     loop_stack.pop_back();
   }
 
   void visit(FrontendForStmt *stmt) override {
-    TI_INFO("FOUND FOR");
     loop_stack.push_back(stmt);
     stmt->body->accept(this);
     loop_stack.pop_back();
   }
 
   void visit(FrontendIfStmt *stmt) override {
-    TI_INFO("FOUND IF");
     if (stmt->true_statements)
       stmt->true_statements->accept(this);
     if (stmt->false_statements)
@@ -37,7 +34,6 @@ class DetectForWithBreak : public BasicStmtVisitor {
   }
 
   void visit(FrontendBreakStmt *stmt) override {
-    TI_INFO("FOUND BREAK!");
     TI_ASSERT_INFO(loop_stack.size() != 0, "break statement out of loop scope");
     auto loop = loop_stack.back();
     if (loop->is<FrontendForStmt>())
--- taichi/transforms/lower_ast.cpp
+++ taichi/transforms/lower_ast.cpp
@@ -172,16 +172,13 @@ class LowerAST : public IRVisitor {
       bool is_good_range_for = capturing_loop == nullptr || // the outer-most for
         std::find(detected_fors_with_break.begin(), detected_fors_with_break.end(), stmt)
         == detected_fors_with_break.end(); // for without break
-      TI_INFO("!!!!!!!!{} {}", (uintptr_t)capturing_loop, detected_fors_with_break.size());
       if (is_good_range_for) {  // #578
-        TI_INFO("GOOD FOR");
         auto &&new_for = std::make_unique<RangeForStmt>(
             stmt->parent->lookup_var(stmt->loop_var_id[0]), begin->stmt,
             end->stmt, std::move(stmt->body), stmt->vectorize,
             stmt->parallelize, stmt->block_dim, stmt->strictly_serialized);
         flattened.push_back(std::move(new_for));
       } else {
-        TI_INFO("BAD FOR");
         // transform into a structure as
         // i = begin; while (1) { if (i > end) break; original body; i += 1; }
         // auto loop_var = Stmt::make<AllocaStmt>(DataType::i32);
@@ -263,7 +260,6 @@ class LowerAST : public IRVisitor {
   void visit(RangeForStmt *for_stmt) override {
     auto old_capturing_loop = capturing_loop;
     capturing_loop = for_stmt;
-    TI_INFO("setting capturing loop to {}", (uintptr_t)capturing_loop);
     for_stmt->body->accept(this);
     capturing_loop = old_capturing_loop;
   }

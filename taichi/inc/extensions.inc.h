8db682f00d3f8c0162e1013ed906f750252e906f
Yuanming Hu <yuanmhu@gmail.com>
2020-03-23 15:30:47 -0400

    New extension 'adstack'

--- taichi/inc/extensions.inc.h
+++ taichi/inc/extensions.inc.h
@@ -1,3 +1,4 @@
 // Lists of extension features
 PER_EXTENSION(sparse)
 PER_EXTENSION(data64)  // Metal doesn't support 64-bit data buffers yet...
+PER_EXTENSION(adstack)  // For keeping the history of mutable local variables

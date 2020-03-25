ac0fee63297f3a81ed05aebea2b7b2b8ea1f3d91
archibate <17721388340@163.com>
2020-03-26 00:34:49 +0800

    add taichi-llvm.zip spec for linux

---
+++ misc/get-taichi-deps.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+mkdir -p taichi-llvm
+cd taichi-llvm
+if [ $TRAVIS_OS_NAME == 'osx' ]; then
+	wget https://github.com/yuanming-hu/taichi_assets/releases/download/llvm8/taichi-llvm-8.0.1.zip --retry-on-http-error=403 --waitretry=3 --tries=5
+	unzip taichi-llvm-8.0.1.zip
+elif [ $TRAVIS_OS_NAME == 'linux' ]; then
+	wget https://github.com/yuanming-hu/taichi_assets/releases/download/llvm8/taichi-llvm-8.0.1-linux-x64.zip
+	unzip taichi-llvm-8.0.1-linux-x64.zip
+	apt-get install clang++-7
+fi
+cd ..

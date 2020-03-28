846dec225c3a1ca0edb9835e46718d056d878b0c
Taichi Gardener <taichigardener@gmail.com>
2020-03-28 10:30:20 -0400

    [skip ci] enforce code format

--- tests/python/test_bitmasked.py
+++ tests/python/test_bitmasked.py
@@ -2,7 +2,7 @@ import taichi as ti
 
 
 def archs_support_bitmasked(func):
-  return ti.archs_excluding(ti.opengl)(func)
+    return ti.archs_excluding(ti.opengl)(func)
 
 
 @archs_support_bitmasked
@@ -36,25 +36,25 @@ def test_basic():
 
 @archs_support_bitmasked
 def test_bitmasked_then_dense():
-    x = ti.var(ti.f32)	
-    s = ti.var(ti.i32)	
+    x = ti.var(ti.f32)
+    s = ti.var(ti.i32)
 
-    n = 128	
+    n = 128
 
-    @ti.layout	
-    def place():	
-        ti.root.bitmasked(ti.i, n).dense(ti.i, n).place(x)	
-        ti.root.place(s)	
+    @ti.layout
+    def place():
+        ti.root.bitmasked(ti.i, n).dense(ti.i, n).place(x)
+        ti.root.place(s)
 
-    @ti.kernel	
-    def func():	
-        for i in x:	
-            s[None] += 1	
+    @ti.kernel
+    def func():
+        for i in x:
+            s[None] += 1
 
-    x[0] = 1	
-    x[127] = 1	
-    x[256] = 1	
-    x[257] = 1	
+    x[0] = 1
+    x[127] = 1
+    x[256] = 1
+    x[257] = 1
 
-    func()	
-    assert s[None] == 256	
+    func()
+    assert s[None] == 256

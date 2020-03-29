3f5bdea11f0c40e6c4d610a008324b554325e1fa
Yuanming Hu <yuanmhu@gmail.com>
2020-03-28 22:38:24 -0400

    [skip ci] format

--- misc/make_changelog.py
+++ misc/make_changelog.py
@@ -6,13 +6,14 @@ import sys
 
 ver = sys.argv[1]
 
-
 g = Repo('.')
 commits = list(g.iter_commits('master', max_count=200))
 begin, end = -1, 0
 
+
 def format(c):
     return f'{c.summary} (by **{c.author}**)'
+
 
 print('Notable changes:')
 
@@ -41,7 +42,7 @@ for i, c in enumerate(commits):
         r = s.find(']')
         tag = s[1:r]
         tags.append(tag)
-        s = s[r+1:]
+        s = s[r + 1:]
     for tag in tags:
         if tag[0].isupper():
             tag = tag.lower()
@@ -51,12 +52,14 @@ for i, c in enumerate(commits):
     if s.startswith('[release]'):
         break
     all_changes.append(format(c))
-    
+
 for tag in sorted(notable_changes.keys()):
     print(f'   - **{details[tag]}**')
     for item in notable_changes[tag]:
         print(f'      -{item}')
-print(f'      - [Full log](https://github.com/taichi-dev/taichi/releases/tag/{ver})')
+print(
+    f'      - [Full log](https://github.com/taichi-dev/taichi/releases/tag/{ver})'
+)
 
 print('Full changelog:')
 for c in all_changes:

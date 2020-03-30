dbb8e8681f1277d3698ff381071d2716896523d4
xumingkuan <xumingkuan0721@126.com>
2020-03-30 18:13:59 -0400

    Move Stmt::is<>() to IRNode, and implement void IRNodeComparator::visit(Block *)

--- taichi/analysis/same_statements.cpp
+++ taichi/analysis/same_statements.cpp
@@ -2,23 +2,43 @@
 
 TLANG_NAMESPACE_BEGIN
 
+#define DEFINE_TYPECHECK(Type)        \
+  if (!other_node->is<Type>()) {      \
+    same = false;                     \
+    return;                           \
+  }                                   \
+  auto other = other_node->as<Type>()
+
 // Compare if two IRNodes are equivalent.
 class IRNodeComparator : public IRVisitor {
  private:
-  IRNode *other;
+  IRNode *other_node;
 
  public:
   bool same;
 
-  IRNodeComparator(IRNode *other) : other(other) {
-    allow_undefined_visitor = false;
-    invoke_default_visitor = false;
+  IRNodeComparator(IRNode *other_node) : other_node(other_node) {
     same = true;
   }
 
+  void visit(Block *stmt_list) override {
+    DEFINE_TYPECHECK(Block);
+    if (stmt_list->statements.size() != other->statements.size()) {
+      same = false;
+      return;
+    }
+    for (int i = 0; i < (int)stmt_list->statements.size(); i++) {
+      other_node = other->statements[i].get();
+      stmt_list->statements[i]->accept(this);
+      if (!same)
+        break;
+    }
+    other_node = other;
+  }
+
   static bool run(IRNode *root1, IRNode *root2) {
-    IRNodeComparator comparator(root1);
-    root2->accept(&comparator);
+    IRNodeComparator comparator(root2);
+    root1->accept(&comparator);
     return comparator.same;
   }
 };

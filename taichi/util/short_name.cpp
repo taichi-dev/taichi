8d44638bb48c9629154e06177205ad2546d10520
archibate <17721388340@163.com>
2020-04-02 10:46:18 +0800

    guard short_name.cpp with TI_NAMESPACE_BEGIN/END

--- taichi/util/short_name.cpp
+++ taichi/util/short_name.cpp
@@ -1,6 +1,7 @@
 #include "short_name.h"
 #include "base64.h"
 
+TI_NAMESPACE_BEGIN
 namespace {
   const std::string alphatable = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; // e.g. `if` is not a good var name, but `If` does.
   const std::string alnumtable = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -17,3 +18,4 @@ std::string make_short_name_by_id(int id)
   std::reverse(res.begin(), res.end());
   return res;
 }
+TI_NAMESPACE_END
--- taichi/util/short_name.h
+++ taichi/util/short_name.h
@@ -2,6 +2,9 @@
 
 #include <string>
 #include "taichi/common/util.h"
-#include "taichi/lang_util.h"
+
+TI_NAMESPACE_BEGIN
 
 std::string make_short_name_by_id(int id);
+
+TI_NAMESPACE_END

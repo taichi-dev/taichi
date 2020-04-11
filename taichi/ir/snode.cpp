7cc7e5e87267d96d0cfa3eea0bc010ee4c9a4cec
Yuanming Hu <yuanmhu@gmail.com>
2020-04-11 13:08:11 -0400

    [misc] Fixed dynamic node out-of-bound checker

--- taichi/ir/snode.cpp
+++ taichi/ir/snode.cpp
@@ -62,6 +62,20 @@ SNode &SNode::create_node(std::vector<Index> indices,
   return new_node;
 }
 
+SNode &SNode::dynamic_chunked(const Index &expr, int n, int chunk_size) {
+  auto &snode = create_node({expr}, {n}, SNodeType::dynamic);
+  snode.chunk_size = chunk_size;
+  return snode;
+  TI_ASSERT(bit::is_power_of_two(n));
+  TI_ASSERT(bit::is_power_of_two(chunk_size));
+  auto &child = insert_children(SNodeType::dynamic);
+  child.extractors[expr.value].activate(bit::log2int(n));
+  child.extractors[expr.value].num_elements = n;
+  child.n = n;
+  child.chunk_size = chunk_size;
+  return child;
+}
+
 void SNode::lazy_grad() {
   if (this->type == SNodeType::place)
     return;

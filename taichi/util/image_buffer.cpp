6e5d9fd980b8dd31b99561a0bfd131fdf79bcdc4
Taichi Gardener <taichigardener@gmail.com>
2020-04-12 11:40:09 -0400

    [misc] Unify #include format

--- taichi/util/image_buffer.cpp
+++ taichi/util/image_buffer.cpp
@@ -9,11 +9,11 @@
 
 #define STBI_FAILURE_USERMSG
 #define STB_IMAGE_IMPLEMENTATION
-#include <stb_image.h>
+#include "stb_image.h"
 #define STB_IMAGE_WRITE_IMPLEMENTATION
-#include <stb_image_write.h>
+#include "stb_image_write.h"
 #define STB_TRUETYPE_IMPLEMENTATION
-#include <stb_truetype.h>
+#include "stb_truetype.h"
 
 TI_NAMESPACE_BEGIN
 

beca66bfc6a3871715410881345bcc8efcefb5ae
Yuanming Hu <yuanming-hu@users.noreply.github.com>
2020-04-13 19:32:23 -0400

    [misc] Update `README.md` (#767)

--- Jenkinsfile
+++ Jenkinsfile
@@ -81,7 +81,7 @@ void build_taichi() {
     $CC --version
     $CXX --version
     echo $WORKSPACE
-    $PYTHON_EXECUTABLE -m pip install twine numpy Pillow scipy pybind11 colorama setuptools astor matplotlib pytest autograd GitPython distutils --user
+    $PYTHON_EXECUTABLE -m pip install twine numpy Pillow scipy pybind11 colorama setuptools astor matplotlib pytest autograd GitPython --user
     export TAICHI_REPO_DIR=$WORKSPACE/
     echo $TAICHI_REPO_DIR
     export PYTHONPATH=$TAICHI_REPO_DIR/python
--- misc/make_changelog.py
+++ misc/make_changelog.py
@@ -31,19 +31,29 @@ details = {
     'opengl': 'OpenGL backend',
     'misc': 'Miscellaneous',
     'opt': 'Optimization',
+    'example': 'Examples',
+    'pypi': 'PyPI package',
+    'autodiff': 'Automatic differentiation',
+    'gui': 'GUI',
+    'llvm': 'LLVM backend (CPU and CUDA)',
 }
 
 print(f'- (, 2020) v{ver} released')
 for i, c in enumerate(commits):
     s = format(c)
     if s.startswith('[release]'):
-        break
+        if ver in s:
+            all_changes = []
+            notable_changes = {}
+        else:
+            break
     tags = []
     while s[0] == '[':
         r = s.find(']')
         tag = s[1:r]
         tags.append(tag)
         s = s[r + 1:]
+        s = s.strip()
     for tag in tags:
         if tag[0].isupper():
             tag = tag.lower()
@@ -57,7 +67,7 @@ for i, c in enumerate(commits):
 for tag in sorted(notable_changes.keys()):
     print(f'   - **{details[tag]}**')
     for item in notable_changes[tag]:
-        print(f'      -{item}')
+        print(f'      - {item}')
 print(
     f'   - [Full log](https://github.com/taichi-dev/taichi/releases/tag/{ver})'
 )

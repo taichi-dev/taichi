0516404be045b7a5f7f5ae7c479ddb53d10ef359
xumingkuan <xumingkuan0721@126.com>
2020-04-14 18:25:54 -0400

    63 -> 27 statements for test_ad_if_mutable, even if not diving into container stmts?

--- taichi/ir/ir.h
+++ taichi/ir/ir.h
@@ -670,6 +670,8 @@ class Stmt : public IRNode {
     return (operand_bitmap & operand_hash(stmt)) != 0;
   }
 
+  bool have_operand(Stmt *stmt) const;
+
   void replace_with(Stmt *new_stmt);
   void replace_with(VecStatement &&new_statements, bool replace_usages = true);
   virtual void replace_operand_with(Stmt *old_stmt, Stmt *new_stmt);
--- taichi/transforms/optimize_local_variable.cpp
+++ taichi/transforms/optimize_local_variable.cpp
@@ -7,13 +7,81 @@ TLANG_NAMESPACE_BEGIN
 class AllocaOptimize : public BasicStmtVisitor {
  private:
   AllocaStmt *alloca;
+  bool stored; // Is this alloca ever stored?
+  LocalStoreStmt *last_store;
+  bool last_store_valid;
+  bool last_store_loaded; // Is the last store ever loaded?
+  AtomicOpStmt *last_atomic;
+  bool last_atomic_valid;
+  bool last_atomic_eliminable;
 
  public:
   using BasicStmtVisitor::visit;
 
-  explicit AllocaOptimize(AllocaStmt *alloca) : alloca(alloca) {
+  explicit AllocaOptimize(AllocaStmt *alloca)
+      : alloca(alloca),
+        stored(false),
+        last_store(nullptr),
+        last_store_valid(false),
+        last_store_loaded(false),
+        last_atomic(nullptr),
+        last_atomic_valid(false),
+        last_atomic_eliminable(false) {
     allow_undefined_visitor = true;
     invoke_default_visitor = true;
+  }
+
+  void preprocess_container_stmt(Stmt *stmt) override {
+    // temporarily being overcautious here
+    stored = true;
+    last_store_valid = false;
+    last_atomic_valid = false;
+  }
+
+  void visit(AtomicOpStmt *stmt) override {
+    if (stmt->dest != alloca)
+      return;
+    stored = true;
+    last_store_valid = false;
+    last_atomic = stmt;
+    last_atomic_valid = true;
+    last_atomic_eliminable = true;
+  }
+
+  void visit(LocalStoreStmt *stmt) override {
+    if (stmt->ptr != alloca)
+      return;
+    stored = true;
+    last_store = stmt;
+    last_store_valid = true;
+    last_store_loaded = false;
+    last_atomic_valid = false;
+  }
+
+  void visit(LocalLoadStmt *stmt) override {
+    bool regular = true;
+    for (int l = 0; l < stmt->width(); l++) {
+      if (stmt->ptr[l].offset != l || stmt->ptr[l].var != alloca) {
+        regular = false;
+      }
+      if (stmt->ptr[l].var == alloca) {
+        last_store_loaded = true;
+        if (last_atomic_valid)
+          last_atomic_eliminable = false;
+      }
+    }
+    if (!regular)
+      return;
+    if (!stored) {
+      auto zero = stmt->insert_after_me(Stmt::make<ConstStmt>(
+          LaneAttribute<TypedConstant>(alloca->ret_type.data_type)));
+      zero->repeat(stmt->width());
+      int current_stmt_id = stmt->parent->locate(stmt);
+      stmt->replace_with(zero);
+      stmt->parent->erase(current_stmt_id);
+      throw IRModified();
+    }
+    //TODO
   }
 
   void run() {
@@ -24,9 +92,33 @@ class AllocaOptimize : public BasicStmtVisitor {
     for (int i = location + 1; i < (int)block->size(); i++) {
       block->statements[i]->accept(this);
     }
+    if (last_store_valid && !last_store_loaded) {
+      // The last store is never loaded.
+      // Eliminate the last store.
+      TI_ASSERT(last_store);
+      last_store->parent->erase(last_store);
+      throw IRModified();
+    }
+    if (last_atomic_valid && last_atomic_eliminable) {
+      // The last AtomicOpStmt is never loaded.
+      TI_ASSERT(last_atomic);
+      if (irpass::analysis::gather_statements(
+          block, [&](Stmt *stmt) {
+            return stmt->have_operand(last_atomic);
+          }).empty()) {
+        // The last AtomicOpStmt is never used.
+        // Eliminate the last AtomicOpStmt.
+        last_atomic->parent->erase(last_atomic);
+        throw IRModified();
+      }
+    }
+    if (!stored && !last_store_loaded) {
+      // Never stored and never loaded.
+      // Eliminate this alloca.
+      block->erase(location);
+      throw IRModified();
+    }
   }
-
-
 };
 
 class AllocaFindAndOptimize : public BasicStmtVisitor {
@@ -74,7 +166,11 @@ class AllocaFindAndOptimize : public BasicStmtVisitor {
 
 namespace irpass {
 void optimize_local_variable(IRNode *root) {
+  std::cout << "before optimize_local_variable:\n";
+  print(root);
   AllocaFindAndOptimize::run(root);
+  std::cout << "after optimize_local_variable:\n";
+  print(root);
 }
 }  // namespace irpass
 

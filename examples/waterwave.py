f8b969356e498b37f3fc03acb66a04e2b43e8726
Taichi Gardener <taichigardener@gmail.com>
2020-04-23 04:05:39 -0400

    [skip ci] enforce code format

--- examples/waterwave.py
+++ examples/waterwave.py
@@ -19,12 +19,12 @@ kappa = 2
 gamma = 0.6
 eta = 1.333
 inv_eta = 1 / eta
-inv_eta2 = inv_eta ** 2
+inv_eta2 = inv_eta**2
 height = 2.5
 dx = 0.02
 dt = 0.01
 inv_dx = 1 / dx
-inv_dx2 = inv_dx ** 2
+inv_dx2 = inv_dx**2
 shape = bg.shape
 aspect = shape[0] / shape[1]
 n = shape[0]
@@ -37,33 +37,38 @@ velocity = ti.var(dt=ti.f32, shape=shape)
 acceleration = ti.var(dt=ti.f32, shape=shape)
 background.from_numpy(bg)
 
+
 @ti.func
 def laplacian(i, j):
-    return inv_dx2 * (
-            -4 * position[i, j]
-            + position[i, j - 1] + position[i, j + 1]
-            + position[i + 1, j] + position[i - 1, j]
-            ) / 4
+    return inv_dx2 * (-4 * position[i, j] + position[i, j - 1] +
+                      position[i, j + 1] + position[i + 1, j] +
+                      position[i - 1, j]) / 4
+
 
 @ti.func
 def gradient(i, j):
-    return ti.Vector([position[i + 1, j] - position[i - 1, j],
-        position[i, j + 1] - position[i, j - 1]]) * inv_dx
+    return ti.Vector([
+        position[i + 1, j] - position[i - 1, j],
+        position[i, j + 1] - position[i, j - 1]
+    ]) * inv_dx
+
 
 @ti.func
 def take_linear(i, j):
     m, n = int(i), int(j)
     i, j = i - m, j - n
-    return (i * j * background[m + 1, n + 1]
-            + (1 - i) * j * background[m, n + 1]
-            + i * (1 - j) * background[m + 1, n]
-            + (1 - i) * (1 - j) * background[m, n])
+    return (i * j * background[m + 1, n + 1] +
+            (1 - i) * j * background[m, n + 1] + i *
+            (1 - j) * background[m + 1, n] + (1 - i) *
+            (1 - j) * background[m, n])
+
 
 @ti.kernel
 def touch_at(hurt: ti.f32, x: ti.f32, y: ti.f32):
     for i, j in ti.ndrange((1, shape[0] - 1), (1, shape[1] - 1)):
         r2 = ti.sqr(i - x) + ti.sqr(j - y)
         position[i, j] = position[i, j] + hurt * ti.exp(-0.02 * r2)
+
 
 @ti.kernel
 def update():
@@ -73,6 +78,7 @@ def update():
     for i, j in ti.ndrange((1, shape[0] - 1), (1, shape[1] - 1)):
         velocity[i, j] = velocity[i, j] + acceleration[i, j] * dt
         position[i, j] = position[i, j] + velocity[i, j] * dt
+
 
 @ti.kernel
 def paint():
@@ -86,6 +92,7 @@ def paint():
         k, l = g[0] * coh, g[1] * coh
         color = take_linear(i + k, j + l)
         pixels[i, j] = (1 - fr) * color + fr * light_color
+
 
 i = 0
 gui = ti.GUI("Water Wave", shape)

200053dbcdcd3830d3722960040df3bf6a8ed19d
archibate <17721388340@163.com>
2020-04-25 18:09:01 +0800

    [skip travis] revert

--- misc/generate_commit_hash.py
+++ misc/generate_commit_hash.py
@@ -6,12 +6,5 @@ commit_hash = str(repo.head.commit)
 print(f"Building commit {commit_hash}")
 
 output_fn = os.path.join(repo_dir, 'taichi/common/commit_hash.h')
-try:
-    with open(output_fn, 'r') as f:
-        old_hash = f.read().split('"')[1]  # parse last commit hash
-        if commit_hash == old_hash:
-            exit()  # not changed
-except:
-    pass
 with open(output_fn, 'w') as f:
     f.write(f"#define TI_COMMIT_HASH \"{commit_hash}\"\n")

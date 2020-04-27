f8818bb8f9ea6f07f678cbc2ffa7c4a742d719d7
Taichi Gardener <taichigardener@gmail.com>
2020-04-26 23:29:56 -0400

    [skip ci] enforce code format

--- taichi/transforms/simplify.cpp
+++ taichi/transforms/simplify.cpp
@@ -457,16 +457,16 @@ class BasicBlockSimplify : public IRVisitor {
           continue;
         }
         if (!irpass::analysis::gather_statements(
-            block->statements[i].get(),
-            [&](Stmt *s) {
-              if (auto load = s->cast<LocalLoadStmt>())
-                return load->has_source(stmt->ptr);
-              else if (auto atomic = s->cast<AtomicOpStmt>())
-                return atomic->dest == stmt->ptr;
-              else
-                return false;
-            })
-            .empty()) {
+                 block->statements[i].get(),
+                 [&](Stmt *s) {
+                   if (auto load = s->cast<LocalLoadStmt>())
+                     return load->has_source(stmt->ptr);
+                   else if (auto atomic = s->cast<AtomicOpStmt>())
+                     return atomic->dest == stmt->ptr;
+                   else
+                     return false;
+                 })
+                 .empty()) {
           has_related = true;
           break;
         }

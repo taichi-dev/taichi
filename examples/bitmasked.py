f1a2bc313f7bc0fdd26ff1486524addbf70a0285
archibate <17721388340@163.com>
2020-05-02 23:01:06 +0800

    [skip ci] fix random alias

--- examples/bitmasked.py
+++ examples/bitmasked.py
@@ -4,12 +4,14 @@ import math
 ti.init(arch=ti.gpu)
 
 n = 256
-x = ti.var(ti.f32)
+mask = ti.var(ti.f32)
+img = ti.var(ti.f32)
 # `bitmasked` is a tensor that supports sparsity, in that each element can be
 # activated individually. (It can be viewed as `dense`, with an extra bit for each
 # element to mark its activation). Assigning to an element will activate it
 # automatically. Use struct-for syntax to loop over the active elements only.
-ti.root.bitmasked(ti.ij, (n, n)).place(x)
+ti.root.bitmasked(ti.ij, (n, n)).place(mask)
+ti.root.dense(ti.ij, (n, n)).place(img)
 
 
 @ti.kernel
@@ -17,29 +19,30 @@ def activate():
     # All elements in bitmasked is initially deactivated
     # Let's activate elements in the rectangle now!
     for i, j in ti.ndrange((100, 125), (100, 125)):
-        x[i, j] = 0  # assign any value to activate the element at (i, j)
+        mask[i, j] = 233  # assign any value to activate the element at (i, j)
 
 
 @ti.kernel
-def paint_active_pixels(t: ti.f32):
-    # struct-for syntax: loop over active pixels, inactive pixels are excluded
-    for i, j in x:
-        x[i, j] = t
+def paint_active_pixels(color: ti.f32):
+    # struct-for syntax: loop over active pixels, inactive pixels are skipped
+    for i, j in mask:
+        img[i, j] = color
 
 
 @ti.kernel
-def paint_all_pixels(t: ti.f32):
+def paint_all_pixels(color: ti.f32):
     # range-for syntax: loop over all pixels, no matter active or not
     for i, j in ti.ndrange(n, n):
-        x[i, j] = t
+        img[i, j] = color
 
+
+ti.root.deactivate_all()
+activate()
 
 gui = ti.GUI('bitmasked', (n, n))
 for frame in range(10000):
-    ti.root.deactivate_all()
-    activate()
-    t = math.sin(frame * 0.05) * 0.5 + 0.5
-    paint_active_pixels(t)
-    #paint_all_pixels(t)  # try this and compare the difference!
-    gui.set_image(x)
+    color = math.sin(frame * 0.05) * 0.5 + 0.5
+    paint_active_pixels(color)
+    #paint_all_pixels(color)  # try this and compare the difference!
+    gui.set_image(img)
     gui.show()

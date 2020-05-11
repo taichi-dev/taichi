21fdc98f06515e1b92e8a89239c579d3d5238333
Yuanming Hu <yuanmhu@gmail.com>
2020-05-11 01:10:21 -0400

    test negative offset

--- tests/python/test_offset.py
+++ tests/python/test_offset.py
@@ -12,7 +12,7 @@ def test_accessor():
 
 
 @ti.all_archs
-def test_struct_for():
+def test_struct_for_huge_offsets():
     a = ti.var(dt=ti.i32)
 
     offset = 1024, 2048, 2100, 2200
@@ -30,3 +30,22 @@ def test_struct_for():
             for k in range(offset[2], offset[2] + 4):
                 for l in range(offset[3], offset[3] + 4):
                     assert a[i, j, k, l] == i + j * 10 + k * 100 + l * 1000
+
+
+@ti.all_archs
+def test_struct_for_negative():
+    a = ti.var(dt=ti.i32)
+
+    offset = 16, -16
+    ti.root.dense(ti.ij, 32).place(a, offset=offset)
+
+    @ti.kernel
+    def test():
+        for i, j in a:
+            a[i, j] = i + j * 10
+
+    test()
+
+    for i in range(16, 48):
+        for j in range(-16, 16):
+            assert a[i, j] == i + j * 10

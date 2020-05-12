96ee2c7912a071665836f720484e983b279427a1
archibate <17721388340@163.com>
2020-05-13 00:12:23 +0800

    fix

--- taichi/util/environ_config.h
+++ taichi/util/environ_config.h
@@ -4,10 +4,14 @@
 #include <string>
 #include <cstdlib>
 
-int get_environ_config(const std::string &name, int default_value = 0)
+TLANG_NAMESPACE_BEGIN
+
+static int get_environ_config(const std::string &name, int default_value = 0)
 {
   char *res = std::getenv(name.c_str());
   if (res == nullptr)
     return default_value;
   return std::stoi(res);
 }
+
+TLANG_NAMESPACE_END

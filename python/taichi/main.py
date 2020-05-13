2bcaa449f3a725fdc5f2d6401c74409d73e44be3
archibate <17721388340@163.com>
2020-05-13 11:38:18 +0800

    [skip ci] clean

--- python/taichi/main.py
+++ python/taichi/main.py
@@ -73,12 +73,12 @@ def test_cpp(args):
 
 def get_benchmark_baseline_dir():
     import taichi as ti
-    return os.path.join(ti.core.get_repo_dir(), 'benchmarks', 'baselines')
+    return os.path.join(ti.core.get_repo_dir(), 'benchmarks', 'baseline')
 
 
 def get_benchmark_output_dir():
     import taichi as ti
-    return os.path.join(ti.core.get_repo_dir(), 'benchmarks', 'outputs')
+    return os.path.join(ti.core.get_repo_dir(), 'benchmarks', 'output')
 
 
 def display_benchmark_regression(xd, yd):

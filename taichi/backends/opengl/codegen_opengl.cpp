90f6f47470e9cc767f59c931476c18e9781346d0
archibate <17721388340@163.com>
2020-05-16 01:06:02 +0800

    tmp work save
    upd

--- taichi/backends/opengl/codegen_opengl.cpp
+++ taichi/backends/opengl/codegen_opengl.cpp
@@ -249,7 +249,7 @@ class KernelGen : public IRVisitor {
 
     emit("int {} = {} + {} * {}; // {}", stmt->short_name(),
          parent->short_name(),
-         struct_compiled_->class_children_map[parent_type],
+         struct_compiled_->snode_map[parent_type].elem_stride,
          stmt->input_index->short_name(), stmt->snode->node_type_name);
   }
 
@@ -258,8 +258,8 @@ class KernelGen : public IRVisitor {
   void visit(GetChStmt *stmt) override {
     emit("int {} = {} + {}; // {}", stmt->short_name(),
          stmt->input_ptr->short_name(),
-         struct_compiled_
-             ->class_get_map[stmt->input_snode->node_type_name][stmt->chid],
+         struct_compiled_->snode_map[stmt->input_snode->node_type_name]
+         .children_offsets[stmt->chid],
          stmt->output_snode->node_type_name);
     if (stmt->output_snode->is_place())
       ptr_signats[stmt->id] = "data";
--- taichi/backends/opengl/struct_opengl.cpp
+++ taichi/backends/opengl/struct_opengl.cpp
@@ -16,8 +16,7 @@ OpenglStructCompiler::CompiledResult OpenglStructCompiler::run(SNode &node) {
     generate_types(*n);
   }
   CompiledResult result;
-  result.class_get_map = std::move(class_get_map_);
-  result.class_children_map = std::move(class_children_map_);
+  result.snode_map = std::move(snode_map_);
   result.root_size = compute_snode_size(node);
   return result;
 }
@@ -32,41 +31,41 @@ void OpenglStructCompiler::collect_snodes(SNode &snode) {
 
 void OpenglStructCompiler::generate_types(const SNode &snode) {
   const bool is_place = snode.is_place();
+  const auto &node_name = snode.node_type_name;
   if (!is_place) {
-    const std::string class_name = snode.node_type_name + "_ch";
+    const std::string child_name = node_name + "_ch";
     size_t stride_num = 0;
-    auto &gimme = class_get_map_[snode.node_type_name];
-    gimme.resize(snode.ch.size());
-    std::vector<std::pair<int, SNode *>> smp;
+    auto &snode_info = snode_map_[node_name];
+    snode_info.children_offsets.resize(snode.ch.size());
+    std::vector<std::pair<int, SNode *>> table;
     for (int i = 0; i < (int)snode.ch.size(); i++) {
-      smp.push_back(std::pair<int, SNode *>(i, snode.ch[i].get()));
+      table.push_back(std::pair<int, SNode *>(i, snode.ch[i].get()));
     }
     // Discussion: https://github.com/taichi-dev/taichi/issues/804
     // The NVIDIA OpenGL seems to stuck at compile (a very long time to compile)
     // when a small SNode is placed at a very high address... And I've no idea
     // why. So let's sort by SNode size, smaller first:
-    std::sort(smp.begin(), smp.end(),
+    std::sort(table.begin(), table.end(),
               [this](const std::pair<int, SNode *> &a,
                      const std::pair<int, SNode *> &b) {
-                return stride_map_.at(a.second->node_type_name) <
-                       stride_map_.at(b.second->node_type_name);
+                return snode_map_[a.second->node_type_name].stride <
+                       snode_map_[b.second->node_type_name].stride;
               });
-    for (auto &&[i, ch] : smp) {
-      gimme[i] = stride_num;
-      stride_num += stride_map_.at(ch->node_type_name);
+    for (auto &&[i, ch] : table) {
+      snode_info.children_offsets[i] = stride_num;
+      stride_num += snode_map_[ch->node_type_name].stride;
     }
-    stride_map_[class_name] = stride_num;
+    snode_map_[child_name].stride = stride_num;
   }
-  const auto &node_name = snode.node_type_name;
   if (is_place) {
     const auto dt_name = opengl_data_type_name(snode.dt);
-    stride_map_[node_name] = data_type_size(snode.dt);
+    snode_map_[node_name].stride = data_type_size(snode.dt);
   } else if (snode.type == SNodeType::dense || snode.type == SNodeType::root) {
-    const int n = (snode.type == SNodeType::dense) ? snode.n : 1;
-    length_map_[node_name] = n;
-    stride_map_[node_name] =
-        stride_map_[node_name + "_ch"] * length_map_[node_name];
-    class_children_map_[node_name] = stride_map_.at(node_name + "_ch");
+    const int n = (snode.type == SNodeType::root) ? 1 : snode.n;
+    snode_map_[node_name].length = n;
+    snode_map_[node_name].stride =
+        snode_map_[node_name + "_ch"].stride * snode_map_[node_name].length;
+    snode_map_[node_name].elem_stride = snode_map_[node_name + "_ch"].stride;
   } else {
     TI_ERROR("SNodeType={} not supported on OpenGL",
              snode_type_name(snode.type));
@@ -82,7 +81,7 @@ size_t OpenglStructCompiler::compute_snode_size(const SNode &sn) {
   for (const auto &ch : sn.ch) {
     ch_size += compute_snode_size(*ch);
   }
-  const int n = (sn.type == SNodeType::dense) ? sn.n : 1;
+  const int n = (sn.type == SNodeType::root) ? 1 : sn.n;
   return n * ch_size;
 }
 

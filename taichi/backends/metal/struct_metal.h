2a9d019bb5934b40393a98b4b53dc9941aa21ff5
Ye Kuang <k-ye@users.noreply.github.com>
2020-05-22 00:54:06 +0900

    [metal] Fix SNode off-by-one problem (#986)
    * proper fix
    
    * [skip ci] enforce code format
    
    * add comment
    
    Co-authored-by: Taichi Gardener <taichigardener@gmail.com>

--- taichi/backends/metal/struct_metal.h
+++ taichi/backends/metal/struct_metal.h
@@ -11,22 +11,29 @@ namespace metal {
 struct SNodeDescriptor {
   const SNode *snode = nullptr;
   // Stride (bytes) of a single child element of this SNode.
+  // TODO(k-ye): Rename to |per_ch_stride|.
   int element_stride = 0;
   // This is NOT the number of elements per |snode|. Instead, it's that number
   // padded to the closest power of two.
+  // TODO(k-ye): Rename to |num_ch_slots|.
   int num_slots = 0;
   // Total bytes this SNode needs
   int stride = 0;
-  // Total number of this SNodes, NOT padded to PoT.
+  // Total number of CHILDREN of this SNode, NOT padded to PoT.
   // For example, for a layout of
   // ti.root
   //   .dense(ti.ij, (3, 2))  // S1
   //   .dense(ti.ij, (5, 3))  // S2
-  // |total_num_elems_from_root| for S2 is 3x2x5x3 = 90
+  // |total_num_elems_from_root| for S2 is 3x2x5x3 = 90. That is, S2 has a total
+  // of 90 children. Note that the number of S2 itself is 3x2=6. S2's
+  // ListManager also has a size of 6!
   int total_num_elems_from_root = 0;
   // An SNode can have any number of children, where each child starts at a
   // fixed offset in its parent's memory.
   int mem_offset_in_parent = 0;
+
+  int total_num_self_from_root(
+      const std::unordered_map<int, SNodeDescriptor> &sn_descs) const;
 };
 
 struct CompiledStructs {

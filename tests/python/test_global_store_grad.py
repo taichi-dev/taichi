9e77fb768930a6105694dea2dd8ad75ac9a8d62d
Rex W <rexwangcc@gmail.com>
2020-05-31 13:39:23 -0400

    [skip ci] test_global_store_branching. (why this was disabled?)

--- tests/python/test_global_store_grad.py
+++ tests/python/test_global_store_grad.py
@@ -1,36 +1,30 @@
-'''
 import taichi as ti
 
 ti.cfg.print_ir = True
 
 def test_global_store_branching():
-  # ti.reset()
+    # ti.reset()
 
-  N = 16
-  ti.runtime.print_preprocessed = True
-  x = ti.var(ti.f32)
-  y = ti.var(ti.f32)
+    N = 16
+    ti.runtime.print_preprocessed = True
+    x = ti.var(ti.f32)
+    y = ti.var(ti.f32)
 
-  @ti.layout
-  def place():
     ti.root.dense(ti.i, N).place(x)
     ti.root.dense(ti.i, N).place(y)
     ti.root.lazy_grad()
 
-  @ti.kernel
-  def oldeven():
+    @ti.kernel
+    def oldeven():
+        for i in range(N):
+            if i % 2 == 0:
+                x[i] = y[i]
+
     for i in range(N):
-      if i % 2 == 0:
-        x[i] = y[i]
+        x.grad[i] = 1
 
-  for i in range(N):
-    x.grad[i] = 1
+    oldeven()
+    oldeven.grad()
 
-  oldeven()
-  oldeven.grad()
-
-  for i in range(N):
-    assert y.grad[i] == (i % 2 == 0)
-
-test_global_store_branching()
-'''
+    for i in range(N):
+        assert y.grad[i] == (i % 2 == 0)

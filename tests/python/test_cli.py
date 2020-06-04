892c82f1b2908d6fc1fcedeab27a690244314700
archibate <17721388340@163.com>
2020-06-04 09:31:24 +0800

    fix test_cli

--- tests/python/test_cli.py
+++ tests/python/test_cli.py
@@ -172,7 +172,7 @@ def test_cli_video():
 
 
 def test_cli_format():
-    with patch_sys_argv_helper(["ti", "format", "-d",
+    with patch_sys_argv_helper(["ti", "format",
                                 "51fff7af"]) as custom_argv:
         cli = TaichiMain(test_mode=True)
         args = cli()

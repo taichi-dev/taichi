515b64649f560cf66bb3f771c528ea8677624b01
archibate <17721388340@163.com>
2020-06-04 09:28:35 +0800

    [skip ci] Proxy.xyzw, add test

--- python/taichi/lang/matrix.py
+++ python/taichi/lang/matrix.py
@@ -298,6 +298,38 @@ class Matrix(TaichiOperations):
                 key = [key]
             self.mat(*key)[self.index] = value
 
+        @property
+        def x(self):
+            return self[0]
+
+        @property
+        def y(self):
+            return self[1]
+
+        @property
+        def z(self):
+            return self[2]
+
+        @property
+        def w(self):
+            return self[3]
+
+        @x.setter
+        def x(self, value):
+            self[0] = value
+
+        @y.setter
+        def y(self, value):
+            self[1] = value
+
+        @z.setter
+        def z(self, value):
+            self[2] = value
+
+        @w.setter
+        def w(self, value):
+            self[3] = value
+
     # host access
     def __getitem__(self, index):
         return Matrix.Proxy(self, index)

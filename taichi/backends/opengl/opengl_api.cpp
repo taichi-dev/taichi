c6e6456773dac50b18c2ecf3b8dbf5a995b214a9
archibate <17721388340@163.com>
2020-06-06 15:33:32 +0800

    [Bug] [opengl] (try) Fix OpenGL to_numpy error on AMD cards

--- taichi/backends/opengl/opengl_api.cpp
+++ taichi/backends/opengl/opengl_api.cpp
@@ -341,8 +341,10 @@ bool initialize_opengl(bool error_tolerance) {
 void display_kernel_info(std::string const &kernel_name,
                          std::string const &kernel_source_code,
                          KernelParallelAttrib const &kpa) {
-  if (!taichi::starts_with(kernel_name, "snode_") &&
-      !taichi::starts_with(kernel_name, "tensor_"))
+  bool is_accessor = taichi::starts_with(kernel_name, "snode_")
+    || taichi::starts_with(kernel_name, "tensor_");
+  is_accessor = false;
+  if (!is_accessor)
     TI_DEBUG("source of kernel [{}] * {}:\n{}", kernel_name, kpa.num_groups,
              kernel_source_code);
 #ifdef _GLSL_DEBUG

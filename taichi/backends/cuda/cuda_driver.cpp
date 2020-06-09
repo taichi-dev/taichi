21352614174bfffa6fb38cb593b1e84b5dd174e7
Yuanming Hu <yuanmhu@gmail.com>
2020-06-09 13:36:45 -0400

    compiles

--- taichi/backends/cuda/cuda_driver.cpp
+++ taichi/backends/cuda/cuda_driver.cpp
@@ -43,7 +43,7 @@ CUDADriver::CUDADriver() {
     int version;
     driver_get_version(&version);
 
-    TI_TRACE("CUDA driver API (v{}.{}) loaded.", version / 1000,
+    TI_INFO("CUDA driver API (v{}.{}) loaded.", version / 1000,
              version % 1000 / 10);
   } else {
     TI_DEBUG("CUDA driver not found.");

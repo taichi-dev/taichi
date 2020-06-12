47eec5d83f96be90327396616d1896b24c09f779
Yuanming Hu <yuanmhu@gmail.com>
2020-06-12 16:31:43 -0400

    reproduce mutable integer ad bug

--- tests/python/test_ad_for.py
+++ tests/python/test_ad_for.py
@@ -139,7 +139,44 @@ def test_ad_fibonacci():
         assert b.grad[i] == f[i]
 
 
+# @ti.require(ti.extension.adstack)
+# @ti.all_archs
+def test_integer_stack():
+    ti.init()
+    N = 5
+    a = ti.var(ti.f32, shape=N, needs_grad=True)
+    b = ti.var(ti.f32, shape=N, needs_grad=True)
+    c = ti.var(ti.i32, shape=N)
+    f = ti.var(ti.f32, shape=N, needs_grad=True)
+    
+    
+    @ti.kernel
+    def int_stack():
+        for i in range(N):
+            weight = 1
+            s = 0.0
+            for j in range(c[i]):
+                s += weight * a[i] + b[i]
+                weight *= 10
+            f[i] = s
+
+    a.fill(1)
+    b.fill(1)
+    
+    for i in range(N):
+        c[i] = i
+    
+    int_stack()
+    
+    for i in range(N):
+        print(f[i])
+        f.grad[i] = 1
+    
+    int_stack.grad()
+    
+    for i in range(N):
+        print(a.grad[i], b.grad[i])
+
 # TODO: test integer stack (primal without adjoint)
 
-# test_ad_fibonacci()
-test_ad_sum_local_atomic()
+test_integer_stack()

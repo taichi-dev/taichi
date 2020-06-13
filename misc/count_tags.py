c61c2c403919949561878a527bfa0bcaa36f60ec
彭于斌 <1931127624@qq.com>
2020-06-13 11:14:15 +0800

    [workflow] Add a workflow to enforce PR title format (#1219)
    * [skip ci] [workflow] Add a workflow to enforce PR title format
    
    * [skip ci] fix yml syntax
    
    * [skip ci] fix typo
    
    * [skip ci] enforce code format
    
    * [skip ci] trig
    
    * [skip ci] bad
    
    * [skip ci] bad2
    
    * [skip ci] good
    
    * [skip ci] Update contributor_guide
    
    * [skip ci] enforce code format
    
    * [skip ci] no duplicate uppercase tag like #1189
    
    * [skip ci] Apply suggestions from code review
    
    Co-authored-by: Ye Kuang <k-ye@users.noreply.github.com>
    
    * [skip ci]
    
    * [skip ci] enforce code format
    
    * [skip ci] Apply suggestions from code review
    
    Co-authored-by: Chengchen(Rex) Wang <14366016+rexwangcc@users.noreply.github.com>
    
    * [skip ci] apply (json & exit)
    
    * [skip ci] enforce code format
    
    * [skip ci] Update .github/pull_request_template.md
    
    Co-authored-by: Chengchen(Rex) Wang <14366016+rexwangcc@users.noreply.github.com>
    
    * [skip ci] No colorama :(
    
    * [skip ci] enforce code format
    
    * [skip ci] Update docs/contributor_guide.rst
    
    Co-authored-by: Ye Kuang <k-ye@users.noreply.github.com>
    
    Co-authored-by: Taichi Gardener <taichigardener@gmail.com>
    Co-authored-by: Ye Kuang <k-ye@users.noreply.github.com>
    Co-authored-by: Chengchen(Rex) Wang <14366016+rexwangcc@users.noreply.github.com>

---
+++ misc/count_tags.py
@@ -0,0 +1,48 @@
+import sys, os
+from git import Repo
+
+commits = list(Repo('.').iter_commits('master'))
+
+authors = {}
+notable = {}
+changelog = {}
+
+for i, c in enumerate(commits):
+    s = c.summary
+
+    tags = []
+    while s[0] == '[':
+        r = s.find(']')
+        tag = s[1:r]
+        tags.append(tag)
+        s = s[r + 1:]
+        s = s.strip()
+
+    for tag in tags:
+        if tag[0].isupper():
+            tag = tag.lower()
+            notable[tag] = notable.get(tag, 0) + 1
+        changelog[tag] = changelog.get(tag, 0) + 1
+
+    a = str(c.author).split(' <')[0]
+    authors[a] = authors.get(a, 0) + 1
+
+
+def print_table(tab, name=''):
+    print('')
+    print(f' | {name} | counts |')
+    print(' | :--- | ---: |')
+    tab = sorted(tab.items(), key=lambda x: x[1], reverse=True)
+    for a, n in tab:
+        print(f' | {a} | {n} |')
+    return tab
+
+
+print('Authors:')
+print_table(authors, 'author')
+print('')
+print('Highlights:')
+print_table(notable, 'tag')
+print('')
+print('Full changelog:')
+print_table(changelog, 'tag')

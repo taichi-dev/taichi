0d3bb10cba33d956634ab03216f8d552bc25d565
archibate <1931127624@qq.com>
2020-06-14 13:42:25 +0800

    [skip ci] [workflow] Improve PR title checker for the release tag

--- misc/check_pr_title.py
+++ misc/check_pr_title.py
@@ -1,4 +1,16 @@
-import sys, os, json
+import sys, os, json, semver, git
+
+def get_old_ver():
+    repo = git.Repo('.')
+    for c in repo.iter_commits('master', max_count=200):
+        if c.summary.startswith('[release]'):
+            ver = c.summary.split(']', maxsplit=1)[1]
+            if ver[0] == 'v':
+                ver = ver[1:]
+            ver = ver.split(' ')[0]
+            oldver = semver.VersionInfo.parse(ver)
+            return oldver
+    raise ValueError('Could not find old version!')
 
 title = sys.argv[1]
 print(f'Checking PR title: {title}')
@@ -26,10 +38,6 @@ for x in title.split(']')[1:]:
     if x[1] == ' ':
         exit(f'Extra space before: {x[2:]}')
 
-x = title.split(']')[-1].strip()
-if x[0].islower():
-    exit(f'PR title should be uppercase at: {x}')
-
 had_upper = False
 for x in title.split('] ')[:-1]:
     if x[0] != '[':
@@ -42,5 +50,33 @@ for x in title.split('] ')[:-1]:
         if had_upper:
             exit(f'At most 1 uppercase tag expected, got: [{x[1:]}]')
         had_upper = True
+
+is_release = False
+for x in title.split('] ')[:-1]:
+    x = x[1:]
+    if x.lower() == 'release':
+        is_release = True
+        if not x.islower():
+            exit(f'[release] must be lowercase, got: [{x}]')
+
+if is_release:
+    ts = title.split(']')
+    if len(ts) != 2:
+        exit(f'Release PRs must have only one tag "[release]", got: {title}')
+    ver = ts[1][1:]
+    if ver[0] != 'v':
+        exit(f'Release version must be prepended with "v", got: {ver}')
+    ver = ver[1:]
+    try:
+        ver = semver.VersionInfo.parse(ver)
+    except ValueError:
+        exit(f'Invalid SemVer version: {ver}')
+    oldver = get_old_ver()
+    if ver not in [oldver.bump_minor(), oldver.bump_patch()]:
+        exit(f'Version bump incorrect: {oldver} -> {ver}')
+else:
+    x = title.split(']')[-1].strip()
+    if x[0].islower():
+        exit(f'PR title should be uppercase at: {x}')
 
 print('OK!')

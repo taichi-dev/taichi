3606da0ecb32ba2808dc753ea0877a03569f8795
Taichi Gardener <taichigardener@gmail.com>
2020-06-14 20:58:20 -0400

    [skip ci] enforce code format

--- python/taichi/lang/transformer.py
+++ python/taichi/lang/transformer.py
@@ -131,7 +131,7 @@ class ASTTransformer(ast.NodeTransformer):
             stmts = []
 
             holder = self.parse_stmt('__tmp_tuple = ti.expr_init_list(0, '
-                                    f'{len(targets)})')
+                                     f'{len(targets)})')
             holder.value.args[0] = node.value
 
             stmts.append(holder)

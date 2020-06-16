0463cb88b484cb25bc69c0ca546d6713320a2b5e
Taichi Gardener <taichigardener@gmail.com>
2020-06-15 20:35:55 -0400

    [skip ci] enforce code format

--- examples/odop_solar.py
+++ examples/odop_solar.py
@@ -16,17 +16,17 @@ class SolarSystem:
     def random_vector_in(rmax):
         # create a random vector
         a = ti.random() * math.tau
-        r = (ti.random() ** 0.8) * rmax  # 0.8 for adjusting pdf
+        r = (ti.random()**0.8) * rmax  # 0.8 for adjusting pdf
         return r * ti.Vector([ti.cos(a), ti.sin(a)])
 
     @ti.kernel
     def initialize(self):
         for i in range(self.n):
             offset = self.random_vector_in(0.5)
-            self.x[i] = self.center[None] + offset    # Offset from center
-            self.v[i] = [-offset.y, offset.x]         # Perpendicular to offset
+            self.x[i] = self.center[None] + offset  # Offset from center
+            self.v[i] = [-offset.y, offset.x]  # Perpendicular to offset
             self.v[i] += self.random_vector_in(0.02)  # Shaking
-            self.v[i] *= 1 / offset.norm()**1.5       # Kepler's 3rd law
+            self.v[i] *= 1 / offset.norm()**1.5  # Kepler's 3rd law
 
     @ti.func
     def gravity(self, pos):

ee55c6c6b8dd084821348f8c2b72dc314f5c48fd
Yuanming Hu <yuanmhu@gmail.com>
2020-06-18 14:21:57 -0400

    rename

--- taichi/transforms/ir_printer.cpp
+++ taichi/transforms/ir_printer.cpp
@@ -8,7 +8,7 @@
 
 TLANG_NAMESPACE_BEGIN
 
-std::string serialize_scratch_pad(const ScratchPadOptions &opt) {
+std::string scratch_pad_info(const ScratchPadOptions &opt) {
   std::string ser;
   if (!opt.empty()) {
     ser += "scratch_pad [ ";
@@ -29,7 +29,7 @@ std::string serialize_scratch_pad(const ScratchPadOptions &opt) {
   return ser;
 }
 
-std::string block_dim_hinted(int block_dim) {
+std::string block_dim_info(int block_dim) {
   return "block_dim=" +
          (block_dim == 0 ? "adaptive" : std::to_string(block_dim)) + " ";
 }
@@ -291,13 +291,13 @@ class IRPrinter : public IRVisitor {
     if (for_stmt->is_ranged()) {
       print("{} : for {} in range({}, {}) {}{{", for_stmt->name(), vars,
             for_stmt->begin->serialize(), for_stmt->end->serialize(),
-            block_dim_hinted(for_stmt->block_dim));
+            block_dim_info(for_stmt->block_dim));
     } else {
       print("{} : for {} in {} {}{}{{", for_stmt->name(), vars,
             for_stmt->global_var.cast<GlobalVariableExpression>()
                 ->snode->get_node_type_name_hinted(),
-            serialize_scratch_pad(for_stmt->scratch_opt),
-            block_dim_hinted(for_stmt->block_dim));
+            scratch_pad_info(for_stmt->scratch_opt),
+            block_dim_info(for_stmt->block_dim));
     }
     for_stmt->body->accept(this);
     print("}}");
@@ -307,7 +307,7 @@ class IRPrinter : public IRVisitor {
     print("{} : {}for in range({}, {}) (vectorize {}) {}{{", for_stmt->name(),
           for_stmt->reversed ? "reversed " : "", for_stmt->begin->name(),
           for_stmt->end->name(), for_stmt->vectorize,
-          block_dim_hinted(for_stmt->block_dim));
+          block_dim_info(for_stmt->block_dim));
     for_stmt->body->accept(this);
     print("}}");
   }
@@ -315,8 +315,8 @@ class IRPrinter : public IRVisitor {
   void visit(StructForStmt *for_stmt) override {
     print("{} : struct for in {} (vectorize {}) {}{}{{", for_stmt->name(),
           for_stmt->snode->get_node_type_name_hinted(), for_stmt->vectorize,
-          serialize_scratch_pad(for_stmt->scratch_opt),
-          block_dim_hinted(for_stmt->block_dim));
+          scratch_pad_info(for_stmt->scratch_opt),
+          block_dim_info(for_stmt->block_dim));
     for_stmt->body->accept(this);
     print("}}");
   }
@@ -474,12 +474,11 @@ class IRPrinter : public IRVisitor {
         end_str = fmt::format("tmp(offset={}B)", stmt->end_offset);
       }
       details = fmt::format("range_for({}, {}) {}", begin_str, end_str,
-                            block_dim_hinted(stmt->block_dim));
+                            block_dim_info(stmt->block_dim));
     } else if (stmt->task_type == stmt->struct_for) {
-      details = fmt::format("struct_for({}) {} {}",
-                            stmt->snode->get_node_type_name_hinted(),
-                            block_dim_hinted(stmt->block_dim),
-                            serialize_scratch_pad(stmt->scratch_opt));
+      details = fmt::format(
+          "struct_for({}) {}{}", stmt->snode->get_node_type_name_hinted(),
+          block_dim_info(stmt->block_dim), scratch_pad_info(stmt->scratch_opt));
     }
     if (stmt->task_type == OffloadedStmt::TaskType::listgen) {
       print("{} = offloaded listgen {}->{}", stmt->name(),

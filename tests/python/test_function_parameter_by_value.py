625fd898e114d3b1c92ec16a4608496f4c465771
彭于斌 <1931127624@qq.com>
2020-06-26 16:42:12 +0800

    Update test_function_parameter_by_value.py

--- tests/python/test_function_parameter_by_value.py
+++ tests/python/test_function_parameter_by_value.py
@@ -2,7 +2,7 @@ import taichi as ti
 
 
 @ti.all_archs
-def test_pass_by_value():
+def test_function_argument_pass_by_value():
     @ti.func
     def set_val(x, i):
         x = i
@@ -15,3 +15,21 @@ def test_pass_by_value():
 
     task()
     assert ret[None] == 0
+
+
+@ti.all_archs
+def test_kernel_argument_pass_by_value():
+    @ti.kernel
+    def task(x1: ti.i32, x2: ti.i32):
+        x1 = x1
+        x2 = x2
+        if x1 > x2:
+            x1 = x2
+        ret[None] = x1
+
+    ret = ti.var(ti.i32, shape=())
+
+    task(15, 13)
+    assert ret[None] == 13
+    task(15, 18)
+    assert ret[None] == 15

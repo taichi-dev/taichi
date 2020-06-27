b48230311486e2e6510f7d3a72112936dc3eab55
archibate <1931127624@qq.com>
2020-06-27 15:51:31 +0800

    what

--- python/taichi/lang/kernel_arguments.py
+++ python/taichi/lang/kernel_arguments.py
@@ -33,9 +33,8 @@ template = Template
 
 
 def decl_scalar_arg(dt):
-    import taichi as ti
     id = taichi_lang_core.decl_arg(dt, False)
-    return ti.expr_init(Expr(taichi_lang_core.make_arg_load_expr(id)))
+    return Expr(taichi_lang_core.make_arg_load_expr(id))
 
 
 def decl_ext_arr_arg(dt, dim):
--- tests/python/test_function_parameter_by_value.py
+++ tests/python/test_function_parameter_by_value.py
@@ -15,19 +15,3 @@ def test_function_argument_pass_by_value():
 
     task()
     assert ret[None] == 0
-
-
-@ti.all_archs
-def test_kernel_argument_pass_by_value():
-    @ti.kernel
-    def task(x1: ti.i32, x2: ti.i32):
-        if x1 > x2:
-            x1 = x2
-        ret[None] = x1
-
-    ret = ti.var(ti.i32, shape=())
-
-    task(15, 13)
-    assert ret[None] == 13
-    task(15, 18)
-    assert ret[None] == 15

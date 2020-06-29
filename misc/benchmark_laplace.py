b54086aa3e654a8e12da81947edc4e65a6520042
Yuanming Hu <yuanmhu@gmail.com>
2020-06-29 12:33:03 -0400

    benchmark laplace

---
+++ misc/benchmark_laplace.py
@@ -0,0 +1,33 @@
+import taichi as ti
+
+ti.init(arch=ti.cuda, kernel_profiler=True)
+x, y = ti.var(ti.f32), ti.var(ti.f32)
+
+N = 4096
+bs = 16
+
+block = ti.root.pointer(ti.ij, N // bs)
+block.dense(ti.ij, bs).place(x)
+block.dense(ti.ij, bs).place(y)
+
+
+@ti.kernel
+def populate():
+    for i in range(bs, N - bs):
+        for j in range(bs, N - bs):
+            x[i, j] = 1
+
+@ti.kernel
+def laplace():
+    for i, j in x:
+            y[i, j] = 4.0 * x[i, j] - x[i - 1,
+                                        j] - x[i + 1,
+                                               j] - x[i, j - 1] - x[i, j + 1]
+
+
+
+populate()
+for i in range(10):
+    laplace()
+
+ti.kernel_profiler_print()

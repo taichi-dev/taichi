8f3d85bdce37558b2a19359add7cf849702688da
archibate <1931127624@qq.com>
2020-06-29 10:10:39 +0800

    Revert "[skip ci] revert ti.chain_compare"
    This reverts commit d1a356e7126720c1a357bfc30ff16c6844b9462b.

--- python/taichi/lang/impl.py
+++ python/taichi/lang/impl.py
@@ -102,13 +102,10 @@ def subscript(value, *indices):
 def chain_compare(comparators, ops):
     assert len(comparators) == len(ops) + 1, \
       f'Chain comparison invoked with {len(comparators)} comparators but {len(ops)} operators'
-    evaluated_comparators = []
-    for i in range(len(comparators)):
-        evaluated_comparators += [expr_init(comparators[i])]
-    ret = expr_init(True)
+    ret = True
     for i in range(len(ops)):
-        lhs = evaluated_comparators[i]
-        rhs = evaluated_comparators[i + 1]
+        lhs = comparators[i]
+        rhs = comparators[i + 1]
         if ops[i] == 'Lt':
             now = lhs < rhs
         elif ops[i] == 'LtE':
@@ -123,7 +120,7 @@ def chain_compare(comparators, ops):
             now = lhs != rhs
         else:
             assert False, f'Unknown operator {ops[i]}'
-        ret = ret.logical_and(now)
+        ret = logical_and(ret, now)
     return ret
 
 

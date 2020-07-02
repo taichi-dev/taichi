7690fc1544a615039247ce2014eb710ad9181627
Yuanming Hu <yuanmhu@gmail.com>
2020-07-01 23:51:37 -0400

    working

--- misc/benchmark_laplace.py
+++ misc/benchmark_laplace.py
@@ -1,35 +1,46 @@
 import taichi as ti
 
 ti.init(arch=ti.cuda, kernel_profiler=True)#, print_ir=True)
+dim = 2
+
 x, y, y2 = ti.var(ti.i32), ti.var(ti.i32), ti.var(ti.i32)
 
 N = 1024
-bs = 8
+bs = 32
 
-block = ti.root.pointer(ti.ij, N // bs)
-block.dense(ti.ij, bs).place(x)
-block.dense(ti.ij, bs).place(y)
-block.dense(ti.ij, bs).place(y2)
+if dim == 2:
+    index = ti.ij
+elif dim == 3:
+    index = ti.ijk
+else:
+    assert False
+    
+block = ti.root.pointer(index, N // bs)
 
+block.dense(index, bs).place(x)
+block.dense(index, bs).place(y)
+block.dense(index, bs).place(y2)
+
+ndrange = ((bs, N - bs),) * dim
+stencil_range = ((-1, 2),) * dim
+print(ndrange)
+print(stencil_range)
 
 @ti.kernel
 def populate():
-    for i in range(bs, N - bs):
-        for j in range(bs, N - bs):
-            x[i, j] = j
+    for I in ti.grouped(ti.ndrange(*ndrange)):
+        x[I] = I.sum()
 
 @ti.kernel
 def laplace(use_bls: ti.template(), y: ti.template()):
     if ti.static(use_bls):
         ti.cache_shared(x)
-    for i, j in x:
-        # y[i, j] = x[i + 3, j - 1]#  - x[i, j]
-        # print(x[i, j + 1], x[i, j])
-        # y[i, j] = x[i, j + 1] - x[i, j]
-        # y[i, j] = - x[i, j]
-        y[i, j] = 4.0 * x[i, j] - x[i - 1,
-                                    j] - x[i + 1,
-                                           j] - x[i, j - 1] - x[i, j + 1]
+    ti.block_dim(bs ** dim)
+    for I in ti.grouped(x):
+        s = 0
+        for offset in ti.static(ti.grouped(ti.ndrange(*stencil_range))):
+            s = s + x[I + offset]
+        y[I] = s
 
 
 
@@ -38,11 +49,12 @@ for i in range(10):
     laplace(False, y2)
 for i in range(10):
     laplace(True, y)
-    
+
+
+ti.kernel_profiler_print()
+
 for i in range(N):
     for j in range(N):
         if y[i, j] != y2[i, j]:
             print(i, j, y[i, j], y2[i, j])
         assert y[i, j] == y2[i, j]
-
-ti.kernel_profiler_print()

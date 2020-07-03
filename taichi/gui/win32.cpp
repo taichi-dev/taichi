ff64c770446140472d552eb380c813cb19cf8932
JYLeeLYJ <JYLeeLYJ@qq.com>
2020-07-04 02:13:09 +0800

    [skip ci] change 'WHEEL' to 'Wheel'

--- taichi/gui/win32.cpp
+++ taichi/gui/win32.cpp
@@ -109,7 +109,7 @@ LRESULT CALLBACK WindowProc(HWND hwnd,
       y = GET_Y_LPARAM(lParam);
       gui->set_mouse_pos(x, gui->height - 1 - y);
       gui->key_events.push_back(
-          GUI::KeyEvent{GUI::KeyEvent::Type::move, "WHEEL", gui->cursor_pos,
+          GUI::KeyEvent{GUI::KeyEvent::Type::move, "Wheel", gui->cursor_pos,
                         Vector2i{0, GET_WHEEL_DELTA_WPARAM(wParam)}});
       break;
     case WM_MOUSEHWHEEL:
@@ -117,7 +117,7 @@ LRESULT CALLBACK WindowProc(HWND hwnd,
       y = GET_Y_LPARAM(lParam);
       gui->set_mouse_pos(x, gui->height - 1 - y);
       gui->key_events.push_back(
-          GUI::KeyEvent{GUI::KeyEvent::Type::move, "WHEEL", gui->cursor_pos,
+          GUI::KeyEvent{GUI::KeyEvent::Type::move, "Wheel", gui->cursor_pos,
                         Vector2i{GET_WHEEL_DELTA_WPARAM(wParam), 0}});
       break;
     case WM_PAINT:

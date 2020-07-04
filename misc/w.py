efc6ab5d5beedab05f73cebed10430d5dcaa8f87
Taichi Gardener <taichigardener@gmail.com>
2020-07-04 14:03:02 -0400

    [skip ci] enforce code format

--- misc/w.py
+++ misc/w.py
@@ -1,25 +1,28 @@
 import taichi as ti
 
+
 @ti.func
 def func3():
     ti.static_assert(1 + 1 == 3)
+
 
 @ti.func
 def func2():
     func3()
 
+
 @ti.func
 def func1():
     func2()
+
 
 @ti.kernel
 def func0():
     func1()
 
+
 ti.enable_excepthook()
 func0()
-
-
 '''Before:
 [Taichi] mode=development
 [Taichi] preparing sandbox at /tmp/taichi-4pwjc_r7

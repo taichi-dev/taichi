a80ce001f3e076d7f2d5f53d1729622d7c50abca
Taichi Gardener <taichigardener@gmail.com>
2020-07-03 21:27:06 -0400

    [skip ci] enforce code format

--- taichi/codegen/codegen_llvm.cpp
+++ taichi/codegen/codegen_llvm.cpp
@@ -1444,7 +1444,7 @@ void CodeGenLLVM::visit(LoopIndexBaseStmt *stmt) {
 void CodeGenLLVM::visit(BlockDimStmt *stmt) {
   TI_NOT_IMPLEMENTED  // No need for this statement for now. Untested so mark it
                       // as a loud failure.
-                          llvm_val[stmt] = create_call("block_dim", {});
+      llvm_val[stmt] = create_call("block_dim", {});
 }
 
 void CodeGenLLVM::visit(GlobalTemporaryStmt *stmt) {

5c43dcf2dbe1469e8192b7b5d839b63ddcb84e98
彭于斌 <1931127624@qq.com>
2020-07-06 21:55:30 +0800

    [error] Instruct user to use static range when matrix accessed with non constant index (#1420)

--- python/taichi/lang/matrix.py
+++ python/taichi/lang/matrix.py
@@ -5,6 +5,7 @@ import numbers
 import numpy as np
 from .util import taichi_scope, python_scope, deprecated, to_numpy_type, to_pytorch_type, in_python_scope
 from .common_ops import TaichiOperations
+from .exception import TaichiSyntaxError
 from collections.abc import Iterable
 import warnings
 
@@ -181,9 +182,16 @@ class Matrix(TaichiOperations):
         assert 0 <= args[1] < self.m
         # TODO(#1004): See if it's possible to support indexing at runtime
         for i, a in enumerate(args):
-            assert isinstance(
-                a, int
-            ), f'The {i}-th index of a Matrix/Vector must be a compile-time constant integer, got {a}'
+            if not isinstance(a, int):
+                raise TaichiSyntaxError(
+                    f'The {i}-th index of a Matrix/Vector must be a compile-time constant '
+                    'integer, got {a}. This is because matrix operations will be **unrolled**'
+                    ' at compile-time for performance reason.\n'
+                    'If you want to *iterate through matrix elements*, use a static range:\n'
+                    '  for i in ti.static(range(3)):\n'
+                    '    print(i, "-th component is", vec[i])\n'
+                    'See https://taichi.readthedocs.io/en/stable/meta.html#when-to-use-for-loops-with-ti-static for more details.'
+                    )
         return args[0] * self.m + args[1]
 
     def __call__(self, *args, **kwargs):

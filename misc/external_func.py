48f25b291e32e62219e7358e207262f7f43a5beb
Yuanming Hu <yuanmhu@gmail.com>
2020-07-09 20:38:31 -0400

    ctypes test

--- misc/external_func.py
+++ misc/external_func.py
@@ -6,6 +6,8 @@ os.system("g++ a.cpp -o a.so -fPIC -shared")
 
 so = ctypes.CDLL("./a.so")
 func = so.add
+func.argtypes = [ctypes.c_float, ctypes.c_float]
+func.restype = ctypes.c_float
 
 print(func(1, 2))
 

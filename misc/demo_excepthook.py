e88be8df36bb554b5347899c1e05c11cedbe919e
archibate <1931127624@qq.com>
2020-07-10 22:05:13 +0800

    [skip ci] revert demo_excepthook

--- misc/demo_excepthook.py
+++ misc/demo_excepthook.py
@@ -4,15 +4,23 @@ ti.enable_excepthook()
 
 
 @ti.func
-def fun():
-    x = ti.Vector([2, 3])
-    for i in range(3):
-        print(x[i])
+def func3():
+    ti.static_assert(1 + 1 == 3)
+
+
+@ti.func
+def func2():
+    func3()
+
+
+@ti.func
+def func1():
+    func2()
 
 
 @ti.kernel
-def kern():
-    fun()
+def func0():
+    func1()
 
 
-kern()
+func0()

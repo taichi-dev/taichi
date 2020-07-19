378573297be9dad5aaf92729bbb690fa5b1fe48c
Ye Kuang <yekuang.ky@gmail.com>
2020-07-19 23:03:21 +0900

    [misc] Add is_path_all_dense to SNode

--- taichi/ir/snode.h
+++ taichi/ir/snode.h
@@ -83,6 +83,9 @@ class SNode {
   Kernel *writer_kernel{};
   Expr expr;
 
+  // Whether the path from root to |this| contains only `dense` SNodes.
+  bool is_path_all_dense{false};
+
   SNode();
 
   SNode(int depth, SNodeType t);
@@ -101,11 +104,7 @@ class SNode {
 
   int get_num_bits(int physical_index) const;
 
-  SNode &insert_children(SNodeType t) {
-    ch.push_back(create(depth + 1, t));
-    // Note: parent will not be set until structural nodes are compiled!
-    return *ch.back();
-  }
+  SNode &insert_children(SNodeType t);
 
   SNode &create_node(std::vector<Index> indices,
                      std::vector<int> sizes,
@@ -162,11 +161,6 @@ class SNode {
 
   SNode &hash(const Index &index, int size) {
     return hash(std::vector<Index>{index}, size);
-  }
-
-  template <typename... Args>
-  static std::unique_ptr<SNode> create(Args &&... args) {
-    return std::make_unique<SNode>(std::forward<Args>(args)...);
   }
 
   std::string type_name() {

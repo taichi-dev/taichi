9ca3591165147ea1dca8778f254e79c294542f19
Yuanming Hu <yuanmhu@gmail.com>
2020-07-22 10:58:47 -0400

    refactor locked_task.h

--- taichi/runtime/llvm/locked_task.h
+++ taichi/runtime/llvm/locked_task.h
@@ -1,28 +1,22 @@
 #pragma once
 
-template <typename T>
+template <typename T, typename G>
 class lock_guard {
-  Ptr lock;
-
  public:
-  lock_guard(Ptr lock, const T &func) : lock(lock) {
+  lock_guard(Ptr lock, const T &func, const G &test) {
 #if ARCH_x64 || ARCH_arm64
     mutex_lock_i32(lock);
     func();
     mutex_unlock_i32(lock);
 #else
     // CUDA
+
     auto fast = false;
     if (fast) {
       auto active_mask = cuda_active_mask();
       auto remaining = active_mask;
       while (remaining) {
         auto leader = cttz_i32(remaining);
-        /*
-        if (~remaining & (1u << leader)) {
-          while(true);
-        }
-         */
         if (warp_idx() == leader) {
           // Memory fences here are necessary since CUDA has a weakly ordered
           // memory model across threads
@@ -31,9 +25,7 @@ class lock_guard {
           func();
           grid_memfence();
           mutex_unlock_i32(lock);
-          grid_memfence();
         }
-        warp_barrier(active_mask);
         remaining &= ~(1u << leader);
       }
     } else {
@@ -41,12 +33,15 @@ class lock_guard {
         if (warp_idx() == i) {
           // Memory fences here are necessary since CUDA has a weakly ordered
           // memory model across threads
-          mutex_lock_i32(lock);
-          grid_memfence();
-          func();
-          grid_memfence();
-          mutex_unlock_i32(lock);
-          // grid_memfence();
+          if (test()) {
+            mutex_lock_i32(lock);
+            grid_memfence();
+            if (test()) {
+              func();
+            }
+            grid_memfence();
+            mutex_unlock_i32(lock);
+          }
         }
       }
     }
@@ -65,42 +60,13 @@ class lock_guard {
 #endif
   }
 };
-template <typename T>
-void locked_task(void *lock, const T &func) {
-  lock_guard<T> _((Ptr)lock, func);
-}
 
-template <typename T, typename G>
-class lock_guard2 {
- public:
-  lock_guard2(Ptr lock, const T &func, const G &test) {
-#if ARCH_x64 || ARCH_arm64
-    mutex_lock_i32(lock);
-    func();
-    mutex_unlock_i32(lock);
-#else
-    // CUDA
-
-    for (int i = 0; i < warp_size(); i++) {
-      if (warp_idx() == i) {
-        // Memory fences here are necessary since CUDA has a weakly ordered
-        // memory model across threads
-        if (test()) {
-          mutex_lock_i32(lock);
-          grid_memfence();
-          if (test()) {
-            func();
-          }
-          grid_memfence();
-          mutex_unlock_i32(lock);
-          // grid_memfence();
-        }
-      }
-    }
-#endif
-  }
-};
 template <typename T, typename G>
 void locked_task(void *lock, const T &func, const G &test) {
-  lock_guard2<T, G> _((Ptr)lock, func, test);
+  lock_guard<T, G> _((Ptr)lock, func, test);
+}
+
+template <typename T>
+void locked_task(void *lock, const T &func) {
+  locked_task(lock, func, []() { return true; });
 }

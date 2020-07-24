62700958f785140007f1503562122cdc63b43a53
Yuanming Hu <yuanmhu@gmail.com>
2020-07-24 11:26:16 -0400

    1d

--- misc/test_gc.py
+++ misc/test_gc.py
@@ -8,20 +8,18 @@ res = (128, 128)
 dx = 1 / 128
 inv_dx = 1.0 / dx
 
-indices = ti.ij
-
 m = ti.var(dt=ti.f32)
 
-grid = ti.root.pointer(indices, 32)
-grid.pointer(indices, 32).dense(indices, 8).place(m)
+grid = ti.root.pointer(ti.i, 32)
+grid.pointer(ti.i, 32).dense(ti.i, 8).place(m)
 
 @ti.kernel
-def build_pid(i: ti.i32):
+def build_pid():
     ti.parallelize(2)
     for j in range(100):
-        m[j, j] += 1
+        m[j] += 1
 
 for i in range(100):
     grid.deactivate_all()
-    build_pid(0)
+    build_pid()
     ti.memory_profiler_print()

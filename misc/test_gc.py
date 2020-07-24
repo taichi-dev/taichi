ba34948f70fa589c2bacab89133ed9e8afaf0807
Yuanming Hu <yuanmhu@gmail.com>
2020-07-24 11:45:45 -0400

    simplify

--- misc/test_gc.py
+++ misc/test_gc.py
@@ -1,7 +1,6 @@
 import taichi as ti
-import random
 
-ti.init(arch=ti.cpu, print_ir=True)
+ti.init(arch=ti.cpu)
 
 
 res = (128, 128)
@@ -15,11 +14,9 @@ grid.pointer(ti.i, 128).dense(ti.i, 32).place(m)
 
 @ti.kernel
 def act():
-    ti.parallelize(2)
-    for j in range(100000):
+    ti.parallelize(4)
+    for j in range(10000000):
         m[0] += 1
 
-for i in range(100):
-    # grid.deactivate_all()
-    # act()
-    ti.memory_profiler_print()
+act()
+ti.memory_profiler_print()

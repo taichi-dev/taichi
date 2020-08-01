160bbc1edfb67f8136e60074d532e7a028be1c8e
Yuanming Hu <yuanming-hu@users.noreply.github.com>
2020-07-31 20:11:47 -0400

    Revert "[Infra] Add "ti.profiler" (PythonProfiler) for intuitive Python-scope profiling (#1493)"
    This reverts commit 7574254a5ddcdda708378ff3ad945102f4cf6226.

--- python/taichi/misc/__init__.py
+++ python/taichi/misc/__init__.py
@@ -2,7 +2,6 @@ from .util import *
 from .gui import *
 from .image import *
 from .error import *
-from .timing import profiler
 from .task import Task
 
 __all__ = [s for s in dir() if not s.startswith('_')]

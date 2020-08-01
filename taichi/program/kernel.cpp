6ae8c7e2b9687ff91f831af2f5f3689354992649
archibate <1931127624@qq.com>
2020-08-01 21:53:52 +0800

    app

--- taichi/program/kernel.cpp
+++ taichi/program/kernel.cpp
@@ -119,12 +119,8 @@ void Kernel::set_arg_float(int i, float64 d) {
   TI_ASSERT_INFO(
       !args[i].is_nparray,
       "Assigning a scalar value to a numpy array argument is not allowed");
-
-  ActionRecorder::get_instance().record(
-      "set_kernel_arg_float64", {ActionArg("kernel_name", name),
-                                 ActionArg("arg_id", i), ActionArg("val", d)});
-
   auto dt = args[i].dt;
+
   if (dt == DataType::f32) {
     program.context.set_arg(i, (float32)d);
   } else if (dt == DataType::f64) {
@@ -154,12 +150,8 @@ void Kernel::set_arg_int(int i, int64 d) {
   TI_ASSERT_INFO(
       !args[i].is_nparray,
       "Assigning scalar value to numpy array argument is not allowed");
-
-  ActionRecorder::get_instance().record(
-      "set_kernel_arg_int64", {ActionArg("kernel_name", name),
-                               ActionArg("arg_id", i), ActionArg("val", d)});
-
   auto dt = args[i].dt;
+
   if (dt == DataType::i32) {
     program.context.set_arg(i, (int32)d);
   } else if (dt == DataType::i64) {
@@ -246,12 +238,6 @@ void Kernel::set_extra_arg_int(int i, int j, int32 d) {
 void Kernel::set_arg_nparray(int i, uint64 ptr, uint64 size) {
   TI_ASSERT_INFO(args[i].is_nparray,
                  "Assigning numpy array to scalar argument is not allowed");
-
-  ActionRecorder::get_instance().record(
-      "set_kernel_arg_ext_ptr",
-      {ActionArg("kernel_name", name), ActionArg("arg_id", i),
-       ActionArg("address", fmt::format("0x{:x}", ptr)),
-       ActionArg("array_size_in_bytes", (int64)size)});
 
   args[i].size = size;
   program.context.set_arg(i, ptr);

60b44c95442bd0d94d62bb335594433e6ca44406
archibate <1931127624@qq.com>
2020-08-05 23:46:07 +0800

    nit

--- misc/ci_download.py
+++ misc/ci_download.py
@@ -1,9 +1,6 @@
 import os
 
 platform = os.environ['CI_PLATFORM']
-
-print(f'CI_PLATFORM={platform}')
-
 if platform.startswith('macos'):
     platform = 'macos'
 elif platform.startswith('ubuntu'):
@@ -13,6 +10,6 @@ elif platform.startswith('windows'):
 else:
     raise Exception(f'Bad CI_PLATFORM={platform}')
 
-url = f'https://github.com/taichi-dev/taichi_assets/releases/download/llvm10/taichi-llvm-10.0.0-{platform}.zip'
-print(f'Downloading LLVM from {url}...')
-os.system(f'wget {url} --waitretry=3 --tries=5 -O taichi-llvm.zip')
+llvm_url = f'https://github.com/taichi-dev/taichi_assets/releases/download/llvm10/taichi-llvm-10.0.0-{platform}.zip'
+print(f'Downloading LLVM from {llvm_url}...')
+os.system(f'wget {llvm_url} --waitretry=3 --tries=5 -O taichi-llvm.zip')

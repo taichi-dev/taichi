71336af8aadb1fa92d9bcca307233015c1dac80a
archibate <1931127624@qq.com>
2020-08-05 23:34:29 +0800

    [workflow] Add Mac OS X to Github CI

---
+++ misc/ci_download.py
@@ -0,0 +1,15 @@
+import os
+
+platform = os.environ['CI_PLATFORM']
+
+if platform.startswith('macos'):
+    platform = 'macos'
+elif platform.startswith('ubuntu'):
+    platform = 'linux'
+elif platform.startswith('windows'):
+    platform = 'msvc2019'
+else:
+    raise Exception(f'Bad CI_PLATFORM={platform}')
+
+url = f'https://github.com/taichi-dev/taichi_assets/releases/download/llvm10/taichi-llvm-10.0.0-${platform}.zip'
+os.system(f'wget {url} --waitretry=3 --tries=5 -O llvm.zip')

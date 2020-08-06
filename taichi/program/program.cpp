9c940f4f2af77ce4b3e3654a02986c160f2cd528
彭于斌 <1931127624@qq.com>
2020-08-06 13:09:58 +0800

    [benchmark] Fix test_test.py benchmark including '<lambda>' in filename (#1650)
    * [benchmark] Fix test_test.py benchmark including '<lambda>' in filename
    
    * better deprecation warning fix #1651

--- taichi/program/program.cpp
+++ taichi/program/program.cpp
@@ -664,6 +664,12 @@ void Program::finalize() {
       auto first_space_pos = file_name.find_first_of(' ');
       TI_ASSERT(first_space_pos != std::string::npos);
       file_name = file_name.substr(0, first_space_pos);
+      if (auto lt_pos = file_name.find('<'); lt_pos != std::string::npos) {
+        file_name[lt_pos] = '_';
+      }
+      if (auto gt_pos = file_name.find('>'); gt_pos != std::string::npos) {
+        file_name[gt_pos] = '_';
+      }
       file_name += ".dat";
       file_name = std::string(output_dir) + "/" + file_name;
       TI_INFO("Saving benchmark result to {}", file_name);

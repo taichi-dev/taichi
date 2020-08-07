b066177b1a0618b2e0da9aea9fc8f0ad52344db3
archibate <1931127624@qq.com>
2020-08-07 23:27:27 +0800

    [skip ci] system hint

--- misc/ci_build.py
+++ misc/ci_build.py
@@ -3,9 +3,10 @@ import os
 platform = os.environ['CI_PLATFORM']
 
 def system(x):
+    print(f'[ci] executing: {x}')
     ret = os.system(x)
     if ret != 0:
-        exit(ret >> 8)
+        exit(1)
 
 def add_path(x):
     path_sep = ';' if platform.startswith('windows') else ':'

a5c9c93d4c4d7daa09dc1fea4bf51a0c1381ed55
Xuanda Yang <th3charlie@gmail.com>
2020-08-25 01:10:45 +0800

    update tests according to comments

--- tests/python/test_bit_operations.py
+++ tests/python/test_bit_operations.py
@@ -4,16 +4,12 @@ import operator as ops
 from taichi import allclose
 import pytest
 
-func_table = [
-    (ops.lshift, ) * 2,
-]
+@ti.test()
+def test_bit_shl():
 
+    @ti.kernel
+    def shl(a: ti.i32, b: ti.i32) -> ti.i32:
+        return a << b
 
-@pytest.mark.parametrize('ti_func,np_func', func_table)
-def test_python_scope_vector_binary_bit_operations(ti_func, np_func):
-    x = ti.Vector([2, 3])
-    y = ti.Vector([5, 4])
-
-    result = ti_func(x, y).to_numpy()
-    expected = np_func(x.to_numpy(), y.to_numpy())
-    assert allclose(result, expected)
+    for i in range(8):
+        assert shl(3, i) == 3 * 2 ** i

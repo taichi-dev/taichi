8e9ab12a09a812b3946a1945ce8f7fd07d6fd307
Xuanda Yang <th3charlie@gmail.com>
2020-08-25 02:21:07 +0800

    [IR] [lang] Support SHL operator: x << y (#1762)

---
+++ tests/python/test_bit_operations.py
@@ -0,0 +1,15 @@
+import taichi as ti
+import numpy as np
+import operator as ops
+from taichi import allclose
+import pytest
+
+
+@ti.test()
+def test_bit_shl():
+    @ti.kernel
+    def shl(a: ti.i32, b: ti.i32) -> ti.i32:
+        return a << b
+
+    for i in range(8):
+        assert shl(3, i) == 3 * 2**i

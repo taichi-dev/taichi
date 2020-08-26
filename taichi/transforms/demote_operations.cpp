ec5de7765910db0afa9388afa7e7a405f5417bbf
archibate <1931127624@qq.com>
2020-08-26 15:07:27 +0800

    fix test: drop @xumingkuan's method for safety

--- taichi/transforms/demote_operations.cpp
+++ taichi/transforms/demote_operations.cpp
@@ -21,36 +21,45 @@ class DemoteOperations : public BasicStmtVisitor {
       if (is_integral(rhs->element_type()) && is_integral(lhs->element_type())) {
         // @ti.func
         // def ifloordiv(a, b):
-        //     return (a if (a < 0) == (b < 0) else (a - b + 1)) / b
+        //     r = ti.raw_div(a, b)
+        //     if (a < 0) != (b < 0) and a and b * r != a:
+        //         r = r - 1
+        //     return r
+        auto ret = Stmt::make<BinaryOpStmt>(BinaryOpType::div, lhs, rhs);
         auto zero = Stmt::make<ConstStmt>(LaneAttribute<TypedConstant>(0));
-        auto one = Stmt::make<ConstStmt>(LaneAttribute<TypedConstant>(1));
-        auto lhs_ltz = Stmt::make<BinaryOpStmt>(
-            BinaryOpType::cmp_lt, lhs, zero.get());
-        auto rhs_ltz = Stmt::make<BinaryOpStmt>(
-            BinaryOpType::cmp_lt, rhs, zero.get());
-        auto lhs_ltz_eq_rhs_ltz = Stmt::make<BinaryOpStmt>(
-            BinaryOpType::cmp_eq, lhs_ltz.get(), rhs_ltz.get());
-        auto lhs_sub_rhs = Stmt::make<BinaryOpStmt>(
-            BinaryOpType::sub, lhs, rhs);
-        auto lhs_sub_rhs_add_one = Stmt::make<BinaryOpStmt>(
-            BinaryOpType::add, lhs_sub_rhs.get(), one.get());
-        auto ternary = Stmt::make<TernaryOpStmt>(
-            TernaryOpType::select, lhs_ltz_eq_rhs_ltz.get(),
-            lhs, lhs_sub_rhs_add_one.get());
-        auto ret = Stmt::make<BinaryOpStmt>(
-            BinaryOpType::div, ternary.get(), rhs);
+        auto lhs_ltz =
+            Stmt::make<BinaryOpStmt>(BinaryOpType::cmp_lt, lhs, zero.get());
+        auto rhs_ltz =
+            Stmt::make<BinaryOpStmt>(BinaryOpType::cmp_lt, rhs, zero.get());
+        auto rhs_mul_ret =
+            Stmt::make<BinaryOpStmt>(BinaryOpType::mul, rhs, ret.get());
+        auto cond1 = Stmt::make<BinaryOpStmt>(BinaryOpType::cmp_ne,
+                                              lhs_ltz.get(), rhs_ltz.get());
+        auto cond2 =
+            Stmt::make<BinaryOpStmt>(BinaryOpType::cmp_ne, lhs, zero.get());
+        auto cond3 = Stmt::make<BinaryOpStmt>(BinaryOpType::cmp_ne,
+                                              rhs_mul_ret.get(), lhs);
+        auto cond12 = Stmt::make<BinaryOpStmt>(BinaryOpType::bit_and,
+                                               cond1.get(), cond2.get());
+        auto cond = Stmt::make<BinaryOpStmt>(BinaryOpType::bit_and,
+                                             cond12.get(), cond3.get());
+        auto real_ret = Stmt::make<BinaryOpStmt>(BinaryOpType::add,
+            ret.get(), cond.get());
 
-        stmt->replace_with(ret.get());
+        stmt->replace_with(real_ret.get());
+        modifier.insert_before(stmt, std::move(ret));
         modifier.insert_before(stmt, std::move(zero));
-        modifier.insert_before(stmt, std::move(one));
         modifier.insert_before(stmt, std::move(lhs_ltz));
         modifier.insert_before(stmt, std::move(rhs_ltz));
-        modifier.insert_before(stmt, std::move(lhs_ltz_eq_rhs_ltz));
-        modifier.insert_before(stmt, std::move(lhs_sub_rhs));
-        modifier.insert_before(stmt, std::move(lhs_sub_rhs_add_one));
-        modifier.insert_before(stmt, std::move(ternary));
-        modifier.insert_before(stmt, std::move(ret));
+        modifier.insert_before(stmt, std::move(rhs_mul_ret));
+        modifier.insert_before(stmt, std::move(cond1));
+        modifier.insert_before(stmt, std::move(cond2));
+        modifier.insert_before(stmt, std::move(cond3));
+        modifier.insert_before(stmt, std::move(cond12));
+        modifier.insert_before(stmt, std::move(cond));
+        modifier.insert_before(stmt, std::move(real_ret));
         modifier.erase(stmt);
+
       } else {
         // @ti.func
         // def ffloordiv(a, b):

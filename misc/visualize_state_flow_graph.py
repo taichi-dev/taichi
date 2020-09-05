67c747b57be83279674fb316531d85ec0a625f1d
Yuanming Hu <yuanmhu@gmail.com>
2020-09-05 01:24:02 -0400

    finalize

--- misc/visualize_state_flow_graph.py
+++ misc/visualize_state_flow_graph.py
@@ -4,19 +4,23 @@ ti.init(arch=ti.cpu, async_mode=True)
 
 x = ti.field(ti.i32)
 y = ti.field(ti.i32)
+z = ti.field(ti.i32)
 
-ti.root.pointer(ti.i, 128).dense(ti.i, 16).place(x, y)
+ti.root.pointer(ti.i, 128).dense(ti.i, 16).place(x, y, z)
+
 
 @ti.kernel
 def foo():
     for i in x:
-        y[i] = x[i]
-        
+        y[i] = x[i] + 1
+
+
 @ti.kernel
 def bar():
     for i in y:
-        y[i] = x[i] + 1
-        
+        z[i] = y[i] + 1
+
+
 foo()
 bar()
 

133238a72769ea14def7e6d1ec4f1d15b581d3b6
Yuanming Hu <yuanmhu@gmail.com>
2020-10-01 15:37:33 -0400

    remove unused get_primitive_type_node

--- taichi/python/export_lang.cpp
+++ taichi/python/export_lang.cpp
@@ -546,9 +546,6 @@ void export_lang(py::module &m) {
 #include "taichi/inc/data_type.inc.h"
 #undef PER_TYPE
 
-  // Maybe useful for Python-side pickling?
-  m.def("get_primitive_type_node", PrimitiveType::get);
-
   m.def("is_integral", is_integral);
   m.def("is_signed", is_signed);
   m.def("is_unsigned", is_unsigned);

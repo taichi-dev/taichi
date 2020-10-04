fa34480e3ad0f8315ee5f3989d8fcc0921283e65
archibate <1931127624@qq.com>
2020-10-04 23:07:18 +0800

    environ control

--- python/taichi/misc/gui.py
+++ python/taichi/misc/gui.py
@@ -2,6 +2,7 @@ import numbers
 import numpy as np
 from taichi.core import ti_core
 from .util import deprecated, core_veci
+import os
 
 
 class GUI:
@@ -40,6 +41,11 @@ class GUI:
                  background_color=0x0,
                  show_gui=True,
                  fullscreen=False):
+        if 'TI_GUI_SHOW' in os.environ:
+            show_gui = bool(int(os.environ['TI_GUI_SHOW']))
+        if 'TI_GUI_FULLSCREEN' in os.environ:
+            fullscreen = bool(int(os.environ['TI_GUI_FULLSCREEN']))
+
         self.name = name
         if isinstance(res, numbers.Number):
             res = (res, res)

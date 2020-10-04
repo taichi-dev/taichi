def5991fc8ef0760364f29d789b1012294dda6dd
archibate <1931127624@qq.com>
2020-10-04 20:42:10 +0800

    [example] Upgrade mass_spring_3d.py to Taichi THREE v0.0.5

--- examples/mass_spring_3d.py
+++ examples/mass_spring_3d.py
@@ -5,14 +5,15 @@ try:
     import taichi_three as t3
 except ImportError:
     print('This example needs the extension library Taichi THREE to work.'
-          'Please run `pip install --user taichi_three` to install it.')
-assert t3.__version__ == (0, 0, 3)
+          'Please run `pip install taichi_three==0.0.5` to install it.')
+if t3.__version__ != (0, 0, 5):
+    print('WARNING: Taichi THREE v0.0.5 expected, got v' + '.'.join(map(str, t3.__version__)))
 
 ti.init(arch=ti.gpu)
 
 ### Parameters
 
-N = 256
+N = 128
 NN = N, N
 W = 1
 L = W / N
@@ -49,8 +50,7 @@ def substep():
         v[i] += stiffness * acc * dt
     for i in ti.grouped(x):
         v[i].y -= gravity * dt
-        v[i] = tl.ballBoundReflect(x[i], v[i], tl.vec(+0.0, +0.2, -0.0), 0.4,
-                                   6)
+        v[i] = tl.ballBoundReflect(x[i], v[i], tl.vec(+0.0, +0.2, -0.0), 0.4, 6)
     for i in ti.grouped(x):
         v[i] *= ti.exp(-damping * dt)
         x[i] += dt * v[i]
@@ -59,7 +59,8 @@ def substep():
 ### Rendering GUI
 
 scene = t3.Scene()
-model = t3.Model(f_n=(N - 1)**2 * 4, vi_n=N**2, vt_n=N**2, f_m=1)
+model = t3.Model(faces_n=(N - 1)**2 * 4, pos_n=N**2, tex_n=N**2, nrm_n=N**2 * 2)
+#model.load_texture(ti.imread('/path/to/your_texture.jpg'))
 scene.add_model(model)
 camera = t3.Camera(fov=24, pos=[0, 1.1, -1.5], target=[0, 0.25, 0])
 scene.add_camera(camera)
@@ -79,20 +80,39 @@ def init_display():
         i.x -= 1
         d = i.dot(tl.vec(N, 1))
         i.y -= 1
-        model.faces[a * 4 + 0] = [a, c, b]
-        model.faces[a * 4 + 1] = [a, d, c]
-        model.faces[a * 4 + 2] = [a, b, c]
-        model.faces[a * 4 + 3] = [a, c, d]
+        for _ in ti.static(range(3)):
+            for __ in ti.static(range(3)):
+                model.faces[a * 4 + 0][_, __] = [a, c, b][_]
+                model.faces[a * 4 + 1][_, __] = [a, d, c][_]
+        for _ in ti.static(range(3)):
+            for __ in ti.static(range(2)):
+                model.faces[a * 4 + 2][_, __] = [a, b, c][_]
+                model.faces[a * 4 + 3][_, __] = [a, c, d][_]
+        a += N**2
+        b += N**2
+        c += N**2
+        d += N**2
+        for _ in ti.static(range(3)):
+            model.faces[a * 4 + 2][_, 2] = [a, b, c][_]
+            model.faces[a * 4 + 3][_, 2] = [a, c, d][_]
     for i in ti.grouped(x):
         j = i.dot(tl.vec(N, 1))
-        model.vt[j] = tl.D.yx + i.xY / N
+        model.tex[j] = tl.D._x + i.xY / N
 
 
 @ti.kernel
 def update_display():
     for i in ti.grouped(x):
         j = i.dot(tl.vec(N, 1))
-        model.vi[j] = x[i]
+        model.pos[j] = x[i]
+
+        xa = x[tl.clamp(i + tl.D.x_, 0, tl.vec(*NN) - 1)]
+        xb = x[tl.clamp(i + tl.D.X_, 0, tl.vec(*NN) - 1)]
+        ya = x[tl.clamp(i + tl.D._x, 0, tl.vec(*NN) - 1)]
+        yb = x[tl.clamp(i + tl.D._X, 0, tl.vec(*NN) - 1)]
+        normal = (ya - yb).cross(xa - xb).normalized()
+        model.nrm[j] = normal
+        model.nrm[N**2 + j] = -normal
 
 
 init()
@@ -100,10 +120,9 @@ init_display()
 
 with ti.GUI('Mass Spring') as gui:
     while gui.running and not gui.get_event(gui.ESCAPE):
-        if not gui.is_pressed(gui.SPACE):
-            for i in range(steps):
-                substep()
-            update_display()
+        for i in range(steps):
+            substep()
+        update_display()
 
         camera.from_mouse(gui)
 

6552711dd714d4f96ad7c132e0798e1219dd494a
Yuanming Hu <yuanmhu@gmail.com>
2020-10-10 19:29:44 -0400

    finalize

--- misc/fetch_active_contributors.py
+++ misc/fetch_active_contributors.py
@@ -1,19 +1,24 @@
 import requests
 
 
-def fetch_active_contributors():
+def print_active_contributors():
+    api_prefix = 'https://api.github.com/repos/taichi-dev/taichi'
     per_page = 100
-    contributors_json = requests.get(
-        f'https://api.github.com/repos/taichi-dev/taichi/contributors?per_page={per_page}'
-    ).json()
-    contributors = []
-    print(contributors_json)
-    for c in contributors_json:
-        contributors.append(c['login'])
 
-    assert len(
-        contributors
-    ) < per_page, 'Please extend the program to handle >= 100 contributors'
+    contributors = []
+
+    page = 1
+    while True:
+        contributors_json = requests.get(
+            f'{api_prefix}/contributors?per_page={per_page}&page={page}').json(
+            )
+
+        for c in contributors_json:
+            contributors.append(c['login'])
+
+        if len(contributors_json) == 0:
+            break
+        page += 1
 
     print(len(contributors), contributors)
 
@@ -26,8 +31,7 @@ def fetch_active_contributors():
     while not eof:
         # Note: for some reason the 'page' argument is 1-based
         commits_json = requests.get(
-            f'https://api.github.com/repos/taichi-dev/taichi/commits?per_page={per_page}&page={page}'
-        ).json()
+            f'{api_prefix}/commits?per_page={per_page}&page={page}').json()
         for c in commits_json:
             date = c['commit']['committer']['date']
             try:
@@ -49,8 +53,9 @@ def fetch_active_contributors():
                                  key=lambda rec: -rec[1]):
         print(f'- [{login}](https://github.com/{login}/)    {contrib}')
 
+    print('Irregular records:')
     print(irregular_records)
 
 
 if __name__ == '__main__':
-    fetch_active_contributors()
+    print_active_contributors()

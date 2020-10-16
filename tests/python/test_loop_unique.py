40da31c087ec674334d37f7677e5615fada8603c
xumingkuan <xumingkuan0721@126.com>
2020-10-16 18:08:34 +0800

    Add nested loop test

--- tests/python/test_loop_unique.py
+++ tests/python/test_loop_unique.py
@@ -24,3 +24,29 @@ def test_loop_unique_simple_1d():
     expected_result = {2: 1, 3: 3, 4: 3, 5: 1}
     for i in range(N):
         assert y[i] == expected_result.get(i, 0)
+
+
+@ti.test(require=ti.extension.sparse)
+def test_loop_unique_nested_1d():
+    x, y = ti.field(ti.i32), ti.field(ti.i32)
+
+    N = 16
+    ti.root.pointer(ti.i, N).place(x)
+    ti.root.pointer(ti.i, N).place(y)
+
+    @ti.kernel
+    def inc_y():
+        for i in x:
+            for j in range(i):
+                a = ti.loop_unique(x[i])
+                y[a] += 1
+
+    x[1] = 2
+    x[2] = 3
+    x[7] = 5
+    y[3] = 2
+    y[4] = 3
+    inc_y()
+    expected_result = {2: 1, 3: 4, 4: 3, 5: 7}
+    for i in range(N):
+        assert y[i] == expected_result.get(i, 0)

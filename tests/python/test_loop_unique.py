084645b2d614c03f36f27df4dda3f11037fc42bd
xumingkuan <xumingkuan0721@126.com>
2020-10-18 14:32:07 +0800

    Add tests

--- tests/python/test_loop_unique.py
+++ tests/python/test_loop_unique.py
@@ -27,6 +27,31 @@ def test_loop_unique_simple_1d():
 
 
 @ti.test(require=ti.extension.sparse)
+def test_loop_unique_binary_op_1d():
+    x, y = ti.field(ti.i32), ti.field(ti.i32)
+
+    N = 16
+    ti.root.pointer(ti.i, N).place(x)
+    ti.root.pointer(ti.i, N).place(y)
+
+    @ti.kernel
+    def inc_y():
+        for i in x:
+            a = ti.loop_unique(x[i])
+            y[a + 1] += 1
+
+    x[1] = 2
+    x[2] = 3
+    x[7] = 5
+    y[3] = 2
+    y[4] = 3
+    inc_y()
+    expected_result = {3: 3, 4: 4, 6: 1}
+    for i in range(N):
+        assert y[i] == expected_result.get(i, 0)
+
+
+@ti.test(require=ti.extension.sparse)
 def test_loop_unique_nested_1d():
     x, y = ti.field(ti.i32), ti.field(ti.i32)
 
@@ -96,3 +121,38 @@ def test_loop_unique_2d():
         for j in range(N):
             assert y[i, j] == expected_result_y.get((i, j), 0)
             assert z[i, j] == expected_result_z.get((i, j), 0)
+
+
+@ti.test()
+def test_loop_unique_ndrange():
+    x, y, z = ti.field(ti.i32), ti.field(ti.i32), ti.field(ti.i32)
+
+    N = 8
+    M = 32
+    ti.root.dense(ti.ij, N).place(x)
+    ti.root.dense(ti.i, M).place(y)
+    ti.root.dense(ti.ij, N).place(z)
+
+    a = 3
+    b = 5
+
+    @ti.kernel
+    def prepare_x():
+        for i, j in ti.ndrange(a, b):
+            x[i, j] = i * (b + 1) + j + 1
+
+    @ti.kernel
+    def inc_y_z():
+        for i, j in ti.ndrange(a, b):
+            u = ti.loop_unique(x[i, j])
+            y[u] += i
+            z[i, j + 1] += 10  # TODO: demote this
+
+    prepare_x()
+    inc_y_z()
+    for i in range(a * (b + 1)):
+        assert y[i] == (0 if i % (b + 1) == 0 else i // (b + 1))
+
+    for i in range(a):
+        for j in range(b + 1):
+            assert z[i, j] == (0 if j == 0 else 10)

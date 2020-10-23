4cba15ac4a27093e838278fb3277d0866616a1fd
Ye Kuang <k-ye@users.noreply.github.com>
2020-10-23 19:24:56 +0900

    [GUI] [mac] Support fast_gui on macOS (#1981)
    * [GUI] [mac] Support fast_gui on macOS
    
    * address comments

--- python/taichi/lang/meta.py
+++ python/taichi/lang/meta.py
@@ -19,10 +19,20 @@ def tensor_to_ext_arr(tensor: ti.template(), arr: ti.ext_arr()):
 def vector_to_fast_image(img: ti.template(), out: ti.ext_arr()):
     # FIXME: Why is ``for i, j in img:`` slower than:
     for i, j in ti.ndrange(*img.shape):
-        u, v, w = min(255, max(0, int(img[i, img.shape[1] - 1 - j] * 255)))
+        r, g, b = min(255, max(0, int(img[i, img.shape[1] - 1 - j] * 255)))
+        idx = j * img.shape[0] + i
         # We use i32 for |out| since OpenGL and Metal doesn't support u8 types
-        # TODO: treat Cocoa and Big-endian machines, with XOR logic
-        out[j * img.shape[0] + i] = w + (v << 8) + (u << 16)
+        if ti.static(ti.get_os_name() != 'osx'):
+            out[idx] = (r << 16) + (g << 8) + b
+        else:
+            # What's -16777216?
+            #
+            # On Mac, we need to set the alpha channel to 0xff. Since Mac's GUI
+            # is big-endian, the color is stored in ABGR order, and we need to
+            # add 0xff000000, which is -16777216 in I32's legit range. (Albeit
+            # the clarity, adding 0xff000000 doesn't work.)
+            alpha = -16777216
+            out[idx] = (b << 16) + (g << 8) + r + alpha
 
 
 @ti.kernel

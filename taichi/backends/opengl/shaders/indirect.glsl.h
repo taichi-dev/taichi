8e754c2d1c6d7eca50670eb0ca1b84767d1ed8f2
archibate <1931127624@qq.com>
2020-10-30 16:41:29 +0800

    according to gtmp

--- taichi/backends/opengl/shaders/indirect.glsl.h
+++ taichi/backends/opengl/shaders/indirect.glsl.h
@@ -6,14 +6,34 @@
 #include "taichi/backends/opengl/shaders/runtime.h"
 #undef __GLSL__
 STR(
+// taichi uses gtmp for storing dynamic range endpoints
+layout(std430, binding = 1) buffer gtmp_i32 { int _gtmp_i32_[]; };
+
+// indirect work group size evaluator kernel template
 void _compute_indirect(
   int const_begin, int const_end,
-  int gtmp_begin, int range_end,
+  int range_begin, int range_end,
   int SPT, int TPG) {
-  // indirect work group size evaluator kernel
-  _indirect_x_ = 128;
+
+  // dynamic range for
+  if (const_begin == 0) {
+    range_begin = _gtmp_i32_[range_begin >> 2];
+  }
+  if (const_end == 0) {
+    range_end = _gtmp_i32_[range_end >> 2];
+  }
+  int nstrides = 1;
+  if (range_end > range_begin) {
+    nstrides = range_end - range_begin;
+  }
+
+  int nthreads = max((nstrides + SPT - 1) / SPT, 1);
+  int nblocks = max((nthreads + TPG - 1) / TPG, 1);
+
+  _indirect_x_ = nblocks;
   _indirect_y_ = 1;
   _indirect_z_ = 1;
 }
+
 // get_indirect_evaluator() will prepend a main here, with template arguments
 )

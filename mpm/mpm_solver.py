19d78eb1d75a351a0459d88ec7414d51a6bc4348
Yuanming Hu <yuanmhu@gmail.com>
2020-10-31 00:12:49 -0400

    simplify

--- mpm/mpm_solver.py
+++ mpm/mpm_solver.py
@@ -1,14 +1,12 @@
 import taichi as ti
-ti.init(arch=ti.cpu, debug=True)
+ti.init(arch=ti.cpu)
 
 x = ti.field(dtype=ti.f32)
 y = ti.field(dtype=ti.f32)
 
-grid_size = 4
-
-grid1 = ti.root.pointer(ti.i, grid_size)
-grid2 = ti.root.dense(ti.i, grid_size)
-grid1.pointer(ti.i, grid_size).place(x)
+grid1 = ti.root.pointer(ti.i, 1)
+grid2 = ti.root.dense(ti.i, 1)
+grid1.pointer(ti.i, 1).place(x)
 grid2.place(y)
 
 ti.get_runtime().prog.print_snode_tree()

80a29983a13215be95a58f64477efff1bc52413a
Yuanming Hu <yuanmhu@gmail.com>
2020-10-30 23:49:29 -0400

    simplify

--- mpm/mpm_solver.py
+++ mpm/mpm_solver.py
@@ -1,5 +1,4 @@
 import taichi as ti
-import math
 
 @ti.data_oriented
 class MPMSolver:
@@ -8,25 +7,11 @@ class MPMSolver:
     def __init__(
             self,
             res,
-            size=1,
-            max_num_particles=2 ** 27,
-            # Max 128 MB particles
-            unbounded=False,
-            dt_scale=1,
-            E_scale=1,
-            voxelizer_super_sample=2):
-        self.dim = len(res)
-        assert self.dim in (
-            2, 3), "MPM solver supports only 2D and 3D simulations."
+            max_num_particles=2 ** 27):
+        self.dim = 2
 
         self.res = res
         self.n_particles = ti.var(ti.i32, shape=())
-        self.dx = size / res[0]
-        self.inv_dx = 1.0 / self.dx
-        self.default_dt = 2e-2 * self.dx / size * dt_scale
-        self.p_vol = self.dx ** self.dim
-        self.p_rho = 1000
-        self.p_mass = self.p_vol * self.p_rho
         self.max_num_particles = max_num_particles
         self.gravity = ti.Vector(self.dim, dt=ti.f32, shape=())
         self.source_bound = ti.Vector(self.dim, dt=ti.f32, shape=2)
@@ -35,21 +20,8 @@ class MPMSolver:
         # position
         self.x = ti.Vector(self.dim, dt=ti.f32)
         # velocity
-        self.v = ti.Vector(self.dim, dt=ti.f32)
-        # affine velocity field
-        self.C = ti.Matrix(self.dim, self.dim, dt=ti.f32)
-        # deformation gradient
-        self.F = ti.Matrix(self.dim, self.dim, dt=ti.f32)
-        # material id
-        self.material = ti.var(dt=ti.i32)
-        self.color = ti.var(dt=ti.i32)
-        # plastic deformation volume ratio
-        self.Jp = ti.var(dt=ti.f32)
 
-        if self.dim == 2:
-            indices = ti.ij
-        else:
-            indices = ti.ijk
+        indices = ti.ij
 
         offset = tuple(-self.grid_size // 2 for _ in range(self.dim))
         self.offset = offset
@@ -109,20 +81,12 @@ class MPMSolver:
 
 
         self.particle = ti.root.dynamic(ti.i, max_num_particles, 2 ** 20)
-        self.particle.place(self.x, self.v, self.C, self.F, self.material,
-                            self.color, self.Jp)
-
-
-    def step(self, frame_dt, print_stat=False):
-        print(111)
-        self.grid1.deactivate_all()
-        ti.sync()
-        print('successfully finishes')
-        exit()
+        self.particle.place(self.x)
 
 
 ti.init(arch=ti.cpu, async_mode=False, debug=True)
 
 mpm = MPMSolver(res=(128, 128))
-mpm.step(8e-3)
-
+mpm.grid1.deactivate_all()
+ti.sync()
+print('successfully finishes')

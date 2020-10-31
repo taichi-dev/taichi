6a7d8a170d6e6041445ee94b57aa08310e90332e
Yuanming Hu <yuanmhu@gmail.com>
2020-10-30 23:45:10 -0400

    still reproduce

--- mpm/demo_2d.py
+++ mpm/demo_2d.py
@@ -1,22 +1,9 @@
-import math
-import time
-
-import numpy as np
 import taichi as ti
 
 from mpm_solver import MPMSolver
 
-write_to_disk = False
-
 ti.init(arch=ti.gpu, async_mode=False, debug=True)
-        # async_opt_intermediate_file="mpm")  # Try to run on GPU
-
-gui = ti.GUI("Taichi Elements", res=512, background_color=0x112F41,
-             show_gui=False)
 
 mpm = MPMSolver(res=(128, 128))
-
-ti.sync()
-t = time.time()
 mpm.step(8e-3)
 

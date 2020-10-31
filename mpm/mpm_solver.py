b2d040ab77621cc71591b1732b0141cfac6a1b3a
Yuanming Hu <yuanmhu@gmail.com>
2020-10-31 00:07:21 -0400

    simplify

--- mpm/mpm_solver.py
+++ mpm/mpm_solver.py
@@ -2,22 +2,23 @@ import taichi as ti
 
 ti.init(arch=ti.cpu, debug=True)
 
-indices = ti.ij
 m = ti.field(dtype=ti.f32)
 
 grid_size = 4
-grid1 = ti.root.pointer(indices, grid_size)
+grid1 = ti.root.pointer(ti.i, grid_size)
 
 use2 = True
-if use2:
-    grid2 = ti.root.dense(indices, grid_size)
 
-grid1.pointer(indices, grid_size).place(m)
+if use2:
+    grid2 = ti.root.dense(ti.i, grid_size)
+
+grid1.pointer(ti.i, grid_size).place(m)
 
 if use2:
     m2 = ti.field(dtype=ti.f32)
     grid2.place(m2)
 
+ti.get_runtime().prog.print_snode_tree()
 
 grid1.deactivate_all()
 ti.sync()

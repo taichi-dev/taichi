98a2a2334b09a15eedf73632d17883ba1d8dc7b9
Yuanming Hu <yuanmhu@gmail.com>
2020-10-30 23:59:44 -0400

    simplify

--- mpm/mpm_solver.py
+++ mpm/mpm_solver.py
@@ -5,7 +5,7 @@ grid_size = 1024
 
 indices = ti.ij
 
-grid_m = ti.var(dt=ti.f32)
+m = ti.var(dt=ti.f32)
 
 grid_block_size = 128
 grid1 = ti.root.pointer(indices, grid_size // grid_block_size)
@@ -17,19 +17,12 @@ if use2:
 leaf_block_size = 16
 
 block = grid1.pointer(indices,
-                          grid_block_size // leaf_block_size)
-
-def block_component(blk, c):
-    blk.dense(indices, leaf_block_size).place(c)
-
-block_component(block, grid_m)
+                          grid_block_size // leaf_block_size).place(m)
 
 if use2:
+    m2 = ti.var(dt=ti.f32)
     block2 = grid2.dense(indices,
-                              grid_block_size // leaf_block_size)
-    grid_m2 = ti.var(dt=ti.f32)
-
-    block_component(block2, grid_m2)
+                              grid_block_size // leaf_block_size).place(m2)
 
 
 grid1.deactivate_all()

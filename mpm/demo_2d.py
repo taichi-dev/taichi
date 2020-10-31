7424721951eb24ac71a5976b531cd6ebdf15813c
Yuanming Hu <yuanmhu@gmail.com>
2020-10-30 23:47:51 -0400

    reproduce with single file

--- mpm/demo_2d.py
+++
@@ -1,9 +0,0 @@
-import taichi as ti
-
-from mpm_solver import MPMSolver
-
-ti.init(arch=ti.gpu, async_mode=False, debug=True)
-
-mpm = MPMSolver(res=(128, 128))
-mpm.step(8e-3)
-
--- mpm/mpm_solver.py
+++ mpm/mpm_solver.py
@@ -119,3 +119,10 @@ class MPMSolver:
         ti.sync()
         print('successfully finishes')
         exit()
+
+
+ti.init(arch=ti.gpu, async_mode=False, debug=True)
+
+mpm = MPMSolver(res=(128, 128))
+mpm.step(8e-3)
+

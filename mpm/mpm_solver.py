05475ecc3950c40737440cf1d4acdec72e2435e6
Yuanming Hu <yuanmhu@gmail.com>
2020-10-30 23:57:17 -0400

    remove class

--- mpm/mpm_solver.py
+++ mpm/mpm_solver.py
@@ -1,44 +1,37 @@
 import taichi as ti
 
-@ti.data_oriented
-class MPMSolver:
-    grid_size = 1024
-
-    def __init__(self):
-        self.dim = 2
-        
-        indices = ti.ij
-
-        self.grid_m = ti.var(dt=ti.f32)
-
-        grid_block_size = 128
-        self.grid1 = ti.root.pointer(indices, self.grid_size // grid_block_size)
-        
-        use2 = True
-        if use2:
-            self.grid2 = ti.root.dense(indices, self.grid_size // grid_block_size)
-
-        self.leaf_block_size = 16
-
-        block = self.grid1.pointer(indices,
-                                  grid_block_size // self.leaf_block_size)
-
-        def block_component(blk, c):
-            blk.dense(indices, self.leaf_block_size).place(c)
-
-        block_component(block, self.grid_m)
-
-        if use2:
-            block2 = self.grid2.dense(indices,
-                                      grid_block_size // self.leaf_block_size)
-            self.grid_m2 = ti.var(dt=ti.f32)
-
-            block_component(block2, self.grid_m2)
-
-
 ti.init(arch=ti.cpu, async_mode=False, debug=True)
+grid_size = 1024
 
-mpm = MPMSolver()
-mpm.grid1.deactivate_all()
+indices = ti.ij
+
+grid_m = ti.var(dt=ti.f32)
+
+grid_block_size = 128
+grid1 = ti.root.pointer(indices, grid_size // grid_block_size)
+
+use2 = True
+if use2:
+    grid2 = ti.root.dense(indices, grid_size // grid_block_size)
+
+leaf_block_size = 16
+
+block = grid1.pointer(indices,
+                          grid_block_size // leaf_block_size)
+
+def block_component(blk, c):
+    blk.dense(indices, leaf_block_size).place(c)
+
+block_component(block, grid_m)
+
+if use2:
+    block2 = grid2.dense(indices,
+                              grid_block_size // leaf_block_size)
+    grid_m2 = ti.var(dt=ti.f32)
+
+    block_component(block2, grid_m2)
+
+
+grid1.deactivate_all()
 ti.sync()
 print('successfully finishes')

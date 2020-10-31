b534404cb386ce87d16770b8175a8fd5c01f6bd0
Yuanming Hu <yuanmhu@gmail.com>
2020-10-30 23:48:03 -0400

    on cpu

--- mpm/mpm_solver.py
+++ mpm/mpm_solver.py
@@ -121,7 +121,7 @@ class MPMSolver:
         exit()
 
 
-ti.init(arch=ti.gpu, async_mode=False, debug=True)
+ti.init(arch=ti.cpu, async_mode=False, debug=True)
 
 mpm = MPMSolver(res=(128, 128))
 mpm.step(8e-3)

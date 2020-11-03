2f809aa6b311a3019308c8612ff3c4fdefe07854
彭于斌 <1931127624@qq.com>
2020-11-03 11:48:17 +0800

    [opengl] [refactor] Reduce SSBO numbers: merge earg with args (#2020)

--- taichi/backends/opengl/opengl_kernel_util.h
+++ taichi/backends/opengl/opengl_kernel_util.h
@@ -12,6 +12,8 @@ class SNode;
 
 namespace opengl {
 
+constexpr int taichi_opengl_earg_base = taichi_max_num_args * sizeof(uint64_t);
+
 struct UsedFeature {
   // types:
   bool simulated_atomic_float{false};
@@ -60,7 +62,6 @@ enum class GLBufId {
   Listman = 7,
   Gtmp = 1,
   Args = 2,
-  Earg = 3,
   Extr = 4,
 };
 

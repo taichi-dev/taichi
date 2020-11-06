ae32d9677eed44e62df37fe09d46bde3567633ba
archibate <1931127624@qq.com>
2020-11-06 13:48:38 +0800

    rev

--- tests/python/test_bit_struct.py
+++ tests/python/test_bit_struct.py
@@ -2,8 +2,8 @@ import taichi as ti
 import numpy as np
 
 
-@ti.test(arch=ti.cpu, debug=True, print_ir=True, cfg_optimization=False)
 def test_simple_array():
+    ti.init(arch=ti.cpu, debug=True, print_ir=True, cfg_optimization=False)
     ci13 = ti.type_factory_.get_custom_int_type(13, True)
     cu19 = ti.type_factory_.get_custom_int_type(19, False)
 
@@ -32,8 +32,8 @@ def test_simple_array():
     verify_val()
 
 
-@ti.test(arch=ti.cpu, debug=True, print_ir=True, cfg_optimization=False)
 def test_custom_int_load_and_store():
+    ti.init(arch=ti.cpu, debug=True, print_ir=True, cfg_optimization=False)
     ci13 = ti.type_factory_.get_custom_int_type(13, True)
     cu14 = ti.type_factory_.get_custom_int_type(14, False)
     ci5 = ti.type_factory_.get_custom_int_type(5, True)

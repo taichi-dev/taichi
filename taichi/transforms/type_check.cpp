3a084009cbb1478c2699a7e9ae06d775bb4dc106
Taichi Gardener <taichigardener@gmail.com>
2020-11-12 14:15:39 -0500

    [skip ci] enforce code format

--- taichi/transforms/type_check.cpp
+++ taichi/transforms/type_check.cpp
@@ -399,13 +399,14 @@ class TypeCheck : public IRVisitor {
   void visit(SNodeLookupStmt *stmt) {
     if (stmt->snode->type == SNodeType::bit_array) {
       auto bit_array_type = stmt->snode->dt.get_ptr();
-      auto element_type = bit_array_type->cast<BitArrayType>()->get_element_type();
-      auto pointer_type = TypeFactory::get_instance().get_pointer_type(
-        element_type, true);
+      auto element_type =
+          bit_array_type->cast<BitArrayType>()->get_element_type();
+      auto pointer_type =
+          TypeFactory::get_instance().get_pointer_type(element_type, true);
       stmt->ret_type = pointer_type;
     } else {
-      stmt->ret_type =
-        TypeFactory::create_vector_or_scalar_type(1, PrimitiveType::gen, true);
+      stmt->ret_type = TypeFactory::create_vector_or_scalar_type(
+          1, PrimitiveType::gen, true);
     }
   }
 

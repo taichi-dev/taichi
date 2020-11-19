a20a0e86dfd9366e78087f8c580a02ad87ae75ff
liujiafeng <jiafengliu@zju.edu.cn>
2020-11-19 15:06:28 +0800

    modify test cases

--- tests/python/test_bit_struct.py
+++ tests/python/test_bit_struct.py
@@ -77,113 +77,40 @@ def test_custom_int_load_and_store():
         verify_val.__wrapped__(idx)
 
 
-@ti.test(arch=ti.cpu, debug=True, cfg_optimization=False)
-def test_bit_struct_with_physical_type():
-    ci8_5 = ti.type_factory_._get_custom_int_type(8, 5, True)
-    ci8_3 = ti.type_factory_._get_custom_int_type(8, 3, False)
-    ci16_4 = ti.type_factory_._get_custom_int_type(16, 4, True)
-    cu16_12 = ti.type_factory_._get_custom_int_type(16, 12, False)
+def test_bit_struct():
+    def test_single_bit_struct(physical_type, compute_type, custom_bits, test_case):
+        ti.init(arch=ti.cpu, debug=True, print_ir=False, cfg_optimization=False)
 
-    ci32_17 = ti.type_factory_._get_custom_int_type(32, 17, True)
-    ci32_11 = ti.type_factory_._get_custom_int_type(32, 11, True)
-    cu32_4 = ti.type_factory_._get_custom_int_type(32, 4, False)
+        cit1 = ti.type_factory_._get_custom_int_type(compute_type, custom_bits[0], True)
+        cit2 = ti.type_factory_._get_custom_int_type(compute_type, custom_bits[1], False)
+        cit3 = ti.type_factory_._get_custom_int_type(compute_type, custom_bits[2], True)
 
-    ci64_33 = ti.type_factory_._get_custom_int_type(64, 32, True)
-    ci64_20 = ti.type_factory_._get_custom_int_type(64, 21, False)
-    ci64_7 = ti.type_factory_._get_custom_int_type(64, 7, False)
+        a = ti.field(dtype=cit1)
+        b = ti.field(dtype=cit2)
+        c = ti.field(dtype=cit3)
+        ti.root._bit_struct(num_bits=physical_type).place(a, b, c)
 
-    a = ti.field(dtype=ci8_5)
-    b = ti.field(dtype=ci8_3)
+        @ti.kernel
+        def set_val(test_val: ti.ext_arr()):
+            a[None] = test_val[0]
+            b[None] = test_val[1]
+            c[None] = test_val[2]
 
-    c = ti.field(dtype=ci16_4)
-    d = ti.field(dtype=cu16_12)
+        @ti.kernel
+        def verify_val(test_val: ti.ext_arr()):
+            assert a[None] == test_val[0]
+            assert b[None] == test_val[1]
+            assert c[None] == test_val[2]
 
-    e = ti.field(dtype=ci32_17)
-    f = ti.field(dtype=ci32_11)
-    g = ti.field(dtype=cu32_4)
+        set_val(test_case)
+        verify_val(test_case)
 
-    h = ti.field(dtype=ci64_33)
-    i = ti.field(dtype=ci64_20)
-    j = ti.field(dtype=ci64_7)
+    test_single_bit_struct(8, 8, [3, 3, 2], np.array([2**2-1, 2**3-1, -2**1]))
+    test_single_bit_struct(16, 16, [4, 7, 5], np.array([2**3-1, 2**7-1, -2**4]))
+    test_single_bit_struct(32, 32, [17, 11, 4], np.array([2**16-1, 2**10-1, -2**3]))
+    test_single_bit_struct(64, 64, [32, 23, 9], np.array([2**31-1, 2**23-1, -2**8]))
+    test_single_bit_struct(32, 16, [7, 12, 13], np.array([2**6-1, 2**12-1, -2**12]))
+    test_single_bit_struct(64, 32, [18, 22, 24], np.array([2**17-1, 2**22-1, -2**23]))
 
-    k = ti.field(dtype=ci16_4)
-    l = ti.field(dtype=cu16_12)
-
-    m = ti.field(dtype=ci32_17)
-    n = ti.field(dtype=ci32_11)
-    o = ti.field(dtype=cu32_4)
-
-    ti.root._bit_struct(num_bits=8).place(a, b)
-    ti.root._bit_struct(num_bits=16).place(c, d)
-    ti.root._bit_struct(num_bits=32).place(e, f, g)
-    ti.root._bit_struct(num_bits=64).place(h, i, j)
-
-    ti.root._bit_struct(num_bits=32).place(k, l)
-    ti.root._bit_struct(num_bits=64).place(m, n, o)
-
-    test_case_np = np.array(
-        [[
-            2**4 - 1, 2**3 - 1, 2**3 - 1, 2**12 - 1, 2**16 - 1, 2**10 - 1,
-            2**4 - 1, 2**31 - 1, 2**21 - 1, 2**7 - 1, 2**3 - 1, 2**12 - 1,
-            2**16 - 1, 2**10 - 1, 2**4 - 1
-        ],
-         [
-             -2**3,
-             2**2 - 1,
-             -2**2,
-             2**11 - 1,
-             -2**15,
-             -2**9,
-             2**2 - 1,
-             -2**30,
-             2**20 - 1,
-             2**6 - 1,
-             -2**2,
-             2**11 - 1,
-             -2**15,
-             -2**9,
-             2**2 - 1,
-         ], [3, 4, 5, 16, 21, 34, 1, 2020, 456, 123, 5, 16, 21, 34, 1]],
-        dtype=np.int32)
-    test_case = ti.Vector.field(15, dtype=ti.i32, shape=len(test_case_np))
-    test_case.from_numpy(test_case_np)
-
-    @ti.kernel
-    def set_val(idx: ti.i32):
-        a[None] = test_case[idx][0]
-        b[None] = test_case[idx][1]
-        c[None] = test_case[idx][2]
-        d[None] = test_case[idx][3]
-        e[None] = test_case[idx][4]
-        f[None] = test_case[idx][5]
-        g[None] = test_case[idx][6]
-        h[None] = test_case[idx][7]
-        i[None] = test_case[idx][8]
-        j[None] = test_case[idx][9]
-        k[None] = test_case[idx][10]
-        l[None] = test_case[idx][11]
-        m[None] = test_case[idx][12]
-        n[None] = test_case[idx][13]
-        o[None] = test_case[idx][14]
-
-    @ti.kernel
-    def verify_val(idx: ti.i32):
-        assert a[None] == test_case[idx][0]
-        assert b[None] == test_case[idx][1]
-        assert c[None] == test_case[idx][2]
-        assert d[None] == test_case[idx][3]
-        assert e[None] == test_case[idx][4]
-        assert f[None] == test_case[idx][5]
-        assert g[None] == test_case[idx][6]
-        assert h[None] == test_case[idx][7]
-        assert i[None] == test_case[idx][8]
-        assert j[None] == test_case[idx][9]
-        assert k[None] == test_case[idx][10]
-        assert l[None] == test_case[idx][11]
-        assert m[None] == test_case[idx][12]
-        assert n[None] == test_case[idx][13]
-        assert o[None] == test_case[idx][14]
-
-    for idx in range(len(test_case_np)):
-        set_val(idx)
-        verify_val(idx)
+    test_single_bit_struct(16, 16, [5, 5, 6], np.array([15, 5, 20]))
+    test_single_bit_struct(32, 32, [10, 10, 12], np.array([11, 19, 2020]))

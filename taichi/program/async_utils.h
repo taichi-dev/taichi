ec0ae2a339457c850141cc97001da0784ffbb010
Taichi Gardener <taichigardener@gmail.com>
2020-11-23 08:01:05 -0500

    [skip ci] enforce code format

--- taichi/program/async_utils.h
+++ taichi/program/async_utils.h
@@ -162,8 +162,8 @@ TLANG_NAMESPACE_END
 namespace std {
 template <>
 struct hash<taichi::lang::IRHandle> {
-  std::size_t operator()(
-      const taichi::lang::IRHandle &ir_handle) const noexcept {
+  std::size_t operator()(const taichi::lang::IRHandle &ir_handle) const
+      noexcept {
     return ir_handle.hash();
   }
 };

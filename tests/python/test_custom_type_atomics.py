650ea91f96ab00c487d2aebc59bff6829b9b7e24
Yuanming Hu <yuanmhu@gmail.com>
2020-11-30 18:38:20 -0500

    test non i32 types

--- tests/python/test_custom_type_atomics.py
+++ tests/python/test_custom_type_atomics.py
@@ -26,3 +26,35 @@ def test_custom_int_atomics():
 
     assert x[None] == 43
     assert y[None] == 3
+
+
+# @ti.test(require=ti.extension.quant, debug=True, cfg_optimization=False)
+def test_custom_int_atomics_non_i32():
+    ti.init(arch=ti.cpu, debug=True, cfg_optimization=False)
+    ci13 = ti.type_factory_.get_custom_int_type(13, True)
+    cu2 = ti.type_factory_.get_custom_int_type(2, False)
+
+    x = ti.field(dtype=ci13)
+    y = ti.field(dtype=cu2)
+
+    ti.root._bit_struct(num_bits=16).place(x)
+    ti.root._bit_struct(num_bits=64).place(y)
+
+    x[None] = 3
+    y[None] = 0
+
+    @ti.kernel
+    def foo():
+        for i in range(10):
+            x[None] += 4
+
+        for j in range(3):
+            y[None] += 1
+
+    foo()
+
+    assert x[None] == 43
+    assert y[None] == 3
+
+
+test_custom_int_atomics_non_i32()

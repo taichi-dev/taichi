f66efb8e49026755eec439ccb89312f72c14b0d6
Xuanda Yang <th3charlie@gmail.com>
2020-12-22 01:12:31 +0800

    make sure coding style in test comforms demo

--- misc/test_bit_array_vectorization.py
+++ misc/test_bit_array_vectorization.py
@@ -1,22 +1,37 @@
 import taichi as ti
 
-ti.init(debug=True, print_ir=True, cfg_optimization=False)
+ti.init(debug=True, cfg_optimization=False)
+# ti.init(debug=True, cfg_optimization=False, print_ir=True)
 
 ci1 = ti.type_factory_.get_custom_int_type(1, False)
 
 x = ti.field(dtype=ci1)
 y = ti.field(dtype=ci1)
 
-N = 64
-ti.root._bit_array(ti.i, N, num_bits=64).place(x)
-ti.root._bit_array(ti.i, N, num_bits=64).place(y)
+N = 1024
+bits = 32
+ti.root.dense(ti.ij,  (N, N // bits))._bit_array(ti.j, bits, num_bits=bits).place(x)
+ti.root.dense(ti.ij,  (N, N // bits))._bit_array(ti.j, bits, num_bits=bits).place(y)
 
 
 @ti.kernel
-def foo():
-    ti.bit_vectorize(64)
-    for i in x:
+def init():
+    for i, j in ti.ndrange(N, N):
+        x[i, j] = (N * i + j) % 2
+
+
+@ti.kernel
+def assign():
+    for i in ti.grouped(x):
         y[i] = x[i]
 
 
-foo()
+@ti.kernel
+def verify():
+    for i, j in ti.ndrange(N, N):
+        assert y[i, j] == (N * i + j) % 2
+
+
+init()
+assign()
+verify()

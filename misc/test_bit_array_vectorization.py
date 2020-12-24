1eff43a26115e1416779293c1b07c61ccec4707b
Xuanda Yang <th3charlie@gmail.com>
2020-12-25 00:32:58 +0800

    fix codegen for struct-for

--- misc/test_bit_array_vectorization.py
+++ misc/test_bit_array_vectorization.py
@@ -1,14 +1,15 @@
 import taichi as ti
 
-ti.init(debug=True, cfg_optimization=False)
-# ti.init(debug=True, cfg_optimization=False, print_ir=True)
+ti.init(debug=True, cfg_optimization=False, kernel_profiler=True)
+
+vectorize = False
 
 ci1 = ti.type_factory_.get_custom_int_type(1, False)
 
 x = ti.field(dtype=ci1)
 y = ti.field(dtype=ci1)
 
-N = 256
+N = 4096
 block_size = 4
 bits = 32
 boundary_offset = 64
@@ -28,18 +29,30 @@ def init():
 
 
 @ti.kernel
-def assign():
+def assign_vectorized():
     ti.bit_vectorize(32)
     for i, j in x:
         y[i, j] = x[i, j]
 
 
-# TODO: crash here
-# @ti.kernel
-# def verify():
-#     for i, j in ti.ndrange((boundary_offset, N - boundary_offset), (boundary_offset, N - boundary_offset)):
-#         # assert y[i, j] == (N * i + j) % 2
+@ti.kernel
+def assign_naive():
+    for i, j in ti.ndrange((boundary_offset, N - boundary_offset),
+                           (boundary_offset, N - boundary_offset)):
+        y[i, j] = x[i, j]
+
+
+@ti.kernel
+def verify():
+    for i, j in ti.ndrange((boundary_offset, N - boundary_offset), (boundary_offset, N - boundary_offset)):
+        assert y[i, j] == (N * i + j) % 2
+
 
 init()
-assign()
-# verify()
+if vectorize:
+    assign_vectorized()
+else:
+    assign_naive()
+verify()
+
+ti.kernel_profiler_print()

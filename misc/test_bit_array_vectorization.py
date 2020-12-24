40c2097094cdec3888c99763e9a094e9b3afb184
Xuanda Yang <th3charlie@gmail.com>
2020-12-25 00:41:40 +0800

    format

--- misc/test_bit_array_vectorization.py
+++ misc/test_bit_array_vectorization.py
@@ -44,7 +44,8 @@ def assign_naive():
 
 @ti.kernel
 def verify():
-    for i, j in ti.ndrange((boundary_offset, N - boundary_offset), (boundary_offset, N - boundary_offset)):
+    for i, j in ti.ndrange((boundary_offset, N - boundary_offset),
+                           (boundary_offset, N - boundary_offset)):
         assert y[i, j] == (N * i + j) % 2
 
 

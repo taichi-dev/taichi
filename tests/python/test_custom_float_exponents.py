3eb2920c4a02d61bf04098b8378f5645b7ff0f62
Yuanming Hu <yuanmhu@gmail.com>
2020-12-26 14:32:42 -0500

    POT working

--- tests/python/test_custom_float_exponents.py
+++ tests/python/test_custom_float_exponents.py
@@ -5,12 +5,14 @@ from pytest import approx
 # @ti.test(require=ti.extension.quant)
 # def test_custom_float():
 
+
 def main():
     ti.init(print_ir=True)
     ci13 = ti.type_factory_.get_custom_int_type(13, True)
     exp = ti.type_factory_.get_custom_int_type(6, True)
     cft = ti.type_factory.custom_float(significand_type=ci13,
-                                       exponent_type=exp, scale=1)
+                                       exponent_type=exp,
+                                       scale=1)
     x = ti.field(dtype=cft)
 
     ti.root._bit_struct(num_bits=32).place(x)
@@ -18,8 +20,10 @@ def main():
     ti.get_runtime().materialize()
     ti.get_runtime().print_snode_tree()
 
+    for v in [128, 256, 512, 1024]:
+        x[None] = v
+        print(x[None])
+        assert x[None] == v
 
-    x[None] = 1024
-    print(x[None])
 
 main()

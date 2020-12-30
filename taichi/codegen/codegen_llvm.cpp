694dc5426501ad5c7fa5a2ea4622b72c239b42fc
Yuanming Hu <yuanming-hu@users.noreply.github.com>
2020-12-30 00:26:57 -0500

    Update taichi/codegen/codegen_llvm.cpp
    Co-authored-by: Jiafeng Liu <jiafengliu@zju.edu.cn>

--- taichi/codegen/codegen_llvm.cpp
+++ taichi/codegen/codegen_llvm.cpp
@@ -1768,9 +1768,9 @@ void CodeGenLLVM::create_offload_struct_for(OffloadedStmt *stmt, bool spmd) {
 
   llvm::Function *body = nullptr;
   auto leaf_block = stmt->snode;
-  // When looping over bit_arrays, we generate struct for on bit array's parent
+  // When looping over bit_arrays and bit_structs, we generate struct for on their parent
   // node (usually "dense") instead of itself for higher performance. Also, note
-  // that the loop must be bit_vectorized.
+  // that the loop must be bit_vectorized for bit_arrays.
   if ((leaf_block->type == SNodeType::bit_array ||
        leaf_block->type == SNodeType::bit_struct) &&
       leaf_block->parent) {

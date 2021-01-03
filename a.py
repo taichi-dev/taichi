09df5ddd71a7100f5651f7056a3f2eac4f8bb3d1
Yuanming Hu <yuanmhu@gmail.com>
2021-01-03 15:23:31 -0500

    reproduce

---
+++ a.py
@@ -0,0 +1,20 @@
+import taichi as ti
+
+ti.init(arch=ti.gpu, print_ir=True)
+
+exp = ti.type_factory.custom_int(5, False)
+cit = ti.type_factory.custom_int(22, True)
+cft = ti.type_factory.custom_float(significand_type=cit, exponent_type=exp, scale=1)
+v = ti.field(dtype=cft)
+ti.root._bit_struct(num_bits=32).place(v)
+
+@ti.kernel
+def set_val():
+    v[None] = 1.0
+
+@ti.kernel
+def p():
+    print(v[None])
+
+set_val()
+p()

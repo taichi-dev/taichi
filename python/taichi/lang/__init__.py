1b730556656fbd29b60268de0c06767be74cb0db
xumingkuan <xumingkuan0721@126.com>
2021-01-23 22:58:54 +0800

    [async] Set the default value of async_flush_every to 50 (#2169)

--- python/taichi/lang/__init__.py
+++ python/taichi/lang/__init__.py
@@ -371,7 +371,8 @@ def benchmark(func, repeat=300, args=()):
         avg = elapsed / repeat
         ti.stat_write('wall_clk_t', avg)
         device_time = ti.kernel_profiler_total_time()
-        ti.stat_write('exec_t', device_time)
+        avg_device_time = device_time / repeat
+        ti.stat_write('exec_t', avg_device_time)
 
     run_benchmark()
 
@@ -379,12 +380,14 @@ def benchmark(func, repeat=300, args=()):
 def benchmark_plot(fn=None,
                    cases=None,
                    columns=None,
+                   column_titles=None,
                    archs=None,
                    title=None,
                    bars='sync_vs_async',
                    bar_width=0.4,
                    bar_distance=0,
-                   left_margin=0):
+                   left_margin=0,
+                   size=(12, 8)):
     import taichi as ti
     import yaml
     import matplotlib.pyplot as plt
@@ -412,13 +415,15 @@ def benchmark_plot(fn=None,
 
     if columns is None:
         columns = list(data[cases[0]].keys())
+    if column_titles is None:
+        column_titles = columns
     normalize_to_lowest = lambda x: True
     figure, subfigures = plt.subplots(len(cases), len(columns))
     if title is None:
         title = 'Taichi Performance Benchmarks (Higher means more)'
     figure.suptitle(title, fontweight="bold")
     for col_id in range(len(columns)):
-        subfigures[0][col_id].set_title(columns[col_id])
+        subfigures[0][col_id].set_title(column_titles[col_id])
     for case_id in range(len(cases)):
         case = cases[case_id]
         subfigures[case_id][0].annotate(
@@ -435,7 +440,9 @@ def benchmark_plot(fn=None,
             if archs is None:
                 current_archs = data[case][col].keys()
             else:
-                current_archs = archs & data[case][col].keys()
+                current_archs = [
+                    x for x in archs if x in data[case][col].keys()
+                ]
             if bars == 'sync_vs_async':
                 y_left = [
                     data[case][col][arch]['sync'] for arch in current_archs
@@ -480,7 +487,7 @@ def benchmark_plot(fn=None,
                               height=y_left,
                               width=bar_width,
                               label=label_left,
-                              color=(0.3, 0.7, 0.9, 1.0))
+                              color=(0.47, 0.69, 0.89, 1.0))
             bar_right = ax.bar(x=[
                 i + bar_width / 2 + bar_distance / 2
                 for i in range(len(current_archs))
@@ -488,7 +495,7 @@ def benchmark_plot(fn=None,
                                height=y_right,
                                width=bar_width,
                                label=label_right,
-                               color=(0.8, 0.2, 0.3, 1.0))
+                               color=(0.68, 0.26, 0.31, 1.0))
             ax.set_xticks(range(len(current_archs)))
             ax.set_xticklabels(current_archs)
             figure.legend((bar_left, bar_right), (label_left, label_right),
@@ -496,7 +503,7 @@ def benchmark_plot(fn=None,
     figure.subplots_adjust(left=left_margin)
 
     fig = plt.gcf()
-    fig.set_size_inches(13, 8)
+    fig.set_size_inches(size)
 
     plt.show()
 

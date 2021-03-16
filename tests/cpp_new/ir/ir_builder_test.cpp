588fc8ff5ca828554acfc1ac8c00df9128642d44
Ye Kuang <k-ye@users.noreply.github.com>
2021-03-16 09:40:07 +0800

    [test] [ir] Add a simple unit test for IRBuilder (#2214)
    * [test] [ir] Add basic IR unit test
    
    * comments

---
+++ tests/cpp_new/ir/ir_builder_test.cpp
@@ -0,0 +1,22 @@
+#include "gtest/gtest.h"
+
+#include "taichi/ir/ir_builder.h"
+#include "taichi/ir/statements.h"
+
+namespace taichi {
+namespace lang {
+
+TEST(IRBuilder, Basic) {
+  IRBuilder builder;
+  auto *lhs = builder.get_int32(40);
+  auto *rhs = builder.get_int32(2);
+  auto *add = builder.create_add(lhs, rhs);
+  ASSERT_TRUE(add->is<BinaryOpStmt>());
+  auto *addc = add->cast<BinaryOpStmt>();
+  EXPECT_EQ(addc->lhs, lhs);
+  EXPECT_EQ(addc->rhs, rhs);
+  EXPECT_EQ(addc->op_type, BinaryOpType::add);
+}
+
+}  // namespace lang
+}  // namespace taichi

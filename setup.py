7c104b3609938bc9e0d8f274ad2ef2b71dd1dc22
hooyuser <hooyuser@outlook.com>
2021-04-19 19:26:21 +0800

    Support python 3.9

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ classifiers = [
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
     'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
 ]
 
 data_files = glob.glob('python/lib/*')

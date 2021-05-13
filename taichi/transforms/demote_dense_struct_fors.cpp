e5f439b916b2c9f41e4d096072811e06a85237e6
xumingkuan <xumingkuan0721@126.com>
2021-05-13 20:34:24 +0800

    [opt] Simplify replace_statements and improve demote_dense_struct_fors (#2335)
    * [opt] Simplify replace_statements and improve demote_dense_struct_fors
    
    * Apply review

--- taichi/transforms/demote_dense_struct_fors.cpp
+++ taichi/transforms/demote_dense_struct_fors.cpp
@@ -86,21 +86,22 @@ void convert_to_range_for(OffloadedStmt *offloaded) {
     }
   }
 
-  for (int i = 0; i < num_loop_vars; i++) {
-    // TODO: Use only one (instead num_loop_vars) invocation(s) of
-    //  replace_statements_with
-    irpass::replace_statements_with(
-        body.get(),
-        [&](Stmt *s) {
-          if (auto loop_index = s->cast<LoopIndexStmt>()) {
-            return loop_index->loop == offloaded &&
-                   loop_index->index ==
-                       snodes.back()->physical_index_position[i];
-          }
+  irpass::replace_statements(
+      body.get(), /*filter=*/
+      [&](Stmt *s) {
+        if (auto loop_index = s->cast<LoopIndexStmt>()) {
+          return loop_index->loop == offloaded;
+        } else {
           return false;
-        },
-        [&](Stmt *) { return new_loop_vars[i]; });
-  }
+        }
+      },
+      /*finder=*/
+      [&](Stmt *s) {
+        auto index = std::find(physical_indices.begin(), physical_indices.end(),
+                               s->as<LoopIndexStmt>()->index);
+        TI_ASSERT(index != physical_indices.end());
+        return new_loop_vars[index - physical_indices.begin()];
+      });
 
   if (has_test) {
     // Create an If statement

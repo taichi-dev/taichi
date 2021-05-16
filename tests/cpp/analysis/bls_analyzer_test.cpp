d4f28306013f5290914e56d8d89dd16152dfa092
KLozes <lozesk@gmail.com>
2021-05-16 09:38:52 -0700

    [test] add bls test for bit_shl

--- tests/cpp/analysis/bls_analyzer_test.cpp
+++ tests/cpp/analysis/bls_analyzer_test.cpp
@@ -8,11 +8,24 @@
 #include "taichi/ir/snode.h"
 #include "taichi/ir/transforms.h"
 #include "taichi/struct/struct.h"
-#include "tests/cpp/struct/fake_struct_compiler.h"
 
 namespace taichi {
 namespace lang {
 namespace {
+
+class TestStructCompiler : public StructCompiler {
+ public:
+  TestStructCompiler() : StructCompiler(/*prog=*/nullptr) {
+  }
+  void generate_types(SNode &) override {
+  }
+
+  void generate_child_accessors(SNode &) override {
+  }
+
+  void run(SNode &, bool) override {
+  }
+};
 
 constexpr int kBlockSize = 8;
 
@@ -25,8 +38,8 @@ class BLSAnalyzerTest : public ::testing::Test {
     child_snode_ = &(parent_snode_->insert_children(SNodeType::place));
     child_snode_->dt = PrimitiveType::i32;
 
-    FakeStructCompiler sc;
-    sc.run(*root_snode_);
+    TestStructCompiler sc;
+    sc.infer_snode_properties(*root_snode_);
 
     for_stmt_ = std::make_unique<OffloadedStmt>(
         /*task_type=*/OffloadedTaskType::struct_for,
@@ -46,6 +59,7 @@ class BLSAnalyzerTest : public ::testing::Test {
   ScratchPads pads_;
 
   IRBuilder builder_;
+  LoopIndexStmt *loop_index_{nullptr};
 };
 
 TEST_F(BLSAnalyzerTest, Basic) {
@@ -88,6 +102,37 @@ TEST_F(BLSAnalyzerTest, Mul) {
   builder_.create_global_load(glb_ptr);
   // x[i * 8, j * 4 - 3]
   idx1 = builder_.create_mul(loop_idx1_, c2);
+  idx1 = builder_.create_sub(idx1, c3);
+  glb_ptr = builder_.create_global_ptr(child_snode_,
+                                       /*indices=*/{idx0, idx1});
+  builder_.create_global_load(glb_ptr);
+
+  BLSAnalyzer bls(for_stmt_.get(), &pads_);
+  pads_.finalize();
+  const auto &pad = pads_.get(child_snode_);
+  EXPECT_EQ(pad.bounds.size(), 2);
+  EXPECT_EQ(pad.coefficients[0], 8);
+  EXPECT_EQ(pad.coefficients[1], 4);
+  EXPECT_EQ(pad.bounds[0].low, 0);
+  EXPECT_EQ(pad.bounds[0].high, kBlockSize);
+  EXPECT_EQ(pad.bounds[1].low, -3);
+  EXPECT_EQ(pad.bounds[1].high, kBlockSize);
+}
+
+TEST_F(BLSAnalyzerTest, Shl) {
+  auto *loop_idx0_ = builder_.get_loop_index(for_stmt_.get(), /*index=*/0);
+  auto *loop_idx1_ = builder_.get_loop_index(for_stmt_.get(), /*index=*/1);
+  auto *c1 = builder_.get_int32(/*value=*/3);
+  auto *c2 = builder_.get_int32(/*value=*/2);
+  auto *c3 = builder_.get_int32(/*value=*/3);
+  // x[i << 3, j << 2]
+  auto *idx0 = builder_.create_shl(loop_idx0_, c1);
+  auto *idx1 = builder_.create_shl(loop_idx1_, c2);
+  auto *glb_ptr = builder_.create_global_ptr(child_snode_,
+                                             /*indices=*/{idx0, idx1});
+  builder_.create_global_load(glb_ptr);
+  // x[i << 3, (j << 2) - 3]
+  idx1 = builder_.create_shl(loop_idx1_, c2);
   idx1 = builder_.create_sub(idx1, c3);
   glb_ptr = builder_.create_global_ptr(child_snode_,
                                        /*indices=*/{idx0, idx1});

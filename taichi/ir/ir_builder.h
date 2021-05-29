dd29a360ed7aa3aa0b94a600bdd102088e183605
Taichi Gardener <taichigardener@gmail.com>
2021-05-29 03:46:42 -0400

    [skip ci] enforce code format

--- taichi/ir/ir_builder.h
+++ taichi/ir/ir_builder.h
@@ -92,8 +92,7 @@ class IRBuilder {
   template <typename XStmt>
   [[nodiscard]] LoopGuard get_loop_guard(XStmt *loop) {
     return LoopGuard(*this, loop);
-  }
-  [[nodiscard]] IfGuard get_if_guard(IfStmt *if_stmt, bool true_branch) {
+  }[[nodiscard]] IfGuard get_if_guard(IfStmt *if_stmt, bool true_branch) {
     return IfGuard(*this, if_stmt, true_branch);
   }
 

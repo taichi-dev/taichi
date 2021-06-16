4bbcfa49fb9866980062adf9dcc20cd08eef202f
Yu Fang <squarefk@gmail.com>
2021-06-16 15:10:05 +0800

    [wasm] Rename get_root() into get_root_address() and add set_root()

--- taichi/backends/wasm/codegen_wasm.cpp
+++ taichi/backends/wasm/codegen_wasm.cpp
@@ -141,11 +141,11 @@ class CodeGenLLVMWASM : public CodeGenLLVM {
     // TI_INFO("Kernel function verified.");
   }
 
-  std::string create_taichi_get_root_function() {
+  std::string create_taichi_get_root_address_function() {
     auto task_function_type =
         llvm::FunctionType::get(llvm::Type::getInt32Ty(*llvm_context),
                                 {llvm::PointerType::get(context_ty, 0)}, false);
-    auto task_kernel_name = fmt::format("get_root");
+    auto task_kernel_name = fmt::format("get_root_address");
     auto func = llvm::Function::Create(task_function_type,
                                        llvm::Function::ExternalLinkage,
                                        task_kernel_name, module.get());
@@ -177,6 +177,45 @@ class CodeGenLLVMWASM : public CodeGenLLVM {
     return task_kernel_name;
   }
 
+  std::string create_taichi_set_root_function() {
+    auto task_function_type =
+        llvm::FunctionType::get(llvm::Type::getVoidTy(*llvm_context),
+                                {llvm::PointerType::get(context_ty, 0),
+                                 llvm::Type::getInt32Ty(*llvm_context)}, false);
+    auto task_kernel_name = fmt::format("set_root");
+    auto func = llvm::Function::Create(task_function_type,
+                                       llvm::Function::ExternalLinkage,
+                                       task_kernel_name, module.get());
+
+    std::vector<llvm::Value *> kernel_args;
+    for (auto &arg : func->args()) {
+      kernel_args.push_back(&arg);
+    }
+    kernel_args[0]->setName("context");
+    kernel_args[1]->setName("root");
+
+    auto entry_block = llvm::BasicBlock::Create(*llvm_context, "entry", func);
+    auto func_body_bb = llvm::BasicBlock::Create(*llvm_context, "body", func);
+    builder->SetInsertPoint(func_body_bb);
+
+    // memory reserved for Context object shouldn't be polluted
+    llvm::Value *runtime_ptr = create_call("Context_get_runtime", {kernel_args[0]});
+    llvm::Value *runtime = builder->CreateBitCast(
+        runtime_ptr, llvm::PointerType::get(get_runtime_type("LLVMRuntime"), 0));
+
+    llvm::Value *root_ptr = builder->CreateIntToPtr(
+        kernel_args[1], llvm::Type::getInt8PtrTy(*llvm_context));
+    llvm::Value *ret_ptr = create_call("LLVMRuntime_set_root", {runtime, root_ptr});
+    builder->CreateRetVoid();
+
+    builder->SetInsertPoint(entry_block);
+    builder->CreateBr(func_body_bb);
+
+    TI_ASSERT(!llvm::verifyFunction(*func, &llvm::errs()));
+    // TI_INFO("Kernel function verified.");
+    return task_kernel_name;
+  }
+
   FunctionType gen() override {
     TI_AUTO_PROF
     // emit_to_module
@@ -185,13 +224,15 @@ class CodeGenLLVMWASM : public CodeGenLLVM {
     ir->accept(this);
     finalize_taichi_kernel_function();
 
-    auto get_root_name = create_taichi_get_root_function();
+    auto get_root_address_name = create_taichi_get_root_address_function();
+    auto set_root_name = create_taichi_set_root_function();
 
     // compile_module_to_executable
     // only keep the current func
     TaichiLLVMContext::eliminate_unused_functions(
       module.get(), [&](std::string func_name) {
-        return offloaded_task_name == func_name || get_root_name == func_name;
+        return offloaded_task_name == func_name || get_root_address_name == func_name
+              || set_root_name == func_name;
       });
     tlctx->add_module(std::move(module));
     auto kernel_symbol = tlctx->lookup_function_pointer(offloaded_task_name);

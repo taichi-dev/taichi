d01283c5dd92f4bfa1624eb3718bc64d49556c1d
xumingkuan <xumingkuan0721@126.com>
2021-06-22 13:32:34 +0800

    Update taichi/ir/control_flow_graph.cpp
    Co-authored-by: Yuanming Hu <yuanming-hu@users.noreply.github.com>

--- taichi/ir/control_flow_graph.cpp
+++ taichi/ir/control_flow_graph.cpp
@@ -950,7 +950,7 @@ void ControlFlowGraph::determine_ad_stack_size(int max_ad_stack_size) {
     }
     TI_WARN(
         "Unable to determine capacity for autodiff stacks: {}. "
-        "Use default capacity {} instead.",
+        "Use configured maximum capacity (CompileConfig::max_ad_stack_size) {} instead.",
         fmt::join(oversized_stacks_name, ", "), max_ad_stack_size);
   }
 

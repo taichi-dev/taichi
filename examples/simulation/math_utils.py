927b30510c2debc1a1fdacb6598067a0413ae776
Andrew Sun <victoriacity74@gmail.com>
2021-07-01 00:36:02 -0500

    [skip ci] internalize tree_gravity example

--- examples/simulation/math_utils.py
+++ examples/simulation/math_utils.py
@@ -3,34 +3,62 @@ import math
 import taichi as ti
 
 @ti.func
-def randUnit2D():
+def rand_vector(n):
     '''
-    Generate a 2-D random unit vector whose length is equal to 1.0.
-    The return value is a 2-D vector, whose tip distributed evenly
-    **on the border** of a unit circle.
-    :return:
-        The return value is computed as::
-            a = rand() * math.tau
-            return vec(ti.cos(a), ti.sin(a))
+    Samples a n-dimensional random uniform vector.
+    '''
+    return ti.Vector([ti.random() for _ in range(n)])
+
+
+@ti.func
+def clamp(x, minval, maxval):
+    return min(max(x, minval), maxval)
+
+
+@ti.func
+def rand_unit_2d():
+    '''
+    Uniformly samples a vector on a 2D unit circle.
     '''
     a = ti.random() * math.tau
     return ti.Vector([ti.cos(a), ti.sin(a)])
 
 
 @ti.func
-def randUnit3D():
+def rand_unit_3d():
     '''
-    Generate a 3-D random unit vector whose length is equal to 1.0.
-    The return value is a 3-D vector, whose tip distributed evenly
-    **on the surface** of a unit sphere.
-    :return:
-        The return value is computed as::
-            u = randUnit2D()
-            s = rand() * 2 - 1
-            c = sqrt(1 - s ** 2)
-            return vec3(c * u, s)
+    Uniformly samples a vector on a 3D unit sphere.
     '''
-    u = randUnit2D()
+    u = rand_unit_2d()
     s = ti.random() * 2 - 1
     c = ti.sqrt(1 - s ** 2)
     return ti.Vector([c * u[0], c * u[1], s])
+
+
+@ti.func
+def rand_disk_2d():
+    '''
+    Uniformly samples a point within the area of a 2D unit disk.
+    '''
+    x = 2 * ti.random() - 1
+    y = 2 * ti.random() - 1
+    while x * x + y * y > 1:
+        x = 2 * ti.random() - 1
+        y = 2 * ti.random() - 1
+    return ti.Vector([x, y])
+
+
+@ti.func
+def reflect_boundary(pos, vel, pmin=0, pmax=1, rebound=1, rebound_perpendicular=1):
+    '''
+    Reflects particle velocity from a rectangular boundary (if collides).
+    `boundaryReflect` takes particle position, velocity and other parameters.
+    '''
+    cond = pos < pmin and vel < 0 or pos > pmax and vel > 0
+    for j in ti.static(range(pos.n)):
+        if cond[j]:
+            vel[j] *= -rebound
+            for k in ti.static(range(pos.n)):
+                if k != j:
+                    vel[k] *= rebound_perpendicular
+    return vel

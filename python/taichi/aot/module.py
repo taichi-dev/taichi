6876b21c9a9fd8cf75a9a0073f8a3ad43442a6b4
rgu <gubeini@seas.upenn.edu>
2021-07-06 13:54:17 +0800

    add kernel template structs

--- python/taichi/aot/module.py
+++ python/taichi/aot/module.py
@@ -43,19 +43,14 @@ class KernelTemplate:
 
 class Module:
     """An AOT module to save and load Taichi kernels.
-
     This module serializes the Taichi kernels for a specific arch. The
     serialized module can later be loaded to run on that backend, without the
     Python environment.
-
     Example::
-
         m = ti.aot.Module(ti.metal)
         m.add_kernel(foo)
         m.add_kernel(bar)
-
         m.save('/path/to/module')
-
         # Now the module file '/path/to/module' contains the Metal kernels
         # for running ``foo`` and ``bar``.
     """
@@ -72,35 +67,30 @@ class Module:
         Args:
         name: name of taichi field
         field: taichi field
-
         a = ti.field(ti.f32, shape=(4,4))
         b = ti.field("something")
-
         m.add_field(a)
         m.add_field(b)
-
         Must add in sequence
       """
-        is_scalar = True
+        is_vector = False
         self._fields[name] = field
         vector_size = 1
         if type(field) is matrix.Matrix:
             assert isinstance(field, matrix.Matrix)
-            is_scalar = False
+            is_vector = True
             vector_size = field.n
         else:
             assert isinstance(field, expr.Expr)
-        self._aot_builder.add_field(name, is_scalar, field.dtype,
+        self._aot_builder.add_field(name, is_vector, field.dtype,
                                     tuple(field.snode.shape), vector_size)
 
     def add_kernel(self, kernel_fn, name=None):
         """Add a taichi kernel to the AOT module.
-
         Args:
           kernel_fn (Function): the function decorated by taichi `kernel`.
           name (str): Name to identify this kernel in the module. If not
             provided, uses the built-in ``__name__`` attribute of `kernel_fn`.
-
         TODO:
           * Support external array
         """
@@ -124,34 +114,26 @@ class Module:
     @contextmanager
     def add_kernel_template(self, kernel_fn):
         """Add a taichi kernel (with template parameters) to the AOT module.
-
         Args:
           kernel_fn (Function): the function decorated by taichi `kernel`.
-
         Example:
-
           Usage::
-
             @ti.kernel
             def bar_tmpl(a: ti.template()):
               x = a
               # or y = a
               # do something with `x` or `y`
-
             m = ti.aot.Module(arch)
             with m.add_kernel_template(bar_tmpl) as kt:
               kt.instantiate(a=x)
               kt.instantiate(a=y)
-
             @ti.kernel
             def bar_tmpl_multiple_args(a: ti.template(), b: ti.template())
               x = a
               y = b
               # do something with `x` and `y`
-
             with m.add_kernel_template(bar_tmpl) as kt:
               kt.instantiate(a=x, b=y)
-
         TODO:
           * Support external array
         """

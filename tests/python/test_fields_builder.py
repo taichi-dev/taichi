29bea36f9a365f73155d81c9c372a9a4b770518e
JC Liu <ljcc0930@qq.com>
2021-07-08 13:16:40 +0800

    edit test

--- tests/python/test_fields_builder.py
+++ tests/python/test_fields_builder.py
@@ -26,77 +26,8 @@ def test_fields_with_shape():
     with pytest.raises(InvalidOperationError, match='FieldsBuilder finalized'):
         y = ti.field(ti.f32, [n])
 
-
 @ti.test(arch=[ti.cpu, ti.cuda])
-def test_fields_builder1():
-    n = 5
-    x = ti.field(ti.f32, [n])
-
-    @ti.kernel
-    def func1():
-        for i in range(n):
-            x[i] = i * 2
-
-    func1()
-    for i in range(n):
-        assert x[i] == i * 2
-
-    fb = ti.FieldsBuilder()
-    y = ti.field(ti.f32)
-    fb.dense(ti.i, n).place(y)
-    fb.finalize()
-
-    @ti.kernel
-    def func2():
-        for i in range(n):
-            y[i] = i // 2
-
-    func2()
-    for i in range(n):
-        assert y[i] == i // 2
-
-    func1()
-    for i in range(n):
-        assert x[i] == i * 2
-
-@ti.test(arch=[ti.cpu, ti.cuda])
-def test_fields_builder2():
-    n = 5
-
-    fb1 = ti.FieldsBuilder()
-    x = ti.field(ti.f32)
-    fb1.dense(ti.i, n).place(x)
-    fb1.finalize()
-
-    @ti.kernel
-    def func1():
-        for i in range(n):
-            x[i] = i * 3
-
-    func1()
-    for i in range(n):
-        assert x[i] == i * 3
-
-    fb2 = ti.FieldsBuilder()
-    y = ti.field(ti.f32)
-    fb2.dense(ti.i, n).place(y)
-    fb2.finalize()
-
-    @ti.kernel
-    def func2():
-        for i in range(n):
-            x[i] = i * 2
-        for i in range(n):
-            y[i] = i + 5
-
-    func2()
-    for i in range(n):
-        assert x[i] == i * 2
-        assert y[i] == i + 5
-
-
-@ti.test(arch=[ti.cpu, ti.cuda])
-def test_fields_builder3():
+def test_fields_builder_dense():
     n = 5
 
     fb1 = ti.FieldsBuilder()
@@ -118,6 +49,50 @@ def test_fields_builder3():
     fb2.dense(ti.i, n).place(y)
     z = ti.field(ti.f32)
     fb2.dense(ti.i, n).place(z)
+    fb2.finalize()
+
+    @ti.kernel
+    def func2():
+        for i in range(n):
+            x[i] = i * 2
+        for i in range(n):
+            y[i] = i + 5
+        for i in range(n):
+            z[i] = i + 10
+
+    func2()
+    for i in range(n):
+        assert x[i] == i * 2
+        assert y[i] == i + 5
+        assert z[i] == i + 10
+
+    func1()
+    for i in range(n):
+        assert x[i] == i * 3
+
+@ti.test(arch=[ti.cpu, ti.cuda])
+def test_fields_builder_pointer():
+    n = 5
+
+    fb1 = ti.FieldsBuilder()
+    x = ti.field(ti.f32)
+    fb1.pointer(ti.i, n).place(x)
+    fb1.finalize()
+
+    @ti.kernel
+    def func1():
+        for i in range(n):
+            x[i] = i * 3
+
+    func1()
+    for i in range(n):
+        assert x[i] == i * 3
+
+    fb2 = ti.FieldsBuilder()
+    y = ti.field(ti.f32)
+    fb2.pointer(ti.i, n).place(y)
+    z = ti.field(ti.f32)
+    fb2.pointer(ti.i, n).place(z)
     fb2.finalize()
 
     @ti.kernel

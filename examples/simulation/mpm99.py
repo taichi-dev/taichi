37078922c2159d516be3a61d60315f4eab6e76e5
Taichi Gardener <taichigardener@gmail.com>
2021-07-09 06:16:20 +0000

    Auto Format

--- examples/simulation/mpm99.py
+++ examples/simulation/mpm99.py
@@ -122,5 +122,9 @@ gui = ti.GUI("Taichi MLS-MPM-99", res=512, background_color=0x112F41)
 while not gui.get_event(ti.GUI.ESCAPE, ti.GUI.EXIT):
     for s in range(int(2e-3 // dt)):
         substep()
-    gui.circles(x.to_numpy(), radius=1.5, palette=[0x068587, 0xED553B, 0xEEEEF0], palette_indices=material)
-    gui.show()  # Change to gui.show(f'{frame:06d}.png') to write images to disk
+    gui.circles(x.to_numpy(),
+                radius=1.5,
+                palette=[0x068587, 0xED553B, 0xEEEEF0],
+                palette_indices=material)
+    gui.show(
+    )  # Change to gui.show(f'{frame:06d}.png') to write images to disk

8a03d9976fcf0d2c4ea11c3c71625124fef6d4c9
rgu <gubeini@seas.upenn.edu>
2021-07-14 18:34:34 +0800

    add vector size in dump field data, delete offset property in struct

--- taichi/backends/metal/aot_module_builder_impl.cpp
+++ taichi/backends/metal/aot_module_builder_impl.cpp
@@ -64,7 +64,7 @@ void AotModuleBuilderImpl::add_per_backend_field(const std::string &identifier,
   field_data.is_scalar = is_scalar;
   field_data.dtype = to_metal_type(dt);
   field_data.dtype_name = metal_data_type_name(dt);
-  field_data.dimension = {shape.first, shape.second};
+  field_data.dimension = shape;
   field_data.vector_size = vector_size;
   ti_aot_data_.fields.push_back(field_data);
 }

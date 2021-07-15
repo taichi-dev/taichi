fe51c4acec6e03f001a4472f6fc5e28f0f5d07a2
Yi Xu <xy_xuyi@foxmail.com>
2021-07-15 15:04:51 +0800

    Support packed mode for demote_dense_struct_for

--- taichi/transforms/demote_dense_struct_fors.cpp
+++ taichi/transforms/demote_dense_struct_fors.cpp
@@ -27,10 +27,21 @@ void convert_to_range_for(OffloadedStmt *offloaded) {
   std::reverse(snodes.begin(), snodes.end());
   TI_ASSERT(total_bits <= 30);
 
+  // general shape calculation - no dependence on POT
+  int total_n = 1;
+  std::array<int, taichi_max_num_indices> total_shape;
+  total_shape.fill(1);
+  for (const auto *s : snodes) {
+    for (int j = 0; j < taichi_max_num_indices; j++) {
+      total_shape[j] *= s->extractors[j].shape;
+    }
+    total_n *= s->n;
+  }
+
   offloaded->const_begin = true;
   offloaded->const_end = true;
   offloaded->begin_value = 0;
-  offloaded->end_value = 1 << total_bits;
+  offloaded->end_value = total_n;
 
   ////// Begin core transformation
   auto body = std::move(offloaded->body);
@@ -51,47 +62,70 @@ void convert_to_range_for(OffloadedStmt *offloaded) {
   auto main_loop_var = body_header.push_back<LoopIndexStmt>(nullptr, 0);
   // We will set main_loop_var->loop later.
 
-  int offset = total_bits;
-  int start_bits[taichi_max_num_indices] = {0};
-  std::copy(std::begin(start_bits_root), std::end(start_bits_root),
-            std::begin(start_bits));
   Stmt *test = body_header.push_back<ConstStmt>(TypedConstant(-1));
   bool has_test = false;
-  for (int i = 0; i < (int)snodes.size(); i++) {
-    auto snode = snodes[i];
-    offset -= snode->total_num_bits;
-    for (int j = 0; j < (int)physical_indices.size(); j++) {
-      auto p = physical_indices[j];
-      auto ext = snode->extractors[p];
-      Stmt *delta = body_header.push_back<BitExtractStmt>(
-          main_loop_var, ext.acc_offset + offset,
-          ext.acc_offset + offset + ext.num_bits);
-      start_bits[p] -= ext.num_bits;
-      auto multiplier =
-          body_header.push_back<ConstStmt>(TypedConstant(1 << start_bits[p]));
-      delta = body_header.push_back<BinaryOpStmt>(BinaryOpType::mul, delta,
-                                                  multiplier);
-      new_loop_vars[j] = body_header.push_back<BinaryOpStmt>(
-          BinaryOpType::add, new_loop_vars[j], delta);
+  if (get_current_program().config.packed) {  // no dependence on POT
+    for (int i = 0; i < (int)snodes.size(); i++) {
+      auto snode = snodes[i];
+      auto prev_n = body_header.push_back<ConstStmt>(TypedConstant(total_n));
+      total_n /= snode->n;
+      auto next_n = body_header.push_back<ConstStmt>(TypedConstant(total_n));
+      auto mod_n = body_header.push_back<BinaryOpStmt>(BinaryOpType::mod, main_loop_var, prev_n);
+      auto div_n = body_header.push_back<BinaryOpStmt>(BinaryOpType::div, mod_n, next_n);
+      for (int j = 0; j < (int)physical_indices.size(); j++) {
+        auto p = physical_indices[j];
+        auto ext = snode->extractors[p];
+        auto prev_acc_shape = body_header.push_back<ConstStmt>(TypedConstant(ext.acc_shape * ext.shape));
+        auto next_acc_shape = body_header.push_back<ConstStmt>(TypedConstant(ext.acc_shape));
+        auto mod_acc_shape = body_header.push_back<BinaryOpStmt>(BinaryOpType::mod, div_n, prev_acc_shape);
+        auto div_acc_shape = body_header.push_back<BinaryOpStmt>(BinaryOpType::div, mod_acc_shape, next_acc_shape);
+        total_shape[p] /= ext.shape;
+        auto multiplier = body_header.push_back<ConstStmt>(TypedConstant(total_shape[p]));
+        auto delta = body_header.push_back<BinaryOpStmt>(BinaryOpType::mul, div_acc_shape, multiplier);
+        new_loop_vars[j] = body_header.push_back<BinaryOpStmt>(BinaryOpType::add, new_loop_vars[j], delta);
+      }
     }
-  }
+  } else {
+    int offset = total_bits;
+    int start_bits[taichi_max_num_indices] = {0};
+    std::copy(std::begin(start_bits_root), std::end(start_bits_root),
+              std::begin(start_bits));
+    for (int i = 0; i < (int)snodes.size(); i++) {
+      auto snode = snodes[i];
+      offset -= snode->total_num_bits;
+      for (int j = 0; j < (int)physical_indices.size(); j++) {
+        auto p = physical_indices[j];
+        auto ext = snode->extractors[p];
+        Stmt *delta = body_header.push_back<BitExtractStmt>(
+            main_loop_var, ext.acc_offset + offset,
+            ext.acc_offset + offset + ext.num_bits);
+        start_bits[p] -= ext.num_bits;
+        auto multiplier =
+            body_header.push_back<ConstStmt>(TypedConstant(1 << start_bits[p]));
+        delta = body_header.push_back<BinaryOpStmt>(BinaryOpType::mul, delta,
+                                                    multiplier);
+        new_loop_vars[j] = body_header.push_back<BinaryOpStmt>(
+            BinaryOpType::add, new_loop_vars[j], delta);
+      }
+    }
 
-  std::copy(std::begin(start_bits_root), std::end(start_bits_root),
-            std::begin(start_bits));
-  for (int i = 0; i < (int)snodes.size(); i++) {
-    auto snode = snodes[i];
-    for (int j = 0; j < (int)physical_indices.size(); j++) {
-      auto p = physical_indices[j];
-      start_bits[p] -= snode->extractors[p].num_bits;
-      auto num_elements = snode->extractors[p].num_elements << start_bits[p];
-      if (!bit::is_power_of_two(num_elements)) {
-        has_test = true;
-        auto bound =
-            body_header.push_back<ConstStmt>(TypedConstant(num_elements));
-        auto cmp = body_header.push_back<BinaryOpStmt>(BinaryOpType::cmp_lt,
-                                                       new_loop_vars[j], bound);
-        test = body_header.push_back<BinaryOpStmt>(BinaryOpType::bit_and, test,
-                                                   cmp);
+    std::copy(std::begin(start_bits_root), std::end(start_bits_root),
+              std::begin(start_bits));
+    for (int i = 0; i < (int)snodes.size(); i++) {
+      auto snode = snodes[i];
+      for (int j = 0; j < (int)physical_indices.size(); j++) {
+        auto p = physical_indices[j];
+        start_bits[p] -= snode->extractors[p].num_bits;
+        auto num_elements = snode->extractors[p].num_elements << start_bits[p];
+        if (!bit::is_power_of_two(num_elements)) {
+          has_test = true;
+          auto bound =
+              body_header.push_back<ConstStmt>(TypedConstant(num_elements));
+          auto cmp = body_header.push_back<BinaryOpStmt>(
+              BinaryOpType::cmp_lt, new_loop_vars[j], bound);
+          test = body_header.push_back<BinaryOpStmt>(BinaryOpType::bit_and,
+                                                     test, cmp);
+        }
       }
     }
   }

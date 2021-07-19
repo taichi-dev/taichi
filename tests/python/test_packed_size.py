5427216f136c5acc4761981dd36dd3916251a1b1
Yi Xu <xy_xuyi@foxmail.com>
2021-07-19 18:00:44 +0800

    Update tests/python/test_packed_size.py
    Co-authored-by: Ye Kuang <k-ye@users.noreply.github.com>

--- tests/python/test_packed_size.py
+++ tests/python/test_packed_size.py
@@ -5,4 +5,4 @@ import taichi as ti
 def test_packed_size():
     x = ti.field(ti.i32)
     ti.root.dense(ti.i, 20).dense(ti.ijk, 334).place(x)
-    assert x.snode.parent().parent().cell_size_bytes == 149038816
+    assert x.snode.parent().parent().cell_size_bytes == 4 * 334 ** 3

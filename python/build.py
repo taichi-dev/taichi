d5c2a8fcb91dea05af07487c68237b47f0da35c5
Ailing <ailzhang@users.noreply.github.com>
2021-07-19 02:39:38 -0700

    [Refactor] Use setup.py from root dir. (#2540)
    A few things included in this PR:
    - Move `setup.py` up to root dir so that we can run `python setup.py
      install/develop`.
    - Remove dependency of `taichi_core` in build process. We used to call
      `ti.core.get_runtime_dir()/get_version_string()/libdevice_path()` from
    C++, but these information are already available in build process.
    - Simplify `python/build.py` to call `setup.py` in the root. Tested
      `build/test` commands still work as expected.
    - Update github workflows to use `python setup.py install` in linux/mac instead of
      setting env `TAICHI_REPO_DIR`.
    - Developers used to customize taichi_core.so build using args passed
      into cmake. Under this new mechanism they can put everything inside
    `TAICHI_CMAKE_ARGS` and run `setup.py` using: `TAICHI_CMAKE_ARGS="-DXXXX -DXXXX" python setup.py ..`
    - Move `tests/conftest.py` into `tests/python` folder. I think it was a
      bug that we skipped it when we copied `tests` folder when building wheels.
    
    To explain how build works after this PR:
    In short, c++ build is no longer a separate step. It's built as an
    extension when you build `taichi` python package.
    
    Developers can use
    - `python setup.py develop`(or `pip install -e .`) in root dir to
      install taichi into their current python environment. Note in
    `develop` mode any local changes to python files take effect
    immediately. You only need to rerun this command when you change C++
    code.
    - `python setup.py install`(or `pip install .`) to install taichi into
      your their current python environment. Note in `install` mode files
    are copied over to python's `site-packages` so your local changes won't
    take effect until you rerun the command.
    - No more manipulation with `TAICHI_REPO_DIR` and `PYTHONPATH`.
    
    A few known followups:
    - I left windows job to remain using `TAICHI_REPO_DIR` since I don't
      have a windows machine. But it should be straightfoward to update if
      someone else can help.
    - We can start removing `TAICHI_REPO_DIR` in the codebase after this PR since our
      development mode has changed.
    - Update developer install documentation.
    - I'm pretty sure the handling for windows in `setup.py` might have
      minor problems about relative paths but will leave it to debug in CI.
       It should be a simple fix.
    - We probably need to reconsider if we want to include assets/examples
      and a few other .py files under `python/taichi` in the release.

--- python/build.py
+++ python/build.py
@@ -5,8 +5,6 @@ import re
 import shutil
 import sys
 
-import taichi as ti
-
 
 def get_os_name():
     name = platform.platform()
@@ -26,94 +24,33 @@ def get_python_executable():
 
 
 def build(project_name):
-    """Build and package the wheel file in `python/dist`"""
+    """Build and package the wheel file in root `dist` dir"""
     if platform.system() == 'Linux':
         if re.search("^clang\+\+-*\d*", str(os.environ.get('CXX'))) is None:
             raise RuntimeError(
                 'Only the wheel with clang will be released to PyPI')
-    version = ti.core.get_version_string()
-    with open('../setup.py') as fin:
-        with open('setup.py', 'w') as fout:
-            print("project_name = '{}'".format(project_name), file=fout)
-            print("version = '{}'".format(version), file=fout)
-            for l in fin:
-                print(l, file=fout, end='')
-
-    print("*** project_name = '{}'".format(project_name))
-
-    try:
-        os.remove('taichi/CHANGELOG.md')
-    except FileNotFoundError:
-        pass
-    shutil.rmtree('taichi/lib', ignore_errors=True)
-    shutil.rmtree('taichi/tests', ignore_errors=True)
-    shutil.rmtree('taichi/examples', ignore_errors=True)
-    shutil.rmtree('taichi/assets', ignore_errors=True)
-    os.makedirs('taichi/lib', exist_ok=True)
-    shutil.rmtree('build', ignore_errors=True)
-    shutil.rmtree('dist', ignore_errors=True)
-    shutil.rmtree('taichi/include', ignore_errors=True)
-    # shutil.copytree('../include/', 'taichi/include')
-    build_dir = '../build'
-
-    if get_os_name() == 'linux':
-        shutil.copy('../build/libtaichi_core.so', 'taichi/lib/taichi_core.so')
-    elif get_os_name() == 'osx':
-        shutil.copy('../build/libtaichi_core.dylib',
-                    'taichi/lib/taichi_core.so')
-    else:
-        shutil.copy('../runtimes/RelWithDebInfo/taichi_core.dll',
-                    'taichi/lib/taichi_core.pyd')
-
-    os.system(f'cd .. && {get_python_executable()} -m taichi changelog --save')
-
-    try:
-        with open('../CHANGELOG.md') as f:
-            print(f.read())
-    except FileNotFoundError:
-        print('CHANGELOG.md not found')
-        pass
-
-    try:
-        shutil.copy('../CHANGELOG.md', './taichi/CHANGELOG.md')
-    except FileNotFoundError:
-        pass
-    shutil.copytree('../tests/python', './taichi/tests')
-    shutil.copytree('../examples', './taichi/examples')
-    shutil.copytree('../external/assets', './taichi/assets')
-
-    if get_os_name() != 'osx':
-        libdevice_path = ti.core.libdevice_path()
-        print("copying libdevice:", libdevice_path)
-        assert os.path.exists(libdevice_path)
-        shutil.copy(libdevice_path, 'taichi/lib/slim_libdevice.10.bc')
-
-    runtime_dir = ti.core.get_runtime_dir()
-    for f in os.listdir(runtime_dir):
-        if f.startswith('runtime_') and f.endswith('.bc'):
-            print(f"Fetching runtime file {f}")
-            shutil.copy(os.path.join(runtime_dir, f), 'taichi/lib')
 
     print("Using python executable", get_python_executable())
     os.system(
         '{} -m pip install --user --upgrade twine setuptools wheel'.format(
             get_python_executable()))
 
-    if get_os_name() == 'linux':
-        os.system('{} setup.py bdist_wheel -p manylinux1_x86_64'.format(
-            get_python_executable()))
-    else:
-        os.system('{} setup.py bdist_wheel'.format(get_python_executable()))
+    os.system(f'{get_python_executable()} -m taichi changelog --save')
 
-    shutil.rmtree('taichi/lib')
-    shutil.rmtree('taichi/tests')
-    shutil.rmtree('taichi/examples')
-    shutil.rmtree('taichi/assets')
+    if get_os_name() == 'linux':
+        os.system(
+            f'cd ..; PROJECT_NAME={project_name} {get_python_executable()} setup.py bdist_wheel -p manylinux1_x86_64'
+        )
+    else:
+        os.system(
+            f'cd ..; PROJECT_NAME={project_name} {get_python_executable()} setup.py bdist_wheel'
+        )
+
     try:
         os.remove('taichi/CHANGELOG.md')
     except FileNotFoundError:
         pass
-    shutil.rmtree('./build')
+    shutil.rmtree('../build')
 
 
 def parse_args():
@@ -148,6 +85,8 @@ def main():
 
     env_pypi_pwd = os.environ.get('PYPI_PWD', '')
 
+    shutil.rmtree('../dist', ignore_errors=True)
+
     if mode == 'try_upload':
         if env_pypi_pwd == '':
             print("Missing environment variable PYPI_PWD")
@@ -173,16 +112,18 @@ def main():
             get_python_executable(), pypi_repo, pypi_user))
     elif mode == 'test':
         print('Uninstalling old taichi packages...')
-        os.system(f'{get_python_executable()} -m pip uninstall taichi-nightly')
-        os.system(f'{get_python_executable()} -m pip uninstall taichi')
-        dists = os.listdir('dist')
+        os.system(
+            f'{get_python_executable()} -m pip uninstall -y taichi-nightly')
+        os.system(f'{get_python_executable()} -m pip uninstall -y taichi')
+        dists = os.listdir('../dist')
         assert len(dists) == 1
         dist = dists[0]
         print('Installing ', dist)
         os.environ['PYTHONPATH'] = ''
         os.makedirs('test_env', exist_ok=True)
-        os.system('cd test_env && {} -m pip install ../dist/{} --user'.format(
-            get_python_executable(), dist))
+        os.system(
+            'cd test_env && {} -m pip install ../../dist/{} --user'.format(
+                get_python_executable(), dist))
         print('Entering test environment...')
         if get_os_name() == 'win':
             os.system(

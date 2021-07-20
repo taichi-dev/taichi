89b6a3e7ecc23c2a8d933e3c633e899f38f7a025
Jiasheng Zhang <leonz5288@hotmail.com>
2021-07-20 17:32:32 +0800

    [Lang] Support very basic python string.format() now (#2552)
    * [Lang] Support very basic python string.format() now
    
    * fixed bug
    
    * Auto Format
    
    * fixed bug
    
    * Auto Format
    
    * Improved method
    
    * Update python/taichi/lang/transformer.py
    
    Co-authored-by: Taichi Gardener <taichigardener@gmail.com>
    Co-authored-by: ljcc0930 <ljcc0930@qq.com>

--- python/taichi/lang/impl.py
+++ python/taichi/lang/impl.py
@@ -517,7 +517,13 @@ def ti_print(*vars, sep=' ', end='\n'):
             if hasattr(var, '__ti_repr__'):
                 res = var.__ti_repr__()
             elif isinstance(var, (list, tuple)):
-                res = list_ti_repr(var)
+                res = var
+                # If the first element is '__ti_format__', this list is the result of ti_format.
+                if len(var) > 0 and isinstance(
+                        var[0], str) and var[0] == '__ti_format__':
+                    res = var[1:]
+                else:
+                    res = list_ti_repr(var)
             else:
                 yield var
                 continue
@@ -550,6 +556,35 @@ def ti_print(*vars, sep=' ', end='\n'):
     entries = fused_string(entries)
     contentries = [entry2content(entry) for entry in entries]
     _ti_core.create_print(contentries)
+
+
+@taichi_scope
+def ti_format(*args):
+    content = args[0]
+    mixed = args[1:]
+    new_mixed = []
+    args = []
+    for x in mixed:
+        if isinstance(x, ti.Expr):
+            new_mixed.append('{}')
+            args.append(x)
+        else:
+            new_mixed.append(x)
+
+    try:
+        content = content.format(*new_mixed)
+    except ValueError:
+        print('Number formatting is not supported with Taichi fields')
+        exit(1)
+    res = content.split('{}')
+    assert len(res) == len(
+        args
+    ) + 1, 'Number of args is different from number of positions provided in string'
+
+    for i in range(len(args)):
+        res.insert(i * 2 + 1, args[i])
+    res.insert(0, '__ti_format__')
+    return res
 
 
 @taichi_scope

daa0c372fe36918aa8237075e5dadbaf9199b21c
Yi Xu <xy_xuyi@foxmail.com>
2021-07-22 19:04:12 +0800

    Fix test_packed_size

--- tests/python/test_packed_size.py
+++ tests/python/test_packed_size.py
@@ -1,7 +1,7 @@
 import taichi as ti
 
 
-@ti.test(packed=True)
+@ti.test(require=ti.extension.packed, packed=True)
 def test_packed_size():
     x = ti.field(ti.i32)
     ti.root.dense(ti.i, 20).dense(ti.ijk, 334).place(x)

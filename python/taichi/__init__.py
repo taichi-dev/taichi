9d2bfc37c85e3a8754efcefb82f3b6167223a219
Andrew Sun <victoriacity74@gmail.com>
2021-07-26 22:15:08 -0500

    revert abort python 2 import for PR branches

--- python/taichi/__init__.py
+++ python/taichi/__init__.py
@@ -1,19 +1,15 @@
-import sys
-if sys.version_info[0] == 3:
-    # Abort importing Taichi in a Python2 environment. 
-    # This is to make Houdini load correctly under a Taichi dev install.
-    from taichi.core import *
-    from taichi.lang import *  # TODO(archibate): It's `taichi.lang.core` overriding `taichi.core`
-    from taichi.main import main
-    from taichi.misc import *
-    from taichi.testing import *
-    from taichi.tools import *
-    from taichi.torch_io import from_torch, to_torch
+from taichi.core import *
+from taichi.lang import *  # TODO(archibate): It's `taichi.lang.core` overriding `taichi.core`
+from taichi.main import main
+from taichi.misc import *
+from taichi.testing import *
+from taichi.tools import *
+from taichi.torch_io import from_torch, to_torch
 
-    # Issue#2223: Do not reorder, or we're busted with partially initialized module
-    from taichi import aot  # isort:skip
+# Issue#2223: Do not reorder, or we're busted with partially initialized module
+from taichi import aot  # isort:skip
 
-        __all__ = ['core', 'misc', 'lang', 'tools', 'main', 'torch_io']
+__all__ = ['core', 'misc', 'lang', 'tools', 'main', 'torch_io']
 
-        __version__ = (core.get_version_major(), core.get_version_minor(),
-                    core.get_version_patch())
+__version__ = (core.get_version_major(), core.get_version_minor(),
+               core.get_version_patch())

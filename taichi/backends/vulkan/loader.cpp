8b4633b7261f804b83e7ff1df7f3447312ae35e6
Taichi Gardener <taichigardener@gmail.com>
2021-08-01 11:12:18 +0000

    Auto Format

--- taichi/backends/vulkan/loader.cpp
+++ taichi/backends/vulkan/loader.cpp
@@ -5,35 +5,32 @@
 
 #include "taichi/backends/vulkan/loader.h"
 
-
 namespace taichi {
 namespace lang {
 namespace vulkan {
 
-
-VulkanLoader::VulkanLoader(){
-    initialized = false;
+VulkanLoader::VulkanLoader() {
+  initialized = false;
 }
 
-bool VulkanLoader::init(){
-    if(initialized){
-        return true;
-    }
-    VkResult result = volkInitialize();
-    initialized = result == VK_SUCCESS;
-    return initialized;
+bool VulkanLoader::init() {
+  if (initialized) {
+    return true;
+  }
+  VkResult result = volkInitialize();
+  initialized = result == VK_SUCCESS;
+  return initialized;
 }
 
-void VulkanLoader::load_instance(VkInstance instance){
-    vulkan_instance = instance;
-    volkLoadInstance(instance);
+void VulkanLoader::load_instance(VkInstance instance) {
+  vulkan_instance = instance;
+  volkLoadInstance(instance);
 }
-void VulkanLoader::load_device(VkDevice device){
-    vulkan_device = device;
-    volkLoadDevice(device);
+void VulkanLoader::load_device(VkDevice device) {
+  vulkan_device = device;
+  volkLoadDevice(device);
 }
 
 }  // namespace vulkan
 }  // namespace lang
 }  // namespace taichi
-

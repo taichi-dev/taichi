f69de9531c60373793526b68fd8aeb5da0b8b55c
Taichi Gardener <taichigardener@gmail.com>
2021-08-01 11:12:18 +0000

    Auto Format

--- taichi/backends/vulkan/loader.h
+++ taichi/backends/vulkan/loader.h
@@ -5,39 +5,30 @@ namespace taichi {
 namespace lang {
 namespace vulkan {
 
-
 class VulkanLoader {
-public:
-    static VulkanLoader& instance()
-    {
-        static VulkanLoader   instance; 
-        return instance;
-    }
+ public:
+  static VulkanLoader &instance() {
+    static VulkanLoader instance;
+    return instance;
+  }
 
-public:
-    VulkanLoader(VulkanLoader const&) = delete;
-    void operator=(VulkanLoader const&) = delete;
+ public:
+  VulkanLoader(VulkanLoader const &) = delete;
+  void operator=(VulkanLoader const &) = delete;
 
-    void load_instance(VkInstance instance_);
-    void load_device(VkDevice device_);
-    bool init();
+  void load_instance(VkInstance instance_);
+  void load_device(VkDevice device_);
+  bool init();
 
-private:
+ private:
+  bool initialized;
 
-    bool initialized;
+  VulkanLoader();
 
-    VulkanLoader();
-
-    VkInstance vulkan_instance;
-    VkDevice vulkan_device;
+  VkInstance vulkan_instance;
+  VkDevice vulkan_device;
 };
-
-
-
 
 }  // namespace vulkan
 }  // namespace lang
 }  // namespace taichi
-
-
-

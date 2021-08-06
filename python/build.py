b71fb8108ddd33ba07166488261cfe29ed3df93d
Jiasheng Zhang <leonz5288@hotmail.com>
2021-08-06 16:46:32 +0800

    [ci] Fix mac release and integrate windows release into github (#2641)
    * [ci] Fix mac release and integrate windows release into github
    
    * Auto Format
    
    * Update python/build.py
    
    Co-authored-by: Ye Kuang <k-ye@users.noreply.github.com>
    
    Co-authored-by: Taichi Gardener <taichigardener@gmail.com>
    Co-authored-by: Ye Kuang <k-ye@users.noreply.github.com>

--- python/build.py
+++ python/build.py
@@ -42,9 +42,8 @@ def build(project_name):
             f'cd ..; PROJECT_NAME={project_name} {get_python_executable()} setup.py bdist_wheel -p manylinux1_x86_64'
         )
     else:
-        os.system(
-            f'cd ..; PROJECT_NAME={project_name} {get_python_executable()} setup.py bdist_wheel'
-        )
+        os.environ['PROJECT_NAME'] = project_name
+        os.system(f'cd .. && {get_python_executable()} setup.py bdist_wheel')
 
     try:
         os.remove('taichi/CHANGELOG.md')

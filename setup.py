a565a1b16181a1143e78acf03b58065abbcab3ee
Leon <leonz5288@hotmail.com>
2021-08-13 19:09:35 +0800

    added nightly version assign with respect to date & commit sha

--- setup.py
+++ setup.py
@@ -30,6 +30,11 @@ TI_VERSION_MAJOR = 0
 TI_VERSION_MINOR = 7
 TI_VERSION_PATCH = 29
 version = f'{TI_VERSION_MAJOR}.{TI_VERSION_MINOR}.{TI_VERSION_PATCH}'
+if project_name == 'taichi-nightly':
+    from datetime import date
+    today = date.today().strftime('%Y-%m-%d')
+    commit_sha = os.getenv('COMMIT_SHA')
+    version += f'.{today}.{commit_sha}'
 
 data_files = glob.glob('python/lib/*')
 print(data_files)

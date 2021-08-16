fd69af05cd3c4b4012996788991ce447c2f012d5
Yu Fang <squarefk@gmail.com>
2021-08-17 00:34:55 +0800

    update benchmarks

--- benchmarks/mpm2d.py
+++ benchmarks/mpm2d.py
@@ -122,7 +122,7 @@ def benchmark_range():
     ti.benchmark(substep, repeat=4000)
 
 
-@ti.archs_excluding(ti.opengl)
+@ti.test(exclude=ti.opengl)
 def benchmark_struct():
     quality = 1  # Use a larger value for higher-res simulations
     n_particles, n_grid = 9000 * quality**2, 128 * quality

15e23723e8cb982c82de458c3c0f43f10ee6d29d
Taichi Gardener <taichigardener@gmail.com>
2021-08-24 12:19:02 +0000

    Auto Format

--- python/taichi/__init__.py
+++ python/taichi/__init__.py
@@ -14,7 +14,10 @@ deprecated_names = {'SOA': 'Layout.SOA', 'AOS': 'Layout.AOS'}
 
 def __getattr__(attr):
     if attr in deprecated_names:
-        warning('ti.{} is deprecated. Please use ti.{} instead.'.format(attr, deprecated_names[attr]), DeprecationWarning, stacklevel=2)
+        warning('ti.{} is deprecated. Please use ti.{} instead.'.format(
+            attr, deprecated_names[attr]),
+                DeprecationWarning,
+                stacklevel=2)
         exec(f'{attr} = {deprecated_names[attr]}')
         return locals()[attr]
     raise AttributeError(f"module '{__name__}' has no attribute '{attr}'")

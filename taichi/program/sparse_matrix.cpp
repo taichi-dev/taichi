0b4e6fee6b62d622c9de8b7eb8eac8a58726daaa
FantasyVR <6712304+FantasyVR@users.noreply.github.com>
2021-08-24 21:34:29 +0800

    Update sparse_matrix.cpp

--- taichi/program/sparse_matrix.cpp
+++ taichi/program/sparse_matrix.cpp
@@ -7,122 +7,116 @@ namespace taichi {
 namespace lang {
 
 SparseMatrixBuilder::SparseMatrixBuilder(int n, int m, int max_num_triplets)
-    : n(n), m(m), max_num_triplets(max_num_triplets) {
-  data.reserve(max_num_triplets * 3);
-  data_base_ptr = get_data_base_ptr();
+    : n_(n), m_(m), max_num_triplets_(max_num_triplets) {
+  data_.reserve(max_num_triplets * 3);
+  data_base_ptr_ = get_data_base_ptr();
 }
 
 void *SparseMatrixBuilder::get_data_base_ptr() {
-  return data.data();
+  return data_.data();
 }
 
 void SparseMatrixBuilder::print_triplets() {
-  fmt::print("n={}, m={}, num_triplets={} (max={})", n, m, num_triplets,
-             max_num_triplets);
-  for (int64 i = 0; i < num_triplets; i++) {
-    fmt::print("({}, {}) val={}", data[i * 3], data[i * 3 + 1],
-               taichi_union_cast<float32>(data[i * 3 + 2]));
+  fmt::print("n={}, m={}, num_triplets={} (max={})", n_, m_, num_triplets_,
+             max_num_triplets_);
+  for (int64 i = 0; i < num_triplets_; i++) {
+    fmt::print("({}, {}) val={}", data_[i * 3], data_[i * 3 + 1],
+               taichi_union_cast<float32>(data_[i * 3 + 2]));
   }
   fmt::print("\n");
 }
 
 void SparseMatrixBuilder::build(SparseMatrix *sm) {
   TI_ASSERT(built == false);
-  built = true;
+  built_ = true;
   using T = Eigen::Triplet<float32>;
   std::vector<T> triplets;
-  for (int i = 0; i < num_triplets; i++) {
-    triplets.push_back({data[i * 3], data[i * 3 + 1],
-                        taichi_union_cast<float32>(data[i * 3 + 2])});
+  for (int i = 0; i < num_triplets_; i++) {
+    triplets.push_back({data_[i * 3], data_[i * 3 + 1],
+                        taichi_union_cast<float32>(data_[i * 3 + 2])});
   }
   sm->get_matrix().setFromTriplets(triplets.begin(), triplets.end());
 }
 
 SparseMatrix::SparseMatrix(Eigen::SparseMatrix<float32> &matrix) {
-  this->matrix = matrix;
+  this->matrix_ = matrix;
 }
 
-SparseMatrix::SparseMatrix(int n, int m) : n(n), m(m), matrix(n, m) {
+SparseMatrix::SparseMatrix(int n, int m) : n_(n), m_(m), matrix_(n, m) {
 }
 
 void SparseMatrix::print() {
   Eigen::IOFormat clean_fmt(4, 0, ", ", "\n", "[", "]");
   // Note that the code below first converts the sparse matrix into a dense one.
   // https://stackoverflow.com/questions/38553335/how-can-i-print-in-console-a-formatted-sparse-matrix-with-eigen
-  std::cout << Eigen::MatrixXf(matrix).format(clean_fmt) << std::endl;
+  std::cout << Eigen::MatrixXf(matrix_).format(clean_fmt) << std::endl;
 }
 
 int SparseMatrix::num_rows() {
-  return matrix.rows();
+  return matrix_.rows();
 }
 int SparseMatrix::num_cols() {
-  return matrix.cols();
+  return matrix_.cols();
 }
 
 Eigen::SparseMatrix<float32> &SparseMatrix::get_matrix() {
-  return matrix;
+  return matrix_;
 }
 
 SparseMatrix *operator+(const SparseMatrix &sm1, const SparseMatrix &sm2) {
-  SparseMatrix *res = new SparseMatrix(sm1.n, sm1.m);
-  res->matrix = sm1.matrix + sm2.matrix;
+  SparseMatrix *res = new SparseMatrix(sm1.n_, sm1.m_);
+  res->matrix_ = sm1.matrix_ + sm2.matrix_;
   return res;
 }
 
 SparseMatrix *operator-(const SparseMatrix &sm1, const SparseMatrix &sm2) {
-  SparseMatrix *res = new SparseMatrix(sm1.n, sm1.m);
-  res->matrix = sm1.matrix - sm2.matrix;
+  SparseMatrix *res = new SparseMatrix(sm1.n_, sm1.m_);
+  res->matrix_ = sm1.matrix_ - sm2.matrix_;
   return res;
 }
 
 SparseMatrix *operator*(float scale, const SparseMatrix &sm) {
-  SparseMatrix *res = new SparseMatrix(sm.n, sm.m);
-  res->matrix = scale * sm.matrix;
+  SparseMatrix *res = new SparseMatrix(sm.n_, sm.m_);
+  res->matrix_ = scale * sm.matrix_;
   return res;
 }
 
 SparseMatrix *operator*(const SparseMatrix &sm1, const SparseMatrix &sm2) {
-  SparseMatrix *res = new SparseMatrix(sm1.n, sm1.m);
-  res->matrix = sm1.matrix.cwiseProduct(sm2.matrix);
+  SparseMatrix *res = new SparseMatrix(sm1.n_, sm1.m_);
+  res->matrix_ = sm1.matrix_.cwiseProduct(sm2.matrix_);
   return res;
 }
 
 SparseMatrix *SparseMatrix::matmult(const SparseMatrix &sm) {
-  SparseMatrix *res = new SparseMatrix(n, m);
-  res->matrix = matrix * sm.matrix;
+  SparseMatrix *res = new SparseMatrix(n_, m_);
+  res->matrix_ = matrix_ * sm.matrix_;
   return res;
 }
 
 SparseMatrix *SparseMatrix::transpose() {
-  SparseMatrix *res = new SparseMatrix(n, m);
-  res->matrix = matrix.transpose();
+  SparseMatrix *res = new SparseMatrix(n_, m_);
+  res->matrix_ = matrix_.transpose();
   return res;
 }
 
 float32 SparseMatrix::get_coeff(int row, int col) {
-  return matrix.coeff(row, col);
+  return matrix_.coeff(row, col);
 }
 
 void SparseMatrix::solve(SparseMatrix *b_) {
   using namespace Eigen;
 
-  VectorXf x(n), b(m);
+  VectorXf x(n_), b(m_);
 
   b.setZero();
 
-  for (int k = 0; k < b_->matrix.outerSize(); ++k)
-    for (Eigen::SparseMatrix<float32>::InnerIterator it(b_->matrix, k); it;
+  for (int k = 0; k < b_->matrix_.outerSize(); ++k)
+    for (Eigen::SparseMatrix<float32>::InnerIterator it(b_->matrix_, k); it;
          ++it) {
       b[it.row()] = it.value();
     }
 
-  /*
-  SparseLU<Eigen::SparseMatrix<float32>, COLAMDOrdering<int>> solver;
-  solver.analyzePattern(matrix);
-  solver.factorize(matrix);
-  x = solver.solve(b);
-   */
-  x = Eigen::MatrixXf(matrix).ldlt().solve(b);
+  x = Eigen::MatrixXf(matrix_).ldlt().solve(b);
 
   Eigen::IOFormat clean_fmt(4, 0, ", ", "\n", "[", "]");
   // Note that the code below first converts the sparse matrix into a dense one.

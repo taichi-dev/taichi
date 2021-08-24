b66d82c25a2abc810525f41126088b5c0b5df9cf
Ailing Zhang <ailing@taichi.graphics>
2021-08-23 21:13:41 -0400

    [Refactor] Move check_runtime_error to LlvmProgramImpl.
    ghstack-source-id: bccdc7f5dc578c9f395b6d354cedd13cdb6ee20e
    Pull Request resolved: https://github.com/taichi-dev/taichi/pull/2780

--- taichi/llvm/llvm_program.cpp
+++ taichi/llvm/llvm_program.cpp
@@ -5,6 +5,7 @@
 #include "taichi/platform/cuda/detect_cuda.h"
 #include "taichi/math/arithmetic.h"
 #include "taichi/runtime/llvm/mem_request.h"
+#include "taichi/util/str.h"
 
 namespace taichi {
 namespace lang {
@@ -351,6 +352,54 @@ void LlvmProgramImpl::materialize_runtime(MemoryPool *memory_pool,
     runtime_jit->call<void *, void *>(
         "LLVMRuntime_set_profiler_stop", llvm_runtime,
         (void *)&KernelProfilerBase::profiler_stop);
+  }
+}
+
+void LlvmProgramImpl::check_runtime_error(uint64 *result_buffer) {
+  synchronize();
+  auto tlctx = llvm_context_host.get();
+  if (llvm_context_device) {
+    // In case there is a standalone device context (e.g. CUDA without unified
+    // memory), use the device context instead.
+    tlctx = llvm_context_device.get();
+  }
+  auto *runtime_jit_module = tlctx->runtime_jit_module;
+  runtime_jit_module->call<void *>("runtime_retrieve_and_reset_error_code",
+                                   llvm_runtime);
+  auto error_code =
+      fetch_result<int64>(taichi_result_buffer_error_id, result_buffer);
+
+  if (error_code) {
+    std::string error_message_template;
+
+    // Here we fetch the error_message_template char by char.
+    // This is not efficient, but fortunately we only need to do this when an
+    // assertion fails. Note that we may not have unified memory here, so using
+    // "fetch_result" that works across device/host memroy is necessary.
+    for (int i = 0;; i++) {
+      runtime_jit_module->call<void *>("runtime_retrieve_error_message",
+                                       llvm_runtime, i);
+      auto c = fetch_result<char>(taichi_result_buffer_error_id, result_buffer);
+      error_message_template += c;
+      if (c == '\0') {
+        break;
+      }
+    }
+
+    if (error_code == 1) {
+      const auto error_message_formatted = format_error_message(
+          error_message_template,
+          [runtime_jit_module, result_buffer, this](int argument_id) {
+            runtime_jit_module->call<void *>(
+                "runtime_retrieve_error_message_argument", llvm_runtime,
+                argument_id);
+            return fetch_result<uint64>(taichi_result_buffer_error_id,
+                                        result_buffer);
+          });
+      TI_ERROR("Assertion failure: {}", error_message_formatted);
+    } else {
+      TI_NOT_IMPLEMENTED
+    }
   }
 }
 

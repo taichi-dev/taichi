bd380bed1d99b94270f04338c951ec099af17437
dunfan.lu <dunfan.lu@taichi.graphics>
2021-08-24 21:32:35 -0400

    vertex and kernels

---
+++ taichi/ui/backends/vulkan/vertex.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <array>
+#include <volk.h>
+
+namespace taichi {
+namespace ui {
+
+struct Vertex {
+  struct vec3 {
+    float x;
+    float y;
+    float z;
+  };
+  struct vec2 {
+    float x;
+    float y;
+  };
+  vec3 pos;
+  vec3 normal;
+  vec2 texCoord;
+  vec3 color;
+};
+
+}  // namespace ui
+}  // namespace taichi

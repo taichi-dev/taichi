428d7e1a2bf2d85dc0110c7fd9caa119ab768cb3
Cheng Cao <bobcaocheng@gmail.com>
2021-08-26 09:23:01 -0700

    Move ref counted pool to commons

--- taichi/backends/vulkan/vulkan_device.h
+++ taichi/backends/vulkan/vulkan_device.h
@@ -14,6 +14,7 @@
 
 #include <taichi/backends/device.h>
 #include <taichi/backends/vulkan/vulkan_utils.h>
+#include <taichi/common/ref_counted_pool.h>
 
 namespace taichi {
 namespace lang {
@@ -22,99 +23,6 @@ namespace vulkan {
 class VulkanDevice;
 class VulkanResourceBinder;
 class VulkanStream;
-
-class RefCount {
- public:
-  void inc() {
-    ref_count++;
-  }
-  int dec() {
-    return --ref_count;
-  }
-  int count() {
-    return ref_count;
-  }
-
- private:
-  int ref_count{1};
-};
-
-template <class T, bool sync>
-class RefCountedPool {
- public:
-  void inc(T obj) {
-    if constexpr (sync) {
-      gc_pool_lock_.lock();
-    }
-
-    auto iter = counts_.find(obj);
-
-    if (iter == counts_.end()) {
-      counts_[obj] = RefCount();
-    } else {
-      iter->second.inc();
-    }
-
-    if constexpr (sync) {
-      gc_pool_lock_.unlock();
-    }
-  }
-
-  void dec(T obj) {
-    if constexpr (sync) {
-      gc_pool_lock_.lock();
-    }
-
-    auto iter = counts_.find(obj);
-
-    if (iter == counts_.end()) {
-      TI_ERROR("Can not find counted reference");
-    } else {
-      int c = iter->second.dec();
-      if (c == 0) {
-        gc_pool_.push_back(iter->first);
-        counts_.erase(iter);
-      }
-    }
-
-    if constexpr (sync) {
-      gc_pool_lock_.unlock();
-    }
-  }
-
-  T gc_pop_one(T null) {
-    if constexpr (sync) {
-      gc_pool_lock_.lock();
-    }
-
-    T obj = null;
-
-    if (gc_pool_.size()) {
-      obj = gc_pool_.back();
-      gc_pool_.pop_back();
-    }
-
-    if constexpr (sync) {
-      gc_pool_lock_.unlock();
-    }
-
-    return obj;
-  }
-
-  void gc_remove_all(std::function<void(T)> deallocator) {
-    std::lock_guard<std::mutex> lg(gc_pool_lock_);
-
-    for (T : gc_pool_) {
-      deallocator(gc_pool_);
-    }
-    gc_pool_.clear();
-  }
-
- private:
-  std::unordered_map<T, RefCount> counts_;
-  std::vector<T> gc_pool_;
-  std::mutex gc_pool_lock_;
-};
 
 struct SpirvCodeView {
   const uint32_t *data = nullptr;

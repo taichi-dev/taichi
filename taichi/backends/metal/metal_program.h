f840eea72a7143667a1e0ee45e7129ee989d7b59
Ailing Zhang <ailing@taichi.graphics>
2021-09-01 09:27:28 -0400

    Update base for Update on "[refactor] Unify llvm_program_ and metal_program_ in Program class."
    [ghstack-poisoned]

--- taichi/backends/metal/metal_program.h
+++ taichi/backends/metal/metal_program.h
@@ -14,10 +14,8 @@ namespace taichi {
 namespace lang {
 class MetalProgramImpl : public ProgramImpl {
  public:
-  // CompileConfig config;
   MetalProgramImpl(CompileConfig &config);
   FunctionType compile(Kernel *kernel, OffloadedStmt *offloaded) override;
-  // TODO: materialize_runtime
 
   std::size_t get_snode_num_dynamically_allocated(
       SNode *snode,

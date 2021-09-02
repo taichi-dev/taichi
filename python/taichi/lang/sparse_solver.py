87e91ce573fdc308837b426a1948fd96d4467445
FantasyVR <y2505418927@gmail.com>
2021-09-02 18:09:46 +0800

    M1 support

--- python/taichi/lang/sparse_solver.py
+++ python/taichi/lang/sparse_solver.py
@@ -8,7 +8,7 @@ class SparseSolver:
             from taichi.core.util import ti_core as _ti_core
             from taichi.lang.impl import get_runtime
             taichi_arch = get_runtime().prog.config.arch
-            assert taichi_arch == _ti_core.Arch.x64, f"{taichi_arch}: SparseSolver only supports CPU for now."
+            assert taichi_arch == _ti_core.Arch.x64 or taichi_arch == _ti_core.Arch.arm64, "SparseSolver only supports CPU for now."
             self.solver = _ti_core.get_sparse_solver(solver_type)
         else:
             assert False, f"The solver type {solver_type} is not support for now. Only {solver_type_list} are supported."

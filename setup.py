fa68168a70ce4318479345ffad005e2884ff5cb1
Jiasheng Zhang <leonz5288@hotmail.com>
2021-09-02 14:07:26 +0800

    [release] v0.7.31 (#2867)

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ classifiers = [
 project_name = os.getenv('PROJECT_NAME', 'taichi')
 TI_VERSION_MAJOR = 0
 TI_VERSION_MINOR = 7
-TI_VERSION_PATCH = 30
+TI_VERSION_PATCH = 31
 version = f'{TI_VERSION_MAJOR}.{TI_VERSION_MINOR}.{TI_VERSION_PATCH}'
 
 data_files = glob.glob('python/lib/*')

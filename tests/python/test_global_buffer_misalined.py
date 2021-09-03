05c6bf372a211cfb15815be5afd4975cc5eb5860
Taichi Gardener <taichigardener@gmail.com>
2021-09-03 07:40:41 +0000

    Auto Format

--- tests/python/test_global_buffer_misalined.py
+++ tests/python/test_global_buffer_misalined.py
@@ -1,12 +1,12 @@
 import taichi as ti
 
+
 @ti.test(require=ti.extension.data64)
 def test_global_buffer_misalignment():
-
     @ti.kernel
-    def test(x:ti.f32):
+    def test(x: ti.f32):
         a = x
-        b = ti.cast(0.12,ti.f64)
+        b = ti.cast(0.12, ti.f64)
         for i in range(8):
             b += a
 

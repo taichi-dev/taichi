d1ecb56dcec37cfd22da0754c82f30895be5cdcb
Bo Qiao <boqiao@taichi.graphics>
2021-09-06 10:18:39 +0800

    Re-enable sfg test on CUDA (#2874)

--- tests/python/test_sfg.py
+++ tests/python/test_sfg.py
@@ -4,9 +4,7 @@ import pytest
 import taichi as ti
 
 
-# TODO: #2847
 @ti.test(require=[ti.extension.async_mode, ti.extension.sparse],
-         exclude=[ti.cuda],
          async_mode=True)
 def test_remove_clear_list_from_fused_serial():
     x = ti.field(ti.i32)

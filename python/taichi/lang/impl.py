801721bab250f897d7e55f71d27eddf67e2af4d6
g1n0st <g1n0st@live.com>
2021-09-07 10:10:52 +0800

    [Mesh] Clean up field template based residual & fix bugs
    * wip
    
    * auto format

--- python/taichi/lang/impl.py
+++ python/taichi/lang/impl.py
@@ -344,7 +344,7 @@ class PyTaichi:
             if _var.ptr.snode() is None:
                 tb = getattr(_var, 'declaration_tb', str(_var.ptr))
                 not_placed.append(tb)
-        '''
+
         if len(not_placed):
             bar = '=' * 44 + '\n'
             raise RuntimeError(
@@ -352,7 +352,6 @@ class PyTaichi:
                 f'{bar}'.join(not_placed) +
                 f'{bar}Please consider specifying a shape for them. E.g.,' +
                 '\n\n  x = ti.field(float, shape=(2, 3))')
-        '''
 
         for callback in self.materialize_callbacks:
             callback()
@@ -623,7 +622,7 @@ def field(dtype, shape=None, name="", offset=None, needs_grad=False):
     del _taichi_skip_traceback
 
     x, x_grad = create_field_member(dtype, name)
-    x, x_grad = ScalarField(x, dtype), ScalarField(x_grad, dtype)
+    x, x_grad = ScalarField(x), ScalarField(x_grad)
     x.set_grad(x_grad)
 
     if shape is not None:

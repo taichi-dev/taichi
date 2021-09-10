6d1dfe6ce6ef9d66500e3579ac575d4cf6970342
FantasyVR <y2505418927@gmail.com>
2021-09-10 18:20:22 +0800

    Add anonymous namespace and move pair_hash to top

--- taichi/program/sparse_solver.cpp
+++ taichi/program/sparse_solver.cpp
@@ -1,5 +1,27 @@
 #include "sparse_solver.h"
+
 #include <unordered_map>
+
+#define MAKE_SOLVER(type, order)                                              \
+  {                                                                           \
+    {#type, #order}, []() -> std::unique_ptr<SparseSolver> {                  \
+      using T =                                                               \
+          Eigen::Simplicial##type<Eigen::SparseMatrix<float32>, Eigen::Lower, \
+                                  Eigen::order##Ordering<int>>;               \
+      return std::make_unique<EigenSparseSolver<T>>();                        \
+    }                                                                         \
+  }
+
+namespace {
+struct pair_hash {
+  template <class T1, class T2>
+  std::size_t operator()(const std::pair<T1, T2> &p) const {
+    auto h1 = std::hash<T1>{}(p.first);
+    auto h2 = std::hash<T2>{}(p.second);
+    return h1 ^ h2;
+  }
+};
+}  // namespace
 
 namespace taichi {
 namespace lang {
@@ -33,35 +55,17 @@ bool EigenSparseSolver<EigenSolver>::info() {
   return solver_.info() == Eigen::Success;
 }
 
-#define MAKE_SOLVER(type, order)                                              \
-  {                                                                           \
-    {#type, #order}, []() -> std::unique_ptr<SparseSolver> {                  \
-      using T =                                                               \
-          Eigen::Simplicial##type<Eigen::SparseMatrix<float32>, Eigen::Lower, \
-                                  Eigen::order##Ordering<int>>;               \
-      return std::make_unique<EigenSparseSolver<T>>();                        \
-    }                                                                         \
-  }
-
-struct pair_hash {
-  template <class T1, class T2>
-  std::size_t operator()(const std::pair<T1, T2> &p) const {
-    auto h1 = std::hash<T1>{}(p.first);
-    auto h2 = std::hash<T2>{}(p.second);
-    return h1 ^ h2;
-  }
-};
-
 std::unique_ptr<SparseSolver> make_sparse_solver(const std::string &solver_type,
                                                  const std::string &ordering) {
   using key_type = std::pair<std::string, std::string>;
   using func_type = std::unique_ptr<SparseSolver> (*)();
-  static const std::unordered_map<key_type, func_type, pair_hash> solver_factory = {
-      MAKE_SOLVER(LLT, AMD),
-      MAKE_SOLVER(LLT, COLAMD),
-      MAKE_SOLVER(LDLT, AMD),
-      MAKE_SOLVER(LDLT, COLAMD),
-  };
+  static const std::unordered_map<key_type, func_type, pair_hash>
+      solver_factory = {
+          MAKE_SOLVER(LLT, AMD),
+          MAKE_SOLVER(LLT, COLAMD),
+          MAKE_SOLVER(LDLT, AMD),
+          MAKE_SOLVER(LDLT, COLAMD),
+      };
   std::pair<std::string, std::string> solver_key =
       std::make_pair(solver_type, ordering);
 

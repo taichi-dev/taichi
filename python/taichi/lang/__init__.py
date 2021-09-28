2091b587dcc7b50bd0824491d6d2da9b30569445
Ye Kuang <k-ye@users.noreply.github.com>
2021-09-28 17:54:45 +0800

    Update python/taichi/lang/__init__.py
    Co-authored-by: Yi Xu <xy_xuyi@foxmail.com>

--- python/taichi/lang/__init__.py
+++ python/taichi/lang/__init__.py
@@ -334,7 +334,7 @@ def init(arch=None,
             * ``cpu_max_num_threads`` (int): Sets the number of threads used by the CPU thread pool.
             * ``debug`` (bool): Enables the debug mode, under which Taichi does a few more things like boundary checks.
             * ``print_ir`` (bool): Prints the CHI IR of the Taichi kernels
-            * ``packed`` (bool): Enables the packed memory layout. See https://docs.taichi.graphics/docs/lang/articles/advanced/layout
+            * ``packed`` (bool): Enables the packed memory layout. See https://docs.taichi.graphics/docs/lang/articles/advanced/layout.
     """
     # Make a deepcopy in case these args reference to items from ti.cfg, which are
     # actually references. If no copy is made and the args are indeed references,

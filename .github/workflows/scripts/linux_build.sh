d5efaced1c9ba9124fa645b97747569368ca5ccd
Leon <leonz5288@hotmail.com>
2021-09-28 16:11:51 +0800

    [ci] Run shell scripts in CI

---
+++ .github/workflows/scripts/linux_build.sh
@@ -0,0 +1,9 @@
+python3 -m pip uninstall taichi -y
+python3 -m pip install -r requirements_dev.txt
+cd python
+git fetch origin master
+TAICHI_CMAKE_ARGS=$CI_SETUP_CMAKE_ARGS python3 build.py build
+cd ..
+export NUM_WHL=`ls dist/*.whl | wc -l`
+if [ $NUM_WHL -ne 1 ]; then echo `ERROR: created more than 1 whl.` && exit 1; fi
+python3 -m pip install dist/*.whl

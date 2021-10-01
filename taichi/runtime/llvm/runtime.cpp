20747ab7cc5b6244e8c84b908fa10e476c6c3a98
Ye Kuang <yekuang.ky@gmail.com>
2021-10-01 23:35:55 +0800

    print CUDA info

--- taichi/runtime/llvm/runtime.cpp
+++ taichi/runtime/llvm/runtime.cpp
@@ -798,6 +798,9 @@ Ptr LLVMRuntime::allocate_from_buffer(std::size_t size, std::size_t alignment) {
   });
   if (!success) {
 #if ARCH_cuda
+    taichi_printf(this, "OOM! preallocated {head=%lld tail=%lld} size=%lld\n",
+                  (intptr_t)preallocated_head, (intptr_t)preallocated_tail,
+                  size);
     // Here unfortunately we have to rely on a native CUDA assert failure to
     // halt the whole grid. Using a taichi_assert_runtime will not finish the
     // whole kernel execution immediately.

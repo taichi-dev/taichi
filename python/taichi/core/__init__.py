6bdf14bc1f8eb48723567569e21f99de65684267
Ailing Zhang <ailing@taichi.graphics>
2021-10-01 19:16:16 +0800

    [refactor] Move primitive_types.py to types folder.
    [ghstack-poisoned]

--- python/taichi/core/__init__.py
+++ python/taichi/core/__init__.py
@@ -1,6 +1,9 @@
-from taichi.core.primitive_types import *
 from taichi.core.settings import get_os_name
 from taichi.core.util import *
+# TODO: move this to taichi/__init__.py.
+#       This is blocked since we currently require import this before taichi.lang
+#       but yapf refuse to give up formatting there.
+from taichi.types import *
 
 ti_core.build = build
 ti_core.load_module = load_module
---
+++ python/taichi/types/__init__.py
@@ -0,0 +1,1 @@
+from .primitive_types import *

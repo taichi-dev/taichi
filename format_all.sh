fa3e0162183c694713edd0c898011a63cffb9dde
Cheng (Bob) Cao <bobcaocheng@gmail.com>
2021-10-02 01:34:46 -0700

    Add Clang-tidy formatter

---
+++ format_all.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+rm -rf build_clang_tidy
+mkdir -p build_clang_tidy/
+cd build_clang_tidy
+cmake .. -DCMAKE_CXX_COMPILER=clang -DCMAKE_C_COMPILER=clang -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+cd ..
+python3 run-clang-tidy.py $PWD/taichi -header-filter="$PWD/taichi/" -p build_clang_tidy -j16 -fix
+
+find taichi -name \*.cpp -o -name \*.cu -exec clang-format -i {} \;
+find taichi -name \*.h -exec clang-format -i {} \;

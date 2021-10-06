99d3a40adf1e7aa0b8fbe281499a9bcd2ddec8bc
Taichi Gardener <taichigardener@gmail.com>
2021-10-06 17:57:25 +0000

    Auto Format

--- tests/python/test_packed_size.py
+++ tests/python/test_packed_size.py
@@ -6,4 +6,4 @@ def test_packed_size():
     x = ti.field(ti.i32)
     ti.root.dense(ti.i, 17).dense(ti.ijk, 129).place(x)
     assert x.shape == (17 * 129, 129, 129)
-    assert x.snode.parent().parent().cell_size_bytes == 4 * 129 ** 3
+    assert x.snode.parent().parent().cell_size_bytes == 4 * 129**3

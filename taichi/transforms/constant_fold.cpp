1e0caab0c4103da23f6ddc1537ccab6ce21976e4
Cheng Cao <bobcaocheng@gmail.com>
2021-10-06 03:55:04 -0700

    draft: batched constant folding JIT evaluator, and somehow this JIT evaluator blows up Vulkan codegen

--- taichi/transforms/constant_fold.cpp
+++ taichi/transforms/constant_fold.cpp
@@ -2,6 +2,8 @@
 #include <deque>
 #include <set>
 #include <thread>
+#include <unordered_map>
+#include <unordered_set>
 
 #include "taichi/ir/ir.h"
 #include "taichi/ir/snode.h"
@@ -23,49 +25,158 @@ class ConstantFold : public BasicStmtVisitor {
       : BasicStmtVisitor(), program(program) {
   }
 
-  Kernel *get_jit_evaluator_kernel(JITEvaluatorId const &id) {
-    auto &cache = program->jit_evaluator_cache;
-    // Discussion:
-    // https://github.com/taichi-dev/taichi/pull/954#discussion_r423442606
-    std::lock_guard<std::mutex> _(program->jit_evaluator_cache_mut);
-    auto it = cache.find(id);
-    if (it != cache.end())  // cached?
-      return it->second.get();
+  struct deferred_jit_eval {
+    int op;
+    bool is_binary;
+    const TypedConstant &lhs;
+    const TypedConstant &rhs;
+    DataType ret_type;
+    Stmt *replaced_stmt;
+  };
 
-    auto kernel_name = fmt::format("jit_evaluator_{}", cache.size());
-    auto func = [&id]() {
-      auto lhstmt =
-          Stmt::make<ArgLoadStmt>(/*arg_id=*/0, id.lhs, /*is_ptr=*/false);
-      auto rhstmt =
-          Stmt::make<ArgLoadStmt>(/*arg_id=*/1, id.rhs, /*is_ptr=*/false);
-      pStmt oper;
-      if (id.is_binary) {
-        oper = Stmt::make<BinaryOpStmt>(id.binary_op(), lhstmt.get(),
-                                        rhstmt.get());
-      } else {
-        oper = Stmt::make<UnaryOpStmt>(id.unary_op(), lhstmt.get());
-        if (unary_op_is_cast(id.unary_op())) {
-          oper->cast<UnaryOpStmt>()->cast_type = id.rhs;
-        }
+  std::vector<deferred_jit_eval> batched_eval;
+
+  static int get_i64_multiplier(DataType dt) {
+    if (dt == PrimitiveType::i8) {
+      return 8;
+    } else if (dt == PrimitiveType::u8) {
+      return 8;
+    } else if (dt == PrimitiveType::i16) {
+      return 4;
+    } else if (dt == PrimitiveType::u16) {
+      return 4;
+    } else if (dt == PrimitiveType::f16) {
+      return 4;
+    } else if (dt == PrimitiveType::i32) {
+      return 2;
+    } else if (dt == PrimitiveType::u32) {
+      return 2;
+    } else if (dt == PrimitiveType::f32) {
+      return 2;
+    } else {
+      return 1;
+    }
+  }
+
+  struct DataTypeHasher {
+    size_t operator()(const DataType &dt) const {
+      return dt.hash();
+    }
+  };
+
+  void evaluate_batched(size_t eval_kernel_id) {
+    if (!batched_eval.size())
+      return;
+
+    auto kernel_name = fmt::format("jit_evaluator_{}", eval_kernel_id);
+
+    auto func = [&]() {
+      std::unordered_map<DataType, Stmt *, DataTypeHasher> ext_ptr_arg_stmts;
+      std::unordered_set<DataType, DataTypeHasher> dtypes;
+      for (auto &e : batched_eval) {
+        dtypes.insert(e.ret_type);
+        dtypes.insert(e.lhs.dt);
+        dtypes.insert(e.rhs.dt);
       }
-      auto ret = Stmt::make<ReturnStmt>(oper.get());
-      current_ast_builder().insert(std::move(lhstmt));
-      if (id.is_binary)
-        current_ast_builder().insert(std::move(rhstmt));
-      current_ast_builder().insert(std::move(oper));
-      current_ast_builder().insert(std::move(ret));
+      for (auto dt : dtypes) {
+        auto ext_ptr_arg_stmt_unique =
+            Stmt::make<ArgLoadStmt>(0, dt, /*is_ptr=*/true);
+        ext_ptr_arg_stmts[dt] = ext_ptr_arg_stmt_unique.get();
+        current_ast_builder().insert(std::move(ext_ptr_arg_stmt_unique));
+      }
+
+      int param_count = 0;
+      for (auto &e : batched_eval) {
+        auto ret_index_stmt =
+            Stmt::make<ConstStmt>(LaneAttribute<TypedConstant>(
+                param_count * get_i64_multiplier(e.ret_type)));
+        auto lhs_index_stmt =
+            Stmt::make<ConstStmt>(LaneAttribute<TypedConstant>(
+                (param_count++) * get_i64_multiplier(e.lhs.dt)));
+        auto rhs_index_stmt =
+            Stmt::make<ConstStmt>(LaneAttribute<TypedConstant>(
+                (param_count++) * get_i64_multiplier(e.rhs.dt)));
+
+        auto ret_ptr_stmt = Stmt::make<ExternalPtrStmt>(
+            LaneAttribute(ext_ptr_arg_stmts.at(e.ret_type)),
+            std::vector<Stmt *>{ret_index_stmt.get()});
+        auto lhs_ptr_stmt = Stmt::make<ExternalPtrStmt>(
+            LaneAttribute(ext_ptr_arg_stmts.at(e.lhs.dt)),
+            std::vector<Stmt *>{lhs_index_stmt.get()});
+        auto rhs_ptr_stmt = Stmt::make<ExternalPtrStmt>(
+            LaneAttribute(ext_ptr_arg_stmts.at(e.rhs.dt)),
+            std::vector<Stmt *>{rhs_index_stmt.get()});
+
+        auto lhstmt =
+            Stmt::make_typed<GlobalLoadStmt>(lhs_ptr_stmt.get());
+        auto rhstmt =
+            Stmt::make_typed<GlobalLoadStmt>(rhs_ptr_stmt.get());
+
+        pStmt oper;
+        if (e.is_binary) {
+          oper = Stmt::make<BinaryOpStmt>((BinaryOpType)e.op, lhstmt.get(),
+                                          rhstmt.get());
+        } else {
+          oper = Stmt::make<UnaryOpStmt>((UnaryOpType)e.op, lhstmt.get());
+          if (unary_op_is_cast((UnaryOpType)e.op)) {
+            oper->cast<UnaryOpStmt>()->cast_type = e.ret_type;
+          }
+        }
+        auto writeback =
+            Stmt::make<GlobalStoreStmt>(ret_ptr_stmt.get(), oper.get());
+
+        current_ast_builder().insert(std::move(ret_index_stmt));
+        current_ast_builder().insert(std::move(ret_ptr_stmt));
+        current_ast_builder().insert(std::move(lhs_index_stmt));
+        current_ast_builder().insert(std::move(lhs_ptr_stmt));
+        current_ast_builder().insert(std::move(lhstmt));
+        if (e.is_binary) {
+          current_ast_builder().insert(std::move(rhs_index_stmt));
+          current_ast_builder().insert(std::move(rhs_ptr_stmt));
+          current_ast_builder().insert(std::move(rhstmt));
+        }
+        current_ast_builder().insert(std::move(oper));
+        current_ast_builder().insert(std::move(writeback));
+      }
     };
+
     auto ker = std::make_unique<Kernel>(*program, func, kernel_name);
-    ker->insert_ret(id.ret);
-    ker->insert_arg(id.lhs, false);
-    if (id.is_binary)
-      ker->insert_arg(id.rhs, false);
+    ker->insert_arg(PrimitiveType::i32, true);
     ker->is_evaluator = true;
-    auto *ker_ptr = ker.get();
-    TI_TRACE("Saving JIT evaluator cache entry id={}",
-             std::hash<JITEvaluatorId>{}(id));
-    cache[id] = std::move(ker);
-    return ker_ptr;
+
+    std::string output;
+    irpass::print(ker->ir.get(), &output);
+    std::cout << output << std::flush;
+
+    std::vector<int64> ext_arr(batched_eval.size() * 2);
+
+    int i = 0;
+    for (auto &e : batched_eval) {
+      ext_arr[i] = e.lhs.val_i64;
+      if (e.is_binary)
+        ext_arr[i + 1] = e.rhs.val_i64;
+      i += 2;
+    }
+
+    auto launch_ctx = ker->make_launch_context();
+    launch_ctx.set_arg_external_array(0, uint64(ext_arr.data()),
+                                      uint64(ext_arr.size() * sizeof(int64_t)));
+    (*ker)(launch_ctx);
+
+    i = 0;
+    for (auto &e : batched_eval) {
+      TypedConstant new_constant(e.ret_type);
+      new_constant.val_i64 = ext_arr[i];
+      TI_TRACE("{} result = {}", e.replaced_stmt->id, new_constant.val_cast_to_float64());
+      auto evaluated =
+          Stmt::make<ConstStmt>(LaneAttribute<TypedConstant>(new_constant));
+      e.replaced_stmt->replace_with(evaluated.get());
+      modifier.insert_before(e.replaced_stmt, std::move(evaluated));
+      modifier.erase(e.replaced_stmt);
+      i += 2;
+    }
+
+    batched_eval.clear();
   }
 
   static bool is_good_type(DataType dt) {
@@ -83,49 +194,31 @@ class ConstantFold : public BasicStmtVisitor {
       return false;
   }
 
-  bool jit_evaluate_binary_op(TypedConstant &ret,
+  bool jit_evaluate_binary_op(DataType ret_type,
                               BinaryOpStmt *stmt,
                               const TypedConstant &lhs,
                               const TypedConstant &rhs) {
-    if (!is_good_type(ret.dt))
+    if (!is_good_type(ret_type))
       return false;
-    JITEvaluatorId id{std::this_thread::get_id(),
-                      (int)stmt->op_type,
-                      ret.dt,
-                      lhs.dt,
-                      rhs.dt,
-                      true};
-    auto *ker = get_jit_evaluator_kernel(id);
-    auto launch_ctx = ker->make_launch_context();
-    launch_ctx.set_arg_raw(0, lhs.val_u64);
-    launch_ctx.set_arg_raw(1, rhs.val_u64);
-    {
-      std::lock_guard<std::mutex> _(program->jit_evaluator_cache_mut);
-      (*ker)(launch_ctx);
-      ret.val_i64 = program->fetch_result<int64_t>(0);
-    }
+
+    TI_TRACE("JIT Binary {} {} {}", stmt->id, lhs.val_cast_to_float64(),
+             rhs.val_cast_to_float64());
+    batched_eval.push_back(
+        {(int)stmt->op_type, true, lhs, rhs, ret_type, stmt});
+
     return true;
   }
 
-  bool jit_evaluate_unary_op(TypedConstant &ret,
+  bool jit_evaluate_unary_op(DataType ret_type,
                              UnaryOpStmt *stmt,
                              const TypedConstant &operand) {
-    if (!is_good_type(ret.dt))
+    if (!is_good_type(ret_type))
       return false;
-    JITEvaluatorId id{std::this_thread::get_id(),
-                      (int)stmt->op_type,
-                      ret.dt,
-                      operand.dt,
-                      stmt->cast_type,
-                      false};
-    auto *ker = get_jit_evaluator_kernel(id);
-    auto launch_ctx = ker->make_launch_context();
-    launch_ctx.set_arg_raw(0, operand.val_u64);
-    {
-      std::lock_guard<std::mutex> _(program->jit_evaluator_cache_mut);
-      (*ker)(launch_ctx);
-      ret.val_i64 = program->fetch_result<int64_t>(0);
-    }
+
+    TI_TRACE("JIT Unary {} {}", stmt->id, operand.val_cast_to_float64());
+    batched_eval.push_back(
+        {(int)stmt->op_type, false, operand, operand, ret_type, stmt});
+
     return true;
   }
 
@@ -137,14 +230,7 @@ class ConstantFold : public BasicStmtVisitor {
     if (stmt->width() != 1)
       return;
     auto dst_type = stmt->ret_type;
-    TypedConstant new_constant(dst_type);
-    if (jit_evaluate_binary_op(new_constant, stmt, lhs->val[0], rhs->val[0])) {
-      auto evaluated =
-          Stmt::make<ConstStmt>(LaneAttribute<TypedConstant>(new_constant));
-      stmt->replace_with(evaluated.get());
-      modifier.insert_before(stmt, std::move(evaluated));
-      modifier.erase(stmt);
-    }
+    jit_evaluate_binary_op(dst_type, stmt, lhs->val[0], rhs->val[0]);
   }
 
   void visit(UnaryOpStmt *stmt) override {
@@ -159,14 +245,7 @@ class ConstantFold : public BasicStmtVisitor {
     if (stmt->width() != 1)
       return;
     auto dst_type = stmt->ret_type;
-    TypedConstant new_constant(dst_type);
-    if (jit_evaluate_unary_op(new_constant, stmt, operand->val[0])) {
-      auto evaluated =
-          Stmt::make<ConstStmt>(LaneAttribute<TypedConstant>(new_constant));
-      stmt->replace_with(evaluated.get());
-      modifier.insert_before(stmt, std::move(evaluated));
-      modifier.erase(stmt);
-    }
+    jit_evaluate_unary_op(dst_type, stmt, operand->val[0]);
   }
 
   void visit(BitExtractStmt *stmt) override {
@@ -197,6 +276,7 @@ class ConstantFold : public BasicStmtVisitor {
     bool modified = false;
     while (true) {
       node->accept(&folder);
+      folder.evaluate_batched(0);
       if (folder.modifier.modify_ir()) {
         modified = true;
       } else {

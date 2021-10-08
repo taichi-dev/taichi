8f8643d48609a3c07675b422c15fd36014286c41
Jiasheng Zhang <leonz5288@hotmail.com>
2021-10-08 17:53:20 +0800

    [ci] Run shell scripts in CI (#3034)
    * [ci] Run shell scripts in CI
    
    * post submit file
    
    * set env when running gpu machine
    
    * unify m1 build

---
+++ .github/workflows/scripts/unix_test.sh
@@ -0,0 +1,14 @@
+TAICHI_REPO_DIR=`pwd`
+TI_LIB_DIR=`python3 -c "import taichi;print(taichi.__path__[0])" | tail -1`
+TI_LIB_DIR="$TI_LIB_DIR/lib" ./build/taichi_cpp_tests
+export PATH=$TAICHI_REPO_DIR/taichi-llvm/bin/:$PATH
+## Only GPU machine uses system python.
+[ -z $GPU_TEST ] || export PATH=$PATH:$HOME/.local/bin
+hash -r
+python3 examples/algorithm/laplace.py
+ti diagnose
+ti changelog
+[ -z $GPU_TEST ] && ti test -vr2 -t2
+
+[ -z $GPU_TEST ] || ti test -vr2 -t2 -k "not ndarray"
+[ -z $GPU_TEST ] || ti test -vr2 -t1 -k "ndarray"

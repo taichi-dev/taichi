7294301a3076893623a4706caa87841b1712d752
lin-hitonami <90667349+lin-hitonami@users.noreply.github.com>
2021-10-08 22:12:12 +0800

    [Bug] Fix argument shadowed when there is a local variable with the same name (#3105)
    * [Bug] Fix argument shadowed when there is a local variable with the same name
    
    * add tests
    
    * add error reporting
    
    * use f-string

--- tests/python/test_kernel_arg_errors.py
+++ tests/python/test_kernel_arg_errors.py
@@ -15,3 +15,16 @@ def test_pass_float_as_i32():
     assert e.type is ti.KernelArgError
     assert e.value.args[
         0] == "Argument 0 (type=<class 'float'>) cannot be converted into required type i32"
+
+
+@ti.test(arch=ti.cpu)
+def test_argument_redefinition():
+    @ti.kernel
+    def foo(a: ti.i32):
+        a = 1
+
+    with pytest.raises(ti.TaichiSyntaxError) as e:
+        foo(5)
+
+    assert e.value.args[
+        0] == "Kernel argument \"a\" cannot be redefined in the kernel"

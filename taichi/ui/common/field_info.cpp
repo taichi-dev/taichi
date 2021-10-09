e50c593142514857b1353a6c8efde5d1c230a125
unknown <ldfrank533@outlook.com>
2021-10-09 03:26:01 +0100

    format

--- taichi/ui/common/field_info.cpp
+++ taichi/ui/common/field_info.cpp
@@ -10,14 +10,14 @@ using namespace taichi::lang;
 DevicePtr get_device_ptr(taichi::lang::Program *program, SNode *snode) {
   /*
   GGUI makes the assumption that the input fields are created directly from
-  ti.field() or ti.Vector field with `shape` specified. In other words, we assume that the fields are
-  created via ti.root.dense.place() That is, the parent of the snode is a dense,
-  and the parent of that node is a root. Note that, GGUI's python-side code
-  creates a staging buffer to construct the VBO, which obeys this assumption.
-  Thus, the only situation where this assumption may be violated is for
-  set_image(), because the image isn't part of the VBO. Using this assumption,
-  we will compute the offset of this field relative to the begin of the root
-  buffer.
+  ti.field() or ti.Vector field with `shape` specified. In other words, we
+  assume that the fields are created via ti.root.dense.place() That is, the
+  parent of the snode is a dense, and the parent of that node is a root. Note
+  that, GGUI's python-side code creates a staging buffer to construct the VBO,
+  which obeys this assumption. Thus, the only situation where this assumption
+  may be violated is for set_image(), because the image isn't part of the VBO.
+  Using this assumption, we will compute the offset of this field relative to
+  the begin of the root buffer.
   */
 
   SNode *dense_parent = snode->parent;

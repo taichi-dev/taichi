98531ce1851cb85903479c850254c515fde8e1a8
Ye Kuang <yekuang.ky@gmail.com>
2021-10-09 10:26:25 +0800

    enabel GL

--- cmake/TaichiCore.cmake
+++ cmake/TaichiCore.cmake
@@ -1,7 +1,7 @@
 option(USE_STDCPP "Use -stdlib=libc++" OFF)
 option(TI_WITH_CUDA "Build with the CUDA backend" ON)
 option(TI_WITH_CUDA_TOOLKIT "Build with the CUDA toolkit" OFF)
-option(TI_WITH_OPENGL "Build with the OpenGL backend" OFF)
+option(TI_WITH_OPENGL "Build with the OpenGL backend" ON)
 option(TI_WITH_CC "Build with the C backend" OFF)
 option(TI_WITH_VULKAN "Build with the Vulkan backend" OFF)
 

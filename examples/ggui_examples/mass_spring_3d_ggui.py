415332557f2704c1cbb6769b8fccb2e706991b6c
unknown <ldfrank533@outlook.com>
2021-10-11 02:36:05 +0100

    fix

--- examples/ggui_examples/mass_spring_3d_ggui.py
+++ examples/ggui_examples/mass_spring_3d_ggui.py
@@ -1,25 +1,16 @@
-import sys
-
-import numpy as np
-
 import taichi as ti
 
-ti.init(arch=ti.cuda)
-
-### Parameters
+ti.init(arch=ti.cuda)  # Alternatively, ti.init(arch=ti.cpu)
 
 N = 128
-W = 1
-L = W / N
+cell_size = 1.0 / N
 gravity = 0.5
 stiffness = 1600
 damping = 2
-steps = 30
 dt = 5e-4
 
-num_balls = 1
-ball_radius = 0.3
-ball_centers = ti.Vector.field(3, float, num_balls)
+ball_radius = 0.2
+ball_center = ti.Vector.field(3, float, (1, ))
 
 x = ti.Vector.field(3, float, (N, N))
 v = ti.Vector.field(3, float, (N, N))
@@ -29,70 +20,64 @@ indices = ti.field(int, num_triangles * 3)
 vertices = ti.Vector.field(3, float, N * N)
 
 
-@ti.kernel
-def init():
+def init_scene():
     for i, j in ti.ndrange(N, N):
-        x[i, j] = ti.Vector([(i + 0.5) * L - 0.5, (j + 0.5) * L / ti.sqrt(2),
-                             (N - j) * L / ti.sqrt(2)])
-
-        if i < N - 1 and j < N - 1:
-            tri_id = ((i * (N - 1)) + j) * 2
-            indices[tri_id * 3] = i * N + j
-            indices[tri_id * 3 + 1] = (i + 1) * N + j
-            indices[tri_id * 3 + 2] = i * N + (j + 1)
-
-            tri_id += 1
-            indices[tri_id * 3] = (i + 1) * N + j + 1
-            indices[tri_id * 3 + 1] = i * N + (j + 1)
-            indices[tri_id * 3 + 2] = (i + 1) * N + j
-    ball_centers[0] = ti.Vector([0.0, -0.5, -0.0])
-
-
-links = [(-1, 0), (1, 0), (0, -1), (0, 1), (-1, -1), (1, -1), (-1, 1), (1, 1)]
-links = [ti.Vector([*v]) for v in links]
-
-
-@ti.func
-def reflect(v, normal):
-    return v.dot(normal) * 2 - v
-
-
-@ti.func
-def ballBoundReflect(pos, vel, center, radius, bounce_magnitude=0.1):
-    ret = vel
-    distance = (pos - center).norm()
-    if distance <= radius:
-        normal = (pos - radius).normalized()
-        ret = -1 * bounce_magnitude * reflect(vel, normal)
-    return ret
+        x[i, j] = ti.Vector([
+            i * cell_size, j * cell_size / ti.sqrt(2),
+            (N - j) * cell_size / ti.sqrt(2)
+        ])
+    ball_center[0] = ti.Vector([0.5, -0.5, -0.0])
 
 
 @ti.kernel
-def substep():
-    for i in ti.grouped(x):
-        acc = x[i] * 0
-        for d in ti.static(links):
-            disp = x[min(max(i + d, 0), ti.Vector([N - 1, N - 1]))] - x[i]
-            length = L * float(d).norm()
-            acc += disp * (disp.norm() - length) / length**2
-        v[i] += stiffness * acc * dt
+def set_indices():
+    for i, j in ti.ndrange(N, N):
+        if i < N - 1 and j < N - 1:
+            square_id = (i * (N - 1)) + j
+            # 1st triangle of the square
+            indices[square_id * 6 + 0] = i * N + j
+            indices[square_id * 6 + 1] = (i + 1) * N + j
+            indices[square_id * 6 + 2] = i * N + (j + 1)
+            # 2nd triangle of the square
+            indices[square_id * 6 + 3] = (i + 1) * N + j + 1
+            indices[square_id * 6 + 4] = i * N + (j + 1)
+            indices[square_id * 6 + 5] = (i + 1) * N + j
+
+
+links = [[-1, 0], [1, 0], [0, -1], [0, 1], [-1, -1], [1, -1], [-1, 1], [1, 1]]
+links = [ti.Vector(v) for v in links]
+
+
+@ti.kernel
+def step():
     for i in ti.grouped(x):
         v[i].y -= gravity * dt
-        for b in range(num_balls):
-            v[i] = ballBoundReflect(x[i], v[i], ball_centers[b],
-                                    ball_radius * 1.2)
+    for i in ti.grouped(x):
+        force = ti.Vector([0.0, 0.0, 0.0])
+        for d in ti.static(links):
+            j = min(max(i + d, 0), [N - 1, N - 1])
+            relative_pos = x[j] - x[i]
+            current_length = relative_pos.norm()
+            original_length = cell_size * float(i - j).norm()
+            if original_length != 0:
+                force += stiffness * relative_pos.normalized() * (
+                    current_length - original_length) / original_length
+        v[i] += force * dt
     for i in ti.grouped(x):
         v[i] *= ti.exp(-damping * dt)
+        if (x[i] - ball_center[0]).norm() <= ball_radius:
+            v[i] = ti.Vector([0.0, 0.0, 0.0])
         x[i] += dt * v[i]
 
 
 @ti.kernel
-def update_verts():
+def set_vertices():
     for i, j in ti.ndrange(N, N):
         vertices[i * N + j] = x[i, j]
 
 
-init()
+init_scene()
+set_indices()
 
 window = ti.ui.Window("Cloth", (800, 800), vsync=True)
 canvas = window.get_canvas()
@@ -100,18 +85,19 @@ scene = ti.ui.Scene()
 camera = ti.ui.make_camera()
 
 while window.running:
-    update_verts()
+    for i in range(30):
+        step()
+    set_vertices()
 
-    for i in range(steps):
-        substep()
-
-    camera.position(0, 0, 3)
-    camera.lookat(0, 0, 0)
-    camera.up(0, 1, 0)
+    camera.position(0.5, -0.5, 2)
+    camera.lookat(0.5, -0.5, 0)
     scene.set_camera(camera)
 
     scene.point_light(pos=(0.5, 1, 2), color=(1, 1, 1))
-    scene.mesh(vertices, indices=indices, color=(0.5, 0.5, 0.5))
-    scene.particles(ball_centers, radius=ball_radius, color=(0.5, 0, 0))
+    scene.mesh(vertices,
+               indices=indices,
+               color=(0.5, 0.5, 0.5),
+               two_sided=True)
+    scene.particles(ball_center, radius=ball_radius, color=(0.5, 0, 0))
     canvas.scene(scene)
     window.show()

3a8e2b6208e2e9f2d9b7cc501f4b5a5a1ff570ce
Bob Cao <bobcaocheng@gmail.com>
2021-10-16 01:45:38 -0700

    [opengl] OpenGL AOT Module Builder & Making GL CompiledProgram serializable (#3202)
    * Make OpenGL CompiledProgram serialziable & add GL AOT module builder
    
    * finish up aot module builder
    
    * Auto Format
    
    * fix compile
    
    Co-authored-by: Taichi Gardener <taichigardener@gmail.com>

--- taichi/backends/opengl/opengl_program.cpp
+++ taichi/backends/opengl/opengl_program.cpp
@@ -1,4 +1,5 @@
 #include "taichi/backends/opengl/opengl_program.h"
+#include "taichi/backends/opengl/aot_module_builder_impl.h"
 using namespace taichi::lang::opengl;
 
 namespace taichi {
@@ -6,17 +7,29 @@ namespace lang {
 
 FunctionType OpenglProgramImpl::compile(Kernel *kernel,
                                         OffloadedStmt *offloaded) {
+#ifdef TI_WITH_OPENGL
   opengl::OpenglCodeGen codegen(kernel->name, &opengl_struct_compiled_.value(),
                                 opengl_runtime_.get());
-  return codegen.compile(*kernel);
+  auto ptr = opengl_runtime_->keep(codegen.compile(*kernel));
+
+  return [ptr, runtime = opengl_runtime_.get()](Context &ctx) {
+    ptr->launch(ctx, runtime);
+  };
+#else
+  return [](Context &ctx) {};
+#endif
 }
 
 void OpenglProgramImpl::materialize_runtime(MemoryPool *memory_pool,
                                             KernelProfilerBase *profiler,
                                             uint64 **result_buffer_ptr) {
+#ifdef TI_WITH_OPENGL
   *result_buffer_ptr = (uint64 *)memory_pool->allocate(
       sizeof(uint64) * taichi_result_buffer_entries, 8);
   opengl_runtime_ = std::make_unique<opengl::OpenGlRuntime>();
+#else
+  TI_NOT_IMPLEMENTED;
+#endif
 }
 
 void OpenglProgramImpl::materialize_snode_tree(
@@ -24,6 +37,7 @@ void OpenglProgramImpl::materialize_snode_tree(
     std::vector<std::unique_ptr<SNodeTree>> &,
     std::unordered_map<int, SNode *> &,
     uint64 *result_buffer) {
+#ifdef TI_WITH_OPENGL
   // TODO: support materializing multiple snode trees
   auto *const root = tree->root();
   opengl::OpenglStructCompiler scomp;
@@ -31,6 +45,19 @@ void OpenglProgramImpl::materialize_snode_tree(
   TI_TRACE("OpenGL root buffer size: {} B", opengl_struct_compiled_->root_size);
   opengl_runtime_->add_snode_tree(opengl_struct_compiled_->root_size);
   opengl_runtime_->result_buffer = result_buffer;
+#else
+  TI_NOT_IMPLEMENTED;
+#endif
+}
+
+std::unique_ptr<AotModuleBuilder> OpenglProgramImpl::make_aot_module_builder() {
+#ifdef TI_WITH_OPENGL
+  return std::make_unique<AotModuleBuilderImpl>(opengl_struct_compiled_.value(),
+                                                *opengl_runtime_);
+#else
+  TI_NOT_IMPLEMENTED;
+  return nullptr;
+#endif
 }
 
 }  // namespace lang

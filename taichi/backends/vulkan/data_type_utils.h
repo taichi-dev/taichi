6aa2fd140461e58536c317a2503406d5ed507af5
Dunfan Lu <ldfrank533@outlook.com>
2021-10-22 09:22:00 +0100

    [vulkan] Support for ti.u8 in vulkan (#3247)
    * u8
    
    * remove legacy comments

--- taichi/backends/vulkan/data_type_utils.h
+++ taichi/backends/vulkan/data_type_utils.h
@@ -10,9 +10,7 @@ namespace lang {
 namespace vulkan {
 
 inline std::size_t vk_data_type_size(DataType dt) {
-  // Vulkan buffers require a minimum alignment of 4 bytes.
-  // https://vulkan-tutorial.com/Uniform_buffers/Descriptor_pool_and_sets#page_Alignment-requirements
-  return std::max(data_type_size(dt), 4);
+  return data_type_size(dt);
 }
 
 }  // namespace vulkan

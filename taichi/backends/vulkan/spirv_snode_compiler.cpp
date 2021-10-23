d9c3a0d54753a5f79bf05e31043027898cdb0d5b
Cheng Cao <bobcaocheng@gmail.com>
2021-10-23 16:58:33 -0700

    fix atomics

--- taichi/backends/vulkan/spirv_snode_compiler.cpp
+++ taichi/backends/vulkan/spirv_snode_compiler.cpp
@@ -31,8 +31,7 @@ class SpirvSNodeCompiler {
                             SNodeSTypeTbl *snode_id_array_stype_tbl_) {
     const auto &sn = sn_desc.snode;
     if (sn->is_place()) {
-      return ir_->get_primitive_buffer_type(
-          ir_->get_primitive_type_size(sn->dt));
+      return ir_->get_primitive_buffer_type(sn->dt);
     } else {
       SType sn_type = ir_->get_null_type();
       sn_type.snode_desc = sn_desc;

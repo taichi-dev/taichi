bc7f02090d651fa91a23cdca1ac828ea8680a6c2
jiasheng <leonz5288@hotmail.com>
2021-10-25 20:33:04 +0800

    add authentication
    permission and restore docker file

--- .github/workflows/scripts/unix_docker_build.sh
+++ .github/workflows/scripts/unix_docker_build.sh
@@ -4,12 +4,22 @@ set -ex
 
 # Parse ARGs
 PY=$1
-CI_SETUP_CMAKE_ARGS=$2
+GPU_BUILD=$2
+CI_SETUP_CMAKE_ARGS=$3
+
+ls -al
 
 source /home/dev/miniconda/etc/profile.d/conda.sh
 conda activate $PY
 
 cd taichi
+ls -al
+[[ $GPU_BUILD == "OFF" ]] && python3 -m pip install -r requirements_dev.txt
+cd python
 TAICHI_CMAKE_ARGS=$CI_SETUP_CMAKE_ARGS python3 build.py build
 # Run basic cpp tests
-TI_LIB_DIR="$TI_LIB_DIR/lib" ./build/taichi_cpp_tests
+cd ..
+CUR_DIR=`pwd`
+TI_LIB_DIR=$CUR_DIR/python/taichi/lib ./build/taichi_cpp_tests
+
+tail -f /dev/null

624c9088d585771501b6f42d316991e2fc84bdde
jiasheng <leonz5288@hotmail.com>
2021-10-26 20:08:50 +0800

    fix

--- .github/workflows/scripts/unix_docker_build.sh
+++ .github/workflows/scripts/unix_docker_build.sh
@@ -10,6 +10,8 @@ CI_SETUP_CMAKE_ARGS=$3
 source /home/dev/miniconda/etc/profile.d/conda.sh
 conda activate $PY
 
+sudo apt-get install zlib1g-dev
+
 cd taichi
 [[ $GPU_BUILD == "OFF" ]] && python3 -m pip install -r requirements_dev.txt
 cd python
@@ -17,6 +19,6 @@ TAICHI_CMAKE_ARGS=$CI_SETUP_CMAKE_ARGS python3 build.py build
 # Run basic cpp tests
 cd ..
 CUR_DIR=`pwd`
-#TI_LIB_DIR=$CUR_DIR/python/taichi/lib ./build/taichi_cpp_tests
+TI_LIB_DIR=$CUR_DIR/python/taichi/lib ./build/taichi_cpp_tests
 
 cp dist/*.whl /wheel/

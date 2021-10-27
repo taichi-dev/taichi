658b77c75f17720e22b6d4482676330db8f4e82e
Yi Xu <xy_xuyi@foxmail.com>
2021-10-27 22:55:28 +0800

    Remove boilerplate test

--- tests/cpp/ir/frontend_type_inference_test.cpp
+++ tests/cpp/ir/frontend_type_inference_test.cpp
@@ -1,29 +1,10 @@
 #include "gtest/gtest.h"
 
 #include "taichi/ir/frontend_ir.h"
-#include "tests/cpp/program/test_program.h"
+#include "taichi/program/program.h"
 
 namespace taichi {
 namespace lang {
-
-TEST(Type, BitTypesO) {
-  auto i32 = TypeFactory::get_instance()
-                 .get_primitive_type(PrimitiveTypeID::i32)
-                 ->as<PrimitiveType>();
-  auto ci5 = TypeFactory::get_instance().get_custom_int_type(5, true, i32);
-  auto cu11 = TypeFactory::get_instance().get_custom_int_type(11, false, i32);
-  auto u16 = TypeFactory::get_instance().get_primitive_int_type(16, false);
-
-  auto bs =
-      TypeFactory::get_instance().get_bit_struct_type(u16, {ci5, cu11}, {0, 5});
-
-  EXPECT_EQ(bs->to_string(), "bs(ci5@0, cu11@5)");
-
-  auto ci1 = TypeFactory::get_instance().get_custom_int_type(1, true, i32);
-  auto ba = TypeFactory::get_instance().get_bit_array_type(i32, ci1, 32);
-
-  EXPECT_EQ(ba->to_string(), "ba(ci1x32)");
-}
 
 TEST(FrontendTypeInference, Const) {
   auto const_i64 = Expr::make<ConstExpression, int64>(1LL << 63);

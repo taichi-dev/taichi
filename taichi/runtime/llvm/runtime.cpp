f7a40b5fc2e5b70fb67a931e8969671a14c75679
FantasyVR <6712304+FantasyVR@users.noreply.github.com>
2021-10-28 17:22:14 +0800

    [Bug] Add missing integral datatype support for ti.min/ti.max (#3248)
    * [Bug] Support ti.min with ti.u16 datatype
    
    * Auto Format
    
    * Update taichi/codegen/codegen_llvm.cpp
    
    Co-authored-by: Yi Xu <xy_xuyi@foxmail.com>
    
    * support ti.min and ti.max with datatype u16, i16, u32
    
    * Correct misspelled word
    
    * Exclude ti.min test on Vulkan
    
    * bug fix
    
    * Exclude opengl u16
    
    * simplify code
    
    * Remove cc backbend bug
    
    * To re-test
    
    * Improve
    
    * Remove duplicate test
    
    * Apply suggestions from code review
    
    Co-authored-by: Yi Xu <xy_xuyi@foxmail.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Yi Xu <xy_xuyi@foxmail.com>
    
    * Test with different type
    
    * Update tests/python/test_scalar_op.py
    
    Co-authored-by: Yi Xu <xy_xuyi@foxmail.com>
    
    Co-authored-by: Taichi Gardener <taichigardener@gmail.com>
    Co-authored-by: Yi Xu <xy_xuyi@foxmail.com>

--- taichi/runtime/llvm/runtime.cpp
+++ taichi/runtime/llvm/runtime.cpp
@@ -214,27 +214,51 @@ i64 floordiv_i64(i64 a, i64 b) {
   return ifloordiv(a, b);
 }
 
-int min_i32(i32 a, i32 b) {
+u16 min_u16(u16 a, u16 b) {
   return a < b ? a : b;
 }
 
-int min_i64(i64 a, i64 b) {
+i16 min_i16(i16 a, i16 b) {
   return a < b ? a : b;
-}
-
-int max_i32(i32 a, i32 b) {
-  return a > b ? a : b;
 }
 
 u32 min_u32(u32 a, u32 b) {
   return a < b ? a : b;
 }
 
+int min_i32(i32 a, i32 b) {
+  return a < b ? a : b;
+}
+
+u64 min_u64(u64 a, u64 b) {
+  return a < b ? a : b;
+}
+
+i64 min_i64(i64 a, i64 b) {
+  return a < b ? a : b;
+}
+
+u16 max_u16(u16 a, u16 b) {
+  return a > b ? a : b;
+}
+
+i16 max_i16(i16 a, i16 b) {
+  return a > b ? a : b;
+}
+
 u32 max_u32(u32 a, u32 b) {
   return a > b ? a : b;
 }
 
-int max_i64(i64 a, i64 b) {
+int max_i32(i32 a, i32 b) {
+  return a > b ? a : b;
+}
+
+u64 max_u64(u64 a, u64 b) {
+  return a > b ? a : b;
+}
+
+i64 max_i64(i64 a, i64 b) {
   return a > b ? a : b;
 }
 

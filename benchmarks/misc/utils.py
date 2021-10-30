ae55a614010207dfe6b20e8ec35eb717887ac612
yolo2themoon <markbench@163.com>
2021-10-30 17:09:29 +0800

    update

--- benchmarks/misc/utils.py
+++ benchmarks/misc/utils.py
@@ -5,17 +5,26 @@ import jsbeautifier
 
 import taichi as ti
 
-kibibyte = 1024
 
-bls2str = {False: "BLS_off", True: "BLS_on"}
-dense2str = {False: "Struct_for", True: "Range_for"}
+def dtype2str(ti_dtype):
+    type_str_dict = {
+        ti.i32: "i32",
+        ti.i64: "i64",
+        ti.f32: "f32",
+        ti.f64: "f64"
+    }
+    if ti_dtype not in type_str_dict:
+        raise RuntimeError('Unsupported ti.dtype: ' + str(type(ti_dtype)))
+    else:
+        return type_str_dict[ti_dtype]
 
-dtype2str = {ti.i32: "i32", ti.i64: "i64", ti.f32: "f32", ti.f64: "f64"}
-dtype_size = {ti.i32: 4, ti.i64: 8, ti.f32: 4, ti.f64: 8}
 
-# for output string
-size_subsection = [(0.0, 'B'), (1024.0, 'KB'), (1048576.0, 'MB'),
-                   (1073741824.0, 'GB'), (float('inf'), 'INF')]  #B KB MB GB
+def dtype_size(ti_dtype):
+    dtype_size_dict = {ti.i32: 4, ti.i64: 8, ti.f32: 4, ti.f64: 8}
+    if ti_dtype not in dtype_size_dict:
+        raise RuntimeError('Unsupported ti.dtype: ' + str(type(ti_dtype)))
+    else:
+        return dtype_size_dict[ti_dtype]
 
 
 def arch_name(arch):
@@ -37,6 +46,10 @@ def dump2json(obj):
 
 
 def size2str(size_in_byte):
+    # for output string
+    size_subsection = [(0.0, 'B'), (1024.0, 'KB'), (1048576.0, 'MB'),
+                       (1073741824.0, 'GB'),
+                       (float('inf'), 'INF')]  #B KB MB GB
     for dsize, units in reversed(size_subsection):
         if size_in_byte >= dsize:
             return str(round(size_in_byte / dsize, 4)) + units

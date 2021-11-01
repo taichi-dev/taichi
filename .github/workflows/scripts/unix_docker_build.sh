7e576451aceaf23b3229894f1d269b2c3db29931
jiasheng <leonz5288@hotmail.com>
2021-11-01 14:33:35 +0800

    fix typo

--- .github/workflows/scripts/unix_docker_build.sh
+++ .github/workflows/scripts/unix_docker_build.sh
@@ -23,7 +23,7 @@ fi
 cd python
 # This is for changelog
 git fetch origin master
-TAICHI_CMAKE_ARGS=$CI_SETUP_CMAKE_ARGS python3 build.py build --project_name $PEOJECT_NAME
+TAICHI_CMAKE_ARGS=$CI_SETUP_CMAKE_ARGS python3 build.py build --project_name $PROJECT_NAME
 # Run basic cpp tests
 cd ..
 CUR_DIR=`pwd`

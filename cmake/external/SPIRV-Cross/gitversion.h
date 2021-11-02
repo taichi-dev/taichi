8917b93df73977dfb21c2e12b66c536f5fbe1953
Bob Cao <bobcaocheng@gmail.com>
2021-11-02 12:52:38 -0700

    Delete gitversion.h

--- cmake/external/SPIRV-Cross/gitversion.h
+++
@@ -1,9 +0,0 @@
-// Copyright 2016-2021 The Khronos Group Inc.
-// SPDX-License-Identifier: Apache-2.0
-
-#ifndef SPIRV_CROSS_GIT_VERSION_H_
-#define SPIRV_CROSS_GIT_VERSION_H_
-
-#define SPIRV_CROSS_GIT_REVISION "Git commit: 97a438d2 Timestamp: 2021-11-02T00:36:30"
-
-#endif

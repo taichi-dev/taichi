dd7372df5cbff75218158dec213b6bed75361cfa
Cheng Cao <bobcaocheng@gmail.com>
2021-11-01 22:28:41 -0700

    prototype EGL

--- taichi/backends/opengl/opengl_api.cpp
+++ taichi/backends/opengl/opengl_api.cpp
@@ -13,6 +13,11 @@
 #include "glad/glad.h"
 #include "GLFW/glfw3.h"
 #include "taichi/backends/opengl/opengl_device.h"
+
+#ifdef __linux__
+#include <EGL/egl.h>
+#endif
+
 #endif
 
 #include <list>
@@ -60,6 +65,7 @@ bool initialize_opengl(bool error_tolerance) {
     }
   }
 
+  /*
   glfwInit();
   // Compute Shader requires OpenGL 4.3+ (or OpenGL ES 3.1+)
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
@@ -84,8 +90,53 @@ bool initialize_opengl(bool error_tolerance) {
     TI_ERROR("[glsl] cannot create GLFW window: error {}: {}", status, desc);
   }
   glfwMakeContextCurrent(window);
+   */
 
-  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+  static const EGLint configAttribs[] = {
+      EGL_SURFACE_TYPE, EGL_PBUFFER_BIT,
+      EGL_BLUE_SIZE, 8,
+      EGL_GREEN_SIZE, 8,
+      EGL_RED_SIZE, 8,
+      EGL_DEPTH_SIZE, 8,
+      EGL_RENDERABLE_TYPE, EGL_OPENGL_BIT,
+      EGL_NONE
+  };
+
+  static const int pbufferWidth = 9;
+  static const int pbufferHeight = 9;
+
+  static const EGLint pbufferAttribs[] = {
+      EGL_WIDTH, pbufferWidth,
+      EGL_HEIGHT, pbufferHeight,
+      EGL_NONE,
+  };
+
+  // 1. Initialize EGL
+  EGLDisplay eglDpy = eglGetDisplay(EGL_DEFAULT_DISPLAY);
+  EGLint major, minor;
+  eglInitialize(eglDpy, &major, &minor);
+
+  // 2. Select an appropriate configuration
+  EGLint numConfigs;
+  EGLConfig eglCfg;
+
+  eglChooseConfig(eglDpy, configAttribs, &eglCfg, 1, &numConfigs);
+
+  // 3. Create a surface
+  EGLSurface eglSurf = eglCreatePbufferSurface(eglDpy, eglCfg,
+                                               pbufferAttribs);
+
+  // 4. Bind the API
+  eglBindAPI(EGL_OPENGL_API);
+
+  // 5. Create a context and make it current
+  EGLContext eglCtx = eglCreateContext(eglDpy, eglCfg, EGL_NO_CONTEXT,
+                                       NULL);
+
+  eglMakeCurrent(eglDpy, eglSurf, eglSurf, eglCtx);
+
+//  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+  if (!gladLoadGLLoader((GLADloadproc)eglGetProcAddress)) {
     if (error_tolerance) {
       TI_WARN("[glsl] cannot initialize GLAD");
       supported = std::make_optional<bool>(false);

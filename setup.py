5924dedcc56487939610e96ede427c82bd7540f1
sjwsl <xts.sjwsl@gmail.com>
2021-11-02 14:23:35 +0800

    fix

--- setup.py
+++ setup.py
@@ -212,10 +212,9 @@ class Clean(clean):
         for d in generated_folders:
             if os.path.exists(d):
                 remove_tree(d, dry_run=self.dry_run)
-        generated_files = [
-            glob.glob('taichi/runtime/llvm/runtime_*.bc'),
-            glob.glob('taichi/runtime/llvm/runtime_*.ll'),
-        ]
+        generated_files = []
+        generated_files += glob.glob('taichi/runtime/llvm/runtime_*.bc')
+        generated_files += glob.glob('taichi/runtime/llvm/runtime_*.ll')
         for f in generated_files:
             if os.path.exists(f):
                 print(f'removing generated file {f}')

295041f7266c5654607bb4330b41c2e0c7222894
Cheng Cao <bobcaocheng@gmail.com>
2021-11-02 21:19:03 -0700

    clean

--- cmake/external/SPIRV-Cross/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/bobcao3/taichi/external/SPIRV-Cross
-# Build directory: /home/bobcao3/taichi/cmake-build-debug/external/SPIRV-Cross
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.

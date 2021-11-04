50577dceda83aa842cb06b713f3ecd9e1ecb8405
Bo Qiao <boqiao@taichi.graphics>
2021-11-04 10:13:21 +0800

    Remove dupl device code

--- taichi/llvm/llvm_program.cpp
+++ taichi/llvm/llvm_program.cpp
@@ -474,11 +474,7 @@ void LlvmProgramImpl::finalize() {
   }
 #endif
   for (auto &alloc : ndarray_allocs_) {
-    if (config->arch == Arch::cuda) {
-      cuda_device()->dealloc_memory(alloc);
-    } else {
-      cpu_device()->dealloc_memory(alloc);
-    }
+    get_compute_device()->dealloc_memory(alloc);
   }
 }
 
@@ -572,23 +568,11 @@ DevicePtr LlvmProgramImpl::get_snode_tree_device_ptr(int tree_id) {
 }
 
 DeviceAllocation LlvmProgramImpl::allocate_memory_ndarray(std::size_t alloc_size) {
-  if (config->arch == Arch::cuda) {
-#if defined(TI_WITH_CUDA)
-    Device::AllocParams device_buffer_alloc_params;
-    device_buffer_alloc_params.size = alloc_size;
-    DeviceAllocation alloc = cuda_device()->allocate_memory(device_buffer_alloc_params);
-    ndarray_allocs_.push_back(alloc);
-    return alloc;
-#else
-    TI_NOT_IMPLEMENTED
-#endif
-  } else {
-    Device::AllocParams device_buffer_alloc_params;
-    device_buffer_alloc_params.size = alloc_size;
-    DeviceAllocation alloc = cpu_device()->allocate_memory(device_buffer_alloc_params);
-    ndarray_allocs_.push_back(alloc);
-    return alloc;
-  }
+  Device::AllocParams device_buffer_alloc_params;
+  device_buffer_alloc_params.size = alloc_size;
+  DeviceAllocation alloc = get_compute_device()->allocate_memory(device_buffer_alloc_params);
+  ndarray_allocs_.push_back(alloc);
+  return alloc;
 }
 
 uint64_t *LlvmProgramImpl::get_ndarray_alloc_info_ptr(DeviceAllocation &alloc) {

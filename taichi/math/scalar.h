57582f344ab0975ef4734307eb8533a039f26923
Niclas Schwalbe <kuhmilchtutorial@gmail.com>
2021-11-06 21:48:11 +0100

    Update scalar.h
    retry workflow

--- taichi/math/scalar.h
+++ taichi/math/scalar.h
@@ -163,7 +163,7 @@ inline int64 get_largest_pot(int64 a) noexcept {
                  "a should be positive, instead of " + std::to_string(a));
 
   /* This code was copied from https://stackoverflow.com/a/20207950 and edited
-  It uses loop unrolling, which all compilers will do.*/
+  It uses loop unrolling, which all (modern) compilers will do. */
   for (int64 i = 1; i < 64; i *= 2) {
     a |= (a >> i);
   }

a422d383b2f78b173c92eea3da94210ab4263d31
lin-hitonami <linjiang@taichi.graphics>
2021-11-08 17:42:53 +0800

    format

--- tests/python38/test_ast_refactor.py
+++ tests/python38/test_ast_refactor.py
@@ -1,16 +1,16 @@
+from sys import version_info
+
 import pytest
 
 import taichi as ti
-from sys import version_info
 
-if version_info >= (3, 8):
 
-    @ti.test(experimental_ast_refactor=True)
-    def test_namedexpr():
-        @ti.kernel
-        def foo() -> ti.i32:
-            b = 2 + (a := 5)
-            b += a
-            return b
+@ti.test(experimental_ast_refactor=True)
+def test_namedexpr():
+    @ti.kernel
+    def foo() -> ti.i32:
+        b = 2 + (a := 5)
+        b += a
+        return b
 
-        assert foo() == 12
+    assert foo() == 12

c7c97e25e702ac3921e9847a8cef6160f74a8962
Bo Qiao <boqiao@taichi.graphics>
2021-11-09 12:54:31 +0800

    [lang] [refactor] Use preallocated memory via device allocation for Ndarray (#3395)
    * Use preallocated memory from runtime
    
    * Auto Format
    
    * Add cuda guard
    
    * Unify the memory runtime function
    
    * Auto Format
    
    * Add a helper for cpu and cuda mem alloc
    
    * Auto Format
    
    Co-authored-by: Taichi Gardener <taichigardener@gmail.com>

--- taichi/program/ndarray.cpp
+++ taichi/program/ndarray.cpp
@@ -17,8 +17,9 @@ Ndarray::Ndarray(Program *prog,
                                 std::multiplies<>())),
       element_size_(data_type_size(dtype)) {
   LlvmProgramImpl *prog_impl = prog->get_llvm_program_impl();
-  ndarray_alloc_ =
-      prog_impl->allocate_memory_ndarray(nelement_ * element_size_);
+  ndarray_alloc_ = prog_impl->allocate_memory_ndarray(nelement_ * element_size_,
+                                                      prog->result_buffer);
+
   data_ptr_ = prog_impl->get_ndarray_alloc_info_ptr(ndarray_alloc_);
 }
 

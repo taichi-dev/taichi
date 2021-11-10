12e41ad69e7e8223e96c3ee8dd4c1b9a7b59e826
Frost Ming <mianghong@gmail.com>
2021-11-10 10:07:51 +0800

    check the error message

--- tests/python/test_type_check.py
+++ tests/python/test_type_check.py
@@ -10,7 +10,7 @@ def test_unary_op():
         a = 1
         b = ti.floor(a)
 
-    with pytest.raises(SystemExit):
+    with pytest.raises(TypeError, match="'floor' takes real inputs only"):
         floor()
 
 
@@ -22,7 +22,8 @@ def test_binary_op():
         b = 3.1
         c = a & b
 
-    with pytest.raises(SystemExit):
+    with pytest.raises(TypeError,
+                       match=r"unsupported operand type\(s\) for '&'"):
         bitwise_float()
 
 
@@ -35,7 +36,8 @@ def test_ternary_op():
         c = 3.6
         d = b if a else c
 
-    with pytest.raises(SystemExit):
+    with pytest.raises(TypeError,
+                       match="for 'select': 'f32', 'i32' and 'f32'"):
         select()
 
 
@@ -48,5 +50,5 @@ def test_subscript():
     def any_array(x: ti.any_arr()):
         b = x[3, 1.1]
 
-    with pytest.raises(TypeError):
+    with pytest.raises(TypeError, match="indices must be integers"):
         any_array(a)

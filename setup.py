45c6ad483ea8bf43cc5626bded7dc72eb31927f0
Ailing <ailzhang@users.noreply.github.com>
2021-11-10 19:51:15 +0800

    [misc] Fix python wheel versioning. (#3450)

--- setup.py
+++ setup.py
@@ -35,6 +35,10 @@ classifiers = [
 ]
 
 project_name = os.getenv('PROJECT_NAME', 'taichi')
+TI_VERSION_MAJOR = 0
+TI_VERSION_MINOR = 8
+TI_VERSION_PATCH = 5
+version = f'{TI_VERSION_MAJOR}.{TI_VERSION_MINOR}.{TI_VERSION_PATCH}'
 
 data_files = glob.glob('python/lib/*')
 print(data_files)
@@ -127,6 +131,9 @@ class CMakeBuild(build_ext):
         cmake_args += [
             f'-DCMAKE_LIBRARY_OUTPUT_DIRECTORY={build_directory}',
             f'-DPYTHON_EXECUTABLE={get_python_executable()}',
+            f'-DTI_VERSION_MAJOR={TI_VERSION_MAJOR}',
+            f'-DTI_VERSION_MINOR={TI_VERSION_MINOR}',
+            f'-DTI_VERSION_PATCH={TI_VERSION_PATCH}',
         ]
 
         self.debug = os.getenv('DEBUG', '0') in ('1', 'ON')
@@ -223,6 +230,7 @@ class Clean(clean):
 setup(name=project_name,
       packages=packages,
       package_dir={"": package_dir},
+      version=version,
       description='The Taichi Programming Language',
       author='Taichi developers',
       author_email='yuanmhu@gmail.com',

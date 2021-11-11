193aabb0b13298268e9f8edf95d0529b646d5722
Shay P.C <3180102686@zju.edu.cn>
2021-11-11 17:17:53 +0800

    update test

--- tests/python/test_global_thread_idx.py
+++ tests/python/test_global_thread_idx.py
@@ -1,4 +1,5 @@
 import taichi as ti
+import numpy as np
 
 
 @ti.test(arch=[ti.cuda, ti.cpu])
@@ -14,3 +15,17 @@ def test_thread_idx():
 
     func()
     assert x.to_numpy().sum() == 256
+
+@ti.test(arch=ti.cuda)
+def test_global_thread_idx():
+    n = 2048
+    x = ti.field(ti.i32, shape=(n))
+
+    @ti.kernel
+    def func():
+        for i in range(n):
+            tid = ti.global_thread_idx()
+            x[tid] = tid
+    
+    func()
+    assert np.arange(n).sum() == x.to_numpy().sum()

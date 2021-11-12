cfa9d8bfb14811745b81a40bb4c463534a60cb8b
tczhangzhi <850734033@qq.com>
2021-11-12 18:19:51 +0800

    [Lang] Fix pylint rule W0611_{W0401}

--- python/taichi/ui/ui.py
+++ python/taichi/ui/ui.py
@@ -4,7 +4,7 @@ if _ti_core.GGUI_AVAILABLE:
 
     from .camera import Camera # pylint: disable=unused-import
     from .canvas import Canvas # pylint: disable=unused-import
-    from .constants import * # pylint: disable=unused-import
+    from .constants import * # pylint: disable=unused-import,wildcard-import
     from .gui import Gui # pylint: disable=unused-import
     from .scene import Scene # pylint: disable=unused-import
     from .window import Window # pylint: disable=unused-import

689420ef8000e53f0753cd5648ea4aa0fdfb7ce7
nasnoisaac <nasnoisaac@gmail.com>
2021-11-13 10:52:33 +0800

    format code

--- python/taichi/lang/__init__.py
+++ python/taichi/lang/__init__.py
@@ -101,8 +101,8 @@ cpu = _ti_core.host_arch()
 
 When this is used, Taichi automatically picks the matching CPU backend.
 """
-timeline_clear = lambda: impl.get_runtime().prog.timeline_clear() # pylint: disable=unnecessary-lambda
-timeline_save = lambda fn: impl.get_runtime().prog.timeline_save(fn) # pylint: disable=unnecessary-lambda
+timeline_clear = lambda: impl.get_runtime().prog.timeline_clear()  # pylint: disable=unnecessary-lambda
+timeline_save = lambda fn: impl.get_runtime().prog.timeline_save(fn)  # pylint: disable=unnecessary-lambda
 
 # Legacy API
 type_factory_ = _ti_core.get_type_factory_instance()

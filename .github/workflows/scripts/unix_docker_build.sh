6ba6484620698b84f6dbaee5b239223e87da0579
lin-hitonami <linjiang@taichi.graphics>
2021-11-19 16:57:07 +0800

    add sccache to cpu

--- .github/workflows/scripts/unix_docker_build.sh
+++ .github/workflows/scripts/unix_docker_build.sh
@@ -7,11 +7,16 @@ PY=$1
 GPU_BUILD=$2
 PROJECT_NAME=$3
 CI_SETUP_CMAKE_ARGS=$4
-
+export SCCACHE_BUCKET=$5
+export AWS_ACCESS_KEY_ID=$6
+export AWS_SECRET_ACCESS_KEY=$7
 source /home/dev/miniconda/etc/profile.d/conda.sh
 conda activate $PY
 
 python3 -m pip uninstall taichi taichi-nightly -y
+curl -L https://github.com/mozilla/sccache/releases/download/v0.2.15/sccache-v0.2.15-x86_64-unknown-linux-musl.tar.gz > sccache.tar.gz
+tar -xzf sccache.tar.gz
+export PATH=$(pwd)/sccache-v0.2.15-x86_64-unknown-linux-musl/sccache:$PATH
 
 cd taichi
 

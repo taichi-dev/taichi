4e2b3bf91cec6f7eefc3d07de68da42c5adac939
Bo Qiao <boqiao@taichi.graphics>
2021-11-22 00:13:10 +0800

    Rename var

--- taichi/backends/cuda/cuda_caching_allocator.cpp
+++ taichi/backends/cuda/cuda_caching_allocator.cpp
@@ -5,18 +5,18 @@ namespace lang {
 namespace cuda {
 
 bool CudaCachingAllocator::find_block(size_t sz) const {
-  return mem_blocks.find(sz) != mem_blocks.end();
+  return mem_blocks_.find(sz) != mem_blocks_.end();
 }
 
 uint64_t *CudaCachingAllocator::allocate(size_t sz) {
-  auto blk = mem_blocks.find(sz);
+  auto blk = mem_blocks_.find(sz);
   uint64_t *ret = blk->second;
-  mem_blocks.erase(blk);
+  mem_blocks_.erase(blk);
   return ret;
 }
 
 void CudaCachingAllocator::release(size_t sz, uint64_t *ptr) {
-  mem_blocks.insert(std::pair<size_t, uint64_t *>(sz, ptr));
+  mem_blocks_.insert(std::pair<size_t, uint64_t *>(sz, ptr));
 }
 
 }  // namespace cuda

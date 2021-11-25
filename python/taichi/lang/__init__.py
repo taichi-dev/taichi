5c6c8dc7710c2bc95caa39d533c7b5b3f3740ee0
Taichi Gardener <taichigardener@gmail.com>
2021-11-25 08:32:53 +0000

    Auto Format

--- python/taichi/lang/__init__.py
+++ python/taichi/lang/__init__.py
@@ -536,8 +536,8 @@ def init(arch=None,
     else:
         check_version()
         with open(timestamp_path, 'w') as f:
-            f.write((cur_date +
-                     datetime.timedelta(days=7)).strftime('%Y-%m-%d'))
+            f.write(
+                (cur_date + datetime.timedelta(days=7)).strftime('%Y-%m-%d'))
 
     # Make a deepcopy in case these args reference to items from ti.cfg, which are
     # actually references. If no copy is made and the args are indeed references,

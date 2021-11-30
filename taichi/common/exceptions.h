629d387bf21753f440b2eace16f2a6a294c057a3
lin-hitonami <linjiang@taichi.graphics>
2021-11-30 17:26:09 +0800

    format

--- taichi/common/exceptions.h
+++ taichi/common/exceptions.h
@@ -7,9 +7,11 @@ class IRModified {};
 
 class TaichiTypeError : public std::exception {
   std::string msg_;
+
  public:
-  TaichiTypeError(const std::string msg) : msg_(msg) {}
-  const char *what() const throw () override {
+  TaichiTypeError(const std::string msg) : msg_(msg) {
+  }
+  const char *what() const throw() override {
     return msg_.c_str();
   }
 };

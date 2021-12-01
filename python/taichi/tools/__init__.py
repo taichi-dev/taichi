14adc7a62cf3dee90423fe17b8520c44fd293314
Yi Xu <xy_xuyi@foxmail.com>
2021-12-01 19:56:56 +0800

    [refactor] Move image.py from misc/ to tools/ and remove it from top level package

--- python/taichi/tools/__init__.py
+++ python/taichi/tools/__init__.py
@@ -1,5 +1,6 @@
+from .image import imdisplay, imread, imresize, imshow, imwrite
 from .np2ply import PLYWriter
 from .patterns import taichi_logo
 from .video import VideoManager
 
-__all__ = ['PLYWriter', 'taichi_logo', 'VideoManager']
+__all__ = ['PLYWriter', 'taichi_logo', 'VideoManager', 'imdisplay', 'imread', 'imresize', 'imshow', 'imwrite']

02fb1d76845a72a432db8294e57f5d95894651f5
Yi Xu <xy_xuyi@foxmail.com>
2021-12-01 15:10:10 +0800

    [refactor] Remove np2ply, patterns, video from top level package

--- python/taichi/tools/__init__.py
+++ python/taichi/tools/__init__.py
@@ -2,4 +2,4 @@ from .np2ply import PLYWriter
 from .patterns import taichi_logo
 from .video import VideoManager
 
-__all__ = [s for s in dir() if not s.startswith('_')]
+__all__ = ['PLYWriter', 'taichi_logo', 'VideoManager']

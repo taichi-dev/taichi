7e817397963ac2ad7c4837fb88b8cfd649981472
lin-hitonami <linjiang@taichi.graphics>
2021-12-01 11:37:09 +0800

    test

--- .github/workflows/scripts/unix_docker_build.sh
+++ .github/workflows/scripts/unix_docker_build.sh
@@ -17,7 +17,7 @@ tar -xzf sccache-v0.2.15-x86_64-unknown-linux-musl.tar.gz
 chmod +x sccache-v0.2.15-x86_64-unknown-linux-musl/sccache
 export PATH=$(pwd)/sccache-v0.2.15-x86_64-unknown-linux-musl:$PATH
 #tar xf /home/dev/sccache.tar -C /home/dev
-ls -la /home/dev/sccache_cache
+ls -la /home/dev/sccache_cache/0
 exit 1
 
 source /home/dev/miniconda/etc/profile.d/conda.sh

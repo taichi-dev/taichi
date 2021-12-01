65a886c8494e51131f45e24e805fed771e53d95d
Taichi Gardener <taichigardener@gmail.com>
2021-12-01 11:59:40 +0000

    Auto Format

--- python/taichi/tools/__init__.py
+++ python/taichi/tools/__init__.py
@@ -3,4 +3,7 @@ from .np2ply import PLYWriter
 from .patterns import taichi_logo
 from .video import VideoManager
 
-__all__ = ['PLYWriter', 'taichi_logo', 'VideoManager', 'imdisplay', 'imread', 'imresize', 'imshow', 'imwrite']
+__all__ = [
+    'PLYWriter', 'taichi_logo', 'VideoManager', 'imdisplay', 'imread',
+    'imresize', 'imshow', 'imwrite'
+]

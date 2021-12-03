30782f1a790e575b72ad74dcaa031833650f2c57
knight42 <anonymousknight96@gmail.com>
2021-12-03 11:31:04 +0800

    debug

--- .github/workflows/scripts/unix_docker_build.sh
+++ .github/workflows/scripts/unix_docker_build.sh
@@ -42,12 +42,10 @@ fi
 
 python3 misc/make_changelog.py origin/master ./ True
 TAICHI_CMAKE_ARGS=$CI_SETUP_CMAKE_ARGS PROJECT_NAME=$PROJECT_NAME python3 setup.py $PROJECT_TAGS bdist_wheel $EXTRA_ARGS
-# Run basic cpp tests
 
-CUR_DIR=`pwd`
-TI_LIB_DIR=$CUR_DIR/python/taichi/lib ./build/taichi_cpp_tests
 cat /home/dev/sccache_error
 sccache -s
 chmod -R 777 $SCCACHE_DIR
-cp dist/*.whl /wheel/
+cp dist/*.whl /shared/
+cp -r build/ /shared/
 rm -f python/CHANGELOG.md

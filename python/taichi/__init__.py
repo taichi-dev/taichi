8ad45d8e2596eedfa611f4fac870e4bbcc7166e6
lin-hitonami <linjiang@taichi.graphics>
2021-12-10 13:28:16 +0800

    save

--- python/taichi/__init__.py
+++ python/taichi/__init__.py
@@ -2,6 +2,7 @@ import sys
 
 from taichi._funcs import *
 from taichi._logging import *
+from taichi._misc import *
 from taichi.lang import *  # pylint: disable=W0622 # TODO(archibate): It's `taichi.lang.core` overriding `taichi.core`
 from taichi.main import main
 from taichi.tools import *
@@ -9,7 +10,6 @@ from taichi.tools.patterns import taichi_logo
 from taichi.types.annotations import *
 # Provide a shortcut to types since they're commonly used.
 from taichi.types.primitive_types import *
-from taichi._lib import core as _ti_core
 
 from taichi import ad
 from taichi.ui import GUI, hex_to_rgb, rgb_to_hex, ui
@@ -18,30 +18,4 @@ from taichi.ui import GUI, hex_to_rgb, rgb_to_hex, ui
 from taichi import aot  # isort:skip
 from taichi._testing import *  # isort:skip
 
-deprecated_names = {'SOA': 'Layout.SOA', 'AOS': 'Layout.AOS'}
-if sys.version_info.minor < 7:
-    for name, alter in deprecated_names.items():
-        exec(f'{name} = {alter}')
-else:
-
-    def __getattr__(attr):
-        if attr in deprecated_names:
-            warning(
-                f'ti.{attr} is deprecated. Please use ti.{deprecated_names[attr]} instead.',
-                DeprecationWarning,
-                stacklevel=2)
-            exec(f'{attr} = {deprecated_names[attr]}')
-            return locals()[attr]
-        raise AttributeError(f"module '{__name__}' has no attribute '{attr}'")
-
-
 __all__ = ['ad', 'lang', 'tools', 'main', 'ui', 'profiler']
-
-complex_kernel = deprecated('ti.complex_kernel',
-                            'ti.ad.grad_replaced')(ad.grad_replaced)
-
-complex_kernel_grad = deprecated('ti.complex_kernel_grad',
-                                 'ti.ad.grad_for')(ad.grad_for)
-
-__version__ = (_ti_core.get_version_major(), _ti_core.get_version_minor(),
-               _ti_core.get_version_patch())
---
+++ python/taichi/_misc.py
@@ -0,0 +1,32 @@
+import taichi._lib.core as _ti_core
+
+__all__ = ["__version__", "complex_kernel", "complex_kernel_grad", "deprecated_names"]
+
+deprecated_names = {'SOA': 'Layout.SOA', 'AOS': 'Layout.AOS'}
+if sys.version_info.minor < 7:
+    for name, alter in deprecated_names.items():
+        exec(f'{name} = {alter}')
+        __all__ += name
+else:
+
+    def __getattr__(attr):
+        if attr in deprecated_names:
+            warning(
+                f'ti.{attr} is deprecated. Please use ti.{deprecated_names[attr]} instead.',
+                DeprecationWarning,
+                stacklevel=2)
+            exec(f'{attr} = {deprecated_names[attr]}')
+            return locals()[attr]
+        raise AttributeError(f"module '{__name__}' has no attribute '{attr}'")
+    __all__ += "__getattr__"
+
+
+
+complex_kernel = deprecated('ti.complex_kernel',
+                            'ti.ad.grad_replaced')(ad.grad_replaced)
+
+complex_kernel_grad = deprecated('ti.complex_kernel_grad',
+                                 'ti.ad.grad_for')(ad.grad_for)
+
+__version__ = (_ti_core.get_version_major(), _ti_core.get_version_minor(),
+               _ti_core.get_version_patch())

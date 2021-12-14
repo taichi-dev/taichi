128335471b05c235368fba5a89f2d9f5085d1a67
Dunfan Lu <ldfrank533@outlook.com>
2021-12-14 00:30:45 +0000

    Add parallel sort utility

---
+++ python/taichi/tools/sort.py
@@ -0,0 +1,29 @@
+import taichi as ti
+
+
+def parallel_sort(x):
+    N = x.shape[0]
+
+    @ti.kernel
+    def sort_stage(x: ti.template(), N: int, p: int, k: int, invocations: int):
+        for inv in range(invocations):
+            j = k % p + inv * 2 * k
+            for i in range(0, min(k, N - j - k)):
+                a = i + j
+                b = i + j + k
+                if int(a / (p * 2)) == int(b / (p * 2)):
+                    val_a = x[a]
+                    val_b = x[b]
+                    if val_a > val_b:
+                        x[a] = val_b
+                        x[b] = val_a
+
+    p = 1
+    while p < N:
+        k = p
+        while k >= 1:
+            invocations = int((N - k - k % p) / (2 * k)) + 1
+            sort_stage(x, N, p, k, invocations)
+            ti.sync()
+            k = int(k / 2)
+        p = int(p * 2)
---
+++ tests/python/test_sort.py
@@ -0,0 +1,27 @@
+import taichi as ti
+
+
+@ti.test()
+def test_sort():
+    def test_sort_for_dtype(dtype, N):
+        x = ti.field(dtype, N)
+
+        @ti.kernel
+        def fill():
+            for i in x:
+                x[i] = ti.random() * N
+
+        fill()
+        ti.parallel_sort(x)
+
+        x_host = x.to_numpy()
+
+        for i in range(N - 1):
+            assert x_host[i] <= x_host[i + 1]
+
+    test_sort_for_dtype(ti.i32, 1)
+    test_sort_for_dtype(ti.i32, 256)
+    test_sort_for_dtype(ti.i32, 100001)
+    test_sort_for_dtype(ti.f32, 1)
+    test_sort_for_dtype(ti.f32, 256)
+    test_sort_for_dtype(ti.f32, 100001)

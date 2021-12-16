7e7e226dc0e7c1e3240e16c61e5af6d54031eb46
Dunfan Lu <ldfrank533@outlook.com>
2021-12-16 09:57:23 +0000

    [lang] Add parallel sort utility (#3790)
    * Add parallel sort utility
    
    * add comments
    
    * exclude cc backend
    
    * allow sorting values by keys
    
    * fix

--- python/taichi/tools/__init__.py
+++ python/taichi/tools/__init__.py
@@ -1,26 +1,14 @@
 from .image import imdisplay, imread, imresize, imshow, imwrite
 from .np2ply import PLYWriter
+from .sort import parallel_sort
 from .util import *
 # Don't import taichi_logo here which will cause circular import.
 # If you need it, just import from taichi.tools.patterns
 from .video import VideoManager
 
 __all__ = [
-    'PLYWriter',
-    'VideoManager',
-    'imdisplay',
-    'imread',
-    'imresize',
-    'imshow',
-    'imwrite',
-    'deprecated',
-    'warning',
-    'dump_dot',
-    'dot_to_pdf',
-    'obsolete',
-    'get_kernel_stats',
-    'get_traceback',
-    'set_gdb_trigger',
-    'print_profile_info',
-    'clear_profile_info',
+    'PLYWriter', 'VideoManager', 'imdisplay', 'imread', 'imresize', 'imshow',
+    'imwrite', 'deprecated', 'warning', 'dump_dot', 'dot_to_pdf', 'obsolete',
+    'get_kernel_stats', 'get_traceback', 'set_gdb_trigger',
+    'print_profile_info', 'clear_profile_info', 'parallel_sort'
 ]

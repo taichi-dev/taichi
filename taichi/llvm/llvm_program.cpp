42ca7938f15493c0184fb0d51c7d0a4dadf1b9dc
Bo Qiao <boqiao@taichi.graphics>
2021-12-22 18:34:50 +0800

    Reduce tail effect

--- taichi/llvm/llvm_program.cpp
+++ taichi/llvm/llvm_program.cpp
@@ -82,7 +82,7 @@ LlvmProgramImpl::LlvmProgramImpl(CompileConfig &config_,
 
     if (config_.saturating_grid_dim == 0) {
       TI_INFO("CUDA max blocks per SM = {}", query_max_block_per_sm);
-      config_.saturating_grid_dim = num_SMs * query_max_block_per_sm;
+      config_.saturating_grid_dim = num_SMs * query_max_block_per_sm * 2;
     }
 #endif
   }

b69a4c9637b2df10af2ba10474812500abce4052
Yi Xu <xy_xuyi@foxmail.com>
2021-12-23 12:52:21 +0800

    Add C++ tests

--- tests/cpp/analysis/alias_analysis_test.cpp
+++ tests/cpp/analysis/alias_analysis_test.cpp
@@ -94,6 +94,58 @@ TEST(AliasAnalysis, GlobalPtr_DiffSNodes) {
 
 // TODO(#2193): Add tests for other edge cases and other kinds of statements.
 
+TEST(AliasAnalysis, ExternalPtr_Same) {
+  IRBuilder builder;
+  auto *arg1 = builder.create_arg_load(1, PrimitiveType::i32, true);
+  auto *arg2 = builder.create_arg_load(2, PrimitiveType::i32, false);
+  const auto indices = std::vector<Stmt *>{arg2, arg2};
+  auto *eptr1 = builder.create_external_ptr(arg1, indices);
+  auto *eptr2 = builder.create_external_ptr(arg1, indices);
+
+  const auto aa = alias_analysis(eptr1, eptr2);
+  EXPECT_EQ(aa, AliasResult::same);
+}
+
+TEST(AliasAnalysis, ExternalPtr_Different) {
+  IRBuilder builder;
+  auto *arg1 = builder.create_arg_load(1, PrimitiveType::i32, true);
+  auto *arg2 = builder.create_arg_load(2, PrimitiveType::i32, false);
+  const auto indices1 = std::vector<Stmt *>{arg2, builder.get_int32(1)};
+  const auto indices2 = std::vector<Stmt *>{arg2, builder.get_int32(2)};
+  auto *eptr1 = builder.create_external_ptr(arg1, indices1);
+  auto *eptr2 = builder.create_external_ptr(arg1, indices2);
+
+  const auto aa = alias_analysis(eptr1, eptr2);
+  EXPECT_EQ(aa, AliasResult::different);
+}
+
+TEST(AliasAnalysis, ExternalPtr_Uncertain) {
+  IRBuilder builder;
+  auto *arg1 = builder.create_arg_load(1, PrimitiveType::i32, true);
+  auto *arg2 = builder.create_arg_load(2, PrimitiveType::i32, false);
+  auto *arg3 = builder.create_arg_load(3, PrimitiveType::i32, false);
+  const auto indices1 = std::vector<Stmt *>{arg2, arg2};
+  const auto indices2 = std::vector<Stmt *>{arg2, arg3};
+  auto *eptr1 = builder.create_external_ptr(arg1, indices1);
+  auto *eptr2 = builder.create_external_ptr(arg1, indices2);
+
+  const auto aa = alias_analysis(eptr1, eptr2);
+  EXPECT_EQ(aa, AliasResult::uncertain);
+}
+
+TEST(AliasAnalysis, ExternalPtr_DiffPtr) {
+  IRBuilder builder;
+  auto *arg1 = builder.create_arg_load(1, PrimitiveType::i32, true);
+  auto *arg2 = builder.create_arg_load(2, PrimitiveType::i32, true);
+  auto *arg3 = builder.create_arg_load(3, PrimitiveType::i32, false);
+  const auto indices = std::vector<Stmt *>{arg3, arg3};
+  auto *eptr1 = builder.create_external_ptr(arg1, indices);
+  auto *eptr2 = builder.create_external_ptr(arg2, indices);
+
+  const auto aa = alias_analysis(eptr1, eptr2);
+  EXPECT_EQ(aa, AliasResult::uncertain);
+}
+
 }  // namespace analysis
 }  // namespace irpass
 }  // namespace lang

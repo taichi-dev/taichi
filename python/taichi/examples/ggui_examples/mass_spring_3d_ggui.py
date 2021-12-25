b6618467d05376390b85ed4cef5c3f8fcae08bdd
Alex Brown <alex.brown.code@gmail.com>
2021-12-25 17:28:08 +0800

    parameter tweaks

--- python/taichi/examples/ggui_examples/mass_spring_3d_ggui.py
+++ python/taichi/examples/ggui_examples/mass_spring_3d_ggui.py
@@ -3,71 +3,86 @@ import taichi as ti
 ti.init(arch=ti.cuda)  # Alternatively, ti.init(arch=ti.cpu)
 
 n = 128
-cell_size = 1.0 / n
 gravity = ti.Vector([0, -9.8, 0])
-stiffness = 1e5
-damping = 3
-dt = 1e-4
+quad_size = 1.0 / n
+dt = 4e-2 / n
+substeps = int(1 / 60 // dt)
 
-ball_radius = 0.2
-ball_center = ti.Vector.field(3, float, (1, ))
+spring_Y = 3e4
+dashpot_damping = 1e4
+drag_damping = 1
 
-x = ti.Vector.field(3, float, (n, n))
-v = ti.Vector.field(3, float, (n, n))
+ball_radius = 0.3
+ball_center = ti.Vector.field(3, dtype=float, shape=(1, ))
+ball_center[0] = [0, 0, 0]
+
+x = ti.Vector.field(3, dtype=float, shape=(n, n))
+v = ti.Vector.field(3, dtype=float, shape=(n, n))
 
 num_triangles = (n - 1) * (n - 1) * 2
-indices = ti.field(int, num_triangles * 3)
-vertices = ti.Vector.field(3, float, n * n)
+indices = ti.field(int, shape=num_triangles * 3)
+vertices = ti.Vector.field(3, dtype=float, shape=n * n)
 
-def init_scene():
-    for i, j in ti.ndrange(n, n):
-        x[i, j] = [
-            i * cell_size - 0.5, 0.7,
-            j * cell_size - 0.5
-        ]
-    ball_center[0] = [0, 0, 0]
 
 @ti.kernel
-def set_indices():
+def initialize_mass_points():
+    random_offset = ti.Vector([ti.random() - 0.5, ti.random() - 0.5]) * 0.1
+
+    for i, j in x:
+        x[i, j] = [
+            i * quad_size - 0.5 + random_offset[0], 0.6,
+            j * quad_size - 0.5 + random_offset[1]
+        ]
+        v[i, j] = [0, 0, 0]
+
+
+@ti.kernel
+def initialize_mesh_indices():
     for i, j in ti.ndrange(n - 1, n - 1):
-        square_id = (i * (n - 1)) + j
+        quad_id = (i * (n - 1)) + j
         # 1st triangle of the square
-        indices[square_id * 6 + 0] = i * n + j
-        indices[square_id * 6 + 1] = (i + 1) * n + j
-        indices[square_id * 6 + 2] = i * n + (j + 1)
+        indices[quad_id * 6 + 0] = i * n + j
+        indices[quad_id * 6 + 1] = (i + 1) * n + j
+        indices[quad_id * 6 + 2] = i * n + (j + 1)
         # 2nd triangle of the square
-        indices[square_id * 6 + 3] = (i + 1) * n + j + 1
-        indices[square_id * 6 + 4] = i * n + (j + 1)
-        indices[square_id * 6 + 5] = (i + 1) * n + j
+        indices[quad_id * 6 + 3] = (i + 1) * n + j + 1
+        indices[quad_id * 6 + 4] = i * n + (j + 1)
+        indices[quad_id * 6 + 5] = (i + 1) * n + j
 
 
-links = []
+initialize_mesh_indices()
 
-# Link to 8 neighbors
+spring_offsets = []
+
+# Link to neighbors
 for i in range(-2, 3):
     for j in range(-2, 3):
         if (i, j) != (0, 0) and abs(i) + abs(j) <= 2:
-            links.append(ti.Vector([i, j]))
+            spring_offsets.append(ti.Vector([i, j]))
+
 
 @ti.kernel
-def step():
+def substep():
     for i in ti.grouped(x):
         v[i] += gravity * dt
 
     for i in ti.grouped(x):
         force = ti.Vector([0.0, 0.0, 0.0])
-        for offset in ti.static(links):
-            j = i + offset
+        for spring_offset in ti.static(spring_offsets):
+            j = i + spring_offset
             if 0 <= j[0] < n and 0 <= j[1] < n:
-                relative_pos = x[j] - x[i]
-                current_length = relative_pos.norm()
-                original_length = cell_size * float(i - j).norm()
-                force += stiffness * relative_pos.normalized() * (
-                    current_length - original_length) / original_length
+                x_ij = x[i] - x[j]
+                v_ij = v[i] - v[j]
+                d = x_ij.normalized()
+                current_dist = x_ij.norm()
+                original_dist = quad_size * float(i - j).norm()
+                force += -spring_Y * d * (current_dist / original_dist - 1
+                                          ) - v_ij.dot(d) * d * dashpot_damping * quad_size
+
         v[i] += force * dt
 
     for i in ti.grouped(x):
-        v[i] *= ti.exp(-damping * dt)
+        v[i] *= ti.exp(-drag_damping * dt)
         offset_to_center = x[i] - ball_center[0]
         if offset_to_center.norm() <= ball_radius:
             # Velocity projection
@@ -82,21 +97,27 @@ def update_vertices():
         vertices[i * n + j] = x[i, j]
 
 
-init_scene()
-set_indices()
-
-window = ti.ui.Window("Taichi Cloth Simulation", (800, 800), vsync=True)
+window = ti.ui.Window("Taichi Cloth Simulation on GGUI", (1024, 1024), vsync=True)
 canvas = window.get_canvas()
 scene = ti.ui.Scene()
 camera = ti.ui.make_camera()
 
+current_t = 0.0
+initialize_mass_points()
+
 while window.running:
-    for i in range(30):
-        step()
+    if current_t > 1.5:
+        # Reset
+        initialize_mass_points()
+        current_t = 0
+
+    for i in range(substeps):
+        substep()
+        current_t += dt
     update_vertices()
 
-    camera.position(0.0, 0.3, 2)
-    camera.lookat(0.0, 0.3, 0)
+    camera.position(0.0, 0.0, 3)
+    camera.lookat(0.0, 0.0, 0)
     scene.set_camera(camera)
 
     scene.point_light(pos=(0.5, 1, 2), color=(1, 1, 1))
@@ -104,6 +125,10 @@ while window.running:
                indices=indices,
                color=(0.5, 0.5, 0.5),
                two_sided=True)
+
+    # Draw a smaller ball to avoid visual penetration
     scene.particles(ball_center, radius=ball_radius * 0.95, color=(0.5, 0, 0))
     canvas.scene(scene)
     window.show()
+
+#TODO: include self-collision handling

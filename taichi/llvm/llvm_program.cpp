4b427a11ad64c6795253fad0a28f93c760946c1b
Bo Qiao <boqiao@taichi.graphics>
2021-12-31 15:54:57 +0800

    use std fill for cpu

--- taichi/llvm/llvm_program.cpp
+++ taichi/llvm/llvm_program.cpp
@@ -608,13 +608,15 @@ uint64_t *LlvmProgramImpl::get_ndarray_alloc_info_ptr(DeviceAllocation &alloc) {
 void LlvmProgramImpl::fill_ndarray(DeviceAllocation &alloc,
                                    std::size_t size,
                                    uint32_t data) {
+  auto ptr = get_ndarray_alloc_info_ptr(alloc);
   if (config->arch == Arch::cuda) {
 #if defined(TI_WITH_CUDA)
-    auto ptr = get_ndarray_alloc_info_ptr(alloc);
     CUDADriver::get_instance().memsetd32((void *)ptr, data, size);
 #else
     TI_NOT_IMPLEMENTED
 #endif
+  } else {
+    std::fill((uint32_t *)ptr, (uint32_t *)ptr + size, data);
   }
 }
 }  // namespace lang

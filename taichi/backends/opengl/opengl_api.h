02ff5b1723f37f098bfa0dce710de37948a72482
Ye Kuang <yekuang.ky@gmail.com>
2022-01-07 10:33:02 +0800

    [aot] Add task_type for OpenGL

--- taichi/backends/opengl/opengl_api.h
+++ taichi/backends/opengl/opengl_api.h
@@ -1,20 +1,21 @@
 #pragma once
 
-#include "taichi/common/core.h"
-#include "taichi/ir/transforms.h"
-
+#include <optional>
 #include <string>
 #include <vector>
-#include <optional>
 
 #include "taichi/backends/device.h"
-#include "taichi/backends/opengl/opengl_kernel_util.h"
 #include "taichi/backends/opengl/opengl_kernel_launcher.h"
+#include "taichi/backends/opengl/opengl_kernel_util.h"
+#include "taichi/common/core.h"
+#include "taichi/ir/offloaded_task_type.h"
+#include "taichi/ir/transforms.h"
 #define TI_RUNTIME_HOST
 #include "taichi/program/context.h"
 #undef TI_RUNTIME_HOST
 
-TLANG_NAMESPACE_BEGIN
+namespace taichi {
+namespace lang {
 
 class Kernel;
 class OffloadedStmt;
@@ -43,6 +44,7 @@ extern int opengl_threads_per_block;
 struct CompiledOffloadedTask {
   std::string name;
   std::string src;
+  OffloadedTaskType type;
   int workgroup_size;
   int num_groups;
 
@@ -74,6 +76,7 @@ struct CompiledTaichiKernel {
   void init_args(Kernel *kernel);
   void add(const std::string &name,
            const std::string &source_code,
+           OffloadedTaskType type,
            int num_workgrous,
            int workgroup_size,
            std::unordered_map<int, irpass::ExternalPtrAccess> *ext_ptr_access =
@@ -130,5 +133,5 @@ class DeviceCompiledTaichiKernel {
 };
 
 }  // namespace opengl
-
-TLANG_NAMESPACE_END
+}  // namespace lang
+}  // namespace taichi
--- taichi/program/aot_module.h
+++ taichi/program/aot_module.h
@@ -3,35 +3,15 @@
 #include <string>
 #include <vector>
 
-#include "taichi/ir/snode.h"
+#include "taichi/aot/module_data.h"
 #include "taichi/backends/device.h"
+#include "taichi/ir/snode.h"
+
 namespace taichi {
 namespace lang {
 
 class Kernel;
 class DataType;
-
-namespace aot {
-struct CompiledFieldData {
-  std::string field_name;
-  uint32_t dtype;
-  std::string dtype_name;
-  size_t mem_offset_in_parent{0};
-  std::vector<int> shape;
-  bool is_scalar{false};
-  int row_num{0};
-  int column_num{0};
-
-  TI_IO_DEF(field_name,
-            dtype,
-            dtype_name,
-            mem_offset_in_parent,
-            shape,
-            is_scalar,
-            row_num,
-            column_num);
-};
-}  // namespace aot
 
 class AotModuleLoader {
  public:

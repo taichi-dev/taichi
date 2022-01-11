9e73178c937b989f55eac75136e7b923e2a187e1
Yi Xu <xy_xuyi@foxmail.com>
2022-01-11 15:32:00 +0800

    [test] [example] Add a video generator for examples and test mpm99

---
+++ misc/generate_example_videos.py
@@ -0,0 +1,16 @@
+import re
+import os
+import subprocess
+import sys
+
+if len(sys.argv) != 2:
+    print('usage: generate_example_videos.py <output_directory>')
+    exit(0)
+
+example_root = os.path.join('..', 'tests', 'python', 'examples')
+for example_dir in os.listdir(example_root):
+    full_dir = os.path.join(example_root, example_dir)
+    for filename in os.listdir(full_dir):
+        match = re.match(r'test_(\w+)\.py', filename)
+        if match:
+            subprocess.run(["python", os.path.join(full_dir, filename), os.path.join(sys.argv[1], match.group(1))])
---
+++ tests/python/examples/simulation/test_mpm99.py
@@ -0,0 +1,37 @@
+import sys
+
+FRAMES = 100
+
+
+def test_mpm99():
+    from taichi.examples.simulation.mpm99 import dt, initialize, substep
+
+    initialize()
+    for i in range(FRAMES):
+        for s in range(int(2e-3 // dt)):
+            substep()
+
+
+def video_mpm99(result_dir):
+    from taichi.examples.simulation.mpm99 import (dt, initialize, material,
+                                                  substep, ti, x)
+
+    video_manager = ti.VideoManager(output_dir=result_dir, framerate=24,
+                                    automatic_build=False)
+    initialize()
+    gui = ti.GUI("Taichi MLS-MPM-99", res=512, background_color=0x112F41,
+                 show_gui=False)
+    for i in range(FRAMES):
+        for s in range(int(2e-3 // dt)):
+            substep()
+        gui.circles(x.to_numpy(), radius=1.5,
+                    palette=[0x068587, 0xED553B, 0xEEEEF0],
+                    palette_indices=material)
+        video_manager.write_frame(gui.get_image())
+        gui.clear()
+    video_manager.make_video(mp4=True, gif=False)
+
+
+if __name__ == '__main__':
+    assert len(sys.argv) == 2
+    video_mpm99(sys.argv[1])

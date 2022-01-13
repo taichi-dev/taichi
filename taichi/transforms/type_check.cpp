6e765ba9e96de3cbee4f8b1da3ba6a8ca5990bb5
XiangyunYang <xiangyunyang@taichi.graphics>
2022-01-13 11:53:54 +0800

    fix cpptest no kernel

--- taichi/transforms/type_check.cpp
+++ taichi/transforms/type_check.cpp
@@ -432,12 +432,15 @@ class TypeCheck : public IRVisitor {
 
   void visit(ReturnStmt *stmt) override {
     // TODO: Support stmt->ret_id?
-    TI_ASSERT(stmt->get_kernel()->rets.size() == 1);
-    if (auto ret_tensor = stmt->get_kernel()->rets[0].dt->cast<TensorType>()) {
-      TI_ASSERT(ret_tensor->get_num_elements() == stmt->values.size());
-    } else {
-      TI_ASSERT(stmt->values.size() == 1);
-      TI_ASSERT(stmt->values[0]->ret_type->vector_width() == 1);
+    if (stmt->get_kernel() != nullptr) {
+      TI_ASSERT(stmt->get_kernel()->rets.size() == 1);
+      if (auto ret_tensor =
+              stmt->get_kernel()->rets[0].dt->cast<TensorType>()) {
+        TI_ASSERT(ret_tensor->get_num_elements() == stmt->values.size());
+      } else {
+        TI_ASSERT(stmt->values.size() == 1);
+        TI_ASSERT(stmt->values[0]->ret_type->vector_width() == 1);
+      }
     }
   }
 

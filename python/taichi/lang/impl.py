e42e42c2eff9b01062c0197e6b0bf834979b35f1
XiangyunYang <xiangyunyang@taichi.graphics>
2022-01-13 10:30:51 +0800

    fix bug

--- python/taichi/lang/impl.py
+++ python/taichi/lang/impl.py
@@ -12,6 +12,7 @@ from taichi.lang.exception import InvalidOperationError
 from taichi.lang.expr import Expr, make_expr_group
 from taichi.lang.field import Field, ScalarField
 from taichi.lang.kernel_arguments import SparseMatrixProxy
+from taichi.lang.matrix import MatrixType  # pylint: disable=W0611
 from taichi.lang.matrix import (Matrix, MatrixField, _IntermediateMatrix,
                                 _MatrixFieldElement)
 from taichi.lang.mesh import (ConvType, MeshElementFieldProxy, MeshInstance,

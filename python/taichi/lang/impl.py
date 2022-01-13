99413f94d763d1bed3e6a94ef6b2db755eca6a66
XiangyunYang <xiangyunyang@taichi.graphics>
2022-01-13 09:36:57 +0800

    fix bug

--- python/taichi/lang/impl.py
+++ python/taichi/lang/impl.py
@@ -12,8 +12,8 @@ from taichi.lang.exception import InvalidOperationError
 from taichi.lang.expr import Expr, make_expr_group
 from taichi.lang.field import Field, ScalarField
 from taichi.lang.kernel_arguments import SparseMatrixProxy
-from taichi.lang.matrix import (Matrix, MatrixField, MatrixType,
-                                _IntermediateMatrix, _MatrixFieldElement)
+from taichi.lang.matrix import (Matrix, MatrixField, _IntermediateMatrix,
+                                _MatrixFieldElement)
 from taichi.lang.mesh import (ConvType, MeshElementFieldProxy, MeshInstance,
                               MeshRelationAccessProxy,
                               MeshReorderedMatrixFieldProxy,

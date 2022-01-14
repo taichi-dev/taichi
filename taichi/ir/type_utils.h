14735a57239dbb7b82b936478adca2d8482fd0cb
XiangyunYang <xiangyunyang@taichi.graphics>
2022-01-14 13:32:07 +0800

    remove unused function

--- taichi/ir/type_utils.h
+++ taichi/ir/type_utils.h
@@ -7,8 +7,6 @@ namespace lang {
 
 std::string data_type_name(DataType t);
 
-std::string data_types_name(std::vector<DataType> types);
-
 std::string data_type_format(DataType dt);
 
 int data_type_size(DataType t);

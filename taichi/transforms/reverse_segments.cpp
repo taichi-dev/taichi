c0c7406ac1fa0a263074f8cfbd2d75bdacb046a1
mingrui <mingrui.zhang@taichi.graphics>
2022-01-20 10:51:37 +0800

    polish the error message

--- taichi/transforms/reverse_segments.cpp
+++ taichi/transforms/reverse_segments.cpp
@@ -68,8 +68,8 @@ void reverse_segments(IRNode *root) {
     TI_ERROR(
         "Invalid program input for autodiff: "
         "Mixed usage of for-loops and statements without looping. \n"
-        "It is recommended to split them into two kernels."
-        "Please check the documentation:\n"
+        "Please split them into two kernels."
+        "Please check the documentation for more details:\n"
         "https://docs.taichi.graphics/lang/articles/advanced/"
         "differentiable_programming");
   }

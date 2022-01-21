9643518d59e05a0030b6e1a1614d9bb06091318a
quadpixels <suichen6@gmail.com>
2022-01-20 19:44:34 -0800

    [dx11] Add underlying DX11 device, memory allocation, and some tests (#3971)
    * [dx11] Add ID3D11Device and ID3D11DeviceContext
    
    This change adds the actual D3D11 device class (ID3D11Device and
    ID3D11DeviceContext) from <d3d11.h>.
    
    Tested: Tested with unit test and saw the Dx11Device and the D3D11
    device it creates can be successfully created. Enabled Graphics Debug
    and confirmed there are no remaining live objects when the test is
    finished.
    
    * [dx11] Add a class for getting info from the D3D11 info queue
    
    This class fetches information from the D3D11 info queue. This enables
    us to count the number of live D3D11 objects. This is most meant for
    usage with unit tests and during development.
    
    Tested: Tested with unit tests
    
    * [dx11] Allocate and deallocate memory
    
    This change adds Dx11Device::allocate_memory and
    Dx11Device::dealloc_memory.
    
    Tested: Tested with unit tests. The D3D11 objects (Buffer and UAV) are
    created and released as expected.
    
    * Auto Format
    
    * Don't build DX11 test when the backend is not built
    
    * Update prtags.json
    
    * [dx11] Make kD3d11DebugEnabled a compile-time constant
    
    This change makes kD3d11DebugEnabled a constant.
    
    Tested: rebuilt-and ran unit test with either kD3d11DebugEnabled set to
    ON or OFF.
    
    * Update taichi/backends/dx/dx_info_queue.h
    
    Co-authored-by: Ye Kuang <k-ye@users.noreply.github.com>
    
    * Auto Format
    
    Co-authored-by: Taichi Gardener <taichigardener@gmail.com>
    Co-authored-by: Bob Cao <bobcaocheng@gmail.com>
    Co-authored-by: Ye Kuang <k-ye@users.noreply.github.com>

---
+++ taichi/backends/dx/dx_info_queue.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "taichi/backends/device.h"
+#include <d3d11.h>
+
+namespace taichi {
+namespace lang {
+namespace directx11 {
+
+class Dx11InfoQueue {
+ public:
+  struct Entry {
+    std::string type;
+    void *addr;
+    int refcount;
+    int intref;
+  };
+  static std::vector<Entry> parse_reference_count(
+      const std::vector<std::string> &);
+  explicit Dx11InfoQueue(ID3D11Device *device);
+  int live_object_count();
+
+ private:
+  bool has_updated_messages();
+  std::vector<std::string> get_updated_messages();
+  std::vector<Entry> live_objects_;
+  void init();
+  ID3D11Device *device_{};
+  ID3D11Debug *debug_{};
+  ID3D11InfoQueue *info_queue_{};
+  int last_message_count_;
+};
+
+}  // namespace directx11
+}  // namespace lang
+}  // namespace taichi

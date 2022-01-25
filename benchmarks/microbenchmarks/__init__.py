a4ff87c970d39114007807c84c963c744029048b
yolo2themoon <markbench@163.com>
2022-01-25 13:49:43 +0800

    [misc] Add math operators throughout to micro-benchmarks

--- benchmarks/microbenchmarks/__init__.py
+++ benchmarks/microbenchmarks/__init__.py
@@ -1,3 +1,7 @@
 from .fill import FillPlan
+from .math_opts_throughput import MathOpsThroughputPlan
 
-benchmark_plan_list = [FillPlan]
+benchmark_plan_list = [
+    FillPlan,
+    MathOpsThroughputPlan
+]

fc4eb8e57ffa9c4605ce90bfd0b7a356e36b77de
Bo Qiao <boqiao@taichi.graphics>
2022-01-26 23:49:36 +0800

    Expose ti.FieldsBuilder

--- python/taichi/__init__.py
+++ python/taichi/__init__.py
@@ -3,6 +3,7 @@ import sys
 from taichi._funcs import *
 from taichi._lib import core as _ti_core
 from taichi._logging import *
+from taichi._snode import *
 from taichi.lang import *  # pylint: disable=W0622 # TODO(archibate): It's `taichi.lang.core` overriding `taichi.core`
 from taichi.tools import *
 from taichi.tools.patterns import taichi_logo
--- python/taichi/_snode/__init__.py
+++ python/taichi/_snode/__init__.py
@@ -1,1 +1,3 @@
 from taichi._snode.fields_builder import FieldsBuilder
+
+__all__ = ['FieldsBuilder']

2dd9f21dc5bc0fcb0eaa5bf4256e2b43817901e7
Bo Qiao <boqiao@taichi.graphics>
2022-02-07 15:04:59 +0800

    [misc] Add memcpy to micro-benchmarks

--- benchmarks/microbenchmarks/__init__.py
+++ benchmarks/microbenchmarks/__init__.py
@@ -1,5 +1,6 @@
 from .atomic_ops import AtomicOpsPlan
 from .fill import FillPlan
 from .math_opts_throughput import MathOpsThroughputPlan
+from .memcpy import MemcpyPlan
 
-benchmark_plan_list = [FillPlan, AtomicOpsPlan, MathOpsThroughputPlan]
+benchmark_plan_list = [FillPlan, AtomicOpsPlan, MathOpsThroughputPlan, MemcpyPlan]

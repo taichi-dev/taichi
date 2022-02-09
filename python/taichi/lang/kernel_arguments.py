5a13ad4b24f7ef60b649fa7f851f863378ec3ce8
PGZXB <420254146@qq.com>
2022-02-09 17:44:41 +0800

    Code format

--- python/taichi/lang/kernel_arguments.py
+++ python/taichi/lang/kernel_arguments.py
@@ -1,9 +1,9 @@
 import taichi.lang
 from taichi._lib import core as _ti_core
+from taichi.lang import impl
 from taichi.lang.any_array import AnyArray
 from taichi.lang.enums import Layout
 from taichi.lang.expr import Expr
-from taichi.lang import impl
 from taichi.lang.matrix import Matrix
 from taichi.lang.util import cook_dtype
 from taichi.types.primitive_types import u64

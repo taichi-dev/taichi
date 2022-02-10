3df5cb9d764193bdc32fa8ae95f8647f349bd3ef
Ailing <ailzhang@users.noreply.github.com>
2022-02-10 16:02:54 +0800

    [misc] Update master version to 0.9.0 (#4248)

--- setup.py
+++ setup.py
@@ -36,8 +36,8 @@ classifiers = [
 
 project_name = os.getenv('PROJECT_NAME', 'taichi')
 TI_VERSION_MAJOR = 0
-TI_VERSION_MINOR = 8
-TI_VERSION_PATCH = 12
+TI_VERSION_MINOR = 9
+TI_VERSION_PATCH = 0
 version = f'{TI_VERSION_MAJOR}.{TI_VERSION_MINOR}.{TI_VERSION_PATCH}'
 
 data_files = glob.glob('python/_lib/runtime/*')

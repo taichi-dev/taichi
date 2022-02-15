27665fa2b4577681669d72f7509a3b4e800d1f53
yolo2themoon <markbench@163.com>
2022-02-15 11:03:21 +0800

    [misc] Code cleanup in benchmarks

--- benchmarks/utils.py
+++ benchmarks/utils.py
@@ -4,34 +4,10 @@ import json
 import os
 
 import jsbeautifier
-from taichi._lib import core as ti_core
-
-import taichi as ti
 
 
 def get_benchmark_dir():
     return os.path.dirname(os.path.realpath(__file__))
-
-
-def benchmark_async(func):
-    @functools.wraps(func)
-    def body():
-        for arch in [ti.cpu, ti.cuda]:
-            for async_mode in [True, False]:
-                os.environ['TI_CURRENT_BENCHMARK'] = func.__name__
-                ti.init(arch=arch,
-                        async_mode=async_mode,
-                        kernel_profiler=True,
-                        verbose=False)
-                if arch == ti.cpu:
-                    scale = 2
-                else:
-                    # Use more data to hide compilation overhead
-                    # (since CUDA runs much faster than CPUs)
-                    scale = 64
-                func(scale)
-
-    return body
 
 
 def dump2json(obj):
@@ -43,7 +19,3 @@ def dump2json(obj):
 
 def datatime_with_format():
     return datetime.datetime.now().isoformat()
-
-
-def get_commit_hash():
-    return ti_core.get_commit_hash()

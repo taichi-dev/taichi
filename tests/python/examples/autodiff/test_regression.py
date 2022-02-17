c721b8659737f7ef277c23503a4e489c6c7b1153
Tiny-Box <tinybox.17@gmail.com>
2022-02-17 18:27:22 +0800

    [Test] Add UT about autodiff/regression

---
+++ tests/python/examples/autodiff/test_regression.py
@@ -0,0 +1,58 @@
+import argparse
+
+
+def mkdir_p(dir_path):
+    '''Creates a directory. equivalent to using mkdir -p on the command line'''
+    from errno import EEXIST
+    from os import makedirs,path
+
+    try:
+        makedirs(dir_path)
+    except OSError as exc: # Python >2.5
+        if exc.errno == EEXIST and path.isdir(dir_path):
+            pass
+        else: raise
+
+
+def test_regression():
+    from taichi.examples.autodiff.regression import initialize, regress_raw
+    initialize()
+    regress_raw()
+
+
+def pic_regression(result_dir):
+    from taichi.examples.autodiff.regression import (initialize, regress_raw,
+                                                            number_coeffs, coeffs,
+                                                            xs, ys)
+    from matplotlib import pyplot as plt
+    import numpy as np
+    initialize()
+    regress_raw()
+
+    curve_xs = np.arange(-2.5, 2.5, 0.01)
+    curve_ys = curve_xs * 0
+    for i in range(number_coeffs):
+        curve_ys += coeffs[i] * np.power(curve_xs, i)
+
+    plt.title('Nonlinear Regression with Gradient Descent (3rd order polynomial)')
+    ax = plt.gca()
+    ax.scatter(xs, ys, label='data', color='r')
+    ax.plot(curve_xs, curve_ys, label='fitted')
+    ax.legend()
+    ax.grid(True)
+    ax.spines['left'].set_position('zero')
+    ax.spines['right'].set_color('none')
+    ax.spines['bottom'].set_position('zero')
+    ax.spines['top'].set_color('none')
+    print(result_dir + '/output.png')
+
+    # Create new directory
+    mkdir_p(result_dir)
+    plt.savefig(result_dir + '/output.png')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Generate regression pic')
+    parser.add_argument('output_directory',
+                        help='output directory of generated pic')
+    pic_regression(parser.parse_args().output_directory)

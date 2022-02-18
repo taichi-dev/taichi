f8613cf2ce5526a788124f453d84cbbbaa37e186
Yi Xu <xy_xuyi@foxmail.com>
2022-02-18 17:28:41 +0800

    Make pylint happy

--- python/taichi/ui/ui.py
+++ python/taichi/ui/ui.py
@@ -1,11 +1,11 @@
 from taichi._lib import core as _ti_core
 
-from .camera import Camera
-from .canvas import Canvas
-from .constants import *
-from .imgui import Gui
-from .scene import Scene
-from .window import Window
+from .camera import Camera  # pylint: disable=unused-import
+from .canvas import Canvas  # pylint: disable=unused-import
+from .constants import *  # pylint: disable=unused-import,wildcard-import
+from .imgui import Gui  # pylint: disable=unused-import
+from .scene import Scene  # pylint: disable=unused-import
+from .window import Window  # pylint: disable=unused-import
 from .utils import check_ggui_availability
 
 

a0dae1513611b99aa5383729710725a70d0cd193
lin-hitonami <linjiang@taichi.graphics>
2022-02-18 15:53:30 +0800

    [lang] Remove is_integral and is_signed

--- python/taichi/types/__init__.py
+++ python/taichi/types/__init__.py
@@ -2,4 +2,3 @@ from taichi.types.annotations import *
 from taichi.types.compound_types import *
 from taichi.types.primitive_types import *
 from taichi.types.quantized_types import *
-from taichi.types.utils import *
--- python/taichi/types/utils.py
+++
@@ -1,7 +0,0 @@
-from taichi._lib import core as ti_core
-
-is_signed = ti_core.is_signed
-
-is_integral = ti_core.is_integral
-
-__all__ = ['is_signed', 'is_integral']

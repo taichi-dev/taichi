f529344e42ca7b8c918ab89b14cccc263b1744f2
Ye Kuang <yekuang.ky@gmail.com>
2022-02-26 00:51:39 +0800

    fix

--- taichi/aot/module_loader.cpp
+++ taichi/aot/module_loader.cpp
@@ -6,7 +6,7 @@ namespace lang {
 AotKernel *AotModuleLoader::get_kernel(const std::string &name) {
   auto itr = loaded_kernels_.find(name);
   if (itr != loaded_kernels_.end()) {
-    return itr->second.get();
+    return itr->second;
   }
   auto k = make_new_kernel(name);
   auto *kptr = k.get();

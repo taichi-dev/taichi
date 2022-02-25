a09a6f18ef64d96b368cec74f582c0c14c51eee7
Frost Ming <mianghong@gmail.com>
2022-02-25 12:20:52 +0800

    Update .github/workflows/scripts/win_test.ps1

--- .github/workflows/scripts/win_test.ps1
+++ .github/workflows/scripts/win_test.ps1
@@ -7,12 +7,11 @@ ti changelog
 echo wanted arch: $env:TI_WANTED_ARCHS
 pip install -r requirements_test.txt
 # TODO relax this when torch supports 3.10
-if (-not "$env:PYTHON".Contains("3.10")) {
-  if ("$env:TI_WANTED_ARCHS".Contains("cuda")) {
-      pip install torch==1.10.1+cu113 -f https://download.pytorch.org/whl/cu113/torch_stable.html
-  } else {
-      pip install torch
-  }
+if ("$env:TI_WANTED_ARCHS".Contains("cuda")) {
+    pip install "torch==1.10.1+cu113; python_version < '3.10'" -f https://download.pytorch.org/whl/cu113/torch_stable.html
+} else {
+    pip install torch
+}
 }
 if ("$env:TI_WANTED_ARCHS".Contains("cuda")) {
   python tests/run_tests.py -vr2 -t4 -k "not torch" -a cuda

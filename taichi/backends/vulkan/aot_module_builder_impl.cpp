8fd01497af018f5e57c83bd2f6e51e8b4b42b20c
Ye Kuang <k-ye@users.noreply.github.com>
2022-02-25 10:53:29 +0800

    Update taichi/backends/vulkan/aot_module_builder_impl.cpp

--- taichi/backends/vulkan/aot_module_builder_impl.cpp
+++ taichi/backends/vulkan/aot_module_builder_impl.cpp
@@ -54,7 +54,7 @@ class AotDataConverter {
         arr_arg.dtype_name = arg.dt.to_string();
         arr_arg.field_dim = arg.field_dim;
         arr_arg.element_shape = arg.element_shape;
-        arr_arg.shape_offset_in_args_buf = arg.index * sizeof(int);
+        arr_arg.shape_offset_in_args_buf = arg.index * sizeof(int32_t);
         res.arr_args[arg.index] = arr_arg;
       }
     }

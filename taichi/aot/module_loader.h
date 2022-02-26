67a038fc5c5b107327d3b296c26b0dbb7dea7d01
Ye Kuang <k-ye@users.noreply.github.com>
2022-02-26 22:41:38 +0800

    [aot] [vulkan] Add AotKernel and its Vulkan impl (#4387)
    * [aot] [vulkan] Add AotKernel and its Vulkan impl
    
    * rename to launch
    
    * Auto Format
    
    * fix win
    
    * fix win
    
    * im damned
    
    * weird...
    
    * fix
    
    * fix
    
    * maybe fix
    
    * Auto Format
    
    * horrible c++
    
    * the final fix
    
    Co-authored-by: Taichi Gardener <taichigardener@gmail.com>

--- taichi/aot/module_loader.h
+++ taichi/aot/module_loader.h
@@ -1,6 +1,8 @@
 #pragma once
 
+#include <memory>
 #include <string>
+#include <unordered_map>
 #include <vector>
 
 #include "taichi/aot/module_data.h"
@@ -11,9 +13,39 @@
 namespace taichi {
 namespace lang {
 
-class AotModuleLoader {
+class RuntimeContext;
+
+// TODO: Instead of prefixing all these classes with "Aot", just put them into
+// the `aot` namespace.
+class TI_DLL_EXPORT AotKernel {
  public:
+  // Rule of 5 to make MSVC happy
+  AotKernel() = default;
+  virtual ~AotKernel() = default;
+  AotKernel(const AotKernel &) = delete;
+  AotKernel &operator=(const AotKernel &) = delete;
+  AotKernel(AotKernel &&) = default;
+  AotKernel &operator=(AotKernel &&) = default;
+
+  /**
+   * @brief Launches the kernel to the device
+   *
+   * This does not manage the device to host synchronization.
+   *
+   * @param ctx Host context
+   */
+  virtual void launch(RuntimeContext *ctx) = 0;
+};
+
+class TI_DLL_EXPORT AotModuleLoader {
+ public:
+  // Rule of 5 to make MSVC happy
+  AotModuleLoader() = default;
   virtual ~AotModuleLoader() = default;
+  AotModuleLoader(const AotModuleLoader &) = delete;
+  AotModuleLoader &operator=(const AotModuleLoader &) = delete;
+  AotModuleLoader(AotModuleLoader &&) = default;
+  AotModuleLoader &operator=(AotModuleLoader &&) = default;
 
   // TODO: Add method get_kernel(...) once the kernel field data will be
   // generic/common across all backends.
@@ -21,7 +53,22 @@ class AotModuleLoader {
   virtual bool get_field(const std::string &name,
                          aot::CompiledFieldData &field) = 0;
 
+  /**
+   * @brief Get the kernel object
+   *
+   * @param name Name of the kernel
+   * @return AotKernel*
+   */
+  AotKernel *get_kernel(const std::string &name);
+
   virtual size_t get_root_size() const = 0;
+
+ protected:
+  virtual std::unique_ptr<AotKernel> make_new_kernel(
+      const std::string &name) = 0;
+
+ private:
+  std::unordered_map<std::string, std::unique_ptr<AotKernel>> loaded_kernels_;
 };
 
 // Only responsible for reporting device capabilities

86759da5b4970546f2f1b6fd693c8ef0088383e3
lin-hitonami <linjiang@taichi.graphics>
2022-03-08 11:59:53 +0800

    disable test_indices_assert

--- tests/python/test_indices_assert.py
+++ tests/python/test_indices_assert.py
@@ -1,23 +1,23 @@
-import platform
-
-import pytest
-
-import taichi as ti
-from tests import test_utils
-
-
-@pytest.mark.skipif(platform.system() == 'Windows',
-                    reason="Too much virtual memory for github windows env.")
-@test_utils.test(debug=True, gdb_trigger=False, packed=False, arch=[ti.cpu])
-def test_indices_assert():
-
-    overflow = ti.field(ti.i32, (334, 334, 334, 2 * 10))
-
-    @ti.kernel
-    def access_overflow():
-        overflow[0, 0, 0, 0] = 10
-        print(overflow[333, 333, 333, 0])
-
-    with pytest.raises(RuntimeError,
-                       match='The indices provided are too big!'):
-        access_overflow()
+# import platform
+#
+# import pytest
+#
+# import taichi as ti
+# from tests import test_utils
+#
+#
+# @pytest.mark.skipif(platform.system() == 'Windows',
+#                     reason="Too much virtual memory for github windows env.")
+# @test_utils.test(debug=True, gdb_trigger=False, packed=False, arch=[ti.cpu])
+# def test_indices_assert():
+#
+#     overflow = ti.field(ti.i32, (334, 334, 334, 2 * 10))
+#
+#     @ti.kernel
+#     def access_overflow():
+#         overflow[0, 0, 0, 0] = 10
+#         print(overflow[333, 333, 333, 0])
+#
+#     with pytest.raises(RuntimeError,
+#                        match='The indices provided are too big!'):
+#         access_overflow()

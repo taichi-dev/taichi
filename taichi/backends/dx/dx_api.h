1981bdd6b928c3a392bc1a8eede210f09b3d5469
Ailing <ailzhang@users.noreply.github.com>
2022-03-11 15:14:43 +0800

    [build] Guard dx lib search with TI_WITH_DX11 (#4505)

--- taichi/backends/dx/dx_api.h
+++ taichi/backends/dx/dx_api.h
@@ -1,11 +1,10 @@
 #pragma once
-#pragma comment(lib, "d3d11.lib")
-#pragma comment(lib, "d3dcompiler.lib")
-#pragma comment(lib, "dxguid.lib")
-
 #include "taichi/common/core.h"
 
 #ifdef TI_WITH_DX11
+#pragma comment(lib, "d3d11.lib")
+#pragma comment(lib, "d3dcompiler.lib")
+#pragma comment(lib, "dxguid.lib")
 #include <d3d11.h>
 #endif
 

14e4b9a11a56f150b77b24ad6dd2e4a3bc8ba288
Taichi Gardener <taichigardener@gmail.com>
2022-03-15 07:27:39 +0000

    Auto Format

--- taichi/backends/vulkan/runtime.cpp
+++ taichi/backends/vulkan/runtime.cpp
@@ -591,10 +591,10 @@ void VkRuntime::add_root_buffer(size_t root_buffer_size) {
     root_buffer_size = 4;  // there might be empty roots
   }
   std::unique_ptr<DeviceAllocationGuard> new_buffer =
-      device_->allocate_memory_unique({root_buffer_size,
-                                       /*host_write=*/false, /*host_read=*/false,
-                                       /*export_sharing=*/false,
-                                       AllocUsage::Storage});
+      device_->allocate_memory_unique(
+          {root_buffer_size,
+           /*host_write=*/false, /*host_read=*/false,
+           /*export_sharing=*/false, AllocUsage::Storage});
   Stream *stream = device_->get_compute_stream();
   auto cmdlist = stream->new_command_list();
   cmdlist->buffer_fill(new_buffer->get_ptr(0), root_buffer_size, /*data=*/0);

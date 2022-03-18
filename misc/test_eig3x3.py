bafbb9ed816b8246702b784309a585fa0a8b8d7f
FantasyVR <y2505418927@gmail.com>
2022-03-18 16:28:48 +0800

    format

--- misc/test_eig3x3.py
+++ misc/test_eig3x3.py
@@ -1,10 +1,13 @@
-import taichi as ti 
+import taichi as ti
+
 ti.init()
+
 
 @ti.kernel
 def test():
-    A = ti.Matrix([[3, 1, 1],[1, 2, 2], [1, 2, 2]])
+    A = ti.Matrix([[3, 1, 1], [1, 2, 2], [1, 2, 2]])
     diagnal = ti.sym_eig(A)
     print(diagnal)
+
 
 test()
--- taichi/math/eig.h
+++ taichi/math/eig.h
@@ -21,24 +21,24 @@ std::tuple<Expr, Expr, Expr> eig_3x3_export(ASTBuilder *ast_builder,
   auto Var =
       std::bind(&ASTBuilder::make_var, ast_builder, std::placeholders::_1);
 
-    auto A00 = Var(Expr(Tf(0.0)));
-    auto A01 = Var(Expr(Tf(0.0)));
-    auto A02 = Var(Expr(Tf(0.0)));
-    auto A10 = Var(Expr(Tf(0.0)));
-    auto A11 = Var(Expr(Tf(0.0)));
-    auto A12 = Var(Expr(Tf(0.0)));
-    auto A20 = Var(Expr(Tf(0.0)));
-    auto A21 = Var(Expr(Tf(0.0)));
-    auto A22 = Var(Expr(Tf(0.0)));
-    ast_builder->insert_assignment(A00, a00);
-    ast_builder->insert_assignment(A01, a01);
-    ast_builder->insert_assignment(A02, a02);
-    ast_builder->insert_assignment(A10, a10);
-    ast_builder->insert_assignment(A11, a11);
-    ast_builder->insert_assignment(A12, a12);
-    ast_builder->insert_assignment(A20, a20);
-    ast_builder->insert_assignment(A21, a21);
-    ast_builder->insert_assignment(A22, a22);
+  auto A00 = Var(Expr(Tf(0.0)));
+  auto A01 = Var(Expr(Tf(0.0)));
+  auto A02 = Var(Expr(Tf(0.0)));
+  auto A10 = Var(Expr(Tf(0.0)));
+  auto A11 = Var(Expr(Tf(0.0)));
+  auto A12 = Var(Expr(Tf(0.0)));
+  auto A20 = Var(Expr(Tf(0.0)));
+  auto A21 = Var(Expr(Tf(0.0)));
+  auto A22 = Var(Expr(Tf(0.0)));
+  ast_builder->insert_assignment(A00, a00);
+  ast_builder->insert_assignment(A01, a01);
+  ast_builder->insert_assignment(A02, a02);
+  ast_builder->insert_assignment(A10, a10);
+  ast_builder->insert_assignment(A11, a11);
+  ast_builder->insert_assignment(A12, a12);
+  ast_builder->insert_assignment(A20, a20);
+  ast_builder->insert_assignment(A21, a21);
+  ast_builder->insert_assignment(A22, a22);
 
   auto w0 = Var(Expr(Tf(0.0)));
   auto w1 = Var(Expr(Tf(0.0)));
@@ -63,15 +63,23 @@ std::tuple<Expr, Expr, Expr> eig_3x3_export(ASTBuilder *ast_builder,
   ast_builder->insert_assignment(ee, A12 * A12);
   ast_builder->insert_assignment(ff, A02 * A02);
   ast_builder->insert_assignment(m, A00 + A11 + A22);
-  ast_builder->insert_assignment(c1, A00 * A11 + A00 * A22 + A11 * A22 - (dd + ee + ff));
-  ast_builder->insert_assignment(c0, A22 * dd + A00 * ee + A11 * ff - A00 * A11 * A22 - Expr(Tf(2.0)) * A02 * de);
+  ast_builder->insert_assignment(
+      c1, A00 * A11 + A00 * A22 + A11 * A22 - (dd + ee + ff));
+  ast_builder->insert_assignment(c0, A22 * dd + A00 * ee + A11 * ff -
+                                         A00 * A11 * A22 -
+                                         Expr(Tf(2.0)) * A02 * de);
   ast_builder->insert_assignment(p, m * m - Expr(Tf(3.0)) * c1);
-  ast_builder->insert_assignment(q, m * (p - Expr(Tf(1.5)) * c1) - Expr(Tf(13.5)) * c0);
+  ast_builder->insert_assignment(
+      q, m * (p - Expr(Tf(1.5)) * c1) - Expr(Tf(13.5)) * c0);
   ast_builder->insert_assignment(sqrt_p, sqrt(abs(p)));
-  ast_builder->insert_assignment(phi, Expr(Tf(27.0)) * (Expr(Tf(0.25)) * c1 * c1 * (p - c1) + c0 * (q + Expr(Tf(6.75)) * c0)));
-  ast_builder->insert_assignment(phi, Expr(Tf(1.0))/ Expr(Tf(3.0)) * atan2(sqrt(abs(phi)), q));
+  ast_builder->insert_assignment(
+      phi, Expr(Tf(27.0)) * (Expr(Tf(0.25)) * c1 * c1 * (p - c1) +
+                             c0 * (q + Expr(Tf(6.75)) * c0)));
+  ast_builder->insert_assignment(
+      phi, Expr(Tf(1.0)) / Expr(Tf(3.0)) * atan2(sqrt(abs(phi)), q));
   ast_builder->insert_assignment(c, sqrt_p * cos(phi));
-  ast_builder->insert_assignment(s, Expr(Tf(1.0)) / Expr(M_SQRT3) * sqrt_p * sin(phi));
+  ast_builder->insert_assignment(
+      s, Expr(Tf(1.0)) / Expr(M_SQRT3) * sqrt_p * sin(phi));
 
   ast_builder->insert_assignment(w2, Expr(Tf(1.0)) / Expr(Tf(3.0)) * (m - c));
   ast_builder->insert_assignment(w1, w2 + s);

5e03f07d1b869d0b7209601f98cda9d17c39f97a
neozhaoliang <mathzhaoliang@gmail.com>
2022-03-22 09:21:19 +0800

    update docstring for ui Window

--- python/taichi/ui/ui.py
+++ python/taichi/ui/ui.py
@@ -10,6 +10,17 @@ from .window import Window  # pylint: disable=unused-import
 
 
 def make_camera():
+    """Return an instance of :class:`~taichi.ui.Camera`. This is the
+    recommended way to create a camera in ggui.
+
+    You should also mannually set the camera parameters like `camera.position`,
+    `camera.lookat`, `camera.up`, etc. The default settings may not work for
+    your scene.
+
+    Example::
+
+        >>> camera = ti.ui.make_camera()
+    """
     check_ggui_availability()
     return Camera(_ti_core.PyCamera())
 

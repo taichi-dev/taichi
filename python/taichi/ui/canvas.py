e19f0a5633f1ebec0a5fde8222f8ed6c2c29b94f
neozhaoliang <mathzhaoliang@gmail.com>
2022-03-22 10:14:19 +0800

    update docstring for ui Canvas

--- python/taichi/ui/canvas.py
+++ python/taichi/ui/canvas.py
@@ -4,13 +4,31 @@ from .utils import get_field_info
 
 
 class Canvas:
+
+    """The Canvas class.
+
+    This is the context manager for managing drawing commands on a window.
+    You should not instantiate this class directly via `__init__`, instead
+    please call the `get_canvas()` method of :class:`~taichi.ui.Window`.
+    """
     def __init__(self, canvas) -> None:
         self.canvas = canvas  #reference to a PyCanvas
 
     def set_background_color(self, color):
+        """Set the background color of this canvas.
+
+        Args:
+            color (tuple(float)): RGB triple in the range [0, 1].
+        """
         self.canvas.set_background_color(color)
 
     def set_image(self, img):
+        """Set the content of this canvas to an `img`.
+
+        Args:
+            img (numpy.ndarray, :class:`~taichi.MatrixField`, :class:`~taichi.Field`): \
+                the image to be shown.
+        """
         staging_img = to_u8_rgba(img)
         info = get_field_info(staging_img)
         self.canvas.set_image(info)
@@ -20,13 +38,19 @@ class Canvas:
                   color=(0.5, 0.5, 0.5),
                   indices=None,
                   per_vertex_color=None):
-        """Declare a set of 2D triangles inside the scene.
+        """Draw a set of 2D triangles on this canvas.
 
         Args:
-            vertices: a taichi 2D Vector field, where each element indicate the 3D location of a vertex.
-            indices: a taichi int field of shape (3 * #triangles), which indicate the vertex indices of the triangles. If this is None, then it is assumed that the vertices are already arranged in triangles order.
-            color: a global color for the triangles as 3 floats representing RGB values. If `per_vertex_color` is provided, this is ignored.
-            per_vertex_color (Tuple[float]): a taichi 3D vector field, where each element indicate the RGB color of a vertex.
+            vertices: a taichi 2D Vector field, where each element indicate \
+                the 3D location of a vertex.
+            indices: a taichi int field of shape (3 * #triangles), which \
+                indicate the vertex indices of the triangles. If this is None, \
+                then it is assumed that the vertices are already arranged in \
+                triangles order.
+            color: a global color for the triangles as 3 floats representing \
+                RGB values. If `per_vertex_color` is provided, this is ignored.
+            per_vertex_color (Tuple[float]): a taichi 3D vector field, \
+                where each element indicate the RGB color of a vertex.
         """
         vbo = get_vbo_field(vertices)
         copy_vertices_to_vbo(vbo, vertices)
@@ -44,14 +68,19 @@ class Canvas:
               indices=None,
               color=(0.5, 0.5, 0.5),
               per_vertex_color=None):
-        """Declare a set of 2D lines inside the scene.
+        """Draw a set of 2D lines on this canvas.
 
         Args:
-            vertices: a taichi 2D Vector field, where each element indicate the 3D location of a vertex.
+            vertices: a taichi 2D Vector field, where each element indicate the \
+                3D location of a vertex.
             width (float): width of the lines, relative to the height of the screen.
-            indices: a taichi int field of shape (2 * #lines), which indicate the vertex indices of the lines. If this is None, then it is assumed that the vertices are already arranged in lines order.
-            color: a global color for the triangles as 3 floats representing RGB values. If `per_vertex_color` is provided, this is ignored.
-            per_vertex_color (Tuple[float]): a taichi 3D vector field, where each element indicate the RGB color of a vertex.
+            indices: a taichi int field of shape (2 * #lines), which indicate \
+                the vertex indices of the lines. If this is None, then it is \
+                assumed that the vertices are already arranged in lines order.
+            color: a global color for the triangles as 3 floats representing \
+                RGB values. If `per_vertex_color` is provided, this is ignored.
+            per_vertex_color (tuple[float]): a taichi 3D vector field, where \
+                each element indicate the RGB color of a vertex.
         """
         vbo = get_vbo_field(vertices)
         copy_vertices_to_vbo(vbo, vertices)
@@ -68,13 +97,16 @@ class Canvas:
                 radius,
                 color=(0.5, 0.5, 0.5),
                 per_vertex_color=None):
-        """Declare a set of 2D circles inside the scene.
+        """Draw a set of 2D circles on this canvas.
 
         Args:
-            centers: a taichi 2D Vector field, where each element indicate the 3D location of a vertex.
+            centers: a taichi 2D Vector field, where each element indicate the \
+                3D location of a vertex.
             radius (float): radius of the circles, relative to the height of the screen.
-            color: a global color for the triangles as 3 floats representing RGB values. If `per_vertex_color` is provided, this is ignored.
-            per_vertex_color (Tuple[float]): a taichi 3D vector field, where each element indicate the RGB color of a circle.
+            color: a global color for the triangles as 3 floats representing \
+                RGB values. If `per_vertex_color` is provided, this is ignored.
+            per_vertex_color (Tuple[float]): a taichi 3D vector field, where \
+                each element indicate the RGB color of a circle.
         """
         vbo = get_vbo_field(centers)
         copy_vertices_to_vbo(vbo, centers)

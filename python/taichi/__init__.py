13f2bc87597c36fd57daffc2ec577b986e74b4be
Jiasheng Zhang <leonz5288@hotmail.com>
2022-03-23 12:51:23 +0800

    [lang] Replace keywords in python (#4606)
    * [lang] Replace keywords in python
    
    * prefix origin and replace

--- python/taichi/__init__.py
+++ python/taichi/__init__.py
@@ -50,8 +50,8 @@ __customized_deprecations__ = {
 if sys.version_info.minor < 7:
     for name, alter in __deprecated_names__.items():
         exec(f'{name} = {alter}')
-    for attribute, (message, func) in __customized_deprecations__.items():
-        exec(f'{attribute} = {func}')
+    for _origin, (_msg, _replace) in __customized_deprecations__.items():
+        exec(f'{_origin} = {_replace}')
 else:
 
     def __getattr__(attr):

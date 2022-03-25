a3071d736a35c3358c6715a1dbd3559acddf83e2
Yuanming Hu <yuanmhu@gmail.com>
2022-03-25 19:04:46 +0800

    add test

---
+++ tests/python/test_simt.py
@@ -0,0 +1,24 @@
+import taichi as ti
+from tests import test_utils
+
+
+@test_utils.test(arch=ti.cuda)
+def test_shfl_down_i32():
+    a = ti.field(dtype=ti.i32, shape=32)
+    b = ti.field(dtype=ti.i32, shape=32)
+
+
+    @ti.kernel
+    def foo():
+        ti.loop_config(block_dim=32)
+        for i in range(32):
+            a[i] = ti.lang.shfl_down_i32(ti.u32(0xFFFFFFFF), b[i], 1)
+
+
+    for i in range(32):
+        b[i] = i * i
+
+    foo()
+
+    for i in range(31):
+        assert a[i] == b[i + 1]

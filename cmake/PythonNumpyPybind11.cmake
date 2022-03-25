82e24d91dd0a1070cd49fbf6a63ba7e5e4703386
Frost Ming <mianghong@gmail.com>
2022-03-25 16:14:05 +0800

    Add more message

--- cmake/PythonNumpyPybind11.cmake
+++ cmake/PythonNumpyPybind11.cmake
@@ -3,6 +3,7 @@ find_package(Python REQUIRED COMPONENTS Interpreter Development NumPy)
 set(pybind11_DIR ${PYBIND11_CMAKE_DIR})
 find_package(pybind11 CONFIG REQUIRED)
 
+message("Using ${Python_EXECUTABLE} as the interpreter")
 message("    version: ${Python_VERSION}")
 message("    include: ${Python_INCLUDE_DIRS}")
 message("    library: ${Python_LIBRARIES}")

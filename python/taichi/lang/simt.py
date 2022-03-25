a068cf9625407fa155e8f0e1e39887b07a6bb2aa
Yuanming Hu <yuanmhu@gmail.com>
2022-03-25 22:45:00 +0800

    add stubs

--- python/taichi/lang/simt.py
+++ python/taichi/lang/simt.py
@@ -1,6 +1,25 @@
 from taichi._lib import core as _ti_core
 from taichi.lang import expr
 
+def all():
+    # TODO
+    pass
+
+def any():
+    # TODO
+    pass
+
+def uni():
+    # TODO
+    pass
+
+def ballot():
+    # TODO
+    pass
+
+def shfl_i32(mask, val, offset):
+    # TODO
+    pass
 
 def shfl_down_i32(mask, val, offset):
     return expr.Expr(
@@ -8,5 +27,40 @@ def shfl_down_i32(mask, val, offset):
             "cuda_shfl_down_sync_i32",
             expr.make_expr_group(mask, val, offset, 31), False))
 
+def shfl_up_i32(mask, val, offset):
+    # TODO
+    pass
 
-__all__ = ['shfl_down_i32']
+def shfl_xor_i32(mask, val, offset):
+    # TODO
+    pass
+
+def match_any():
+    # TODO
+    pass
+
+def match_all():
+    # TODO
+    pass
+
+def active_mask():
+    # TODO
+    pass
+
+def sync():
+    # TODO
+    pass
+
+__all__ = [
+    'all',
+    'any',
+    'uni',
+    'ballot',
+    'shfl_i32',
+    'shfl_up_i32',
+    'shfl_down_i32',
+    'match_any',
+    'match_all',
+    'active_mask',
+    'sync',
+]
--- tests/python/test_simt.py
+++ tests/python/test_simt.py
@@ -1,6 +1,35 @@
 import taichi as ti
 from tests import test_utils
 
+@test_utils.test(arch=ti.cuda)
+def test_all():
+    # TODO
+    pass
+
+@test_utils.test(arch=ti.cuda)
+def test_any():
+    # TODO
+    pass
+
+@test_utils.test(arch=ti.cuda)
+def test_ballot():
+    # TODO
+    pass
+
+@test_utils.test(arch=ti.cuda)
+def test_uni():
+    # TODO
+    pass
+
+@test_utils.test(arch=ti.cuda)
+def test_shfl_i32():
+    # TODO
+    pass
+
+@test_utils.test(arch=ti.cuda)
+def test_shfl_up_i32():
+    # TODO
+    pass
 
 @test_utils.test(arch=ti.cuda)
 def test_shfl_down_i32():
@@ -20,3 +49,23 @@ def test_shfl_down_i32():
 
     for i in range(31):
         assert a[i] == b[i + 1]
+
+@test_utils.test(arch=ti.cuda)
+def test_match_any():
+    # TODO
+    pass
+
+@test_utils.test(arch=ti.cuda)
+def test_match_all():
+    # TODO
+    pass
+
+@test_utils.test(arch=ti.cuda)
+def test_match_active_mask():
+    # TODO
+    pass
+
+@test_utils.test(arch=ti.cuda)
+def test_match_active_sync():
+    # TODO
+    pass

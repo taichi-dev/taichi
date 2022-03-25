ac1ba3df40704bab794cb3b74c4a5f0afac1e0fb
Frost Ming <mianghong@gmail.com>
2022-03-25 18:13:22 +0800

    Kickstart CI

--- cmake/PythonNumpyPybind11.cmake
+++ cmake/PythonNumpyPybind11.cmake
@@ -11,7 +11,7 @@ find_package(Python REQUIRED COMPONENTS Development.Module NumPy)
 set(pybind11_DIR ${PYBIND11_CMAKE_DIR})
 find_package(pybind11 CONFIG REQUIRED)
 
-message("Using ${Python_EXECUTABLE} as the interpreter")
+message("-- Python: Using ${Python_EXECUTABLE} as the interpreter")
 message("    version: ${Python_VERSION}")
 message("    include: ${Python_INCLUDE_DIRS}")
 message("    library: ${Python_LIBRARIES}")

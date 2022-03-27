2074224bded388b09c8eeb939193fbc8d6cff7a9
Andrew Sun <victoriacity74@gmail.com>
2022-03-26 23:58:07 -0500

    Add test

---
+++ tests/python/test_type_promotion.py
@@ -0,0 +1,61 @@
+import numpy as np
+import pytest
+
+import taichi as ti
+from tests import test_utils
+
+
+@test_utils.test()
+def test_log():
+    N = 16
+    x = ti.field(ti.i32, shape=(N,))
+    y = ti.field(ti.f32, shape=(N,))
+
+    @ti.kernel
+    def func():
+        for i in range(N):
+            u = ti.log(x[i])
+            y[i] = u
+
+    x.from_numpy(np.arange(1, N + 1))
+
+    func()
+
+    assert np.allclose(y.to_numpy(), np.log(x.to_numpy()))
+
+
+@test_utils.test()
+def test_exp():
+    N = 16
+    x = ti.field(ti.i32, shape=(N,))
+    y = ti.field(ti.f32, shape=(N,))
+
+    @ti.kernel
+    def func():
+        for i in range(N):
+            u = ti.exp(x[i])
+            y[i] = u
+
+    x.from_numpy(np.arange(1, N + 1))
+
+    func()
+
+    assert np.allclose(y.to_numpy(), np.exp(x.to_numpy()))
+
+@test_utils.test()
+def test_sqrt():
+    N = 16
+    x = ti.field(ti.i32, shape=(N,))
+    y = ti.field(ti.f32, shape=(N,))
+
+    @ti.kernel
+    def func():
+        for i in range(N):
+            u = ti.sqrt(x[i])
+            y[i] = u
+
+    x.from_numpy(np.arange(1, N + 1))
+
+    func()
+
+    assert np.allclose(y.to_numpy(), np.sqrt(x.to_numpy()))

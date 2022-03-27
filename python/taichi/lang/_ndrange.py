8402a4030c7df6db8a5f6ec1afff8da5e15c548b
此Repo仅供测试用 <xidalapuda@126.com>
2022-03-27 10:20:54 +0800

    Update _ndrange.py

--- python/taichi/lang/_ndrange.py
+++ python/taichi/lang/_ndrange.py
@@ -18,6 +18,8 @@ class _Ndrange:
 
         self.dimensions = [None] * len(args)
         for i, bound in enumerate(self.bounds):
+            if not (isinstance(bound[0], int) and isinstance(bound[1], int)):
+                raise TypeError("Float object cannot be interpreted as an integer in ndrange")
             self.dimensions[i] = bound[1] - bound[0]
 
         self.acc_dimensions = self.dimensions.copy()

c902db6236648d938656c2ededc8062284da3291
Yu Zhang <YuCrazing@users.noreply.github.com>
2022-03-27 04:46:43 -0700

    [SIMT] Add shfl_up_i32/f32 warp intrinsics (#4632)

--- tests/python/test_simt.py
+++ tests/python/test_simt.py
@@ -1,3 +1,5 @@
+from pytest import approx
+
 import taichi as ti
 from tests import test_utils
 
@@ -58,6 +60,44 @@ def test_shfl_down_i32():
         assert a[i] == b[i + 1]
 
     # TODO: make this test case stronger
+
+
+@test_utils.test(arch=ti.cuda)
+def test_shfl_up_i32():
+    a = ti.field(dtype=ti.i32, shape=32)
+
+    @ti.kernel
+    def foo():
+        ti.loop_config(block_dim=32)
+        for i in range(32):
+            a[i] = ti.simt.warp.shfl_up_i32(ti.u32(0xFFFFFFFF), a[i], 1)
+
+    for i in range(32):
+        a[i] = i * i
+
+    foo()
+
+    for i in range(1, 32):
+        assert a[i] == (i - 1) * (i - 1)
+
+
+@test_utils.test(arch=ti.cuda)
+def test_shfl_up_f32():
+    a = ti.field(dtype=ti.f32, shape=32)
+
+    @ti.kernel
+    def foo():
+        ti.loop_config(block_dim=32)
+        for i in range(32):
+            a[i] = ti.simt.warp.shfl_up_f32(ti.u32(0xFFFFFFFF), a[i], 1)
+
+    for i in range(32):
+        a[i] = i * i * 0.9
+
+    foo()
+
+    for i in range(1, 32):
+        assert a[i] == approx((i - 1) * (i - 1) * 0.9, abs=1e-4)
 
 
 @test_utils.test(arch=ti.cuda)

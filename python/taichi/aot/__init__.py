d4ce55266a6df05cc4206ffbb4267dcfa8074c9b
neozhaoliang <mathzhaoliang@gmail.com>
2022-03-28 18:48:09 +0800

    add docstring for aot module

--- python/taichi/aot/__init__.py
+++ python/taichi/aot/__init__.py
@@ -1,2 +1,6 @@
+"""Taichi's AOT (ahead of time) module. Users can use Taichi
+as a GPU compute shader/kernel compiler by compiling their
+Taichi kernels into an AOT module.
+"""
 from taichi.aot.module import Module
 from taichi.aot.record import *

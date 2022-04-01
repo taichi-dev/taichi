7d37ea588bcd384c97fc45664da4938c910ee133
Ye Kuang <yekuang.ky@gmail.com>
2022-04-01 22:17:39 +0800

    wip

--- cmake/TaichiExportCore.cmake
+++ cmake/TaichiExportCore.cmake
@@ -4,3 +4,12 @@ set(TAICHI_EXPORT_CORE_NAME taichi_export_core)
 
 add_library(${TAICHI_EXPORT_CORE_NAME} SHARED)
 target_link_libraries(${TAICHI_EXPORT_CORE_NAME} taichi_isolated_core)
+
+set(TAICHI_C_API_LIB_NAME taichi_c_api)
+
+file(GLOB C_API_SOURCE
+    "c_api/src/*.cpp"
+)
+add_library(${TAICHI_C_API_LIB_NAME} SHARED ${C_API_SOURCE})
+
+target_link_libraries(${TAICHI_C_API_LIB_NAME} taichi_isolated_core)

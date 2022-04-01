4c1014661578df0ed16fc44d2b3d994806660232
jiasheng <leonz5288@hotmail.com>
2022-04-01 14:18:52 +0800

    update method to confirm python version

--- python/taichi/_version_check.py
+++ python/taichi/_version_check.py
@@ -30,17 +30,8 @@ def check_version(cur_uuid):
         else:
             payload['platform'] = 'macosx_11_0_arm64'
 
-    python_version = platform.python_version()
-    if python_version.startswith('3.6.'):
-        payload['python'] = 'cp36'
-    elif python_version.startswith('3.7.'):
-        payload['python'] = 'cp37'
-    elif python_version.startswith('3.8.'):
-        payload['python'] = 'cp38'
-    elif python_version.startswith('3.9.'):
-        payload['python'] = 'cp39'
-    elif python_version.startswith('3.10.'):
-        payload['python'] = 'cp310'
+    python_version = platform.python_version().split('.')
+    payload['python'] = 'cp' + python_version[0] + python_version[1]
 
     payload['uuid'] = cur_uuid
     if os.getenv('TI_CI') == '1':

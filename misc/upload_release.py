0aa6d82fe0ca443755558f0f441899e71dc3d23d
Bo Qiao <boqiao@taichi.graphics>
2022-04-02 15:50:39 +0800

    [ci] Switch to self hosted Pypi for nightly release

--- misc/upload_release.py
+++ misc/upload_release.py
@@ -45,9 +45,11 @@ def upload_artifact(is_taichi):
         sys.exit(f'Missing password env var {pwd_env}')
     command = [sys.executable, '-m', 'twine', 'upload']
     if not is_taichi:
-        command.extend(['--repository', 'testpypi'])
+        command.extend(['--repository-url', 'https://pypi.taichi.graphics/simple/'])
+    uname = '__token__' if is_taichi else 'admin'
     command.extend(
-        ['--verbose', '-u', '__token__', '-p', twine_password, 'dist/*'])
+        ['--verbose', '-u', uname, '-p', twine_password, 'dist/*'])
+
     try:
         subprocess.check_call(command)
     except subprocess.CalledProcessError as e:

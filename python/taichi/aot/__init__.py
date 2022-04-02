5377390c073f8f8f4655dbf5e444caa81d51f40c
Zhao Liang <mathzhaoliang@gmail.com>
2022-04-02 09:59:31 +0800

    Update __init__.py

--- python/taichi/aot/__init__.py
+++ python/taichi/aot/__init__.py
@@ -1,5 +1,6 @@
-"""Taichi's AOT (ahead of time) module. Users can use Taichi
-as a GPU compute shader/kernel compiler by compiling their
+"""Taichi's AOT (ahead of time) module.
+
+Users can use Taichi as a GPU compute shader/kernel compiler by compiling their
 Taichi kernels into an AOT module.
 """
 from taichi.aot.module import Module

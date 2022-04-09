b97a732d2dd070954660582de461a5c49e211c95
shwnyao <shawn.swyao@gmail.com>
2022-04-09 22:38:03 +0800

    [Autodiff]: implement ti.ad.no_gradto skip autograd

--- python/taichi/ad.py
+++ python/taichi/ad.py
@@ -79,3 +79,42 @@ def grad_for(primal):
         return decorated
 
     return decorator
+
+
+def no_grad(func):
+    """A decorator for python function to skip gradient calculation within Taichi's
+    autodiff system, e.g. `ti.Tape()` and `kernel.grad()`.
+    This decorator forces Taichi's autodiff system to use an empty gradient function
+    for the decorated function.
+
+    Args:
+        fn (Callable): The python function to be decorated.
+
+    Returns:
+        Callable: The decorated function.
+
+    Example::
+
+        >>> @ti.kernel
+        >>> def multiply(a: ti.float32):
+        >>>     for I in ti.grouped(x):
+        >>>         y[I] = x[I] * a
+        >>>
+        >>> @ti.no_grad
+        >>> def foo(a):
+        >>>     multiply(a)"""
+    def decorated(*args, **kwargs):
+        # TODO [#3025]: get rid of circular imports and move this to the top.
+        impl.get_runtime().grad_replaced = True
+        if impl.get_runtime().target_tape:
+            impl.get_runtime().target_tape.insert(decorated, args)
+        try:
+            func(*args, **kwargs)
+        finally:
+            impl.get_runtime().grad_replaced = False
+
+    def placeholder(*args, **kwargs):
+        return
+
+    decorated.grad = placeholder
+    return decorated

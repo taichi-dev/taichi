ad2625f04eee861e05f6fbe9c3366b81f9d9288f
Lin Jiang <90667349+lin-hitonami@users.noreply.github.com>
2022-04-12 13:37:00 +0800

    [Lang] Deprecate ti.pyfunc (#4764)
    * [Lang] Deprecate ti.pyfunc
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- python/taichi/__init__.py
+++ python/taichi/__init__.py
@@ -44,9 +44,13 @@ __deprecated_names__ = {
 }
 
 __customized_deprecations__ = {
-    'parallelize': ('loop_config(parallelize=...)', 'lang.misc._parallelize'),
-    'serialize': ('loop_config(serialize=True)', 'lang.misc._serialize'),
-    'block_dim': ('loop_config(block_dim=...)', 'lang.misc._block_dim')
+    'parallelize': ('Please use ti.loop_config(parallelize=...) instead.',
+                    'lang.misc._parallelize'),
+    'serialize': ('Please use ti.loop_config(serialize=True) instead.',
+                  'lang.misc._serialize'),
+    'block_dim': ('Please use ti.loop_config(block_dim=...) instead.',
+                  'lang.misc._block_dim'),
+    'pyfunc': ('Please avoid using it.', 'lang.kernel_impl.pyfunc')
 }
 
 if sys.version_info.minor < 7:
@@ -71,9 +75,8 @@ else:
             return locals()[attr]
         if attr in __customized_deprecations__:
             msg, fun = __customized_deprecations__[attr]
-            warnings.warn(
-                f'ti.{attr} is deprecated. Please use ti.{msg} instead.',
-                DeprecationWarning)
+            warnings.warn(f'ti.{attr} is deprecated. {msg}',
+                          DeprecationWarning)
             exec(f'{attr} = {fun}')
             return locals()[attr]
         raise AttributeError(f"module '{__name__}' has no attribute '{attr}'")

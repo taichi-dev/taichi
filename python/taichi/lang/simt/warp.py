4fd981e1c4ce29e65feb9730cb1d3587793ea0fc
Yongmin Hu <840428827@qq.com>
2022-04-12 08:57:58 +0800

    [SIMT] Add any_sync warp intrinsics (#4719)
    * [SIMT] Add any_sync warp intrinsics
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- python/taichi/lang/simt/warp.py
+++ python/taichi/lang/simt/warp.py
@@ -7,9 +7,10 @@ def all_nonzero():
     pass
 
 
-def any_nonzero():
-    # TODO
-    pass
+def any_nonzero(mask, predicate):
+    return expr.Expr(
+        _ti_core.insert_internal_func_call(
+            "cuda_any_sync_i32", expr.make_expr_group(mask, predicate), False))
 
 
 def unique():

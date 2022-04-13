b26921f661c461146f8db3c75900afc32fe56063
neozhaoliang <mathzhaoliang@gmail.com>
2022-04-13 18:02:49 +0800

    add basic complex arithmetic in math module, and add a mandelbrot set example

--- tests/python/test_api.py
+++ tests/python/test_api.py
@@ -44,10 +44,10 @@ user_api[ti.FieldsBuilder] = [
     'dynamic', 'finalize', 'lazy_grad', 'place', 'pointer'
 ]
 user_api[ti.math] = [
-    'clamp', 'cross', 'degrees', 'distance', 'dot', 'e', 'fract', 'ivec2',
-    'ivec3', 'ivec4', 'log2', 'mat2', 'mat3', 'mat4', 'mix', 'normalize', 'pi',
-    'radians', 'reflect', 'refract', 'sign', 'smoothstep', 'step', 'vec2',
-    'vec3', 'vec4'
+    'clamp', 'cconj', 'cdiv', 'cmul', 'cross', 'csqrt','degrees', 'distance',
+    'dot', 'e', 'fract', 'ivec2', 'ivec3', 'ivec4', 'log2', 'mat2',
+    'mat3', 'mat4', 'mix', 'normalize', 'pi', 'radians', 'reflect',
+    'refract', 'sign', 'smoothstep', 'step', 'vec2', 'vec3', 'vec4'
 ]
 user_api[ti.Matrix] = [
     'all', 'any', 'cast', 'cols', 'cross', 'determinant', 'diag', 'dot',

3642ad3cb54ec57e78a47c8ba3d0b1cdc195f30a
neozhaoliang <mathzhaoliang@gmail.com>
2022-04-14 16:58:21 +0800

    add complex arithmetic support

--- python/taichi/math/__init__.py
+++ python/taichi/math/__init__.py
@@ -2,10 +2,9 @@
 
 The math module supports glsl-style vectors, matrices and functions.
 """
-from .complex import *
+from ._complex import *
 from .mathimpl import *
 from .vectypes import *
 
-del complex
 del mathimpl
 del vectypes

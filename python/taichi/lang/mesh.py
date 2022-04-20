5e5727585d7175c22530197eccddd007cf6d470d
bx2k <BillXu2000@126.com>
2022-04-20 18:50:14 +0800

    todo

--- python/taichi/lang/mesh.py
+++ python/taichi/lang/mesh.py
@@ -564,7 +564,7 @@ class Mesh:
     def generate_meta(data):
         return MeshMetadata(data)
     
-    class RelationVisitor(ast.NodeVisitor):
+    class RelationVisitor(ast.NodeVisitor): # TODO: only works for simple cases
         def __init__(self, ctx):
             self.vars = {}
             self.visits = []

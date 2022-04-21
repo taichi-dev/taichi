4f02cc497c941bd29fe308764c4bf7756ff22944
Lin Jiang <90667349+lin-hitonami@users.noreply.github.com>
2022-04-21 12:00:27 +0800

    [Lang] Add support for keyword arguments (#4794)
    * [Lang] Add support for keyword arguments
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * fix
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * add too many
    
    * add not found
    
    * fix
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- python/taichi/lang/kernel_arguments.py
+++ python/taichi/lang/kernel_arguments.py
@@ -1,3 +1,5 @@
+import inspect
+
 import taichi.lang
 from taichi._lib import core as _ti_core
 from taichi.lang import impl, ops
@@ -10,9 +12,10 @@ from taichi.types.primitive_types import u64
 
 
 class KernelArgument:
-    def __init__(self, _annotation, _name):
+    def __init__(self, _annotation, _name, _default=inspect.Parameter.empty):
         self.annotation = _annotation
         self.name = _name
+        self.default = _default
 
 
 class SparseMatrixEntry:

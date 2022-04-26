f96dde4762a2d31048024e6aaa5a2409e3eb7690
Bo Qiao <boqiao@taichi.graphics>
2022-04-26 22:53:45 +0800

    Use keywords instead of plain target_link_libraries

--- cmake/TaichiExportCore.cmake
+++ cmake/TaichiExportCore.cmake
@@ -3,4 +3,4 @@ cmake_minimum_required(VERSION 3.0)
 set(TAICHI_EXPORT_CORE_NAME taichi_export_core)
 
 add_library(${TAICHI_EXPORT_CORE_NAME} SHARED)
-target_link_libraries(${TAICHI_EXPORT_CORE_NAME} taichi_isolated_core)
+target_link_libraries(${TAICHI_EXPORT_CORE_NAME} PRIVATE taichi_isolated_core)

7e3523c0386f1f4ce247662dcea716b79c8dddfd
Frost Ming <mianghong@gmail.com>
2022-04-28 09:04:32 +0800

    Change the library output dir for export core

--- cmake/TaichiExportCore.cmake
+++ cmake/TaichiExportCore.cmake
@@ -1,6 +1,7 @@
 cmake_minimum_required(VERSION 3.0)
 
 set(TAICHI_EXPORT_CORE_NAME taichi_export_core)
+set(CMAKE_LIBRARY_OUTPUT_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/build")
 
 add_library(${TAICHI_EXPORT_CORE_NAME} SHARED)
 target_link_libraries(${TAICHI_EXPORT_CORE_NAME} taichi_isolated_core)

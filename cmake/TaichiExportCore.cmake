a1fe7d34ef56df932a0a1b446ebb60e04fb7749b
Frost Ming <mianghong@gmail.com>
2022-04-28 10:26:06 +0800

    limit the change to the target

--- cmake/TaichiExportCore.cmake
+++ cmake/TaichiExportCore.cmake
@@ -1,7 +1,8 @@
 cmake_minimum_required(VERSION 3.0)
 
 set(TAICHI_EXPORT_CORE_NAME taichi_export_core)
-set(CMAKE_LIBRARY_OUTPUT_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/build")
 
 add_library(${TAICHI_EXPORT_CORE_NAME} SHARED)
 target_link_libraries(${TAICHI_EXPORT_CORE_NAME} taichi_isolated_core)
+set_target_properties(${TAICHI_EXPORT_CORE_NAME} PROPERTIES
+    CMAKE_LIBRARY_OUTPUT_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/build")

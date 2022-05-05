5891db798eb44144b1aecee38d05ae0e49382a56
pengyu <6712304+FantasyVR@users.noreply.github.com>
2022-05-05 22:22:46 +0800

    Update tests/python/test_sparse_matrix.py
    Co-authored-by: Yi Xu <xy_xuyi@foxmail.com>

--- tests/python/test_sparse_matrix.py
+++ tests/python/test_sparse_matrix.py
@@ -18,7 +18,7 @@ def test_sparse_matrix_builder_deprecated_anno(dtype, storage_format):
                                              storage_format=storage_format)
 
     @ti.kernel
-    def fill(Abuilder: ti.linalg.sparse_matrix_builder()):
+    def fill(Abuilder: ti.types.sparse_matrix_builder()):
         for i, j in ti.ndrange(n, n):
             Abuilder[i, j] += i + j
 

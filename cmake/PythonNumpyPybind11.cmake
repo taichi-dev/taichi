a43bebff9b11153918431c359817d46a621b74ac
Bo Qiao <boqiao@taichi.graphics>
2022-05-12 17:21:11 +0800

    Update include dir

--- cmake/PythonNumpyPybind11.cmake
+++ cmake/PythonNumpyPybind11.cmake
@@ -4,15 +4,29 @@ execute_process(COMMAND ${PYTHON_EXECUTABLE} -m pybind11 --cmake
 execute_process(COMMAND ${PYTHON_EXECUTABLE} -c "import numpy;print(numpy.get_include())"
                 OUTPUT_VARIABLE NUMPY_INCLUDE_DIR OUTPUT_STRIP_TRAILING_WHITESPACE)
 
+
+execute_process(COMMAND ${PYTHON_EXECUTABLE} -c
+        "import sys; import pybind11; sys.stdout.write(pybind11.get_include() + ';' + pybind11.get_include(True))"
+        OUTPUT_VARIABLE PYBIND11_INCLUDE_DIR
+        RESULT_VARIABLE PYBIND11_IMPORT_RET)
+if (NOT PYBIND11_IMPORT_RET)
+    # returns zero if success
+    message("  ======== old pybind11 include: ${PYBIND11_INCLUDE_DIR}")
+else ()
+    message(FATAL_ERROR "Cannot import pybind11. Please install. ([sudo] pip3 install --user pybind11)")
+endif ()
+
+
 message("-- Python: Using ${PYTHON_EXECUTABLE} as the interpreter")
 message("    version: ${PYTHON_VERSION_STRING}")
 message("    include: ${PYTHON_INCLUDE_DIR}")
 message("    library: ${PYTHON_LIBRARY}")
 message("    numpy include: ${NUMPY_INCLUDE_DIR}")
+message("    pybind11 include: ${pybind11_DIR}")
 
 
 include_directories(${NUMPY_INCLUDE_DIR})
-
+include_directories(${PYTHON_INCLUDE_DIRS})
 include_directories(${pybind11_DIR})
 
 find_package(pybind11 CONFIG REQUIRED)

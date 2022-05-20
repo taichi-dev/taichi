cde179764dc47b9af1e1b5cd8534eaebb3ae8129
Ailing Zhang <ailing@taichi.graphics>
2022-05-20 12:10:55 +0800

    Update on "[aot] Build and run graph without serialization"
    This PR servces as the base PR with a minimal example of building and
    running a Graph. Runtime values for graph arguments can be either
    scalars or ndarrays.
    
    For detailed proposal please see #4786.
    
    Things handled in this PR:
    - Maximize common code/runtime shared by the two workflows below:
      1. build -> compile -> run
      2. build -> compile -> serialize -> deserilize -> run
    - Graph arguments are annotated with dtype and element shape for ndarray (temporary
    until we have vec3 types in C++)
    
    Things that we've discussed but not included in this PR:
    - C API: I'll leave that for a unified C API PR in the future.
    - bind IValues to graph: easy, will add later.
    
    [ghstack-poisoned]

--- tests/cpp/program/graph_test.cpp
+++ tests/cpp/program/graph_test.cpp
@@ -30,7 +30,7 @@ TEST(GraphTest, SimpleGraphRun) {
   auto g = std::make_unique<Graph>("test");
   auto seq = g->seq();
   auto arr_arg = aot::Arg{
-      "arr", PrimitiveType::i32.to_string(), aot::ArgKind::NDARRAY, {size}};
+      "arr", PrimitiveType::i32.to_string(), aot::ArgKind::NDARRAY, {}};
   seq->emplace(ker1.get(), {arr_arg});
   seq->emplace(ker2.get(),
                {arr_arg, aot::Arg{"x", PrimitiveType::i32.to_string(),

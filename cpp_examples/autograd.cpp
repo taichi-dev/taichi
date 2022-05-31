e84afc718ccaf48fea78df9fbdbc24cfa7fbad65
mingrui <mingrui.zhang@taichi.graphics>
2022-05-31 10:27:06 +0800

    update the unifying adjoint and dual

--- cpp_examples/autograd.cpp
+++ cpp_examples/autograd.cpp
@@ -55,10 +55,7 @@ void autograd() {
       bool is_primal() const override {
         return true;
       }
-      SNode *adjoint_snode() const override {
-        return snode;
-      }
-      SNode *dual_snode() const override {
+      SNode *grad_snode() const override {
         return snode;
       }
     };
@@ -69,10 +66,7 @@ void autograd() {
       bool is_primal() const override {
         return false;
       }
-      SNode *adjoint_snode() const override {
-        return nullptr;
-      }
-      SNode *dual_snode() const override {
+      SNode *grad_snode() const override {
         return nullptr;
       }
     };
@@ -82,8 +76,8 @@ void autograd() {
     snode->dt = PrimitiveType::f32;
     snode->grad_info = std::make_unique<GradInfoPrimal>(
         &root->dense(Axis(0), n, false).insert_children(SNodeType::place));
-    snode->get_adjoint()->dt = PrimitiveType::f32;
-    snode->get_adjoint()->grad_info = std::make_unique<GradInfoAdjoint>();
+    snode->get_grad()->dt = PrimitiveType::f32;
+    snode->get_grad()->grad_info = std::make_unique<GradInfoAdjoint>();
     return snode;
   };
   auto *a = get_snode_grad(), *b = get_snode_grad(), *c = get_snode_grad();
@@ -107,11 +101,11 @@ void autograd() {
       builder.create_global_store(builder.create_global_ptr(c, {i}), zero);
 
       builder.create_global_store(
-          builder.create_global_ptr(a->get_adjoint(), {i}), zero);
+          builder.create_global_ptr(a->get_grad(), {i}), zero);
       builder.create_global_store(
-          builder.create_global_ptr(b->get_adjoint(), {i}), zero);
+          builder.create_global_ptr(b->get_grad(), {i}), zero);
       builder.create_global_store(
-          builder.create_global_ptr(c->get_adjoint(), {i}), one);
+          builder.create_global_ptr(c->get_grad(), {i}), one);
     }
 
     kernel_init =
@@ -147,13 +141,13 @@ void autograd() {
       auto *ext_a = builder.create_external_ptr(
           builder.create_arg_load(0, PrimitiveType::f32, true), {i});
       auto *a_grad_i = builder.create_global_load(
-          builder.create_global_ptr(a->get_adjoint(), {i}));
+          builder.create_global_ptr(a->get_grad(), {i}));
       builder.create_global_store(ext_a, a_grad_i);
 
       auto *ext_b = builder.create_external_ptr(
           builder.create_arg_load(1, PrimitiveType::f32, true), {i});
       auto *b_grad_i = builder.create_global_load(
-          builder.create_global_ptr(b->get_adjoint(), {i}));
+          builder.create_global_ptr(b->get_grad(), {i}));
       builder.create_global_store(ext_b, b_grad_i);
 
       auto *ext_c = builder.create_external_ptr(

3ea667a58a88f76f5dfbe8e4a07cc7bf873b3ceb
jim19930609 <jim19930609@gmail.com>
2022-06-10 13:50:49 +0800

    Added tests for activate/deactivate operations

--- tests/cpp/backends/llvm/field_aot_cpu.py
+++ tests/cpp/backends/llvm/field_aot_cpu.py
@@ -4,6 +4,8 @@ import taichi as ti
 
 
 def compile_aot():
+    # Make sure "debug" mode is on
+    # in both python & C++ tests
     ti.init(arch=ti.x64, debug=True)
 
     x = ti.field(ti.i32)
@@ -16,21 +18,24 @@ def compile_aot():
     p.dense(ti.i, 8).place(y)
 
     @ti.kernel
-    def run(base: int):
+    def init_fields(base: int):
+        # Dense SNode
         for i in range(4 * 8):
             x[i] = base + i
+
+        # Pointer SNode
         y[32] = 4
         y[33] = 5
         y[9] = 10
 
     @ti.kernel
-    def check_field_x(base: int):
+    def check_init_x(base: int):
         # Check numerical accuracy for Dense SNodes
         for i in range(4 * 8):
-            assert (x[i] == base + 1)
+            assert (x[i] == base + i)
 
     @ti.kernel
-    def check_field_y():
+    def check_init_y():
         # Check sparsity for Pointer SNodes
         for i in range(8):
             if i == 1 or i == 4:
@@ -53,15 +58,50 @@ def compile_aot():
             else:
                 assert (y[i] == 0)
 
+    @ti.kernel
+    def deactivate_pointer_fields():
+        ti.deactivate(p, [1])
+        ti.deactivate(p, [4])
+
+    @ti.kernel
+    def activate_pointer_fields():
+        ti.activate(p, [7])
+        ti.activate(p, [3])
+
+    @ti.kernel
+    def check_deactivate_pointer_fields():
+        assert (not ti.is_active(p, [1]))
+        assert (not ti.is_active(p, [4]))
+
+    @ti.kernel
+    def check_activate_pointer_fields():
+        assert (ti.is_active(p, [7]))
+        assert (ti.is_active(p, [3]))
+
+        for i in range(7 * 8, 7 * 8 + 8):
+            assert (y[i] == 0)
+
+        for i in range(3 * 8, 3 * 8 + 8):
+            assert (y[i] == 0)
+
     assert "TAICHI_AOT_FOLDER_PATH" in os.environ.keys()
     dir_name = str(os.environ["TAICHI_AOT_FOLDER_PATH"])
 
     m = ti.aot.Module(ti.x64)
-    m.add_kernel(run, template_args={})
-    m.add_kernel(check_field_x, template_args={})
-    m.add_kernel(check_field_y, template_args={})
+
+    m.add_kernel(init_fields, template_args={})
+    m.add_kernel(check_init_x, template_args={})
+    m.add_kernel(check_init_y, template_args={})
+
+    m.add_kernel(deactivate_pointer_fields, template_args={})
+    m.add_kernel(activate_pointer_fields, template_args={})
+
+    m.add_kernel(check_deactivate_pointer_fields, template_args={})
+    m.add_kernel(check_activate_pointer_fields, template_args={})
+
     m.add_field("x", x)
     m.add_field("y", y)
+
     m.save(dir_name, 'x64-aot')
 
 
--- tests/cpp/backends/llvm/field_aot_test.cpp
+++ tests/cpp/backends/llvm/field_aot_test.cpp
@@ -18,12 +18,12 @@ namespace lang {
 
 TEST(LlvmAOTTest, Field) {
   CompileConfig cfg;
-  cfg.debug = true;
   cfg.arch = Arch::x64;
   cfg.kernel_profiler = false;
   constexpr KernelProfilerBase *kNoProfiler = nullptr;
   LlvmProgramImpl prog{cfg, kNoProfiler};
   auto *compute_device = prog.get_compute_device();
+
   // Must have handled all the arch fallback logic by this point.
   auto memory_pool = std::make_unique<MemoryPool>(cfg.arch, compute_device);
   prog.initialize_host();
@@ -38,39 +38,82 @@ TEST(LlvmAOTTest, Field) {
   aot_params.module_path = aot_mod_ss.str();
   aot_params.program = &prog;
   std::unique_ptr<aot::Module> mod = cpu::make_aot_module(aot_params);
-  aot::Kernel *k_run = mod->get_kernel("run");
-  aot::Kernel *k_check_field_x = mod->get_kernel("check_field_x");
-  aot::Kernel *k_check_field_y = mod->get_kernel("check_field_y");
+
+  aot::Kernel *k_init_fields = mod->get_kernel("init_fields");
+  aot::Kernel *k_check_init_x = mod->get_kernel("check_init_x");
+  aot::Kernel *k_check_init_y = mod->get_kernel("check_init_y");
+
+  aot::Kernel *k_deactivate_pointer_fields =
+      mod->get_kernel("deactivate_pointer_fields");
+  aot::Kernel *k_activate_pointer_fields =
+      mod->get_kernel("activate_pointer_fields");
+
+  aot::Kernel *k_check_deactivate_pointer_fields =
+      mod->get_kernel("check_deactivate_pointer_fields");
+  aot::Kernel *k_check_activate_pointer_fields =
+      mod->get_kernel("check_activate_pointer_fields");
 
   // Initialize Fields
   aot::Field *field_x = mod->get_field("0" /*snode_tree_id*/);
   aot::Field *field_y = mod->get_field("0" /*snode_tree_id*/);
+
   finalize_aot_field(mod.get(), field_x, result_buffer);
   finalize_aot_field(mod.get(), field_y, result_buffer);
 
   int base_value = 10;
-  // Kernel: run(int)
+  /* -------- Test Case 1 ------ */
+  // Kernel: init_fields(int)
   {
     RuntimeContext ctx;
     ctx.runtime = prog.get_llvm_runtime();
     ctx.set_arg(0, base_value);
-    k_run->launch(&ctx);
+    k_init_fields->launch(&ctx);
   }
 
-  // Kernel: check_field_x(int)
+  // Kernel: check_init_x(int)
   {
     RuntimeContext ctx;
     ctx.runtime = prog.get_llvm_runtime();
     ctx.set_arg(0, base_value);
-    k_check_field_x->launch(&ctx);
+    k_check_init_x->launch(&ctx);
   }
-
-  // Kernel: check_field_y()
+  // Kernel: check_init_y()
   {
     RuntimeContext ctx;
     ctx.runtime = prog.get_llvm_runtime();
-    k_check_field_y->launch(&ctx);
+    k_check_init_y->launch(&ctx);
   }
+
+  /* -------- Test Case 2 ------ */
+  // Kernel: deactivate_pointer_fields()
+  {
+    RuntimeContext ctx;
+    ctx.runtime = prog.get_llvm_runtime();
+    k_deactivate_pointer_fields->launch(&ctx);
+  }
+  // Kernel: check_deactivate_pointer_fields()
+  {
+    RuntimeContext ctx;
+    ctx.runtime = prog.get_llvm_runtime();
+    k_check_deactivate_pointer_fields->launch(&ctx);
+  }
+
+  /* -------- Test Case 3 ------ */
+  // Kernel: activate_pointer_fields()
+  {
+    RuntimeContext ctx;
+    ctx.runtime = prog.get_llvm_runtime();
+    k_activate_pointer_fields->launch(&ctx);
+  }
+  // Kernel: check_activate_pointer_fields()
+  {
+    RuntimeContext ctx;
+    ctx.runtime = prog.get_llvm_runtime();
+    k_check_activate_pointer_fields->launch(&ctx);
+  }
+
+  // Check assertion error from ti.kernel
+  prog.check_runtime_error(result_buffer);
 }
 
 }  // namespace lang

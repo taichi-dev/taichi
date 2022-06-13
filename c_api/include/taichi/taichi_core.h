0ae11ca3dd77f52a5d357e8647abf18817361e1d
PENGUINLIONG <admin@penguinliong.moe>
2022-06-13 14:57:36 +0800

    C-APIs for Taichi runtime distribution

---
+++ c_api/include/taichi/taichi_core.h
@@ -0,0 +1,171 @@
+#pragma once
+#include <taichi/taichi_platform.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif  // __cplusplus
+
+// alias.bool
+typedef uint32_t TiBool;
+
+// definition.false
+#define FALSE 0
+
+// definition.true
+#define TRUE 1
+
+// alias.flags
+typedef uint32_t TiFlags;
+
+// definition.null_handle
+#define NULL_HANDLE 0
+
+// handle.runtime
+typedef struct TiRuntime_t *TiRuntime;
+
+// handle.aot_module
+typedef struct TiAotModule_t *TiAotModule;
+
+// handle.memory
+typedef struct TiMemory_t *TiMemory;
+
+// handle.kernel
+typedef struct TiKernel_t *TiKernel;
+
+// handle.compute_graph
+typedef struct TiComputeGraph_t *TiComputeGraph;
+
+// enumeration.arch
+typedef enum TiArch {
+  TI_ARCH_X64 = 0,
+  TI_ARCH_ARM64 = 1,
+  TI_ARCH_JS = 2,
+  TI_ARCH_CC = 3,
+  TI_ARCH_WASM = 4,
+  TI_ARCH_CUDA = 5,
+  TI_ARCH_METAL = 6,
+  TI_ARCH_OPENGL = 7,
+  TI_ARCH_DX11 = 8,
+  TI_ARCH_OPENCL = 9,
+  TI_ARCH_AMDGPU = 10,
+  TI_ARCH_VULKAN = 11,
+  TI_ARCH_MAX_ENUM = 0xffffffff,
+} TiArch;
+
+// enumeration.argument_type
+typedef enum TiArgumentType {
+  TI_ARGUMENT_TYPE_I32 = 0,
+  TI_ARGUMENT_TYPE_F32 = 1,
+  TI_ARGUMENT_TYPE_NDARRAY = 2,
+  TI_ARGUMENT_TYPE_MAX_ENUM = 0xffffffff,
+} TiArgumentType;
+
+// bit_field.memory_usage
+typedef enum TiMemoryUsageFlagBits {
+  TI_MEMORY_USAGE_STORAGE_BIT = 0,
+  TI_MEMORY_USAGE_UNIFORM_BIT = 1,
+  TI_MEMORY_USAGE_VERTEX_BIT = 2,
+  TI_MEMORY_USAGE_INDEX_BIT = 3,
+} TiMemoryUsageFlagBits;
+typedef TiFlags TiMemoryUsageFlags;
+
+// structure.memory_allocate_info
+typedef struct TiMemoryAllocateInfo {
+  uint64_t size;
+  bool host_write;
+  bool host_read;
+  bool export_sharing;
+  TiMemoryUsageFlagBits usage;
+} TiMemoryAllocateInfo;
+
+// structure.nd_shape
+typedef struct TiNdShape {
+  uint32_t dim_count;
+  uint32_t dims;
+} TiNdShape;
+
+// structure.nd_array
+typedef struct TiNdArray {
+  TiMemory memory;
+  TiNdShape shape;
+  TiNdShape elem_shape;
+} TiNdArray;
+
+// union.argument_value
+typedef union TiArgumentValue {
+  int32_t i32;
+  float f32;
+  TiNdArray ndarray;
+} TiArgumentValue;
+
+// structure.argument
+typedef struct TiArgument {
+  TiArgumentType type;
+  TiArgumentValue value;
+} TiArgument;
+
+// structure.named_argument
+typedef struct TiNamedArgument {
+  const char *name;
+  TiArgument argument;
+} TiNamedArgument;
+
+// function.create_runtime
+TI_DLL_EXPORT TiRuntime TI_API_CALL ti_create_runtime(TiArch arch);
+
+// function.destroy_runtime
+TI_DLL_EXPORT void TI_API_CALL ti_destroy_runtime(TiRuntime runtime);
+
+// function.allocate_memory
+TI_DLL_EXPORT TiMemory TI_API_CALL
+ti_allocate_memory(TiRuntime runtime, TiMemoryAllocateInfo allocate_info);
+
+// function.free_memory
+TI_DLL_EXPORT void TI_API_CALL ti_free_memory(TiRuntime runtime,
+                                              TiMemory memory);
+
+// function.map_memory
+TI_DLL_EXPORT void *TI_API_CALL ti_map_memory(TiRuntime runtime,
+                                              TiMemory memory);
+
+// function.unmap_memory
+TI_DLL_EXPORT void TI_API_CALL ti_unmap_memory(TiRuntime runtime,
+                                               TiMemory memory);
+
+// function.launch_kernel
+TI_DLL_EXPORT void TI_API_CALL ti_launch_kernel(TiRuntime runtime,
+                                                TiKernel kernel,
+                                                uint32_t arg_count,
+                                                TiArgument args);
+
+// function.launch_compute_graph
+TI_DLL_EXPORT void TI_API_CALL
+ti_launch_compute_graph(TiRuntime runtime,
+                        TiComputeGraph compute_graph,
+                        uint32_t arg_count,
+                        TiArgument args);
+
+// function.submit
+TI_DLL_EXPORT void TI_API_CALL ti_submit(TiRuntime runtime);
+
+// function.wait
+TI_DLL_EXPORT void TI_API_CALL ti_wait(TiRuntime runtime);
+
+// function.load_aot_module
+TI_DLL_EXPORT TiAotModule TI_API_CALL
+ti_load_aot_module(TiRuntime runtime, const char *module_path);
+
+// function.destroy_aot_module
+TI_DLL_EXPORT void TI_API_CALL ti_destroy_aot_module(TiAotModule aot_module);
+
+// function.get_aot_module_kernel
+TI_DLL_EXPORT TiKernel TI_API_CALL
+ti_get_aot_module_kernel(TiAotModule aot_module, const char *name);
+
+// function.get_aot_module_compute_graph
+TI_DLL_EXPORT TiComputeGraph TI_API_CALL
+ti_get_aot_module_compute_graph(TiAotModule aot_module, const char *name);
+
+#ifdef __cplusplus
+}  // extern "C"
+#endif  // __cplusplus
---
+++ misc/generate-c-api.py
@@ -0,0 +1,384 @@
+import json
+from os import system
+import re
+
+JSON = None
+
+with open("c_api/taichi.json") as f:
+    JSON = json.load(f)
+
+VERSION = JSON["version"]
+print("taichi c-api version is:", VERSION)
+
+
+class Name:
+    def __init__(self, name: str):
+        assert re.match('^[@a-z0-9_]+$', name)
+        self._segs = name.split("_")
+
+    @property
+    def snake_case(self) -> str:
+        return '_'.join(self._segs)
+
+    @property
+    def screaming_snake_case(self) -> str:
+        return '_'.join(x.upper() for x in self._segs)
+
+    @property
+    def upper_camel_case(self) -> str:
+        return ''.join(x.title() for x in self._segs)
+
+    def __repr__(self) -> str:
+        return self.snake_case
+
+
+class DeclarationRegistry:
+    def __init__(self):
+        # "xxx.yyy" -> Xxx(yyy) Look-up table.
+        self._inner = {}
+        self._imported = {}
+
+    def resolve(self, id: str):
+        if id in self._inner:
+            return self._inner[id]
+        elif id in self._imported:
+            return self._imported[id]
+        else:
+            return None
+
+    def register(self, x):
+        self._inner[x.id] = x
+
+    def import_declrs(self, other):
+        for x in other._inner.values():
+            self._imported[x.id] = x
+
+    def __iter__(self):
+        return iter(self._inner)
+
+
+DECLR_REG = None
+
+
+class Alias:
+    def __init__(self, j):
+        self.name = Name(j["name"])
+        self.id = f"alias.{self.name}"
+        self.alias_of = j["alias_of"]
+
+    @property
+    def type_name(self) -> str:
+        return "Ti" + self.name.upper_camel_case
+
+    def declr(self):
+        return f"typedef {self.alias_of} {self.type_name};"
+
+
+class Definition:
+    def __init__(self, j):
+        self.name = Name(j["name"])
+        self.id = f"definition.{self.name}"
+        self.value = j["value"]
+
+    def declr(self):
+        return f"#define {self.name.screaming_snake_case} {self.value}"
+
+
+class Handle:
+    def __init__(self, j):
+        self.name = Name(j["name"])
+        self.id = f"handle.{self.name}"
+        self.is_dispatchable = j["is_dispatchable"]
+
+    @property
+    def type_name(self) -> str:
+        return "Ti" + self.name.upper_camel_case
+
+    def declr(self):
+        return f"typedef struct {self.type_name}_t* {self.type_name};"
+
+
+class Enumeration:
+    def __init__(self, j):
+        self.name = Name(j["name"])
+        self.id = f"enumeration.{self.name}"
+
+        cases = {}
+        if "inc_cases" in j:
+            inc_file_name = "taichi/inc/" + j["inc_cases"] + ".inc.h"
+            with open(inc_file_name) as f:
+                for line in f.readlines():
+                    m = re.match(r"\w+\((\w+)\).*", line)
+                    if m:
+                        case_name = self.get_case_name(Name(m[1]))
+                        cases[case_name] = len(cases)
+        else:
+            for name, value in j["cases"].items():
+                cases[self.get_case_name(Name(name))] = value
+        self.cases = cases
+
+    @property
+    def type_name(self):
+        return "Ti" + self.name.upper_camel_case
+
+    def get_case_name(self, case_name: Name):
+        return "TI_" + self.name.screaming_snake_case + "_" + case_name.screaming_snake_case
+
+    def declr(self):
+        out = ["typedef enum " + self.type_name + " {"]
+        for name, value in self.cases.items():
+            out += [f"  {name} = {value},"]
+        out += [f"  {self.get_case_name(Name('max_enum'))} = 0xffffffff,"]
+        out += ["} " + self.type_name + ";"]
+        return '\n'.join(out)
+
+
+class BitField:
+    def __init__(self, j):
+        self.name = Name(j["name"])
+        self.id = f"bit_field.{self.name}"
+
+        bits = {}
+        if "inc_bits" in j:
+            inc_file_name = "taichi/inc/" + j["inc_bits"] + ".inc.h"
+            with open(inc_file_name) as f:
+                for line in enumerate(f.readlines()):
+                    m = re.match(r"\w+\((\w+)\).*", line)
+                    if m:
+                        bit_name = self.get_flag_name(Name(m[1]))
+                        bits[bit_name] = len(bits)
+        else:
+            for name, value in j["bits"].items():
+                bits[self.get_flag_name(Name(name))] = value
+        self.bits = bits
+
+    @property
+    def type_name(self):
+        return "Ti" + self.name.upper_camel_case + "FlagBits"
+
+    @property
+    def field_type_name(self):
+        return "Ti" + self.name.upper_camel_case + "Flags"
+
+    def get_flag_name(self, flag_name: Name):
+        return "TI_" + self.name.screaming_snake_case + "_" + flag_name.screaming_snake_case + "_BIT"
+
+    def declr(self):
+        out = ["typedef enum " + self.type_name + " {"]
+        for name, value in self.bits.items():
+            out += [f"  {name} = {value},"]
+        out += ["} " + self.type_name + ";"]
+        out += [f"typedef TiFlags {self.field_type_name};"]
+        return '\n'.join(out)
+
+
+class CType:
+    def __init__(self, name: str):
+        self.name = name
+
+    @property
+    def type_name(self):
+        return self.name
+
+
+class Field:
+    def __init__(self, j):
+        ty = DECLR_REG.resolve(j["type"])
+        if ty != None:
+            # The type has been registered.
+            self.type = ty
+            if "name" in j:
+                self.name = Name(j["name"])
+            else:
+                self.name = ty.name
+        else:
+            # The type is not (yet) registered, treat it as a untracked C type.
+            self.type = CType(j["type"])
+            self.name = Name(j["name"])
+
+    def declr(self):
+        return f"{self.type.type_name} {self.name}"
+
+
+class Structure:
+    def __init__(self, j):
+        self.name = Name(j["name"])
+        self.id = f"structure.{self.name}"
+        self.fields = []
+        if "fields" in j:
+            for x in j["fields"]:
+                self.fields += [Field(x)]
+
+    @property
+    def type_name(self):
+        return "Ti" + self.name.upper_camel_case
+
+    def declr(self):
+        out = ["typedef struct " + self.type_name + " {"]
+        for x in self.fields:
+            out += [f"  {x.declr()};"]
+        out += ["} " + self.type_name + ";"]
+        return '\n'.join(out)
+
+
+class Union:
+    def __init__(self, j):
+        self.name = Name(j["name"])
+        self.id = f"union.{self.name}"
+        self.variants = []
+        if "variants" in j:
+            for x in j["variants"]:
+                self.variants += [Field(x)]
+
+    @property
+    def type_name(self):
+        return "Ti" + self.name.upper_camel_case
+
+    def declr(self):
+        out = ["typedef union " + self.type_name + " {"]
+        for x in self.variants:
+            out += [f"  {x.declr()};"]
+        out += ["} " + self.type_name + ";"]
+        return '\n'.join(out)
+
+
+class Function:
+    def __init__(self, j):
+        self.name = Name(j["name"])
+        self.id = f"function.{self.name}"
+        self.version = 1
+        self.is_extension = False
+        self.return_value_type = None
+        self.params = []
+
+        if "version" in j:
+            self.version = j["version"]
+
+        if "is_extension" in j:
+            self.is_extension = j["is_extension"]
+
+        if "parameters" in j:
+            for x in j["parameters"]:
+                field = Field(x)
+                if field.name.snake_case == "@return":
+                    self.return_value_type = field.type
+                else:
+                    self.params += [field]
+
+    @property
+    def func_name(self):
+        name = "ti_" + self.name.snake_case
+        if self.is_extension:
+            name += "_ext"
+        if self.version > 1:
+            name += f"_{self.version}"
+        return name
+
+    def declr(self):
+        return_value_type = "void" if self.return_value_type == None else self.return_value_type.type_name
+        out = ["TI_DLL_EXPORT " + return_value_type +
+               " TI_API_CALL " + self.func_name + "("]
+        out += [',\n'.join(f"  {param.declr()}" for param in self.params)]
+        out += [");"]
+        return '\n'.join(out)
+
+
+MODULES = {}
+
+
+class Module:
+    def __init__(self, j):
+        self.is_built_in = False
+        self.declr_reg = DeclarationRegistry()
+        self.required_modules = []
+
+        global DECLR_REG
+        DECLR_REG = self.declr_reg
+
+        if "is_built_in" in j:
+            self.is_built_in = True
+            # Built-in headers are hand-written so we can return right away.
+            return
+
+        if "required_modules" in j:
+            for x in j["required_modules"]:
+                assert x in MODULES
+                module = MODULES[x]
+                self.declr_reg.import_declrs(module.declr_reg)
+                self.required_modules += [x]
+
+        if "declarations" in j:
+            for k in j["declarations"]:
+                ty = k["type"]
+
+                if ty == "alias":
+                    self.declr_reg.register(Alias(k))
+                elif ty == "definition":
+                    self.declr_reg.register(Definition(k))
+                elif ty == "handle":
+                    self.declr_reg.register(Handle(k))
+                elif ty == "enumeration":
+                    self.declr_reg.register(Enumeration(k))
+                elif ty == "bit_field":
+                    self.declr_reg.register(BitField(k))
+                elif ty == "structure":
+                    self.declr_reg.register(Structure(k))
+                elif ty == "union":
+                    self.declr_reg.register(Union(k))
+                elif ty == "function":
+                    self.declr_reg.register(Function(k))
+                else:
+                    print(f"ignored unrecognized type declaration '{k}'")
+
+        DECLR_REG = None
+
+    def declr(self):
+        out = ["#pragma once"]
+
+        for x in self.required_modules:
+            out += [f"#include <{x}>"]
+
+        out += [
+            "",
+            "#ifdef __cplusplus",
+            'extern "C" {',
+            "#endif // __cplusplus",
+            "",
+        ]
+
+        for x in self.declr_reg:
+            out += [
+                "",
+                f"// {x}",
+                self.declr_reg.resolve(x).declr(),
+            ]
+
+        out += [
+            "",
+            "#ifdef __cplusplus",
+            '} // extern "C"',
+            "#endif // __cplusplus",
+            "",
+        ]
+
+        return '\n'.join(out)
+
+
+def generate_module_header(j):
+    module_name = j["name"]
+    module = Module(j)
+    MODULES[module_name] = module
+
+    if module.is_built_in:
+        return
+
+    print(f"processing module '{module_name}'")
+    path = f"c_api/include/{module_name}"
+    with open(path, "w") as f:
+        f.write(module.declr())
+
+    system(f"clang-format {path} -i")
+
+
+for module in JSON["modules"]:
+    generate_module_header(module)

ae244a40622934a8d3f51559b219b21909db79c6
Bo Qiao <boqiao@taichi.graphics>
2022-06-17 17:38:50 +0800

    Fix cl linking

--- taichi/program/program.cpp
+++ taichi/program/program.cpp
@@ -102,8 +102,12 @@ Program::Program(Arch desired_arch)
     TI_ERROR("This taichi is not compiled with DX11");
 #endif
   } else if (config.arch == Arch::opengl) {
+#ifdef TI_WITH_OPENGL
     TI_ASSERT(opengl::initialize_opengl(config.use_gles));
     program_impl_ = std::make_unique<OpenglProgramImpl>(config);
+#else
+    TI_ERROR("This taichi is not compiled with OpenGL");
+#endif
   } else if (config.arch == Arch::cc) {
 #ifdef TI_WITH_CC
     program_impl_ = std::make_unique<CCProgramImpl>(config);

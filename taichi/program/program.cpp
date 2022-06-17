89af1f366a385ae153c0384799fd69b35c065f6b
jim19930609 <jim19930609@gmail.com>
2022-06-17 16:02:31 +0800

    Minor bug fix

--- taichi/program/program.cpp
+++ taichi/program/program.cpp
@@ -6,7 +6,6 @@
 #include "taichi/program/extension.h"
 #include "taichi/backends/cpu/codegen_cpu.h"
 #include "taichi/struct/struct.h"
-#include "taichi/llvm/struct_llvm.h"
 #include "taichi/backends/metal/api.h"
 #include "taichi/backends/wasm/aot_module_builder_impl.h"
 #include "taichi/backends/opengl/opengl_program.h"

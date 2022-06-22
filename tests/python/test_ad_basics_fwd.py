fbe7eee078e44bdd067b09071bffd4b179c04944
mingrui <mingrui.zhang@taichi.graphics>
2022-06-22 20:44:46 +0800

    Update base for Update on "[autodiff] Support for-loop and mutation for forward mode"
    [ghstack-poisoned]

--- tests/python/test_ad_basics_fwd.py
+++ tests/python/test_ad_basics_fwd.py
@@ -38,3 +38,48 @@ def test_ad_fwd_multiply():
         ad_fwd_multiply()
 
     assert loss.grad[1] == 7
+
+
+@test_utils.test(arch=[ti.cpu, ti.gpu])
+def test_multiple_calls():
+    N = 5
+    a = ti.field(float, shape=N)
+    b = ti.field(float, shape=N)
+    loss_1 = ti.field(float, shape=())
+    loss_2 = ti.field(float, shape=())
+
+    for i in range(N):
+        a[i] = i
+        b[i] = i
+
+    @ti.kernel
+    def multiple_calls():
+        loss_1[None] += 3 * b[1]**2 + 5 * a[3]**2
+        loss_2[None] += 4 * b[2]**2 + 6 * a[4]**2
+
+    with ti.ad.FwdMode(loss=loss_1, parameters=a,
+                       seed=[1.0 for _ in range(N)]):
+        multiple_calls()
+    assert loss_1.grad[None] == 30
+    assert not loss_2.snode.ptr.has_dual() and not b.snode.ptr.has_dual()
+
+    with ti.ad.FwdMode(loss=loss_1, parameters=b,
+                       seed=[1.0 for _ in range(N)]):
+        multiple_calls()
+    assert loss_1.grad[None] == 6
+    assert not loss_2.snode.ptr.has_dual(
+    ) and not a.snode.ptr.is_dual_activated()
+
+    with ti.ad.FwdMode(loss=loss_2, parameters=b,
+                       seed=[1.0 for _ in range(N)]):
+        multiple_calls()
+    assert loss_2.grad[None] == 16
+    assert not loss_1.snode.ptr.is_dual_activated(
+    ) and not a.snode.ptr.is_dual_activated()
+
+    with ti.ad.FwdMode(loss=loss_2, parameters=a,
+                       seed=[1.0 for _ in range(N)]):
+        multiple_calls()
+    assert loss_2.grad[None] == 48
+    assert not loss_1.snode.ptr.is_dual_activated(
+    ) and not b.snode.ptr.is_dual_activated()

198986fa9061e080ec3a93d9e22fb68745d984b5
neozhaoliang <mathzhaoliang@gmail.com>
2022-06-28 01:32:28 +0800

    add inf and nan

---
+++ tests/python/test_inf_nan.py
@@ -0,0 +1,31 @@
+from email.policy import default
+import pytest
+
+import taichi as ti
+from taichi.math import nan, inf, isnan, isinf
+from tests import test_utils
+
+
+def _test_inf_nan(dt):
+    ti.init(default_fp=dt)
+
+    @ti.kernel
+    def make_tests():
+        assert isnan(nan) == isnan(-nan) == True
+        x = -1.0
+        assert isnan(ti.sqrt(x)) == True
+        assert isnan(inf) == isnan(1.0) == isnan(-1) == False
+        assert isinf(inf) == isinf(-inf) == True
+        assert isinf(nan) == isinf(1.0) == isinf(-1) == False
+
+    make_tests()
+
+
+@test_utils.test(default_fp=ti.f32)
+def test_inf_nan_f32():
+    _test_inf_nan(ti.f32)
+
+
+@test_utils.test(default_fp=ti.f64)
+def test_inf_nan_f64():
+    _test_inf_nan(ti.f64)

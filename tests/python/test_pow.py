cde1ade326d05dac93455fdef7a314d8b1514409
AD1024 <dh63@cs.washington.edu>
2022-06-28 15:23:41 -0700

    fix format

--- tests/python/test_pow.py
+++ tests/python/test_pow.py
@@ -1,4 +1,5 @@
 from email.policy import default
+
 import taichi as ti
 from tests import test_utils
 
@@ -48,23 +49,26 @@ def test_pow_i32():
 def test_pow_i64():
     _test_pow_i(ti.i64)
 
+
 def _test_negative_exp(dt):
     z = ti.field(ti.lang.impl.get_runtime().default_fp, shape=())
-    
+
     @ti.kernel
     def workload(x: dt, y: ti.template()):
-        z[None] = x ** y
-        
+        z[None] = x**y
+
     for x in range(-5, 5):
         for y in range(-5, -1):
             if x != 0:
                 workload(x, y)
                 assert abs(z[None] / x**y - 1) < 0.00001
 
+
 @test_utils.test(default_fp=ti.f32, require=ti.extension.data64)
 def test_negative_exp_f32():
     _test_negative_exp(ti.i32)
     _test_negative_exp(ti.i64)
+
 
 @test_utils.test(default_fp=ti.f64, require=ti.extension.data64)
 def test_negative_exp_f64():

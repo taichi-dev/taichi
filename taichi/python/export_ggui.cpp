a2965ec9bc5b8c725a35baa5bda6615ea657f0d6
Mocki <34432001+Morcki@users.noreply.github.com>
2022-07-06 15:12:07 +0800

    Update taichi/python/export_ggui.cpp
    Co-authored-by: YuZhang <YuCrazing@users.noreply.github.com>

--- taichi/python/export_ggui.cpp
+++ taichi/python/export_ggui.cpp
@@ -40,7 +40,7 @@ pybind11::tuple vec3_to_tuple(glm::vec3 v) {
 // https://stackoverflow.com/questions/44659924/returning-numpy-arrays-via-pybind11
 py::array_t<float> mat4_to_nparray(glm::mat4 mat) {
 // Here we must explicitly pass args using py::detail::any_container<ssize_t>. Refs:
-  // ref:https://stackoverflow.com/questions/54055530/error-no-matching-function-for-call-to-pybind11buffer-infobuffer-info
+// https://stackoverflow.com/questions/54055530/error-no-matching-function-for-call-to-pybind11buffer-infobuffer-info
   return py::array_t<float>(
       py::detail::any_container<ssize_t>({4, 4}),  // shape (rows, cols)
       py::detail::any_container<ssize_t>(

f56201d57e6cec9dd50715c140d0b067880fa66f
neozhaoliang <mathzhaoliang@gmail.com>
2022-07-09 10:34:59 +0800

    update doc and pytest

--- tests/python/test_floor_dtype_argument.py
+++ tests/python/test_floor_dtype_argument.py
@@ -29,9 +29,9 @@ def _test_floor_ceil_round(dt):
         assert ti.round(x, dt) == 2.0
         assert ti.round(x, int) == 2
 
-        assert all(ti.round(v) == [1, 2, 4])
-        assert all(ti.round(v, dt) == [1.0, 2.0, 4.0])
-        assert all(ti.round(v, int) == [1, 2, 4])
+        assert all(ti.round(v) == [1, 2, 3])
+        assert all(ti.round(v, dt) == [1.0, 2.0, 3.0])
+        assert all(ti.round(v, int) == [1, 2, 3])
 
     make_tests()
 

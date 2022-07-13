f1068ae3d6265067d4614387bbb232967e9e994b
Lin Jiang <linjiang@taichi.graphics>
2022-07-13 17:30:28 +0800

    [llvm] Avoid creating new LLVM contexts when updating struct module (#5397)
    * [llvm] Avoid creating new LLVM contexts when updating struct module
    
    * fix
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * fix
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * fix
    
    * fix
    
    * add comment
    
    * add comments on renaming
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * add comments
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- taichi/codegen/llvm/llvm_codegen_utils.cpp
+++ taichi/codegen/llvm/llvm_codegen_utils.cpp
@@ -6,12 +6,59 @@ namespace lang {
 std::string type_name(llvm::Type *type) {
   std::string type_name_str;
   llvm::raw_string_ostream rso(type_name_str);
-  type->print(rso);
+  type->print(rso, /*IsForDebug=*/false, /*NoDetails=*/true);
   return type_name_str;
 }
 
+/*
+ * Determine whether two types are the same
+ * (a type is a renamed version of the other one) based on the
+ * type name. Check recursively if the types are function types.
+ *
+ * The name of a type imported multiple times is added a suffix starting with a
+ * "." following by a number. For example, "RuntimeContext" may be renamed to
+ * names like "RuntimeContext.0" and "RuntimeContext.8".
+ */
+bool is_same_type(llvm::Type *required, llvm::Type *provided) {
+  if (required == provided) {
+    return true;
+  }
+  if (required->isPointerTy() != provided->isPointerTy()) {
+    return false;
+  }
+  if (required->isPointerTy()) {
+    required = required->getPointerElementType();
+    provided = provided->getPointerElementType();
+  }
+  if (required->isFunctionTy() != provided->isFunctionTy()) {
+    return false;
+  }
+  if (required->isFunctionTy()) {
+    auto req_func = llvm::dyn_cast<llvm::FunctionType>(required);
+    auto prov_func = llvm::dyn_cast<llvm::FunctionType>(provided);
+    if (!is_same_type(req_func->getReturnType(), prov_func->getReturnType())) {
+      return false;
+    }
+    if (req_func->getNumParams() != prov_func->getNumParams()) {
+      return false;
+    }
+    for (int j = 0; j < req_func->getNumParams(); j++) {
+      if (!is_same_type(req_func->getParamType(j),
+                        prov_func->getParamType(j))) {
+        return false;
+      }
+    }
+    return true;
+  }
+  auto req_name = type_name(required);
+  auto prov_name = type_name(provided);
+  int min_len = std::min(req_name.size(), prov_name.size());
+  return req_name.substr(0, min_len) == prov_name.substr(0, min_len);
+}
+
 void check_func_call_signature(llvm::Value *func,
-                               std::vector<llvm::Value *> arglist) {
+                               std::vector<llvm::Value *> &arglist,
+                               llvm::IRBuilder<> *builder) {
   llvm::FunctionType *func_type = nullptr;
   if (llvm::Function *fn = llvm::dyn_cast<llvm::Function>(func)) {
     func_type = fn->getFunctionType();
@@ -31,7 +78,19 @@ void check_func_call_signature(llvm::Value *func,
   for (int i = 0; i < num_params; i++) {
     auto required = func_type->getFunctionParamType(i);
     auto provided = arglist[i]->getType();
+    /*
+     * When importing a module from file, the imported `llvm::Type`s can get
+     * conflict with the same type in the llvm::Context. In such scenario,
+     * the imported types will be renamed from "original_type" to
+     * "original_type.xxx", making them separate types in essence.
+     * To make the types of the argument and parameter the same,
+     * a pointer cast must be performed.
+     */
     if (required != provided) {
+      if (is_same_type(required, provided)) {
+        arglist[i] = builder->CreatePointerCast(arglist[i], required);
+        continue;
+      }
       TI_INFO("Function : {}", std::string(func->getName()));
       TI_INFO("    Type : {}", type_name(func->getType()));
       if (&required->getContext() != &provided->getContext()) {

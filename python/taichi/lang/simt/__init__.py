f5117e229e1f511dfb18db14a23fae8b0cae8c26
Haidong Lan <haidonglan@taichi.graphics>
2022-07-18 11:09:52 +0800

    format

--- python/taichi/lang/simt/__init__.py
+++ python/taichi/lang/simt/__init__.py
@@ -1,4 +1,5 @@
 from taichi.lang.simt import block, grid, subgroup, warp
+
 #from taichi.lang.simt.block import SharedArray
 
 __all__ = ['warp', 'subgroup', 'block', 'grid']
--- python/taichi/lang/simt/block.py
+++ python/taichi/lang/simt/block.py
@@ -16,5 +16,4 @@ class SharedArray:
 
     @taichi_scope
     def _subscript(self, indices, get_ref=False):
-        return impl.make_index_expr(self.shared_array_proxy, (indices,))
-    
+        return impl.make_index_expr(self.shared_array_proxy, (indices, ))

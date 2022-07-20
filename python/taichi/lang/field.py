f124d498c09e1a89e5b91c5231d424ba06106dce
neozhaoliang <mathzhaoliang@gmail.com>
2022-07-20 16:54:06 +0800

    more versatile initialization for MatrixType

--- python/taichi/lang/field.py
+++ python/taichi/lang/field.py
@@ -1,6 +1,6 @@
 import taichi.lang
 from taichi._lib import core as _ti_core
-from taichi.lang.util import (python_scope, to_numpy_type, to_paddle_type, in_python_scope,
+from taichi.lang.util import (python_scope, to_numpy_type, to_paddle_type,
                               to_pytorch_type)
 
 
@@ -264,6 +264,7 @@ class ScalarField(Field):
     def __init__(self, var):
         super().__init__([var])
 
+    @python_scope
     def fill(self, val):
         """Fills this scalar field with a specified value.
         """

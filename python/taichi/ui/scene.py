0579d46e3abb8cea9d8f26a7d307cb7caada495f
Morcki <cocolytrees@gmail.com>
2022-07-21 10:59:50 +0800

    fix a bug when there is no indices, index_cout cannot be given

--- python/taichi/ui/scene.py
+++ python/taichi/ui/scene.py
@@ -142,7 +142,10 @@ class Scene:
         if vertex_count is None:
             vertex_count = vertices.shape[0]
         if index_count is None:
-            index_count = indices.shape[0]
+            if indices is None:
+                index_count = vertex_count # FIXME : Need to confirm
+            else:
+                index_count = indices.shape[0]
         copy_normals_to_vbo(vbo, normals)
         vbo_info = get_field_info(vbo)
         indices_info = get_field_info(indices)

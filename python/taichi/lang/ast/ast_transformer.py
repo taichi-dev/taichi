0f5a088e7abfff6e0e7a8140f8648686db4e5ce8
Yi Xu <xy_xuyi@foxmail.com>
2022-07-21 16:52:27 +0800

    Fix tests

--- python/taichi/lang/ast/ast_transformer.py
+++ python/taichi/lang/ast/ast_transformer.py
@@ -783,7 +783,9 @@ class ASTTransformer(Builder):
                     raise TaichiSyntaxError(
                         f'"{type(node_op).__name__}" is not supported in Taichi kernels.'
                     )
-            val = op(l, r) if val is True else ti_ops.bit_and(val, op(l, r))
+            val = ti_ops.bit_and(val, op(l, r))
+        if not isinstance(val, bool):
+            val = ti_ops.cast(val, primitive_types.i32)
         node.ptr = val
         return node.ptr
 

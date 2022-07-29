46d24b10c33b3021b45691b0365e16e3e2da1c2b
Morcki <cocolytrees@gmail.com>
2022-07-29 20:34:24 +0800

    Set default background color

--- taichi/ui/backends/vulkan/renderer.h
+++ taichi/ui/backends/vulkan/renderer.h
@@ -78,8 +78,8 @@ class TI_DLL_EXPORT Renderer {
   taichi::lang::StreamSemaphore get_render_complete_semaphore();
 
  private:
-  glm::vec3 background_color_ = glm::vec3(0.f, 0.f, 0.f);
-
+  glm::vec3 background_color_ = glm::vec3(0.45f, 0.55f, 0.60f);
+  
   std::vector<std::unique_ptr<Renderable>> renderables_;
   int next_renderable_;
 

fc3af55e56a17c9e9b770cefd33e7389c44ac0aa
Yi Xu <xy_xuyi@foxmail.com>
2022-08-01 13:52:37 +0800

    [type] Refine SNode with quant 10/n: Add validity checks and simplify BitStructType (#5573)
    * [type] Add validity checks and simplify BitStructType
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Fix
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- taichi/ir/type_factory.cpp
+++ taichi/ir/type_factory.cpp
@@ -82,12 +82,11 @@ BitStructType *TypeFactory::get_bit_struct_type(
     PrimitiveType *physical_type,
     const std::vector<Type *> &member_types,
     const std::vector<int> &member_bit_offsets,
-    const std::vector<bool> &member_owns_shared_exponents,
     const std::vector<int> &member_exponents,
     const std::vector<std::vector<int>> &member_exponent_users) {
   bit_struct_types_.push_back(std::make_unique<BitStructType>(
-      physical_type, member_types, member_bit_offsets,
-      member_owns_shared_exponents, member_exponents, member_exponent_users));
+      physical_type, member_types, member_bit_offsets, member_exponents,
+      member_exponent_users));
   return bit_struct_types_.back().get();
 }
 

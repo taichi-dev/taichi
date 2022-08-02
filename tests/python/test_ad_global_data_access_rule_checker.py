012e5d8a98ab3403d43958a5f557a96ecbd4d4cf
mingrui <mingrui.zhang@taichi.graphics>
2022-08-02 15:58:09 +0800

    Update on "[autodiff] Add gradient flag for global data access rule checker"
    [ghstack-poisoned]

--- tests/python/test_ad_global_data_access_rule_checker.py
+++ tests/python/test_ad_global_data_access_rule_checker.py
@@ -1,3 +1,4 @@
+import pytest
 from numpy import float16
 
 import taichi as ti
@@ -7,7 +8,6 @@ from tests import test_utils
 @test_utils.test(debug=True)
 def test_adjoint_visited_needs_grad():
     x = ti.field(float, shape=(), needs_grad=True)
-    ti.root.root._allocate_grad_visited()
 
     @ti.kernel
     def test():
@@ -22,7 +22,6 @@ def test_adjoint_visited_needs_grad():
 def test_adjoint_visited_lazy_grad():
     x = ti.field(float, shape=())
     ti.root.lazy_grad()
-    ti.root.root._allocate_grad_visited()
 
     @ti.kernel
     def test():
@@ -38,7 +37,6 @@ def test_adjoint_visited_place_grad():
     x = ti.field(float)
     y = ti.field(float)
     ti.root.place(x, x.grad, y)
-    ti.root.root._allocate_grad_visited()
 
     @ti.kernel
     def test():
@@ -48,3 +46,23 @@ def test_adjoint_visited_place_grad():
 
     assert x.snode.ptr.has_adjoint_visited()
     assert not y.snode.ptr.has_adjoint_visited()
+
+
+@test_utils.test(debug=False)
+def test_adjoint_visited_needs_grad():
+    x = ti.field(float, shape=(), needs_grad=True)
+
+    @ti.kernel
+    def test():
+        x[None] = 1
+
+    with pytest.warns(Warning) as record:
+        with ti.ad.Tape(loss=x, check_autodiff_valid=True):
+            test()
+
+    warn_raised = False
+    for warn in record:
+        if 'Debug mode is disabled, autodiff valid check will not work. Please specify `ti.init(debug=True)` to enable the check.' in warn.message.args[
+                0]:
+            warn_raised = True
+    assert warn_raised

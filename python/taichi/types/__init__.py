4f4ef523e4557072bfce9e988925e906b7b6e541
Yi Xu <xy_xuyi@foxmail.com>
2022-08-03 10:48:11 +0800

    [type] Rename module quantized_types to quant

--- python/taichi/types/__init__.py
+++ python/taichi/types/__init__.py
@@ -5,9 +5,9 @@ This module defines data types in Taichi:
 - compound: matrix, vector, struct.
 - template: for reference types.
 - ndarray: for arbitrary arrays.
-- quantized: for quantized types, see "https://yuanming.taichi.graphics/publication/2021-quantaichi/quantaichi.pdf"
+- quant: for quantized types, see "https://yuanming.taichi.graphics/publication/2021-quantaichi/quantaichi.pdf"
 """
-from taichi.types import quantized_types as quant
+from taichi.types import quant
 from taichi.types.annotations import *
 from taichi.types.compound_types import *
 from taichi.types.ndarray_type import *

c92a41d1310c5563a27959e2b5b519b8be2a0db5
PGZXB <pgzxb@qq.com>
2022-08-03 22:31:31 +0800

    Reset default offline_cache_max_size_of_files and offline_cache_cleaning_factor

--- taichi/program/compile_config.h
+++ taichi/program/compile_config.h
@@ -98,8 +98,8 @@ struct CompileConfig {
   std::string offline_cache_file_path{get_repo_dir() + "ticache"};
   std::string offline_cache_cleaning_policy{
       "lru"};  // "never"|"version"|"lru"|"fifo"
-  int offline_cache_max_size_of_files{1024 * 1024};  // bytes
-  double offline_cache_cleaning_factor{0.25};        // [0.f, 1.f]
+  int offline_cache_max_size_of_files{100 * 1024 * 1024};  // bytes, default: 100MB
+  double offline_cache_cleaning_factor{0.5};        // [0.f, 1.f]
 
   int num_compile_threads{0};
   std::string vk_api_version;

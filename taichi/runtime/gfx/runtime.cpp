7682b79b231235baf09da7583bfa5f3724d3406d
PENGUINLIONG <admin@penguinliong.moe>
2022-08-07 09:12:46 +0800

    Update runtime.cpp

--- taichi/runtime/gfx/runtime.cpp
+++ taichi/runtime/gfx/runtime.cpp
@@ -562,7 +562,6 @@ void GfxRuntime::transition_image(DeviceAllocation image, ImageLayout layout) {
   ImageLayout &last_layout = last_image_layouts_[image.alloc_id];
   ensure_current_cmdlist();
   current_cmdlist_->image_transition(image, last_layout, layout);
-  submit_current_cmdlist_if_timeout();
   last_layout = layout;
 }
 

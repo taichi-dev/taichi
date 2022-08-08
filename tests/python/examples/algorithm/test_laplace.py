9e65c5a46773da8be7053b2e6e50d51ab8ac2a86
Proton <feisuzhu@163.com>
2022-08-08 13:56:17 +0800

    [ci] Only run portion of tests on PR draft (#5626)

--- tests/python/examples/algorithm/test_laplace.py
+++ tests/python/examples/algorithm/test_laplace.py
@@ -1,3 +1,9 @@
+import os
+
+import pytest
+
+
+@pytest.mark.skipif(os.environ.get('TI_LITE_TEST'), reason='Lite test')
 def test_laplace():
     from taichi.examples.algorithm.laplace import laplace, x, y
 

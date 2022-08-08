63278741eb6ed2fbe5b7b695a6203be1c224191a
Xy Ren <xy.r@outlook.com>
2022-08-08 17:28:39 +0800

    [IR] Further work on the type system

--- taichi/ir/type_system.cpp
+++ taichi/ir/type_system.cpp
@@ -3,7 +3,9 @@
 namespace taichi {
 namespace lang {
 
-void TyVar::unify(DataType dt, std::map<Identifier, DataType> &solutions) {
+void TyVar::unify(int pos,
+                  DataType dt,
+                  std::map<Identifier, DataType> &solutions) const {
   if (solutions.find(name) != solutions.end()) {
     if (solutions[name] != dt) {
       throw TyVarMismatch(name, solutions[name], dt);
@@ -13,7 +15,7 @@ void TyVar::unify(DataType dt, std::map<Identifier, DataType> &solutions) {
   }
 }
 
-DataType TyVar::resolve(const std::map<Identifier, DataType> &solutions) {
+DataType TyVar::resolve(const std::map<Identifier, DataType> &solutions) const {
   if (solutions.find(name) == solutions.end()) {
     throw TyVarUnsolved(name);
   } else {
@@ -21,71 +23,77 @@ DataType TyVar::resolve(const std::map<Identifier, DataType> &solutions) {
   }
 }
 
-std::string TyVar::to_string() {
+std::string TyVar::to_string() const {
   return name.name();
 }
 
-void TyLub::unify(DataType dt, std::map<Identifier, DataType> &solutions) {
-  TyMono(resolve(solutions)).unify(dt, solutions);
+void TyLub::unify(int pos,
+                  DataType dt,
+                  std::map<Identifier, DataType> &solutions) const {
+  TyMono(resolve(solutions)).unify(pos, dt, solutions);
 }
 
-DataType TyLub::resolve(const std::map<Identifier, DataType> &solutions) {
+DataType TyLub::resolve(const std::map<Identifier, DataType> &solutions) const {
   return promoted_type(lhs->resolve(solutions)->get_compute_type(), rhs->resolve(solutions)->get_compute_type());
 }
 
-std::string TyLub::to_string() {
+std::string TyLub::to_string() const {
   return lhs->to_string() + " | " + rhs->to_string();
 }
 
-void TyCompute::unify(DataType dt, std::map<Identifier, DataType> &solutions) {
-  TyMono(resolve(solutions)).unify(dt, solutions);
+void TyCompute::unify(int pos,
+                      DataType dt,
+                      std::map<Identifier, DataType> &solutions) const {
+  TyMono(resolve(solutions)).unify(pos, dt, solutions);
 }
 
-DataType TyCompute::resolve(const std::map<Identifier, DataType> &solutions) {
+DataType TyCompute::resolve(const std::map<Identifier, DataType> &solutions) const {
   return exp->resolve(solutions)->get_compute_type();
 }
 
-std::string TyCompute::to_string() {
+std::string TyCompute::to_string() const {
   return "comp(" + exp->to_string() + ")";
 }
 
-void TyMono::unify(DataType dt, std::map<Identifier, DataType> &solutions) {
+void TyMono::unify(int pos,
+                   DataType dt,
+                   std::map<Identifier, DataType> &solutions) const {
   if (monotype != dt) {
-    throw TypeMismatch(monotype, dt);
+    throw TypeMismatch(pos, monotype, dt);
   }
 }
 
-DataType TyMono::resolve(const std::map<Identifier, DataType> &solutions) {
+DataType TyMono::resolve(const std::map<Identifier, DataType> &solutions) const {
   return monotype;
 }
 
-std::string TyMono::to_string() {
+std::string TyMono::to_string() const {
   return monotype.to_string();
 }
 
-std::string TypeMismatch::to_string() {
-  return "the argument type " + arg.to_string() + " is incompatible with the expected type " + param.to_string();
+std::string TypeMismatch::to_string() const {
+  return "expected " + arg.to_string() + " for argument " + std::to_string(position) + ", but got " + param.to_string();
 }
 
-std::string TyVarUnsolved::to_string() {
+std::string TyVarUnsolved::to_string() const {
   return "cannot infer the type variable " + var.name() + ". this is not supposed to happen; please report this as a bug";
 }
 
-std::string TraitMismatch::to_string() {
+std::string TraitMismatch::to_string() const {
   return "the argument type " + dt.to_string() + " is not " + trait->to_string();
 }
 
-std::string ArgLengthMismatch::to_string() {
+std::string ArgLengthMismatch::to_string() const {
   return std::to_string(arg) + " arguments were passed in but expected " + std::to_string(param) + ". this is not supposed to happen; please report this as a bug";
 }
 
-DataType Signature::type_check(std::vector<DataType> arguments) {
+DataType Signature::type_check(std::vector<DataType> arguments) const {
   if (parameters.size() != arguments.size()) {
     throw ArgLengthMismatch(parameters.size(), arguments.size());
   }
   std::map<Identifier, DataType> solutions;
   for (int i = 0; i < parameters.size(); i++) {
-    parameters[i]->unify(arguments[i], solutions);
+    parameters[i]->unify(i, arguments[i], solutions);
   }
   for (auto &c: constraints) {
     auto dt = c.tyvar->resolve(solutions);
@@ -94,6 +102,25 @@ DataType Signature::type_check(std::vector<DataType> arguments) {
     }
   }
   return ret_type->resolve(solutions);
+}
+
+void Operation::type_check(std::vector<DataType> arg_types) const {
+  try {
+    sig.type_check(arg_types);
+  } catch (TypeSystemError &err) {
+    std::string msg;
+    msg += "In a call to the operation `" + name + "`:\n";
+    msg += "  " + err.to_string();
+    msg += "  ( called with argument types ";
+    for (int i = 0; i < arg_types.size(); i++) {
+      msg += arg_types[i].to_string();
+      if (i != arg_types.size())
+        msg += ", ";
+      else
+        msg += " )\n";
+    }
+    throw TaichiTypeError(msg);
+  }
 }
 
 }
--- taichi/ir/type_system.h
+++ taichi/ir/type_system.h
@@ -1,21 +1,25 @@
 #pragma once
 
 #include "taichi/ir/ir.h"
+#include "taichi/ir/frontend_ir.h"
 
 namespace taichi {
 namespace lang {
 
 class TypeSystemError {
-  virtual std::string to_string() = 0;
+ public:
+  virtual std::string to_string() const = 0;
 };
 
 class TyVar;
 
 class TypeExpression {
  public:
-  virtual void unify(DataType dt, std::map<Identifier, DataType> &solutions) = 0;
-  virtual DataType resolve(std::map<Identifier, DataType> const &solutions) = 0;
-  virtual std::string to_string() = 0;
+  virtual void unify(int pos,
+                     DataType dt,
+                     std::map<Identifier, DataType> &solutions) const = 0;
+  virtual DataType resolve(std::map<Identifier, DataType> const &solutions) const = 0;
+  virtual std::string to_string() const = 0;
 };
 
 using TypeExpr = std::shared_ptr<TypeExpression>;
@@ -24,36 +28,44 @@ class TyVar : public TypeExpression {
   const Identifier name;
  public:
   TyVar(Identifier id) : name(id) {}
-  void unify(DataType dt, std::map<Identifier, DataType> &solutions) override;
-  DataType resolve(std::map<Identifier, DataType> const &solutions) override;
-  std::string to_string() override;
+  void unify(int pos,
+             DataType dt,
+             std::map<Identifier, DataType> &solutions) const override;
+  DataType resolve(std::map<Identifier, DataType> const &solutions) const override;
+  std::string to_string() const override;
 };
 
 class TyLub : public TypeExpression {
   const TypeExpr lhs, rhs;
  public:
   TyLub(TypeExpr lhs, TypeExpr rhs) : lhs(lhs), rhs(rhs) {}
-  void unify(DataType dt, std::map<Identifier, DataType> &solutions) override;
-  DataType resolve(std::map<Identifier, DataType> const &solutions) override;
-  std::string to_string() override;
+  void unify(int pos,
+             DataType dt,
+             std::map<Identifier, DataType> &solutions) const override;
+  DataType resolve(std::map<Identifier, DataType> const &solutions) const override;
+  std::string to_string() const override;
 };
 
 class TyCompute : public TypeExpression {
   const TypeExpr exp;
  public:
   TyCompute(TypeExpr exp) : exp(exp) {}
-  void unify(DataType dt, std::map<Identifier, DataType> &solutions) override;
-  DataType resolve(std::map<Identifier, DataType> const &solutions) override;
-  std::string to_string() override;
+  void unify(int pos,
+             DataType dt,
+             std::map<Identifier, DataType> &solutions) const override;
+  DataType resolve(std::map<Identifier, DataType> const &solutions) const override;
+  std::string to_string() const override;
 };
 
 class TyMono : public TypeExpression {
   const DataType monotype;
  public:
   TyMono(DataType dt) : monotype(dt) {}
-  void unify(DataType dt, std::map<Identifier, DataType> &solutions) override;
-  DataType resolve(std::map<Identifier, DataType> const &solutions) override;
-  std::string to_string() override;
+  void unify(int pos,
+             DataType dt,
+             std::map<Identifier, DataType> &solutions) const override;
+  DataType resolve(std::map<Identifier, DataType> const &solutions) const override;
+  std::string to_string() const override;
 };
 
 class TyVarMismatch : public TypeSystemError {
@@ -61,27 +73,28 @@ class TyVarMismatch : public TypeSystemError {
   const DataType original, conflicting;
  public:
   TyVarMismatch(Identifier var, DataType original, DataType conflicting) : var(var), original(original), conflicting(conflicting) {}
-  std::string to_string() override;
+  std::string to_string() const override;
 };
 
 class TypeMismatch : public TypeSystemError {
+  int position;
   const DataType param, arg;
  public:
-  TypeMismatch(DataType param, DataType arg) : param(param), arg(arg) {}
-  std::string to_string() override;
+  TypeMismatch(int pos, DataType param, DataType arg) : position(pos), param(param), arg(arg) {}
+  std::string to_string() const override;
 };
 
 class TyVarUnsolved : public TypeSystemError {
   const Identifier var;
  public:
   TyVarUnsolved(Identifier var) : var(var){}
-  std::string to_string() override;
+  std::string to_string() const override;
 };
 
 class Trait {
  public:
   virtual bool validate(const DataType dt) = 0;
-  virtual std::string to_string() = 0;
+  virtual std::string to_string() const = 0;
 };
 
 class TraitMismatch : public TypeSystemError {
@@ -89,14 +102,14 @@ class TraitMismatch : public TypeSystemError {
   const std::shared_ptr<Trait> trait;
  public:
   TraitMismatch(DataType dt, std::shared_ptr<Trait> trait) : dt(dt), trait(trait) {}
-  std::string to_string() override;
+  std::string to_string() const override;
 };
 
 class ArgLengthMismatch : public TypeSystemError {
   const int param, arg;
  public:
   ArgLengthMismatch(int param, int arg) : param(param), arg(arg) {}
-  std::string to_string() override;
+  std::string to_string() const override;
 };
 
 class Constraint {
@@ -111,7 +124,15 @@ class Signature {
   const TypeExpr ret_type;
 
  public:
-  DataType type_check(std::vector<DataType> arguments);
+  DataType type_check(std::vector<DataType> arguments) const;
+};
+
+class Operation {
+  const Signature sig;
+  const std::string name;
+ public:
+  void type_check(std::vector<DataType> arg_types) const;
+  virtual void flatten(Expression::FlattenContext *ctx, std::vector<Expr> args) const = 0;
 };
 
 }

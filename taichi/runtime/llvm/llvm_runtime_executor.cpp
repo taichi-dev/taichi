8f339471d23bdb973ef8ae71aa434fbab1500bae
Lin Jiang <linjiang@taichi.graphics>
2022-08-10 20:47:24 +0800

    fix

--- taichi/runtime/llvm/llvm_runtime_executor.cpp
+++ taichi/runtime/llvm/llvm_runtime_executor.cpp
@@ -598,7 +598,7 @@ void LlvmRuntimeExecutor::materialize_runtime(MemoryPool *memory_pool,
 
 void LlvmRuntimeExecutor::destroy_snode_tree(SNodeTree *snode_tree) {
   get_llvm_context(config_->arch)
-      ->delete_functions_of_snode_tree(snode_tree->id());
+      ->delete_snode_tree(snode_tree->id());
   snode_tree_buffer_manager_->destroy(snode_tree);
 }
 

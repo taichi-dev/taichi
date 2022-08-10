eb0ab1cafea582f979578a2c104cc3a90688f168
Mingrui Zhang <33411325+erizmr@users.noreply.github.com>
2022-08-10 16:45:55 +0800

    [autodiff] Fix AdStackAllocaStmt not correctly backup (#5692)
    * [autodiff] Fix AdStackAllocaStmt not correctly backup
    
    * remove redundant replace
    
    * add comments
    
    * add polar decompose test
    
    * erase outdated AdStackAllocaStmt

---
+++ tests/python/test_ad_math_func.py
@@ -0,0 +1,20 @@
+import taichi as ti
+from tests import test_utils
+
+
+@test_utils.test(require=ti.extension.adstack)
+def test_polar_decompose_2D():
+    # `polar_decompose3d` in current Taichi version (v1.1) does not support autodiff,
+    # becasue it mixed usage of for-loops and statements without looping.
+    dim = 2
+    F_1 = ti.Matrix.field(dim, dim, dtype=ti.f32, shape=(), needs_grad=True)
+    F = ti.Matrix.field(dim, dim, dtype=ti.f32, shape=(), needs_grad=True)
+    loss = ti.field(dtype=ti.f32, shape=(), needs_grad=True)
+
+    @ti.kernel
+    def polar_decompose_2D():
+        r, s = ti.polar_decompose(F[None])
+        F_1[None] += r
+
+    with ti.ad.Tape(loss=loss):
+        polar_decompose_2D()

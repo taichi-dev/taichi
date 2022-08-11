b5f218954e5ecffb64583f18d33a3f342351df15
Morcki <cocolytrees@gmail.com>
2022-08-11 19:06:42 +0800

    fix conflict bugs

--- tests/python/test_matrix.py
+++ tests/python/test_matrix.py
@@ -5,6 +5,7 @@ import numpy as np
 from taichi.lang import impl
 from taichi.lang.kernel_impl import kernel
 from taichi.lang.misc import get_host_arch_list
+import pytest
 
 import taichi as ti
 from tests import test_utils
@@ -360,6 +361,126 @@ def test_scale():
     error += check_epsilon_equal(scale_mat, scale_ref, 0.00001)
     assert error == 0
     
+@test_utils.test(arch=get_host_arch_list())
+def test_python_scope_vector_operations():
+    for ops in vector_operation_types:
+        a, b = test_vector_arrays[:2]
+        m1, m2 = ti.Vector(a), ti.Vector(b)
+        c = ops(m1, m2)
+        assert np.allclose(c.to_numpy(), ops(a, b))
+
+
+@test_utils.test(arch=get_host_arch_list())
+def test_python_scope_matrix_operations():
+    for ops in operation_types:
+        a, b = test_matrix_arrays[:2]
+        m1, m2 = ti.Matrix(a), ti.Matrix(b)
+        c = ops(m1, m2)
+        assert np.allclose(c.to_numpy(), ops(a, b))
+
+
+# TODO: Loops inside the function will cause AssertionError:
+# No new variables can be declared after kernel invocations
+# or Python-scope field accesses.
+# ideally we should use pytest.fixture to parameterize the tests
+# over explicit loops
+@pytest.mark.parametrize('ops', vector_operation_types)
+@test_utils.test(arch=get_host_arch_list())
+def test_python_scope_vector_field(ops):
+    t1 = ti.Vector.field(2, dtype=ti.i32, shape=())
+    t2 = ti.Vector.field(2, dtype=ti.i32, shape=())
+    a, b = test_vector_arrays[:2]
+    t1[None], t2[None] = a.tolist(), b.tolist()
+
+    c = ops(t1[None], t2[None])
+    assert np.allclose(c.to_numpy(), ops(a, b))
+
+
+@pytest.mark.parametrize('ops', vector_operation_types)
+@test_utils.test(arch=get_host_arch_list())
+def test_python_scope_matrix_field(ops):
+    t1 = ti.Matrix.field(2, 2, dtype=ti.i32, shape=())
+    t2 = ti.Matrix.field(2, 2, dtype=ti.i32, shape=())
+    a, b = test_matrix_arrays[:2]
+    # ndarray not supported here
+    t1[None], t2[None] = a.tolist(), b.tolist()
+
+    c = ops(t1[None], t2[None])
+    print(c)
+
+    assert np.allclose(c.to_numpy(), ops(a, b))
+
+
+@test_utils.test(arch=get_host_arch_list())
+def test_constant_matrices():
+    assert ti.cos(math.pi / 3) == test_utils.approx(0.5)
+    assert np.allclose((-ti.Vector([2, 3])).to_numpy(), np.array([-2, -3]))
+    assert ti.cos(ti.Vector([2, 3])).to_numpy() == test_utils.approx(
+        np.cos(np.array([2, 3])))
+    assert ti.max(2, 3) == 3
+    res = ti.max(4, ti.Vector([3, 4, 5]))
+    assert np.allclose(res.to_numpy(), np.array([4, 4, 5]))
+    res = ti.Vector([2, 3]) + ti.Vector([3, 4])
+    assert np.allclose(res.to_numpy(), np.array([5, 7]))
+    res = ti.atan2(ti.Vector([2, 3]), ti.Vector([3, 4]))
+    assert res.to_numpy() == test_utils.approx(
+        np.arctan2(np.array([2, 3]), np.array([3, 4])))
+    res = ti.Matrix([[2, 3], [4, 5]]) @ ti.Vector([2, 3])
+    assert np.allclose(res.to_numpy(), np.array([13, 23]))
+    v = ti.Vector([3, 4])
+    w = ti.Vector([5, -12])
+    r = ti.Vector([1, 2, 3, 4])
+    s = ti.Matrix([[1, 2], [3, 4]])
+    assert v.normalized().to_numpy() == test_utils.approx(np.array([0.6, 0.8]))
+    assert v.cross(w) == test_utils.approx(-12 * 3 - 4 * 5)
+    w.y = v.x * w[0]
+    r.x = r.y
+    r.y = r.z
+    r.z = r.w
+    r.w = r.x
+    assert np.allclose(w.to_numpy(), np.array([5, 15]))
+    assert ti.select(ti.Vector([1, 0]), ti.Vector([2, 3]),
+                     ti.Vector([4, 5])) == ti.Vector([2, 5])
+    s[0, 1] = 2
+    assert s[0, 1] == 2
+
+    @ti.kernel
+    def func(t: ti.i32):
+        m = ti.Matrix([[2, 3], [4, t]])
+        print(m @ ti.Vector([2, 3]))
+        m += ti.Matrix([[3, 4], [5, t]])
+        print(m @ v)
+        print(r.x, r.y, r.z, r.w)
+        s = w.transpose() @ m
+        print(s)
+        print(m)
+
+    func(5)
+
+
+@pytest.mark.parametrize('ops', vector_operation_types)
+@test_utils.test(arch=get_host_arch_list())
+def test_taichi_scope_vector_operations_with_global_vectors(ops):
+    a, b, c = test_vector_arrays[:3]
+    m1, m2 = ti.Vector(a), ti.Vector(b)
+    r1 = ti.Vector.field(2, dtype=ti.i32, shape=())
+    r2 = ti.Vector.field(2, dtype=ti.i32, shape=())
+    m3 = ti.Vector.field(2, dtype=ti.i32, shape=())
+    m3.from_numpy(c)
+
+    @ti.kernel
+    def run():
+        r1[None] = ops(m1, m2)
+        r2[None] = ops(m1, m3[None])
+
+    run()
+
+    assert np.allclose(r1[None].to_numpy(), ops(a, b))
+    assert np.allclose(r2[None].to_numpy(), ops(a, c))
+
+
+@pytest.mark.parametrize('ops', vector_operation_types)
+@test_utils.test(arch=get_host_arch_list())
 def test_taichi_scope_matrix_operations_with_global_matrices(ops):
     a, b, c = test_matrix_arrays[:3]
     m1, m2 = ti.Matrix(a), ti.Matrix(b)
@@ -681,3 +802,142 @@ def test_matrix_field_dynamic_index_different_cell_size_bytes():
 def test_matrix_field_dynamic_index_different_offset_bytes_in_parent_cell():
     temp_a = ti.field(ti.f32)
     temp_b = ti.field(ti.f32)
+
+    v = ti.Vector.field(2, ti.i32)
+    x = v.get_scalar_field(0)
+    y = v.get_scalar_field(1)
+
+    ti.root.dense(ti.i, 8).place(temp_a, x)
+    ti.root.dense(ti.i, 8).place(y, temp_b)
+
+    impl.get_runtime().materialize()
+    assert v.dynamic_index_stride is None
+
+
+@test_utils.test(arch=[ti.cpu, ti.cuda])
+def test_matrix_field_dynamic_index_different_stride():
+    temp = ti.field(ti.f32)
+
+    v = ti.Vector.field(3, ti.i32)
+    x = v.get_scalar_field(0)
+    y = v.get_scalar_field(1)
+    z = v.get_scalar_field(2)
+
+    ti.root.dense(ti.i, 8).place(x, y, temp, z)
+
+    impl.get_runtime().materialize()
+    assert v.dynamic_index_stride is None
+
+
+@test_utils.test(arch=[ti.cpu, ti.cuda], dynamic_index=True)
+def test_matrix_field_dynamic_index_multiple_materialize():
+    @ti.kernel
+    def empty():
+        pass
+
+    empty()
+
+    n = 5
+    a = ti.Vector.field(3, dtype=ti.i32, shape=n)
+
+    @ti.kernel
+    def func():
+        for i in a:
+            a[i][i % 3] = i
+
+    func()
+    for i in range(n):
+        for j in range(3):
+            assert a[i][j] == (i if j == i % 3 else 0)
+
+
+@test_utils.test(arch=[ti.cpu, ti.cuda], dynamic_index=True, debug=True)
+def test_local_vector_initialized_in_a_loop():
+    @ti.kernel
+    def foo():
+        for c in range(10):
+            p = ti.Vector([c, c * 2])
+            for i in range(2):
+                assert p[i] == c * (i + 1)
+
+    foo()
+
+
+@test_utils.test(debug=True)
+def test_vector_dtype():
+    @ti.kernel
+    def foo():
+        a = ti.Vector([1, 2, 3], ti.f32)
+        a /= 2
+        assert all(abs(a - (0.5, 1., 1.5)) < 1e-6)
+        b = ti.Vector([1.5, 2.5, 3.5], ti.i32)
+        assert all(b == (1, 2, 3))
+
+    foo()
+
+
+@test_utils.test(debug=True)
+def test_matrix_dtype():
+    @ti.kernel
+    def foo():
+        a = ti.Vector([[1, 2], [3, 4]], ti.f32)
+        a /= 2
+        assert all(abs(a - ((0.5, 1.), (1.5, 2.))) < 1e-6)
+        b = ti.Vector([[1.5, 2.5], [3.5, 4.5]], ti.i32)
+        assert all(b == ((1, 2), (3, 4)))
+
+    foo()
+
+
+inplace_operation_types = [
+    operator.iadd, operator.isub, operator.imul, operator.ifloordiv,
+    operator.imod, operator.ilshift, operator.irshift, operator.ior,
+    operator.ixor, operator.iand
+]
+
+
+@test_utils.test()
+def test_python_scope_inplace_operator():
+    for ops in inplace_operation_types:
+        a, b = test_matrix_arrays[:2]
+        m1, m2 = ti.Matrix(a), ti.Matrix(b)
+        m1 = ops(m1, m2)
+        assert np.allclose(m1.to_numpy(), ops(a, b))
+
+
+@test_utils.test()
+def test_slice_assign_basic():
+    @ti.kernel
+    def foo():
+        m = ti.Matrix([[0., 0., 0., 0.] for _ in range(3)])
+        vec = ti.Vector([1., 2., 3., 4.])
+        m[0, :] = vec.transpose()
+        ref = ti.Matrix([[1., 2., 3., 4.], [0., 0., 0., 0.], [0., 0., 0., 0.]])
+        assert all(m == ref)
+
+        m[1, 1:3] = ti.Vector([1., 2.]).transpose()
+        ref = ti.Matrix([[1., 2., 3., 4.], [0., 1., 2., 0.], [0., 0., 0., 0.]])
+        assert all(m == ref)
+
+        m1 = ti.Matrix([[1., 1., 1., 1.] for _ in range(2)])
+        m[:2, :] = m1
+        ref = ti.Matrix([[1., 1., 1., 1.], [1., 1., 1., 1.], [0., 0., 0., 0.]])
+        assert all(m == ref)
+
+    foo()
+
+
+@test_utils.test(dynamic_index=True)
+def test_slice_assign_dynamic_index():
+    @ti.kernel
+    def foo(i: ti.i32, ref: ti.template()):
+        m = ti.Matrix([[0., 0., 0., 0.] for _ in range(3)])
+        vec = ti.Vector([1., 2., 3., 4.])
+        m[i, :] = vec.transpose()
+        assert all(m == ref)
+
+    for i in range(3):
+        foo(
+            i,
+            ti.Matrix([[1., 2., 3., 4.] if j == i else [0., 0., 0., 0.]
+                       for j in range(3)]))

b534a1d017b23c74d447fdb951cab91935e14917
jim19930609 <jim19930609@gmail.com>
2022-08-12 16:24:25 +0800

    Fixed issue with build structure

--- c_api/src/gui_utils/gui_utils.h
+++ c_api/src/gui_utils/gui_utils.h
@@ -3,28 +3,30 @@
 // handle.gui
 typedef struct TiGui_t *TiGui;
 
-TiGui ti_create_gui(TiArch arch,
-                    const char *shader_path,
-                    int window_h,
-                    int window_w,
-                    bool is_packed_mode);
+TI_DLL_EXPORT TiGui TI_API_CALL ti_create_gui(TiArch arch,
+                                              const char *shader_path,
+                                              int window_h,
+                                              int window_w,
+                                              bool is_packed_mode);
 
-void ti_destroy_gui(TiGui gui);
+TI_DLL_EXPORT void TI_API_CALL ti_destroy_gui(TiGui gui);
 
-int ti_set_image_info(TiGui gui,
-                      TiMemory memory,
-                      TiDataType dtype,
-                      TiArch source_arch,
-                      int *shape,
-                      int num_axes);
+TI_DLL_EXPORT int TI_API_CALL ti_set_image_info(TiGui gui,
+                                                TiMemory memory,
+                                                TiDataType dtype,
+                                                TiArch source_arch,
+                                                int *shape,
+                                                int num_axes);
 
-int ti_set_circle_info(TiGui gui,
-                       TiMemory memory,
-                       TiDataType dtype,
-                       TiArch source_arch,
-                       int *shape,
-                       int num_axes);
+TI_DLL_EXPORT int TI_API_CALL ti_set_circle_info(TiGui gui,
+                                                 TiMemory memory,
+                                                 TiDataType dtype,
+                                                 TiArch source_arch,
+                                                 int *shape,
+                                                 int num_axes);
 
-int ti_render_circle(TiGui gui, int circle_renderable_handle);
+TI_DLL_EXPORT int TI_API_CALL ti_render_circle(TiGui gui,
+                                               int circle_renderable_handle);
 
-int ti_render_image(TiGui gui, int image_renderable_handle);
+TI_DLL_EXPORT int TI_API_CALL ti_render_image(TiGui gui,
+                                              int image_renderable_handle);

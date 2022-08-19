d77e1b5032a4fa9e1aa770a1847dfe3f565e4e61
Morcki <cocolytrees@gmail.com>
2022-08-19 12:55:58 +0800

    Update test_api

--- tests/python/test_api.py
+++ tests/python/test_api.py
@@ -102,15 +102,7 @@ user_api[ti.FieldsBuilder] = [
     'lazy_grad', 'place', 'pointer', 'quant_array'
 ]
 user_api[ti.math] = [
-    'acos', 'asin', 'atan2', 'cconj', 'cdiv', 'ceil', 'cexp', 'cinv', 'clamp',
-    'clog', 'cmul', 'cos', 'cpow', 'cross', 'csqrt', 'degrees', 'determinant',
-    'distance', 'dot', 'e', 'exp', 'eye', 'floor', 'fract', 'inf', 'inverse',
-    'isinf', 'isnan', 'ivec2', 'ivec3', 'ivec4', 'length', 'log', 'log2',
-    'mat2', 'mat3', 'mat4', 'max', 'min', 'mix', 'mod', 'translate', 'scale',
-    'nan', 'normalize', 'pi', 'pow', 'radians', 'reflect', 'refract',
-    "rot3d_by_axis", "rot3d_yaw_pitch_roll", 'rotation2d', "rotation3d",
-    'round', 'sign', 'sin', 'smoothstep', 'sqrt', 'step', 'tan', 'tanh',
-    'uvec2', 'uvec3', 'uvec4', 'vdir', 'vec2', 'vec3', 'vec4'
+    'acos', 'asin', 'atan2', 'cconj', 'cdiv', 'ceil', 'cexp', 'cinv', 'clamp', 'clog', 'cmul', 'cos', 'cpow', 'cross', 'csqrt', 'degrees', 'determinant', 'distance', 'dot', 'e', 'exp', 'eye', 'floor', 'fract', 'inf', 'inverse', 'isinf', 'isnan', 'ivec2', 'ivec3', 'ivec4', 'length', 'log', 'log2', 'mat2', 'mat3', 'mat4', 'max', 'min', 'mix', 'mod', 'translate', 'scale', 'nan', 'normalize', 'pi', 'pow', 'radians', 'reflect', 'refract', 'rot3d_by_axis', 'rot3d_yaw_pitch_roll', 'rotation2d', 'rotation3d', 'round', 'sign', 'sin', 'smoothstep', 'sqrt', 'step', 'tan', 'tanh', 'uvec2', 'uvec3', 'uvec4', 'vdir', 'vec2', 'vec3', 'vec4'
 ]
 user_api[ti.Matrix] = _get_expected_matrix_apis()
 user_api[ti.MatrixField] = [

e00680c3b9e2901644ef908f0406d1f17663095d
Ailing <ailzhang@users.noreply.github.com>
2022-08-25 14:21:22 +0800

    Apply suggestions from code review

--- cpp_examples/run_snode.cpp
+++ cpp_examples/run_snode.cpp
@@ -123,7 +123,6 @@ void run_snode() {
 
     kernel_ext = std::make_unique<Kernel>(program, builder.extract_ir(), "ext");
     kernel_ext->insert_arr_arg(get_data_type<int>(), /*total_dim=*/1, {n});
-    ;
   }
 
   auto ctx_init = kernel_init->make_launch_context();

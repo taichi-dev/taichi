256c7e820a070fdb14032b6bca00cda5acb1a7c5
Zhanlue Yang <jim19930609@gmail.com>
2022-08-29 16:49:07 +0800

    [Lang] MatrixNdarray refactor part1: Refactor Taichi kernel argument to use TensorType (#5881)
    * [Lang] MatrixNdarray refactor part0: Support direct TensorType construction in Ndarray and refactored the use of element_shape
    
    * Fixed minor issue
    
    * Fixed CI failures
    
    * Minor refactor
    
    * Fixed minor issue
    
    * [Lang] MatrixNdarray refactor part1: Refactored Taichi kernel argument to use TensorType
    
    * Fixed CI failure with Metal backend
    
    * Addressed review comments
    
    * Fixed format issue with clang-tidy
    
    * Review comments
    
    * Fixed CI failure
    
    * Renamed interface
    
    * Added comments

--- taichi/program/callable.h
+++ taichi/program/callable.h
@@ -16,22 +16,58 @@ class TI_DLL_EXPORT Callable {
   std::unique_ptr<FrontendContext> context{nullptr};
 
   struct Arg {
-    DataType dt;
     bool is_array{
         false};  // This is true for both ndarray and external array args.
-    std::size_t total_dim{0};             // total dim of array
-    std::vector<int> element_shape = {};  // shape of each element
+    std::size_t total_dim{0};  // total dim of array
 
+    /* [arguments with TensorType]
+
+    Taichi used to represent TensorType with the combination of "PrimitiveType"
+    & "element_shape" and there are a bunch of interfaces designed like this (it
+    allows creating TensorType by passing in PrimitiveType + element_shape)
+
+    Here we removed the "element_shape" member in the underlying objects (class
+    Arg, class ExternalTensorExpression, ...), and forced them to use TensorType
+    in their "dtype" member.
+
+    However we kept the interfaces unchanged temporarily, so as to minimize
+    possible regressions.
+    */
     explicit Arg(const DataType &dt = PrimitiveType::unknown,
                  bool is_array = false,
                  std::size_t size_unused = 0,
                  int total_dim = 0,
-                 std::vector<int> element_shape = {})
-        : dt(dt),
-          is_array(is_array),
-          total_dim(total_dim),
-          element_shape(std::move(element_shape)) {
+                 std::vector<int> element_shape = {}) {
+      if (dt->is<PrimitiveType>() && element_shape.size() > 0) {
+        this->dt_ = taichi::lang::TypeFactory::get_instance().get_tensor_type(
+            element_shape, dt.operator->());
+
+      } else {
+        this->dt_ = dt;
+      }
+
+      this->is_array = is_array;
+      this->total_dim = total_dim;
     }
+
+    std::vector<int> get_element_shape() const {
+      return dt_.get_shape();
+    }
+
+    DataType get_element_type() const {
+      return dt_.get_element_type();
+    }
+
+    int get_element_size() const {
+      return data_type_size(dt_);
+    }
+
+    DataType get_dtype() const {
+      return dt_;
+    }
+
+   private:
+    DataType dt_;
   };
 
   struct Ret {

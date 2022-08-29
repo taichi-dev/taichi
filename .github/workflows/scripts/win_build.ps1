dd394a2fcb154f18d7b221fb0d6b4d8bc84bdbc9
Xiang Li <python3kgae@outlook.com>
2022-08-29 02:18:30 -0700

    [ci] Switch windows cpu build to llvm 15. (#5832)

--- .github/workflows/scripts/win_build.ps1
+++ .github/workflows/scripts/win_build.ps1
@@ -5,7 +5,8 @@ param (
     [switch]$installVulkan = $false,
     [switch]$develop = $false,
     [switch]$install = $false,
-    [string]$libsDir = "."
+    [string]$libsDir = ".",
+    [string]$llvmVer = "10"
 )
 
 $ErrorActionPreference = "Stop"
@@ -44,19 +45,42 @@ if (-not (Test-Path $libsDir)) {
     New-Item -ItemType Directory -Path $libsDir
 }
 Push-Location $libsDir
-if (-not (Test-Path "taichi_llvm")) {
-    WriteInfo("Download and extract LLVM")
-    curl.exe --retry 10 --retry-delay 5 https://github.com/taichi-dev/taichi_assets/releases/download/llvm10/taichi-llvm-10.0.0-msvc2019.zip -LO
-    if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE; }
-    7z x taichi-llvm-10.0.0-msvc2019.zip -otaichi_llvm
+
+if (!$llvmVer.CompareTo("10")) {
+    if (-not (Test-Path "taichi_llvm")) {
+        WriteInfo("Download and extract LLVM")
+        curl.exe --retry 10 --retry-delay 5 https://github.com/taichi-dev/taichi_assets/releases/download/llvm10/taichi-llvm-10.0.0-msvc2019.zip -LO
+        if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE; }
+        7z x taichi-llvm-10.0.0-msvc2019.zip -otaichi_llvm
+    }
+	if (-not (Test-Path "taichi_clang")) {
+		WriteInfo("Download and extract Clang")
+		curl.exe --retry 10 --retry-delay 5 https://github.com/taichi-dev/taichi_assets/releases/download/llvm10/clang-10.0.0-win.zip -LO
+		if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE; }
+		7z x clang-10.0.0-win.zip -otaichi_clang
+	}
+} else {
+    if (-not (Test-Path "taichi_llvm_15")) {
+        WriteInfo("Download and extract LLVM")
+        curl.exe --retry 10 --retry-delay 5 https://github.com/python3kgae/taichi_assets/releases/download/llvm15_vs2019_clang_220731/taichi-llvm-15.0.0-msvc2019.zip -LO
+        if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE; }
+        7z x taichi-llvm-15.0.0-msvc2019.zip -otaichi_llvm_15
+    }
+	if (-not (Test-Path "taichi_clang_15")) {
+		WriteInfo("Download and extract Clang")
+		curl.exe --retry 10 --retry-delay 5 https://github.com/python3kgae/taichi_assets/releases/download/llvm15_vs2022_clang/clang-15.0.0-win.zip -LO
+		if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE; }
+		7z x clang-15.0.0-win.zip -otaichi_clang_15
+	}
 }
-if (-not (Test-Path "taichi_clang")) {
-    WriteInfo("Download and extract Clang")
-    curl.exe --retry 10 --retry-delay 5 https://github.com/taichi-dev/taichi_assets/releases/download/llvm10/clang-10.0.0-win.zip -LO
-    if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE; }
-    7z x clang-10.0.0-win.zip -otaichi_clang
+
+
+if (!$llvmVer.CompareTo("10")) {
+    $env:LLVM_DIR = "$libsDir\taichi_llvm"
+} else {
+    $env:LLVM_DIR = "$libsDir\taichi_llvm_15"
 }
-$env:LLVM_DIR = "$libsDir\taichi_llvm"
+
 $env:TAICHI_CMAKE_ARGS += " -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_C_COMPILER=clang"
 if ($installVulkan) {
     WriteInfo("Download and install Vulkan")
@@ -84,8 +108,14 @@ python -m pip install wheel
 python -m pip install -r requirements_dev.txt
 if (-not $?) { exit 1 }
 WriteInfo("Building Taichi")
-$env:TAICHI_CMAKE_ARGS += " -DCLANG_EXECUTABLE=$libsDir\\taichi_clang\\bin\\clang++.exe"
-$env:TAICHI_CMAKE_ARGS += " -DLLVM_AS_EXECUTABLE=$libsDir\\taichi_llvm\\bin\\llvm-as.exe"
+if (!$llvmVer.CompareTo("10")) {
+	$env:TAICHI_CMAKE_ARGS += " -DCLANG_EXECUTABLE=$libsDir\\taichi_clang\\bin\\clang++.exe"
+	$env:TAICHI_CMAKE_ARGS += " -DLLVM_AS_EXECUTABLE=$libsDir\\taichi_llvm\\bin\\llvm-as.exe"
+} else {
+	$env:TAICHI_CMAKE_ARGS += " -DCLANG_EXECUTABLE=$libsDir\\taichi_clang_15\\bin\\clang++.exe"
+	$env:TAICHI_CMAKE_ARGS += " -DLLVM_AS_EXECUTABLE=$libsDir\\taichi_llvm_15\\bin\\llvm-as.exe"
+    $env:TAICHI_CMAKE_ARGS += " -DTI_LLVM_15:BOOL=ON"
+}
 if ($install) {
     if ($develop) {
         python setup.py develop

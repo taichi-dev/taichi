56ec5247bc617e9863257d6a3da65327cb044970
Haidong Lan <haidonglan@taichi.graphics>
2022-08-29 11:47:29 +0800

    Guard reset only for CUDA targets

--- python/taichi/lang/impl.py
+++ python/taichi/lang/impl.py
@@ -404,7 +404,8 @@ def get_runtime():
 
 def reset():
     global pytaichi
-    _ti_core.push_cuda_context()
+    if get_runtime().prog.config.arch == _ti_core.cuda:
+        _ti_core.push_cuda_context()
     old_kernels = pytaichi.kernels
     pytaichi.clear()
     pytaichi = PyTaichi(old_kernels)

08190c77b50f4777a041f0c8202429071f4fbc88
Lin Jiang <linjiang@taichi.graphics>
2022-08-31 17:41:11 +0800

    [llvm] [refactor] Move the common parts of compilation to the base class (#5926)
    * [llvm] [refactor] Move the common parts of compilation to the base class
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * fix weird formatting
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- taichi/codegen/codegen.h
+++ taichi/codegen/codegen.h
@@ -33,11 +33,14 @@ class KernelCodeGen {
   }
 
 #ifdef TI_WITH_LLVM
+  std::vector<LLVMCompiledData> compile_kernel_to_module();
+
   virtual LLVMCompiledData compile_task(
       std::unique_ptr<llvm::Module> &&module = nullptr,
       OffloadedStmt *stmt = nullptr) {
     TI_NOT_IMPLEMENTED
   }
+
   bool maybe_read_compilation_from_cache(const std::string &kernel_key,
                                          std::vector<LLVMCompiledData> &data);
   void cache_module(const std::string &kernel_key,

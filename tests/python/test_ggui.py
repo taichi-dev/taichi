1ee3108c36716c85bfb718217fc4e1385fe12fc6
Mocki <34432001+Morcki@users.noreply.github.com>
2022-09-04 10:49:27 +0800

    [test] [gui] Add a test of fetching depth attachment (#5947)
    * Add a test of fetching depth attachment
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * generate ground-truth depth image
    
    * pass mac test and fix this bug latter
    
    * Add a test of drawing mesh instances
    
    * Revert "Add a test of drawing mesh instances"
    
    This reverts commit b443eb0a918498c6707c7c18bbd935c3eeeef1ed.
    
    * fix a bug that mac not support f64
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- tests/python/test_ggui.py
+++ tests/python/test_ggui.py
@@ -391,6 +391,35 @@ def test_fetching_color_attachment():
 
 @pytest.mark.skipif(not _ti_core.GGUI_AVAILABLE, reason="GGUI Not Available")
 @test_utils.test(arch=supported_archs)
+def test_fetching_depth_attachment():
+    window = ti.ui.Window("test", (512, 512), vsync=True, show_window=False)
+    canvas = window.get_canvas()
+    scene = ti.ui.Scene()
+    camera = ti.ui.make_camera()
+
+    ball_center = ti.Vector.field(3, dtype=float, shape=(1, ))
+    ball_center[0] = ti.math.vec3(0, 0, 0.5)
+
+    def render():
+        camera.position(0.0, 0.0, 1)
+        camera.lookat(0.0, 0.0, 0)
+        scene.set_camera(camera)
+        scene.point_light(pos=(0, 1, 2), color=(1, 1, 1))
+        scene.ambient_light((0.5, 0.5, 0.5))
+        scene.particles(ball_center, radius=0.05, color=(0.5, 0.42, 0.8))
+        canvas.scene(scene)
+
+    for _ in range(RENDER_REPEAT):
+        render()
+        window.get_image_buffer_as_numpy()
+
+    render()
+    verify_image(window.get_depth_buffer_as_numpy(), 'test_depth')
+    window.destroy()
+
+
+@pytest.mark.skipif(not _ti_core.GGUI_AVAILABLE, reason="GGUI Not Available")
+@test_utils.test(arch=supported_archs)
 def test_draw_lines():
     N = 10
     particles_pos = ti.Vector.field(3, dtype=ti.f32, shape=N)

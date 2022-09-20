b0be772cd92a47a9069d34f58e72ee2b7373303f
Xiang Li <python3kgae@outlook.com>
2022-09-20 00:04:11 -0700

    Update comments.

--- taichi/runtime/dx12/aot_module_builder_impl.cpp
+++ taichi/runtime/dx12/aot_module_builder_impl.cpp
@@ -41,13 +41,9 @@ void AotModuleBuilderImpl::add_field_per_backend(const std::string &identifier,
                                                  std::vector<int> shape,
                                                  int row_num,
                                                  int column_num) {
-  // Note that currently we only support adding dense fields in AOT for all
-  // backends. In opengl backend we only error out when a non dense field is
-  // added to the aot module, but in metal backend we error out earlier when
-  // constructing aot module. Ideally we will unify this behavior but it doesn't
-  // matter too much for now.
+  // FIXME: support sparse fields.
   TI_ERROR_IF(!all_fields_are_dense_in_container(rep_snode->parent),
-              "AOT: only supports dense field");
+              "AOT: D12 supports only dense fields for now");
 
   const auto &field = prog->get_cached_field(rep_snode->get_snode_tree_id());
 

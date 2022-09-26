63dd4b6ac03d371fe3163f52f457cf873b682659
PGZXB <pgzxb@qq.com>
2022-09-26 11:01:04 +0800

    [misc] Move some offline cache utils from analysis/ to util/ (#6155)
    Issue: #4401

--- taichi/analysis/offline_cache_util.h
+++ taichi/analysis/offline_cache_util.h
@@ -16,15 +16,4 @@ std::string get_hashed_offline_cache_key_of_snode(SNode *snode);
 std::string get_hashed_offline_cache_key(CompileConfig *config, Kernel *kernel);
 void gen_offline_cache_key(Program *prog, IRNode *ast, std::ostream *os);
 
-namespace offline_cache {
-
-std::string get_cache_path_by_arch(const std::string &base_path, Arch arch);
-bool enabled_wip_offline_cache(bool enable_hint);
-std::string mangle_name(const std::string &primal_name, const std::string &key);
-bool try_demangle_name(const std::string &mangled_name,
-                       std::string &primal_name,
-                       std::string &key);
-
-}  // namespace offline_cache
-
 }  // namespace taichi::lang

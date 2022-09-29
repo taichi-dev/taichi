8be973b15900a1f2d8bb15506170b580fde38eaa
Zhanlue Yang <jim19930609@gmail.com>
2022-09-29 14:56:31 +0800

    [Lang] MatrixNdarray refactor part9: Add scalarization for AllocaStmt (#6168)
    Related issue = https://github.com/taichi-dev/taichi/issues/5873,
    https://github.com/taichi-dev/taichi/issues/5819
    
    This PR is working "Part ④" in
    https://github.com/taichi-dev/taichi/issues/5873.
    
    [AllocaStmt scalarization]
    ```
    Before:
      TensorType<4 x i32>* addr = AllocaStmt(TensorType<4 x i32>)
    
    After:
      i32 addr0 = AllocaStmt(i32)
      i32 addr1 = AllocaStmt(i32)
      i32 addr2 = AllocaStmt(i32)
      i32 addr3 = AllocaStmt(i32)
    
      scalarized_local_tensor_map_[addr] = {addr0, addr1, addr2, addr3}
    ```
    
    [Load AllocaStmt]
    ```
    Before:
      TensorType<4 x i32> val = LoadStmt(TensorType<4 x i32>* alloca_src)
    
    After:
      i32 val0 = LoadStmt(scalarized_local_tensor_map_[stmt][0])
      i32 val1 = LoadStmt(scalarized_local_tensor_map_[stmt][1])
      i32 val2 = LoadStmt(scalarized_local_tensor_map_[stmt][2])
      i32 val3 = LoadStmt(scalarized_local_tensor_map_[stmt][3])
    
      tmp = MatrixInitStmt(val0, val1, val2, val3)
      stmt->replace_all_usages_with(tmp)
    ```
    
    [Store to AllocaStmt]
    ```
    Before:
      StoreStmt(TensorType<4 x i32>* alloca_dest_stmt, TensorType<4 x i32> val)
    
    After:
      StoreStmt(i32* scalarized_local_tensor_map_[stmt][0], 
                i32 val->cast<MatrixInitStmt>()->val[0]) 
      StoreStmt(i32* scalarized_local_tensor_map_[stmt][1], 
                i32 val->cast<MatrixInitStmt>()->val[1]) 
      StoreStmt(i32* scalarized_local_tensor_map_[stmt][2], 
                i32 val->cast<MatrixInitStmt>()->val[2]) 
      StoreStmt(i32* scalarized_local_tensor_map_[stmt][3], 
                i32 val->cast<MatrixInitStmt>()->val[3])
    ```
    
    Co-authored-by: Yi Xu <xy_xuyi@foxmail.com>

--- tests/cpp/transforms/scalarize_test.cpp
+++ tests/cpp/transforms/scalarize_test.cpp
@@ -6,9 +6,8 @@
 
 namespace taichi::lang {
 
-// Basic tests within a basic block
-template <typename T>
-void test_store_scalarize() {
+TEST(Scalarize, ScalarizeGlobalStore) {
+  // Basic tests within a basic block
   TestProgram test_prog;
   test_prog.setup();
 
@@ -32,15 +31,10 @@ void test_store_scalarize() {
   auto const_2_stmt = block->push_back<ConstStmt>(TypedConstant(2));
   auto argload_stmt = block->push_back<ArgLoadStmt>(0 /*arg_id*/, tensor_type);
 
-  Stmt *dest_stmt = nullptr;
-  if (std::is_same<T, GlobalStoreStmt>::value) {
-    std::vector<Stmt *> indices = {};
-    dest_stmt = block->push_back<ExternalPtrStmt>(
-        argload_stmt, indices);  // fake ExternalPtrStmt
+  std::vector<Stmt *> indices = {};
+  Stmt *dest_stmt = block->push_back<ExternalPtrStmt>(
+      argload_stmt, indices);  // fake ExternalPtrStmt
 
-  } else {
-    dest_stmt = block->push_back<AllocaStmt>(tensor_type);
-  }
   dest_stmt->ret_type = type_factory.get_pointer_type(tensor_type);
 
   std::vector<Stmt *> matrix_init_vals = {const_1_stmt, const_1_stmt,
@@ -49,9 +43,10 @@ void test_store_scalarize() {
       block->push_back<MatrixInitStmt>(std::move(matrix_init_vals));
   matrix_init_stmt->ret_type = tensor_type;
 
-  block->push_back<T>(dest_stmt, matrix_init_stmt);
+  block->push_back<GlobalStoreStmt>(dest_stmt, matrix_init_stmt);
 
   irpass::scalarize(block.get());
+  irpass::die(block.get());
 
   EXPECT_EQ(block->size(), 2 /*const*/ + 1 /*argload*/ + 1 /*external_ptr*/ +
                                1 /*matrix_init*/ + 4 /*const*/ +
@@ -60,23 +55,22 @@ void test_store_scalarize() {
   // Check for scalarized statements
   EXPECT_EQ(block->statements[5]->is<ConstStmt>(), true);
   EXPECT_EQ(block->statements[6]->is<MatrixPtrStmt>(), true);
-  EXPECT_EQ(block->statements[7]->is<T>(), true);
+  EXPECT_EQ(block->statements[7]->is<GlobalStoreStmt>(), true);
 
   EXPECT_EQ(block->statements[8]->is<ConstStmt>(), true);
   EXPECT_EQ(block->statements[9]->is<MatrixPtrStmt>(), true);
-  EXPECT_EQ(block->statements[10]->is<T>(), true);
+  EXPECT_EQ(block->statements[10]->is<GlobalStoreStmt>(), true);
 
   EXPECT_EQ(block->statements[11]->is<ConstStmt>(), true);
   EXPECT_EQ(block->statements[12]->is<MatrixPtrStmt>(), true);
-  EXPECT_EQ(block->statements[13]->is<T>(), true);
+  EXPECT_EQ(block->statements[13]->is<GlobalStoreStmt>(), true);
 
   EXPECT_EQ(block->statements[14]->is<ConstStmt>(), true);
   EXPECT_EQ(block->statements[15]->is<MatrixPtrStmt>(), true);
-  EXPECT_EQ(block->statements[16]->is<T>(), true);
+  EXPECT_EQ(block->statements[16]->is<GlobalStoreStmt>(), true);
 }
 
-template <typename T>
-void test_load_scalarize() {
+TEST(Scalarize, ScalarizeGlobalLoad) {
   TestProgram test_prog;
   test_prog.setup();
 
@@ -102,9 +96,10 @@ void test_load_scalarize() {
       argload_stmt, indices);  // fake ExternalPtrStmt
   src_stmt->ret_type = type_factory.get_pointer_type(tensor_type);
 
-  block->push_back<T>(src_stmt);
+  block->push_back<GlobalLoadStmt>(src_stmt);
 
   irpass::scalarize(block.get());
+  irpass::die(block.get());
 
   EXPECT_EQ(block->size(), 1 /*argload*/ + 1 /*external_ptr*/ + 4 /*const*/ +
                                4 /*matrix_ptr*/ + 4 /*load*/ +
@@ -113,31 +108,121 @@ void test_load_scalarize() {
   // Check for scalarized statements
   EXPECT_EQ(block->statements[2]->is<ConstStmt>(), true);
   EXPECT_EQ(block->statements[3]->is<MatrixPtrStmt>(), true);
-  EXPECT_EQ(block->statements[4]->is<T>(), true);
+  EXPECT_EQ(block->statements[4]->is<GlobalLoadStmt>(), true);
 
   EXPECT_EQ(block->statements[5]->is<ConstStmt>(), true);
   EXPECT_EQ(block->statements[6]->is<MatrixPtrStmt>(), true);
-  EXPECT_EQ(block->statements[7]->is<T>(), true);
+  EXPECT_EQ(block->statements[7]->is<GlobalLoadStmt>(), true);
 
   EXPECT_EQ(block->statements[8]->is<ConstStmt>(), true);
   EXPECT_EQ(block->statements[9]->is<MatrixPtrStmt>(), true);
-  EXPECT_EQ(block->statements[10]->is<T>(), true);
+  EXPECT_EQ(block->statements[10]->is<GlobalLoadStmt>(), true);
 
   EXPECT_EQ(block->statements[11]->is<ConstStmt>(), true);
   EXPECT_EQ(block->statements[12]->is<MatrixPtrStmt>(), true);
-  EXPECT_EQ(block->statements[13]->is<T>(), true);
+  EXPECT_EQ(block->statements[13]->is<GlobalLoadStmt>(), true);
 
   EXPECT_EQ(block->statements[14]->is<MatrixInitStmt>(), true);
 }
 
-TEST(Scalarize, ScalarizeStore) {
-  test_store_scalarize<GlobalStoreStmt>();
-  test_store_scalarize<LocalStoreStmt>();
+TEST(Scalarize, ScalarizeLocalStore) {
+  // Basic tests within a basic block
+  TestProgram test_prog;
+  test_prog.setup();
+
+  auto block = std::make_unique<Block>();
+
+  auto func = []() {};
+  auto kernel =
+      std::make_unique<Kernel>(*test_prog.prog(), func, "fake_kernel");
+  block->kernel = kernel.get();
+
+  auto &type_factory = TypeFactory::get_instance();
+
+  /*
+    TensorType<4 x i32>* %1 = AllocaStmt()
+    TensorType<4 x i32>  %2 = MatrixInitStmt([1, 1, 2, 2])
+    StoreStmt(%1, %2)
+  */
+  Type *tensor_type = type_factory.get_tensor_type(
+      {2, 2}, type_factory.get_primitive_type(PrimitiveTypeID::i32));
+  Stmt *dest_stmt = block->push_back<AllocaStmt>(tensor_type);
+  dest_stmt->ret_type = type_factory.get_pointer_type(tensor_type);
+
+  auto const_1_stmt = block->push_back<ConstStmt>(TypedConstant(1));
+  auto const_2_stmt = block->push_back<ConstStmt>(TypedConstant(2));
+  std::vector<Stmt *> matrix_init_vals = {const_1_stmt, const_1_stmt,
+                                          const_2_stmt, const_2_stmt};
+  auto matrix_init_stmt =
+      block->push_back<MatrixInitStmt>(std::move(matrix_init_vals));
+  matrix_init_stmt->ret_type = tensor_type;
+
+  block->push_back<LocalStoreStmt>(dest_stmt, matrix_init_stmt);
+
+  irpass::scalarize(block.get());
+  irpass::die(block.get());
+
+  EXPECT_EQ(block->size(),
+            2 /*const*/ + 1 /*matrix_init*/ + 4 /*alloca*/ + 4 /*store*/);
+
+  // Check for scalarized statements
+  EXPECT_EQ(block->statements[0]->is<AllocaStmt>(), true);
+  EXPECT_EQ(block->statements[1]->is<AllocaStmt>(), true);
+  EXPECT_EQ(block->statements[2]->is<AllocaStmt>(), true);
+  EXPECT_EQ(block->statements[3]->is<AllocaStmt>(), true);
+
+  EXPECT_EQ(block->statements[4]->is<ConstStmt>(), true);
+  EXPECT_EQ(block->statements[5]->is<ConstStmt>(), true);
+  EXPECT_EQ(block->statements[6]->is<MatrixInitStmt>(), true);
+
+  EXPECT_EQ(block->statements[7]->is<LocalStoreStmt>(), true);
+  EXPECT_EQ(block->statements[8]->is<LocalStoreStmt>(), true);
+  EXPECT_EQ(block->statements[9]->is<LocalStoreStmt>(), true);
+  EXPECT_EQ(block->statements[10]->is<LocalStoreStmt>(), true);
 }
 
-TEST(Scalarize, ScalarizeLoad) {
-  test_load_scalarize<GlobalLoadStmt>();
-  test_load_scalarize<LocalLoadStmt>();
+TEST(Scalarize, ScalarizeLocalLoad) {
+  // Basic tests within a basic block
+  TestProgram test_prog;
+  test_prog.setup();
+
+  auto block = std::make_unique<Block>();
+
+  auto func = []() {};
+  auto kernel =
+      std::make_unique<Kernel>(*test_prog.prog(), func, "fake_kernel");
+  block->kernel = kernel.get();
+
+  auto &type_factory = TypeFactory::get_instance();
+
+  /*
+    TensorType<4 x i32>* %1 = AllocaStmt()
+    LoadStmt(%1)
+  */
+  Type *tensor_type = type_factory.get_tensor_type(
+      {2, 2}, type_factory.get_primitive_type(PrimitiveTypeID::i32));
+  Stmt *src_stmt = block->push_back<AllocaStmt>(tensor_type);
+  src_stmt->ret_type = type_factory.get_pointer_type(tensor_type);
+
+  block->push_back<LocalLoadStmt>(src_stmt);
+
+  irpass::scalarize(block.get());
+  irpass::die(block.get());
+
+  EXPECT_EQ(block->size(), 4 /*alloca*/ + 4 /*load*/ + 1 /*matrix_init*/);
+
+  // Check for scalarized statements
+  EXPECT_EQ(block->statements[0]->is<AllocaStmt>(), true);
+  EXPECT_EQ(block->statements[1]->is<AllocaStmt>(), true);
+  EXPECT_EQ(block->statements[2]->is<AllocaStmt>(), true);
+  EXPECT_EQ(block->statements[3]->is<AllocaStmt>(), true);
+
+  EXPECT_EQ(block->statements[4]->is<LocalLoadStmt>(), true);
+  EXPECT_EQ(block->statements[5]->is<LocalLoadStmt>(), true);
+  EXPECT_EQ(block->statements[6]->is<LocalLoadStmt>(), true);
+  EXPECT_EQ(block->statements[7]->is<LocalLoadStmt>(), true);
+
+  EXPECT_EQ(block->statements[8]->is<MatrixInitStmt>(), true);
 }
 
 }  // namespace taichi::lang

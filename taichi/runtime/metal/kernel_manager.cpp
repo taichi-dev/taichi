29a421a614dd8f6efb357b94d25bfafcf4d75f30
PGZXB <pgzxb@qq.com>
2022-10-11 21:23:21 +0800

    [bug] Serialize missing fields of metal::TaichiKernelAttributes and metal::KernelAttributes (#6270)
    Issue: #6263 
    Fix bug reported by `TI_WIP_OFFLINE_CACHE=1 python3 run_tests.py -a
    metal -vr2 --with-offline-cache --rerun-with-offline-cache 1 debug
    ast_refactor assert sparse_deactivate sparse_parallel`
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- taichi/runtime/metal/kernel_manager.cpp
+++ taichi/runtime/metal/kernel_manager.cpp
@@ -232,11 +232,7 @@ class SparseRuntimeMtlKernelBase : public CompiledMtlKernelBase {
 
 class ListgenOpMtlKernel : public SparseRuntimeMtlKernelBase {
  public:
-  struct Params : public SparseRuntimeMtlKernelBase::Params {
-    const SNode *snode() const {
-      return kernel_attribs->runtime_list_op_attribs->snode;
-    }
-  };
+  struct Params : public SparseRuntimeMtlKernelBase::Params {};
 
   explicit ListgenOpMtlKernel(Params &params)
       : SparseRuntimeMtlKernelBase(params, /*args_size=*/sizeof(int32_t) * 2) {
@@ -245,8 +241,10 @@ class ListgenOpMtlKernel : public SparseRuntimeMtlKernelBase {
     // args[1] = child_snode_id
     // Note that this args buffer has nothing to do with the one passed to
     // Taichi kernel. See taichi/rhi/metal/shaders/runtime_kernels.metal.h
-    const int parent_snode_id = params.snode()->parent->id;
-    const int child_snode_id = params.snode()->id;
+    const int parent_snode_id =
+        params.kernel_attribs->runtime_list_op_attribs->parent_snode_id;
+    const int child_snode_id =
+        params.kernel_attribs->runtime_list_op_attribs->snode_id;
     auto *mem = reinterpret_cast<int32_t *>(args_mem_->ptr());
     mem[0] = parent_snode_id;
     mem[1] = child_snode_id;
@@ -262,15 +260,11 @@ class ListgenOpMtlKernel : public SparseRuntimeMtlKernelBase {
 
 class GcOpMtlKernel : public SparseRuntimeMtlKernelBase {
  public:
-  struct Params : public SparseRuntimeMtlKernelBase::Params {
-    const SNode *snode() const {
-      return kernel_attribs->gc_op_attribs->snode;
-    }
-  };
+  struct Params : public SparseRuntimeMtlKernelBase::Params {};
 
   explicit GcOpMtlKernel(Params &params)
       : SparseRuntimeMtlKernelBase(params, /*args_size=*/sizeof(int32_t)) {
-    const int snode_id = params.snode()->id;
+    const int snode_id = params.kernel_attribs->gc_op_attribs->snode_id;
     auto *mem = reinterpret_cast<int32_t *>(args_mem_->ptr());
     mem[0] = snode_id;
     TI_DEBUG("Registered GcOpMtlKernel: name={} num_threads={} snode_id={}",

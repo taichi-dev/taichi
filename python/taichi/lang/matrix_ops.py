b27980d5ae29181fadfcdf90aab1f927a1a54831
AD1024 <dh63@cs.washington.edu>
2022-10-12 17:58:19 -0400

    save

--- python/taichi/lang/matrix_ops.py
+++ python/taichi/lang/matrix_ops.py
@@ -1,6 +1,11 @@
+import numbers
+
+from taichi.lang.expr import Expr
 from taichi.lang.impl import static, subscript
 from taichi.lang.matrix import Matrix, Vector
-from taichi.lang.matrix_ops_utils import check_det, check_matmul, preconditions
+from taichi.lang.matrix_ops_utils import (check_matmul, dim_lt, is_int_const,
+                                          is_tensor, preconditions,
+                                          square_matrix)
 
 import taichi as ti
 
@@ -62,10 +67,10 @@ def matmul(x, y):
     return _matmul_helper(x, y)
 
 
+@preconditions(square_matrix)
 @ti.func
 def trace(x):
     shape = static(x.get_shape())
-    # assert shape[0] == shape[1]
     result = 0
     for i in range(shape[0]):
         result += x[i, i]
@@ -76,7 +81,9 @@ def E(m, x, y, n):
     return subscript(m, x % n, y % n)
 
 
-@preconditions(check_det)
+@preconditions(square_matrix,
+               dim_lt(0, 5,
+                      'Determinant of dimension >= 5 is not supported: {}'))
 @ti.func
 def determinant(x):
     shape = static(x.get_shape())
@@ -107,49 +114,128 @@ def determinant(x):
     return None
 
 
-# @ti.func
-# def inverse(x):
-#     shape = static(x.get_shape())
-#     if shape[0] == 1:
-#         return Matrix([1 / x[0, 0]])
-#     if shape[1] == 2:
-#         inv_determinant = impl.expr_init(1.0 / self.determinant())
-#         return inv_determinant * Matrix([[self(
-#             1, 1), -self(0, 1)], [-self(1, 0), self(0, 0)]])
-#     if self.n == 3:
-#         n = 3
-#         inv_determinant = impl.expr_init(1.0 / self.determinant())
-#         entries = [[0] * n for _ in range(n)]
+@preconditions(square_matrix,
+               dim_lt(0, 5, 'Inverse of dimension >= 5 is not supported: {}'))
+@ti.func
+def inverse(x):
+    n = static(x.get_shape()[0])
+    if static(n == 1):
+        return Matrix([1 / x[0, 0]])
+    if static(n == 2):
+        inv_determinant = 1.0 / determinant(x)
+        return inv_determinant * Matrix([[x[1, 1], -x[0, 1]],
+                                         [-x[1, 0], x[0, 0]]])
+    if static(n == 3):
+        n = 3
+        inv_determinant = 1.0 / determinant(x)
+        result = Matrix([[0] * n for _ in range(n)])
 
-#         def E(x, y):
-#             return self(x % n, y % n)
+        # TODO: fix parallelization
+        ti.loop_config(serialize=True)
+        for i in range(n):
+            for j in range(n):
+                result[j, i] = inv_determinant * (
+                    E(x, i + 1, j + 1, n) * E(x, i + 2, j + 2, n) -
+                    E(x, i + 2, j + 1, n) * E(x, i + 1, j + 2, n))
+        return result
+    if static(n == 4):
+        n = 4
+        inv_determinant = 1.0 / determinant(x)
+        result = Matrix([[0] * n for _ in range(n)])
 
-#         for i in range(n):
-#             for j in range(n):
-#                 entries[j][i] = inv_determinant * (
-#                     E(i + 1, j + 1) * E(i + 2, j + 2) -
-#                     E(i + 2, j + 1) * E(i + 1, j + 2))
-#         return Matrix(entries)
-#     if self.n == 4:
-#         n = 4
-#         inv_determinant = impl.expr_init(1.0 / self.determinant())
-#         entries = [[0] * n for _ in range(n)]
+        # TODO: fix parallelization
+        ti.loop_config(serialize=True)
+        for i in range(n):
+            for j in range(n):
+                result[j, i] = inv_determinant * (-1)**(i + j) * (
+                    (E(x, i + 1, j + 1, n) *
+                     (E(x, i + 2, j + 2, n) * E(x, i + 3, j + 3, n) -
+                      E(x, i + 3, j + 2, n) * E(x, i + 2, j + 3, n)) -
+                     E(x, i + 2, j + 1, n) *
+                     (E(x, i + 1, j + 2, n) * E(x, i + 3, j + 3, n) -
+                      E(x, i + 3, j + 2, n) * E(x, i + 1, j + 3, n)) +
+                     E(x, i + 3, j + 1, n) *
+                     (E(x, i + 1, j + 2, n) * E(x, i + 2, j + 3, n) -
+                      E(x, i + 2, j + 2, n) * E(x, i + 1, j + 3, n))))
+        return result
+    return None
 
-#         def E(x, y):
-#             return self(x % n, y % n)
 
-#         for i in range(n):
-#             for j in range(n):
-#                 entries[j][i] = inv_determinant * (-1)**(i + j) * ((
-#                     E(i + 1, j + 1) *
-#                     (E(i + 2, j + 2) * E(i + 3, j + 3) -
-#                         E(i + 3, j + 2) * E(i + 2, j + 3)) - E(i + 2, j + 1) *
-#                     (E(i + 1, j + 2) * E(i + 3, j + 3) -
-#                         E(i + 3, j + 2) * E(i + 1, j + 3)) + E(i + 3, j + 1) *
-#                     (E(i + 1, j + 2) * E(i + 2, j + 3) -
-#                         E(i + 2, j + 2) * E(i + 1, j + 3))))
-#         return Matrix(entries)
-#     raise Exception(
-#         "Inversions of matrices with sizes >= 5 are not supported")
+@preconditions(is_tensor)
+@ti.func
+def transpose(m):
+    shape = static(m.get_shape())
+    if static(len(shape) == 1):
+        return m
+    result = Matrix([[-1 for _ in range(shape[0])] for _ in range(shape[1])],
+                    dt=m.element_type())
 
-__all__ = ['transpose', 'matmul', 'determinant', 'trace']
+    # TODO: fix parallelization
+    ti.loop_config(serialize=True)
+    for i in range(shape[0]):
+        for j in range(shape[1]):
+            result[j, i] = m[i, j]
+    return result
+
+
+@preconditions(lambda dim, _: is_int_const(dim), lambda _, val:
+               (isinstance(val, (numbers.Number, )) or isinstance(val, Expr),
+                f'Invalid argument type: {type(val)}'))
+def diag(dim, val):
+    dt = val.element_type() if isinstance(val, Expr) else type(val)
+
+    @ti.func
+    def diag_impl():
+        result = Matrix([[0 for _ in range(dim)] for _ in range(dim)], dt=dt)
+        ti.loop_config(serialize=True)
+        for i in range(dim):
+            result[i, i] = val
+        return result
+
+    return diag_impl()
+
+
+@preconditions(is_tensor)
+@ti.func
+# pylint: disable=W0622
+def sum(m):
+    result = ti.cast(0, m.element_type())
+    s = static(m.get_shape())
+    if static(len(s) == 1):
+        for i in range(s[0]):
+            result += m[i]
+        return result
+    for i in range(s[0]):
+        for j in range(s[1]):
+            result += m[i, j]
+    return result
+
+
+@preconditions(is_tensor)
+@ti.func
+def norm_sqr(m):
+    return sum(m * m)
+
+
+@preconditions(lambda x, **_: is_tensor(x))
+def norm(m, eps=1e-6):
+    @ti.func
+    def norm_impl():
+        return ti.sqrt(norm_sqr(m) + eps)
+
+    return norm_impl()
+
+
+@preconditions(lambda x, **_: is_tensor(x))
+def norm_inv(m, eps=1e-6):
+    @ti.func
+    def norm_inv_impl():
+        return ti.rsqrt(norm_sqr(m) + eps)
+
+    return norm_inv_impl()
+
+
+__all__ = [
+    'transpose', 'matmul', 'determinant', 'trace', 'inverse', 'transpose',
+    'diag', 'sum', 'norm_sqr', 'norm', 'norm_inv'
+]
--- python/taichi/lang/matrix_ops_utils.py
+++ python/taichi/lang/matrix_ops_utils.py
@@ -1,7 +1,9 @@
+import functools
+
 from taichi.lang.exception import TaichiCompilationError
 from taichi.lang.expr import Expr
-from taichi.lang.matrix import Matrix, Vector
-import functools
+from taichi.lang.matrix import Matrix
+
 
 def preconditions(*checker_funcs):
     def decorator(func):
@@ -10,26 +12,51 @@ def preconditions(*checker_funcs):
             for f in checker_funcs:
                 try:
                     ok, msg = f(*args, **kwargs)
-                except TaichiCompilationError as e:
+                except TaichiCompilationError as _:
                     raise
                 if not ok:
                     raise TaichiCompilationError(msg)
             return func(*args, **kwargs)
+
         return wrapper
+
     return decorator
 
 
-def check_matrix(m, msg):
+def forall(func):
+    def check(*args, **_):
+        for i, arg in enumerate(args):
+            ok, msg = func(arg)
+            if not ok:
+                raise TaichiCompilationError(
+                    f"#{i} argument violates the precondition.\n" + msg)
+
+    return check
+
+
+def is_tensor(m, msg='not a matrix: {}'):
     if isinstance(m, Matrix):
-        return True
+        return True, None
     if isinstance(m, Expr) and m.is_tensor():
-        return True
-    raise TaichiCompilationError(msg)
-    
+        return True, None
+    raise TaichiCompilationError(msg.format(type(m)))
+
+
+def is_matrix(x):
+    is_tensor(x)
+    s = x.get_shape()
+    return len(s) == 2, f'not a matrix: {s}'
+
+
+def is_vector(x):
+    is_tensor(x)
+    s = x.get_shape()
+    return len(s) == 1, f'not a vector: {s}'
+
 
 def check_matmul(x, y):
-    check_matrix(x, f'left hand side is not a matrix: {type(x)}')
-    check_matrix(y, f'right hand side is not a matrix: {type(y)}')
+    is_tensor(x, f'left hand side is not a matrix: {type(x)}')
+    is_tensor(y, f'right hand side is not a matrix: {type(y)}')
     x_shape = x.get_shape()
     y_shape = y.get_shape()
     if len(x_shape) == 1:
@@ -38,15 +65,31 @@ def check_matmul(x, y):
     else:
         if x_shape[0] != y_shape[0]:
             return False, f'dimension mismatch between {x_shape} and {y_shape} for matrix multiplication'
-    return True
+    return True, None
 
 
-def check_det(x):
-    check_matrix(x, f'argument to det(.) is not a matrix: {type(x)}')
-    x_shape = x.get_shape()
-    if len(x_shape) != 2:
-        return False, f'argument to det(.) is not a 2D matrix: {x_shape}'
-    if x_shape[0] != x_shape[1]:
-        return False, f'argument to det(.) is not a square matrix: {x_shape}'
-    if x_shape[0] > 4:
-        return False, f'Determinants of matrices with sizes >= 5 are not supported: {x_shape}'
+def square_matrix(x):
+    is_tensor(x)
+    shape = x.get_shape()
+    if shape[0] != shape[1]:
+        return False, f'not a square matrix: {shape}'
+    return True, None
+
+
+def dim_lt(dim, limit, msg=None):
+    def check(x):
+        is_tensor(x)
+        shape = x.get_shape()
+        return shape[dim] < limit, (
+            f'Dimension >= {limit} is not supported: {shape}'
+            if not msg else msg.format(shape))
+
+    return check
+
+
+def is_int_const(x):
+    if isinstance(x, int):
+        return True, None
+    if isinstance(x, Expr) and x.val_int() is not None:
+        return True, None
+    return False, f'not an integer: {type(x)}'

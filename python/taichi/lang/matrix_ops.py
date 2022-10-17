3324cad7ebf059d21f7a6802e54609b78c80c455
AD1024 <dh63@cs.washington.edu>
2022-10-17 11:24:10 -0700

    address comments

--- python/taichi/lang/matrix_ops.py
+++ python/taichi/lang/matrix_ops.py
@@ -1,19 +1,17 @@
 from taichi.lang.impl import static
-from taichi.lang.kernel_impl import func
+from taichi.lang.kernel_impl import func, pyfunc
 from taichi.lang.matrix_ops_utils import (arg_at, is_tensor, preconditions,
                                           square_matrix)
-from taichi.lang.misc import loop_config
 from taichi.lang.ops import cast
-from taichi.lang.util import taichi_scope
+from taichi.lang.util import in_taichi_scope
+from taichi.types.annotations import Template
 
 
 @preconditions(square_matrix)
-@func
+@pyfunc
 def trace(x):
     shape = static(x.get_shape())
-    result = cast(0, x.element_type())
-    # TODO: fix parallelization
-    loop_config(serialize=True)
+    result = cast(0, x.element_type()) if static(in_taichi_scope()) else 0
     # TODO: get rid of static when
     # CHI IR Tensor repr is ready stable
     for i in static(range(shape[0])):
@@ -22,26 +20,17 @@ def trace(x):
 
 
 @preconditions(arg_at(0, is_tensor))
-@taichi_scope
-def fill(m, val):
-    # capture reference to m
-    @func
-    def fill_impl():
-        s = static(m.get_shape())
-        if static(len(s) == 1):
-            # TODO: fix parallelization
-            loop_config(serialize=True)
-            for i in static(range(s[0])):
-                m[i] = val
-            return m
-        # TODO: fix parallelization
-        loop_config(serialize=True)
+@func
+def fill(m: Template(), val):
+    s = static(m.get_shape())
+    if static(len(s) == 1):
         for i in static(range(s[0])):
-            for j in static(range(s[1])):
-                m[i, j] = val
+            m[i] = val
         return m
-
-    return fill_impl()
+    for i in static(range(s[0])):
+        for j in static(range(s[1])):
+            m[i, j] = val
+    return m
 
 
 __all__ = [

3588a865c5611b2b5d476b311751b25d984bda3b
Lin Jiang <linjiang@taichi.graphics>
2022-10-18 17:20:04 +0800

    [bug] Fix returning uint64 as signed integer

--- tests/python/test_return.py
+++ tests/python/test_return.py
@@ -163,3 +163,19 @@ def test_return_none():
         return None
 
     foo()
+
+@test_utils.test(exclude=ti.metal)
+def test_return_uint64():
+    @ti.kernel
+    def foo()->ti.u64:
+        return ti.u64(2 ** 64 - 1)
+
+    assert(foo() == 2 ** 64 - 1)
+
+@test_utils.test(exclude=ti.metal)
+def test_return_uint64_vec():
+    @ti.kernel
+    def foo()->ti.types.vector(2, ti.u64):
+        return ti.Vector([ti.u64(2 ** 64 - 1), ti.u64(2 ** 64 - 1)])
+
+    assert(foo()[0] == 2 ** 64 - 1)

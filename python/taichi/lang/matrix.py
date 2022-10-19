f75e2067cd09bc5a5e610dafcd65f99c88bbeecc
Yi Xu <xy_xuyi@foxmail.com>
2022-10-19 14:34:36 +0800

    [lang] [refactor] Refine implementation and tests for matrix slice (#6373)
    Issue: #4257, #5819
    
    ### Brief Summary
    
    Let's refine the matrix slice feature a bit before supporting it with
    the new MatrixType.
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- python/taichi/lang/matrix.py
+++ python/taichi/lang/matrix.py
@@ -249,10 +249,10 @@ class _TiScopeMatrixImpl(_MatrixBaseImpl):
         j = 0 if len(indices) == 1 else indices[1]
         has_slice = False
         if isinstance(i, slice):
-            i = self._calc_slice(i, 0)
+            i = impl._calc_slice(i, self.n)
             has_slice = True
         if isinstance(j, slice):
-            j = self._calc_slice(j, 1)
+            j = impl._calc_slice(j, self.m)
             has_slice = True
 
         if has_slice:
@@ -279,21 +279,6 @@ class _TiScopeMatrixImpl(_MatrixBaseImpl):
                                          (self.n, self.m),
                                          self.dynamic_index_stride)
         return self._get_entry(i, j)
-
-    def _calc_slice(self, index, dim):
-        start, stop, step = index.start or 0, index.stop or (
-            self.n if dim == 0 else self.m), index.step or 1
-
-        def helper(x):
-            #  TODO(mzmzm): support variable in slice
-            if isinstance(x, expr.Expr):
-                raise TaichiCompilationError(
-                    "Taichi does not support variables in slice now, please use constant instead of it."
-                )
-            return x
-
-        start, stop, step = helper(start), helper(stop), helper(step)
-        return [_ for _ in range(start, stop, step)]
 
 
 class _MatrixEntriesInitializer:

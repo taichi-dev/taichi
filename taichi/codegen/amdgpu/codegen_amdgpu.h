eb30ea61b2d9c49926f36b7b27d6d5d23274aa59
zeyuli <li_zeyu@pku.edu.cn>
2022-10-28 18:35:52 +0800

    add amdgpu codegen

---
+++ taichi/codegen/amdgpu/codegen_amdgpu.h
@@ -0,0 +1,44 @@
+// The AMDGPU backend
+#pragma once
+
+#include "taichi/codegen/codegen.h"
+#include "taichi/codegen/llvm/codegen_llvm.h"
+
+TLANG_NAMESPACE_BEGIN
+
+class KernelCodeGenAMDGPU : public KernelCodeGen {
+ public:
+  KernelCodeGenAMDGPU(Kernel *kernel, IRNode *ir = nullptr)
+      : KernelCodeGen(kernel, ir) {
+  }
+
+// TODO: Stop defining this macro guards in the headers
+#ifdef TI_WITH_LLVM
+  static std::unique_ptr<TaskCodeGenLLVM> make_codegen_llvm(Kernel *kernel,
+                                                            IRNode *ir);
+  LLVMCompiledData compile_task(
+      std::unique_ptr<llvm::Module> &&module = nullptr,
+      OffloadedStmt *stmt = nullptr) override;
+#endif  // TI_WITH_LLVM
+
+  bool supports_offline_cache() const override {
+    return true;
+  }
+
+  FunctionType compile_to_function() override;
+};
+
+class AMDGPUModuleToFunctionConverter : public ModuleToFunctionConverter {
+ public:
+  explicit AMDGPUModuleToFunctionConverter(TaichiLLVMContext *tlctx,
+                                         LlvmRuntimeExecutor *executor)
+      : ModuleToFunctionConverter(tlctx, executor) {
+  }
+  using ModuleToFunctionConverter::convert;
+
+  FunctionType convert(const std::string &kernel_name,
+                       const std::vector<LlvmLaunchArgInfo> &args,
+                       std::vector<LLVMCompiledData> &&data) const override;
+};
+
+TLANG_NAMESPACE_END

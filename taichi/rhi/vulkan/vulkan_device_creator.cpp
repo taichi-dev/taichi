8ed3e91993470b9e2301d09d7ecc488329422b68
PENGUINLIONG <admin@penguinliong.moe>
2022-11-01 18:05:50 +0800

    Disable!

--- taichi/rhi/vulkan/vulkan_device_creator.cpp
+++ taichi/rhi/vulkan/vulkan_device_creator.cpp
@@ -738,7 +738,7 @@ void VulkanDeviceCreator::create_logical_device(bool manual_create) {
         if (device_supported_features.shaderInt64) {
 // Temporarily disable it on macOS:
 // https://github.com/taichi-dev/taichi/issues/6295
-#if !defined(__APPLE__)
+#if !defined(__APPLE__) && false
           caps.set(DeviceCapability::spirv_has_physical_storage_buffer, true);
 #endif
         }

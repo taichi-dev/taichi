7cdb97fb94d12a9a9357bdfbe87440140c3308d6
Yi Xu <xy_xuyi@foxmail.com>
2022-11-01 13:50:06 +0800

    [opt] Eliminate redundant BitExtractStmt for SNode access under non-packed mode (#6485)
    Issue: #6219
    
    ### Brief Summary
    
    This PR adds optimization similar to #6444 for non-packed mode so that
    we can conduct fair comparisons regarding performance. After this PR,
    the benchmark script in #6219 runs `0.007s` on my local machine no
    matter `packed=True/False`.
    
    The tests are fixed because they are invalid - the out-of-bound access
    used to be hidden by the always inserted `BitExtractStmt` before this
    PR.

--- taichi/transforms/scalar_pointer_lowerer.cpp
+++ taichi/transforms/scalar_pointer_lowerer.cpp
@@ -90,7 +90,6 @@ void ScalarPointerLowerer::run() {
           auto const_next = lowered_->push_back<ConstStmt>(TypedConstant(next));
           extracted = lowered_->push_back<BinaryOpStmt>(
               BinaryOpType::div, indices_[k_], const_next);
-          is_first_extraction[k] = false;
         } else {
           extracted = generate_mod_x_div_y(lowered_, indices_[k_], prev, next);
         }
@@ -98,9 +97,16 @@ void ScalarPointerLowerer::run() {
         const int end = start_bits[k];
         start_bits[k] -= snode->extractors[k].num_bits;
         const int begin = start_bits[k];
-        extracted =
-            lowered_->push_back<BitExtractStmt>(indices_[k_], begin, end);
+        if (is_first_extraction[k] && begin == 0) {
+          // Similar optimization as above. In this case the full user
+          // coordinate is extracted so we don't need a BitExtractStmt.
+          extracted = indices_[k_];
+        } else {
+          extracted =
+              lowered_->push_back<BitExtractStmt>(indices_[k_], begin, end);
+        }
       }
+      is_first_extraction[k] = false;
       lowered_indices.push_back(extracted);
       strides.push_back(snode->extractors[k].shape);
     }

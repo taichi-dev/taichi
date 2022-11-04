6edc26f854e40d762cbbf7703533f2bf3d7adb5d
neozhaoliang <mathzhaoliang@gmail.com>
2022-11-04 16:56:18 +0800

    add deactivate to dynamic snodes

--- tests/python/test_dynamic_attributes.py
+++ tests/python/test_dynamic_attributes.py
@@ -3,35 +3,36 @@ from tests import test_utils
 
 
 def _test_dynamic_append_length(dt):
+    n = 10
     x = ti.field(int)
-    block = ti.root.dense(ti.i, 10)
-    pixel = block.dynamic(ti.j, 10)
+    block = ti.root.dense(ti.i, n)
+    pixel = block.dynamic(ti.j, n)
     pixel.place(x)
 
     y = ti.field(int)
-    ti.root.dynamic(ti.i, 10).place(y)
+    ti.root.dynamic(ti.i, n).place(y)
 
     @ti.kernel
     def test():
-        for i in range(10):
+        for i in range(n):
             for j in range(i):
                 x[i].append(j)
 
-        for i in range(10):
+        for i in range(n):
             assert (ti.length(x.parent(), i) == i)
             for j in range(i):
                 assert (x[i, j] == j)
 
-        for i in range(10):
+        for i in range(n):
             x[i].deactivate()
-            for j in range(10):
+            for j in range(n):
                 assert x[i, j] == 0
 
-        for j in range(10):
+        for j in range(n):
             y[j] = j
 
         y.deactivate()
-        for j in range(10):
+        for j in range(n):
             assert y[j] == 0
 
     test()

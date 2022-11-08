d21d62b4a4505ba2d833603090492f66e835bf80
Yi Xu <xy_xuyi@foxmail.com>
2022-11-08 15:22:06 +0800

    Remove unused import

--- python/taichi/_funcs.py
+++ python/taichi/_funcs.py
@@ -1,8 +1,8 @@
 import math
 
-from taichi.lang import impl, matrix, ops
+from taichi.lang import impl, ops
 from taichi.lang.impl import expr_init, get_runtime, grouped, static
-from taichi.lang.kernel_impl import func, pyfunc
+from taichi.lang.kernel_impl import func
 from taichi.lang.matrix import Matrix, Vector
 from taichi.types import f32, f64
 from taichi.types.annotations import template

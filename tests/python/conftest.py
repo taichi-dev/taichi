a6cd72f5dac43b211a4787b92284d8aaebf8ae5f
Proton <feisuzhu@163.com>
2022-11-09 14:53:44 +0800

    [ci] Terminate (and restart) test worker when test fails

--- tests/python/conftest.py
+++ tests/python/conftest.py
@@ -20,3 +20,28 @@ def pytest_generate_tests(metafunc):
         # fill with empty values to avoid undefined fixtures
         metafunc.parametrize('req_arch,req_options', [(None, None)],
                              ids=['none'])
+
+
+IS_WORKER = False
+
+
+@pytest.hookimpl(trylast=True)
+def pytest_configure(config):
+    global IS_WORKER
+    IS_WORKER = hasattr(config, "workerinput")
+
+
+@pytest.hookimpl(trylast=True)
+def pytest_runtest_logreport(report):
+    '''
+    Intentionally crash test workers when a test fails.
+    This is to avoid the failing test leaving a corrupted GPU state for the
+    following tests.
+    '''
+    if not IS_WORKER:
+        return
+
+    if report.outcome not in ('error', 'failed'):
+        return
+
+    os._exit(0)

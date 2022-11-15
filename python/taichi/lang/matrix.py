6d5657496f4c6428ce78d955e6df23a0463693bf
jim19930609 <jim19930609@gmail.com>
2022-11-15 16:43:03 +0800

    Bug fix

--- python/taichi/lang/matrix.py
+++ python/taichi/lang/matrix.py
@@ -1840,7 +1840,7 @@ class MatrixType(CompoundType):
             return ops_mod.cast(mat, self.dtype)
 
         if isinstance(mat, Matrix) and impl.current_cfg().real_matrix:
-            arr = mat.entries
+            arr = [[mat(i, j) for j in range(self.m)] for i in range(self.n)]
             return ops_mod.cast(make_matrix(arr), self.dtype)
 
         return mat.cast(self.dtype)

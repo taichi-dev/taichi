6d932db0586136a8d95b8e84542afbd2ebbaa8a0
PENGUINLIONG <admin@penguinliong.moe>
2022-11-21 10:45:48 +0800

    Fixed test

--- c_api/tests/c_api_interface_test.cpp
+++ c_api/tests/c_api_interface_test.cpp
@@ -152,9 +152,15 @@ TEST_F(CapiTest, TestLoadTcmAotModule) {
       ti::Runtime runtime(arch);
       ti::AotModule aot_mod = runtime.load_aot_module(aot_mod_ss.str());
       ti::Kernel run = aot_mod.get_kernel("run");
-      ti::NdArray<int32_t> arr = runtime.allocate_ndarray<int32_t>({16});
+      ti::NdArray<int32_t> arr = runtime.allocate_ndarray<int32_t>({16}, {}, true);
       run[0] = arr;
       run.launch();
+      runtime.wait();
+      std::vector<int32_t> data(16);
+      arr.read(data);
+      for (int32_t i = 0; i < 16; ++i) {
+        TI_ASSERT(data.at(i) == i);
+      }
     }
   }
 }

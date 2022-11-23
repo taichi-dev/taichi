185b2e1d165af0f4abf2eb4fa5c55d9fae31e24a
Yi Xu <xy_xuyi@foxmail.com>
2022-11-23 21:11:49 +0800

    [opt] Turn mod/div into bit_and/bit_shr if possible in SNode coordinate calculation

--- taichi/transforms/utils.cpp
+++ taichi/transforms/utils.cpp
@@ -3,11 +3,19 @@
 namespace taichi::lang {
 
 Stmt *generate_mod(VecStatement *stmts, Stmt *x, int y) {
+  if (bit::is_power_of_two(y)) {
+    auto const_stmt = stmts->push_back<ConstStmt>(TypedConstant(y - 1));
+    return stmts->push_back<BinaryOpStmt>(BinaryOpType::bit_and, x, const_stmt);
+  }
   auto const_stmt = stmts->push_back<ConstStmt>(TypedConstant(y));
   return stmts->push_back<BinaryOpStmt>(BinaryOpType::mod, x, const_stmt);
 }
 
 Stmt *generate_div(VecStatement *stmts, Stmt *x, int y) {
+  if (bit::is_power_of_two(y)) {
+    auto const_stmt = stmts->push_back<ConstStmt>(TypedConstant(bit::log2int(y)));
+    return stmts->push_back<BinaryOpStmt>(BinaryOpType::bit_shr, x, const_stmt);
+  }
   auto const_stmt = stmts->push_back<ConstStmt>(TypedConstant(y));
   return stmts->push_back<BinaryOpStmt>(BinaryOpType::div, x, const_stmt);
 }

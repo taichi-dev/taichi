8bfe1a40e1657f182461f4218456ca0c123bd57e
neozhaoliang <mathzhaoliang@gmail.com>
2022-11-23 15:39:13 +0800

    fix min/max warning

--- tests/python/test_packed_size.py
+++ tests/python/test_packed_size.py
@@ -5,6 +5,6 @@ from tests import test_utils
 @test_utils.test(require=ti.extension.packed, packed=True)
 def test_packed_size():
     x = ti.field(ti.i32)
-    ti.root.dense(ti.i, 17).dense(ti.ijk, 129).place(x)
-    assert x.shape == (17 * 129, 129, 129)
+    ti.root.dense(ti.l, 3).dense(ti.ijk, 129).place(x)
+    assert x.shape == (129, 129, 129, 3)
     assert x.snode.parent().parent()._cell_size_bytes == 4 * 129**3

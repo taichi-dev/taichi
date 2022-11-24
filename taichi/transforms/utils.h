1c4420a9fb19e7852b41ef65ead7a9be72a81047
Yi Xu <xy_xuyi@foxmail.com>
2022-11-24 09:33:15 +0800

    [opt] Turn mod/div into bit_and/bit_shr if possible in packed mode (#6718)
    Issue: #6660
    
    ### Brief Summary
    
    After this PR, the two main goals in #6660,
    
    > - Make the runtime overhead of SNode access always the same for both
    modes if the limitation is obeyed.
    > - Make the runtime overhead of struct for on a SNode whose path to
    root contains no non-power-of-two division always the same for both
    modes.
    
    are both achieved.
    
    See comments in the code for implementation details.
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- taichi/transforms/utils.h
+++ taichi/transforms/utils.h
@@ -2,6 +2,9 @@
 
 namespace taichi::lang {
 
+// These two helper functions are targeting cases where x is assumed
+// non-negative but with a signed type so no automatic transformation to bitwise
+// operations can be applied in other compiler passes.
 Stmt *generate_mod(VecStatement *stmts, Stmt *x, int y);
 Stmt *generate_div(VecStatement *stmts, Stmt *x, int y);
 

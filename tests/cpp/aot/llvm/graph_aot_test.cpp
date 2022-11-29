72f5adcbaad8df45e905c29a8105c04a9241e4a2
Zhanlue Yang <jim19930609@gmail.com>
2022-11-29 12:33:24 +0800

    [build] Improve TI_WITH_CUDA guards for CUDA related test cases (#6698)
    Issue: #
    
    ### Brief Summary

--- tests/cpp/aot/llvm/graph_aot_test.cpp
+++ tests/cpp/aot/llvm/graph_aot_test.cpp
@@ -4,10 +4,15 @@
 #include "taichi/runtime/program_impls/llvm/llvm_program.h"
 #include "taichi/system/memory_pool.h"
 #include "taichi/runtime/cpu/aot_module_loader_impl.h"
-#include "taichi/runtime/cuda/aot_module_loader_impl.h"
 #include "taichi/runtime/llvm/llvm_aot_module_loader.h"
+
+#ifdef TI_WITH_CUDA
+
 #include "taichi/rhi/cuda/cuda_driver.h"
 #include "taichi/platform/cuda/detect_cuda.h"
+#include "taichi/runtime/cuda/aot_module_loader_impl.h"
+
+#endif
 
 #define TI_RUNTIME_HOST
 #include "taichi/program/context.h"
@@ -70,6 +75,7 @@ TEST(LlvmCGraph, RunGraphCpu) {
 }
 
 TEST(LlvmCGraph, RunGraphCuda) {
+#ifdef TI_WITH_CUDA
   if (is_cuda_api_available()) {
     CompileConfig cfg;
     cfg.arch = Arch::cuda;
@@ -126,4 +132,5 @@ TEST(LlvmCGraph, RunGraphCuda) {
       EXPECT_EQ(cpu_data[i], 3 * i + base0 + base1 + base2);
     }
   }
+#endif
 }

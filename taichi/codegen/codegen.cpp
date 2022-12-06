a2f1e151fa801e987c8d3ecc8fc08c81e8cb31c9
PGZXB <pgzxb@qq.com>
2022-12-06 14:36:19 +0800

    Refactor2023: Remove `ir` parameter in KernelCodeGen::KernelCodeGen(Kernel *kernel, IRNode *ir)

--- taichi/codegen/codegen.cpp
+++ taichi/codegen/codegen.cpp
@@ -21,29 +21,27 @@
 
 namespace taichi::lang {
 
-KernelCodeGen::KernelCodeGen(Kernel *kernel, IRNode *ir)
-    : prog(kernel->program), kernel(kernel), ir(ir) {
-  if (ir == nullptr)
-    this->ir = kernel->ir.get();
+KernelCodeGen::KernelCodeGen(Kernel *kernel)
+    : prog(kernel->program), kernel(kernel) {
+  this->ir = kernel->ir.get();
 }
 
 std::unique_ptr<KernelCodeGen> KernelCodeGen::create(Arch arch,
-                                                     Kernel *kernel,
-                                                     Stmt *stmt) {
+                                                     Kernel *kernel) {
 #ifdef TI_WITH_LLVM
   if (arch_is_cpu(arch) && arch != Arch::wasm) {
-    return std::make_unique<KernelCodeGenCPU>(kernel, stmt);
+    return std::make_unique<KernelCodeGenCPU>(kernel);
   } else if (arch == Arch::wasm) {
-    return std::make_unique<KernelCodeGenWASM>(kernel, stmt);
+    return std::make_unique<KernelCodeGenWASM>(kernel);
   } else if (arch == Arch::cuda) {
 #if defined(TI_WITH_CUDA)
-    return std::make_unique<KernelCodeGenCUDA>(kernel, stmt);
+    return std::make_unique<KernelCodeGenCUDA>(kernel);
 #else
     TI_NOT_IMPLEMENTED
 #endif
   } else if (arch == Arch::dx12) {
 #if defined(TI_WITH_DX12)
-    return std::make_unique<KernelCodeGenDX12>(kernel, stmt);
+    return std::make_unique<KernelCodeGenDX12>(kernel);
 #else
     TI_NOT_IMPLEMENTED
 #endif

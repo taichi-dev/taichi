b079c14430bfeb832e4d64672ade8aac984d8c0b
jim19930609 <jim19930609@gmail.com>
2022-12-07 15:39:50 +0800

    Bug fix

--- cmake/utils.cmake
+++ cmake/utils.cmake
@@ -1,7 +1,7 @@
 function(configure_target_linker TARGET_NAME USE_MOLD)
 
 if (${USE_MOLD})
-    target_link_options(${TARGET_NAME} PRIVATE -Wl,-fuse-ld=mold)
+    target_link_options(${TARGET_NAME} PRIVATE -fuse-ld=mold)
 elseif(WIN32 OR LINUX)
     # Use "lld" as default linker instead of "ld" because:
     # 1. lld is a drop-in replacement for ld
@@ -11,7 +11,7 @@ elseif(WIN32 OR LINUX)
     #
     # Ref1: https://stackoverflow.com/questions/29361801/is-the-lld-linker-a-drop-in-replacement-for-ld-and-gold
     # Ref2: https://community.haxe.org/t/hxcpp-on-linux-tip-replace-ld-linker-with-lld-to-seriously-improve-compilation-time/2134
-    target_link_options(${TARGET_NAME} PRIVATE -Wl,-fuse-ld=lld)
+    target_link_options(${TARGET_NAME} PRIVATE -fuse-ld=lld)
     if(WIN32 AND CMAKE_BUILD_TYPE EQUAL "RelWithDebInfo")
         # -debug for LLD generates PDB files
         target_link_options(${TARGET_NAME} PRIVATE -Wl,-debug)

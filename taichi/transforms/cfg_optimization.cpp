edbe92e83b1ed93478f973cffc54cca62265761d
Cheng Cao <bobcaocheng@gmail.com>
2022-12-07 13:35:58 -0800

    disable cfg for real matrix

--- taichi/transforms/cfg_optimization.cpp
+++ taichi/transforms/cfg_optimization.cpp
@@ -17,11 +17,13 @@ bool cfg_optimization(
   TI_AUTO_PROF;
   auto cfg = analysis::build_cfg(root);
   bool result_modified = false;
-  cfg->simplify_graph();
-  if (cfg->store_to_load_forwarding(after_lower_access, autodiff_enabled))
-    result_modified = true;
-  if (cfg->dead_store_elimination(after_lower_access, lva_config_opt))
-    result_modified = true;
+  if (!real_matrix_enabled) {
+    cfg->simplify_graph();
+    if (cfg->store_to_load_forwarding(after_lower_access, autodiff_enabled))
+      result_modified = true;
+    if (cfg->dead_store_elimination(after_lower_access, lva_config_opt))
+      result_modified = true;
+  }
   // TODO: implement cfg->dead_instruction_elimination()
   die(root);  // remove unused allocas
   return result_modified;

507eed1edcb8267eb5fe3fbf74c72a19b30db7a1
damnkk <17611288725@163.com>
2022-12-07 16:23:24 +0800

    MapMemory

--- c_api/tests/c_api_behavior_test.cpp
+++ c_api/tests/c_api_behavior_test.cpp
@@ -92,7 +92,7 @@ TEST_F(CapiTest, TestBehaviorAllocateMemory)
 {
   TiError error = TI_ERROR_SUCCESS;
 
-  //不同用法测一轮，内存极大测一轮，多种arch测一轮，runtime为空测一波，info为空测一波
+ 
   TiMemoryAllocateInfo* allocate_info = new TiMemoryAllocateInfo;
   allocate_info->size = 1024;
   if(ti::is_arch_available(TI_ARCH_VULKAN))
@@ -113,41 +113,10 @@ TEST_F(CapiTest, TestBehaviorAllocateMemory)
     allocate_info->size = 1024;
   }
   allocate_info->size = 1024;
-  if(ti::is_arch_available(TI_ARCH_CUDA))
-  {
-    TiRuntime runtime = ti_create_runtime(TI_ARCH_CUDA);
-    for(int i = 0;i<4;++i)
-    {
-      allocate_info->usage = TI_MEMORY_USAGE_STORAGE_BIT<<i;
-      TiMemory memory = ti_allocate_memory(runtime,allocate_info);
-      TI_ASSERT(memory!=TI_NULL_HANDLE);
-    }
-    allocate_info->size = 1000000000000000000;
-    ti_allocate_memory(runtime,allocate_info);
-    error = ti_get_last_error(0,nullptr);
-    //std::cout<<error<<std::endl;
-    TI_ASSERT(error==TI_ERROR_OUT_OF_MEMORY);                   
-    allocate_info->size = 1024;  
-  }
+   
   if(ti::is_arch_available(TI_ARCH_OPENGL))
   {
     TiRuntime runtime = ti_create_runtime(TI_ARCH_OPENGL);
-    for(int i = 0;i<4;++i)
-    {
-      allocate_info->usage = TI_MEMORY_USAGE_STORAGE_BIT<<i;
-      TiMemory memory = ti_allocate_memory(runtime,allocate_info);
-      TI_ASSERT(memory!=TI_NULL_HANDLE);
-    }
-    allocate_info->size = 1000000000000000000;
-    ti_allocate_memory(runtime,allocate_info);
-    error = ti_get_last_error(0,nullptr);
-    //std::cout<<error<<std::endl;
-    TI_ASSERT(error==TI_ERROR_OUT_OF_MEMORY);                   
-    allocate_info->size = 1024;
-  }
-  if(ti::is_arch_available(TI_ARCH_X64))
-  {
-    TiRuntime runtime = ti_create_runtime(TI_ARCH_X64);
     for(int i = 0;i<4;++i)
     {
       allocate_info->usage = TI_MEMORY_USAGE_STORAGE_BIT<<i;
@@ -166,9 +135,7 @@ TEST_F(CapiTest, TestBehaviorAllocateMemory)
   error = ti_get_last_error(0,nullptr);
   TI_ASSERT(error==TI_ERROR_ARGUMENT_NULL);
   ti_set_last_error(TI_ERROR_SUCCESS,nullptr);
- 
 }
-
 
 TEST_F(CapiTest, TestBehaviorFreeMemory)    
 {
@@ -181,20 +148,53 @@ TEST_F(CapiTest, TestBehaviorFreeMemory)
     TiMemory memory = ti_allocate_memory(runtime,allocate_info);
     ti_free_memory(runtime,memory);
     TiError error = ti_get_last_error(0,nullptr);
-
-    TI_ASSERT(error==TI_ERROR_ARGUMENT_NOT_FOUND);
+    TI_ASSERT(error == TI_ERROR_SUCCESS);
   }
 }
 
 TEST_F (CapiTest, TestBehaviorMapMemory)
 {
+  TiMemoryAllocateInfo* allocate_info = new TiMemoryAllocateInfo;
+  allocate_info->size = 1024;
+  allocate_info->usage = TI_MEMORY_USAGE_STORAGE_BIT;
   if(ti::is_arch_available(TI_ARCH_VULKAN))
   {
     TiRuntime runtime = ti_create_runtime(TI_ARCH_VULKAN);
-    TiMemoryAllocateInfo* allocate_info = new TiMemoryAllocateInfo;
-    allocate_info->size = 1024;
-    allocate_info->usage = TI_MEMORY_USAGE_STORAGE_BIT;
     TiMemory memory = ti_allocate_memory(runtime,allocate_info);
-    
+    ti_map_memory(runtime,memory);
+    TiError error = ti_get_last_error(0,nullptr);
+    TI_ASSERT(error == TI_ERROR_SUCCESS);
+    ti_set_last_error(TI_ERROR_SUCCESS,nullptr);
   }
+   if(ti::is_arch_available(TI_ARCH_CUDA))
+  {
+    TiRuntime runtime = ti_create_runtime(TI_ARCH_CUDA);
+    TiMemory memory = ti_allocate_memory(runtime,allocate_info);
+    ti_map_memory(runtime,memory);
+    TiError error = ti_get_last_error(0,nullptr);
+    TI_ASSERT(error == TI_ERROR_SUCCESS);
+    ti_set_last_error(TI_ERROR_SUCCESS,nullptr);
+  }
+   if(ti::is_arch_available(TI_ARCH_X64))
+  {
+    TiRuntime runtime = ti_create_runtime(TI_ARCH_X64);
+    TiMemory memory = ti_allocate_memory(runtime,allocate_info);
+    ti_map_memory(runtime,memory);
+    TiError error = ti_get_last_error(0,nullptr);
+    TI_ASSERT(error == TI_ERROR_SUCCESS);
+    ti_set_last_error(TI_ERROR_SUCCESS,nullptr);
+  }
+   if(ti::is_arch_available(TI_ARCH_OPENGL))
+  {
+    TiRuntime runtime = ti_create_runtime(TI_ARCH_OPENGL);
+    TiMemory memory = ti_allocate_memory(runtime,allocate_info);
+    ti_map_memory(runtime,memory);
+    TiError error = ti_get_last_error(0,nullptr);
+    TI_ASSERT(error == TI_ERROR_SUCCESS);
+    ti_set_last_error(TI_ERROR_SUCCESS,nullptr);
+  }
+  ti_map_memory(TI_NULL_HANDLE,TI_NULL_HANDLE);
+  TiError error = ti_get_last_error(0,nullptr);
+  TI_ASSERT(error,TI_ERROR_ARGUMENT_NULL);
+  ti_set_last_error(TI_ERROR_SUCCESS,nullptr);
 }

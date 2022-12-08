a53a8ffc6839db2b0d62d75bb74fb855ddad2771
Zhanlue Yang <zy2284@columbia.edu>
2022-12-08 11:00:09 +0800

    Update tests/python/test_matrix.py
    Co-authored-by: Yi Xu <xy_xuyi@foxmail.com>

--- tests/python/test_matrix.py
+++ tests/python/test_matrix.py
@@ -1195,10 +1195,7 @@ def test_atomic_op_scalarize():
     _test_field_and_ndarray(field, ndarray, func, verify)
 
 
-@test_utils.test(arch=[ti.cuda, ti.cpu],
-                 real_matrix=True,
-                 real_matrix_scalarize=True,
-                 debug=True)
+@test_utils.test()
 def test_unsupported_logical_operations():
     @ti.kernel
     def test():

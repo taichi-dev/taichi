e97d9bf92825019e560cff29556508053f4ff0e3
Zhanlue Yang <zy2284@columbia.edu>
2022-12-09 16:30:40 +0800

    Update tests/python/test_ast_refactor.py
    Co-authored-by: Yi Xu <xy_xuyi@foxmail.com>

--- tests/python/test_ast_refactor.py
+++ tests/python/test_ast_refactor.py
@@ -1066,7 +1066,7 @@ def _test_grouped_static_for_cast():
     def foo() -> ti.f32:
         ret = 0.
         for I in ti.static(ti.grouped(ti.ndrange((4, 5), (3, 5), 5))):
-            tmp = ti.cast(I, float)
+            tmp = I.cast(float)
             ret += tmp[2] / 2
         return ret
 

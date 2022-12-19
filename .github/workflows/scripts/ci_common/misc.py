4b3ff6d5a863e764898498058058bb3238b0c568
Proton <feisuzhu@163.com>
2022-12-19 16:44:35 +0800

    banner to stderr

--- .github/workflows/scripts/ci_common/misc.py
+++ .github/workflows/scripts/ci_common/misc.py
@@ -6,10 +6,12 @@ from typing import Callable
 import inspect
 import os
 import platform
+import sys
 
 # -- third party --
 # -- own --
 from .escapes import escape_codes
+
 
 # -- code --
 def is_manylinux2014() -> bool:
@@ -32,13 +34,13 @@ def banner(msg: str) -> Callable:
         N = escape_codes['reset']
         def wrapper(*args, **kwargs):
             _args = sig.bind(*args, **kwargs)
-            print(f'{C}:: -----BEGIN {msg}-----{N}'.format(**_args.arguments))
+            print(f'{C}:: -----BEGIN {msg}-----{N}'.format(**_args.arguments), file=sys.stderr)
             try:
                 ret = f(*args, **kwargs)
-                print(f'{C}:: -----END {msg}-----{N}'.format(**_args.arguments))
+                print(f'{C}:: -----END {msg}-----{N}'.format(**_args.arguments), file=sys.stderr)
                 return ret
             except BaseException as e:
-                print(f'{R}!! -----EXCEPTION {msg}-----{N}'.format(**_args.arguments))
+                print(f'{R}!! -----EXCEPTION {msg}-----{N}'.format(**_args.arguments), file=sys.stderr)
                 raise
 
         return wrapper

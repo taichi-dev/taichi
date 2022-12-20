6fc23278cc3bb8d0d2ef3bf24634f665034f97a7
PENGUINLIONG <admin@penguinliong.moe>
2022-12-20 17:40:09 +0800

    Remove third party includes

--- c_api/include/taichi/taichi.h
+++ c_api/include/taichi/taichi.h
@@ -5,28 +5,14 @@
 #include "taichi/taichi_core.h"
 
 #ifdef TI_WITH_VULKAN
-#ifndef TI_NO_VULKAN_INCLUDES
-#include <vulkan/vulkan.h>
-#endif  // TI_NO_VULKAN_INCLUDES
-
 #include "taichi/taichi_vulkan.h"
 #endif  // TI_WITH_VULKAN
 
 #ifdef TI_WITH_OPENGL
-#ifndef TI_NO_OPENGL_INCLUDES
-#include <GL/gl.h>
-#endif  // TI_NO_OPENGL_INCLUDES
-
 #include "taichi/taichi_opengl.h"
 #endif  // TI_WITH_OPENGL
 
 #ifdef TI_WITH_CUDA
-#ifndef TI_NO_CUDA_INCLUDES
-// Only a few CUDA types is needed, including the entire <cuda.h> is overkill
-// for this
-typedef void *CUdeviceptr;
-#endif  // TI_NO_CUDA_INCLUDES
-
 #include "taichi/taichi_cuda.h"
 #endif  // TI_WITH_CUDA
 

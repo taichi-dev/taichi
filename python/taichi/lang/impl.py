847989b1042416d46a92763dbefe9eedd8997153
jim19930609 <jim19930609@gmail.com>
2022-12-23 11:44:54 +0800

    Bug fix

--- python/taichi/lang/impl.py
+++ python/taichi/lang/impl.py
@@ -259,11 +259,11 @@ def subscript(ast_builder, value, *_indices, skip_reordered=False):
 
 
 @taichi_scope
-def make_index_expr(_var, _indices):
+def make_index_expr(_var, indices_expr_group):
 
     ast_builder = get_runtime().prog.current_ast_builder()
     return Expr(
-        ast_builder.expr_subscript(_var, make_expr_group(*_indices),
+        ast_builder.expr_subscript(_var, indices_expr_group,
                                    get_runtime().get_current_src_info()))
 
 

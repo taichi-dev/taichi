1763ca1ee0a1ee2d6d70414c49b190c59c36ccae
PENGUINLIONG <admin@penguinliong.moe>
2022-12-26 15:03:07 +0800

    [aot] Recover AOT CI script (#6970)

--- .github/workflows/scripts/aot-demo.sh
+++ .github/workflows/scripts/aot-demo.sh
@@ -4,11 +4,11 @@ set -ex
 export TI_SKIP_VERSION_CHECK=ON
 export TI_CI=1
 
-export TAICHI_AOT_DEMO_URL=https://github.com/PENGUINLIONG/taichi-aot-demo
-export TAICHI_AOT_DEMO_BRANCH=fix-cgraph-demo
+export TAICHI_AOT_DEMO_URL=https://github.com/taichi-dev/taichi-aot-demo
+export TAICHI_AOT_DEMO_BRANCH=master
 
 export TAICHI_UNITY2_URL=https://github.com/taichi-dev/taichi-unity2
-export TAICHI_UNITY2_BRANCH=fix-include
+export TAICHI_UNITY2_BRANCH=main
 
 export TAICHI_UNITY_EXAMPLE_URL=https://github.com/taichi-dev/Taichi-UnityExample
 export TAICHI_UNITY_EXAMPLE_BRANCH=main

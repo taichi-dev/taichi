6ae8e325716b36bec14858fb4356917ea595f49d
Lin Jiang <linjiang@taichi.graphics>
2022-12-28 11:33:01 +0800

    add reference

--- taichi/util/hash.h
+++ taichi/util/hash.h
@@ -5,11 +5,6 @@
 
 namespace taichi::hashing {
 
-namespace {
-inline void hash_combine(size_t &seed, size_t value) {
-  seed ^= value + 0x9e3779b9 + (seed << 6) + (seed >> 2);
-}
-}  // namespace
 
 template <typename T>
 struct Hasher {
@@ -19,13 +14,21 @@ struct Hasher {
   }
 };
 
+namespace {
+template <typename T>
+inline void hash_combine(size_t &seed, T const &value) {
+  // Reference: https://www.boost.org/doc/libs/1_55_0/doc/html/hash/reference.html#boost.hash_combine
+  seed ^= Hasher<T>{}(value) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+}
+}  // namespace
+
 template <typename T>
 struct Hasher<std::vector<T>> {
  public:
   size_t operator()(std::vector<T> const &vec) const {
     size_t ret = 0;
     for (const auto &i : vec) {
-      hash_combine(ret, Hasher<T>()(i));
+      hash_combine(ret, i);
     }
     return ret;
   }

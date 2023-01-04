f661c52dd45206238d4e98c2c8a8ee8bca6aab4a
zeyuli <li_zeyu@pku.edu.cn>
2023-01-04 13:45:29 +0800

    revert

--- taichi/rhi/amdgpu/amdgpu_context.h
+++ taichi/rhi/amdgpu/amdgpu_context.h
@@ -34,11 +34,6 @@ class AMDGPUContext {
     return dev_count_ != 0;
   }
 
-  void pack_args(std::vector<void *> arg_pointers,
-                 std::vector<int> arg_sizes, char *arg_packed);
-
-  int get_args_byte(std::vector<int> arg_sizes)
-
   void launch(void *func,
               const std::string &task_name,
               void *arg_pointers,

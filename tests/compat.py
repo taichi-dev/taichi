604ecb6412c978866cf9dc398ae5b0aa602ffd7d
damnkk <17611288725@163.com>
2023-01-05 23:28:01 +0800

    move

--- tests/compat.py
+++ tests/compat.py
@@ -42,8 +42,7 @@ for x in aot_files:
     path_name = pathlib.Path(x).name[:-3]
     os.mkdir('tests/cpp/aot/python_scripts/'+path_name)
     os.environ["TAICHI_AOT_FOLDER_PATH"] = 'tests/cpp/aot/python_scripts/'+path_name
-    
     subprocess.check_call(["python", x, "--arch=vulkan"])
-subprocess.check_call(["python", 'tests/run_tests.py', "-c"])
+#subprocess.check_call(["python", 'tests/run_tests.py', "-c"])
 
 

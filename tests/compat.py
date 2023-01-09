44ec9a196c78b7ca1929244a4f4c2271ad510cc3
damnkk <17611288725@163.com>
2023-01-09 17:25:14 +0800

    Update compat.py

--- tests/compat.py
+++ tests/compat.py
@@ -13,24 +13,7 @@ capi_test_filename = 'taichi_c_api_tests'
 cpp_tests_path = os.path.join(build_dir, capi_test_filename)
 c_api_tests_path = os.path.join(build_dir, cpp_test_filename)
 
-# aot_copy_list = [c_api_tests+'--gtest_filter=CapiTest.TestCompat*']
-# graph_aot_test_list = [c_api_tests+' --gtest_filter=CapiTest.TestCompatLoadAOT']
-# aot_module_test_list = [c_api_tests+' --gtest_filter=CapiTest.TestBehaviorLoadAOTModuleVulkan']
-# tcm_test_list = [c_api_tests+' --gtest_filter=CapiTest.*TcmAotModule']
-# kernel_aot_test1_list = [c_api_tests+' --gtest_filter=CapiTest.AotTestVulkanKernel', c_api_tests+' --gtest_filter=CapiTest.DryRunVulkanAotModule']
-# dense_field_aot_test_list = [cpp_tests+' --gtest_filter=GfxAotTest.VulkanDenseField']
-# kernel_aot_test2_list = [cpp_tests+' --gtest_filter=GfxAotTest.VulkanKernelTest2',cpp_tests+' --gtest_filter=CGraphAotTest.VulkanRunCGraph2']
-# mpm88_graph_aot_list = [c_api_tests+' --gtest_filter=CapiTest.Mpm88TestVulkan']
-# sph_aot_list = [c_api_tests+' --gtest_filter=CapiTest.SphTestVulkan']
-# tcm_test_list = [c_api_tests+' --gtest_filter=CapiTest.TestLoadTcmAotModule',c_api_tests+' --gtest_filter=CapiTest.TestCreateTcmAotModule']
-# texture_aot_test_list = [c_api_tests+' --gtest_filter=CapiTest.GraphTestVulkanTextureGraph']
-
-# run_dict = {'aotcopy':aot_copy_list,'aot_module_test':aot_module_test_list,'dense_field_aot_test':dense_field_aot_test_list,
-# 'graph_aot_test':graph_aot_test_list,'kernel_aot_test1':kernel_aot_test1_list,'kernel_aot_test2':kernel_aot_test2_list,
-# 'mpm88_grpah_aot':mpm88_graph_aot_list,'sph_aot':sph_aot_list,'tcm_test': tcm_test_list,'texture_aot_test': texture_aot_test_list}
-
 run_dict = {}
-
 
 def init_dict(run_dict, aot_files):
     curr_dir = os.path.dirname(os.path.abspath(__file__))
@@ -71,7 +54,6 @@ def generate():
         except subprocess.CalledProcessError:
             continue
 
-
 def run():
     aot_files = glob.glob('tests/cpp/aot/python_scripts/*.py')
     init_dict(run_dict, aot_files)
@@ -93,18 +75,6 @@ def run():
                 continue
             except FileNotFoundError:
                 continue
-
-
-# def test():
-#     templist = os.path.dirname(os.path.abspath(__file__))
-#     root_opath = os.path.dirname(templist)
-#     print(root_opath)
-#     new_dir = os.path.join(root_opath, 'build')
-#     print(os.listdir(new_dir))
-#     json_path = 'C:/Users/admin/JYF_Repositories/taichi_fock/tests/cpp/aot/python_scripts/dense_field_aot_test/metadata.json'
-#     with open(json_path,'r') as f:
-#         temp_json = json.load(f)
-#         print(temp_json)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()

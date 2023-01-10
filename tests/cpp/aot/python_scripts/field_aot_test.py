6f8e0edb9085ac12413602c70b8f42504b547779
Haidong Lan <haidonglan@taichi.graphics>
2023-01-10 10:47:12 +0800

    [Aot] Deprecate element shape and field dim for AOT symbolic args (#7100)
    Issue: #6572 
    
    * Fix using dtype for symbolic args in AOT usages
    * Cleanup all the usages of `field_dim` and `element_shape` in examples and tests

--- tests/cpp/aot/python_scripts/field_aot_test.py
+++ tests/cpp/aot/python_scripts/field_aot_test.py
@@ -91,8 +91,8 @@ def compile_field_aot(arch, compile_for_cgraph=False):
     if compile_for_cgraph:
         g_builder = ti.graph.GraphBuilder()
 
-        base0 = ti.graph.Arg(ti.graph.ArgKind.SCALAR, 'base0', ti.i32)
-        base1 = ti.graph.Arg(ti.graph.ArgKind.SCALAR, 'base1', ti.i32)
+        base0 = ti.graph.Arg(ti.graph.ArgKind.SCALAR, 'base0', dtype=ti.i32)
+        base1 = ti.graph.Arg(ti.graph.ArgKind.SCALAR, 'base1', dtype=ti.i32)
 
         g_builder.dispatch(init_fields, base0)
         g_builder.dispatch(check_init_x, base1)
--- tests/cpp/aot/python_scripts/mpm88_graph_aot.py
+++ tests/cpp/aot/python_scripts/mpm88_graph_aot.py
@@ -27,17 +27,16 @@ def compile_mpm88(arch, save_compute_graph):
     ti.init(arch=arch)
 
     @ti.kernel
-    def substep_reset_grid(grid_v: ti.any_arr(field_dim=2),
-                           grid_m: ti.any_arr(field_dim=2)):
+    def substep_reset_grid(grid_v: ti.any_arr(ndim=2),
+                           grid_m: ti.any_arr(ndim=2)):
         for i, j in grid_m:
             grid_v[i, j] = [0, 0]
             grid_m[i, j] = 0
 
     @ti.kernel
-    def substep_p2g(x: ti.any_arr(field_dim=1), v: ti.any_arr(field_dim=1),
-                    C: ti.any_arr(field_dim=1), J: ti.any_arr(field_dim=1),
-                    grid_v: ti.any_arr(field_dim=2),
-                    grid_m: ti.any_arr(field_dim=2)):
+    def substep_p2g(x: ti.any_arr(ndim=1), v: ti.any_arr(ndim=1),
+                    C: ti.any_arr(ndim=1), J: ti.any_arr(ndim=1),
+                    grid_v: ti.any_arr(ndim=2), grid_m: ti.any_arr(ndim=2)):
         for p in x:
             dx = 1 / grid_v.shape[0]
             p_vol = (dx * 0.5)**2
@@ -57,8 +56,8 @@ def compile_mpm88(arch, save_compute_graph):
                 grid_m[base + offset] += weight * p_mass
 
     @ti.kernel
-    def substep_update_grid_v(grid_v: ti.any_arr(field_dim=2),
-                              grid_m: ti.any_arr(field_dim=2)):
+    def substep_update_grid_v(grid_v: ti.any_arr(ndim=2),
+                              grid_m: ti.any_arr(ndim=2)):
         for i, j in grid_m:
             num_grid = grid_v.shape[0]
             if grid_m[i, j] > 0:
@@ -74,10 +73,9 @@ def compile_mpm88(arch, save_compute_graph):
                 grid_v[i, j].y = 0
 
     @ti.kernel
-    def substep_g2p(x: ti.any_arr(field_dim=1), v: ti.any_arr(field_dim=1),
-                    C: ti.any_arr(field_dim=1), J: ti.any_arr(field_dim=1),
-                    grid_v: ti.any_arr(field_dim=2),
-                    pos: ti.any_arr(field_dim=1)):
+    def substep_g2p(x: ti.any_arr(ndim=1), v: ti.any_arr(ndim=1),
+                    C: ti.any_arr(ndim=1), J: ti.any_arr(ndim=1),
+                    grid_v: ti.any_arr(ndim=2), pos: ti.any_arr(ndim=1)):
         for p in x:
             dx = 1 / grid_v.shape[0]
             Xp = x[p] / dx
@@ -100,8 +98,8 @@ def compile_mpm88(arch, save_compute_graph):
             C[p] = new_C
 
     @ti.kernel
-    def init_particles(x: ti.any_arr(field_dim=1), v: ti.any_arr(field_dim=1),
-                       J: ti.any_arr(field_dim=1)):
+    def init_particles(x: ti.any_arr(ndim=1), v: ti.any_arr(ndim=1),
+                       J: ti.any_arr(ndim=1)):
         for i in range(x.shape[0]):
             x[i] = [ti.random() * 0.4 + 0.2, ti.random() * 0.4 + 0.2]
             v[i] = [0, -1]
@@ -115,39 +113,29 @@ def compile_mpm88(arch, save_compute_graph):
 
         sym_x = ti.graph.Arg(ti.graph.ArgKind.NDARRAY,
                              'x',
-                             ti.f32,
-                             field_dim=1,
-                             element_shape=(2, ))
+                             dtype=ti.math.vec2,
+                             ndim=1)
         sym_v = ti.graph.Arg(ti.graph.ArgKind.NDARRAY,
                              'v',
-                             ti.f32,
-                             field_dim=1,
-                             element_shape=(2, ))
+                             dtype=ti.math.vec2,
+                             ndim=1)
         sym_C = ti.graph.Arg(ti.graph.ArgKind.NDARRAY,
                              'C',
-                             ti.f32,
-                             field_dim=1,
-                             element_shape=(2, 2))
-        sym_J = ti.graph.Arg(ti.graph.ArgKind.NDARRAY,
-                             'J',
-                             ti.f32,
-                             field_dim=1,
-                             element_shape=())
+                             dtype=ti.math.mat2,
+                             ndim=1)
+        sym_J = ti.graph.Arg(ti.graph.ArgKind.NDARRAY, 'J', ti.f32, ndim=1)
         sym_grid_v = ti.graph.Arg(ti.graph.ArgKind.NDARRAY,
                                   'grid_v',
-                                  ti.f32,
-                                  field_dim=2,
-                                  element_shape=(2, ))
+                                  dtype=ti.math.vec2,
+                                  ndim=2)
         sym_grid_m = ti.graph.Arg(ti.graph.ArgKind.NDARRAY,
                                   'grid_m',
-                                  ti.f32,
-                                  field_dim=2,
-                                  element_shape=())
+                                  dtype=ti.f32,
+                                  ndim=2)
         sym_pos = ti.graph.Arg(ti.graph.ArgKind.NDARRAY,
                                'pos',
-                               ti.f32,
-                               field_dim=1,
-                               element_shape=(3, ))
+                               dtype=ti.math.vec3,
+                               ndim=1)
 
         g_init_builder = ti.graph.GraphBuilder()
         g_init_builder.dispatch(init_particles, sym_x, sym_v, sym_J)

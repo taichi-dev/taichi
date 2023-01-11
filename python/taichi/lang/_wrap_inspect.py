4e0b89cd2064ba3fca31fc33c81eeeaca03b31a3
neozhaoliang <mathzhaoliang@gmail.com>
2023-01-11 11:58:32 +0800

    save

--- python/taichi/lang/_wrap_inspect.py
+++ python/taichi/lang/_wrap_inspect.py
@@ -9,6 +9,9 @@ _builtin_findsource = inspect.findsource
 
 
 def _find_source_with_custom_getfile_func(func, obj):
+    """Use a custom function `func` to replace inspect's `getfile`, return the
+    source found by the new routine and restore the original `getfile` back.
+    """
     inspect.getfile = func  # replace with our custom func
     source = inspect.findsource(obj)
     inspect.getfile = _builtin_getfile  # restore
@@ -16,12 +19,19 @@ def _find_source_with_custom_getfile_func(func, obj):
 
 
 def _blender_get_text_name(filename: str):
+    """Extract filename from path in the Blender mode."""
+    # In Blender's scripting mode, unsaved files are named
+    # like `/Text`, `/Text.001`, `/test.py`, etc.
+    # We simply remove this path seperator.
     if filename.startswith(os.path.sep) and filename.count(os.path.sep) == 1:
         return filename[1:]  # "/Text.001" --> "Text.001"
 
+    # Saved text files are named like `some-path/xxx.blend/Text` or
+    # `some-path/xxx.blend/test.py`
+    # We drop the path and extract the filename with extension.
     index = filename.rfind('.blend' + os.path.sep)
     if index != -1:
-        return filename[index + 7:]  # "hello.blend/test.py" --> "test.py"
+        return filename[index + 7:]  # "xxx.blend/test.py" --> "test.py"
 
     return None
 
@@ -29,21 +39,26 @@ def _blender_get_text_name(filename: str):
 def _blender_findsource(obj):
     try:
         import bpy  # pylint: disable=import-outside-toplevel
-    except ImportError:
-        raise IOError('Not in Blender environment!')
+    except ImportError('Not in Blender environment!'):
+        return None
 
+    # Inspect's built-in `getfile` returns the filename like
+    # `/Text`, `/Text.001`, `some-path/xxx.blend/test.py`
+    # This filename may not be a full valid path.
     filename = _builtin_getfile(obj)
+    # Extract the text name without path
     text_name = _blender_get_text_name(filename)
     if text_name is None:
         raise IOError(
             'Object `{obj.__name__}` is not defined in a .blend file!')
-
+    # Get the lines of code via text_name
     lines = bpy.data.texts[text_name].as_string()
-    # Now we have found the filename and code lines.
+    # Now we have found the lines of code.
     # We first check if they are already cached, to avoid file io in each query.
     try:
         filename = _blender_findsource._saved_inspect_cache[lines]  # pylint: disable=no-member
     except KeyError:
+        # Save the code to a valid path.
         fd, filename = tempfile.mkstemp(prefix='_Blender_',
                                         suffix=f'_{text_name}.py')
         os.close(fd)
@@ -54,6 +69,7 @@ def _blender_findsource(obj):
         _blender_findsource._saved_inspect_cache[lines] = filename  # pylint: disable=no-member
         atexit.register(os.unlink, filename)  # Remove file when program exits
 
+    # Our custom getfile function
     def wrapped_getfile(ob):
         if id(ob) == id(obj):
             return filename
@@ -72,20 +88,24 @@ def _Python_IPython_findsource(obj):
         return _builtin_findsource(obj)
     except IOError:
         # Except that the cell has a magic command like %%time or %%timeit
-        # In this case the filename returned by getfile is wrong
+        # In this case the filename returned by the built-in's getfile is wrong,
+        # it becomes something like `<timed exec>` or `<magic-timeit>`.
         filename = _builtin_getfile(obj)
         if (filename in {"<timed exec>", "<magic-timeit>"}):
             try:
                 ip = get_ipython()
                 if ip is not None:
+                    # So we are in IPython's cell magic
                     session_id = ip.history_manager.get_last_session_id()
                     fd, filename = tempfile.mkstemp(prefix='_IPython_',
                                                     suffix=f'_{session_id}.py')
                     os.close(fd)
-                    # The latest lines of code are stored in this file
+                    # The latest lines of code can be retrived from here
                     lines = ip.history_manager._i00
 
-                    # Remove the magic command (and spaces/sep around it) before saving to a file
+                    # `lines` is a string that also contains the cell magic command,
+                    # we need to remove the magic command (and spaces/sep around it)
+                    # to obtain a valid Python code snippet before saving it to a file
                     index = lines.find("%time")
                     lines_stripped = lines[index:]
                     lines_stripped = lines_stripped.split(maxsplit=1)[1]
@@ -124,22 +144,15 @@ class _InspectContextManager:
 
 
 def getsourcelines(obj):
-    try:
-        with _InspectContextManager():
-            return inspect.getsourcelines(obj)
-    except:
-        raise IOError(f"Cannot get the source lines of {obj}. This is possibly because of you are running Taichi in an environment in which Taichi's own inspect module cannot find the source file. Please report an issue to help us fix this problem: https://github.com/taichi-dev/taichi/issues")
-
+    with _InspectContextManager():
+        return inspect.getsourcelines(obj)
 
 def getsourcefile(obj):
-    try:
-        with _InspectContextManager():
-            ret = inspect.getsourcefile(obj)
-            if ret is None:
-                ret = inspect.getfile(obj)
-            return ret
-    except:
-        raise IOError(f"Cannot get the source file of {obj}. This is possibly because of you are running Taichi in an environment in which Taichi's own inspect module cannot find the source file. Please report an issue to help us fix this problem: https://github.com/taichi-dev/taichi/issues")
+    with _InspectContextManager():
+        ret = inspect.getsourcefile(obj)
+        if ret is None:
+            ret = inspect.getfile(obj)
+        return ret
 
 import sourceinspect
 #getsourcefile = sourceinspect.getsourcefile

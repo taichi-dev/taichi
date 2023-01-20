4e0060c93161580890f37860b35811ff56b1a0c8
lin-hitonami <linjiang@taichi.graphics>
2023-01-20 14:05:58 +0800

    [Error] Remove deprecations in taichi/__init__.py in v1.6.0

--- tests/python/test_deprecation.py
+++ tests/python/test_deprecation.py
@@ -135,3 +135,12 @@ def test_incomplete_info_rwtexture():
             for i, j in ti.ndrange(n, n):
                 ret = ti.cast(1, ti.f32)
                 tex.store(ti.Vector([i, j]), ti.Vector([ret, 0.0, 0.0, 0.0]))
+
+
+@test_utils.test()
+def test_deprecation_in_taichi_init_py():
+    with pytest.warns(
+            DeprecationWarning,
+            match=
+            "ti.SOA is deprecated, and it will be removed in Taichi v1.6.0."):
+        ti.SOA

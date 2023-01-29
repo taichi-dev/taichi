93f81b37573410d87b46235c75449af9f28b45e3
Lin Jiang <linjiang@taichi.graphics>
2023-01-29 10:56:31 +0800

    [Error] Allow IfExp on matrices when the condition is scalar (#7241)
    Issue: #7189
    
    ### Brief Summary

--- tests/python/test_if.py
+++ tests/python/test_if.py
@@ -5,7 +5,7 @@ from tests import test_utils
 
 
 @test_utils.test()
-def test_ifexpr():
+def test_ifexpr_vector():
     n_grids = 10
 
     g_v = ti.Vector.field(3, float, (n_grids, n_grids, n_grids))
@@ -20,3 +20,20 @@ def test_ifexpr():
     with pytest.raises(ti.TaichiSyntaxError,
                        match='Please use "ti.select" instead.'):
         func()
+
+
+@test_utils.test()
+def test_ifexpr_scalar():
+    n_grids = 10
+
+    g_v = ti.Vector.field(3, float, (n_grids, n_grids, n_grids))
+    g_m = ti.field(float, (n_grids, n_grids, n_grids))
+
+    @ti.kernel
+    def func():
+        for I in ti.grouped(g_m):
+            cond = (I[0] < 3) and (g_v[I][0] < 0) or (I[0] > n_grids - 3) and (
+                g_v[I][0] > 0)
+            g_v[I] = 0 if cond else g_v[I]
+
+    func()

cc2ad9ea8f5e64cb21d9d59f1ce21f1dbd1b7362
Proton <feisuzhu@163.com>
2023-02-02 14:28:36 +0800

    why fetching?

--- .github/workflows/scripts/build.py
+++ .github/workflows/scripts/build.py
@@ -155,8 +155,6 @@ def build_wheel(python: Command, pip: Command) -> None:
     Build the Taichi wheel
     '''
     pip.install('-r', 'requirements_dev.txt')
-    git.config('--global', '--add', 'safe.directory', os.getcwd())
-    git.fetch('origin', 'master', '--tags')
     proj = os.environ.get('PROJECT_NAME', 'taichi')
     proj_tags = []
     extra = []

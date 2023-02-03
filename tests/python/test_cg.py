992ce1107d7f592a2da40dff74cc6a875c4de8c8
FantasyVR <y2505418927@gmail.com>
2023-02-03 11:10:13 +0800

    add test

---
+++ tests/python/test_cg.py
@@ -0,0 +1,39 @@
+import numpy as np
+import pytest
+
+import taichi as ti
+from tests import test_utils
+
+
+@pytest.mark.parametrize("ti_dtype", [ti.f32, ti.f64])
+@test_utils.test(arch=ti.cpu)
+def test_cg(ti_dtype):
+    n = 10
+    A = np.random.rand(n, n)
+    A_psd = np.dot(A, A.transpose())
+    Abuilder = ti.linalg.SparseMatrixBuilder(n,
+                                             n,
+                                             max_num_triplets=300,
+                                             dtype=ti_dtype)
+    np_dtype = np.float32 if ti_dtype == ti.f32 else np.float64
+    b = np.ndarray(dtype=np_dtype, shape=n)
+    x0 = np.ndarray(dtype=np_dtype, shape=n)
+
+    @ti.kernel
+    def fill(Abuilder: ti.types.sparse_matrix_builder(),
+             InputArray: ti.types.ndarray(), b: ti.types.ndarray()):
+        for i, j in ti.ndrange(n, n):
+            Abuilder[i, j] += InputArray[i, j]
+        for i in range(n):
+            b[i] = i + 1
+
+    fill(Abuilder, A_psd, b)
+    A = Abuilder.build(dtype=ti_dtype)
+    cg = ti.linalg.CG(A, b, x0, max_iter=50, atol=1e-6)
+    x, exit_code = cg.solve()
+
+    res = np.linalg.solve(A_psd, b)
+
+    assert exit_code == True
+    for i in range(n):
+        assert x[i] == test_utils.approx(res[i], rel=1.0)

9f464ea78f065bbf7a9cc458ad3825cb14ce52c9
PGZXB <pgzxb@qq.com>
2023-02-03 19:38:00 +0800

    Add class TaichiLLVMContext

--- taichi/codegen/codegen.h
+++ taichi/codegen/codegen.h
@@ -8,10 +8,10 @@
 #include "llvm/IR/Module.h"
 #include "taichi/codegen/llvm/codegen_llvm.h"
 #include "taichi/runtime/llvm/launch_arg_info.h"
-#include "taichi/runtime/llvm/llvm_context.h"
 #include "taichi/codegen/llvm/llvm_codegen_utils.h"
 #endif
 namespace taichi::lang {
+class TaichiLLVMContext;
 
 /*
  [Note] Codegen of LLVM-based backends

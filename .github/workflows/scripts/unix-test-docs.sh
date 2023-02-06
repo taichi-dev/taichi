8173a17fba15352aea47bd1f2fb0758fcf48a851
Proton <feisuzhu@163.com>
2023-02-06 16:50:58 +0800

    actions yml

---
+++ .github/workflows/scripts/unix-test-docs.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -ex
+
+. $(dirname $0)/common-utils.sh
+
+export PYTHONUNBUFFERED=1
+
+export TI_CI=1
+export LD_LIBRARY_PATH=$PWD/build/:$LD_LIBRARY_PATH
+export TI_OFFLINE_CACHE_FILE_PATH=$PWD/.cache/taichi
+
+setup_python
+python3 -m pip install dist/*.whl
+
+export PATH=$PATH:$HOME/.local/bin
+python3 -m pip install -r requirements_test.txt
+
+cat docs/cover-in-ci.lst | xargs pytest -v -n 8

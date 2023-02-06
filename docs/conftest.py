6d1ccd0ba25a524d9f8ffc29ec2bc48e45d690fe
Proton <feisuzhu@163.com>
2023-02-06 16:23:10 +0800

    [doc] [ci] Cover code snippets in docs

--- docs/conftest.py
+++ docs/conftest.py
@@ -1,9 +1,17 @@
-# content of conftest.py
+import linecache
+import sys
+import uuid
+import warnings
 from dataclasses import dataclass
+from functools import wraps
 from itertools import count
+from typing import List, Optional
 
 import marko
 import pytest
+from pytest import ExceptionInfo
+
+import taichi as ti
 
 SANE_LANGUAGE_TAGS = {
     'python',
@@ -24,11 +32,76 @@ SANE_LANGUAGE_TAGS = {
     'Gherkin',
 }
 
+PRELUDES = {}
+
+PRELUDES['init'] = '''
+import taichi as ti
+import numpy as np
+import math
+import random
+import torch
+
+ti.init()
+i, j, k = (0, 0, 0)
+N = 16
+M = 8
+
+'''
+
+
+def hook(module, name=None):
+    def inner(hooker):
+        funcname = name or hooker.__name__
+        hookee = getattr(module, funcname)
+
+        @wraps(hookee)
+        def real_hooker(*args, **kwargs):
+            return hooker(hookee, *args, **kwargs)
+
+        real_hooker.orig = hookee
+        setattr(module, funcname, real_hooker)
+        return real_hooker
+
+    return inner
+
+
+@hook(ti.GUI)
+def show(orig, self, *args, **kwargs):
+    if not self.running:
+        self.running = False
+        self.close()
+        return
+
+    self._frames_remaining -= 1
+
+    return orig(self, *args, *kwargs)
+
+
+@hook(ti.ui.Window)
+def show(orig, self, *args, **kwargs):
+    if not self.running:
+        self.running = False
+        self.close()
+        return
+
+    self._frames_remaining -= 1
+
+    return orig(self, *args, *kwargs)
+
+
+ti.GUI._frames_remaining = 10
+ti.GUI.running = property(lambda self: self._frames_remaining <= 0)
+ti.ui.Window._frames_remaining = 10
+ti.ui.Window.running = property(lambda self: self._frames_remaining <= 0)
+
 
 @dataclass
 class PythonSnippet:
     name: str
     code: str
+    skip: Optional[str]
+    known_error: bool = False
+    preludes: Optional[List[str]] = None
 
 
 def pytest_collect_file(parent, file_path):
@@ -40,7 +113,7 @@ class MarkdownFile(pytest.File):
     def collect(self):
         doc = marko.parse(self.path.read_text())
         codes = list(self.extract_fenced_code_blocks(doc))
-        bad_tags = set(c.lang for name, c in codes) - SANE_LANGUAGE_TAGS
+        bad_tags = set(c.lang for _, c in codes) - SANE_LANGUAGE_TAGS
         if bad_tags:
             raise ValueError(
                 f"Invalid language tag {bad_tags} in markdown file")
@@ -49,10 +122,9 @@ class MarkdownFile(pytest.File):
         for name, c in codes:
             if not c.lang == 'python':
                 continue
-            extra = c.extra.split()
+            extra = dict(
+                (v.split(':', 1) + [None])[:2] for v in c.extra.split())
             code = c.children[0].children
-            # if 'ci' not in extra:
-            #     continue
             if 'cont' in extra:
                 assert spec is not None
                 spec.code += code
@@ -61,7 +133,16 @@ class MarkdownFile(pytest.File):
                     yield MarkdownItem.from_parent(self,
                                                    name=spec.name,
                                                    spec=spec)
-                spec = PythonSnippet(name=name, code=code)
+                preludes = extra.get('preludes')
+                if preludes is None:
+                    preludes = ['init']
+                else:
+                    preludes = preludes.split(',')
+                spec = PythonSnippet(name=name,
+                                     code=code,
+                                     skip=extra.get('skip-ci'),
+                                     known_error='known-error' in extra,
+                                     preludes=preludes)
 
         if spec is not None:
             yield MarkdownItem.from_parent(self, name=spec.name, spec=spec)
@@ -108,24 +189,39 @@ class MarkdownItem(pytest.Item):
         self.spec = spec
 
     def runtest(self):
-        return True
-        for name, value in sorted(self.spec.items()):
-            # Some custom test execution (dumb example follows).
-            if name != value:
-                raise MarkdownException(self, name, value)
+        spec = self.spec
+        if spec.skip is not None:
+            pytest.skip(spec.skip)
+
+        if spec.known_error:
+            warnings.warn('Known Error, please fix it')
+            pytest.skip('KnownError')
+            return
+
+        source = [PRELUDES[p] for p in spec.preludes] + [spec.code]
+        source = ''.join(source)
+        fn = f'<snippet:{uuid.uuid4()}>'
+        code = compile(source, fn, 'exec')
+        linecache.cache[fn] = (len(source), None,
+                               [f'{i}\n' for i in source.splitlines()], fn)
+        env = {}
+        try:
+            exec(code, env, env)
+        except Exception:
+            excinfo = sys.exc_info()
+            raise MarkdownException(excinfo)
 
     def repr_failure(self, excinfo):
-        """Called when self.runtest() raises an exception."""
         if isinstance(excinfo.value, MarkdownException):
-            return "\n".join([
-                "usecase execution failed",
-                "   spec failed: {1!r}: {2!r}".format(*excinfo.value.args),
-                "   no further details known at this point.",
-            ])
+            return super().repr_failure(ExceptionInfo(excinfo.value.excinfo))
+
+        return super().repr_failure(excinfo)
 
     def reportinfo(self):
-        return self.path, 0, f"usecase: {self.name}"
+        return self.path, 0, self.name
 
 
 class MarkdownException(Exception):
     """Custom exception for error reporting."""
+    def __init__(self, excinfo):
+        self.excinfo = excinfo

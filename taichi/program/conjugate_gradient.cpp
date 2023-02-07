8eccaa5791bb693b999520e7ad1d1d36dda04574
FantasyVR <y2505418927@gmail.com>
2023-02-07 15:14:34 +0800

    fix

--- taichi/program/conjugate_gradient.cpp
+++ taichi/program/conjugate_gradient.cpp
@@ -1,7 +1,7 @@
 #include "conjugate_gradient.h"
 
 namespace taichi::lang {
-void CUCG::init_solver(){
+void CUCG::init_solver() {
 #if defined(TI_WITH_CUDA)
   if (!CUBLASDriver::get_instance().is_loaded()) {
     bool load_success = CUBLASDriver::get_instance().load_cublas();
@@ -16,8 +16,9 @@ void CUCG::init_solver(){
 #endif
 }
 
-void CUCG::solve(Program *prog, const Ndarray &x, const Ndarray &b){
-  CuSparseMatrix &A = static_cast<CuSparseMatrix &>(A_);  
+void CUCG::solve(Program *prog, const Ndarray &x, const Ndarray &b) {
+#if defined(TI_WITH_CUDA)
+  CuSparseMatrix &A = static_cast<CuSparseMatrix &>(A_);
   size_t dX = prog->get_ndarray_data_ptr_as_int(&x);
   size_t db = prog->get_ndarray_data_ptr_as_int(&b);
   int m = A.num_rows();
@@ -25,13 +26,14 @@ void CUCG::solve(Program *prog, const Ndarray &x, const Ndarray &b){
   float *d_Ax = nullptr;
   float *d_r = nullptr;
   float *d_p = nullptr;
-  CUDADriver::get_instance().malloc((void**)&d_Ax, sizeof(float) * m);
-  CUDADriver::get_instance().malloc((void**)&d_r, sizeof(float) * m);
-  CUDADriver::get_instance().malloc((void**)&d_p, sizeof(float) * m);
+  CUDADriver::get_instance().malloc((void **)&d_Ax, sizeof(float) * m);
+  CUDADriver::get_instance().malloc((void **)&d_r, sizeof(float) * m);
+  CUDADriver::get_instance().malloc((void **)&d_p, sizeof(float) * m);
 
   // r = b
-  CUDADriver::get_instance().memcpy_device_to_device((void*)d_r, (void*)db, sizeof(float) * m);
-  
+  CUDADriver::get_instance().memcpy_device_to_device((void *)d_r, (void *)db,
+                                                     sizeof(float) * m);
+
   // Ax = A @ x
   A.spmv(dX, size_t(d_Ax));
 
@@ -42,20 +44,20 @@ void CUCG::solve(Program *prog, const Ndarray &x, const Ndarray &b){
   float r1 = 0.0f;
   CUBLASDriver::get_instance().cubSdot(handle, m, d_r, 1, d_r, 1, &r1);
 
-
   int k = 1;
   float alpha = 1.0, beta = 0.0, r0 = 0.0, dot = 0.0;
 
-  while(r1 > tol_ * tol_ && k <= max_iters_){
-    if (k > 1){
+  while (r1 > tol_ * tol_ && k <= max_iters_) {
+    if (k > 1) {
       // beta = r'_{k+1} @ r_{k+1} / r'_k @ r_k
       beta = r1 / r0;
       // p = r + beta * p
       CUBLASDriver::get_instance().cubSscal(handle, m, &beta, d_p, 1);
       CUBLASDriver::get_instance().cubSaxpy(handle, m, &alpha, d_r, 1, d_p, 1);
-    } else{
+    } else {
       // p = r
-      CUDADriver::get_instance().memcpy_device_to_device((void*)d_p, (void*)d_r, sizeof(float) * m);
+      CUDADriver::get_instance().memcpy_device_to_device(
+          (void *)d_p, (void *)d_r, sizeof(float) * m);
     }
 
     // Ap = A @ p
@@ -64,7 +66,8 @@ void CUCG::solve(Program *prog, const Ndarray &x, const Ndarray &b){
     CUBLASDriver::get_instance().cubSdot(handle, m, d_p, 1, d_Ax, 1, &dot);
     float a = r1 / dot;
     // x = x + a * p
-    CUBLASDriver::get_instance().cubSaxpy(handle, m, &a, d_p, 1, (float*)dX, 1);
+    CUBLASDriver::get_instance().cubSaxpy(handle, m, &a, d_p, 1, (float *)dX,
+                                          1);
     // r = r - a * Ap
     float na = -a;
     CUBLASDriver::get_instance().cubSaxpy(handle, m, &na, d_Ax, 1, d_r, 1);
@@ -75,8 +78,8 @@ void CUCG::solve(Program *prog, const Ndarray &x, const Ndarray &b){
       fmt::print("iter: {}, r1: {}\n", k, r1);
     k++;
   }
+#endif
 }
-
 
 std::unique_ptr<CUCG> make_cucg_solver(SparseMatrix &A,
                                        int max_iters,
@@ -84,4 +87,4 @@ std::unique_ptr<CUCG> make_cucg_solver(SparseMatrix &A,
                                        bool verbose) {
   return std::make_unique<CUCG>(A, max_iters, tol, verbose);
 }
-} // namespace taichi::lang
+}  // namespace taichi::lang

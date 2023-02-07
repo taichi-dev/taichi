1e76088d8b26f157d42e5ba9191be60e420ddd0d
FantasyVR <y2505418927@gmail.com>
2023-02-07 13:42:43 +0800

    correct cg

--- misc/cucg.py
+++ misc/cucg.py
@@ -18,12 +18,6 @@ def fill_K(K: ti.types.sparse_matrix_builder()):
     K[1, 0] += 1.0
     K[1, 1] += 3.0
 
-
-@ti.kernel 
-def fill_b(b: ti.types.ndarray()):
-    b[0] = 1.0
-    b[1] = 2.0
-
 @ti.kernel
 def print_x(x: ti.types.ndarray()):
     for i in range(2):
@@ -35,7 +29,10 @@ print(A)
 
 cg = ti.linalg.CG(A, b, x0, max_iter=50, atol=1e-6)
 
-fill_b(b)
+np_b = np.asarray([1.0, 2.0], dtype=np_type)
+b.from_numpy(np_b)
+print("b =", b.to_numpy())
+
 cg.solve_cu(x, b)
 
 print_x(x)
--- taichi/program/conjugate_gradient.cpp
+++ taichi/program/conjugate_gradient.cpp
@@ -9,75 +9,92 @@ void CUCG::init_solver(){
       TI_ERROR("Failed to load cublas library!");
     }
   }
-  // CUBLASDriver::get_instance().cubCreate(&handle);
+  CUBLASDriver::get_instance().cubCreate(&handle);
   // int version;
   // CUBLASDriver::get_instance().cubGetVersion(handle, &version);
   // fmt::print("CUBLAS version: {}\n", version);
 #endif
 }
 
+void print_array(float *arr, int n){
+  for(int i = 0; i < n; i++){
+    fmt::print("{} ", arr[i]);
+  }
+  fmt::print("\n");
+}
+
+void print_cuda_arr(float *arr, int n){
+  float *h_arr = new float[n];
+  CUDADriver::get_instance().memcpy_device_to_host((void*)h_arr, (void*)arr, sizeof(float) * n);
+  print_array(h_arr, n);
+  delete[] h_arr;
+}
 
 void CUCG::solve(Program *prog, const Ndarray &x, const Ndarray &b){
   CuSparseMatrix &A = static_cast<CuSparseMatrix &>(A_);  
   size_t dX = prog->get_ndarray_data_ptr_as_int(&x);
-  size_t db = prog->get_ndarray_data_ptr_as_int(&x);
+  size_t db = prog->get_ndarray_data_ptr_as_int(&b);
   int m = A.num_rows();
 
-  void *d_Ax = nullptr;
-  void *d_r = nullptr;
-  void *d_p = nullptr, *d_p_next=nullptr;
+  float *d_Ax = nullptr;
+  float *d_r = nullptr;
+  float *d_p = nullptr;
   CUDADriver::get_instance().malloc((void**)&d_Ax, sizeof(float) * m);
   CUDADriver::get_instance().malloc((void**)&d_r, sizeof(float) * m);
   CUDADriver::get_instance().malloc((void**)&d_p, sizeof(float) * m);
-  CUDADriver::get_instance().malloc((void**)&d_p_next, sizeof(float) * m);
 
-  // // compute d_Ax = A @ x
-  A.spmv(dX, size_t(d_Ax));
-
-  // // compute d_r = b - d_Ax
+  // r = b
   CUDADriver::get_instance().memcpy_device_to_device((void*)d_r, (void*)db, sizeof(float) * m);
   
-  // float alpha = -1.0f;
-  // CUBLASDriver::get_instance().cbSaxpy(handle, m, &alpha, (float*)d_Ax, 1, (float*)d_r, 1);
+  fmt::print("b: \n");
+  print_cuda_arr(d_r, m);
 
-  // float r_norm = 0.0f;
-  // CUBLASDriver::get_instance().cbSnrm2(handle, m, (float*)d_r, 1, &r_norm);
+  // Ax = A @ x
+  A.spmv(dX, size_t(d_Ax));
 
-  // // if r_norm is sufficiently small, return
-  // if (r_norm < 1.0e-6){
-  //   return;
-  // }
+  fmt::print("Ax: \n");
+  print_cuda_arr(d_Ax, m);
 
-  // // p0 = r0
-  // CUDADriver::get_instance().memcpy_device_to_device((void*)d_p, (void*)d_r, sizeof(float) * m);
+  // r = r - Ax = b - Ax
+  float alpham1 = -1.0f;
+  CUBLASDriver::get_instance().cubSaxpy(handle, m, &alpham1, d_Ax, 1, d_r, 1);
 
-  // int k=0;
-  // while(k < max_iters_){
-  //   // sqrt(r^T @ r)
-  //   CUBLASDriver::get_instance().cbSnrm2(handle, m, (float*)d_r, 1, &r_norm);
-  //   // Ap 
-  //   A.spmv(size_t(d_p), size_t(d_Ax));
-  //   // p^T @ Ap
-  //   float pAp = 0.0f;
-  //   CUBLASDriver::get_instance().cbSdot(handle, m, (float*)d_p, 1, (float*)d_Ax, 1, &pAp);
-  //   alpha = r_norm * r_norm / pAp;
-  //   // x = x + alpha * p
-  //   CUBLASDriver::get_instance().cbSaxpy(handle, m, &alpha, (float*)d_p, 1, (float*)dX, 1);
-  //   // r = r - alpha * Ap
-  //   alpha = -alpha;
-  //   CUBLASDriver::get_instance().cbSaxpy(handle, m, &alpha, (float*)d_Ax, 1, (float*)d_r, 1);
-  //   // r^T @ r
-  //   float r_next_norm = 0.0f;
-  //   CUBLASDriver::get_instance().cbSnrm2(handle, m, (float*)d_r, 1, &r_next_norm);
-  //   // if r_k+1 is sufficiently small, return
-  //   if (r_next_norm < 1.0e-6) return;
-  //   float beta = r_next_norm * r_next_norm / r_norm * r_norm;
-  //   // p_next = r_next + beta * p
-  //   CUDADriver::get_instance().memcpy_device_to_device((void*)d_p_next, (void*)d_r, sizeof(float) * m);
-  //   CUBLASDriver::get_instance().cbSaxpy(handle, m, &beta, (float*)d_p, 1, (float*)d_p_next, 1);
-  //   CUDADriver::get_instance().memcpy_device_to_device((void*)d_p, (void*)d_p_next, sizeof(float) * m);
-  //   k++;
-  // }
+  float r1 = 0.0f;
+  CUBLASDriver::get_instance().cubSdot(handle, m, d_r, 1, d_r, 1, &r1);
+
+
+  int k = 1;
+  float alpha = 1.0, beta = 0.0, r0 = 0.0, dot = 0.0;
+
+  while(r1 > tol_ * tol_ && k <= max_iters_){
+    if (k > 1){
+      // beta = r'_{k+1} @ r_{k+1} / r'_k @ r_k
+      beta = r1 / r0;
+      // p = r + beta * p
+      CUBLASDriver::get_instance().cubSscal(handle, m, &beta, d_p, 1);
+      CUBLASDriver::get_instance().cubSaxpy(handle, m, &alpha, d_r, 1, d_p, 1);
+    } else{
+      // p = r
+      CUDADriver::get_instance().memcpy_device_to_device((void*)d_p, (void*)d_r, sizeof(float) * m);
+    }
+
+    // Ap = A @ p
+    A.spmv(size_t(d_p), size_t(d_Ax));
+    // dot = p @ Ap
+    CUBLASDriver::get_instance().cubSdot(handle, m, d_p, 1, d_Ax, 1, &dot);
+    float a = r1 / dot;
+    // x = x + a * p
+    CUBLASDriver::get_instance().cubSaxpy(handle, m, &a, d_p, 1, (float*)dX, 1);
+    // r = r - a * Ap
+    float na = -a;
+    CUBLASDriver::get_instance().cubSaxpy(handle, m, &na, d_Ax, 1, d_r, 1);
+    r0 = r1;
+    // r1 = r @ r
+    CUBLASDriver::get_instance().cubSdot(handle, m, d_r, 1, d_r, 1, &r1);
+
+    fmt::print("iter: {}, r1: {}\n", k, r1);
+    k++;
+  }
 }
 
 

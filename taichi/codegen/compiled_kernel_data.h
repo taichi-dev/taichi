b23f382d6c1672d7a79ec2c144c0cace3153e443
PGZXB <pgzxb@qq.com>
2023-02-09 19:56:18 +0800

    Introduce lang::CompiledKernelData

---
+++ taichi/codegen/compiled_kernel_data.h
@@ -0,0 +1,105 @@
+#pragma once
+
+#include <string>
+#include <memory>
+#include <algorithm>
+
+#include "taichi/rhi/arch.h"
+
+namespace taichi::lang {
+
+class CompiledKernelDataFile {
+ public:
+  static constexpr char kHeadStr[] = "TIC";
+  static constexpr std::size_t kHeadSize = std::size(kHeadStr);
+  static constexpr std::size_t kHashSize = 64;
+  enum class Err {
+    kNoError,
+    kNotTicFile,
+    kCorruptedFile,
+    kOutOfMemeory,
+    kIOStreamError,
+  };
+
+  Err dump(std::ostream &os);
+  Err load(std::istream &is);
+
+  CompiledKernelDataFile() {
+    std::copy(kHeadStr, kHeadStr + kHeadSize, head_);
+  }
+
+  Arch &arch() {
+    return arch_;
+  }
+
+  std::string &metadata() {
+    return metadata_;
+  }
+
+  std::string &src_code() {
+    return src_code_;
+  }
+
+ private:
+  bool update_hash();
+
+  char head_[kHeadSize];
+  Arch arch_;
+  std::string metadata_;
+  std::string src_code_;
+  std::string hash_;
+};
+
+class CompiledKernelData {
+ public:
+  enum class Err {
+    kNoError = 0,
+    kNotTicFile,
+    kCorruptedFile,
+    kParseMetadataFailed,
+    kParseSrcCodeFailed,
+    kSerMetadataFailed,
+    kSerSrcCodeFailed,
+    kIOStreamError,
+    kOutOfMemeory,
+    kTiWithoutLLVM,
+    kTiWithoutSpirv,
+    kUnknown,
+  };
+
+  CompiledKernelData() = default;
+  CompiledKernelData(const CompiledKernelData &) = delete;
+  CompiledKernelData &operator=(const CompiledKernelData &) = delete;
+  virtual ~CompiledKernelData() = default;
+
+  virtual std::size_t size() const = 0;
+
+  Err load(std::istream &is);
+  Err dump(std::ostream &os) const;
+
+  virtual std::unique_ptr<CompiledKernelData> clone() const = 0;
+
+  virtual Err debug_print(std::ostream &os) const {
+    return dump(os);
+  }
+
+  virtual Err check() const {
+    return Err::kNoError;
+  }
+
+  static std::unique_ptr<CompiledKernelData> load(std::istream &is,
+                                                  Err *p_err = nullptr);
+
+ protected:
+  virtual Err load_impl(const CompiledKernelDataFile &file) = 0;
+  virtual Err dump_impl(CompiledKernelDataFile &file) const = 0;
+
+ private:
+  using Creator = std::unique_ptr<CompiledKernelData>();
+  static Creator *const llvm_creator;
+  static Creator *const spriv_creator;
+
+  static std::unique_ptr<CompiledKernelData> create(Arch arch, Err &err);
+};
+
+}  // namespace taichi::lang

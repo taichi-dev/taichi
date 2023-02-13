674662bc6cc52124db81b271183776b9300360e9
Cheng Cao <bobcaocheng@gmail.com>
2023-02-13 12:27:00 -0800

    mac changes

--- taichi/runtime/program_impls/vulkan/vulkan_program.cpp
+++ taichi/runtime/program_impls/vulkan/vulkan_program.cpp
@@ -96,6 +96,10 @@ void VulkanProgramImpl::materialize_runtime(MemoryPool *memory_pool,
   GLFWwindow *glfw_window = nullptr;
 
   if (window_system::glfw_context_acquire()) {
+#ifdef __APPLE__
+    glfwInitVulkanLoader(vkGetInstanceProcAddr);
+#endif
+
     // glfw init success
     glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
     glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);

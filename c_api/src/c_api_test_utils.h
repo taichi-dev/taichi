8b19aa6944d78a0338ccc3f47bf8aee0b8a8b458
Zhanlue Yang <jim19930609@gmail.com>
2023-02-14 11:38:34 +0800

    [aot] Enable 16-bit dtype support for Taichi AOT (#7315)
    Issue: #
    
    ### Brief Summary

--- c_api/src/c_api_test_utils.h
+++ c_api/src/c_api_test_utils.h
@@ -9,5 +9,8 @@ TI_DLL_EXPORT void TI_API_CALL check_runtime_error(TiRuntime runtime);
 TI_DLL_EXPORT bool TI_API_CALL check_cuda_value(void *ptr, float value);
 TI_DLL_EXPORT bool TI_API_CALL check_cuda_value(void *ptr, double value);
 
+TI_DLL_EXPORT uint16_t to_float16(float in);
+TI_DLL_EXPORT float to_float32(uint16_t in);
+
 }  // namespace utils
 }  // namespace capi

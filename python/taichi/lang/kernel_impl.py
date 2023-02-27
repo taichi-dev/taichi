c3f446487bf67ebed0737c8391adb9df3f20cde9
PENGUINLIONG <admin@penguinliong.moe>
2023-02-27 18:38:11 +0800

    rw_texture

--- python/taichi/lang/kernel_impl.py
+++ python/taichi/lang/kernel_impl.py
@@ -387,8 +387,26 @@ class TaichiCallableTemplateMapper:
             # [Primitive arguments] Return the value
             return arg
         if isinstance(anno, texture_type.TextureType):
+            if not isinstance(arg, taichi.lang._texture.Texture):
+                raise TaichiRuntimeTypeError(
+                    f'Argument must be a texture, got {type(arg)}')
+            if arg.num_dims != anno.num_dimensions:
+                raise TaichiRuntimeTypeError(
+                    f'TextureType dimension mismatch: expected {anno.num_dimensions}, got {arg.num_dims}'
+                )
             return (arg.num_dims, )
         if isinstance(anno, texture_type.RWTextureType):
+            if not isinstance(arg, taichi.lang._texture.Texture):
+                raise TaichiRuntimeTypeError(
+                    f'Argument must be a texture, got {type(arg)}')
+            if arg.num_dims != anno.num_dimensions:
+                raise TaichiRuntimeTypeError(
+                    f'RWTextureType dimension mismatch: expected {anno.num_dimensions}, got {arg.num_dims}'
+                )
+            if arg.fmt != anno.fmt:
+                raise TaichiRuntimeTypeError(
+                    f'RWTextureType data type mismatch: expected {anno.fmt}, got {arg.fmt}'
+                )
             # (penguinliong) '0' is the assumed LOD level. We currently don't
             # support mip-mapping.
             return arg.num_dims, arg.fmt, 0

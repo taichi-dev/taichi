b1b6e2f82633f6192b82a7ccc781f8ddf8307c42
lin-hitonami <linjiang@taichi.graphics>
2023-03-13 16:39:46 +0800

    Update base for Update on "[llvm] Compile the kernel arguments to a StructType"
    [ghstack-poisoned]

--- taichi/program/callable.cpp
+++ taichi/program/callable.cpp
@@ -33,6 +33,11 @@ int Callable::insert_texture_param(int total_dim) {
   return (int)parameter_list.size() - 1;
 }
 
+int Callable::insert_pointer_param(const DataType &dt) {
+  parameter_list.emplace_back(dt->get_compute_type(), /*is_array=*/true);
+  return (int)parameter_list.size() - 1;
+}
+
 int Callable::insert_rw_texture_param(int total_dim, BufferFormat format) {
   // FIXME: we shouldn't abuse is_array for texture parameters
   parameter_list.emplace_back(PrimitiveType::f32, /*is_array=*/true, 0,

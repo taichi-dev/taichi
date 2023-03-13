a2348e42b18c565db518b6a2f2f338f7df1203c4
PENGUINLIONG <admin@penguinliong.moe>
2023-03-14 07:07:13 +0800

    Interface for Taichi Unity glue extension

--- .github/workflows/scripts/aot-demo.sh
+++ .github/workflows/scripts/aot-demo.sh
@@ -8,8 +8,8 @@ export TI_CI=1
 export TAICHI_AOT_DEMO_URL=https://github.com/taichi-dev/taichi-aot-demo
 export TAICHI_AOT_DEMO_BRANCH=master
 
-export TAICHI_UNITY2_URL=https://github.com/taichi-dev/taichi-unity2
-export TAICHI_UNITY2_BRANCH=main
+export TAICHI_UNITY2_URL=https://github.com/PENGUINLIONG/taichi-unity2
+export TAICHI_UNITY2_BRANCH=extend-unity1
 
 export TAICHI_UNITY_EXAMPLE_URL=https://github.com/taichi-dev/Taichi-UnityExample
 export TAICHI_UNITY_EXAMPLE_BRANCH=main

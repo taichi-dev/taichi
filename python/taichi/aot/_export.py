ba8bbc22053fcddc4a8f1982c060f43125f5fcbe
PENGUINLIONG <admin@penguinliong.moe>
2023-03-16 12:58:38 +0800

    [aot] Taichi kernel AOT command (#7565)
    Issue: #
    
    ### Brief Summary
    
    This PR intruduces a `ti` command to build Taichi AOT module from the
    shell. To build an AOT module, you no longer need to construct a
    `ti.aot.Module` and add the kernels with `add_kernel` manually. Instead,
    after your `ti.init` you mark the kernels to be exported to the module
    with `@ti.aot.export` and execute `ti module build YOUR_AOT_SCRIPT.py
    [-o OUTPUT_DIR_OR_TCM_FILE_NAME]` in your shell. If you need to specify
    extra device capabilities, assign the list of capabilities to global
    variable `REQUIRED_CAPS` in your AOT script. The builder will echo the
    names of kernels and device capabilities from your input.
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ python/taichi/aot/_export.py
@@ -0,0 +1,9 @@
+_aot_kernels = []
+
+
+def export(f):
+    assert hasattr(f,
+                   "_is_wrapped_kernel"), "Only Taichi kernels can be exported"
+    out = f
+    _aot_kernels.append(out)
+    return out

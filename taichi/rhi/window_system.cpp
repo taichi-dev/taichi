e17458d9f5e57a686e1a863c25ba19fb3e347ca1
Proton <feisuzhu@163.com>
2023-03-22 10:34:23 +0800

    Update base for Update on "[ci] Rewrite Dockerfiles"
    [ghstack-poisoned]

--- taichi/rhi/window_system.cpp
+++ taichi/rhi/window_system.cpp
@@ -53,8 +53,7 @@ bool glfw_context_acquire() {
 void glfw_context_release() {
   std::lock_guard lg(glfw_state.mutex);
   if (glfw_state.glfw_ref_count <= 0) {
-    assert(glfw_state.glfw_ref_count == 0 && "GLFW ref count underflow");
-    return;
+    assert(false && "GLFW ref count underflow");
   }
   glfw_state.glfw_ref_count--;
   if (glfw_state.glfw_ref_count == 0) {

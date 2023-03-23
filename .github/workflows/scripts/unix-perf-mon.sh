d534e06c955ab0f345e3437ba35595294501ddfc
Zhanlue Yang <jim19930609@gmail.com>
2023-03-23 14:02:38 +0800

    [build] Setup Android SDK environment for performance bot (#7635)
    Issue: #
    
    ### Brief Summary

--- .github/workflows/scripts/unix-perf-mon.sh
+++ .github/workflows/scripts/unix-perf-mon.sh
@@ -7,10 +7,11 @@ export PYTHONUNBUFFERED=1
 
 [[ "$IN_DOCKER" == "true" ]] && cd taichi
 
-python3 .github/workflows/scripts/build.py --write-env=/tmp/ti-env.sh
+python3 .github/workflows/scripts/build.py android --write-env=/tmp/ti-env.sh
 . /tmp/ti-env.sh
 
 python -m pip install dist/*.whl
+
 git clone https://github.com/taichi-dev/taichi_benchmark
 cd taichi_benchmark
 pip install -r requirements.txt

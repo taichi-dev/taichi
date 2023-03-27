a1f76ceac2fdb6bfead450a2395ce8797271efae
Qian Bao <houkensjtu@gmail.com>
2023-03-27 11:57:37 +0800

    Implement write_vtk() to export a taichi scalar field.

---
+++ python/taichi/tools/vtk.py
@@ -0,0 +1,24 @@
+import numpy as np
+from pyevtk.hl import gridToVTK
+
+def write_vtk(scalar_field, filename):
+    scalar_field_np = scalar_field.to_numpy()
+    field_shape = scalar_field_np.shape
+    dimensions = len(field_shape)
+
+    if dimensions not in (2, 3):
+        raise ValueError("The input field must be a 2D or 3D scalar field.")
+
+    if dimensions == 2:
+        scalar_field_np = scalar_field_np[np.newaxis, :, :]
+        zcoords = np.array([0, 1])
+    elif dimensions == 3:
+        zcoords = np.arange(0, field_shape[2])
+    gridToVTK(
+        filename,
+        x=np.arange(0,field_shape[0]),
+        y=np.arange(0,field_shape[1]),
+        z=zcoords,
+        cellData={filename: scalar_field_np})
+
+__all__ = ['write_vtk']    

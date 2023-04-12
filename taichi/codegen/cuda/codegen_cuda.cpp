20882665e4f82affeaf900cd3bb92f6d863f09f0
lgystoic <linggaoyuan@gmail.com>
2023-04-12 12:38:19 +0800

    fix: cast cuda u64 ptr to i64 ptr

--- taichi/codegen/cuda/codegen_cuda.cpp
+++ taichi/codegen/cuda/codegen_cuda.cpp
@@ -279,11 +279,16 @@ class TaskCodeGenCUDA : public TaskCodeGenLLVM {
       builder->CreateStore(output, frac_ptr);
       llvm_val[stmt] = res;
     } else if (op == UnaryOpType::popcnt) {
+      if(input_taichi_type->is_primitive(PrimitiveTypeID::u64)) {
+        auto i64_input = builder->CreateBitCast(
+          input, llvm::Type::getInt64PtrTy(*tlctx->get_this_thread_context()));
+      }
       if (input_taichi_type->is_primitive(PrimitiveTypeID::i32) ||
           input_taichi_type->is_primitive(PrimitiveTypeID::u32)) {
         llvm_val[stmt] = call("__nv_popc", input);
-      } else if (input_taichi_type->is_primitive(PrimitiveTypeID::i64) ||
-                 input_taichi_type->is_primitive(PrimitiveTypeID::u64)) {
+      } else if (input_taichi_type->is_primitive(PrimitiveTypeID::u64)) {
+        llvm_val[stmt] = call("__nv_popcll", i64_input);
+      } else if (input_taichi_type->is_primitive(PrimitiveTypeID::i64)) {
         llvm_val[stmt] = call("__nv_popcll", input);
       } else {
         TI_NOT_IMPLEMENTED

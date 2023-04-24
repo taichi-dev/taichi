607cd4b4aaf6d158f03abbde50da92be71429ae0
jim19930609 <jim19930609@gmail.com>
2023-04-24 15:08:39 +0800

    add test

---
+++ tests/python/test_ad_tensor.py
@@ -0,0 +1,34 @@
+import functools
+
+import numpy as np
+import pytest
+
+import taichi as ti
+from tests import test_utils
+
+has_autograd = False
+
+try:
+    import autograd.numpy as np
+    from autograd import grad
+
+    has_autograd = True
+except:
+    pass
+
+
+@test_utils.test()
+def test_ad_tensor_store_load():
+    x = ti.Vector.field(4, dtype=ti.f32, shape=(), needs_grad=True)
+    y = ti.Vector.field(4, dtype=ti.f32, shape=(), needs_grad=True)
+
+    @ti.kernel
+    def test(tmp: ti.f32):
+        b = ti.Vector([tmp, tmp, tmp, tmp])
+        b[0] = tmp * 4
+        y[None] = b * x[None]
+
+    y.grad.fill(2.0)
+    test.grad(10)
+
+    assert (x.grad.to_numpy() == [80.0, 20.0, 20.0, 20.0]).all()

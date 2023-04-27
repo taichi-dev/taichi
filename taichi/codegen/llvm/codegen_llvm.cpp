73f3f819f5b43d4ae11267c3fbad0993b930e05e
listerily <listerily@outlook.com>
2023-04-27 15:03:11 +0800

    fix: while condition

--- taichi/codegen/llvm/codegen_llvm.cpp
+++ taichi/codegen/llvm/codegen_llvm.cpp
@@ -1090,7 +1090,7 @@ void TaskCodeGenLLVM::visit(WhileControlStmt *stmt) {
       BasicBlock::Create(*llvm_context, "after_break", func);
   TI_ASSERT(current_while_after_loop);
   llvm::Value *cond =
-      builder->CreateTrunc(builder->CreateIsNotNull(llvm_val[stmt->cond]),
+      builder->CreateTrunc(builder->CreateIsNull(llvm_val[stmt->cond]),
                            tlctx->get_data_type(PrimitiveType::u1));
   builder->CreateCondBr(cond, current_while_after_loop, after_break);
   builder->SetInsertPoint(after_break);

8d4501ea5ab544d0d020e23dce5a57b6f37ab090
listerily <listerily@outlook.com>
2023-05-06 17:03:27 +0800

    revert

--- taichi/ir/frontend_ir.cpp
+++ taichi/ir/frontend_ir.cpp
@@ -357,8 +357,8 @@ void BinaryOpExpression::type_check(const CompileConfig *config) {
                                   !is_integral(rhs_type.get_element_type())))
     error();
   if (binary_is_logical(type) &&
-      (is_tensor_op || lhs_type != PrimitiveType::u1 ||
-       rhs_type != PrimitiveType::u1))
+      (lhs_type.get_element_type() != PrimitiveType::u1 ||
+       rhs_type.get_element_type() != PrimitiveType::u1))
     error();
   if (is_comparison(type) || binary_is_logical(type)) {
     ret_type = make_dt(PrimitiveType::u1);

d6e92cbfc71f7c1991a237fef7d5285e33b6eb67
jim19930609 <jim19930609@gmail.com>
2023-05-06 14:53:57 +0800

    [Lang] Migrate irpass::scalarize() after irpass::detect_read_only()

--- taichi/transforms/compile_to_offloads.cpp
+++ taichi/transforms/compile_to_offloads.cpp
@@ -150,14 +150,6 @@ void compile_to_offloads(IRNode *ir,
   irpass::full_simplify(ir, config, {false, /*autodiff_enabled*/ false});
   print("Simplified III");
   irpass::analysis::verify(ir);
-
-  if (config.real_matrix_scalarize) {
-    irpass::scalarize(ir);
-
-    // Remove redundant MatrixInitStmt inserted during scalarization
-    irpass::full_simplify(ir, config, {false, /*autodiff_enabled*/ false});
-    print("Scalarized");
-  }
 }
 
 void offload_to_executable(IRNode *ir,

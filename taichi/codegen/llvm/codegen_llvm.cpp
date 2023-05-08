3b2bfc9645de069d17124c0fe49e75e3a4b161be
listerily <listerily@outlook.com>
2023-05-08 14:11:07 +0800

    fix: type check of SNode

--- taichi/codegen/llvm/codegen_llvm.cpp
+++ taichi/codegen/llvm/codegen_llvm.cpp
@@ -589,17 +589,8 @@ void TaskCodeGenLLVM::visit(BinaryOpStmt *stmt) {
                builder->CreateGlobalStringPtr(stmt->tb));
 #endif
     } else {
-      //      TI_INFO("Create Add, lhs bits = " +
-      //      std::to_string(llvm_val[stmt->lhs]->getType()->getPrimitiveSizeInBits())
-      //      + " rhs bits = " +
-      //      std::to_string(llvm_val[stmt->rhs]->getType()->getPrimitiveSizeInBits()));
-      //      TI_INFO("Create Add, ret bits = " + stmt->ret_type.to_string());
       llvm_val[stmt] =
           builder->CreateAdd(llvm_val[stmt->lhs], llvm_val[stmt->rhs]);
-      //      llvm_val[stmt] = builder->CreateAdd(
-      //          llvm_val[stmt->lhs],
-      //          builder->CreateZExt(llvm_val[stmt->rhs],
-      //                              tlctx->get_data_type(stmt->ret_type)));
     }
   } else if (op == BinaryOpType::sub) {
     if (is_real(stmt->ret_type.get_element_type())) {
--- taichi/transforms/type_check.cpp
+++ taichi/transforms/type_check.cpp
@@ -118,6 +118,8 @@ class TypeCheck : public IRVisitor {
     } else if (stmt->op_type == SNodeOpType::allocate) {
       stmt->ret_type = PrimitiveType::gen;
       stmt->ret_type.set_is_pointer(true);
+    } else if (stmt->op_type == SNodeOpType::is_active) {
+      stmt->ret_type = PrimitiveType::u1;
     } else {
       stmt->ret_type = PrimitiveType::i32;
     }
@@ -337,8 +339,6 @@ class TypeCheck : public IRVisitor {
     }
 
     if (stmt->lhs->ret_type != stmt->rhs->ret_type) {
-      //      TI_INFO("NOT EQUAL: lhs=" + stmt->lhs->ret_type.to_string() + ",
-      //      rhs=" + stmt->rhs->ret_type.to_string());
       DataType ret_type;
       if (is_shift_op(stmt->op_type)) {
         // shift_ops does not follow the same type promotion rule as numerical
@@ -357,7 +357,6 @@ class TypeCheck : public IRVisitor {
         }
       } else {
         ret_type = promoted_type(stmt->lhs->ret_type, stmt->rhs->ret_type);
-        //        TI_INFO("Entered Here #1, promoted=" + ret_type.to_string());
       }
 
       if (ret_type != stmt->lhs->ret_type) {
@@ -369,7 +368,6 @@ class TypeCheck : public IRVisitor {
         // promote rhs
         auto cast_stmt = insert_type_cast_before(stmt, stmt->rhs, ret_type);
         stmt->rhs = cast_stmt;
-        //        TI_INFO("Promoted RHS");
       }
     }
     bool matching = true;

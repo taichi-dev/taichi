1b3ef873b1465740feebf652f8f2ad951c1f1292
Tianyi Liu <tyi.liu@outlook.com>
2023-05-09 22:21:13 +0800

    Fix a typo

--- python/taichi/_kernels.py
+++ python/taichi/_kernels.py
@@ -133,16 +133,14 @@ def vector_to_image(mat: template(), arr: ndarray_type.ndarray()):
 
 @kernel
 def tensor_to_tensor(tensor: template(), other: template()):
+    # assumes that tensor and other have the same shape
+    shape = static(tensor.shape)
     tensor_offset = static(tensor.snode.ptr.offset)
-    tensor_shape = static(tensor.shape)
-    tensor_offset_new = static([0] * len(tensor_shape) if len(tensor_offset) == 0 else tensor_offset)
-
+    tensor_offset_new = static([0] * len(shape) if len(tensor_offset) == 0 else tensor_offset)
     other_offset = static(other.snode.ptr.offset)
-    other_shape = static(other.shape)
-    other_offset_new = static([0] * len(other_shape) if len(other_offset) == 0 else other_offset)
+    other_offset_new = static([0] * len(shape) if len(other_offset) == 0 else other_offset)
 
-    for I in grouped(ndrange(*tensor_shape)):
-        print("index ", I)
+    for I in grouped(ndrange(*shape)):
         tensor[I + tensor_offset_new] = other[I + other_offset_new]
 
 

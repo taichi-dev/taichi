a9fcf0062914b8aebe6fce4d6af613c9bff7bf78
Tianyi Liu <tyi.liu@outlook.com>
2023-05-10 16:47:30 +0800

    Update test for set_image

--- tests/python/test_gui.py
+++ tests/python/test_gui.py
@@ -31,19 +31,47 @@ def test_save_image_without_window(dtype):
         assert delta == 0, "Expected image difference to be 0 but got {} instead.".format(delta)
 
 
-@pytest.mark.parametrize("fast_gui", [True, False])
-@pytest.mark.parametrize("dtype, color", [(ti.u8, 128), (ti.f32, 0.5), (ti.f64, 0.5)])
-@pytest.mark.parametrize("offset", [(-299, -299), (-150, -150), (0, 0), (150, 150), (299, 299)])
+@pytest.mark.parametrize("vector_field", [True, False])
+@pytest.mark.parametrize("dtype", [ti.u8,ti.f32,ti.f64])
+@pytest.mark.parametrize("color", [0, 32, 64, 128, 255])
+@pytest.mark.parametrize("offset", [(-150, -150), (0, 0), (150, 150)])
 @test_utils.test(arch=get_host_arch_list())
-def test_set_image_with_offset(fast_gui, offset, dtype, color):
+def test_set_image_with_offset(vector_field, dtype, color, offset):
     n = 300
     shape = (n, n)
-    if fast_gui is True or dtype is ti.f64:
-        img = ti.Vector.field(dtype=dtype, n=3, shape=shape, offset=offset)
-    else:
-        img = ti.field(dtype=dtype, shape=shape, offset=offset)
-    img.fill(color)
 
-    gui = ti.GUI(name="test", res=shape, show_gui=False, fast_gui=fast_gui)
+    img = ti.Vector.field(dtype=dtype, n=3, shape=shape, offset=offset) if vector_field else ti.field(dtype=dtype, shape=shape, offset=offset)
+    img.fill(color if dtype is ti.u8 else color * 1.0 / 255)
+
+    gui = ti.GUI(name="test", res=shape, show_gui=False, fast_gui=False)
     gui.set_image(img)
-    gui.show()
+
+    image_path = test_utils.make_temp_file(suffix=".png")
+    gui.show(image_path)
+    image = ti.tools.imread(image_path)
+    delta = (image - color).sum()
+    assert delta == 0, "Expected image difference to be 0 but got {} instead.".format(delta)
+
+
+@pytest.mark.parametrize("channel", [3, 4])
+@pytest.mark.parametrize("dtype", [ti.u8, ti.f32, ti.f64])
+@pytest.mark.parametrize("color", [0, 32, 64, 128, 255])
+@pytest.mark.parametrize("offset", [(-150, -150), (0, 0), (150, 150)])
+@test_utils.test(arch=get_host_arch_list())
+def test_set_image_fast_gui_with_offset(channel, dtype, color, offset):
+    n = 300
+    shape = (n, n)
+
+    img = ti.Vector.field(dtype=dtype, n=channel, shape=shape, offset=offset)
+    img.fill(color if dtype is ti.u8 else color * 1.0 / 255)
+
+    gui = ti.GUI(name="test", res=shape, show_gui=False, fast_gui=True)
+    gui.set_image(img)
+    fast_image = gui.img
+
+    alpha = -16777216
+    from taichi._lib.utils import get_os_name # pylint: disable=C0415
+    rgb_color= (color << 16) + (color << 8) + color if ti.static(get_os_name() != "osx") else (color << 16) + (color << 8) + color + alpha
+    ground_truth = np.full(n * n, rgb_color, dtype=np.int32)
+
+    assert np.allclose(fast_image, ground_truth)

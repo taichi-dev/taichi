4727ee0975026b3fe5ac393deaf762ccd4a3b2ca
lin-hitonami <linjiang@taichi.graphics>
2023-06-12 14:55:54 +0800

    [Lang] Support struct arguments for real function
    ghstack-source-id: 95de0518986eb33108c46fbd918c37bbe3831f8a
    Pull Request resolved: https://github.com/taichi-dev/taichi/pull/8171

--- python/taichi/lang/expr.py
+++ python/taichi/lang/expr.py
@@ -148,12 +148,9 @@ def make_expr_group(*exprs, real_func_arg=False):
             exprs = mat.entries
     expr_group = _ti_core.ExprGroup()
     for i in exprs:
-        if isinstance(i, Matrix):
-            assert real_func_arg
-            for item in i.entries:
-                expr_group.push_back(Expr(item).ptr)
-        else:
-            expr_group.push_back(Expr(i).ptr)
+        flattened = _get_flattened_ptrs(i)
+        for item in flattened:
+            expr_group.push_back(item)
     return expr_group
 
 

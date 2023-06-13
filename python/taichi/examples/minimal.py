68a9c292b006fd006508eb718ff70edca80dc866
listerily <listerily@outlook.com>
2023-06-13 15:06:49 +0800

    Update on "[lang] Link cpp ArgPackType object to python ArgPackType"
    [ghstack-poisoned]

--- python/taichi/examples/minimal.py
+++ python/taichi/examples/minimal.py
@@ -2,6 +2,9 @@ import taichi as ti
 
 ti.init()
 
+pack_type = ti.types.argpack(a=ti.i32)
+pack_type2 = ti.types.argpack(a=ti.i32, b=ti.types.ndarray(dtype=ti.math.vec3, ndim=2))
+print(pack_type2.dtype)
 
 @ti.kernel
 def p() -> ti.f32:

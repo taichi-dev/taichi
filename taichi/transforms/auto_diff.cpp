ba812c292e212da278dc3cd415afaf9105b6be02
mingrui <mingrui.zhang@taichi.graphics>
2023-06-18 23:47:23 +0800

    [autodiff] Fix loop index not stored

--- taichi/transforms/auto_diff.cpp
+++ taichi/transforms/auto_diff.cpp
@@ -337,7 +337,7 @@ class PromoteSSA2LocalVar : public BasicStmtVisitor {
     if (execute_once_)
       return;
     if (!(stmt->is<UnaryOpStmt>() || stmt->is<BinaryOpStmt>() ||
-          stmt->is<TernaryOpStmt>() || stmt->is<GlobalLoadStmt>() ||
+          stmt->is<TernaryOpStmt>() || stmt->is<GlobalLoadStmt>() || stmt->is<LoopIndexStmt>() ||
           stmt->is<AllocaStmt>())) {
       // TODO: this list may be incomplete
       return;

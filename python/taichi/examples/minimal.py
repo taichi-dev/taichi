33739627179c04ddc2fbff8f459e80d0450b89f6
listerily <listerily@outlook.com>
2023-06-20 14:34:50 +0800

    Update on "[refactor] Make arg_id a vector"
    [ghstack-poisoned]

--- python/taichi/examples/minimal.py
+++ python/taichi/examples/minimal.py
@@ -1,14 +1,12 @@
 import taichi as ti
 
-ti.init(arch=ti.cpu, print_ir=True)
-
-arr_ = ti.ndarray(dtype=ti.math.mat2, shape=(8, 8))
-arr_[0, 0] = ti.math.mat2([[1, 2], [3, 4]])
+ti.init()
 
 
 @ti.kernel
-def p(arr: ti.types.ndarray(dtype=ti.math.mat2, ndim=2)) -> ti.math.mat2:
-    return arr[0, 0]
+def p() -> ti.f32:
+    print(42)
+    return 40 + 2
 
 
-print(p(arr_))
+print(p())

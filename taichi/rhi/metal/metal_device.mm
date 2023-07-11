9ec4cd06370329794b8dfd8476ee38a47f931f01
Antonio Ferreras <antfs10@gmail.com>
2023-07-11 15:44:38 -0400

    Fix undefined property on IOS

--- taichi/rhi/metal/metal_device.mm
+++ taichi/rhi/metal/metal_device.mm
@@ -618,10 +618,10 @@ MetalSurface::MetalSurface(MetalDevice *device, const SurfaceConfig &config)
   layer_.pixelFormat = format2mtl(image_format_);
   layer_.drawableSize = CGSizeMake(width_, height_);
   layer_.allowsNextDrawableTimeout = NO;
-  if ([layer_ respondsToSelector:@selector(displaySyncEnabled)]) {
-    // Older versions may not have this property, so check if it exists first.
-    layer_.displaySyncEnabled = config.vsync;
-  }
+  #if TARGET_OS_MAC
+  // Older versions may not have this property, so check if it exists first.
+  layer_.displaySyncEnabled = config.vsync;
+  #endif
 }
 
 MetalSurface::~MetalSurface() {

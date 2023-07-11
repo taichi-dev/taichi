ff2b052f50f81fc07c53bd32b2b621cb6b5d61ca
Antonio Ferreras <antfs10@gmail.com>
2023-07-11 14:55:04 -0400

    Change

--- taichi/rhi/metal/metal_device.mm
+++ taichi/rhi/metal/metal_device.mm
@@ -618,7 +618,7 @@ MetalSurface::MetalSurface(MetalDevice *device, const SurfaceConfig &config)
   layer_.pixelFormat = format2mtl(image_format_);
   layer_.drawableSize = CGSizeMake(width_, height_);
   layer_.allowsNextDrawableTimeout = NO;
-  if ([CAMetalLayer instancesRespondToSelector:@selector(displaySyncEnabled)]) {
+  if ([layer_ respondsToSelector:@selector(displaySyncEnabled)]) {
     // Older versions may not have this property, so check if it exists first.
     layer_.displaySyncEnabled = config.vsync;
   }

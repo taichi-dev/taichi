cb1579704e8faf2e45a4cc1b0faef701e6de62ee
Antonio Ferreras <antfs10@gmail.com>
2023-07-13 13:31:04 -0400

    Trigger rerun jobs

--- taichi/rhi/metal/metal_device.mm
+++ taichi/rhi/metal/metal_device.mm
@@ -619,7 +619,7 @@ MetalSurface::MetalSurface(MetalDevice *device, const SurfaceConfig &config)
   layer_.drawableSize = CGSizeMake(width_, height_);
   layer_.allowsNextDrawableTimeout = NO;
 #if TARGET_OS_OSX
-  // Older versions may not have this property, so check if it exists first.
+  // Older versions may not have this property so check if it exists first.
   layer_.displaySyncEnabled = config.vsync;
 #endif
 }

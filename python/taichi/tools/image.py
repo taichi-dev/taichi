f8f2688b7a78b19b7084d2ef07adc5cbbb5805ad
Cheng Cao <bobcaocheng@gmail.com>
2023-08-31 14:33:28 -0700

    Fix image read memory leaks

--- python/taichi/tools/image.py
+++ python/taichi/tools/image.py
@@ -3,6 +3,7 @@ from taichi._lib import core as _ti_core
 
 import taichi as ti
 
+import ctypes
 
 def cook_image_to_bytes(img):
     """
@@ -16,7 +17,7 @@ def cook_image_to_bytes(img):
     if img.dtype in [np.uint16, np.uint32, np.uint64]:
         img = (img // (np.iinfo(img.dtype).max // 256)).astype(np.uint8)
     elif img.dtype in [np.float32, np.float64]:
-        img = (np.clip(img, 0, 1) * 255.0 + 0.5).astype(np.uint8)
+        img = (np.clip(img, 0, 1) * 255.0 ).astype(np.uint8)
     elif img.dtype != np.uint8:
         raise ValueError(f"Data type {img.dtype} not supported in ti.tools.imwrite")
 
@@ -81,11 +82,10 @@ def imread(filename, channels=0):
         np.ndarray : An output image loaded from given filename.
     """
     ptr, resx, resy, comp = _ti_core.imread(filename, channels)
-    img = np.ndarray(shape=(resy, resx, comp), dtype=np.uint8)
-    img = np.ascontiguousarray(img)
-    # TODO(archibate): Figure out how np.ndarray constructor works and replace:
-    _ti_core.C_memcpy(img.ctypes.data, ptr, resx * resy * comp)
-    # Discussion: https://github.com/taichi-dev/taichi/issues/802
+    img = np.copy(np.ctypeslib.as_array(
+        (ctypes.c_uint8 * resx * resy * comp).from_address(ptr)
+    )).reshape(resy, resx, comp)
+    _ti_core.imfree(ptr)
     return img.swapaxes(0, 1)[:, ::-1, :]
 
 

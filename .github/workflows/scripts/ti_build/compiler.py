ea4dd51ec6a91d133e8c3d6db93adde911992c66
jim19930609 <jim19930609@gmail.com>
2023-11-22 11:26:52 +0800

    [misc] Downgrade clang version to 14.0.6 on CI

--- .github/workflows/scripts/ti_build/compiler.py
+++ .github/workflows/scripts/ti_build/compiler.py
@@ -37,7 +37,7 @@ def setup_clang(as_compiler=True) -> None:
 
     elif (u.system, u.machine) == ("Windows", "AMD64"):
         out = get_cache_home() / "clang-15-v2"
-        url = "https://github.com/taichi-dev/taichi_assets/releases/download/llvm15/clang-15.0.0-win-complete.zip"
+        url = "https://github.com/taichi-dev/taichi_assets/releases/download/llvm15/clang-14.0.6-win-complete.zip"
         download_dep(url, out, force=True)
         clang = str(out / "bin" / "clang++.exe").replace("\\", "\\\\")
         clangpp = clang

5be70f2f811e248d849cd9bd1432ad972257f2a4
Johnny <johnnynuca14@gmail.com>
2025-01-26 22:52:24 +0100

    update

--- .github/workflows/scripts/ti_build/vulkan.py
+++ .github/workflows/scripts/ti_build/vulkan.py
@@ -12,23 +12,31 @@ from .python import path_prepend
 
 
 # -- code --
-@banner("Setup Vulkan 1.3.236.0")
+@banner("Setup Vulkan 1.4.304.0")
 def setup_vulkan():
     u = platform.uname()
     if u.system == "Linux":
-        url = "https://sdk.lunarg.com/sdk/download/1.3.236.0/linux/vulkansdk-linux-x86_64-1.3.236.0.tar.gz"
-        prefix = get_cache_home() / "vulkan-1.3.236.0"
+        url = "https://sdk.lunarg.com/sdk/download/1.4.304.0/linux/vulkansdk-linux-x86_64-1.4.304.0.tar.xz"
+        prefix = get_cache_home() / "vulkan-1.4.304.0"
         download_dep(url, prefix, strip=1)
         sdk = prefix / "x86_64"
         os.environ["VULKAN_SDK"] = str(sdk)
         path_prepend("PATH", sdk / "bin")
         path_prepend("LD_LIBRARY_PATH", sdk / "lib")
         os.environ["VK_LAYER_PATH"] = str(sdk / "etc" / "vulkan" / "explicit_layer.d")
-    # elif (u.system, u.machine) == ("Darwin", "arm64"):
+    elif (u.system, u.machine) == ("Linux", "arm64"):
+        url = "https://github.com/johnnynunez/vulkan-sdk-arm/actions/runs/12977374812/artifacts/2487515131"
+        prefix = get_cache_home() / "vulkan-1.4.304.0"
+        download_dep(url, prefix, strip=1)
+        sdk = prefix / "arm64"
+        os.environ["VULKAN_SDK"] = str(sdk)
+        path_prepend("PATH", sdk / "bin")
+        path_prepend("LD_LIBRARY_PATH", sdk / "lib")
+        os.environ["VK_LAYER_PATH"] = str(sdk / "etc" / "vulkan" / "explicit_layer.d")
     # elif (u.system, u.machine) == ("Darwin", "x86_64"):
     elif (u.system, u.machine) == ("Windows", "AMD64"):
-        url = "https://sdk.lunarg.com/sdk/download/1.3.236.0/windows/VulkanSDK-1.3.236.0-Installer.exe"
-        prefix = get_cache_home() / "vulkan-1.3.236.0"
+        url = "https://sdk.lunarg.com/sdk/download/1.4.304.0/windows/VulkanSDK-1.4.304.0-Installer.exe"
+        prefix = get_cache_home() / "vulkan-1.4.304.0"
         download_dep(
             url,
             prefix,

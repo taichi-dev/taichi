a34aad75406bcfb5fb35f046c095dcc304cfc042
johnnynunez <johnnynuca14@gmail.com>
2025-03-06 01:21:54 +0100

    revert

--- taichi/ir/expression_printer.h
+++ taichi/ir/expression_printer.h
@@ -5,7 +5,6 @@
 #include "taichi/ir/frontend_ir.h"
 #include "taichi/program/program.h"
 #include "taichi/analysis/offline_cache_util.h"
-#include "spdlog/fmt/ranges.h"
 
 namespace taichi::lang {
 

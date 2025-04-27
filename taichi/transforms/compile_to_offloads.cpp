0350c414e15f0297895a1f8c8d9e7783f1c4c3cc
Hugh Perkins <hughperkins@gmail.com>
2025-04-27 09:25:35 -0400

    remove redundant cfg optimization, to fix https://github.com/taichi-dev/taichi/issues/8675

--- taichi/transforms/compile_to_offloads.cpp
+++ taichi/transforms/compile_to_offloads.cpp
@@ -140,16 +140,6 @@ void compile_to_offloads(IRNode *ir,
   print("Offloaded");
   irpass::analysis::verify(ir);
 
-  // TODO: This pass may be redundant as cfg_optimization() is already called
-  //  in full_simplify().
-  if (config.opt_level > 0 && config.cfg_optimization) {
-    irpass::cfg_optimization(
-        ir, false, /*autodiff_enabled*/ false,
-        !config.real_matrix_scalarize && !config.force_scalarize_matrix);
-    print("Optimized by CFG");
-    irpass::analysis::verify(ir);
-  }
-
   irpass::flag_access(ir);
   print("Access flagged II");
 

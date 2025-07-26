628a8f94ad7d4f34ed388f4ab63f1a53910c9d1f
Johnny <johnnync13@gmail.com>
2025-07-26 11:30:38 +0200

    [build] Update sccache download URLs to version 0.10.0 for various plat… (#8717)
    upgrade sscache
    
    ---------
    
    Co-authored-by: Proton <feisuzhu@163.com>

--- .github/workflows/scripts/ti_build/sccache.py
+++ .github/workflows/scripts/ti_build/sccache.py
@@ -19,7 +19,7 @@ def setup_sccache() -> Command:
     """
     Download and install sccache, setup compiler wrappers, and return the `sccache` command.
     """
-    root = get_cache_home() / "sccache-v041"
+    root = get_cache_home() / "sccache-v0-10-0"
     bin = root / "bin"
 
     u = platform.uname()
@@ -31,18 +31,16 @@ def setup_sccache() -> Command:
         raise RuntimeError(f"Unsupported platform: {u.system} {u.machine}")
 
     if not exe.exists():
-        if u.system == "Linux":
-            url = "https://github.com/mozilla/sccache/releases/download/v0.4.1/sccache-v0.4.1-x86_64-unknown-linux-musl.tar.gz"
+        if (u.system, u.machine) == ("Linux", "x86_64"):
+            url = "https://github.com/mozilla/sccache/releases/download/v0.10.0/sccache-v0.10.0-x86_64-unknown-linux-musl.tar.gz"
+        elif (u.system, u.machine) in (("Linux", "arm64"), ("Linux", "aarch64")):
+            url = "https://github.com/mozilla/sccache/releases/download/v0.10.0/sccache-v0.10.0-aarch64-unknown-linux-musl.tar.gz"
         elif (u.system, u.machine) == ("Darwin", "arm64"):
-            url = (
-                "https://github.com/mozilla/sccache/releases/download/v0.4.1/sccache-v0.4.1-aarch64-apple-darwin.tar.gz"
-            )
+            url = "https://github.com/mozilla/sccache/releases/download/v0.10.0/sccache-v0.10.0-aarch64-apple-darwin.tar.gz"
         elif (u.system, u.machine) == ("Darwin", "x86_64"):
-            url = (
-                "https://github.com/mozilla/sccache/releases/download/v0.4.1/sccache-v0.4.1-x86_64-apple-darwin.tar.gz"
-            )
+            url = "https://github.com/mozilla/sccache/releases/download/v0.10.0/sccache-v0.10.0-x86_64-apple-darwin.tar.gz"
         elif u.system == "Windows":
-            url = "https://github.com/mozilla/sccache/releases/download/v0.4.1/sccache-v0.4.1-x86_64-pc-windows-msvc.tar.gz"
+            url = "https://github.com/mozilla/sccache/releases/download/v0.10.0/sccache-v0.10.0-x86_64-pc-windows-msvc.tar.gz"
         else:
             raise RuntimeError(f"Unsupported platform: {u.system} {u.machine}")
 

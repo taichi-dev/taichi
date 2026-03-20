cb1f0278e58a5b90c120505b12883f833bf55758
Yunus Berndt <yunus.berndt@gmx.net>
2026-03-20 20:28:31 +1100

    [Lang] Export polynomial APIs in taichi.math

--- python/taichi/math/__init__.py
+++ python/taichi/math/__init__.py
@@ -5,5 +5,5 @@ The math module supports glsl-style vectors, matrices and functions.
 
 from ._complex import *
 from .mathimpl import *  # pylint: disable=W0622
-
+from .polynomial import *
 del mathimpl
